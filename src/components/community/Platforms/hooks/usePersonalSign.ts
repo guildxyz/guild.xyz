ab72efd747ffcb37e47395f692088dd1076040f6
valid <vali.domokos@gmail.com>
2021-06-16 02:30:18 +0200

    switched to ethers's sign message function

--- src/components/community/Platforms/hooks/usePersonalSign.ts
+++ src/components/community/Platforms/hooks/usePersonalSign.ts
@@ -3,14 +3,11 @@ import { useWeb3React } from "@web3-react/core"
 
 type SignErrorType = { code: number; message: string }
 
-const hexlify = (message: string): string =>
-  `0x${Buffer.from(message, "utf8").toString("hex")}`
-
 const usePersonalSign = (): ((message: string) => Promise<any>) => {
   const { library, account } = useWeb3React<Web3Provider>()
 
   return async (message: string): Promise<any> =>
-    library.send("personal_sign", [hexlify(message), account])
+    library.getSigner(account).signMessage(message)
 }
 
 export type { SignErrorType }
