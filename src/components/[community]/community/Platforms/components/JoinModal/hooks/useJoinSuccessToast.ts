a114564f5c483a37fa0433cc7a4a9906323ff47f
valid <vali.domokos@gmail.com>
2021-08-18 03:25:04 +0200

    don't show join success toast on account change

--- src/components/[community]/community/Platforms/components/JoinModal/hooks/useJoinSuccessToast.ts
+++ src/components/[community]/community/Platforms/components/JoinModal/hooks/useJoinSuccessToast.ts
@@ -1,16 +1,36 @@
 import { usePrevious, useToast } from "@chakra-ui/react"
+import { useWeb3React } from "@web3-react/core"
 import platformsContent from "components/[community]/community/Platforms/platformsContent"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import useIsMember from "../../PlatformButton/hooks/useIsMember"
 
 const useJoinSuccessToast = (platform: string) => {
+  const { account } = useWeb3React()
   const isMember = useIsMember(platform)
   const prevIsMember = usePrevious(isMember)
   const toast = useToast()
+  const [prevAccount, setPrevAccount] = useState(account)
 
   useEffect(() => {
-    // the last condition would be enough just for the changes, we need the first two to handle mount
-    if (prevIsMember === undefined || prevIsMember === true || !isMember) return null
+    /**
+     * Since it's a state left out of the dependency array, prevAccount will only
+     * have this newly set value in the next cycle. Not using usePrevious for this
+     * because we want to tie it to this useEffect's lifecycle, not account's. That
+     * way it'd always be different from the current account, not just for the first
+     * time on account change
+     */
+    setPrevAccount(account)
+
+    if (
+      // skip when isMember first gets value after mount
+      prevIsMember === undefined ||
+      // skip if isMember was already true and the effect runs because of something else
+      prevIsMember === true ||
+      // skip if isMember has changed because of account change
+      prevAccount !== account ||
+      !isMember
+    )
+      return null
 
     toast({
       title: `Successfully joined ${platformsContent[platform].title}`,
@@ -23,7 +43,8 @@ const useJoinSuccessToast = (platform: string) => {
       variant: "toastSubtle",
       isClosable: true,
     })
-  }, [isMember, platform, toast]) // intentionally leaving the ref out
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isMember, account, platform, toast]) // intentionally leaving prevIsMember and prevAccount out
 }
 
 export default useJoinSuccessToast
