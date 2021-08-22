3bba6afc613d1c840bba8839628cccb33db3475f
Balázs Csete <csetebalu@gmail.com>
2021-08-22 11:43:02 +0200

    hashing with hashUserId instead of hmac

--- src/pages/api/hash.ts
+++ src/pages/api/hash.ts
@@ -1,13 +1,11 @@
-import { createHmac } from "crypto"
 import type { NextApiRequest, NextApiResponse } from "next"
-
-const hmac = createHmac(process.env.HMAC_ALGORITHM, process.env.HMAC_SECRET)
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const { data } = req.body
   try {
-    hmac.update(data)
-    const hashed = hmac.digest("base64")
+    const response = await fetch(`${process.env.DC_RUNNER_API}/hashUserId/${data}`)
+    if (!response.ok) throw new Error("Network Error")
+    const hashed = await response.json()
     res.status(200).json({ hashed })
   } catch {
     res.status(400).json({ error: "Failed to hash data" })
