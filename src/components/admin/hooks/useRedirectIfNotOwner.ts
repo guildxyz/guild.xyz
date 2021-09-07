08fb77eda8f7b81f6e72d06b4a731650ce0fff40
Balázs Csete <csetebalu@gmail.com>
2021-09-08 00:08:00 +0200

    removed useMemo, fixed conditions

--- src/components/admin/hooks/useRedirectIfNotOwner.ts
+++ src/components/admin/hooks/useRedirectIfNotOwner.ts
@@ -1,27 +1,27 @@
 import { useWeb3React } from "@web3-react/core"
 import { useRouter } from "next/router"
-import { useEffect, useMemo } from "react"
+import { useEffect } from "react"
 import useCommunityData from "./useCommunityData"
 
 const useRedirectIfNotOwner = () => {
   const { account, active } = useWeb3React()
   const router = useRouter()
   const { communityData } = useCommunityData()
-  const isOwner = useMemo(
-    () =>
-      communityData?.owner?.addresses?.some(
-        ({ address }) => address === account?.toLowerCase()
-      ),
-    [communityData, account]
-  )
-  const redirectUrl = `/${communityData?.urlName}`
 
   useEffect(() => {
-    if (typeof account === "string" && !isOwner) router.push(redirectUrl)
-  }, [account, isOwner, router, redirectUrl])
+    if (
+      typeof account === "string" &&
+      !communityData?.owner?.addresses?.some(
+        ({ address }) => address === account?.toLowerCase()
+      )
+    )
+      router.push(`/${communityData?.urlName}`)
+  }, [account, communityData, router])
 
   if (!active) return false
-  return isOwner
+  return communityData?.owner?.addresses?.some(
+    ({ address }) => address === account?.toLowerCase()
+  )
 }
 
 export default useRedirectIfNotOwner
