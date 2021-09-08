61836636f692b80e3bd6d55bc6cac2c972e028b4
KovJonas <kovacsjonas99@gmail.com>
2021-09-08 14:23:43 +0200

    Add guild page base

---
+++ src/pages/add-guild.tsx
@@ -0,0 +1,11 @@
+import Layout from "components/common/Layout"
+
+const AddGuildPage = (): JSX.Element => {
+  return (
+    <Layout title="Add guild">
+      TODO...
+    </Layout>
+  )
+}
+
+export default AddGuildPage
