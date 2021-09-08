a7f3f71beb99a9b3321e01149ee63258ff20df3c
valid <vali.domokos@gmail.com>
2021-09-08 23:31:56 +0200

    build fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,7 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-import {
-  Button,
-  Link,
-} from "@chakra-ui/react"
 import { SimpleGrid } from "@chakra-ui/layout"
+import { Button, Link } from "@chakra-ui/react"
 import { useWeb3React } from "@web3-react/core"
-import AddCard from "components/common/AddCard"
 import Layout from "components/common/Layout"
 import GuildCard from "components/index/GuildCard"
 import React from "react"
@@ -15,25 +11,22 @@ const Page = (): JSX.Element => {
   const { account } = useWeb3React()
 
   return (
-    <Layout title="Guild.xyz"
-            description="A place for Web3 guilds"
-
-        action={
-          <Link 
-            href={"/create-guild"}
-            _hover={{
+    <Layout
+      title="Guild.xyz"
+      description="A place for Web3 guilds"
+      action={
+        <Link
+          href="/create-guild"
+          _hover={{
             textDecor: "none",
-            }}
-          >
-          <Button
-            rounded="2xl"
-            colorScheme="green"
-          >
+          }}
+        >
+          <Button rounded="2xl" colorScheme="green">
             Create Guild
           </Button>
-          </Link>
-          }
-          >
+        </Link>
+      }
+    >
       <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} spacing={{ base: 5, md: 6 }}>
         {guilds.map((guild) => (
           <GuildCard key={guild.id} guildData={guild} />
