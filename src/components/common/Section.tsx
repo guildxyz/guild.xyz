07c1c3be4aba3f98b95d9b826eb7e5417b6858f9
KovJonas <kovacsjonas99@gmail.com>
2021-09-08 15:29:10 +0200

    General Section component

---
+++ src/components/common/Section.tsx
@@ -0,0 +1,20 @@
+import { Box, Heading, Stack } from "@chakra-ui/react"
+import { PropsWithChildren } from "react"
+
+type Props = {
+  title: string
+}
+
+const Section = ({ title, children }: PropsWithChildren<Props>): JSX.Element => (
+  <Stack spacing={5}>
+    <Heading fontSize={{ base: "md", sm: "lg" }} as="h2">
+      {title}
+    </Heading>
+
+    <Box>
+      {children}
+    </Box>
+  </Stack>
+)
+
+export default Section
