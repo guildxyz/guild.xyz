7741d40ea1fe48b92e57fd4e91f51b8d7a0ef585
KovJonas <kovacsjonas99@gmail.com>
2021-09-08 19:45:38 +0200

    Hide join guild button

--- src/pages/[guild]/index.tsx
+++ src/pages/[guild]/index.tsx
@@ -1,4 +1,5 @@
 import { Button, VStack } from "@chakra-ui/react"
+import { useWeb3React } from "@web3-react/core"
 import Layout from "components/common/Layout"
 import Section from "components/common/Section"
 import RequirementCard from "components/[guild]/RequirementCard"
@@ -8,25 +9,31 @@ type Props = {
   guildData: Guild
 }
 
-const GuildPage = ({ guildData }: Props): JSX.Element => (
-  <Layout
-    title={guildData.name}
-    action={
-      <Button rounded="2xl" colorScheme="green">
-        Join guild
-      </Button>
-    }
-  >
-    <Section title="Requirements">
-      <VStack maxWidth="sm" spacing={{ base: 5, md: 6 }}>
-        {guildData.requirements.map((requirement, i) => (
-          // eslint-disable-next-line react/no-array-index-key
-          <RequirementCard key={i} requirement={requirement} />
-        ))}
-      </VStack>
-    </Section>
-  </Layout>
-)
+const GuildPage = ({ guildData }: Props): JSX.Element => {
+  const { account } = useWeb3React()
+
+  return (
+    <Layout
+      title={guildData.name}
+      action={
+        account && (
+          <Button rounded="2xl" colorScheme="green">
+            Join guild
+          </Button>
+        )
+      }
+    >
+      <Section title="Requirements">
+        <VStack maxWidth="sm" spacing={{ base: 5, md: 6 }}>
+          {guildData.requirements.map((requirement, i) => (
+            // eslint-disable-next-line react/no-array-index-key
+            <RequirementCard key={i} requirement={requirement} />
+          ))}
+        </VStack>
+      </Section>
+    </Layout>
+  )
+}
 
 export async function getServerSideProps({ params }) {
   const { guild: guildUrlName } = params
