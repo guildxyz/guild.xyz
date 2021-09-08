9c9170651c95e7de66262907f6200b7fe820711a
valid <vali.domokos@gmail.com>
2021-09-08 13:33:05 +0200

    relevant LogoWithMenu

--- src/components/common/Layout/components/LogoWithMenu/LogoWithMenu.tsx
+++ src/components/common/Layout/components/LogoWithMenu/LogoWithMenu.tsx
@@ -1,5 +1,4 @@
 import {
-  Icon,
   IconButton,
   Menu,
   MenuButton,
@@ -7,74 +6,46 @@ import {
   MenuItem,
   MenuList,
 } from "@chakra-ui/react"
-import { useRouter } from "next/dist/client/router"
-import Link from "next/link"
-import { ArrowLeft, Code, Info, MagnifyingGlass } from "phosphor-react"
+import { Code, Info } from "phosphor-react"
 import Logo from "./components/Logo"
 
-const LogoWithMenu = () => {
-  const router: any = useRouter()
-
-  if (router.route === "/" || !router.components?.["/"]) {
-    return (
-      <Menu>
-        <MenuButton
-          as={IconButton}
-          aria-label="Agora logo"
-          variant="ghost"
-          isRound
-          width={10}
-          height={10}
+const LogoWithMenu = () => (
+  <Menu>
+    <MenuButton
+      as={IconButton}
+      aria-label="Agora logo"
+      variant="ghost"
+      isRound
+      width={10}
+      height={10}
+    >
+      <Logo width="1m" height="1em" />
+    </MenuButton>
+    <MenuList border="none" shadow="md">
+      <MenuGroup title="Powered by agora.space" pb="2">
+        <MenuItem
+          py="2"
+          as="a"
+          target="_blank"
+          href="https://agora.space/"
+          rel="noopener"
+          icon={<Info />}
         >
-          <Logo width="1m" height="1em" />
-        </MenuButton>
-        {/* have to set zIndex, otherwise the search bar's icon lays over it */}
-        <MenuList border="none" shadow="md" zIndex="3">
-          <MenuGroup title="Powered by agora.space" pb="2">
-            <Link href="/" passHref>
-              <MenuItem py="2" as="a" icon={<MagnifyingGlass />}>
-                Explorer
-              </MenuItem>
-            </Link>
-            <MenuItem
-              py="2"
-              as="a"
-              target="_blank"
-              href="https://agora.space/"
-              rel="noopener"
-              icon={<Info />}
-            >
-              About
-            </MenuItem>
-            <MenuItem
-              py="2"
-              as="a"
-              target="_blank"
-              href="https://github.com/AgoraSpaceDAO"
-              rel="noopener"
-              icon={<Code />}
-            >
-              Code
-            </MenuItem>
-          </MenuGroup>
-        </MenuList>
-      </Menu>
-    )
-  }
-
-  return (
-    <Link href="/" passHref>
-      <IconButton
-        as="a"
-        aria-label="Back to explorer"
-        variant="ghost"
-        isRound
-        icon={<Icon as={ArrowLeft} />}
-        width={10}
-        height={10}
-      />
-    </Link>
-  )
-}
+          About
+        </MenuItem>
+        <MenuItem
+          py="2"
+          as="a"
+          target="_blank"
+          href="https://github.com/AgoraSpaceDAO/guild.xyz"
+          rel="noopener"
+          icon={<Code />}
+        >
+          Code
+        </MenuItem>
+      </MenuGroup>
+    </MenuList>
+  </Menu>
+)
 
 export default LogoWithMenu
