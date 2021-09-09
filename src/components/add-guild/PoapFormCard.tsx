a3d8747f5d0ef7f32cc4716d192d99b78a891c25
KovJonas <kovacsjonas99@gmail.com>
2021-09-09 21:21:59 +0200

    PoapFormCard base

---
+++ src/components/add-guild/PoapFormCard.tsx
@@ -0,0 +1,50 @@
+import { useColorMode, VStack } from "@chakra-ui/react"
+import Card from "components/common/Card"
+import { useFormContext } from "react-hook-form"
+import { HoldTypeColors } from "temporaryData/types"
+
+type Props = {
+  index: number
+}
+
+const PoapFormCard = ({ index }: Props): JSX.Element => {
+  const {
+    register,
+    getValues,
+    formState: { errors },
+  } = useFormContext()
+  const holdType = getValues(`requirements.${index}.holdType`)
+
+  const { colorMode } = useColorMode()
+
+  return (
+    <Card
+      role="group"
+      position="relative"
+      px={{ base: 5, sm: 7 }}
+      py="7"
+      w="full"
+      bg={colorMode === "light" ? "white" : "gray.700"}
+      borderWidth={2}
+      borderColor={HoldTypeColors[holdType]}
+      overflow="visible"
+      _before={{
+        content: `""`,
+        position: "absolute",
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+        bg: "primary.300",
+        opacity: 0,
+        transition: "opacity 0.2s",
+      }}
+    >
+      <VStack spacing={4} alignItems="start">
+        PoapFormCard
+      </VStack>
+    </Card>
+  )
+}
+
+export default PoapFormCard
