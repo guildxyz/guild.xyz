14bb93c575e9a3a8d5a54df5de67036361e41858
KovJonas <kovacsjonas99@gmail.com>
2021-09-11 20:50:48 +0200

    Coming soon!

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,35 +1,50 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { SimpleGrid } from "@chakra-ui/layout"
-import { Button } from "@chakra-ui/react"
-import Layout from "components/common/Layout"
-import Link from "components/common/Link"
-import GuildCard from "components/index/GuildCard"
+import { HStack, Text } from "@chakra-ui/layout"
+import Head from "next/head"
 import React from "react"
-import guilds from "temporaryData/guilds"
 
 const Page = (): JSX.Element => (
-  <Layout
-    title="Guildhall"
-    description="A place for Web3 guilds"
-    action={
-      <Link
-        href="/create-guild"
-        _hover={{
-          textDecor: "none",
-        }}
-      >
-        <Button rounded="2xl" colorScheme="green">
-          Create Guild
-        </Button>
-      </Link>
-    }
-  >
-    <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} spacing={{ base: 5, md: 6 }}>
-      {guilds.map((guild) => (
-        <GuildCard key={guild.id} guildData={guild} />
-      ))}
-    </SimpleGrid>
-  </Layout>
+  <>
+    <Head>
+      <title>Guildhall</title>
+      <meta property="og:title" content="Guildhall" />
+      <meta name="description" content="A place for Web3 guilds" />
+      <meta property="og:description" content="A place for Web3 guilds" />
+    </Head>
+    <HStack
+      bgColor="gray.800"
+      minHeight="100vh"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Text fontFamily="display" fontSize="4xl" textAlign="center" fontWeight="bold">
+        Coming soon!
+      </Text>
+    </HStack>
+  </>
 )
+
+// <Layout
+//   title="Guildhall"
+//   description="A place for Web3 guilds"
+//   action={
+//     <Link
+//       href="/create-guild"
+//       _hover={{
+//         textDecor: "none",
+//       }}
+//     >
+//       <Button rounded="2xl" colorScheme="green">
+//         Create Guild
+//       </Button>
+//     </Link>
+//   }
+// >
+//   <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} spacing={{ base: 5, md: 6 }}>
+//     {guilds.map((guild) => (
+//       <GuildCard key={guild.id} guildData={guild} />
+//     ))}
+//   </SimpleGrid>
+// </Layout>
 
 export default Page
