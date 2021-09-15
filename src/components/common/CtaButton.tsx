2e2d8a0aac73e43e391e62ae3a6ebba65ec5d7ce
valid <vali.domokos@gmail.com>
2021-09-16 01:11:16 +0200

    CtaButton added forwardRef to fix console error

--- src/components/common/CtaButton.tsx
+++ src/components/common/CtaButton.tsx
@@ -1,39 +1,48 @@
 import { Button, useBreakpointValue, useColorMode } from "@chakra-ui/react"
 import Card from "components/common/Card"
-import { PropsWithChildren } from "react"
+import { forwardRef, PropsWithChildren } from "react"
 
-const CtaButton = ({ children, ...rest }: PropsWithChildren<any>): JSX.Element => {
-  const { colorMode } = useColorMode()
-  const isMobile = useBreakpointValue({ base: true, md: false })
+const CtaButton = forwardRef(
+  ({ children, ...rest }: PropsWithChildren<any>, ref): JSX.Element => {
+    const { colorMode } = useColorMode()
+    const isMobile = useBreakpointValue({ base: true, md: false })
 
-  if (isMobile) {
+    if (isMobile) {
+      return (
+        <Card
+          ml="auto"
+          position="fixed"
+          left={0}
+          bottom={0}
+          width="100vw"
+          background={colorMode === "light" ? "gray.700" : "gray.600"}
+          borderTop="1px"
+          borderTopColor={colorMode === "light" ? "gray.100" : "gray.500"}
+          borderRadius="none"
+          zIndex="docked"
+          style={{
+            backdropFilter: "blur(10px)",
+          }}
+        >
+          <Button
+            variant="ghost"
+            flexGrow={1}
+            h={14}
+            borderRadius="0"
+            ref={ref}
+            {...rest}
+          >
+            {children}
+          </Button>
+        </Card>
+      )
+    }
     return (
-      <Card
-        ml="auto"
-        position="fixed"
-        left={0}
-        bottom={0}
-        width="100vw"
-        background={colorMode === "light" ? "gray.700" : "gray.600"}
-        borderTop="1px"
-        borderTopColor={colorMode === "light" ? "gray.100" : "gray.500"}
-        borderRadius="none"
-        zIndex="docked"
-        style={{
-          backdropFilter: "blur(10px)",
-        }}
-      >
-        <Button variant="ghost" flexGrow={1} h={14} borderRadius="0" {...rest}>
-          {children}
-        </Button>
-      </Card>
+      <Button rounded="2xl" colorScheme="green" ref={ref} {...rest}>
+        {children}
+      </Button>
     )
   }
-  return (
-    <Button rounded="2xl" colorScheme="green" {...rest}>
-      {children}
-    </Button>
-  )
-}
+)
 
 export default CtaButton
