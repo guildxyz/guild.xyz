b5142cc4f56ab2580ba97d07474a575ede6a93a5
valid <vali.domokos@gmail.com>
2021-09-18 19:36:01 +0200

    etherscan links for token requirements

--- src/components/[guild]/RequirementCard/RequirementCard.tsx
+++ src/components/[guild]/RequirementCard/RequirementCard.tsx
@@ -1,4 +1,4 @@
-import { Text } from "@chakra-ui/react"
+import { Link, Text } from "@chakra-ui/react"
 import ColorCard from "components/common/ColorCard"
 import useTokenData from "hooks/useTokenData"
 import { nfts } from "temporaryData/nfts"
@@ -13,33 +13,40 @@ const RequirementCard = ({ requirement }: Props): JSX.Element => {
     data: [tokenName, tokenSymbol],
   } = useTokenData(requirement.address)
 
-  let cardTitle = ""
-
-  switch (requirement.type) {
-    case "NFT":
-    case "CRYPTOPUNKS":
-    case "BAYC":
-    case "LOOT":
-    case "COOLCATS":
-      cardTitle = `Own a(n) ${nfts.find((_) => _.type === requirement.type).name}`
-      requirement.value &&
-        requirement.data &&
-        (cardTitle += ` with ${requirement.value} ${requirement.data}`)
-      break
-    case "POAP":
-      cardTitle = `Own the ${requirement.value} POAP`
-      break
-    case "TOKEN":
-      cardTitle = `Hold at least ${requirement.value} ${tokenSymbol}`
-      break
-    default:
-      cardTitle = ""
-  }
-
   return (
     <ColorCard color={RequirementTypeColors[requirement.type]}>
       <Text fontWeight="bold" letterSpacing="wide">
-        {cardTitle}
+        {(() => {
+          switch (requirement.type) {
+            case "NFT":
+            case "CRYPTOPUNKS":
+            case "BAYC":
+            case "LOOT":
+            case "COOLCATS":
+              return `Own a(n) ${
+                nfts.find((_) => _.type === requirement.type).name
+              } ${
+                requirement.value &&
+                requirement.data &&
+                `with ${requirement.value} ${requirement.data}`
+              }`
+            case "POAP":
+              return `Own the ${requirement.value} POAP`
+            case "TOKEN":
+              return (
+                <>
+                  {`Hold at least ${requirement.value} `}
+                  <Link
+                    href={`https://etherscan.io/token/${requirement.address}`}
+                    isExternal
+                  >
+                    {tokenSymbol}
+                  </Link>
+                </>
+              )
+            default:
+          }
+        })()}
       </Text>
     </ColorCard>
   )
