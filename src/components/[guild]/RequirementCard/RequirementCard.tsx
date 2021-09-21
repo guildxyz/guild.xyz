35081f64136b76a8f5b7d1ccd81c16b26b94da73
KovJonas <kovacsjonas99@gmail.com>
2021-09-21 17:34:49 +0200

    useNftsList in RequirementCard too

--- src/components/[guild]/RequirementCard/RequirementCard.tsx
+++ src/components/[guild]/RequirementCard/RequirementCard.tsx
@@ -18,38 +18,36 @@ const RequirementCard = ({ requirement }: Props): JSX.Element => {
     <ColorCard color={RequirementTypeColors[requirement.type]}>
       <Text fontWeight="bold" letterSpacing="wide">
         {(() => {
-          switch (requirement.type) {
-            case "NFT":
-            case "CRYPTOPUNKS":
-            case "BAYC":
-            case "LOOT":
-            case "COOLCATS":
-              return `Own a(n) ${
-                nfts?.find((_) => _.type === requirement.type).name
-              } ${
-                requirement.value && requirement.data
-                  ? `with ${requirement.value} ${requirement.data}`
-                  : ""
-              }`
-            case "POAP":
-              return `Own the ${requirement.value} POAP`
-            case "TOKEN":
-              return (
-                <>
-                  {`Hold at least ${requirement.value} `}
-                  <Link
-                    href={`https://etherscan.io/token/${requirement.address}`}
-                    isExternal
-                    title="View on etherscan"
-                  >
-                    {tokenSymbol}
-                  </Link>
-                </>
-              )
-            default:
+          if (nfts?.map((nft) => nft.type).includes(requirement.type)) {
+            return `Own a(n) ${
+              nfts?.find((_) => _.type === requirement.type).name
+            } ${
+              requirement.value && requirement.data
+                ? `with ${requirement.value} ${requirement.data}`
+                : ""
+            }`
+          }
+
+          if (requirement.type === "POAP") return `Own the ${requirement.value} POAP`
+
+          if (requirement.type === "TOKEN") {
+            return (
+              <>
+                {`Hold at least ${requirement.value} `}
+                <Link
+                  href={`https://etherscan.io/token/${requirement.address}`}
+                  isExternal
+                  title="View on etherscan"
+                >
+                  {tokenSymbol}
+                </Link>
+              </>
+            )
           }
         })()}
       </Text>
     </ColorCard>
   )
 }
+
+export default RequirementCard
