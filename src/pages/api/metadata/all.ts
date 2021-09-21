4f62bb35b2ec3ff6098c1e55eaaee4e272dc2cd3
KovJonas <kovacsjonas99@gmail.com>
2021-09-21 17:17:51 +0200

    Revert "Fetching NFT list from the API"
    This reverts commit c09fc602d99b694e3505ed5d63a73537d7b2f0bc.

--- src/pages/api/metadata/all.ts
+++
@@ -1,6 +0,0 @@
-export default async function handler(req, res) {
-  const dataJSON = await fetch(`${process.env.NEXT_PUBLIC_GUILD_API}/metadata/all`)
-  const data = await dataJSON.json()
-
-  res.json(data.map((token) => token.info) || [])
-}
