d9617c8f8bdae6369e293e777f6e7b225ee9e109
KovJonas <kovacsjonas99@gmail.com>
2021-09-22 18:29:07 +0200

    Confirmation alert before deleting a guild

--- src/components/[guild]/JoinButton/DeleteButton.tsx
+++ src/components/[guild]/JoinButton/DeleteButton.tsx
@@ -1,8 +1,17 @@
-import Icon from "@chakra-ui/icon"
+import {
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogOverlay,
+  Button,
+  Icon,
+} from "@chakra-ui/react"
 import { useWeb3React } from "@web3-react/core"
 import ColorButton from "components/common/ColorButton"
 import { TrashSimple } from "phosphor-react"
-import { useMemo } from "react"
+import { useMemo, useRef, useState } from "react"
 import { useGuild } from "../Context"
 
 type Props = {
@@ -14,6 +23,10 @@ const DeleteButton = ({ isLoading = false, onClick }: Props): JSX.Element => {
   const { account } = useWeb3React()
   const { owner } = useGuild()
 
+  const [isAlertOpen, setIsAlertOpen] = useState(false)
+  const onClose = () => setIsAlertOpen(false)
+  const cancelRef = useRef()
+
   const isOwner = useMemo(
     () =>
       owner?.addresses
@@ -24,14 +37,43 @@ const DeleteButton = ({ isLoading = false, onClick }: Props): JSX.Element => {
 
   return (
     isOwner && (
-      <ColorButton
-        color="red.500"
-        rounded="2xl"
-        isLoading={isLoading}
-        onClick={onClick}
-      >
-        <Icon as={TrashSimple} />
-      </ColorButton>
+      <>
+        <ColorButton
+          color="red.500"
+          rounded="2xl"
+          isLoading={isLoading}
+          onClick={() => setIsAlertOpen(true)}
+        >
+          <Icon as={TrashSimple} />
+        </ColorButton>
+
+        <AlertDialog
+          isOpen={isAlertOpen}
+          leastDestructiveRef={cancelRef}
+          onClose={onClose}
+        >
+          <AlertDialogOverlay>
+            <AlertDialogContent>
+              <AlertDialogHeader fontSize="lg" fontWeight="bold">
+                Delete Guild
+              </AlertDialogHeader>
+
+              <AlertDialogBody>
+                Are you sure? You can't undo this action afterwards.
+              </AlertDialogBody>
+
+              <AlertDialogFooter>
+                <Button ref={cancelRef} onClick={onClose}>
+                  Cancel
+                </Button>
+                <Button colorScheme="red" onClick={onClick} ml={3}>
+                  Delete
+                </Button>
+              </AlertDialogFooter>
+            </AlertDialogContent>
+          </AlertDialogOverlay>
+        </AlertDialog>
+      </>
     )
   )
 }
