7198a5082917a71a7634f2b8f08fd9b70b517151
KovJonas <kovacsjonas99@gmail.com>
2021-09-29 10:13:47 +0200

    Fetch NFTs by address & amount form field

--- src/components/create-guild/NftFormCard/NftFormCard.tsx
+++ src/components/create-guild/NftFormCard/NftFormCard.tsx
@@ -1,13 +1,18 @@
 import {
+  Box,
   CloseButton,
   FormControl,
   FormErrorMessage,
   FormLabel,
+  HStack,
   Input,
+  Spinner,
   VStack,
 } from "@chakra-ui/react"
 import Select from "components/common/ChakraReactSelect/ChakraReactSelect"
 import ColorCard from "components/common/ColorCard"
+import useTokenData from "hooks/useTokenData"
+import { useEffect, useMemo } from "react"
 import { useFormContext, useWatch } from "react-hook-form"
 import { RequirementTypeColors } from "temporaryData/types"
 import useNftCustomAttributeNames from "../hooks/useNftCustomAttributeNames"
@@ -22,7 +27,8 @@ const NftFormCard = ({ index, onRemove }: Props): JSX.Element => {
   const {
     register,
     setValue,
-    formState: { errors },
+    trigger,
+    formState: { errors, touchedFields },
   } = useFormContext()
 
   const nfts = useNftsList()
@@ -55,10 +61,37 @@ const NftFormCard = ({ index, onRemove }: Props): JSX.Element => {
   })
   */
 
+  const nftAddress = useWatch({ name: `requirements.${index}.address` })
+  const {
+    data: [nftName, nftSymbol],
+    isValidating: isNftSymbolValidating,
+  } = useTokenData(nftAddress)
+
+  const nftDataFetched = useMemo(
+    () =>
+      typeof nftName === "string" &&
+      nftName.length > 0 &&
+      typeof nftSymbol === "string" &&
+      nftSymbol.length > 0,
+    [nftName, nftSymbol]
+  )
+
+  const onInputChange = (text: string, action: string) => {
+    if (action !== "input-change") return
+    if (text.startsWith("0x")) {
+      setValue(`requirements.${index}.type`, "NFT")
+      setValue(`requirements.${index}.address`, text)
+    }
+  }
+
+  useEffect(() => {
+    if (touchedFields.requirements && touchedFields.requirements[index]?.address)
+      trigger(`requirements.${index}.address`)
+  }, [isNftSymbolValidating, nftDataFetched, trigger, touchedFields])
+
   return (
     <ColorCard color={RequirementTypeColors["NFT"]}>
       {/* logic && shouldShowLogic && <LogicIcon logic={logic} /> */}
-
       {typeof onRemove === "function" && (
         <CloseButton
           position="absolute"
@@ -73,67 +106,150 @@ const NftFormCard = ({ index, onRemove }: Props): JSX.Element => {
         />
       )}
       <VStack spacing={4} alignItems="start">
-        <FormControl isRequired isInvalid={errors?.requirements?.[index]?.type}>
-          <FormLabel>Pick an NFT:</FormLabel>
-          <Select
-            options={nfts?.map((nft) => ({
-              img: nft.info.logoURI, // This will be displayed as an Img tag in the list
-              label: nft.info.name, // This will be displayed as the option text in the list
-              value: nft.info.type, // This will be passed to the hidden input
-            }))}
-            onChange={handleNftSelectChange}
-          />
-          <Input
-            type="hidden"
-            {...register(`requirements.${index}.type`, {
-              required: "This field is required.",
-            })}
-          />
-          <FormErrorMessage>
-            {errors?.requirements?.[index]?.type?.message}
-          </FormErrorMessage>
-        </FormControl>
-        <FormControl isDisabled={!nftCustomAttributeNames?.length}>
-          <FormLabel>Custom attribute:</FormLabel>
-          <Select
-            key={`${pickedNftType}-data-select`}
-            placeholder="Any attribute"
-            options={[""].concat(nftCustomAttributeNames).map((attributeName) => ({
-              label:
-                attributeName.charAt(0).toUpperCase() + attributeName.slice(1) ||
-                "Any attribute",
-              value: attributeName,
-            }))}
-            onChange={handleNftAttributeSelectChange}
-          />
-          <Input type="hidden" {...register(`requirements.${index}.data`)} />
-          <FormErrorMessage>
-            {errors?.requirements?.[index]?.data?.message}
-          </FormErrorMessage>
-        </FormControl>
-        <FormControl
-          isDisabled={!nftCustomAttributeValues?.length}
-          isInvalid={pickedAttribute?.length && errors?.requirements?.[index]?.value}
-        >
-          <FormLabel>Custom attribute value:</FormLabel>
-          <Select
-            key={`${pickedAttribute}-value-select`}
-            placeholder="Any attribute values"
-            options={[""].concat(nftCustomAttributeValues).map((attributeValue) => ({
-              label:
-                attributeValue?.toString().charAt(0).toUpperCase() +
-                  attributeValue?.toString().slice(1) || "Any attribute values",
-              value: attributeValue,
-            }))}
-            onChange={(newValue) =>
-              setValue(`requirements.${index}.value`, newValue.value)
-            }
-          />
-          <Input type="hidden" {...register(`requirements.${index}.value`)} />
-          <FormErrorMessage>
-            {errors?.requirements?.[index]?.value?.message}
-          </FormErrorMessage>
-        </FormControl>
+        {!nftAddress?.length && (
+          <FormControl
+            isRequired={!nftAddress?.length}
+            isInvalid={errors?.requirements?.[index]?.type}
+          >
+            <FormLabel>Pick an NFT:</FormLabel>
+            <Select
+              options={nfts?.map((nft) => ({
+                img: nft.info.logoURI, // This will be displayed as an Img tag in the list
+                label: nft.info.name, // This will be displayed as the option text in the list
+                value: nft.info.type, // This will be passed to the hidden input
+              }))}
+              onInputChange={(text, { action }) => onInputChange(text, action)}
+              onChange={handleNftSelectChange}
+            />
+            <Input
+              type="hidden"
+              {...register(`requirements.${index}.type`, {
+                required: !nftAddress?.length && "This field is required.",
+              })}
+            />
+            <FormErrorMessage>
+              {errors?.requirements?.[index]?.type?.message}
+            </FormErrorMessage>
+          </FormControl>
+        )}
+
+        {nftAddress?.length && (
+          <FormControl
+            isRequired={nftAddress?.length}
+            isInvalid={errors?.requirements?.[index]?.address}
+          >
+            <FormLabel>Pick an NFT:</FormLabel>
+            <HStack maxW="full">
+              {((!errors?.requirements?.[index]?.address &&
+                nftDataFetched &&
+                nftSymbol !== undefined) ||
+                isNftSymbolValidating) && (
+                <Box
+                  bgColor="gray.800"
+                  h={10}
+                  lineHeight={10}
+                  px={2}
+                  mr={1}
+                  borderRadius={6}
+                  fontSize={{ base: "xs", sm: "md" }}
+                  fontWeight="bold"
+                >
+                  {nftSymbol === undefined && isNftSymbolValidating ? (
+                    <HStack px={4} h={10} alignContent="center">
+                      <Spinner size="sm" color="whiteAlpha.400" />
+                    </HStack>
+                  ) : (
+                    nftSymbol
+                  )}
+                </Box>
+              )}
+
+              <Input
+                {...register(`requirements.${index}.address`, {
+                  required: nftAddress?.length && "This field is required.",
+                  pattern: nftAddress?.startsWith("0x") && {
+                    value: /^0x[A-F0-9]{40}$/i,
+                    message:
+                      "Please input a 42 characters long, 0x-prefixed hexadecimal address.",
+                  },
+                })}
+                onChange={() => trigger(`requirements.${index}.address`)}
+              />
+            </HStack>
+            <FormErrorMessage>
+              {errors?.requirements?.[index]?.address?.message}
+            </FormErrorMessage>
+          </FormControl>
+        )}
+
+        {nftAddress?.length ? (
+          <FormControl
+            isRequired={nftAddress?.length}
+            isInvalid={errors?.requirements?.[index]?.amount}
+          >
+            <FormLabel>Amount</FormLabel>
+            <Input
+              type="number"
+              {...register(`requirements.${index}.amount`, {
+                required: nftAddress?.length,
+                min: 1,
+                valueAsNumber: true,
+              })}
+              defaultValue={1}
+            />
+          </FormControl>
+        ) : (
+          <>
+            <FormControl isDisabled={!nftCustomAttributeNames?.length}>
+              <FormLabel>Custom attribute:</FormLabel>
+              <Select
+                key={`${pickedNftType}-data-select`}
+                placeholder="Any attribute"
+                options={[""]
+                  .concat(nftCustomAttributeNames)
+                  .map((attributeName) => ({
+                    label:
+                      attributeName.charAt(0).toUpperCase() +
+                        attributeName.slice(1) || "Any attribute",
+                    value: attributeName,
+                  }))}
+                onChange={handleNftAttributeSelectChange}
+              />
+              <Input type="hidden" {...register(`requirements.${index}.data`)} />
+              <FormErrorMessage>
+                {errors?.requirements?.[index]?.data?.message}
+              </FormErrorMessage>
+            </FormControl>
+            <FormControl
+              isDisabled={!nftCustomAttributeValues?.length}
+              isInvalid={
+                pickedAttribute?.length && errors?.requirements?.[index]?.value
+              }
+            >
+              <FormLabel>Custom attribute value:</FormLabel>
+              <Select
+                key={`${pickedAttribute}-value-select`}
+                placeholder="Any attribute values"
+                options={[""]
+                  .concat(nftCustomAttributeValues)
+                  .map((attributeValue) => ({
+                    label:
+                      attributeValue?.toString().charAt(0).toUpperCase() +
+                        attributeValue?.toString().slice(1) ||
+                      "Any attribute values",
+                    value: attributeValue,
+                  }))}
+                onChange={(newValue) =>
+                  setValue(`requirements.${index}.value`, newValue.value)
+                }
+              />
+              <Input type="hidden" {...register(`requirements.${index}.value`)} />
+              <FormErrorMessage>
+                {errors?.requirements?.[index]?.value?.message}
+              </FormErrorMessage>
+            </FormControl>
+          </>
+        )}
       </VStack>
     </ColorCard>
   )
