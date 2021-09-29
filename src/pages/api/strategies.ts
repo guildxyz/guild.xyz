bbd4c124f608461f07977142bec2094d19cc9c46
valid <vali.domokos@gmail.com>
2021-09-29 17:53:56 +0200

    cleanup

--- src/pages/api/strategies.ts
+++ src/pages/api/strategies.ts
@@ -1,13 +1,6 @@
 export default async function handler(_, res) {
-  let strategies = []
+  const dataJSON = await fetch(`${process.env.NEXT_PUBLIC_GUILD_API}/strategies`)
+  const data = await dataJSON.json()
 
-  try {
-    const dataJSON = await fetch(`${process.env.NEXT_PUBLIC_GUILD_API}/strategies`)
-    const data = await dataJSON.json()
-    strategies = data
-  } catch (_) {
-    strategies = []
-  }
-
-  res.json(strategies)
+  res.json(data || [])
 }
