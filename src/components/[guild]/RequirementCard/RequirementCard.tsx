d91b2c968c419fbef814a96f215a46bea13031c6
KovJonas <kovacsjonas99@gmail.com>
2021-10-01 13:43:16 +0200

    Custom NFT in RequirementCard

--- src/components/[guild]/RequirementCard/RequirementCard.tsx
+++ src/components/[guild]/RequirementCard/RequirementCard.tsx
@@ -1,5 +1,6 @@
 import { Text } from "@chakra-ui/react"
 import ColorCard from "components/common/ColorCard"
+import Link from "components/common/Link"
 import useNftsList from "components/create-guild/NftFormCard/hooks/useNftsList"
 import { Requirement, RequirementTypeColors } from "temporaryData/types"
 import SnapshotStrategy from "./components/SnapshotStrategy"
@@ -26,6 +27,20 @@ const RequirementCard = ({ requirement }: Props): JSX.Element => {
           }
 
           switch (requirement.type) {
+            case "NFT":
+              return (
+                <Text as="span">
+                  {`Own a(n) `}
+                  <Link
+                    href={`https://etherscan.io/token/${requirement.address}`}
+                    isExternal
+                    title="View on Etherscan"
+                  >
+                    {requirement.symbol}
+                  </Link>
+                  {` NFT`}
+                </Text>
+              )
             case "POAP":
               return `Own the ${requirement.value} POAP`
 
