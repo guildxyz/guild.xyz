bbe677aba93bbe594524b81c83c3ffe031112f0c
KovJonas <kovacsjonas99@gmail.com>
2021-10-01 21:03:11 +0200

    Fixed build errors

--- src/components/[guild]/RequirementCard/hooks/useTokenImage.ts
+++ src/components/[guild]/RequirementCard/hooks/useTokenImage.ts
@@ -1,7 +1,7 @@
 import useTokensList from "hooks/useTokensList"
 
 const useTokenImage = (address: string): string => {
-  if (!address.length) return null
+  if (!address?.length) return null
 
   const tokens = useTokensList()
   const foundToken = tokens?.find((token) => token.address === address)
