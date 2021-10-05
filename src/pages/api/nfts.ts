eb7843e2a828e436b620dc1f7373994f866a928a
KovJonas <kovacsjonas99@gmail.com>
2021-10-05 17:49:00 +0200

    Fallback to empty array on API endpoints

--- src/pages/api/nfts.ts
+++ src/pages/api/nfts.ts
@@ -2,5 +2,5 @@ export default async function handler(_, res) {
   const dataJSON = await fetch(`${process.env.NEXT_PUBLIC_GUILD_API}/metadata/info`)
   const data = await dataJSON.json()
 
-  res.json(data || [])
+  res.json(Array.isArray(data) ? data : [])
 }
