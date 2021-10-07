4e8fcd0a79f40bbba246fff1f5e86422cb58b96f
KovJonas <kovacsjonas99@gmail.com>
2021-10-07 10:13:00 +0200

    `/opensea/:address` endpoint

---
+++ src/pages/api/opensea/[address].ts
@@ -0,0 +1,23 @@
+import slugify from "utils/slugify"
+
+export default async function handler(req, res) {
+  const { address } = req.query
+
+  const dataJSON = await fetch(
+    `${process.env.NEXT_PUBLIC_OPENSEA_API}/assets?asset_contract_address=${address}`
+  )
+  const data = await dataJSON.json()
+
+  // Returning it in the same format as we're returning NFTs on the `/metadata/info` API endpoint
+  const nft = data?.assets?.[0]?.asset_contract
+    ? {
+        name: data.assets[0].asset_contract.name,
+        slug: slugify(data.assets[0].asset_contract.name),
+        logoUri: data.assets[0].image_thumbnail_url,
+        address: data.assets[0].asset_contract.address,
+        type: "OPENSEA",
+      }
+    : null
+
+  res.json(nft)
+}
