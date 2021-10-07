a404eb2d44acbb6ab73a50cf8c351b1e7adf24c4
KovJonas <kovacsjonas99@gmail.com>
2021-10-07 22:30:26 +0200

    Proper themeMode control without hidden input

--- src/components/[guild]/EditButton/components/ColorModePicker.tsx
+++ src/components/[guild]/EditButton/components/ColorModePicker.tsx
@@ -2,7 +2,6 @@ import {
   FormControl,
   FormErrorMessage,
   HStack,
-  Input,
   Radio,
   RadioGroup,
   VStack,
@@ -12,28 +11,32 @@ import { useFormContext } from "react-hook-form"
 const ColorModePicker = (): JSX.Element => {
   const {
     register,
-    setValue,
-    watch,
     formState: { errors },
   } = useFormContext()
 
   return (
     <VStack spacing={2} alignItems="start">
       <FormControl isInvalid={errors.themeMode}>
-        <RadioGroup
-          defaultValue="DARK"
-          onChange={(themeMode) => setValue("themeMode", themeMode)}
-        >
+        <RadioGroup defaultValue="DARK" name="themeMode">
           <HStack spacing={4}>
-            <Radio colorScheme="primary" value="DARK">
+            <Radio
+              name="themeMode"
+              {...register("themeMode")}
+              colorScheme="primary"
+              value="DARK"
+            >
               Dark mode
             </Radio>
-            <Radio colorScheme="primary" value="LIGHT">
+            <Radio
+              name="themeMode"
+              {...register("themeMode")}
+              colorScheme="primary"
+              value="LIGHT"
+            >
               Light mode
             </Radio>
           </HStack>
         </RadioGroup>
-        <Input type="hidden" {...register("themeMode")} defaultValue="DARK" />
         <FormErrorMessage>{errors.themeMode?.message}</FormErrorMessage>
       </FormControl>
     </VStack>
