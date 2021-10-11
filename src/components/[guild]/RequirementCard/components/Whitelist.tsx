c47359f24be251ffb618badfb3d3fdc30a3760af
KovJonas <kovacsjonas99@gmail.com>
2021-10-11 19:30:49 +0200

    Better texts on whitelist requirement card

--- src/components/[guild]/RequirementCard/components/Whitelist.tsx
+++ src/components/[guild]/RequirementCard/components/Whitelist.tsx
@@ -5,9 +5,11 @@ import {
   AccordionItem,
   AccordionPanel,
   Box,
+  Divider,
   Table,
   Tbody,
   Td,
+  Text,
   Th,
   Thead,
   Tr,
@@ -18,37 +20,41 @@ type Props = {
 }
 
 const Whitelist = ({ whitelist }: Props): JSX.Element => (
-  <Accordion w="full" allowToggle>
-    <AccordionItem border="none">
-      <AccordionButton px={0} pb={2} _hover={{ bgColor: null }}>
-        <Box flex="1" textAlign="left" fontWeight="bold" fontSize="sm">
-          {whitelist?.length > 0 &&
-            `${whitelist.length} address${whitelist.length > 1 ? "es" : ""}`}
-        </Box>
-        <AccordionIcon />
-      </AccordionButton>
-      <AccordionPanel p={0} overflow="hidden">
-        <Table variant="simple">
-          <Thead>
-            <Tr>
-              <Th pl={0} pr={2} py={1}>
-                Address
-              </Th>
-            </Tr>
-          </Thead>
-          <Tbody fontWeight="normal" fontSize="sm">
-            {whitelist?.map((address) => (
-              <Tr key={address}>
-                <Td px={0} py={0.5}>
-                  {address}
-                </Td>
+  <>
+    <Text>Included in whitelist</Text>
+    <Divider my={4} />
+    <Accordion w="full" allowToggle>
+      <AccordionItem border="none">
+        <AccordionButton px={0} pb={2} _hover={{ bgColor: null }}>
+          <Box flex="1" textAlign="left" fontWeight="bold" fontSize="sm">
+            {whitelist?.length > 0 &&
+              `View ${whitelist.length} address${whitelist.length > 1 ? "es" : ""}`}
+          </Box>
+          <AccordionIcon />
+        </AccordionButton>
+        <AccordionPanel p={0} overflow="hidden">
+          <Table variant="simple">
+            <Thead>
+              <Tr>
+                <Th pl={0} pr={2} py={1}>
+                  Address
+                </Th>
               </Tr>
-            ))}
-          </Tbody>
-        </Table>
-      </AccordionPanel>
-    </AccordionItem>
-  </Accordion>
+            </Thead>
+            <Tbody fontWeight="normal" fontSize="sm">
+              {whitelist?.map((address) => (
+                <Tr key={address}>
+                  <Td px={0} py={0.5}>
+                    {address}
+                  </Td>
+                </Tr>
+              ))}
+            </Tbody>
+          </Table>
+        </AccordionPanel>
+      </AccordionItem>
+    </Accordion>
+  </>
 )
 
 export default Whitelist
