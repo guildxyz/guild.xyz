27ff6313debb5fa08df2b061d16df49fd81c918d
KovJonas <kovacsjonas99@gmail.com>
2021-10-11 14:20:55 +0200

    WhitelistFormCard - custom controller & validator

--- src/components/create-guild/WhitelistFormCard/WhitelistFormCard.tsx
+++ src/components/create-guild/WhitelistFormCard/WhitelistFormCard.tsx
@@ -1,6 +1,13 @@
-import { CloseButton } from "@chakra-ui/react"
+import {
+  CloseButton,
+  FormControl,
+  FormErrorMessage,
+  FormHelperText,
+  FormLabel,
+  Textarea,
+} from "@chakra-ui/react"
 import ColorCard from "components/common/ColorCard"
-import { useFormContext } from "react-hook-form"
+import { Controller, useFormContext, useWatch } from "react-hook-form"
 import { RequirementTypeColors } from "temporaryData/types"
 
 type Props = {
@@ -8,15 +15,20 @@ type Props = {
   onRemove?: () => void
 }
 
+const ADDRESS_REGEX = /^0x[A-F0-9]{40}$/i
+
 const WhitelistFormCard = ({ index, onRemove }: Props): JSX.Element => {
   const {
-    register,
-    setValue,
     getValues,
+    trigger,
     formState: { errors },
+    control,
   } = useFormContext()
 
   const type = getValues(`requirements.${index}.type`)
+  const data = useWatch({ name: `requirements.${index}.data` })
+
+  const validAddress = (address: string) => ADDRESS_REGEX.test(address)
 
   return (
     <ColorCard color={RequirementTypeColors[type]}>
@@ -33,7 +45,36 @@ const WhitelistFormCard = ({ index, onRemove }: Props): JSX.Element => {
           onClick={onRemove}
         />
       )}
-      WIP
+      <FormControl isRequired isInvalid={errors?.requirements?.[index]?.data}>
+        <FormLabel>Whitelisted addresses:</FormLabel>
+        <Controller
+          control={control}
+          name={`requirements.${index}.data`}
+          rules={{
+            required: "This field is required.",
+            validate: () =>
+              data.every(validAddress) || "Please input only valid addresses!",
+          }}
+          render={({ field: { onChange, ref } }) => (
+            <Textarea
+              inputRef={ref}
+              resize="vertical"
+              placeholder="Paste addresses, each one in a new line"
+              onChange={(e) =>
+                onChange(
+                  e.target.value?.split("\n").filter((address) => address !== "")
+                )
+              }
+              onBlur={() => trigger(`requirements.${index}.data`)}
+            />
+          )}
+        />
+
+        <FormHelperText>Paste addresses, each one in a new line</FormHelperText>
+        <FormErrorMessage>
+          {errors?.requirements?.[index]?.data?.message}
+        </FormErrorMessage>
+      </FormControl>
     </ColorCard>
   )
 }
