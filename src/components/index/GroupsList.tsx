1eec931fc85c8d12f83e53764b2f8909afd86f88
KovJonas <kovacsjonas99@gmail.com>
2021-10-12 16:58:52 +0200

    Your groups section & create group button

--- src/components/index/GroupsList.tsx
+++ src/components/index/GroupsList.tsx
@@ -1,11 +1,12 @@
 import { HStack, Tag, Text } from "@chakra-ui/react"
+import AddCard from "components/common/AddCard"
 import { useMemo } from "react"
 import { Group } from "temporaryData/types"
 import CategorySection from "./CategorySection"
 import GroupCard from "./GroupCard"
 
 type Props = {
-  orderedGroups: Group[]
+  orderedGroups: Array<Group>
   searchInput: string
 }
 
@@ -13,7 +14,11 @@ const filterByName = (name: string, searchInput: string) =>
   name.toLowerCase().includes(searchInput.toLowerCase())
 
 const GroupsList = ({ orderedGroups, searchInput }: Props): JSX.Element => {
-  // TODO: userGuilds, filteredUsersGroups
+  // TODO: usersGroups, filteredUsersGroups
+
+  // TEMP
+  const usersGroups: Array<Group> = []
+  const filteredUsersGroups: Array<Group> = []
 
   const filteredGroups = useMemo(
     () => orderedGroups.filter(({ name }) => filterByName(name, searchInput)),
@@ -23,6 +28,21 @@ const GroupsList = ({ orderedGroups, searchInput }: Props): JSX.Element => {
   return (
     <>
       {/* TODO: show user's groups too */}
+      <CategorySection
+        title="You're not part of any guilds yet"
+        fallbackText={`No results for ${searchInput}`}
+      >
+        {usersGroups.length ? (
+          filteredUsersGroups.length &&
+          filteredUsersGroups
+            .map((group) => <GroupCard key={group.id} groupData={group} />)
+            .concat(
+              <AddCard key="create-guild" text="Create guild" link="/create-guild" />
+            )
+        ) : (
+          <AddCard text="Create group" link="/create-group" />
+        )}
+      </CategorySection>
       <CategorySection
         title={
           <HStack spacing={2} alignItems="center">
