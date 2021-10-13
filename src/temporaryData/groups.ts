6f94f03bca04a06150c40a23bb7b4ef12df07bae
KovJonas <kovacsjonas99@gmail.com>
2021-10-13 14:30:51 +0200

    Updated groups temporaryData

--- src/temporaryData/groups.ts
+++ src/temporaryData/groups.ts
@@ -6,17 +6,13 @@ const groups: Array<Group> = [
     id: 1,
     name: "My first group",
     urlName: "my-first-group",
-    guilds: guilds.filter(
-      (guild) => guild.urlName === "johnnytest" || guild.urlName === "deep-space-dao"
-    ),
+    guilds: guilds.filter((guild) => guild.id === 2),
   },
   {
     id: 2,
     name: "Guildhall's group",
     urlName: "guildhalls-group",
-    guilds: guilds.filter(
-      (guild) => guild.urlName === "eth-hold" || guild.urlName === "rekolony"
-    ),
+    guilds: guilds,
   },
 ]
 
