ce301dcdda3512b86f70c1e5c51dc996494228a0
valid <vali.domokos@gmail.com>
2021-10-18 00:27:14 +0200

    fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,32 +1,11 @@
-import type { ExternalProvider, JsonRpcFetchFunc } from "@ethersproject/providers"
-import { Web3Provider } from "@ethersproject/providers"
-import { Web3ReactProvider } from "@web3-react/core"
 import Chakra from "components/_app/Chakra"
-import { Web3ConnectionManager } from "components/_app/Web3ConnectionManager"
 import "focus-visible/dist/focus-visible"
 import type { AppProps } from "next/app"
-import { IconContext } from "phosphor-react"
 import "theme/custom-scrollbar.css"
-
-const getLibrary = (provider: ExternalProvider | JsonRpcFetchFunc) =>
-  new Web3Provider(provider)
 
 const App = ({ Component, pageProps }: AppProps): JSX.Element => (
   <Chakra cookies={pageProps.cookies}>
-    <IconContext.Provider
-      value={{
-        color: "currentColor",
-        size: "1em",
-        weight: "bold",
-        mirrored: false,
-      }}
-    >
-      <Web3ReactProvider getLibrary={getLibrary}>
-        <Web3ConnectionManager>
-          <Component {...pageProps} />
-        </Web3ConnectionManager>
-      </Web3ReactProvider>
-    </IconContext.Provider>
+    <Component {...pageProps} />
   </Chakra>
 )
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,54 +1,7 @@
-import { HStack, Stack, Text } from "@chakra-ui/react"
-import { useWeb3React } from "@web3-react/core"
-import CtaButton from "components/common/CtaButton"
-import Layout from "components/common/Layout"
-import CategorySection from "components/index/CategorySection"
-import GuildCard from "components/index/GuildCard"
-import useUsersGuilds from "components/index/hooks/useUsersGuilds"
-import SearchBar from "components/index/SearchBar"
-import fetchGuilds from "components/index/utils/fetchGuilds"
-import { GetStaticProps } from "next"
+import { HStack, Text } from "@chakra-ui/react"
 import Head from "next/head"
-import NextLink from "next/link"
-import { useMemo, useState } from "react"
-import useSWR from "swr"
-import { Guild } from "temporaryData/types"
 
-type Props = {
-  guilds: Guild[]
-}
-
-const Page = ({ guilds: guildsInitial }: Props): JSX.Element => {
-  const { data: guilds } = useSWR("guilds", fetchGuilds, {
-    fallbackData: guildsInitial,
-  })
-  const { account } = useWeb3React()
-  const usersGuildsIds = useUsersGuilds()
-  const [searchInput, setSearchInput] = useState("")
-
-  const usersGuilds = useMemo(
-    () =>
-      guilds.filter(
-        ({ id, owner: { addresses } }) =>
-          usersGuildsIds.includes(id) ||
-          addresses.map((user) => user.address).includes(account?.toLowerCase())
-      ),
-    [guilds, usersGuildsIds, account]
-  )
-
-  const filterByName = ({ name }: { name: any }): any =>
-    name.toLowerCase().includes(searchInput.toLowerCase())
-
-  const filteredGuilds = useMemo(
-    () => guilds.filter(filterByName),
-    [guilds, searchInput]
-  )
-
-  const filteredUsersGuilds = useMemo(
-    () => usersGuilds.filter(filterByName),
-    [usersGuilds, searchInput]
-  )
-
+const Page = (): JSX.Element => {
   return (
     <>
       <Head>
@@ -75,51 +28,6 @@ const Page = ({ guilds: guildsInitial }: Props): JSX.Element => {
       </HStack>
     </>
   )
-
-  return (
-    <Layout
-      title="Guildhall"
-      description="A place for Web3 guilds"
-      action={
-        <NextLink href="/create-guild" passHref>
-          <CtaButton as="a">Create Guild</CtaButton>
-        </NextLink>
-      }
-    >
-      <SearchBar setSearchInput={setSearchInput} />
-      <Stack spacing={12}>
-        <CategorySection
-          title="Your guilds"
-          fallbackText={
-            usersGuilds.length
-              ? `No results for ${searchInput}`
-              : "You're not part of any guilds yet"
-          }
-        >
-          {filteredUsersGuilds.map((guild) => (
-            <GuildCard key={guild.id} guildData={guild} />
-          ))}
-        </CategorySection>
-        <CategorySection
-          title="All guilds"
-          fallbackText={`No results for ${searchInput}`}
-        >
-          {filteredGuilds.map((guild) => (
-            <GuildCard key={guild.id} guildData={guild} />
-          ))}
-        </CategorySection>
-      </Stack>
-    </Layout>
-  )
-}
-
-export const getStaticProps: GetStaticProps = async () => {
-  const guilds = await fetchGuilds()
-
-  return {
-    props: { guilds },
-    revalidate: 10,
-  }
 }
 
 export default Page
