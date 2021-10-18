be3247ade85fb7aacd34a0f6211e6eee66185e12
KovJonas <kovacsjonas99@gmail.com>
2021-10-18 11:17:00 +0200

    fetchGroup - return type

--- src/components/[group]/utils/fetchGroup.ts
+++ src/components/[group]/utils/fetchGroup.ts
@@ -1,4 +1,6 @@
-export const fetchGroup = (urlName: string) =>
+import { Group } from "temporaryData/types"
+
+export const fetchGroup = (urlName: string): Promise<Group> =>
   fetch(`${process.env.NEXT_PUBLIC_API}/group/urlName/${urlName}`).then(
     (response: Response) => (response.ok ? response.json() : undefined)
   )
