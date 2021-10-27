7f7a5eec91edff5f738ebbccb86403a2aa531e4d
KovJonas <53289941+KovJonas@users.noreply.github.com>
2021-10-27 16:34:21 +0200

    displaying guilds access in halls (#64)

--- src/components/[guild]/JoinButton/hooks/useIsMember.ts
+++ src/components/[guild]/JoinButton/hooks/useIsMember.ts
@@ -1,15 +1,11 @@
 import useUsersGroupsGuilds from "components/index/hooks/useUsersGroupsGuilds"
-import { useGroup } from "components/[group]/Context"
-import { useGuild } from "components/[guild]/Context"
 
-const useIsMember = (): boolean => {
-  const guild = useGuild() || null
-  const group = useGroup() || null
+const useIsMember = (type: "group" | "guild", id: number): boolean => {
   const { usersGroupsIds, usersGuildsIds } = useUsersGroupsGuilds()
 
-  return group
-    ? usersGroupsIds?.includes(group.id)
-    : usersGuildsIds?.includes(guild.id)
+  return type === "group"
+    ? usersGroupsIds?.includes(id)
+    : usersGuildsIds?.includes(id)
 }
 
 export default useIsMember
