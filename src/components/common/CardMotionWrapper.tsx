967d45447f7de790d20ad0a28c275fbd0ce1a1e9
BrickheadJohnny <johnny.zgen@gmail.com>
2021-11-05 10:30:32 +0100

    CardMotionWrapper - improved types

--- src/components/common/CardMotionWrapper.tsx
+++ src/components/common/CardMotionWrapper.tsx
@@ -1,17 +1,18 @@
 import { AnimatePresence, motion } from "framer-motion"
-const CardMotionWrapper = ({ children }): JSX.Element => {
-  return (
-    <AnimatePresence exitBeforeEnter>
-      <motion.div
-        layout
-        initial={{ opacity: 0, scale: 0.5 }}
-        animate={{ opacity: 1, scale: 1 }}
-        exit={{ opacity: 0, scale: 0.5 }}
-      >
-        {children}
-      </motion.div>
-    </AnimatePresence>
-  )
-}
+import { PropsWithChildren } from "react"
+const CardMotionWrapper = ({
+  children,
+}: PropsWithChildren<unknown>): JSX.Element => (
+  <AnimatePresence exitBeforeEnter>
+    <motion.div
+      layout
+      initial={{ opacity: 0, scale: 0.5 }}
+      animate={{ opacity: 1, scale: 1 }}
+      exit={{ opacity: 0, scale: 0.5 }}
+    >
+      {children}
+    </motion.div>
+  </AnimatePresence>
+)
 
 export default CardMotionWrapper
