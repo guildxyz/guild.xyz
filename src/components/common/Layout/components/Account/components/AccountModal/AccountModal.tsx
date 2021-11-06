ad7ee33d0e22bbe1145f8f86e7f40256ce2c2905
valid <valid@zgen.hu>
2021-11-06 19:39:50 +0100

    AccountModal UI & copy improvements

--- src/components/common/Layout/components/Account/components/AccountModal/AccountModal.tsx
+++ src/components/common/Layout/components/Account/components/AccountModal/AccountModal.tsx
@@ -1,8 +1,9 @@
 import {
   Button,
-  CloseButton,
   Heading,
+  HStack,
   Icon,
+  IconButton,
   ModalBody,
   ModalCloseButton,
   ModalContent,
@@ -17,6 +18,7 @@ import {
   Spinner,
   Stack,
   Text,
+  Tooltip,
   VStack,
 } from "@chakra-ui/react"
 import { useWeb3React } from "@web3-react/core"
@@ -25,7 +27,7 @@ import GuildAvatar from "components/common/GuildAvatar"
 import Modal from "components/common/Modal"
 import useUser from "components/[guild]/hooks/useUser"
 import { injected } from "connectors"
-import { Question } from "phosphor-react"
+import { Question, TrashSimple } from "phosphor-react"
 import { useContext } from "react"
 import { Web3Connection } from "../../../../../../_app/Web3ConnectionManager"
 import useUpdateUser from "./hooks/useUpdateUser"
@@ -53,61 +55,16 @@ const AccountModal = ({ isOpen, onClose }) => {
         <ModalHeader>Account</ModalHeader>
         <ModalCloseButton />
         <ModalBody>
-          <Stack mb={12} direction="row" spacing="4" alignItems="center">
+          <Stack mb={9} direction="row" spacing="4" alignItems="center">
             <GuildAvatar address={account} />
             <CopyableAddress address={account} decimals={5} fontSize="2xl" />
           </Stack>
-
-          <Stack direction="row" spacing={2} alignItems="center" mb={4}>
-            <Heading as="h3" fontSize="lg">
-              More authenticated addresses
-            </Heading>
-            <Popover placement="top">
-              <PopoverTrigger>
-                <Icon as={Question} />
-              </PopoverTrigger>
-              <PopoverContent>
-                <PopoverArrow />
-                <PopoverBody>
-                  If you join a guild with another wallet, but with the same Discord
-                  ID, we will link your wallet addresses, and you will see all of
-                  them here.
-                </PopoverBody>
-              </PopoverContent>
-            </Popover>
-          </Stack>
-          <VStack spacing={4} alignItems="start">
-            {!addresses && isLoading ? (
-              <Spinner />
-            ) : addresses?.length > 1 ? (
-              addresses
-                .filter(
-                  (address) => address?.toLowerCase() !== account.toLowerCase()
-                )
-                .map((address) => (
-                  <Stack
-                    key={address}
-                    direction="row"
-                    spacing={4}
-                    alignItems="center"
-                  >
-                    <GuildAvatar address={address} size={6} />
-                    <CopyableAddress address={address} decimals={5} fontSize="md" />
-                    <CloseButton
-                      rounded="full"
-                      onClick={() => removeAddress(address)}
-                    />
-                  </Stack>
-                ))
-            ) : (
-              <Text colorScheme="gray">
-                You do not have any authenticated addresses yet.
-              </Text>
-            )}
-          </VStack>
-        </ModalBody>
-        <ModalFooter>
-          <Stack direction="row" alignItems="center" justifyContent="space-between">
+          <Stack
+            direction="row"
+            alignItems="center"
+            justifyContent="space-between"
+            mb="-1"
+          >
             <Text colorScheme="gray" fontSize="sm" fontWeight="medium">
               Connected with {connector === injected ? "MetaMask" : "WalletConnect"}
             </Text>
@@ -115,7 +72,69 @@ const AccountModal = ({ isOpen, onClose }) => {
               Switch
             </Button>
           </Stack>
-        </ModalFooter>
+        </ModalBody>
+        {addresses?.length && (
+          <ModalFooter bg="gray.800" flexDir="column" pt="10">
+            <Stack direction="row" spacing={2} alignItems="center" mb={7}>
+              <Heading as="h3" fontSize="lg">
+                Linked addresses
+              </Heading>
+              {addresses?.length > 1 && (
+                <Popover placement="top" trigger="hover">
+                  <PopoverTrigger>
+                    <Icon as={Question} />
+                  </PopoverTrigger>
+                  <PopoverContent>
+                    <PopoverArrow />
+                    <PopoverBody>
+                      If you join a guild with another address, but with the same
+                      Discord account, your addresses will be linked together and
+                      each will be used for requirement checks.
+                    </PopoverBody>
+                  </PopoverContent>
+                </Popover>
+              )}
+            </Stack>
+            <VStack spacing={4} alignItems="start" w="full">
+              {!addresses && isLoading ? (
+                <Spinner />
+              ) : addresses?.length > 1 ? (
+                addresses
+                  .filter(
+                    (address) => address?.toLowerCase() !== account.toLowerCase()
+                  )
+                  .map((address) => (
+                    <HStack key={address} spacing={4} alignItems="center" w="full">
+                      <GuildAvatar address={address} size={6} />
+                      <CopyableAddress
+                        address={address}
+                        decimals={5}
+                        fontSize="md"
+                      />
+                      <Tooltip label="Remove address" placement="top" hasArrow>
+                        <IconButton
+                          rounded="full"
+                          variant="ghost"
+                          size="sm"
+                          icon={<Icon as={TrashSimple} />}
+                          colorScheme="red"
+                          ml="auto !important"
+                          onClick={() => removeAddress(address)}
+                          aria-label="Remove address"
+                        />
+                      </Tooltip>
+                    </HStack>
+                  ))
+              ) : (
+                <Text colorScheme="gray">
+                  If you join a guild with another address, but with the same Discord
+                  account, your addresses will be linked together and each will be
+                  used for requirement checks.
+                </Text>
+              )}
+            </VStack>
+          </ModalFooter>
+        )}
       </ModalContent>
     </Modal>
   )
