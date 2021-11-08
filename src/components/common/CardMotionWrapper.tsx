66d0ed569d70f68089e509f84420e33f121dc0c8
valid <valid@zgen.hu>
2021-11-08 13:32:18 +0100

    animation improvements

--- src/components/common/CardMotionWrapper.tsx
+++ src/components/common/CardMotionWrapper.tsx
@@ -1,3 +1,4 @@
+import { EASINGS } from "@chakra-ui/react"
 import { motion } from "framer-motion"
 import { PropsWithChildren } from "react"
 
@@ -5,11 +6,15 @@ const CardMotionWrapper = ({
   children,
 }: PropsWithChildren<unknown>): JSX.Element => (
   <motion.div
-    layout
-    initial={{ opacity: 0, scale: 0.5 }}
+    layout="position"
+    initial={{ opacity: 0, scale: 0.95 }}
     animate={{ opacity: 1, scale: 1 }}
-    exit={{ opacity: 0, scale: 0 }}
-    transition={{ duration: 0.2 }}
+    exit={{
+      opacity: 0,
+      scale: 0.95,
+      transition: { duration: 0.1, ease: EASINGS.easeIn },
+    }}
+    transition={{ duration: 0.2, ease: EASINGS.easeOut }}
   >
     {children}
   </motion.div>
