7fd9b1b2291ec2c670c19e7921f5135f89ea8311
valid <valid@zgen.hu>
2021-11-09 16:39:19 +0100

    animation improvements

--- src/components/common/CardMotionWrapper.tsx
+++ src/components/common/CardMotionWrapper.tsx
@@ -15,13 +15,17 @@ const CardMotionWrapper = ({
     {...(animateOnMount && {
       initial: { opacity: 0, scale: 0.95 },
     })}
-    animate={{ opacity: 1, scale: 1 }}
+    animate={{
+      opacity: 1,
+      scale: 1,
+      transition: { duration: 0.2, ease: EASINGS.easeOut },
+    }}
     exit={{
       opacity: 0,
       scale: 0.95,
       transition: { duration: 0.1, ease: EASINGS.easeIn },
     }}
-    transition={{ duration: 0.2, ease: EASINGS.easeOut }}
+    transition={{ duration: 0.3, ease: EASINGS.easeInOut }}
   >
     {children}
   </motion.div>
