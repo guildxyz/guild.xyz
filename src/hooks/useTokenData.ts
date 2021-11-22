308f9158690e4efc4bc532c1449c3300ded0ed1a
BrickheadJohnny <johnny.zgen@gmail.com>
2021-11-22 12:17:48 +0100

    useTokenData cleanup & handling specific cases

--- src/hooks/useTokenData.ts
+++ src/hooks/useTokenData.ts
@@ -8,20 +8,23 @@ const ENS_ADDRESS = "0x57f1887a8bf19b14fc0df6fd9b2acc9af147ea85"
 const getTokenData = (_: string, chain: string, address: string) =>
   fetchApi(`/guild/symbol/${address}/${chain}`)
 
-const useTokenData = (chain: string, address: string) => {
+const useTokenData = (
+  chain: string,
+  address: string
+): { isValidating: boolean; data: { name: string; symbol: string } } => {
   const shouldFetch = /^0x[A-F0-9]{40}$/i.test(address)
 
   const tokensFromApi = useTokens(chain)
 
-  const tokenDataFromApi = useMemo(
-    () =>
-      address && tokensFromApi
-        ? tokensFromApi.tokens?.find(
-            (token) => token.address?.toLowerCase() === address?.toLowerCase()
-          )
-        : null,
-    [shouldFetch, tokensFromApi, address]
-  )
+  const tokenDataFromApi = useMemo(() => {
+    if (!address || !tokensFromApi) return null
+
+    const lowerCaseAddress = address.toLowerCase()
+    if (lowerCaseAddress === ENS_ADDRESS) return { name: "ENS", symbol: "ENS" }
+    return tokensFromApi.tokens?.find(
+      (token) => token.address?.toLowerCase() === lowerCaseAddress
+    )
+  }, [tokensFromApi, address])
 
   const swrResponse = useSWR<{ name: string; symbol: string }>(
     shouldFetch ? ["tokenData", chain, address] : null,
@@ -40,11 +43,9 @@ const useTokenData = (chain: string, address: string) => {
      * Doing this instead of using initialData to make sure it fetches when
      * shouldFetch becomes true
      */
-    data:
-      (address?.toLowerCase() === ENS_ADDRESS && { name: "ENS", symbol: "ENS" }) ||
-      tokenDataFromApi
-        ? { name: tokenDataFromApi?.name, symbol: tokenDataFromApi?.symbol }
-        : swrResponse.data ?? { name: undefined, symbol: undefined },
+    data: tokenDataFromApi
+      ? { name: tokenDataFromApi?.name, symbol: tokenDataFromApi?.symbol }
+      : swrResponse.data ?? { name: undefined, symbol: undefined },
   }
 }
 
