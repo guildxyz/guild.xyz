7ccddb1435a92c513e5bc6bd9b5951e0c7f52109
BrickheadJohnny <johnny.zgen@gmail.com>
2021-11-24 19:45:51 +0100

    SubmitBUtton - checking requirements properly

--- src/components/create/SubmitButton.tsx
+++ src/components/create/SubmitButton.tsx
@@ -14,6 +14,24 @@ const SubmitButton = ({ type, onErrorHandler }: Props): JSX.Element => {
 
   const { handleSubmit } = useFormContext()
 
+  /**
+   * Removing empty requirements if there are any - this is needed because for some
+   * reason, react-hook-form doesn't always delete all requirements
+   */
+  const onSubmitWithChecks = (data: Record<string, any>) => {
+    if (!data?.requirements) {
+      data.requirements = []
+    } else {
+      data.requirements = data.requirements?.filter(
+        (requirement) =>
+          requirement.type &&
+          (requirement.address || requirement.key || requirement.value)
+      )
+    }
+
+    onSubmit(data)
+  }
+
   const loadingText = (): string => {
     if (isSigning) return "Check your wallet"
     if (isImageLoading) return "Uploading image"
@@ -29,7 +47,7 @@ const SubmitButton = ({ type, onErrorHandler }: Props): JSX.Element => {
       variant="solid"
       isLoading={isLoading || isImageLoading || isSigning}
       loadingText={loadingText()}
-      onClick={handleSubmit(onSubmit, onErrorHandler)}
+      onClick={handleSubmit(onSubmitWithChecks, onErrorHandler)}
     >
       {response ? "Success" : "Summon"}
     </CtaButton>
