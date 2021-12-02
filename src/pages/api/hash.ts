7642d66776aa4f0e57635e9f6755b97d4ce37324
valid <valid@zgen.hu>
2021-12-02 21:06:57 +0100

    using fetcher in hash.ts

--- src/pages/api/hash.ts
+++ src/pages/api/hash.ts
@@ -1,11 +1,10 @@
 import type { NextApiRequest, NextApiResponse } from "next"
+import fetcher from "utils/fetcher"
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const { data } = req.body
   try {
-    const response = await fetch(`${process.env.DC_RUNNER_API}/hashUserId/${data}`)
-    if (!response.ok) throw new Error("Network Error")
-    const hashed = await response.json()
+    const hashed = await fetcher(`${process.env.DC_RUNNER_API}/hashUserId/${data}`)
     res.status(200).json({ hashed })
   } catch {
     res.status(400).json({ error: "Failed to hash data" })
