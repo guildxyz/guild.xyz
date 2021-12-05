4f608c8aaceccc155fe59fe612d0646fff2d6899
valid <valid@zgen.hu>
2021-12-05 22:22:11 +0100

    useIsOwner only for guild

--- src/components/[role]/hooks/useIsOwner.ts
+++ src/components/[role]/hooks/useIsOwner.ts
@@ -1,5 +1,4 @@
 import useGuild from "components/[guild]/hooks/useGuild"
-import useRole from "components/[role]/hooks/useRole"
 import useImmutableSWR from "swr/immutable"
 
 const getIsOwner = async (_, ownerAddresses: Array<string>, checkAddress: string) =>
@@ -7,14 +6,11 @@ const getIsOwner = async (_, ownerAddresses: Array<string>, checkAddress: string
 
 const useIsOwner = (checkAddress: string) => {
   const guild = useGuild()
-  const role = useRole()
 
-  const shouldFetch = (guild?.owner || role?.owner) && checkAddress
+  const shouldFetch = guild?.owner && checkAddress
 
   const { data } = useImmutableSWR(
-    shouldFetch
-      ? ["isOwner", (guild?.owner || role?.owner)?.addresses, checkAddress]
-      : null,
+    shouldFetch ? ["isOwner", guild?.owner?.addresses, checkAddress] : null,
     getIsOwner
   )
 
