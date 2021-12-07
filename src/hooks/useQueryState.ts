194def711c6247956c7751fc08f7fcb4c07a5ea2
BrickheadJohnny <92519134+BrickheadJohnny@users.noreply.github.com>
2021-12-07 01:30:40 +0100

    Filtering/sorting on backend, with URL params (#98)
    Co-authored-by: valid <valid@zgen.hu>

---
+++ src/hooks/useQueryState.ts
@@ -0,0 +1,34 @@
+import { useRouter } from "next/router"
+import { useCallback, useEffect, useState } from "react"
+
+export const useQueryState = <State extends string>(
+  name: string,
+  defaultState: State
+) => {
+  const router = useRouter()
+
+  const getInitialState = () => {
+    const queries = router.query[name]
+    const query = Array.isArray(queries) ? queries[0] : queries
+    return query ? (query as State) : defaultState
+  }
+
+  const [state, setState] = useState(getInitialState)
+
+  useEffect(() => {
+    if (router.isReady) setState(getInitialState)
+  }, [router.isReady])
+
+  const toggle = useCallback(
+    (newState: State) => {
+      setState(newState)
+      const query = { ...router.query, [name]: newState }
+      router.replace({ query }, undefined, {
+        scroll: false,
+      })
+    },
+    [name, router]
+  )
+
+  return [state, toggle] as const
+}
