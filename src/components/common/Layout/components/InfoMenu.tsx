b4ba87230ed5e14ff4b1b8169aa1847599880ca2
valid <valid@zgen.hu>
2021-12-14 21:59:40 +0100

    added roadmap link to InfoMenu

--- src/components/common/Layout/components/InfoMenu.tsx
+++ src/components/common/Layout/components/InfoMenu.tsx
@@ -7,45 +7,68 @@ import {
   MenuItem,
   MenuList,
 } from "@chakra-ui/react"
-import { Code, Info } from "phosphor-react"
+import { useWeb3React } from "@web3-react/core"
+import { Code, Info, RocketLaunch } from "phosphor-react"
+import useSWRImmutable from "swr/immutable"
 
-const InfoMenu = (): JSX.Element => (
-  <Menu>
-    <MenuButton
-      as={IconButton}
-      aria-label="Agora logo"
-      rounded="full"
-      variant="ghost"
-      h="10"
-    >
-      <Icon width="1.2em" height="1.2em" as={Info} />
-    </MenuButton>
-    {/* have to set zIndex, otherwise the search bar's icon lays over it */}
-    <MenuList border="none" shadow="md" zIndex="3">
-      <MenuGroup title="Powered by agora.xyz" pb="2">
-        <MenuItem
-          py="2"
-          as="a"
-          target="_blank"
-          href="https://agora.xyz"
-          rel="noopener"
-          icon={<Info />}
-        >
-          About
-        </MenuItem>
-        <MenuItem
-          py="2"
-          as="a"
-          target="_blank"
-          href="https://github.com/AgoraSpaceDAO/guild.xyz"
-          rel="noopener"
-          icon={<Code />}
-        >
-          Code
-        </MenuItem>
-      </MenuGroup>
-    </MenuList>
-  </Menu>
-)
+const InfoMenu = (): JSX.Element => {
+  const { account } = useWeb3React()
+  const { data: upvotyJWT } = useSWRImmutable(
+    account ? `/user/upvotyAuth/${account}` : null
+  )
+
+  return (
+    <Menu>
+      <MenuButton
+        as={IconButton}
+        aria-label="Agora logo"
+        rounded="full"
+        variant="ghost"
+        h="10"
+      >
+        <Icon width="1.2em" height="1.2em" as={Info} />
+      </MenuButton>
+      {/* have to set zIndex, otherwise the search bar's icon lays over it */}
+      <MenuList border="none" shadow="md" zIndex="3">
+        <MenuGroup title="Powered by agora.xyz" pb="2">
+          <MenuItem
+            py="2"
+            as="a"
+            target="_blank"
+            href="https://agora.xyz"
+            rel="noopener"
+            icon={<Info />}
+          >
+            About
+          </MenuItem>
+          <MenuItem
+            py="2"
+            as="a"
+            target="_blank"
+            href={
+              upvotyJWT
+                ? `https://roadmap.guild.xyz/front/handleSSO/${upvotyJWT}/?redirectUrl=https://roadmap.guild.xyz/?__force`
+                : "https://roadmap.guild.xyz/?__force"
+            }
+            rel="noopener"
+            icon={<RocketLaunch />}
+          >
+            Roadmap
+          </MenuItem>
+          <MenuItem
+            py="2"
+            as="a"
+            target="_blank"
+            href="https://github.com/AgoraSpaceDAO/guild.xyz"
+            rel="noopener"
+            icon={<Code />}
+          >
+            Code
+          </MenuItem>
+        </MenuGroup>
+      </MenuList>
+    </Menu>
+  )
+}
 
 export default InfoMenu
