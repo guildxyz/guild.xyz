da5a23b4ad336b32b9c5eac8754004babe30b9b7
BrickheadJohnny <johnny.zgen@gmail.com>
2022-01-03 11:00:38 +0100

    Requirement chain indicator UI fix

--- src/components/create-guild/Requirements/components/RequirementChainTypeText.tsx
+++ src/components/create-guild/Requirements/components/RequirementChainTypeText.tsx
@@ -1,4 +1,4 @@
-import { Box, HStack, Img, Text, Tooltip, useColorMode } from "@chakra-ui/react"
+import { Flex, HStack, Img, Text, Tooltip, useColorMode } from "@chakra-ui/react"
 import { RPC } from "connectors"
 import { RequirementType, RequirementTypeColors, Rest, SupportedChains } from "types"
 
@@ -15,24 +15,35 @@ const RequirementChainTypeText = ({
   const { colorMode } = useColorMode()
 
   return (
-    <HStack
-      spacing={0}
-      position="absolute"
-      backgroundColor={colorMode === "light" ? "gray.100" : "blackAlpha.300"}
-      overflow="hidden"
-      {...rest}
-    >
+    <HStack spacing={0} position="absolute" h={7} overflow="hidden" {...rest}>
       {["COIN", "ERC20", "ERC721"].includes(requirementType) && requirementChain && (
-        <Box px={2}>
+        <Flex
+          alignItems="center"
+          justifyContent="center"
+          position="relative"
+          mt={-0.5}
+          mr={-5}
+          pl={2}
+          pr={7}
+          h={7}
+          backgroundColor={colorMode === "light" ? "gray.100" : "blackAlpha.300"}
+        >
           <Tooltip label={requirementChain}>
-            <Img src={RPC[requirementChain].iconUrls[0]} boxSize={4} />
+            <Img
+              src={RPC[requirementChain].iconUrls[0]}
+              position="relative"
+              top="px"
+              boxSize={4}
+            />
           </Tooltip>
-        </Box>
+        </Flex>
       )}
       <Text
         as="span"
+        position="relative"
         px={4}
         py={1}
+        h={7}
         backgroundColor={RequirementTypeColors[requirementType]}
         color={requirementType === "WHITELIST" ? "gray.700" : "blackAlpha.600"}
         fontSize="sm"
