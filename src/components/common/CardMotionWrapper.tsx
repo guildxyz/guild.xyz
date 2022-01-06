ab3004eec42f09228cda964e7466de6f12691130
BrickheadJohnny <johnny.zgen@gmail.com>
2022-01-06 10:09:41 +0100

    Using LazyMotion

--- src/components/common/CardMotionWrapper.tsx
+++ src/components/common/CardMotionWrapper.tsx
@@ -1,8 +1,8 @@
 import { Box, EASINGS } from "@chakra-ui/react"
-import { motion } from "framer-motion"
+import { domAnimation, LazyMotion, m } from "framer-motion"
 import { PropsWithChildren } from "react"
 
-const MotionBox = motion(Box)
+const MotionBox = m(Box)
 
 type Props = {
   animateOnMount?: boolean
@@ -12,26 +12,28 @@ const CardMotionWrapper = ({
   animateOnMount = true,
   children,
 }: PropsWithChildren<Props>): JSX.Element => (
-  <MotionBox
-    layout="position"
-    {...(animateOnMount && {
-      initial: { opacity: 0, scale: 0.95 },
-    })}
-    animate={{
-      opacity: 1,
-      scale: 1,
-      transition: { duration: 0.2, ease: EASINGS.easeOut },
-    }}
-    exit={{
-      opacity: 0,
-      scale: 0.95,
-      transition: { duration: 0.1, ease: EASINGS.easeIn },
-    }}
-    transition={{ duration: 0.3, ease: EASINGS.easeInOut }}
-    sx={{ "> *": { height: "full" } }}
-  >
-    {children}
-  </MotionBox>
+  <LazyMotion features={domAnimation}>
+    <MotionBox
+      layout="position"
+      {...(animateOnMount && {
+        initial: { opacity: 0, scale: 0.95 },
+      })}
+      animate={{
+        opacity: 1,
+        scale: 1,
+        transition: { duration: 0.2, ease: EASINGS.easeOut },
+      }}
+      exit={{
+        opacity: 0,
+        scale: 0.95,
+        transition: { duration: 0.1, ease: EASINGS.easeIn },
+      }}
+      transition={{ duration: 0.3, ease: EASINGS.easeInOut }}
+      sx={{ "> *": { height: "full" } }}
+    >
+      {children}
+    </MotionBox>
+  </LazyMotion>
 )
 
 export default CardMotionWrapper
