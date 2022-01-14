ecedc33bb0fbbc9a32b26a7faf666dd7d5bfaa8f
valid <valid@zgen.hu>
2022-01-14 13:10:46 +0100

    validation simplification

--- src/components/create-guild/NameAndIcon.tsx
+++ src/components/create-guild/NameAndIcon.tsx
@@ -1,70 +1,45 @@
-import { Box, FormControl, HStack, Input, Spinner } from "@chakra-ui/react"
+import { FormControl, HStack, Input } from "@chakra-ui/react"
 import FormErrorMessage from "components/common/FormErrorMessage"
 import { useRouter } from "next/router"
-import { useEffect, useMemo, useRef, useState } from "react"
 import { useFormContext, useWatch } from "react-hook-form"
-import useSWR from "swr"
 import IconSelector from "./IconSelector"
 
-const forbiddenNames = ["404", "guild", "hall", "halls", "role", "roles", "guide"]
+const FORBIDDEN_NAMES = ["404", "guild", "hall", "halls", "role", "roles", "guide"]
 
 const NameAndIcon = () => {
   const router = useRouter()
   const {
     register,
-    trigger,
-    formState: { errors, dirtyFields },
+    formState: { errors },
   } = useFormContext()
 
   const urlName = useWatch({ name: "urlName" })
 
-  const swrTimeout = useRef(null)
-  const [searchUrlName, setSearchUrlName] = useState(null)
-  const { data: urlNameFound, isValidating } = useSWR(
-    `/guild/urlName/${searchUrlName}`
-  )
-
-  useEffect(() => {
-    if (swrTimeout.current) window.clearTimeout(swrTimeout.current)
-
-    swrTimeout.current = setTimeout(() => setSearchUrlName(urlName), 500)
-  }, [urlName])
-
-  const isNameAvailable = useMemo(
-    () => (router.pathname === "/create-guild" ? !urlNameFound : true),
-    [router.pathname, urlNameFound]
-  )
-
-  useEffect(() => {
-    if (!dirtyFields.name) return
-    trigger("name")
-  }, [isNameAvailable])
+  const validateName = async (value) => {
+    if (router.pathname !== "/create-guild") return false
+    if (FORBIDDEN_NAMES.includes(urlName)) return "Please pick a different name"
+    const alreadyExists = await fetch(
+      `${process.env.NEXT_PUBLIC_API}/guild/urlName/${value}`
+    ).then(async (response) => response.ok)
+    if (alreadyExists) return "Sorry, this guild name is already taken"
+  }
 
   return (
     <FormControl isRequired isInvalid={errors?.name}>
       <HStack spacing={2}>
         <IconSelector />
-        <Box width="sm" position="relative">
-          <Input
-            size="lg"
-            width="full"
-            pr={10}
-            {...register("name", {
-              required: "This field is required.",
-              maxLength: {
-                value: 50,
-                message: "The maximum possible name length is 50 characters",
-              },
-              validate: () =>
-                (isNameAvailable && !forbiddenNames.includes(urlName)) ||
-                "Please pick a different name.",
-            })}
-          />
-
-          {isValidating && (
-            <Spinner size="sm" position="absolute" top={4} right={4} />
-          )}
-        </Box>
+        <Input
+          size="lg"
+          maxWidth="sm"
+          {...register("name", {
+            required: "This field is required.",
+            maxLength: {
+              value: 50,
+              message: "The maximum possible name length is 50 characters",
+            },
+            validate: validateName,
+          })}
+        />
       </HStack>
       <FormErrorMessage>{errors?.name?.message}</FormErrorMessage>
     </FormControl>
