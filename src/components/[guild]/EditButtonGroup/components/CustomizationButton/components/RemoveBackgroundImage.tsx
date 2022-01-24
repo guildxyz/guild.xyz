b176b4afc225711a19b5fe5a88078a4c6f107ed4
BrickheadJohnny <johnny.zgen@gmail.com>
2022-01-24 16:48:59 +0100

    RemoveBackgroundImage: removing local bg image too

--- src/components/[guild]/EditButtonGroup/components/CustomizationButton/components/RemoveBackgroundImage.tsx
+++ src/components/[guild]/EditButtonGroup/components/CustomizationButton/components/RemoveBackgroundImage.tsx
@@ -1,9 +1,11 @@
 import { Button, Icon } from "@chakra-ui/react"
+import { useThemeContext } from "components/[guild]/ThemeContext"
 import { X } from "phosphor-react"
 import useEdit from "../hooks/useEdit"
 
 const RemoveBackgroundImage = () => {
-  const { onSubmit, isLoading } = useEdit()
+  const { onSubmit, isLoading } = useEdit(() => setLocalBackgroundImage(null))
+  const { setLocalBackgroundImage } = useThemeContext()
 
   const handleRemoveImage = () => onSubmit({ theme: { backgroundImage: "" } })
   return (
