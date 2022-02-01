3a71279fc77e008ed255d1bf714882f795f3c725
BrickheadJohnny <johnny.zgen@gmail.com>
2022-02-01 18:46:45 +0100

    ERC1155FormCard - added a basic input & validation

--- src/components/create-guild/Requirements/components/ERC1155FormCard/ERC1155FormCard.tsx
+++ src/components/create-guild/Requirements/components/ERC1155FormCard/ERC1155FormCard.tsx
@@ -1,4 +1,5 @@
 import { FormControl, FormLabel, Input } from "@chakra-ui/react"
+import FormErrorMessage from "components/common/FormErrorMessage"
 import { useFormContext } from "react-hook-form"
 import { RequirementFormField } from "types"
 import ChainPicker from "../ChainPicker"
@@ -8,11 +9,14 @@ type Props = {
   field: RequirementFormField
 }
 
+const ADDRESS_REGEX = /^0x[A-F0-9]{40}$/i
+
 const ERC1155FormCard = ({ index, field }: Props): JSX.Element => {
   const {
+    register,
     setValue,
     clearErrors,
-    formState: { touchedFields },
+    formState: { errors, touchedFields },
   } = useFormContext()
 
   // Reset form on chain change
@@ -30,9 +34,22 @@ const ERC1155FormCard = ({ index, field }: Props): JSX.Element => {
         onChange={resetForm}
       />
 
-      <FormControl>
+      <FormControl isRequired isInvalid={errors?.requirements?.[index]?.address}>
         <FormLabel>Token:</FormLabel>
-        <Input placeholder="Coming soon" isDisabled />
+        <Input
+          placeholder="Paste address"
+          {...register(`requirements.${index}.address`, {
+            required: "This field is required.",
+            pattern: {
+              value: ADDRESS_REGEX,
+              message:
+                "Please input a 42 characters long, 0x-prefixed hexadecimal address.",
+            },
+          })}
+        />
+        <FormErrorMessage>
+          {errors?.requirements?.[index]?.address?.message}
+        </FormErrorMessage>
       </FormControl>
     </>
   )
