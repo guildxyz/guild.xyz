5bb0a4fd1be9bd23c9e791de8d1b80562900c368
BrickheadJohnny <92519134+BrickheadJohnny@users.noreply.github.com>
2022-02-07 10:10:16 +0100

    Sitemap (#207)

--- src/pages/[guild]/linkpreview.tsx
+++ src/pages/[guild]/linkpreview.tsx
@@ -10,6 +10,7 @@ import {
 } from "@chakra-ui/react"
 import GuildAvatar from "components/common/GuildAvatar"
 import { GetServerSideProps } from "next"
+import Head from "next/head"
 import { Guild, GuildBase } from "types"
 import fetcher from "utils/fetcher"
 
@@ -25,95 +26,100 @@ type Props = {
 }
 
 const LinkPreview = ({ guildData }: Props): JSX.Element => (
-  <Box position="relative" bgColor="gray.800" h="100vh">
-    <SimpleGrid
-      position="absolute"
-      top={0}
-      right={0}
-      px={12}
-      py={16}
-      width="40vw"
-      height="100vh"
-      gridTemplateColumns="repeat(4, 1fr)"
-      gridTemplateRows="repeat(7, 1fr)"
-      gap={10}
-      opacity={0.6}
-    >
-      {guildData.members.map((address) => (
-        <Flex key={address} alignItems="center" justifyContent="center">
-          <GuildAvatar size={24} address={address} />
-        </Flex>
-      ))}
-    </SimpleGrid>
-
-    <Box
-      position="absolute"
-      inset={0}
-      bgGradient="linear-gradient(to right, var(--chakra-colors-gray-800) 0%, var(--chakra-colors-gray-800) 55%, transparent 85%, transparent)"
-    />
-
-    <Flex
-      direction="column"
-      position="relative"
-      pt={28}
-      pl={28}
-      maxW="55vw"
-      height="calc(100vh - 7rem)"
-      alignItems="start"
-    >
-      <HStack w="full" mb={12} spacing={8}>
-        <Img boxSize={24} src={guildData.imageUrl} rounded="full" mt="3" />
-        <Heading textColor="white" fontFamily="display" fontSize="8xl" isTruncated>
-          {guildData.name}
-        </Heading>
-      </HStack>
-      <HStack mb={16} spacing={6}>
-        <Tag
-          height={16}
-          fontSize="4xl"
-          fontWeight="bold"
-          px={8}
-          bgColor="gray.600"
-          textColor="white"
-          rounded="xl"
-        >
-          {`${guildData.memberCount} members`}
-        </Tag>
-        <Tag
-          height={16}
-          fontSize="4xl"
-          fontWeight="bold"
-          px={8}
-          bgColor="gray.600"
-          textColor="white"
-          rounded="xl"
-        >
-          {`${guildData.roles.length} roles`}
-        </Tag>
-      </HStack>
-      <Text
-        fontFamily="display"
-        fontSize="5xl"
-        fontWeight="bold"
-        lineHeight={1.2}
-        noOfLines={3}
+  <>
+    <Head>
+      <meta name="robots" content="noindex" />
+    </Head>
+    <Box position="relative" bgColor="gray.800" h="100vh">
+      <SimpleGrid
+        position="absolute"
+        top={0}
+        right={0}
+        px={12}
+        py={16}
+        width="40vw"
+        height="100vh"
+        gridTemplateColumns="repeat(4, 1fr)"
+        gridTemplateRows="repeat(7, 1fr)"
+        gap={10}
+        opacity={0.6}
       >
-        {guildData.description || (
-          <>
-            That's a great party in there!
-            <br />I dare you to be the plus one guy.
-          </>
-        )}
-      </Text>
+        {guildData.members.map((address) => (
+          <Flex key={address} alignItems="center" justifyContent="center">
+            <GuildAvatar size={24} address={address} />
+          </Flex>
+        ))}
+      </SimpleGrid>
 
-      <HStack mt="auto" spacing={4}>
-        <Img boxSize={10} src="/guildLogos/logo.svg" />
-        <Heading textColor="white" fontFamily="display" fontSize="4xl">
-          guild.xyz
-        </Heading>
-      </HStack>
-    </Flex>
-  </Box>
+      <Box
+        position="absolute"
+        inset={0}
+        bgGradient="linear-gradient(to right, var(--chakra-colors-gray-800) 0%, var(--chakra-colors-gray-800) 55%, transparent 85%, transparent)"
+      />
+
+      <Flex
+        direction="column"
+        position="relative"
+        pt={28}
+        pl={28}
+        maxW="55vw"
+        height="calc(100vh - 7rem)"
+        alignItems="start"
+      >
+        <HStack w="full" mb={12} spacing={8}>
+          <Img boxSize={24} src={guildData.imageUrl} rounded="full" mt="3" />
+          <Heading textColor="white" fontFamily="display" fontSize="8xl" isTruncated>
+            {guildData.name}
+          </Heading>
+        </HStack>
+        <HStack mb={16} spacing={6}>
+          <Tag
+            height={16}
+            fontSize="4xl"
+            fontWeight="bold"
+            px={8}
+            bgColor="gray.600"
+            textColor="white"
+            rounded="xl"
+          >
+            {`${guildData.memberCount} members`}
+          </Tag>
+          <Tag
+            height={16}
+            fontSize="4xl"
+            fontWeight="bold"
+            px={8}
+            bgColor="gray.600"
+            textColor="white"
+            rounded="xl"
+          >
+            {`${guildData.roles.length} roles`}
+          </Tag>
+        </HStack>
+        <Text
+          fontFamily="display"
+          fontSize="5xl"
+          fontWeight="bold"
+          lineHeight={1.2}
+          noOfLines={3}
+        >
+          {guildData.description || (
+            <>
+              That's a great party in there!
+              <br />I dare you to be the plus one guy.
+            </>
+          )}
+        </Text>
+
+        <HStack mt="auto" spacing={4}>
+          <Img boxSize={10} src="/guildLogos/logo.svg" />
+          <Heading textColor="white" fontFamily="display" fontSize="4xl">
+            guild.xyz
+          </Heading>
+        </HStack>
+      </Flex>
+    </Box>
+  </>
 )
 
 export const getServerSideProps: GetServerSideProps = async ({ params }) => {
