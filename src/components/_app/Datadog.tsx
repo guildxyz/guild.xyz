c9f0d4e57cb27db7985176ae48877b50503bbd4b
BrickheadJohnny <johnny.zgen@gmail.com>
2022-02-10 12:12:01 +0100

    Switched to temp/dev datadog project

--- src/components/_app/Datadog.tsx
+++ src/components/_app/Datadog.tsx
@@ -6,15 +6,20 @@ const Datadog = ({ children }: PropsWithChildren<unknown>): JSX.Element => {
   useEffect(() => {
     if (process.env.NODE_ENV !== "production") return
     datadogRum.init({
-      applicationId: "996b7a2a-d610-4235-a5b4-65391973ea76",
-      clientToken: "pub7cf22f3b79a010363cf58c859cfa8ad8",
+      // applicationId: "996b7a2a-d610-4235-a5b4-65391973ea76",
+      // clientToken: "pub7cf22f3b79a010363cf58c859cfa8ad8",
+      // site: "datadoghq.eu",
+      // service: "guild.xyz",
+      // env: "prod",
+      applicationId: "eb05b107-c5ac-490b-addb-67869c1f50a3",
+      clientToken: "pubcb8f3d82e21849dc0d5ed8a8cb24249f",
       site: "datadoghq.eu",
-      service: "guild.xyz",
-      env: "prod",
-      version: "1.0.0",
+      service: "guild-xyz-git-datadog-experiments-zgen.vercel.app",
+      env: "dev",
       sampleRate: 100,
       trackInteractions: true,
       defaultPrivacyLevel: "mask-user-input",
+      // version: "1.0.0",
       beforeSend: (event) => {
         if (process.env.NODE_ENV === "development") {
           console.log("DATADOG EVENT:", event)
