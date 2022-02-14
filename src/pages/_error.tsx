6d514ae1bf6e3bd971ea562f6d530917076cdd25
valid <valid@zgen.hu>
2022-02-14 20:56:43 +0100

    design refinements

--- src/pages/_error.tsx
+++ src/pages/_error.tsx
@@ -1,51 +1,62 @@
-import { Flex, Heading, HStack, Icon, Image, Text, VStack } from "@chakra-ui/react"
-import Link from "components/common/Link"
+import { Flex, Heading, Icon, Text } from "@chakra-ui/react"
+import Button from "components/common/Button"
 import Head from "next/head"
 import { DiscordLogo } from "phosphor-react"
-import NotFOundIcon from "static/avatars/58.svg"
+import NotFoundIcon from "static/avatars/58.svg"
 
-const Error = ({ statusCode }): JSX.Element => (
+const Page = ({ statusCode }): JSX.Element => (
   <>
     <Head>
-      <title>{`${statusCode || "Client-side"} error`}</title>
+      <title>{`Guild - ${statusCode || "Client-side"} error`}</title>
     </Head>
     <Flex
       alignItems="center"
       justifyContent="center"
       direction="column"
-      h="100vh"
-      maxW={{ base: "full", sm: "container.sm" }}
+      minH="100vh"
+      maxW={{ base: "full", sm: "530px" }}
       mx="auto"
-      px={4}
+      p={4}
       textAlign="center"
     >
-      {statusCode === 404 ? (
-        <Icon as={NotFOundIcon} mb={8} boxSize={24} alt="Not found" />
+      <Icon as={NotFoundIcon} boxSize={24} alt="Not found" />
+      {statusCode ? (
+        <Heading
+          mt={2}
+          mb={6}
+          fontFamily="display"
+          as="h1"
+          fontSize={{ base: "8xl", md: "9xl" }}
+        >
+          {statusCode}
+        </Heading>
       ) : (
-        <Image src="/guildLogos/logo.svg" mb={8} boxSize={24} alt="Guild logo" />
+        <Heading mt={6} mb={10} fontFamily="display" as="h1" fontSize={"6xl"}>
+          Client-side error
+        </Heading>
       )}
-      <Heading mb={4} fontFamily="display" fontSize={statusCode ? "9xl" : "6xl"}>
-        {statusCode || "Client-side error"}
-      </Heading>
-      <VStack>
-        <Text as="span" fontSize="lg" mb={8}>
-          Uh-oh! Something went wrong, please contact us on our Discord server if you
-          think you shouldn't see this page!
-        </Text>
-        <HStack>
-          <Icon as={DiscordLogo} boxSize={6} />
-          <Link href="https://discord.gg/bryPA3peuT" isExternal>
-            Guild.xyz Discord Server
-          </Link>
-        </HStack>
-      </VStack>
+      <Text fontSize="lg" mb={10} fontWeight="medium">
+        Uh-oh! Something went wrong, please contact us on our Discord server if you
+        think you shouldn't see this page!
+      </Text>
+      <Button
+        as="a"
+        href="https://discord.gg/bryPA3peuT"
+        target="_blank"
+        leftIcon={<DiscordLogo />}
+        colorScheme="DISCORD"
+        iconSpacing={3}
+        size="lg"
+      >
+        Guild.xyz Discord
+      </Button>
     </Flex>
   </>
 )
 
-Error.getInitialProps = ({ res, err }) => {
+Page.getInitialProps = ({ res, err }) => {
   const statusCode = res ? res.statusCode : err ? err.statusCode : 404
   return { statusCode }
 }
 
-export default Error
+export default Page
