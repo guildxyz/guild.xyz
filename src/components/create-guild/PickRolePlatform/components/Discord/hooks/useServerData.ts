ed6227a2f28a3876bd3d0b96b6cfcf92f45675db
valid <valid@zgen.hu>
2022-02-18 18:54:32 +0100

    optimization: delay fetch when typing DC invite link

--- src/components/create-guild/PickRolePlatform/components/Discord/hooks/useServerData.ts
+++ src/components/create-guild/PickRolePlatform/components/Discord/hooks/useServerData.ts
@@ -1,4 +1,4 @@
-import { useEffect } from "react"
+import { useEffect, useRef, useState } from "react"
 import useSWR from "swr"
 
 const fallbackData = {
@@ -7,19 +7,30 @@ const fallbackData = {
 }
 
 const useServerData = (invite: string) => {
+  const [delayedInvite, setDelayedInvite] = useState(invite)
+  const inviteTimeout = useRef(null)
+
   useEffect(() => {
-    console.log("hook invite", invite)
-    console.log("hook invite.length", invite?.length)
+    if (inviteTimeout.current) window.clearTimeout(inviteTimeout.current)
+
+    inviteTimeout.current = setTimeout(() => setDelayedInvite(invite), 500)
   }, [invite])
 
-  const shouldFetch = invite?.length >= 5
+  useEffect(() => {
+    console.log("hook invite", delayedInvite)
+    console.log("hook invite.length", delayedInvite?.length)
+  }, [delayedInvite])
+
+  const shouldFetch = delayedInvite?.length >= 5
 
   useEffect(() => {
     console.log("shouldFetch", shouldFetch)
   }, [shouldFetch])
 
   const { data, isValidating } = useSWR(
-    shouldFetch ? `/role/discordChannels/${invite.split("/").slice(-1)[0]}` : null,
+    shouldFetch
+      ? `/role/discordChannels/${delayedInvite.split("/").slice(-1)[0]}`
+      : null,
     {
       fallbackData,
     }
