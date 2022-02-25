21b54a7b20dbf6af951b804cc9c771c909735f28
Balázs Csete <csetebalu@gmail.com>
2022-02-25 08:38:34 +0100

    persisting fetched account data with swr

--- src/components/[guild]/hooks/useUser.ts
+++ src/components/[guild]/hooks/useUser.ts
@@ -1,5 +1,5 @@
+import { Web3Provider } from "@ethersproject/providers"
 import { useWeb3React } from "@web3-react/core"
-import { useState } from "react"
 import useSWR from "swr"
 import { User } from "types"
 
@@ -9,19 +9,48 @@ const getlinkedAddressesCount = (addresses: string[] | number) => {
   return addresses > 1 && addresses - 1
 }
 
+type UserFetchProps = {
+  method: "POST"
+  body: unknown
+  validationData: { address: string; library: Web3Provider }
+}
+
 const useUser = () => {
   const { account, library } = useWeb3React()
-  const [validationData, setValidationData] = useState(null)
+
+  const { data: fetchProps, mutate } = useSWR<UserFetchProps | null>(
+    "fetchProps",
+    null,
+    {
+      refreshInterval: 0,
+      revalidateOnFocus: false,
+      revalidateOnMount: false,
+      revalidateOnReconnect: false,
+      revalidateIfStale: false,
+    }
+  )
 
   const { isValidating, data } = useSWR<User>(
-    account ? [`/user/${account}`, { validationData }] : null
+    account && fetchProps !== null ? [`/user/${account}`, fetchProps] : null,
+    null,
+    {
+      refreshInterval: /* !!fetchProps ? 5000 :*/ 0,
+      revalidateOnFocus: false,
+      revalidateOnReconnect: false,
+      revalidateIfStale: false,
+    }
   )
 
   return {
     isLoading: isValidating,
     ...data,
     linkedAddressesCount: getlinkedAddressesCount(data?.addresses),
-    verifyAddress: () => setValidationData({ address: account, library }),
+    verifyAddress: () =>
+      mutate({
+        method: "POST",
+        body: {},
+        validationData: { address: account, library },
+      }),
   }
 }
 
