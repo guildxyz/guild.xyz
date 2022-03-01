95ab18f0c9d883b9c94777c59752c5a29264d0d5
valid <valid@zgen.hu>
2022-03-01 19:02:17 +0100

    UX: auto open WalletSelectorModal on create-guild

--- src/pages/create-guild.tsx
+++ src/pages/create-guild.tsx
@@ -14,8 +14,9 @@ import LogicPicker from "components/create-guild/LogicPicker"
 import PickRolePlatform from "components/create-guild/PickRolePlatform"
 import Requirements from "components/create-guild/Requirements"
 import SubmitButton from "components/create-guild/SubmitButton"
+import { Web3Connection } from "components/_app/Web3ConnectionManager"
 import useWarnIfUnsavedChanges from "hooks/useWarnIfUnsavedChanges"
-import { useEffect, useState } from "react"
+import { useContext, useEffect, useState } from "react"
 import { FormProvider, useForm } from "react-hook-form"
 
 const CreateGuildPage = (): JSX.Element => {
@@ -23,6 +24,7 @@ const CreateGuildPage = (): JSX.Element => {
   const methods = useForm({ mode: "all" })
   const [formErrors, setFormErrors] = useState(null)
   const [uploadPromise, setUploadPromise] = useState<Promise<void>>(null)
+  const { openWalletSelectorModal, triedEager } = useContext(Web3Connection)
 
   useWarnIfUnsavedChanges(
     methods.formState?.isDirty && !methods.formState.isSubmitted
@@ -32,6 +34,10 @@ const CreateGuildPage = (): JSX.Element => {
     methods.register("urlName")
     methods.register("chainName", { value: "ETHEREUM" })
   }, [])
+
+  useEffect(() => {
+    if (triedEager && !account) openWalletSelectorModal()
+  }, [account, triedEager])
 
   return (
     <>
