3897272364c45c586fab2ace8b8be9c31451ba8f
BrickheadJohnny <johnny.zgen@gmail.com>
2022-03-03 16:27:03 +0100

    Deleting unused `attribute` in JSON replacer

--- src/utils/guildJsonReplacer.ts
+++ src/utils/guildJsonReplacer.ts
@@ -2,6 +2,14 @@ const replacer = (key, value) => {
   if (value === null) return undefined
   if (key === "discord_invite") return undefined
   if (key === "description" || key === "name") return value?.trim()
+  if (
+    key === "attribute" &&
+    typeof value === "object" &&
+    !Array.isArray(value) &&
+    value !== null &&
+    Object.keys(value)?.length === 0
+  )
+    return undefined
   return value
 }
 
