f7874f83028d964ea242cf9db5b3494d12526af4
Balázs Csete <csetebalu@gmail.com>
2022-03-10 17:18:30 +0100

    wip

--- src/components/create-guild/Requirements/components/FormCard.tsx
+++ src/components/create-guild/Requirements/components/FormCard.tsx
@@ -1,8 +1,9 @@
-import { CloseButton, VStack } from "@chakra-ui/react"
+import { CloseButton, HStack, Text, VStack } from "@chakra-ui/react"
 import CardMotionWrapper from "components/common/CardMotionWrapper"
 import ColorCard from "components/common/ColorCard"
 import { PropsWithChildren } from "react"
 import { RequirementType, RequirementTypeColors } from "types"
+import useBalancy from "../hooks/useBalancy"
 import RequirementChainTypeText from "./RequirementChainTypeText"
 
 type Props = {
@@ -16,35 +17,50 @@ const FormCard = ({
   index,
   onRemove,
   children,
-}: PropsWithChildren<Props>): JSX.Element => (
-  <CardMotionWrapper>
-    <ColorCard color={RequirementTypeColors[type]}>
-      <CloseButton
-        position="absolute"
-        top={2}
-        right={2}
-        width={8}
-        height={8}
-        rounded="full"
-        aria-label="Remove requirement"
-        zIndex="1"
-        onClick={onRemove}
-      />
-      <VStack spacing={4} alignItems="start" pt={4} h="full">
-        {children}
-      </VStack>
-      <RequirementChainTypeText
-        index={index}
-        requirementType={type}
-        top={"-px"}
-        left={"-px"}
-        textContainerProps={{
-          borderTopLeftRadius: "2xl",
-          borderBottomRightRadius: "xl",
-        }}
-      />
-    </ColorCard>
-  </CardMotionWrapper>
-)
+}: PropsWithChildren<Props>): JSX.Element => {
+  const { holders } = useBalancy(index)
+
+  return (
+    <CardMotionWrapper>
+      <ColorCard color={RequirementTypeColors[type]}>
+        <CloseButton
+          position="absolute"
+          top={2}
+          right={2}
+          width={8}
+          height={8}
+          rounded="full"
+          aria-label="Remove requirement"
+          zIndex="1"
+          onClick={onRemove}
+        />
+        <VStack spacing={4} alignItems="start" pt={4} h="full">
+          {children}
+        </VStack>
+        <RequirementChainTypeText
+          requirementType={type}
+          top={"-px"}
+          left={"-px"}
+          borderTopLeftRadius="2xl"
+          borderBottomRightRadius="xl"
+        />
+        {typeof holders === "number" && (
+          <HStack mt={5}>
+            <Text color="gray">{`${holders} ${
+              holders > 1 ? "addresses" : "address"
+            } ${holders > 1 ? "satisfy" : "satisfies"} this requirement`}</Text>
+            {/*<Tooltip
+              label={`${holders} ${holders > 1 ? "addresses" : "address"} ${
+                holders > 1 ? "satisfy" : "satisfies"
+              } this requirement`}
+            >
+              <Info color="gray" />
+            </Tooltip>*/}
+          </HStack>
+        )}
+      </ColorCard>
+    </CardMotionWrapper>
+  )
+}
 
 export default FormCard
--- src/components/create-guild/Requirements/components/RequirementChainTypeText.tsx
+++ src/components/create-guild/Requirements/components/RequirementChainTypeText.tsx
@@ -1,18 +1,8 @@
-import {
-  Center,
-  HStack,
-  Img,
-  Spinner,
-  Text,
-  Tooltip,
-  useColorMode,
-} from "@chakra-ui/react"
+import { Center, HStack, Img, Text, Tooltip, useColorMode } from "@chakra-ui/react"
 import { RPC } from "connectors"
 import { RequirementType, RequirementTypeColors, Rest, SupportedChains } from "types"
-import useBalancy from "../hooks/useBalancy"
 
 type Props = {
-  index: number
   requirementChain?: SupportedChains
   requirementType: RequirementType
 } & Rest
@@ -20,52 +10,48 @@ type Props = {
 const RequirementChainTypeText = ({
   requirementChain,
   requirementType,
-  textContainerProps,
-  index,
   ...rest
 }: Props): JSX.Element => {
   const { colorMode } = useColorMode()
-  const { holders, isLoading } = useBalancy(index)
 
   return (
-    <HStack spacing={2} position="absolute" h={7} {...rest}>
-      <HStack spacing={0} overflow="hidden" {...textContainerProps}>
-        {["COIN", "ERC20", "ERC721", "ERC1155"].includes(requirementType) &&
-          requirementChain && (
-            <Center
-              pl={2}
-              pr={5}
-              mr="-3"
-              mb="1px"
-              backgroundColor={colorMode === "light" ? "gray.100" : "blackAlpha.300"}
-            >
-              <Tooltip label={requirementChain}>
-                <Img src={RPC[requirementChain].iconUrls[0]} boxSize={4} />
-              </Tooltip>
-            </Center>
-          )}
-        <Text
-          as="span"
-          px={4}
-          py={1}
-          backgroundColor={RequirementTypeColors[requirementType]}
-          color={requirementType === "WHITELIST" ? "gray.700" : "blackAlpha.600"}
-          fontSize="sm"
-          textTransform="uppercase"
-          fontWeight="extrabold"
-          borderTopLeftRadius="xl"
-        >
-          {requirementType === "ERC721" || requirementType === "ERC1155"
-            ? "NFT"
-            : requirementType}
-        </Text>
-      </HStack>
-      {typeof holders === "number" && (
-        <>
-          {isLoading && <Spinner size="sm" color="gray" />}
-          <Text color="gray">{holders} eligible addresses</Text>
-        </>
-      )}
+    <HStack
+      spacing={0}
+      position="absolute"
+      h={7}
+      overflow="hidden"
+      alignItems="stretch"
+      {...rest}
+    >
+      {["COIN", "ERC20", "ERC721", "ERC1155"].includes(requirementType) &&
+        requirementChain && (
+          <Center
+            pl={2}
+            pr={5}
+            mr="-3"
+            mb="1px"
+            backgroundColor={colorMode === "light" ? "gray.100" : "blackAlpha.300"}
+          >
+            <Tooltip label={requirementChain}>
+              <Img src={RPC[requirementChain].iconUrls[0]} boxSize={4} />
+            </Tooltip>
+          </Center>
+        )}
+      <Text
+        as="span"
+        px={4}
+        py={1}
+        backgroundColor={RequirementTypeColors[requirementType]}
+        color={requirementType === "WHITELIST" ? "gray.700" : "blackAlpha.600"}
+        fontSize="sm"
+        textTransform="uppercase"
+        fontWeight="extrabold"
+        borderTopLeftRadius="xl"
+      >
+        {requirementType === "ERC721" || requirementType === "ERC1155"
+          ? "NFT"
+          : requirementType}
+      </Text>
     </HStack>
   )
 }
