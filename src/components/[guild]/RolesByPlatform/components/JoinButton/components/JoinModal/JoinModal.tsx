f49f90369360174380313ecc6d8a83a9834c0881
Balázs Csete <csetebalu@gmail.com>
2022-03-16 08:09:31 +0100

    small improvements

--- src/components/[guild]/RolesByPlatform/components/JoinButton/components/JoinModal/JoinModal.tsx
+++ src/components/[guild]/RolesByPlatform/components/JoinButton/components/JoinModal/JoinModal.tsx
@@ -1,4 +1,6 @@
 import {
+  Flex,
+  Icon,
   ModalBody,
   ModalCloseButton,
   ModalContent,
@@ -11,6 +13,8 @@ import {
 import { Error } from "components/common/Error"
 import { Modal } from "components/common/Modal"
 import ModalButton from "components/common/ModalButton"
+import { CheckCircle } from "phosphor-react"
+import platformsContent from "../../platformsContent"
 import useJoinPlatform from "./hooks/useJoinPlatform"
 import processJoinPlatformError from "./utils/processJoinPlatformError"
 
@@ -20,6 +24,10 @@ type Props = {
 }
 
 const JoinModal = ({ isOpen, onClose }: Props): JSX.Element => {
+  const {
+    title,
+    join: { description },
+  } = platformsContent[""]
   const {
     response,
     isLoading,
@@ -32,27 +40,41 @@ const JoinModal = ({ isOpen, onClose }: Props): JSX.Element => {
     <Modal isOpen={isOpen} onClose={onClose}>
       <ModalOverlay />
       <ModalContent>
-        <ModalHeader>Join Guild</ModalHeader>
+        <ModalHeader>Join {title}</ModalHeader>
         <ModalCloseButton />
         <ModalBody>
           <Error error={joinError} processError={processJoinPlatformError} />
-          {!response ? <Text>TODO</Text> : <Text>Success TODO</Text>}
+          {!response ? (
+            <Text>{description}</Text>
+          ) : response.alreadyJoined ? (
+            <Flex alignItems="center">
+              <Icon as={CheckCircle} color="green.500" boxSize="16" weight="light" />
+              <Text ml="6">Seems like you've already joined!</Text>
+            </Flex>
+          ) : (
+            <Flex alignItems="center">
+              <Icon as={CheckCircle} color="green.500" boxSize="16" weight="light" />
+              <Text ml="6">Successfully joined Guild!</Text>
+            </Flex>
+          )}
         </ModalBody>
-        <ModalFooter>
-          {/* margin is applied on AuthButton, so there's no jump when it collapses and unmounts */}
-          <VStack spacing="0" alignItems="strech" w="full">
-            {(() => {
-              if (isSigning)
-                return <ModalButton isLoading loadingText="Check your wallet" />
-              if (isLoading)
-                return <ModalButton isLoading loadingText="Joining guild" />
-              if (joinError)
-                return <ModalButton onClick={onSubmit}>Try again</ModalButton>
-              if (!response)
-                return <ModalButton onClick={onSubmit}>Verify address</ModalButton>
-            })()}
-          </VStack>
-        </ModalFooter>
+        {(isSigning || isLoading || joinError || !response) && (
+          <ModalFooter>
+            {/* margin is applied on AuthButton, so there's no jump when it collapses and unmounts */}
+            <VStack spacing="0" alignItems="strech" w="full">
+              {(() => {
+                if (isSigning)
+                  return <ModalButton isLoading loadingText="Check your wallet" />
+                if (isLoading)
+                  return <ModalButton isLoading loadingText="Joining guild" />
+                if (joinError)
+                  return <ModalButton onClick={onSubmit}>Try again</ModalButton>
+                if (!response)
+                  return <ModalButton onClick={onSubmit}>Verify address</ModalButton>
+              })()}
+            </VStack>
+          </ModalFooter>
+        )}
       </ModalContent>
     </Modal>
   )
