e1d70f240a1cdcc813bb4e56089ad609fde334e6
valid <valid@zgen.hu>
2022-03-23 14:48:20 +0100

    preselect server by invite on DC OAuth page

--- src/components/create-guild/PickRolePlatform/components/Discord/hooks/useAddBotPupup.ts
+++ src/components/create-guild/PickRolePlatform/components/Discord/hooks/useAddBotPupup.ts
@@ -2,19 +2,19 @@ import { useEffect, useState } from "react"
 import { useWatch } from "react-hook-form"
 import useServerData from "./useServerData"
 
-const OAUTH_URI = `https://discord.com/api/oauth2/authorize?client_id=${process.env.NEXT_PUBLIC_DISCORD_CLIENT_ID}&permissions=8&scope=bot%20applications.commands`
-
 const useAddBotPopup = () => {
   const [popup, setPopup] = useState<Window>(null)
   const invite = useWatch({ name: "discord_invite" })
   const {
-    data: { channels },
+    data: { channels, serverId },
   } = useServerData(invite, {
     refreshInterval: !!popup ? 1000 : 0,
   })
 
+  const OAuthUri = `https://discord.com/api/oauth2/authorize?client_id=${process.env.NEXT_PUBLIC_DISCORD_CLIENT_ID}&guild_id=${serverId}&permissions=8&scope=bot%20applications.commands`
+
   const openWindow = () => {
-    setPopup(window.open(OAUTH_URI, "_blank", "height=750,width=600,scrollbars"))
+    setPopup(window.open(OAuthUri, "_blank", "height=750,width=600,scrollbars"))
   }
 
   useEffect(() => {
