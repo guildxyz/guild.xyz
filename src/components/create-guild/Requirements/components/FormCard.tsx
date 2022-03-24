43e434888d6e80578f362e37ad8bcdcad9119ad7
cs-balazs <44908417+cs-balazs@users.noreply.github.com>
2022-03-24 20:57:16 +0100

    feat: Count eligible addresses for requirements (#251)

--- src/components/create-guild/Requirements/components/FormCard.tsx
+++ src/components/create-guild/Requirements/components/FormCard.tsx
@@ -1,45 +1,71 @@
-import { CloseButton, VStack } from "@chakra-ui/react"
+import { CloseButton, HStack, Spinner, Text, VStack } from "@chakra-ui/react"
 import CardMotionWrapper from "components/common/CardMotionWrapper"
 import ColorCard from "components/common/ColorCard"
 import { PropsWithChildren } from "react"
 import { RequirementType, RequirementTypeColors } from "types"
+import useBalancy from "../hooks/useBalancy"
 import RequirementChainTypeText from "./RequirementChainTypeText"
 
 type Props = {
+  index: number
   type: RequirementType
   onRemove: () => void
 }
 
 const FormCard = ({
   type,
+  index,
   onRemove,
   children,
-}: PropsWithChildren<Props>): JSX.Element => (
-  <CardMotionWrapper>
-    <ColorCard color={RequirementTypeColors[type]}>
-      <CloseButton
-        position="absolute"
-        top={2}
-        right={2}
-        width={8}
-        height={8}
-        rounded="full"
-        aria-label="Remove requirement"
-        zIndex="1"
-        onClick={onRemove}
-      />
-      <VStack spacing={4} alignItems="start" pt={4} h="full">
-        {children}
-      </VStack>
-      <RequirementChainTypeText
-        requirementType={type}
-        top={"-px"}
-        left={"-px"}
-        borderTopLeftRadius="2xl"
-        borderBottomRightRadius="xl"
-      />
-    </ColorCard>
-  </CardMotionWrapper>
-)
+}: PropsWithChildren<Props>): JSX.Element => {
+  const { holders, isLoading } = useBalancy(index)
+
+  return (
+    <CardMotionWrapper>
+      <ColorCard color={RequirementTypeColors[type]}>
+        <CloseButton
+          position="absolute"
+          top={2}
+          right={2}
+          width={8}
+          height={8}
+          rounded="full"
+          aria-label="Remove requirement"
+          zIndex="1"
+          onClick={onRemove}
+        />
+        <VStack spacing={4} alignItems="start" pt={4} h="full">
+          {children}
+        </VStack>
+        <RequirementChainTypeText
+          requirementType={type}
+          top={"-px"}
+          left={"-px"}
+          borderTopLeftRadius="2xl"
+          borderBottomRightRadius="xl"
+        />
+
+        {typeof holders === "number" ? (
+          <HStack mt={5}>
+            <Text color="gray">
+              {isLoading ? (
+                <Spinner color="gray" size="xs" mx={1} />
+              ) : (
+                <Text as="span" fontWeight={"medium"}>
+                  {holders}
+                </Text>
+              )}{" "}
+              {`${holders > 1 ? "addresses" : "address"} ${
+                holders > 1 ? "satisfy" : "satisfies"
+              } this requirement`}
+            </Text>
+          </HStack>
+        ) : (
+          isLoading && <Spinner color="gray" size="sm" mt={5} />
+        )}
+      </ColorCard>
+    </CardMotionWrapper>
+  )
+}
 
 export default FormCard
