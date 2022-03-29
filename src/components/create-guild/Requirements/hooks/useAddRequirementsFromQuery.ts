a8634df4df6fd90e78120eb23abb3251200e301e
Csete Balázs <csetebalu@gmail.com>
2022-03-29 15:43:43 +0200

    validate unlock id

--- src/components/create-guild/Requirements/hooks/useAddRequirementsFromQuery.ts
+++ src/components/create-guild/Requirements/hooks/useAddRequirementsFromQuery.ts
@@ -2,6 +2,7 @@ import { useRouter } from "next/router"
 import { useEffect } from "react"
 import { UseFieldArrayAppend } from "react-hook-form"
 import { GuildFormType, SupportedChains } from "types"
+import useLocks from "../components/UnlockFormCard/hooks/useLocks"
 import { unlockSupportedChains } from "../components/UnlockFormCard/UnlockFormCard"
 
 const useAddRequirementsFromQuery = (
@@ -9,13 +10,18 @@ const useAddRequirementsFromQuery = (
 ) => {
   const router = useRouter()
 
+  const { locks } = useLocks(
+    ((router.query.chain as string)?.toUpperCase() ?? "ETHEREUM") as SupportedChains
+  )
+
   useEffect(() => {
-    if (!router.isReady) return
+    if (!router.isReady || !locks) return
     const chain = (router.query.chain as string)?.toUpperCase()
     const unlockId = (router.query.unlockId as string)?.toLowerCase()
     if (
       unlockId?.length > 0 &&
       chain?.length > 0 &&
+      locks.some((lock) => lock.address === unlockId) &&
       unlockSupportedChains.includes(chain)
     ) {
       append({
@@ -24,7 +30,7 @@ const useAddRequirementsFromQuery = (
         address: unlockId,
       })
     }
-  }, [router])
+  }, [router, locks])
 }
 
 export default useAddRequirementsFromQuery
