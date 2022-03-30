75532aff5b29b46220a3929687f8800cbb7a4cbf
Csete Balázs <csetebalu@gmail.com>
2022-03-30 16:40:46 +0200

    useServerData by serverId

--- src/components/create-guild/PickRolePlatform/components/Discord/hooks/useServerData.ts
+++ src/components/create-guild/PickRolePlatform/components/Discord/hooks/useServerData.ts
@@ -1,4 +1,3 @@
-import useDebouncedState from "hooks/useDebouncedState"
 import { useEffect } from "react"
 import useSWR from "swr"
 
@@ -8,24 +7,15 @@ const fallbackData = {
   isAdmin: null,
 }
 
-const useServerData = (invite: string, swrOptions = {}) => {
-  const debouncedInvite = useDebouncedState(invite)
-
-  useEffect(() => {
-    console.log("hook invite", debouncedInvite)
-    console.log("hook invite.length", debouncedInvite?.length)
-  }, [debouncedInvite])
-
-  const shouldFetch = debouncedInvite?.length >= 5
+const useServerData = (serverId: string, swrOptions = {}) => {
+  const shouldFetch = serverId?.length >= 0
 
   useEffect(() => {
     console.log("shouldFetch", shouldFetch)
   }, [shouldFetch])
 
   const { data, isValidating, error } = useSWR(
-    shouldFetch
-      ? `/discord/server/${debouncedInvite.split("/").slice(-1)[0]}`
-      : null,
+    shouldFetch ? `/discord/server/${serverId}` : null,
     {
       fallbackData,
       ...swrOptions,
