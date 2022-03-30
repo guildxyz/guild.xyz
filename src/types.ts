077fa4e58a568f0044f9522f0448c14dc56d83a1
Csete Balázs <csetebalu@gmail.com>
2022-03-30 15:57:36 +0200

    Added type for oauth returned server data

--- src/types.ts
+++ src/types.ts
@@ -241,7 +241,19 @@ type SelectOption = {
   img?: string
 } & Rest
 
+// Requeted with Discord OAuth token
+type DiscordServerData = {
+  id: string
+  name: string
+  icon: string
+  owner: boolean
+  permissions: number
+  features: string[]
+  permissions_new: string
+}
+
 export type {
+  DiscordServerData,
   GuildAdmin,
   Token,
   DiscordError,
