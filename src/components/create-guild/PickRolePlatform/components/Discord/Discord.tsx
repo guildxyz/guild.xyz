cbe70fbf5aaacd9a68294f041b5cb9d09f5411d1
Csete Balázs <csetebalu@gmail.com>
2022-03-30 16:40:27 +0200

    added guild oauth scope

--- src/components/create-guild/PickRolePlatform/components/Discord/Discord.tsx
+++ src/components/create-guild/PickRolePlatform/components/Discord/Discord.tsx
@@ -148,7 +148,7 @@ const Discord = () => {
               w="full"
               onClick={() =>
                 onDCAuthOpen(
-                  `https://discord.com/api/oauth2/authorize?client_id=${process.env.NEXT_PUBLIC_DISCORD_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_DISCORD_REDIRECT_URI}&response_type=token&scope=identify&state=create-guild`
+                  `https://discord.com/api/oauth2/authorize?client_id=${process.env.NEXT_PUBLIC_DISCORD_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_DISCORD_REDIRECT_URI}&response_type=token&scope=identify%20guilds&state=create-guild`
                 )
               }
               isLoading={isAuthenticating}
