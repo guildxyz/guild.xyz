354b968d453de078002158c237d7e60c736f32c6
Csete Balázs <csetebalu@gmail.com>
2022-04-01 14:18:17 +0200

    test: paste guild name & wait before sign

--- cypress/integration/2-cleanup.spec.js
+++ cypress/integration/2-cleanup.spec.js
@@ -26,6 +26,7 @@ describe("post-test cleanup", () => {
 
             cy.get(".chakra-slide .chakra-button").first().click()
             cy.findByText("Delete").click()
+            cy.wait(2000)
             cy.confirmMetamaskSignatureRequest()
           } else {
             cy.visit("/")
