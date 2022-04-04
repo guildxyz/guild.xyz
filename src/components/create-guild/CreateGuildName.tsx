141330208f5bc51986cacc3578a316cfeaccbbdc
cs-balazs <44908417+cs-balazs@users.noreply.github.com>
2022-04-04 14:35:29 +0200

    Remove urlName validation on create (#282)

--- src/components/create-guild/CreateGuildName.tsx
+++ src/components/create-guild/CreateGuildName.tsx
@@ -39,20 +39,6 @@ const CreateGuildName = (): JSX.Element => {
 
   const urlName = useWatch({ name: "urlName" })
 
-  const validate = async () => {
-    /**
-     * Form mode is set to "all", so validation runs on both change and blur events.
-     * In this case we only want it to run on blur tho, so we cancel when the input is focused
-     */
-    if (document.activeElement === inputRef.current) return null
-
-    if (FORBIDDEN_NAMES.includes(urlName)) return "Please pick a different name"
-    const alreadyExists = await fetch(
-      `${process.env.NEXT_PUBLIC_API}/guild/${urlName}`
-    ).then(async (response) => response.ok && response.status !== 204)
-    if (alreadyExists) return "Sorry, this guild name is already taken"
-  }
-
   const {
     ref,
     onBlur: defaultOnBlur,
@@ -63,7 +49,8 @@ const CreateGuildName = (): JSX.Element => {
       value: 50,
       message: "The maximum possible name length is 50 characters",
     },
-    validate,
+    validate: () =>
+      !FORBIDDEN_NAMES.includes(urlName) || "Please pick a different name",
   })
 
   const onBlur = (e) => {
