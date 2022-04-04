bb693c40e2aa1bf6a01e95815b49a882526583c3
Csete Balázs <csetebalu@gmail.com>
2022-04-04 16:01:24 +0200

    reverted unrelevant changes

--- src/components/create-guild/CreateGuildName.tsx
+++ src/components/create-guild/CreateGuildName.tsx
@@ -6,6 +6,19 @@ import { useFormContext, useWatch } from "react-hook-form"
 import { GuildFormType } from "types"
 import slugify from "utils/slugify"
 
+const FORBIDDEN_NAMES = [
+  "404",
+  "index",
+  "dcauth",
+  "create-guild",
+  "guild",
+  "hall",
+  "halls",
+  "role",
+  "roles",
+  "guide",
+]
+
 const CreateGuildName = (): JSX.Element => {
   const addDatadogAction = useRumAction("trackingAppAction")
   const addDatadogError = useRumError()
@@ -15,16 +28,30 @@ const CreateGuildName = (): JSX.Element => {
     control,
     register,
     setValue,
-    formState: { errors, touchedFields },
+    formState: { errors },
   } = useFormContext<GuildFormType>()
 
   const name = useWatch({ control: control, name: "name" })
 
   useEffect(() => {
-    if (typeof name === "string" && !touchedFields.urlName) {
-      setValue("urlName", slugify(name.toString()))
-    }
+    if (name) setValue("urlName", slugify(name.toString()))
   }, [name])
+
+  const urlName = useWatch({ name: "urlName" })
+
+  const validate = async () => {
+    /**
+     * Form mode is set to "all", so validation runs on both change and blur events.
+     * In this case we only want it to run on blur tho, so we cancel when the input is focused
+     */
+    if (document.activeElement === inputRef.current) return null
+
+    if (FORBIDDEN_NAMES.includes(urlName)) return "Please pick a different name"
+    const alreadyExists = await fetch(
+      `${process.env.NEXT_PUBLIC_API}/guild/${urlName}`
+    ).then(async (response) => response.ok && response.status !== 204)
+    if (alreadyExists) return "Sorry, this guild name is already taken"
+  }
 
   const {
     ref,
@@ -36,6 +63,7 @@ const CreateGuildName = (): JSX.Element => {
       value: 50,
       message: "The maximum possible name length is 50 characters",
     },
+    validate,
   })
 
   const onBlur = (e) => {
