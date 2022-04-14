aeb94443b5b71aaa5ea487ae00ff9bb7c87c18c9
BrickheadJohnny <johnny.zgen@gmail.com>
2022-04-14 08:04:13 +0200

    Composable reqs sections - fixed photo size

--- src/components/landing/ComposableRequirements.tsx
+++ src/components/landing/ComposableRequirements.tsx
@@ -1,9 +1,28 @@
+import { Box, Img } from "@chakra-ui/react"
 import LandingSection from "./LandingSection"
 
 const ComposableRequirements = (): JSX.Element => (
   <LandingSection
     title="Composable membeship requirements"
-    photo="/landing/composable-membership-requirements.png"
+    photo={
+      <Box position="relative" width="full">
+        <Img
+          width="full"
+          src="/landing/composable-membership-requirements.png"
+          alt="Composable membeship requirements"
+        />
+
+        <Img
+          position="absolute"
+          left={-4}
+          bottom={-16}
+          width={{ base: "calc(100% + 3rem)", lg: "calc(100% + 6rem)" }}
+          maxW="none"
+          src="/landing/composable-membership-requirements-icons.png"
+          alt="Composable membeship requirements - icons"
+        />
+      </Box>
+    }
     content={`On-chain integrations and \nexternal APIs are available. \nPlay with logic gates and \ncross-chain opportunities.`}
   />
 )
