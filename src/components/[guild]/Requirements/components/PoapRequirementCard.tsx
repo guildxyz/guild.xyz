1c9e6f2eb84f50a300f1ca939a7f250df6e9852e
BrickheadJohnny <johnny.zgen@gmail.com>
2022-04-20 08:32:25 +0200

    Added image to Juicebox, Mirror & POAP reqs

--- src/components/[guild]/Requirements/components/PoapRequirementCard.tsx
+++ src/components/[guild]/Requirements/components/PoapRequirementCard.tsx
@@ -1,3 +1,6 @@
+import { HStack, Img, SkeletonCircle } from "@chakra-ui/react"
+import usePoaps from "components/create-guild/Requirements/components/PoapFormCard/hooks/usePoaps"
+import { useMemo } from "react"
 import { Requirement } from "types"
 import RequirementCard from "./common/RequirementCard"
 import RequirementText from "./common/RequirementText"
@@ -6,10 +9,31 @@ type Props = {
   requirement: Requirement
 }
 
-const PoapRequirementCard = ({ requirement }: Props) => (
-  <RequirementCard requirement={requirement}>
-    <RequirementText>{`Own the ${requirement.data?.id} POAP`}</RequirementText>
-  </RequirementCard>
-)
+const PoapRequirementCard = ({ requirement }: Props) => {
+  // TODO: maybe somehow we could fetch only 1 POAP by fancy_id?
+  const { poaps, isLoading } = usePoaps()
+
+  const poapImage = useMemo(
+    () => poaps?.find((poap) => poap.fancy_id === requirement?.data?.id)?.image_url,
+    [requirement, poaps]
+  )
+
+  return (
+    <RequirementCard requirement={requirement}>
+      <HStack spacing={4} alignItems="center">
+        <SkeletonCircle minW={6} boxSize={6} isLoaded={!isLoading && !!poapImage}>
+          <Img
+            src={poapImage}
+            alt={requirement.data?.id}
+            width={6}
+            borderRadius="full"
+          />
+        </SkeletonCircle>
+
+        <RequirementText>{`Own the ${requirement.data?.id} POAP`}</RequirementText>
+      </HStack>
+    </RequirementCard>
+  )
+}
 
 export default PoapRequirementCard
