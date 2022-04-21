5df11af4305e8fd808a6c69713ad06e0258f16c4
BrickheadJohnny <johnny.zgen@gmail.com>
2022-04-21 07:48:18 +0200

    Icon on NftRequirementCard

---
+++ src/components/[guild]/Requirements/components/NftRequirementCard/hooks/useNftImage.ts
@@ -0,0 +1,18 @@
+import useSWRImmutable from "swr/immutable"
+import fetcher from "utils/fetcher"
+
+const fetchAndMapNftData = (address: string) =>
+  fetcher(`https://api.opensea.io/api/v1/asset_contract/${address}`)
+    .then((openseaData) => openseaData.image_url)
+    .catch((_) => null)
+
+const useNftImage = (address: string) => {
+  const { data, isValidating } = useSWRImmutable<string>(
+    address ?? null,
+    fetchAndMapNftData
+  )
+
+  return { nftImage: data, isLoading: isValidating }
+}
+
+export default useNftImage
