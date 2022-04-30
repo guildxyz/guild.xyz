8d46f0eff636622b5dfa123c9782dc3534bd04ec
BrickheadJohnny <johnny.zgen@gmail.com>
2022-04-30 09:45:02 +0200

    hotfix: useEditRole - gated channels

--- src/components/[guild]/RolesByPlatform/components/RoleListItem/components/EditRole/hooks/useEditRole.ts
+++ src/components/[guild]/RolesByPlatform/components/RoleListItem/components/EditRole/hooks/useEditRole.ts
@@ -38,24 +38,26 @@ const useEditRole = (roleId: number, onSuccess?: () => void) => {
   return {
     ...useSubmitResponse,
     onSubmit: (data) => {
-      data.gatedChannels = Object.entries(
-        data.gatedChannels as GatedChannels
-      ).reduce(
-        (acc, [categoryId, { channels }]) => {
-          const channelEntries = Object.entries(channels)
-          const filtered = channelEntries.filter(([, { isChecked }]) => isChecked)
+      data.gatedChannels = data.gatedChannels
+        ? Object.entries(data.gatedChannels as GatedChannels).reduce(
+            (acc, [categoryId, { channels }]) => {
+              const channelEntries = Object.entries(channels)
+              const filtered = channelEntries.filter(
+                ([, { isChecked }]) => isChecked
+              )
 
-          if (filtered.length === channelEntries.length) {
-            acc.categories.push(categoryId)
-            return acc
-          }
+              if (filtered.length === channelEntries.length) {
+                acc.categories.push(categoryId)
+                return acc
+              }
 
-          acc.channels = [...acc.channels, ...filtered.map(([id]) => id)]
+              acc.channels = [...acc.channels, ...filtered.map(([id]) => id)]
 
-          return acc
-        },
-        { categories: [], channels: [] }
-      )
+              return acc
+            },
+            { categories: [], channels: [] }
+          )
+        : undefined
 
       return useSubmitResponse.onSubmit(
         JSON.parse(
