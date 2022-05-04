c4080174910eb225a24670a64d93f3023a99c226
Csete Balázs <csetebalu@gmail.com>
2022-05-04 11:15:05 +0200

    small refactor

--- src/components/[guild]/AddRoleButton/components/ExistingRoleSettings/ExistingRoleSettings.tsx
+++ src/components/[guild]/AddRoleButton/components/ExistingRoleSettings/ExistingRoleSettings.tsx
@@ -4,18 +4,13 @@ import {
   FormLabel,
   HStack,
   Select,
-  Spinner,
-  Tag,
-  Text,
-  Tooltip,
   VStack,
 } from "@chakra-ui/react"
 import useGuild from "components/[guild]/hooks/useGuild"
 import useServerData from "hooks/useServerData"
-import { Warning } from "phosphor-react"
 import { useFormContext, useFormState } from "react-hook-form"
 import UnauthenticatedOptions from "../UnauthenticatedOptions"
-import useDiscordRoleMemberCount from "./hooks/useDiscordRoleMemberCount"
+import MemberCount from "./components/MemberCount"
 
 const ExistingRoleSettings = () => {
   const { errors } = useFormState()
@@ -25,43 +20,12 @@ const ExistingRoleSettings = () => {
     data: { roles },
   } = useServerData(platforms?.[0]?.platformId)
 
-  const { memberCount, error, isLoading } = useDiscordRoleMemberCount()
-
   return (
     <VStack px="5" py="4" spacing="8">
       <FormControl isDisabled={!roles?.length}>
         <HStack mb={2} alignItems="center">
           <FormLabel m={0}>Select role</FormLabel>
-          {error ? (
-            <Tooltip
-              label="Failed to count the number of members with the selected role"
-              shouldWrapChildren
-              placement="right"
-            >
-              <Warning color="orange" />
-            </Tooltip>
-          ) : (
-            <Tooltip
-              placement="right"
-              label={
-                isLoading
-                  ? "Counting the number of members with the selected role"
-                  : `${memberCount ?? 0} member${
-                      memberCount === 1 ? " has" : "s have"
-                    } this role on Discord`
-              }
-            >
-              <Tag>
-                {isLoading ? (
-                  <Spinner size="xs" />
-                ) : (
-                  <Text>
-                    {memberCount ?? 0} member{memberCount === 1 ? "" : "s"}
-                  </Text>
-                )}
-              </Tag>
-            </Tooltip>
-          )}
+          <MemberCount />
         </HStack>
 
         <Select {...register("discordRoleId")} maxW="sm">
