9efcb7fe78daad891346d732e2a4391ce0286cbf
BrickheadJohnny <johnny.zgen@gmail.com>
2022-05-05 13:01:44 +0200

    Added discord embed modification events

--- src/components/[guild]/Onboarding/components/SummonMembers/components/PanelBody/components/PanelTitle.tsx
+++ src/components/[guild]/Onboarding/components/SummonMembers/components/PanelBody/components/PanelTitle.tsx
@@ -1,8 +1,23 @@
 import { Editable, EditableInput, EditablePreview } from "@chakra-ui/react"
+import { useRumAction } from "@datadog/rum-react-integration"
+import useDebouncedState from "hooks/useDebouncedState"
+import { useEffect } from "react"
 import { useController } from "react-hook-form"
 
 const PanelTitle = () => {
-  const { field } = useController({ name: "title", rules: { required: true } })
+  const addDatadogAction = useRumAction("trackingAppAction")
+
+  const { field, fieldState } = useController({
+    name: "title",
+    rules: { required: true },
+  })
+
+  const isDirty = useDebouncedState(fieldState.isDirty)
+
+  useEffect(() => {
+    if (!isDirty) return
+    addDatadogAction("modified dc embed title")
+  }, [isDirty])
 
   return (
     <Editable fontWeight={"bold"} {...field} placeholder={"Title"}>
