60dd3be272e4b1232d5228417cb7284677c3afd3
valid <valid@zgen.hu>
2022-05-09 12:54:55 +0200

    UI(create-guild): add button icons

--- src/components/guard/setup/DCServerCard.tsx
+++ src/components/guard/setup/DCServerCard.tsx
@@ -4,6 +4,7 @@ import OptionCard from "components/common/OptionCard"
 import usePopupWindow from "hooks/usePopupWindow"
 import useServerData from "hooks/useServerData"
 import Link from "next/link"
+import { ArrowRight, ArrowSquareOut } from "phosphor-react"
 import { useEffect } from "react"
 import { useFormContext } from "react-hook-form"
 import useGuildByPlatformId from "./hooks/useGuildByPlatformId"
@@ -67,6 +68,7 @@ const DCServerCard = ({ serverData, onSelect, onCancel }: Props): JSX.Element =>
           colorScheme="DISCORD"
           onClick={openAddBotPopup}
           isLoading={!!activeAddBotPopup}
+          rightIcon={<ArrowSquareOut />}
           data-dd-action-name="Add bot [dc server setup]"
         >
           Add bot
@@ -76,6 +78,7 @@ const DCServerCard = ({ serverData, onSelect, onCancel }: Props): JSX.Element =>
           h={10}
           colorScheme="green"
           onClick={() => onSelect(serverData.id)}
+          rightIcon={<ArrowRight />}
           data-dd-action-name="Select [dc server setup]"
         >
           Select
