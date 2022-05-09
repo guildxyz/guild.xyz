fb59ed6da167e3c00e5f2db9be1556dda25a0784
valid <valid@zgen.hu>
2022-05-09 14:55:45 +0200

    UI: LandingSectionText improvements

--- src/components/index/LandingSectionText.tsx
+++ src/components/index/LandingSectionText.tsx
@@ -1,7 +1,13 @@
 import { Text } from "@chakra-ui/react"
 
 const LandingSectionText = ({ children }) => (
-  <Text fontSize="xl" fontWeight="medium" lineHeight="125%" w="full">
+  <Text
+    fontSize="xl"
+    fontWeight="semibold"
+    lineHeight="125%"
+    w="full"
+    colorScheme={"gray"}
+  >
     {children}
   </Text>
 )
