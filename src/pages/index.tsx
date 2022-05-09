6f25b8ebc48cf5d0e6784413a66e217accd62875
valid <valid@zgen.hu>
2022-05-10 00:30:24 +0200

    navigate to explorer if wallet is already connected

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { Box } from "@chakra-ui/react"
+import { useWeb3React } from "@web3-react/core"
 import LinkPreviewHead from "components/common/LinkPreviewHead"
 import CallToAction from "components/index/CallToAction"
 import ComposableRequirements from "components/index/ComposableRequirements"
@@ -11,48 +12,62 @@ import Hero from "components/index/Hero"
 import PlatformAgnosticCommunities from "components/index/PlatformAgnosticCommunities"
 import RealTimeQueryEngine from "components/index/RealTimeQueryEngine"
 import TokenBasedMembership from "components/index/TokenBasedMembership"
+import { Web3Connection } from "components/_app/Web3ConnectionManager"
 import Head from "next/head"
+import { useRouter } from "next/router"
+import { useContext, useEffect } from "react"
 
-const Page = (): JSX.Element => (
-  <>
-    <LinkPreviewHead path="" />
-    <Head>
-      <title>Guild</title>
-      <meta
-        name="description"
-        content="Automated membership management for the platforms your community already use."
-      />
-    </Head>
+const Page = (): JSX.Element => {
+  const { account } = useWeb3React()
+  const router: any = useRouter()
+  const hasNavigated = router.components && Object.keys(router.components).length > 2
+  const { triedEager } = useContext(Web3Connection)
 
-    <Box
-      as="main"
-      position="relative"
-      bgColor="gray.800"
-      h="100vh"
-      justifyContent="start"
-      overflowX="hidden"
-      overflowY="scroll"
-      sx={{
-        perspective: "2px",
-        transformStyle: "preserve-3d",
-        scrollBehavior: "smooth",
-      }}
-    >
-      <Hero />
-      <Box bgColor="gray.800" sx={{ transformStyle: "preserve-3d" }}>
-        <PlatformAgnosticCommunities />
-        <TokenBasedMembership />
-        <GuardAgainstPhishingAttack />
-        <RealTimeQueryEngine />
-        <ComposableRequirements />
-        <ExploreTrendingGuilds />
-        <GuildValues />
-        <Discover />
+  useEffect(() => {
+    if (!hasNavigated && triedEager && account) router.push("/explorer")
+  }, [hasNavigated, account, triedEager])
+
+  return (
+    <>
+      <LinkPreviewHead path="" />
+      <Head>
+        <title>Guild</title>
+        <meta
+          name="description"
+          content="Automated membership management for the platforms your community already use."
+        />
+      </Head>
+
+      <Box
+        as="main"
+        position="relative"
+        bgColor="gray.800"
+        h="100vh"
+        justifyContent="start"
+        overflowX="hidden"
+        overflowY="scroll"
+        sx={{
+          perspective: "2px",
+          transformStyle: "preserve-3d",
+          scrollBehavior: "smooth",
+        }}
+      >
+        <Hero />
+        <Box bgColor="gray.800" sx={{ transformStyle: "preserve-3d" }}>
+          <PlatformAgnosticCommunities />
+          <TokenBasedMembership />
+          <GuardAgainstPhishingAttack />
+          <RealTimeQueryEngine />
+          <ComposableRequirements />
+          <ExploreTrendingGuilds />
+          <GuildValues />
+          <Discover />
+        </Box>
+        <CallToAction />
+        <Footer />
       </Box>
-      <CallToAction />
-      <Footer />
-    </Box>
-  </>
-)
+    </>
+  )
+}
 
 export default Page
