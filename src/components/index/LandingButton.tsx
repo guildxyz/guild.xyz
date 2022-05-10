148a826eb69b0358382775583208b2a9273719a0
valid <valid@zgen.hu>
2022-05-10 02:37:20 +0200

    fix: LandingButton forwardRef

--- src/components/index/LandingButton.tsx
+++ src/components/index/LandingButton.tsx
@@ -1,14 +1,19 @@
 import { ButtonProps, useBreakpointValue } from "@chakra-ui/react"
 import Button from "components/common/Button"
-import React, { PropsWithChildren } from "react"
+import React, { forwardRef, PropsWithChildren, Ref } from "react"
 
-const LandingButton = ({ children, ...rest }: PropsWithChildren<ButtonProps>) => {
-  const size = useBreakpointValue({ base: "md", md: "lg" })
-  return (
-    <Button size={size} fontWeight="bold" {...rest}>
-      {children}
-    </Button>
-  )
-}
+const LandingButton = forwardRef(
+  (
+    { children, ...rest }: PropsWithChildren<ButtonProps>,
+    ref: Ref<HTMLButtonElement>
+  ) => {
+    const size = useBreakpointValue({ base: "md", md: "lg" })
+    return (
+      <Button ref={ref} size={size} fontWeight="bold" {...rest}>
+        {children}
+      </Button>
+    )
+  }
+)
 
 export default LandingButton
