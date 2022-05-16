16561101ef7b7b0a59ab9a7c6c9f3dbd5cc15c62
BrickheadJohnny <johnny.zgen@gmail.com>
2022-05-16 18:31:19 +0200

    Guard setup - added role setup guide video

--- src/components/guard/setup/ServerSetupCard/ServerSetupCard.tsx
+++ src/components/guard/setup/ServerSetupCard/ServerSetupCard.tsx
@@ -1,19 +1,23 @@
-import { SimpleGrid, Stack } from "@chakra-ui/react"
+import { Box, SimpleGrid, Stack } from "@chakra-ui/react"
 import { useRumAction, useRumError } from "@datadog/rum-react-integration"
 import { useWeb3React } from "@web3-react/core"
 import Button from "components/common/Button"
 import Card from "components/common/Card"
 import CardMotionWrapper from "components/common/CardMotionWrapper"
+import DiscordRoleVideo from "components/common/DiscordRoleVideo"
 import useCreateGuild from "components/create-guild/hooks/useCreateGuild"
 import useSetImageAndNameFromPlatformData from "components/create-guild/hooks/useSetImageAndNameFromPlatformData"
 import { Web3Connection } from "components/_app/Web3ConnectionManager"
+import { AnimatePresence, motion } from "framer-motion"
 import usePinata from "hooks/usePinata"
 import useServerData from "hooks/useServerData"
 import useSubmitWithUpload from "hooks/useSubmitWithUpload"
 import { Check } from "phosphor-react"
-import { useContext, useEffect, useMemo } from "react"
+import { useContext, useEffect, useMemo, useState } from "react"
 import { useFormContext, useWatch } from "react-hook-form"
 import getRandomInt from "utils/getRandomInt"
+
+const MotionStack = motion(Stack)
 
 const ServerSetupCard = ({ children }): JSX.Element => {
   const addDatadogAction = useRumAction("trackingAppAction")
@@ -34,6 +38,8 @@ const ServerSetupCard = ({ children }): JSX.Element => {
   } = useServerData(selectedServer, {
     refreshInterval: 0,
   })
+
+  const [watchedVideo, setWatchedVideo] = useState(false)
 
   const { onSubmit, isLoading, response, isSigning, error } = useCreateGuild()
 
@@ -72,31 +78,57 @@ const ServerSetupCard = ({ children }): JSX.Element => {
     <CardMotionWrapper>
       <Card px={{ base: 5, sm: 6 }} py={7}>
         <Stack spacing={8}>
-          {children}
+          <AnimatePresence initial={false} exitBeforeEnter>
+            <MotionStack
+              key={watchedVideo ? "form" : "video"}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 0.24 }}
+              spacing={8}
+            >
+              {watchedVideo ? children : <DiscordRoleVideo />}
+            </MotionStack>
+          </AnimatePresence>
 
           <SimpleGrid columns={2} gap={4}>
-            <Button
-              colorScheme="gray"
-              disabled={!!account}
-              onClick={openWalletSelectorModal}
-              rightIcon={!!account && <Check />}
-              data-dd-action-name="Connect wallet [dc server setup]"
-            >
-              {!account ? "Connect wallet" : "Wallet connected"}
-            </Button>
+            {watchedVideo ? (
+              <>
+                <Button
+                  colorScheme="gray"
+                  disabled={!!account}
+                  onClick={openWalletSelectorModal}
+                  rightIcon={!!account && <Check />}
+                  data-dd-action-name="Connect wallet [dc server setup]"
+                >
+                  {!account ? "Connect wallet" : "Wallet connected"}
+                </Button>
 
-            <Button
-              colorScheme="green"
-              disabled={
-                !account || response || isLoading || isSigning || isUploadingShown
-              }
-              isLoading={isLoading || isSigning || isUploadingShown}
-              loadingText={loadingText}
-              onClick={handleSubmit}
-              data-dd-action-name="Sign to summon [dc server setup]"
-            >
-              Sign to summon
-            </Button>
+                <Button
+                  colorScheme="green"
+                  disabled={
+                    !account ||
+                    response ||
+                    isLoading ||
+                    isSigning ||
+                    isUploadingShown
+                  }
+                  isLoading={isLoading || isSigning || isUploadingShown}
+                  loadingText={loadingText}
+                  onClick={handleSubmit}
+                  data-dd-action-name="Sign to summon [dc server setup]"
+                >
+                  Sign to summon
+                </Button>
+              </>
+            ) : (
+              <>
+                <Box />
+                <Button colorScheme="green" onClick={() => setWatchedVideo(true)}>
+                  Okay
+                </Button>
+              </>
+            )}
           </SimpleGrid>
         </Stack>
       </Card>
--- src/pages/create-guild/discord.tsx
+++ src/pages/create-guild/discord.tsx
@@ -1,6 +1,6 @@
-import { Text } from "@chakra-ui/react"
 import { WithRumComponentContext } from "@datadog/rum-react-integration"
 import DiscordGuildSetup from "components/common/DiscordGuildSetup"
+import DiscordRoleVideo from "components/common/DiscordRoleVideo"
 import Layout from "components/common/Layout"
 import DynamicDevTool from "components/create-guild/DynamicDevTool"
 import useDCAuth from "components/[guild]/RolesByPlatform/components/JoinButton/components/JoinModal/hooks/useDCAuth"
@@ -43,14 +43,7 @@ const CreateDiscordGuildPage = (): JSX.Element => {
     <Layout title="Create Guild on Discord">
       <FormProvider {...methods}>
         <DiscordGuildSetup {...{ defaultValues, selectedServer }}>
-          <Text>
-            Make sure the <i>Guild.xyz bot</i> role is above every user's role it'll
-            have to manage
-          </Text>
-
-          <video src="/videos/dc-bot-role-config-guide.webm" muted autoPlay loop>
-            Your browser does not support the HTML5 video tag.
-          </video>
+          <DiscordRoleVideo />
         </DiscordGuildSetup>
 
         <DynamicDevTool control={methods.control} />
