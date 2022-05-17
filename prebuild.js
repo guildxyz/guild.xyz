b67e1f4a662adddde76f9b78f6f493e857c2117b
BrickheadJohnny <johnny.zgen@gmail.com>
2022-05-17 20:07:47 +0200

    Created a custom prebuild script

---
+++ prebuild.js
@@ -0,0 +1,22 @@
+const { rename, mkdirSync } = require("fs")
+
+/**
+ * Moving this serverless function to the root directory (only in production mode),
+ * so it'll become a Vercel Serverless Function. This way we won't exceed the 50MB
+ * limit with it on Vercel.
+ */
+
+if (process.env.NODE_ENV === "production") {
+  mkdirSync("./api/linkpreview/[hash]", { recursive: true })
+
+  rename(
+    "./src/pages/api/linkpreview/[hash]/[[...urlName]].ts",
+    "./api/linkpreview/[hash]/[[...urlName]].ts",
+    (err) => {
+      if (err) throw err
+      console.log(
+        "Moved `./src/api/linkpreview/[hash]/[[...urlName]].ts` to `./api/linkpreview/[hash]/[[...urlName]].ts`!"
+      )
+    }
+  )
+}
