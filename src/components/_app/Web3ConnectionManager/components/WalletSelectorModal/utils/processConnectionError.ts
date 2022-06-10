c0ace7d80d869becc585877b18a04ac5f6738bf8
BrickheadJohnny <johnny.zgen@gmail.com>
2022-06-10 09:59:44 +0200

    Simplified processConnectionError

--- src/components/_app/Web3ConnectionManager/components/WalletSelectorModal/utils/processConnectionError.ts
+++ src/components/_app/Web3ConnectionManager/components/WalletSelectorModal/utils/processConnectionError.ts
@@ -1,46 +1,17 @@
-import {
-  NoEthereumProviderError,
-  UserRejectedRequestError as UserRejectedRequestErrorInjected,
-} from "@web3-react/injected-connector"
 import { ErrorInfo } from "components/common/Error"
 import { WalletError } from "types"
 
 const processConnectionError = (error: WalletError & Error): ErrorInfo => {
-  switch (error.constructor) {
-    case NoEthereumProviderError:
-      return {
-        title: "Wallet not found",
-        description:
-          "No Ethereum browser extension detected, install MetaMask on desktop or visit from a dApp browser on mobile.",
-      }
-    case UserRejectedRequestErrorInjected:
-      return {
-        title: "Error connecting. Try again!",
-        description:
-          "Please authorize this website to access your Ethereum account.",
-      }
-    case Error:
-      return {
-        title: error.name,
-        description: error.message,
-      }
-    default:
-      if (error.message)
-        return {
-          title: "Error",
-          description: error.message,
-        }
+  // Returning a simpler error message in this case
+  if (error.code === -32002)
+    return {
+      title: "Connection already in progress",
+      description: "Please check your wallet.",
+    }
 
-      return {
-        title:
-          error.code === -32002
-            ? "Connection already in progress"
-            : "An unknown error occurred",
-        description:
-          error.code === -32002
-            ? "Check your wallet for more details."
-            : "Check the console for more details.",
-      }
+  return {
+    title: "Error",
+    description: error.message,
   }
 }
 
