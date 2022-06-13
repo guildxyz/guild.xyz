1f3a66baa96e34c32678d7e37d3c15076a1aea1f
Csete Balázs <csetebalu@gmail.com>
2022-06-13 09:01:57 +0200

    ColorCardLabel abstraction for platform card

--- src/components/[guild]/Requirements/components/common/RequirementCard.tsx
+++ src/components/[guild]/Requirements/components/common/RequirementCard.tsx
@@ -1,14 +1,17 @@
 import {
   Box,
+  Center,
   Circle,
   Divider,
   HStack,
   Img,
   SkeletonCircle,
+  Tooltip,
   useColorMode,
 } from "@chakra-ui/react"
 import ColorCard from "components/common/ColorCard"
-import RequirementChainTypeText from "components/create-guild/Requirements/components/RequirementChainTypeText"
+import ColorCardLabel from "components/common/ColorCard/ColorCardLabel"
+import { RPC } from "connectors"
 import { PropsWithChildren } from "react"
 import { Requirement, RequirementTypeColors, Rest } from "types"
 import RequirementText from "./RequirementText"
@@ -65,14 +68,31 @@ const RequirementCard = ({
         )}
       </Box>
 
-      <RequirementChainTypeText
-        requirementChain={requirement?.chain}
-        requirementType={requirement?.type}
+      <ColorCardLabel
+        type={requirement?.type}
+        typeBackgroundColors={RequirementTypeColors}
+        typeLabel={{ ERC1155: "NFT", ERC721: "NFT" }}
+        typeColors={{ ALLOWLIST: "gray.700" }}
         bottom="-px"
         right="-px"
         borderTopLeftRadius="xl"
         borderBottomRightRadius="xl"
-      />
+      >
+        {["COIN", "ERC20", "ERC721", "ERC1155"].includes(requirement?.type) &&
+          requirement?.chain && (
+            <Center
+              pl={2}
+              pr={5}
+              mr="-3"
+              mb="1px"
+              backgroundColor={colorMode === "light" ? "gray.100" : "blackAlpha.300"}
+            >
+              <Tooltip label={requirement?.chain}>
+                <Img src={RPC[requirement?.chain]?.iconUrls?.[0]} boxSize={4} />
+              </Tooltip>
+            </Center>
+          )}
+      </ColorCardLabel>
     </ColorCard>
   )
 }
