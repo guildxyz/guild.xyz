6f4b5fddb5ff63b62c786ba929c3bf3e8acfcb7c
Csete Balázs <csetebalu@gmail.com>
2022-06-15 11:22:48 +0200

    refactor

--- src/components/[guild]/RolePlatforms/RolePlatforms.tsx
+++ src/components/[guild]/RolePlatforms/RolePlatforms.tsx
@@ -1,18 +1,14 @@
-import { GridItem, SimpleGrid, Text, useBreakpointValue } from "@chakra-ui/react"
+import { SimpleGrid, Text, useBreakpointValue } from "@chakra-ui/react"
 import { useFieldArray, useWatch } from "react-hook-form"
 import { Role } from "types"
 import useGuild from "../hooks/useGuild"
 import PlatformCard from "./components/PlatformCard"
-import EditDiscord from "./components/PlatformCard/components/EditDiscordPlatform"
 import { RolePlatformProvider } from "./components/RolePlatformProvider"
 
 type Props = {
   role?: Role
 }
 
-const rolePlatformEdit = {
-  DISCORD: EditDiscord,
-}
 const RolePlatforms = ({ role }: Props) => {
   const { platforms } = useGuild()
   const { remove } = useFieldArray({
@@ -32,47 +28,22 @@ const RolePlatforms = ({ role }: Props) => {
 
   return (
     <SimpleGrid columns={cols} gap={10}>
-      {(fields ?? []).map((rolePlatform: any, index) => {
-        const isNew =
-          role === undefined || // From add role drawer
-          role?.platforms.every((rp) => rp.platformId !== rolePlatform.platformId)
-        const EditComponent = rolePlatformEdit[rolePlatform.type]
-
-        const card = (
-          <RolePlatformProvider
-            rolePlatform={{
-              ...rolePlatform,
-              // These should be available in rolePlatform
-              nativePlatformId:
-                (typeof rolePlatform.platformId === "string" &&
-                  rolePlatform.platformId) ||
-                platforms?.[0]?.platformId,
-              type: rolePlatform.type,
-            }}
-          >
-            <PlatformCard
-              key={rolePlatform.roleId}
-              imageUrl={role?.imageUrl}
-              name={role?.name}
-              Modal={
-                (isNew && EditComponent?.NewPlatform?.EditModal) ||
-                EditComponent?.EditModal
-              }
-              onRemove={() => remove(index)}
-            >
-              {EditComponent &&
-                ((isNew && <EditComponent.NewPlatform.Label />) || (
-                  <EditComponent.Label />
-                ))}
-            </PlatformCard>
-          </RolePlatformProvider>
-        )
-
-        if (!!EditComponent && cols > 1) {
-          return <GridItem colSpan={2}>{card}</GridItem>
-        }
-        return card
-      })}
+      {(fields ?? []).map((rolePlatform: any, index) => (
+        <RolePlatformProvider
+          key={rolePlatform.roleId}
+          rolePlatform={{
+            ...rolePlatform,
+            // These should be available in rolePlatform
+            nativePlatformId:
+              (typeof rolePlatform.platformId === "string" &&
+                rolePlatform.platformId) ||
+              platforms?.[0]?.platformId,
+            type: rolePlatform.type,
+          }}
+        >
+          <PlatformCard role={role} onRemove={() => remove(index)} />
+        </RolePlatformProvider>
+      ))}
     </SimpleGrid>
   )
 }
--- src/components/[guild]/RolePlatforms/components/PlatformCard/PlatformCard.tsx
+++ src/components/[guild]/RolePlatforms/components/PlatformCard/PlatformCard.tsx
@@ -2,6 +2,7 @@ import {
   Box,
   Divider,
   Flex,
+  GridItem,
   HStack,
   Text,
   useBreakpointValue,
@@ -12,8 +13,9 @@ import { platforms } from "components/create-guild/PlatformsGrid/PlatformsGrid"
 import useServerData from "hooks/useServerData"
 import Image from "next/image"
 import { PropsWithChildren } from "react"
-import { PlatformName, Rest } from "types"
+import { PlatformName, Rest, Role } from "types"
 import { useRolePlatrform } from "../RolePlatformProvider"
+import DiscordCardComponents from "./components/DiscordCardComponents"
 import EditButton from "./components/EditButton"
 
 const platformBackgroundColor: Partial<Record<PlatformName, string>> = {
@@ -26,38 +28,50 @@ const platformTypeLabel = Object.fromEntries(
 )
 
 type Props = {
-  imageUrl: string
-  name: string
-  Modal?: (props: { isOpen: boolean; onClose: () => void }) => JSX.Element
   onRemove: () => void
+  role?: Role
 } & Rest
 
-const PlatformCard = ({
-  imageUrl,
-  name,
-  children,
-  Modal,
-  onRemove,
-  ...rest
-}: PropsWithChildren<Props>) => {
-  const { type, nativePlatformId } = useRolePlatrform()
+const platformSpecificCardComponents = {
+  DISCORD: DiscordCardComponents,
+}
+
+const PlatformCard = ({ role, onRemove, ...rest }: PropsWithChildren<Props>) => {
+  const { type, nativePlatformId, platformId } = useRolePlatrform()
 
   const serverData = useServerData(
     (type === "DISCORD" && nativePlatformId) || undefined
   )
 
   const label =
-    (serverData?.data?.serverName?.length > 0 && serverData.data.serverName) || name
+    (serverData?.data?.serverName?.length > 0 && serverData.data.serverName) ||
+    role?.name
 
   const displayDivider = useBreakpointValue({ base: true, md: false })
 
+  const isNew =
+    role === undefined || // From add role drawer
+    role?.platforms.every((rp) => rp.platformId !== platformId)
+  const EditComponent = platformSpecificCardComponents[type]
+
+  const cols = useBreakpointValue({ base: 1, md: 2 })
+
   return (
-    <ColorCard
-      color={platformBackgroundColor[type]}
-      pt={{ base: 10, sm: 11 }}
-      {...rest}
+    <GridItem
+      colSpan={
+        +(
+          ((!isNew && !!EditComponent?.EditModal) ||
+            (!!isNew && !!EditComponent?.NewPlatform?.EditModal)) &&
+          cols > 1
+        ) + 1
+      }
     >
-      {/*<CloseButton
+      <ColorCard
+        color={platformBackgroundColor[type]}
+        pt={{ base: 10, sm: 11 }}
+        {...rest}
+      >
+        {/*<CloseButton
         position="absolute"
         top={2}
         right={2}
@@ -69,61 +83,72 @@ const PlatformCard = ({
         onClick={onRemove}
       />*/}
 
-      <Flex
-        justifyContent={"space-between"}
-        flexDirection={{ base: "column", md: "row" }}
-      >
-        <HStack spacing={3}>
-          <Box
-            overflow={"hidden"}
-            borderRadius="full"
-            width={10}
-            height={10}
-            position="relative"
+        <Flex
+          justifyContent={"space-between"}
+          flexDirection={{ base: "column", md: "row" }}
+        >
+          <HStack spacing={3}>
+            <Box
+              overflow={"hidden"}
+              borderRadius="full"
+              width={10}
+              height={10}
+              position="relative"
+            >
+              {(serverData?.data?.serverIcon?.length > 0 ||
+                role?.imageUrl?.length > 0) && (
+                <Image
+                  src={
+                    (serverData?.data?.serverIcon?.length > 0 &&
+                      serverData.data.serverIcon) ||
+                    role?.imageUrl
+                  }
+                  alt={label}
+                  layout="fill"
+                />
+              )}
+            </Box>
+            <Text fontWeight={"bold"}>{label}</Text>
+          </HStack>
+
+          {displayDivider && <Divider my={3} />}
+
+          <Flex
+            flexDirection={{ base: "column", md: "row" }}
+            alignItems={{ base: "stretch", md: "center" }}
           >
-            {(serverData?.data?.serverIcon?.length > 0 || imageUrl?.length > 0) && (
-              <Image
-                src={
-                  (serverData?.data?.serverIcon?.length > 0 &&
-                    serverData.data.serverIcon) ||
-                  imageUrl
+            {((isNew && !!EditComponent?.NewPlatform?.Label) ||
+              (!isNew && !!EditComponent?.Label)) &&
+              ((isNew && <EditComponent.NewPlatform.Label />) || (
+                <EditComponent.Label />
+              ))}
+
+            {((isNew && !!EditComponent?.NewPlatform?.EditModal) ||
+              (!isNew && !!EditComponent?.EditModal)) && (
+              <EditButton
+                ml={{ base: 0, md: 3 }}
+                mt={{ base: 3, md: 0 }}
+                Modal={
+                  (isNew && EditComponent.NewPlatform.EditModal) ||
+                  EditComponent.EditModal
                 }
-                alt={label}
-                layout="fill"
               />
             )}
-          </Box>
-          <Text fontWeight={"bold"}>{label}</Text>
-        </HStack>
-
-        {displayDivider && <Divider my={3} />}
-
-        <Flex
-          flexDirection={{ base: "column", md: "row" }}
-          alignItems={{ base: "stretch", md: "center" }}
-        >
-          {children}
-          {Modal && (
-            <EditButton
-              ml={{ base: 0, md: 3 }}
-              mt={{ base: 3, md: 0 }}
-              Modal={Modal}
-            />
-          )}
+          </Flex>
         </Flex>
-      </Flex>
 
-      <ColorCardLabel
-        fallbackColor="white"
-        type={type}
-        typeBackgroundColors={platformBackgroundColor}
-        typeLabel={platformTypeLabel}
-        top="-px"
-        left="-px"
-        borderBottomRightRadius="xl"
-        borderTopLeftRadius="xl"
-      />
-    </ColorCard>
+        <ColorCardLabel
+          fallbackColor="white"
+          type={type}
+          typeBackgroundColors={platformBackgroundColor}
+          typeLabel={platformTypeLabel}
+          top="-px"
+          left="-px"
+          borderBottomRightRadius="xl"
+          borderTopLeftRadius="xl"
+        />
+      </ColorCard>
+    </GridItem>
   )
 }
 
