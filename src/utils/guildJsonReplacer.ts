474fb808139ecc65b25ba11d579919bfe300d2be
cs-balazs <44908417+cs-balazs@users.noreply.github.com>
2022-07-06 16:17:18 +0200

    fix: renamed balancy decimals in form state

--- src/utils/guildJsonReplacer.ts
+++ src/utils/guildJsonReplacer.ts
@@ -1,7 +1,7 @@
 const replacer = (key, value) => {
   if (value === null) return undefined
   if (key === "discord_invite") return undefined
-  if (key === "decimals") return undefined
+  if (key === "balancyDecimals") return undefined
   if (key === "description" || key === "name") return value?.trim()
   if (key === "grantAccessToExistingUsers" && !!value) return value === "true"
   return value
