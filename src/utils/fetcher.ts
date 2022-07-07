a7e6121d9b19eca9fd44788ebc4834943710a6a2
BrickheadJohnny <92519134+BrickheadJohnny@users.noreply.github.com>
2022-07-07 16:26:20 +0200

    Fetcher with Datadog actions (#409)

--- src/utils/fetcher.ts
+++ src/utils/fetcher.ts
@@ -1,11 +1,12 @@
+import { datadogRum } from "@datadog/browser-rum"
+
 const fetcher = async (
   resource: string,
   { body, validation, ...init }: Record<string, any> = {}
 ) => {
-  const api =
-    !resource.startsWith("http") && !resource.startsWith("/api")
-      ? process.env.NEXT_PUBLIC_API
-      : ""
+  const isGuildApiCall = !resource.startsWith("http") && !resource.startsWith("/api")
+
+  const api = isGuildApiCall ? process.env.NEXT_PUBLIC_API : ""
 
   const payload = body ?? {}
 
@@ -30,8 +31,17 @@ const fetcher = async (
     },
   }
 
+  if (isGuildApiCall)
+    datadogRum?.addAction("FETCH", { url: `${api}${resource}`, options })
+
   return fetch(`${api}${resource}`, options).then(async (response: Response) => {
     const res = await response.json?.()
+
+    if (!res.ok && isGuildApiCall)
+      datadogRum?.addError("FETCH ERROR", {
+        url: `${api}${resource}`,
+        response: res,
+      })
 
     return response.ok ? res : Promise.reject(res)
   })
