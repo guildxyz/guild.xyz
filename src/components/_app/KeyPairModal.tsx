ddf24b51b0d1c943496fe964b01056c3520f9eeb
Csete Balázs <csetebalu@gmail.com>
2022-07-11 13:17:54 +0200

    keypair modal improvements

--- src/components/_app/KeyPairModal.tsx
+++ src/components/_app/KeyPairModal.tsx
@@ -1,4 +1,10 @@
 import {
+  Accordion,
+  AccordionButton,
+  AccordionIcon,
+  AccordionItem,
+  AccordionPanel,
+  Box,
   Button,
   Modal,
   ModalBody,
@@ -6,6 +12,7 @@ import {
   ModalFooter,
   ModalHeader,
   ModalOverlay,
+  Text,
 } from "@chakra-ui/react"
 import useKeyPair from "hooks/useKeyPair"
 
@@ -22,11 +29,47 @@ const KeyPairModal = ({ children }) => {
         onClose={() => {}}
       >
         <ModalOverlay />
-        <ModalContent>
+        <ModalContent minW="xl">
           <ModalHeader>Remember wallet</ModalHeader>
           <ModalBody>
-            Skip approving every interaction with your wallet by allowing Guild to
-            remember you
+            <Text>
+              When clicking{" "}
+              <Text as="span" fontStyle={"italic"}>
+                Remember my wallet
+              </Text>
+              , you will be requested a signature, which is needed to assign you a
+              unique signing key pair. With this key pair, you only have to sign once
+              now, not every time you interact with Guild.
+            </Text>
+
+            {/* <Alert status="info" mt={5}>
+              <AlertIcon />
+              The key pair can only be used to sign Guild interactions, so any funds
+              on your wallet are safe, and it is stored safely in your browser, so no
+              one can export your private key.
+            </Alert> */}
+
+            <Accordion
+              mt={5}
+              allowToggle
+              backgroundColor={"whiteAlpha.100"}
+              borderRadius="md"
+            >
+              <AccordionItem border={"none"}>
+                <AccordionButton>
+                  <Box flex="1" textAlign="left">
+                    Is this safe?
+                  </Box>
+                  <AccordionIcon />
+                </AccordionButton>
+                <AccordionPanel pb={4}>
+                  The generated key pair can only be used in this browser, and only
+                  for signing messages. It's not possible to export the private key.
+                  The keys can only be used for Guild interactions, any funds on your
+                  wallet are safe.
+                </AccordionPanel>
+              </AccordionItem>
+            </Accordion>
           </ModalBody>
           <ModalFooter>
             <Button
@@ -34,7 +77,7 @@ const KeyPairModal = ({ children }) => {
               isLoading={set.isLoading}
               loadingText="Generating keypair"
             >
-              Remember me
+              Remember my wallet
             </Button>
           </ModalFooter>
         </ModalContent>
