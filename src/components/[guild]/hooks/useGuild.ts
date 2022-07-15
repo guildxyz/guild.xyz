fb1f4aa2293008611e016e9894b38a87e16572ec
Csete Balázs <csetebalu@gmail.com>
2022-07-15 11:08:25 +0200

    call guild endpoint according to admin permission

--- src/components/[guild]/hooks/useGuild.ts
+++ src/components/[guild]/hooks/useGuild.ts
@@ -1,26 +1,39 @@
 import useKeyPair from "hooks/useKeyPair"
 import { useRouter } from "next/router"
+import { useEffect, useState } from "react"
 import useSWR from "swr"
 import { Guild } from "types"
 import fetcher, { useFetcherWithSign } from "utils/fetcher"
+import useUser from "./useUser"
 
 const useGuild = (guildId?: string | number) => {
   const router = useRouter()
 
+  const { addresses } = useUser()
+
   const fetcherWithSign = useFetcherWithSign()
+
+  const [isAdmin, setIsAdmin] = useState<boolean>(false)
 
   const id = guildId ?? router.query.guild
 
   const { ready, keyPair } = useKeyPair()
 
   const swrKey =
-    ready && keyPair
+    ready && keyPair && isAdmin
       ? [`/guild/details/${id}`, { method: "POST", body: {} }]
       : `/guild/${id}`
 
   const fetcherFunction = ready && keyPair ? fetcherWithSign : fetcher
 
   const { data, isValidating } = useSWR<Guild>(id ? swrKey : null, fetcherFunction)
+
+  useEffect(() => {
+    if (!data || !addresses) return
+    setIsAdmin(
+      !!data.admins?.some((admin) => admin.address === addresses[0].toLowerCase())
+    )
+  }, [data, addresses])
 
   return {
     ...data,
