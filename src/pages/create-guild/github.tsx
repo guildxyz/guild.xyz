b4205dd2f8c02682c2c6e811dbb55ea2190e3684
Csete Balázs <csetebalu@gmail.com>
2022-07-29 14:19:55 +0200

    repo search bar

--- src/pages/create-guild/github.tsx
+++ src/pages/create-guild/github.tsx
@@ -3,16 +3,19 @@ import {
   AlertDescription,
   AlertIcon,
   AlertTitle,
+  Box,
   SimpleGrid,
   Spinner,
   VStack,
 } from "@chakra-ui/react"
+import CardMotionWrapper from "components/common/CardMotionWrapper"
 import Layout from "components/common/Layout"
 import RepoCard from "components/create-guild/github/RepoCard"
+import SearchBar from "components/explorer/SearchBar"
 import useUser from "components/[guild]/hooks/useUser"
 import useGateables from "hooks/useGateables"
 import { useRouter } from "next/router"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 const CreateGithubGuild = () => {
   const { platformUsers } = useUser()
@@ -36,6 +39,14 @@ const CreateGithubGuild = () => {
     if (!response) onSubmit({ platformName: "GITHUB" })
   }, [response])
 
+  const [search, setSearch] = useState<string>("")
+
+  const filteredRepos = response?.filter?.((repo) =>
+    [repo.platformGuildId, repo.repositoryName, repo.description].some((prop) =>
+      prop?.toLowerCase()?.includes(search)
+    )
+  )
+
   return (
     <Layout title="Create Guild on GitHub">
       {isLoading || isSigning ? (
@@ -51,11 +62,19 @@ const CreateGithubGuild = () => {
           </VStack>
         </Alert>
       ) : response ? (
-        <SimpleGrid w="full" columns={{ base: 1, md: 2 }} gap={5}>
-          {response?.map((repo) => (
-            <RepoCard key={repo.platformGuildId} {...repo} />
-          ))}
-        </SimpleGrid>
+        <>
+          <Box maxW="lg" mb={8}>
+            <SearchBar placeholder="Search repo" {...{ search, setSearch }} />
+          </Box>
+
+          <SimpleGrid w="full" columns={{ base: 1, md: 2 }} gap={5}>
+            {(filteredRepos ?? response)?.map?.((repo) => (
+              <CardMotionWrapper key={repo.platformGuildId}>
+                <RepoCard {...repo} />
+              </CardMotionWrapper>
+            ))}
+          </SimpleGrid>
+        </>
       ) : null}
     </Layout>
   )
