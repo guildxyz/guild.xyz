18214d0a4c40181cf34b0ea9c5dbc55d65e37712
Kefir <87959318+kef1rke@users.noreply.github.com>
2022-07-30 20:20:19 +0200

    Grouped icon selector (#428)
    * grouped icons selector -styling
    
    * tab without black border
    
    * IconSelector - tabs UI improvements
    
    * Add optional chaining
    
    * Tabs: add horizontal scroll on small screen sizes
    
    * IconSelector: use clear variable name in map
    
    Co-authored-by: BrickheadJohnny <johnny.zgen@gmail.com>

--- src/components/create-guild/IconSelector/IconSelector.tsx
+++ src/components/create-guild/IconSelector/IconSelector.tsx
@@ -1,13 +1,22 @@
 import {
+  Box,
   FormControl,
   FormLabel,
   IconButton,
+  Img,
   ModalBody,
   ModalCloseButton,
   ModalContent,
   ModalHeader,
   ModalOverlay,
   SimpleGrid,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  Text,
+  useColorModeValue,
   useDisclosure,
   useRadioGroup,
 } from "@chakra-ui/react"
@@ -15,10 +24,12 @@ import GuildLogo from "components/common/GuildLogo"
 import { Modal } from "components/common/Modal"
 import LogicDivider from "components/[guild]/LogicDivider"
 import { Uploader } from "hooks/usePinata/usePinata"
+import React, { useEffect } from "react"
 import { useController, useFormContext } from "react-hook-form"
 import { GuildFormType } from "types"
 import PhotoUploader from "./components/PhotoUploader"
 import SelectorButton from "./components/SelectorButton"
+import icons from "./icons.json"
 
 type Props = {
   uploader: Uploader
@@ -42,7 +53,20 @@ const IconSelector = ({ uploader }: Props) => {
     value: field.value,
   })
 
+  const [tabIndex, setTabIndex] = React.useState(0)
+
   const group = getRootProps()
+  useEffect(() => {
+    const svg = field.value?.split("/").pop().split(".")[0]
+    icons.map((category, i) => {
+      if (category.icons.includes(Number(svg))) {
+        setTabIndex(i)
+      }
+    })
+  }, [field.value])
+
+  const tabBgColor = useColorModeValue("gray.100", "gray.600")
+  const guildLogoSxProp = useColorModeValue({ filter: "invert(0.75)" }, null)
 
   return (
     <>
@@ -64,23 +88,72 @@ const IconSelector = ({ uploader }: Props) => {
         <ModalContent>
           <ModalHeader>Choose logo</ModalHeader>
           <ModalCloseButton />
-          <ModalBody>
+          <ModalBody className="custom-scrollbar">
             <PhotoUploader uploader={uploader} closeModal={onClose} />
             <LogicDivider logic="OR" px="0" my="5" />
             <FormControl>
               <FormLabel>Choose from default icons</FormLabel>
-              <SimpleGrid
-                minChildWidth="var(--chakra-sizes-10)"
-                spacing="4"
-                {...group}
+              <Tabs
+                isFitted
+                variant="enclosed"
+                defaultIndex={tabIndex}
+                colorScheme="white"
+                onChange={(index) => setTabIndex(index)}
               >
-                {[...Array(285).keys()].map((i) => {
-                  const radio = getRadioProps({
-                    value: `/guildLogos/${i}.svg`,
-                  })
-                  return <SelectorButton key={i} {...radio} />
-                })}
-              </SimpleGrid>
+                <Box mx={{ base: -4, md: 0 }}>
+                  <TabList
+                    px={{ base: 4, md: 0 }}
+                    maxW="full"
+                    overflowX="auto"
+                    overflowY="hidden"
+                    sx={{
+                      WebkitMaskImage: [
+                        "linear-gradient(to right, transparent 0px, black 20px, black calc(100% - 20px), transparent)",
+                        "linear-gradient(to right, transparent 0px, black 20px, black calc(100% - 20px), transparent)",
+                        "none",
+                      ],
+                    }}
+                    className="invisible-scrollbar"
+                  >
+                    {icons.map((tab, index) => (
+                      <Tab
+                        border={0}
+                        bgColor={index === tabIndex && tabBgColor}
+                        key={index}
+                        minW="max-content"
+                      >
+                        {index === tabIndex && (
+                          <Text as="span" mr={2} fontSize="sm">
+                            {tab.name}
+                          </Text>
+                        )}
+                        <Img
+                          src={`/guildLogos/${tab.logo}.svg`}
+                          sx={guildLogoSxProp}
+                        />
+                      </Tab>
+                    ))}
+                  </TabList>
+                </Box>
+                <TabPanels>
+                  {icons.map((tab, index) => (
+                    <TabPanel px={0} key={index}>
+                      <SimpleGrid
+                        minChildWidth="var(--chakra-sizes-10)"
+                        spacing="4"
+                        {...group}
+                      >
+                        {tab.icons.map((x, i) => {
+                          const radio = getRadioProps({
+                            value: `/guildLogos/${x}.svg`,
+                          })
+                          return <SelectorButton key={i} {...radio} />
+                        })}
+                      </SimpleGrid>
+                    </TabPanel>
+                  ))}
+                </TabPanels>
+              </Tabs>
             </FormControl>
           </ModalBody>
         </ModalContent>
