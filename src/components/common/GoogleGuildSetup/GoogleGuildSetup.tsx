db781df759b4eab065ee5ff2ddd7572988057539
Csete Balázs <csetebalu@gmail.com>
2022-08-01 18:10:55 +0200

    google help in modal

--- src/components/common/GoogleGuildSetup/GoogleGuildSetup.tsx
+++ src/components/common/GoogleGuildSetup/GoogleGuildSetup.tsx
@@ -3,20 +3,26 @@ import {
   GridItem,
   Heading,
   HStack,
-  IconButton,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
   SimpleGrid,
   Spinner,
   Stack,
   Text,
   Tooltip,
   useClipboard,
+  useDisclosure,
   usePrevious,
 } from "@chakra-ui/react"
 import Button from "components/common/Button"
 import useCreateGuild from "components/create-guild/hooks/useCreateGuild"
 import useGuild from "components/[guild]/hooks/useGuild"
 import { AnimatePresence, AnimateSharedLayout } from "framer-motion"
-import { ArrowLeft, CopySimple } from "phosphor-react"
+import { CopySimple } from "phosphor-react"
 import { useEffect, useState } from "react"
 import { useFormContext, useWatch } from "react-hook-form"
 import AddCard from "../AddCard"
@@ -51,26 +57,27 @@ const GoogleGuildSetup = ({
     (file) => !guildPlatformIds.includes(file.platformGuildId)
   )
 
+  const {
+    isOpen: isHelpModalOpen,
+    onClose: onHeloModalClose,
+    onOpen: onHeloModalOpen,
+  } = useDisclosure()
+
   const prevFilteredGoogleGateables = usePrevious(filteredGoogleGateables)
   useEffect(() => {
     if (filteredGoogleGateables?.length > prevFilteredGoogleGateables?.length) {
-      setShowHelp(false)
+      onHeloModalClose()
     }
   }, [prevFilteredGoogleGateables, filteredGoogleGateables])
 
   const { control, setValue, reset } = useFormContext()
   const platformGuildId = useWatch({ control, name: fieldName })
 
-  const guildGoogleEmailAddress = "guild-xyz@guildxyz.iam.gserviceaccount.com"
-  const { hasCopied, onCopy } = useClipboard(guildGoogleEmailAddress)
-
   const selectedFile = filteredGoogleGateables?.find(
     (file) => file.platformGuildId === platformGuildId
   )
 
   const [showForm, setShowForm] = useState(false)
-
-  const [showHelp, setShowHelp] = useState<boolean>(false)
 
   const resetForm = () => {
     reset(defaultValues)
@@ -87,6 +94,8 @@ const GoogleGuildSetup = ({
     else setShowForm(false)
   }, [selectedFile])
 
+  const guildGoogleEmailAddress = "guild-xyz@guildxyz.iam.gserviceaccount.com"
+
   if (isGoogleGateablesLoading)
     return (
       <Flex justifyContent="center">
@@ -94,78 +103,91 @@ const GoogleGuildSetup = ({
       </Flex>
     )
 
-  if (filteredGoogleGateables?.length && !showHelp)
+  if (filteredGoogleGateables?.length)
     return (
-      <SimpleGrid
-        columns={{ base: 1, sm: 2, lg: 3 }}
-        spacing={{ base: 4, md: 6 }}
-        alignItems="stretch"
-      >
-        <AnimateSharedLayout>
-          <AnimatePresence>
-            {(selectedFile ? [selectedFile] : filteredGoogleGateables).map(
-              (file) => (
-                <CardMotionWrapper key={file.platformGuildId}>
-                  <GridItem>
-                    <GoogleDocCard
-                      file={file}
-                      onSelect={
-                        selectedFile
-                          ? undefined
-                          : (newPlatformGuildId: string) => {
-                              setValue(fieldName, newPlatformGuildId)
-                              if (!fieldNameBase?.length)
-                                setValue(`platformGuildName`, file.name)
-                              if (shouldSetName) setValue("name", file.name)
+      <>
+        <SimpleGrid
+          columns={{ base: 1, sm: 2, lg: 3 }}
+          spacing={{ base: 4, md: 6 }}
+          alignItems="stretch"
+        >
+          <AnimateSharedLayout>
+            <AnimatePresence>
+              {(selectedFile ? [selectedFile] : filteredGoogleGateables).map(
+                (file) => (
+                  <CardMotionWrapper key={file.platformGuildId}>
+                    <GridItem>
+                      <GoogleDocCard
+                        file={file}
+                        onSelect={
+                          selectedFile
+                            ? undefined
+                            : (newPlatformGuildId: string) => {
+                                setValue(fieldName, newPlatformGuildId)
+                                if (!fieldNameBase?.length)
+                                  setValue(`platformGuildName`, file.name)
+                                if (shouldSetName) setValue("name", file.name)
 
-                              setValue(
-                                fieldNameBase?.length
-                                  ? `${fieldNameBase}.platformGuildData.mimeType`
-                                  : "platformGuildData.mimeType",
-                                file.mimeType
-                              )
-                              setValue(
-                                fieldNameBase?.length
-                                  ? `${fieldNameBase}.platformGuildData.iconLink`
-                                  : "platformGuildData.iconLink",
-                                file.iconLink
-                              )
-                            }
-                      }
-                      onCancel={
-                        selectedFile?.platformGuildId !== file.platformGuildId
-                          ? undefined
-                          : resetForm
-                      }
-                    />
-                  </GridItem>
-                </CardMotionWrapper>
-              )
-            )}
+                                setValue(
+                                  fieldNameBase?.length
+                                    ? `${fieldNameBase}.platformGuildData.mimeType`
+                                    : "platformGuildData.mimeType",
+                                  file.mimeType
+                                )
+                                setValue(
+                                  fieldNameBase?.length
+                                    ? `${fieldNameBase}.platformGuildData.iconLink`
+                                    : "platformGuildData.iconLink",
+                                  file.iconLink
+                                )
+                              }
+                        }
+                        onCancel={
+                          selectedFile?.platformGuildId !== file.platformGuildId
+                            ? undefined
+                            : resetForm
+                        }
+                      />
+                    </GridItem>
+                  </CardMotionWrapper>
+                )
+              )}
 
-            <CardMotionWrapper key={"add-file"}>
-              <GridItem>
-                <AddCard
-                  text="Add file"
-                  h={"full"}
-                  onClick={() => setShowHelp(true)}
-                />
-              </GridItem>
-            </CardMotionWrapper>
+              <CardMotionWrapper key={"add-file"}>
+                <GridItem>
+                  <AddCard text="Add file" h={"full"} onClick={onHeloModalOpen} />
+                </GridItem>
+              </CardMotionWrapper>
 
-            {showForm && (
-              <GridItem colSpan={2}>
-                <GoogleDocSetupCard
-                  fieldNameBase={fieldNameBase}
-                  onSubmit={id ? onSelect : onSubmit}
-                  isLoading={isLoading || isSigning}
-                  loadingText={signLoadingText ?? "Creating guild"}
-                />
-              </GridItem>
-            )}
-          </AnimatePresence>
-        </AnimateSharedLayout>
-      </SimpleGrid>
+              {showForm && (
+                <GridItem colSpan={2}>
+                  <GoogleDocSetupCard
+                    fieldNameBase={fieldNameBase}
+                    onSubmit={id ? onSelect : onSubmit}
+                    isLoading={isLoading || isSigning}
+                    loadingText={signLoadingText ?? "Creating guild"}
+                  />
+                </GridItem>
+              )}
+            </AnimatePresence>
+          </AnimateSharedLayout>
+        </SimpleGrid>
+
+        <Modal isOpen={isHelpModalOpen} onClose={onHeloModalClose}>
+          <ModalOverlay />
+          <ModalContent
+            minW={{ base: "auto", md: "2xl" }}
+            maxH="xl"
+            overflowY={"auto"}
+          >
+            <ModalHeader>Share your documents with Guild.xyz</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody as={Stack}>
+              <HelpPanel />
+            </ModalBody>
+          </ModalContent>
+        </Modal>
+      </>
     )
 
   // if (ready && keyPair && !filteredGoogleGateables?.length)
@@ -173,7 +195,7 @@ const GoogleGuildSetup = ({
     <Card mx="auto" px={{ base: 5, sm: 6 }} py={7} maxW="container.sm">
       <Stack alignItems="start" spacing={4}>
         <HStack spacing={4} alignItems="center">
-          {showHelp && (
+          {/* {showHelp && (
             <Tooltip label="Back to documents" placement="top">
               <IconButton
                 size="sm"
@@ -183,28 +205,12 @@ const GoogleGuildSetup = ({
                 aria-label="Go back"
               />
             </Tooltip>
-          )}
+          )} */}
           <Heading as="h2" fontSize="xl" fontFamily="display">
             Share your documents with Guild.xyz
           </Heading>
         </HStack>
-        <Text>
-          We couldn't find any gateable documents. Please share your documents with
-          the e-mail address below.
-        </Text>
-        <Tooltip
-          placement="top"
-          label={hasCopied ? "Copied" : "Click to copy address"}
-          closeOnClick={false}
-          hasArrow
-        >
-          <Button onClick={onCopy} variant="outline" rightIcon={<CopySimple />}>
-            {guildGoogleEmailAddress}
-          </Button>
-        </Tooltip>
-        <video src="/videos/google-config-guide.webm" muted autoPlay loop>
-          Your browser does not support the HTML5 video tag.
-        </video>
+        <HelpPanel />
       </Stack>
     </Card>
   )
@@ -246,6 +252,38 @@ const GoogleGuildSetup = ({
   //     </Stack>
   //   </Alert>
   // )
+}
+
+const HelpPanel = () => {
+  const guildGoogleEmailAddress = "guild-xyz@guildxyz.iam.gserviceaccount.com"
+  const { hasCopied, onCopy } = useClipboard(guildGoogleEmailAddress)
+
+  return (
+    <>
+      <Text>
+        Choose what individual document or a folder the new role can access in your
+        Google Workspace. Invite {guildGoogleEmailAddress} official guild.xyz email
+        address as an editor to that file within Google Drive so guild can manage who
+        can view, comment or edit it automatically based on the rules you set in the
+        next steps.
+      </Text>
+      <Text>We can only manage documents you invite this email address to.</Text>
+
+      <Tooltip
+        placement="top"
+        label={hasCopied ? "Copied" : "Click to copy address"}
+        closeOnClick={false}
+        hasArrow
+      >
+        <Button onClick={onCopy} variant="outline" rightIcon={<CopySimple />}>
+          <Text fontSize={{ base: "sm", md: "md" }}>{guildGoogleEmailAddress}</Text>
+        </Button>
+      </Tooltip>
+      <video src="/videos/google-config-guide.webm" muted autoPlay loop>
+        Your browser does not support the HTML5 video tag.
+      </video>
+    </>
+  )
 }
 
 export default GoogleGuildSetup
