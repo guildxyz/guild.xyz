b235f90a5a3ad6235260f578a1722038347164c6
Csete Balázs <csetebalu@gmail.com>
2022-08-01 17:37:36 +0200

    google - doc card spacing fix

--- src/components/common/GoogleGuildSetup/components/GoogleDocCard.tsx
+++ src/components/common/GoogleGuildSetup/components/GoogleDocCard.tsx
@@ -21,8 +21,8 @@ const GoogleDocCard = ({ file, onSelect, onCancel }: Props): JSX.Element => {
   const imageBgColor = useColorModeValue("gray.100", "gray.800")
 
   return (
-    <Card px={{ base: 5, sm: 6 }} py="7">
-      <Stack w="full" spacing={4}>
+    <Card px={{ base: 5, sm: 6 }} py="7" h="full">
+      <Stack w="full" spacing={4} justifyContent="space-between" h="full">
         <HStack>
           <Circle size={10} bgColor={imageBgColor}>
             <Img src={file.iconLink} alt={file.mimeType} />
