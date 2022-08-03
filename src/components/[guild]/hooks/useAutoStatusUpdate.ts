39c78a022e18979c03f6d2e9a7d035aeb69415f8
Csete Balázs <csetebalu@gmail.com>
2022-08-03 18:47:39 +0200

    fix: automatic statusUpdate on access fetch error

--- src/components/[guild]/hooks/useAutoStatusUpdate.ts
+++ src/components/[guild]/hooks/useAutoStatusUpdate.ts
@@ -1,4 +1,4 @@
-import { useRumAction } from "@datadog/rum-react-integration"
+import { useRumAction, useRumError } from "@datadog/rum-react-integration"
 import { useWeb3React } from "@web3-react/core"
 import useMemberships from "components/explorer/hooks/useMemberships"
 import { useEffect } from "react"
@@ -9,10 +9,11 @@ import useGuild from "./useGuild"
 
 const useAutoStatusUpdate = () => {
   const addDatadogAction = useRumAction("trackingAppAction")
+  const addDatadogError = useRumError()
   const { account } = useWeb3React()
   const { id } = useGuild()
 
-  const { data: roleAccesses } = useAccess()
+  const { data: roleAccesses, error } = useAccess()
   const memberships = useMemberships()
 
   const roleMemberships = memberships?.find(
@@ -20,30 +21,36 @@ const useAutoStatusUpdate = () => {
   )?.roleIds
 
   useEffect(() => {
-    if (!account || !Array.isArray(roleAccesses) || !Array.isArray(roleMemberships))
-      return
+    try {
+      const accesses = roleAccesses ?? error
 
-    const roleMembershipsSet = new Set(roleMemberships)
+      if (!account || !Array.isArray(accesses) || !Array.isArray(roleMemberships))
+        return
 
-    const accessedRoleIds = roleAccesses
-      .filter(({ access }) => !!access)
-      .map(({ roleId }) => roleId)
+      const roleMembershipsSet = new Set(roleMemberships)
 
-    const isMemberInEveryAccessedRole =
-      accessedRoleIds.every((accessedRoleId) =>
-        roleMembershipsSet.has(accessedRoleId)
-      ) && roleMemberships.every((roleId) => accessedRoleIds.includes(roleId))
+      const accessedRoleIds = accesses
+        .filter(({ access }) => !!access)
+        .map(({ roleId }) => roleId)
 
-    if (!isMemberInEveryAccessedRole) {
-      addDatadogAction("Automatic statusUpdate")
-      fetcher(`/user/${account}/statusUpdate/${id}`).then(() =>
-        Promise.all([
-          swrMutate(`/guild/access/${id}/${account}`),
-          swrMutate(`/user/membership/${account}`),
-        ])
-      )
+      const isMemberInEveryAccessedRole =
+        accessedRoleIds.every((accessedRoleId) =>
+          roleMembershipsSet.has(accessedRoleId)
+        ) && roleMemberships.every((roleId) => accessedRoleIds.includes(roleId))
+
+      if (!isMemberInEveryAccessedRole) {
+        addDatadogAction("Automatic statusUpdate")
+        fetcher(`/user/${account}/statusUpdate/${id}`).then(() =>
+          Promise.all([
+            swrMutate(`/guild/access/${id}/${account}`),
+            swrMutate(`/user/membership/${account}`),
+          ])
+        )
+      }
+    } catch (err) {
+      addDatadogError("Automatic statusUpdate error", { error: err }, "custom")
     }
-  }, [roleAccesses, roleMemberships, account, id])
+  }, [roleAccesses, roleMemberships, account, id, error])
 }
 
 export default useAutoStatusUpdate
