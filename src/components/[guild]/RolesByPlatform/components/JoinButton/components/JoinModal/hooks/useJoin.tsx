387f7fb1aa9240648630407916af5fafeba80695
valid <valid@zgen.hu>
2022-08-05 02:25:54 +0200

    join success instant AccessHub & toast

--- src/components/[guild]/RolesByPlatform/components/JoinButton/components/JoinModal/hooks/useJoin.tsx
+++ src/components/[guild]/RolesByPlatform/components/JoinButton/components/JoinModal/hooks/useJoin.tsx
@@ -1,9 +1,16 @@
+import { Text, ToastId, useColorModeValue } from "@chakra-ui/react"
 import { useRumAction, useRumError } from "@datadog/rum-react-integration"
 import { useWeb3React } from "@web3-react/core"
+import Button from "components/common/Button"
 import useGuild from "components/[guild]/hooks/useGuild"
 import useUser from "components/[guild]/hooks/useUser"
 import { manageKeyPairAfterUserMerge } from "hooks/useKeyPair"
 import { useSubmitWithSign, WithValidation } from "hooks/useSubmit"
+import useToast from "hooks/useToast"
+import { useRouter } from "next/router"
+import { TwitterLogo } from "phosphor-react"
+import { useRef } from "react"
+import { mutate } from "swr"
 import { PlatformName, PlatformType } from "types"
 import fetcher, { useFetcherWithSign } from "utils/fetcher"
 
@@ -32,7 +39,8 @@ export type JoinData =
       hash: string
     }
 
-const useJoin = () => {
+const useJoin = (onSuccess?: () => void) => {
+  const router = useRouter()
   const { account } = useWeb3React()
   const addDatadogAction = useRumAction("trackingAppAction")
   const addDatadogError = useRumError()
@@ -40,6 +48,10 @@ const useJoin = () => {
   const guild = useGuild()
   const user = useUser()
   const fetcherWithSign = useFetcherWithSign()
+
+  const toast = useToast()
+  const toastIdRef = useRef<ToastId>()
+  const tweetButtonBackground = useColorModeValue("blackAlpha.100", undefined)
 
   const submit = ({
     data,
@@ -68,6 +80,42 @@ const useJoin = () => {
     // Revalidating the address list in the AccountModal component
     onSuccess: () => {
       addDatadogAction(`Successfully joined a guild`)
+
+      mutate(`/user/membership/${account}`)
+      // show user in guild's members
+      mutate(`/guild/${router.query.guild}`)
+      // show in account modal if new platform/address got connected
+      mutate(`/user/${account}`)
+
+      toastIdRef.current = toast({
+        title: `Successfully joined guild`,
+        duration: 8000,
+        description: (
+          <>
+            <Text>Let others know as well by sharing it on Twitter</Text>
+            <Button
+              as="a"
+              href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(
+                `Just joined the ${guild.name} guild. Continuing my brave quest to explore all corners of web3!
+guild.xyz/${guild.urlName} @guildxyz`
+              )}`}
+              target="_blank"
+              bg={tweetButtonBackground}
+              leftIcon={<TwitterLogo weight="fill" />}
+              size="sm"
+              onClick={() => toast.close(toastIdRef.current)}
+              mt={3}
+              mb="1"
+              borderRadius="lg"
+            >
+              Share
+            </Button>
+          </>
+        ),
+        status: "success",
+      })
+
+      onSuccess?.()
     },
     onError: (err) => {
       addDatadogError(`Guild join error`, { error: err }, "custom")
