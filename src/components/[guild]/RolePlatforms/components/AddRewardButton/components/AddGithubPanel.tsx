55d645a1493a42efa3c61a0969ad20e35149c3c6
Csete Balázs <csetebalu@gmail.com>
2022-08-10 18:13:24 +0200

    GitHubGuildSetup WIP

--- src/components/[guild]/RolePlatforms/components/AddRewardButton/components/AddGithubPanel.tsx
+++ src/components/[guild]/RolePlatforms/components/AddRewardButton/components/AddGithubPanel.tsx
@@ -1,22 +1,4 @@
-import {
-  Alert,
-  AlertDescription,
-  AlertIcon,
-  AlertTitle,
-  Box,
-  Button,
-  GridItem,
-  SimpleGrid,
-  Spinner,
-  VStack,
-} from "@chakra-ui/react"
-import CardMotionWrapper from "components/common/CardMotionWrapper"
-import RepoCard from "components/create-guild/github/RepoCard"
-import SearchBar from "components/explorer/SearchBar"
-import useGateables from "hooks/useGateables"
-import Link from "next/link"
-import { ArrowSquareOut } from "phosphor-react"
-import { useState } from "react"
+import GitHubGuildSetup from "components/common/GitHubGuildSetup"
 import { FormProvider, useFieldArray, useForm } from "react-hook-form"
 
 type Props = {
@@ -34,85 +16,20 @@ const AddGithubPanel = ({ onClose }: Props) => {
     name: "rolePlatforms",
   })
 
-  // const platformGuildId = useWatch({
-  //   control: methods.control,
-  //   name: `platformGuildId`,
-  // })
-
-  const { gateables, isLoading, error } = useGateables("GITHUB")
-
-  const [search, setSearch] = useState<string>("")
-
-  const filteredRepos = gateables?.filter?.((repo) =>
-    [repo.platformGuildId, repo.repositoryName, repo.description].some((prop) =>
-      prop?.toLowerCase()?.includes(search)
-    )
-  )
-
   return (
     <FormProvider {...methods}>
-      {isLoading ? (
-        <Spinner />
-      ) : error ? (
-        <Alert status="error">
-          <AlertIcon />
-          <VStack alignItems="start">
-            <AlertTitle>GitHub error</AlertTitle>
-            <AlertDescription>
-              Failed to retrieve repositories, try disconnecting your GitHub account
-            </AlertDescription>
-          </VStack>
-        </Alert>
-      ) : gateables?.length > 0 ? (
-        <>
-          <Box maxW="lg" mb={8}>
-            <SearchBar placeholder="Search repo" {...{ search, setSearch }} />
-          </Box>
-
-          <SimpleGrid columns={{ base: 1, md: 2 }} spacing={{ base: 4, md: 6 }}>
-            {(filteredRepos ?? gateables)?.map?.((repo) => (
-              <CardMotionWrapper key={repo.platformGuildId}>
-                <GridItem>
-                  <RepoCard
-                    {...repo}
-                    onSelection={(platformGuildId) => {
-                      append({
-                        guildPlatform: {
-                          platformName: "GITHUB",
-                          platformGuildId: encodeURIComponent(platformGuildId),
-                          isNew: true,
-                        },
-                      })
-                      onClose()
-                    }}
-                  />
-                </GridItem>
-              </CardMotionWrapper>
-            ))}
-          </SimpleGrid>
-        </>
-      ) : (
-        <Alert status="error">
-          <AlertIcon />
-          <VStack alignItems={"start"}>
-            <AlertTitle>No repositories</AlertTitle>
-            <AlertDescription>
-              It looks like you don't have any repositories yet. Create one and
-              return here to gate access to it!
-            </AlertDescription>
-            <Link passHref href="https://github.com/new">
-              <Button
-                as="a"
-                target={"_blank"}
-                size="sm"
-                rightIcon={<ArrowSquareOut />}
-              >
-                Create a repository
-              </Button>
-            </Link>
-          </VStack>
-        </Alert>
-      )}
+      <GitHubGuildSetup
+        onSelection={(platformGuildId) => {
+          append({
+            guildPlatform: {
+              platformName: "GITHUB",
+              platformGuildId: encodeURIComponent(platformGuildId),
+              isNew: true,
+            },
+          })
+          onClose()
+        }}
+      />
     </FormProvider>
   )
 }
--- src/pages/create-guild/github.tsx
+++ src/pages/create-guild/github.tsx
@@ -1,24 +1,8 @@
-import {
-  Alert,
-  AlertDescription,
-  AlertIcon,
-  AlertTitle,
-  Box,
-  Button,
-  SimpleGrid,
-  Spinner,
-  VStack,
-} from "@chakra-ui/react"
-import CardMotionWrapper from "components/common/CardMotionWrapper"
+import GitHubGuildSetup from "components/common/GitHubGuildSetup"
 import Layout from "components/common/Layout"
-import RepoCard from "components/create-guild/github/RepoCard"
-import SearchBar from "components/explorer/SearchBar"
 import useUser from "components/[guild]/hooks/useUser"
-import useGateables from "hooks/useGateables"
-import Link from "next/link"
 import { useRouter } from "next/router"
-import { ArrowSquareOut } from "phosphor-react"
-import { useEffect, useState } from "react"
+import { useEffect } from "react"
 
 const CreateGithubGuild = () => {
   const { platformUsers } = useUser()
@@ -33,66 +17,9 @@ const CreateGithubGuild = () => {
     }
   }, [platformUsers, isGithubConnected])
 
-  const { isLoading, error, gateables } = useGateables("GITHUB")
-
-  const [search, setSearch] = useState<string>("")
-
-  const filteredRepos = gateables?.filter?.((repo) =>
-    [repo.platformGuildId, repo.repositoryName, repo.description].some((prop) =>
-      prop?.toLowerCase()?.includes(search)
-    )
-  )
-
   return (
     <Layout title="Create Guild on GitHub">
-      {isLoading ? (
-        <Spinner />
-      ) : error ? (
-        <Alert status="error">
-          <AlertIcon />
-          <VStack alignItems="start">
-            <AlertTitle>GitHub error</AlertTitle>
-            <AlertDescription>
-              Failed to retrieve repositories, try disconnecting your GitHub account
-            </AlertDescription>
-          </VStack>
-        </Alert>
-      ) : gateables?.length > 0 ? (
-        <>
-          <Box maxW="lg" mb={8}>
-            <SearchBar placeholder="Search repo" {...{ search, setSearch }} />
-          </Box>
-
-          <SimpleGrid w="full" columns={{ base: 1, md: 2 }} gap={5}>
-            {(filteredRepos ?? gateables)?.map?.((repo) => (
-              <CardMotionWrapper key={repo.platformGuildId}>
-                <RepoCard {...repo} />
-              </CardMotionWrapper>
-            ))}
-          </SimpleGrid>
-        </>
-      ) : (
-        <Alert status="error">
-          <AlertIcon />
-          <VStack alignItems={"start"}>
-            <AlertTitle>No repositories</AlertTitle>
-            <AlertDescription>
-              It looks like you don't have any repositories yet. Create one and
-              return here to gate access to it!
-            </AlertDescription>
-            <Link passHref href="https://github.com/new">
-              <Button
-                as="a"
-                target={"_blank"}
-                size="sm"
-                rightIcon={<ArrowSquareOut />}
-              >
-                Create a repository
-              </Button>
-            </Link>
-          </VStack>
-        </Alert>
-      )}
+      <GitHubGuildSetup />
     </Layout>
   )
 }
