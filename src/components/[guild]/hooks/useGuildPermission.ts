6b8b1558f1a906e3bfdeb3f103ecfb0ca4775482
BrickheadJohnny <johnny.zgen@gmail.com>
2022-08-10 09:34:13 +0200

    useGuildPermission - fetch isSuperAdmin

--- src/components/[guild]/hooks/useGuildPermission.ts
+++ src/components/[guild]/hooks/useGuildPermission.ts
@@ -1,9 +1,16 @@
+import { useWeb3React } from "@web3-react/core"
 import useGuild from "components/[guild]/hooks/useGuild"
+import useSWRImmutable from "swr/immutable"
 import useUser from "./useUser"
 
 const useGuildPermission = () => {
+  const { account } = useWeb3React()
   const { id } = useUser()
   const guild = useGuild()
+
+  const { data: isSuperAdmin } = useSWRImmutable(
+    account ? `/guild/isSuperAdmin/${account}` : null
+  )
 
   if (!Array.isArray(guild.admins) || typeof id !== "number")
     return { isAdmin: false, isOwner: false }
@@ -11,7 +18,7 @@ const useGuildPermission = () => {
   const admin = guild.admins.find((a) => a?.id === id)
 
   return {
-    isAdmin: !!admin,
+    isAdmin: !!admin || isSuperAdmin,
     isOwner: !!admin?.isOwner,
   }
 }
