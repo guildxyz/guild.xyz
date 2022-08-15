4a295c7f4d3da66195b5bf8b880e9b8dc87e74d1
kef1rke <kocsisbarnabas1@gmail.com>
2022-08-15 16:12:55 +0200

    discord skeleton card

--- src/components/common/DiscordGuildSetup.tsx
+++ src/components/common/DiscordGuildSetup.tsx
@@ -1,4 +1,14 @@
-import { GridItem, HStack, SimpleGrid, Spinner, Text } from "@chakra-ui/react"
+import {
+  Center,
+  ChakraProps,
+  Grid,
+  GridItem,
+  HStack,
+  SimpleGrid,
+  Skeleton,
+  SkeletonCircle,
+} from "@chakra-ui/react"
+import Card from "components/common/Card"
 import CardMotionWrapper from "components/common/CardMotionWrapper"
 import ErrorAlert from "components/common/ErrorAlert"
 import DCServerCard from "components/guard/setup/DCServerCard"
@@ -52,13 +62,44 @@ const DiscordGuildSetup = ({
 
   useEffect(() => console.log(rolePlatforms), [rolePlatforms])
 
+  type Props = {
+    size?: "md" | "lg"
+  } & ChakraProps
+
   if (((!servers || servers.length <= 0) && isValidating) || !authorization) {
     return (
-      <HStack spacing="6" py="5">
-        <Spinner size="md" />
-        <Text fontSize="lg">Loading servers...</Text>
-      </HStack>
+      <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} spacing={{ base: 4, md: 6 }}>
+        {[...Array(5)].map(() => {
+          return (
+            <GridItem>
+              <Card>
+                <Center py={{ base: 12, md: 20 }} bg="gray.600">
+                  <SkeletonCircle pos="absolute" boxSize={{ base: 20, md: 24 }} />
+                </Center>
+                <HStack px={{ base: 5, md: 4 }} py={5} spacing={6}>
+                  <Grid w="full" gap={2.5}>
+                    <Skeleton h={3} maxWidth={250} />
+                    <Skeleton h={2} w={50} />
+                  </Grid>
+                  <Skeleton
+                    h={10}
+                    borderRadius="xl"
+                    w={210}
+                    maxWidth={220}
+                    opacity={0.4}
+                  />
+                </HStack>
+              </Card>
+            </GridItem>
+          )
+        })}
+      </SimpleGrid>
     )
+
+    // <HStack spacing="6" py="5">
+    //   <Spinner size="md" />
+    //   <Text fontSize="lg">Loading servers...</Text>
+    // </HStack>
   }
 
   if (servers?.length <= 0) {
@@ -68,41 +109,45 @@ const DiscordGuildSetup = ({
   }
 
   return (
-    <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} spacing={{ base: 4, md: 6 }}>
-      <AnimatePresence>
-        {(selectedServerOption ? [selectedServerOption] : servers ?? [])
-          .filter(
-            guildPlatformsOfRole
-              ? (serverData) =>
-                  guildPlatformsOfRole?.every(
-                    (gp) => gp.platformGuildId != serverData.id
-                  )
-              : () => true
-          )
-          .map((serverData) => (
-            <CardMotionWrapper key={serverData.id}>
-              <GridItem>
-                <DCServerCard
-                  serverData={serverData}
-                  onSelect={
-                    selectedServer
-                      ? undefined
-                      : (newServerId) => setValue(fieldName, newServerId)
-                  }
-                  onCancel={
-                    selectedServer !== serverData.id ? undefined : () => resetForm()
-                  }
-                />
-              </GridItem>
-            </CardMotionWrapper>
-          ))}
-      </AnimatePresence>
-      {showForm && (
-        <GridItem colSpan={2}>
-          <ServerSetupCard onSubmit={onSubmit}>{children}</ServerSetupCard>
-        </GridItem>
-      )}
-    </SimpleGrid>
+    <>
+      <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} spacing={{ base: 4, md: 6 }}>
+        <AnimatePresence>
+          {(selectedServerOption ? [selectedServerOption] : servers ?? [])
+            .filter(
+              guildPlatformsOfRole
+                ? (serverData) =>
+                    guildPlatformsOfRole?.every(
+                      (gp) => gp.platformGuildId != serverData.id
+                    )
+                : () => true
+            )
+            .map((serverData) => (
+              <CardMotionWrapper key={serverData.id}>
+                <GridItem>
+                  <DCServerCard
+                    serverData={serverData}
+                    onSelect={
+                      selectedServer
+                        ? undefined
+                        : (newServerId) => setValue(fieldName, newServerId)
+                    }
+                    onCancel={
+                      selectedServer !== serverData.id
+                        ? undefined
+                        : () => resetForm()
+                    }
+                  />
+                </GridItem>
+              </CardMotionWrapper>
+            ))}
+        </AnimatePresence>
+        {showForm && (
+          <GridItem colSpan={2}>
+            <ServerSetupCard onSubmit={onSubmit}>{children}</ServerSetupCard>
+          </GridItem>
+        )}
+      </SimpleGrid>
+    </>
   )
 }
 
