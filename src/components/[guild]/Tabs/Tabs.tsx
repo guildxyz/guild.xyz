7b47dfa75c162a0fb87447396815ef50e3b7211f
kef1rke <kocsisbarnabas1@gmail.com>
2022-09-06 00:03:03 +0200

    wip: almost the solution for top menu glitch

--- src/components/[guild]/Tabs/Tabs.tsx
+++ src/components/[guild]/Tabs/Tabs.tsx
@@ -16,6 +16,7 @@ type Props = {
 
 const Tabs = ({ tabTitle, children }: PropsWithChildren<Props>): JSX.Element => {
   const tabsRef = useRef()
+  const seged = useRef()
   const [isSticky, setIsSticky] = useState(false)
   const { colorMode } = useColorMode()
   const { textColor } = useThemeContext()
@@ -29,7 +30,7 @@ const Tabs = ({ tabTitle, children }: PropsWithChildren<Props>): JSX.Element => 
       const current = tabsRef.current || null
       const rect = current?.getBoundingClientRect()
 
-      setIsSticky(rect?.top <= 0)
+      if (rect?.top <= 0) setIsSticky(true)
     }
 
     window.addEventListener("scroll", handleScroll)
@@ -39,72 +40,91 @@ const Tabs = ({ tabTitle, children }: PropsWithChildren<Props>): JSX.Element => 
     }
   }, [])
 
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        //console.info(entries[0].intersectionRatio)
+        // no intersection with screen
+        if (entries[0].intersectionRatio == 0) setIsSticky(true)
+
+        // fully intersects with screen
+        //else if (entries[0].intersectionRatio === 1) setIsSticky(false)
+      },
+      { threshold: 0 }
+    )
+    observer.observe(seged.current)
+  }, [])
+
   return (
-    <Stack
-      ref={tabsRef}
-      direction="row"
-      justifyContent="space-between"
-      alignItems={"center"}
-      position="sticky"
-      top={0}
-      py={3}
-      mt={-3}
-      mb={2}
-      width="full"
-      zIndex={isSticky ? "banner" : "auto"}
-      _before={{
-        content: `""`,
-        position: "fixed",
-        top: 0,
-        left: 0,
-        width: "full",
-        // button height + padding
-        height: "calc(var(--chakra-space-11) + (2 * var(--chakra-space-3)))",
-        bgColor: bgColor,
-        boxShadow: "md",
-        transition: "opacity 0.2s ease, visibility 0.1s ease",
-        visibility: isSticky ? "visible" : "hidden",
-        opacity: isSticky ? 1 : 0,
-      }}
-    >
-      <Box
-        position="relative"
-        ml={-8}
-        minW="0"
-        sx={{
-          "-webkit-mask-image":
-            "linear-gradient(to right, transparent 0px, black 40px, black calc(100% - 40px), transparent)",
+    <>
+      <Box ref={seged} mt={-3} mb={2} zIndex={1000} h="0px" visibility="hidden" />
+      {/* {console.info(isSticky)} */}
+      <Stack
+        ref={tabsRef}
+        direction="row"
+        justifyContent="space-between"
+        alignItems={"center"}
+        position="sticky"
+        top={0}
+        py={3}
+        mt={-3}
+        mb={2}
+        width="full"
+        zIndex={isSticky ? "banner" : "auto"}
+        _before={{
+          content: `""`,
+          position: "fixed",
+          top: 0,
+          left: 0,
+          width: "full",
+          // button height + padding
+          height: "calc(var(--chakra-space-11) + (2 * var(--chakra-space-3)))",
+          bgColor: bgColor,
+          boxShadow: "md",
+          transition: isSticky ? "opacity 0.2s ease, visibility 0.1s ease" : "",
+          visibility: isSticky ? "visible" : "hidden",
+          opacity: isSticky ? 1 : 0,
         }}
       >
-        <HStack
-          overflowX="auto"
-          px={8}
-          color={tabButtonColor}
+        <Box
+          position="relative"
+          ml={-8}
+          minW="0"
           sx={{
-            "&::-webkit-scrollbar": {
-              display: "none",
-            },
-            scrollbarWidth: "none",
+            "-webkit-mask-image":
+              "linear-gradient(to right, transparent 0px, black 40px, black calc(100% - 40px), transparent)",
           }}
         >
-          <TabButton href={`${urlName}`}>{tabTitle}</TabButton>
-          {/* <TabButton href="#" disabled tooltipText="Stay tuned!">
+          <HStack
+            overflowX="auto"
+            px={8}
+            color={tabButtonColor}
+            sx={{
+              "&::-webkit-scrollbar": {
+                display: "none",
+              },
+              scrollbarWidth: "none",
+            }}
+          >
+            <TabButton href={`${urlName}`}>{tabTitle}</TabButton>
+            {/* <TabButton href="#" disabled tooltipText="Stay tuned!">
             More tabs soon
           </TabButton> */}
-        </HStack>
-      </Box>
+          </HStack>
+        </Box>
 
-      <Box
-        color={tabButtonColor}
-        sx={{
-          "> *": {
-            color: tabButtonColor,
-          },
-        }}
-      >
-        {children}
-      </Box>
-    </Stack>
+        <Box
+          color={tabButtonColor}
+          sx={{
+            "> *": {
+              color: tabButtonColor,
+            },
+          }}
+        >
+          {children}
+        </Box>
+      </Stack>
+    </>
   )
 }
 
