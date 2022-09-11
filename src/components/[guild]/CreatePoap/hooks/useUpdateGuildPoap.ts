4b792754923f80c7e48f03c64a211ec24653dfba
BrickheadJohnny <johnny.zgen@gmail.com>
2022-09-11 13:50:40 +0200

    Mutate POAP data on successful update

--- src/components/[guild]/CreatePoap/hooks/useUpdateGuildPoap.ts
+++ src/components/[guild]/CreatePoap/hooks/useUpdateGuildPoap.ts
@@ -1,8 +1,11 @@
+import useGuild from "components/[guild]/hooks/useGuild"
+import usePoap from "components/[guild]/Requirements/components/PoapRequirementCard/hooks/usePoap"
 import useShowErrorToast from "hooks/useShowErrorToast"
 import useSubmit from "hooks/useSubmit"
 import useToast from "hooks/useToast"
 import { GuildPoap } from "types"
 import fetcher from "utils/fetcher"
+import { useCreatePoapContext } from "../components/CreatePoapContext"
 
 type UpdatePoapData = { id: number; expiryDate: number }
 
@@ -16,13 +19,22 @@ const useUpdateGuildPoap = () => {
   const showErrorToast = useShowErrorToast()
   const toast = useToast()
 
+  const { mutateGuild } = useGuild()
+  const { poapData, setPoapData } = useCreatePoapContext()
+  const { poap, mutatePoap } = usePoap(poapData?.fancy_id)
+
   return useSubmit<UpdatePoapData, GuildPoap>(updateGuildPoap, {
     onError: (error) => showErrorToast(error?.error?.message ?? error?.error),
-    onSuccess: () =>
+    onSuccess: async () => {
+      // Mutating guild and POAP data, so the user can see the fresh data in the POAPs list
+      await mutateGuild()
+      await mutatePoap()
+      setPoapData(poap as any)
       toast({
         status: "success",
         title: "Successfully updated POAP",
-      }),
+      })
+    },
   })
 }
 
