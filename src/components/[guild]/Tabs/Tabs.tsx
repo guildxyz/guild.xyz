371c63859f7e34ba7e193dc9ebd68cf7b597b685
kef1rke <kocsisbarnabas1@gmail.com>
2022-09-14 23:38:40 +0200

    wip: solution 1,2,3,4

--- src/components/[guild]/Tabs/Tabs.tsx
+++ src/components/[guild]/Tabs/Tabs.tsx
@@ -16,7 +16,6 @@ type Props = {
 
 const Tabs = ({ tabTitle, children }: PropsWithChildren<Props>): JSX.Element => {
   const tabsRef = useRef()
-  const seged = useRef()
   const [isSticky, setIsSticky] = useState(false)
   const { colorMode } = useColorMode()
   const { textColor } = useThemeContext()
@@ -24,13 +23,33 @@ const Tabs = ({ tabTitle, children }: PropsWithChildren<Props>): JSX.Element => 
 
   const { urlName } = useGuild()
   const bgColor = useColorModeValue("white", "gray.800")
+  const [lastScrollTop, setLastScrollTop] = useState(0)
+
+  //   window.addEventListener("scroll", () => {
+  //     const st = window.pageYOffset || document.documentElement.scrollTop
+  //     if (st > lastScrollTop) {
+  //       handleScroll(true)
+  //     } else {
+  //       handleScroll(false)
+  //     }
+  //     setLastScrollTop(st <= 0 ? 0 : st) // For Mobile or negative scrolling
+  //   })
+
+  //   return () => {
+  //     window.removeEventListener("scroll", handleScroll)
+  //   }
+  // }, [])
 
   // useEffect(() => {
+  //   const current = tabsRef.current || null
+  //   const defaultOffsetTop = current.offsetTop
+  //   console.info(defaultOffsetTop)
   //   const handleScroll = () => {
-  //     const current = tabsRef.current || null
-  //     const rect = current?.getBoundingClientRect()
-
-  //     setIsSticky(rect?.top <= 0)
+  //     const rect = current.offsetTop
+  //     console.info(rect)
+  //     //console.info(current.position.top)
+  //     //console.info(current)
+  //     setIsSticky(rect > defaultOffsetTop)
   //   }
 
   //   window.addEventListener("scroll", handleScroll)
@@ -40,32 +59,14 @@ const Tabs = ({ tabTitle, children }: PropsWithChildren<Props>): JSX.Element => 
   //   }
   // }, [])
 
-  //const current = tabsRef.current || null
   // useEffect(() => {
-  //   document.documentElement.style.setProperty("--visibility", "hidden")
-  //   const observer = new IntersectionObserver(
-  //     (entries) => {
-  //       //console.info(entries[0].intersectionRatio)
-  //       // no intersection with screen
-  //       if (entries[0].intersectionRatio === 0) current.classList.add("data-before")
-  //       // fully intersects with screen
-  //       else if (entries[0].intersectionRatio === 1)
-  //         current.classList.remove("data-before")
-  //       // fully intersects with screen
-  //       //else if (entries[0].intersectionRatio === 1) setIsSticky(false)
-  //     },
-  //     { threshold: 0 }
-  //   )
-  //   observer.observe(seged.current)
-  // }, [])
-
-  // useEffect(() => {
-  //   const current = tabsRef.current || null
+  //   const current = document.querySelectorAll("#tabsRef")[0]
   //   const observer = new IntersectionObserver(
   //     ([e]) => {
   //       console.info(e)
   //       if (e.intersectionRatio < 1) {
-  //       } else document.documentElement.style.setProperty("--visibility", "hidden")
+  //         setIsSticky(true)
+  //       } else setIsSticky(false)
   //     },
   //     { threshold: [1] }
   //   )
@@ -73,76 +74,84 @@ const Tabs = ({ tabTitle, children }: PropsWithChildren<Props>): JSX.Element => 
   //   observer.observe(current)
   // }, [])
 
+  // useEffect(() => {
+  //   const current = tabsRef.current || null
+  //   window.requestAnimationFrame(() => {
+  //     current.scrollTo({ left: 100, behavior: "smooth" })
+  //   })
+  //   setTimeout(() => (current.style.overflow = "hidden"), 500)
+  // }, [])
+  // const { ref, inView } = useInView({ threshold: 1 })
+
   return (
-    <>
-      {/* <Box ref={seged} mt={-3} mb={2} zIndex={1000} h="0px" visibility="hidden" /> */}
-      {/* {console.info(isSticky)} */}
-      <Stack
-        ref={tabsRef}
-        direction="row"
-        justifyContent="space-between"
-        alignItems={"center"}
-        position="sticky"
-        top="-1px"
-        py={3}
-        mt={-3}
-        mb={2}
-        width="full"
-        zIndex="banner"
-        _before={{
-          content: `""`,
-          position: "fixed",
-          top: 0,
-          left: 0,
-          width: "full",
-          // button height + padding
-          height: "calc(var(--chakra-space-11) + (2 * var(--chakra-space-3)))",
-          bgColor: bgColor,
-          boxShadow: "md",
-          //transition: "opacity 0.2s ease, visibility 0.1s ease",
-          visibility: "hidden",
-          opacity: 1,
+    <Stack
+      ref={tabsRef}
+      id="tabsRef"
+      direction="row"
+      justifyContent="space-between"
+      alignItems={"center"}
+      position="sticky"
+      top={0}
+      py={3}
+      mt={-3}
+      mb={2}
+      width="full"
+      zIndex="banner"
+      overflowX="auto"
+      scroll-behavior="smooth"
+      _before={{
+        content: `""`,
+        position: "fixed",
+        top: 0,
+        left: 0,
+        width: "full",
+        // button height + padding
+        height: "calc(var(--chakra-space-11) + (2 * var(--chakra-space-3)))",
+        bgColor: bgColor,
+        boxShadow: "md",
+        transition: "opacity 0.2s ease, visibility 0.1s ease",
+        visibility: isSticky ? "visible" : "hidden",
+        opacity: isSticky ? 1 : 0,
+      }}
+    >
+      <Box
+        position="relative"
+        ml={-8}
+        minW="0"
+        sx={{
+          "-webkit-mask-image":
+            "linear-gradient(to right, transparent 0px, black 40px, black calc(100% - 40px), transparent)",
         }}
       >
-        <Box
-          position="relative"
-          ml={-8}
-          minW="0"
-          sx={{
-            "-webkit-mask-image":
-              "linear-gradient(to right, transparent 0px, black 40px, black calc(100% - 40px), transparent)",
-          }}
-        >
-          <HStack
-            overflowX="auto"
-            px={8}
-            color={tabButtonColor}
-            sx={{
-              "&::-webkit-scrollbar": {
-                display: "none",
-              },
-              scrollbarWidth: "none",
-            }}
-          >
-            <TabButton href={`${urlName}`}>{tabTitle}</TabButton>
-            {/* <TabButton href="#" disabled tooltipText="Stay tuned!">
-            More tabs soon
-          </TabButton> */}
-          </HStack>
-        </Box>
-
-        <Box
+        <HStack
+          overflowX="auto"
+          px={8}
           color={tabButtonColor}
           sx={{
-            "> *": {
-              color: tabButtonColor,
+            "&::-webkit-scrollbar": {
+              display: "none",
             },
+            scrollbarWidth: "none",
           }}
         >
-          {children}
-        </Box>
-      </Stack>
-    </>
+          <TabButton href={`${urlName}`}>{tabTitle}</TabButton>
+          {/* <TabButton href="#" disabled tooltipText="Stay tuned!">
+            More tabs soon
+          </TabButton> */}
+        </HStack>
+      </Box>
+
+      <Box
+        color={tabButtonColor}
+        sx={{
+          "> *": {
+            color: tabButtonColor,
+          },
+        }}
+      >
+        {children}
+      </Box>
+    </Stack>
   )
 }
 
