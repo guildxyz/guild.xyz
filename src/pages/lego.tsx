1d0c4fa611b1cbab8bb1f25cfa4a728c1bb88851
BrickheadJohnny <92519134+BrickheadJohnny@users.noreply.github.com>
2022-09-23 17:20:06 +0200

    LEGO page redesign (#491)

--- src/pages/lego.tsx
+++ src/pages/lego.tsx
@@ -1,52 +1,6 @@
-import {
-  Box,
-  Center,
-  Container,
-  Heading,
-  Image,
-  Link,
-  SimpleGrid,
-} from "@chakra-ui/react"
-import Card from "components/common/Card"
+import { Container, Heading, SimpleGrid, Stack } from "@chakra-ui/react"
+import LegoCard from "components/lego/LegoCard"
 import Head from "next/head"
-
-function LegoCard({ href, img }) {
-  return (
-    <Link _hover={{ textDecor: "none" }} borderRadius="2xl" href={href}>
-      <Card
-        p={{ base: "8", md: "16" }}
-        w="full"
-        h="full"
-        pos="relative"
-        _before={{
-          content: `""`,
-          position: "absolute",
-          top: 0,
-          bottom: 0,
-          left: 0,
-          right: 0,
-          bg: "primary.300",
-          opacity: 0,
-          transition: "opacity 0.2s",
-        }}
-        _hover={{
-          _before: {
-            opacity: 0.1,
-          },
-        }}
-        _active={{
-          _before: {
-            opacity: 0.17,
-          },
-        }}
-      >
-        <Center h="full" w="full">
-          <Image src={img} alt="Lego Assembly" maxH={{ base: 48, lg: 64 }} />
-        </Center>
-      </Card>
-    </Link>
-  )
-}
 
 const Page = () => (
   <>
@@ -54,15 +8,10 @@ const Page = () => (
       <title>Guild Lego</title>
     </Head>
 
-    <Container maxW="container.lg">
-      <Box
-        p={{ base: "6", md: "28" }}
-        minH="100vh"
-        display="flex"
-        flexDirection={"column"}
-      >
+    <Container maxW={{ base: "container.sm", lg: "container.lg" }}>
+      <Stack pt={16}>
         <Heading
-          fontFamily={"display"}
+          fontFamily="display"
           fontSize={{ base: "4xl", md: "5xl", lg: "7xl" }}
           textAlign="center"
           mb={{ base: 10, md: 20 }}
@@ -70,32 +19,61 @@ const Page = () => (
           Guild Lego Assemblies
         </Heading>
         <SimpleGrid
-          columns={{ md: 2 }}
+          columns={{ lg: 2 }}
           spacing={{ base: "6", md: "8", lg: "10" }}
           flexGrow={1}
+          pb={{ base: 10, md: 20 }}
         >
           <LegoCard
             href="/lego/BrandenburgGuildAssembly.pdf"
             img="/lego/brandenburg-guild.png"
+            name="Brandenburg Guild"
+            pieces={362}
           />
           <LegoCard
             href="/lego/ArcDeGuildAssembly.pdf"
             img="/lego/arc-de-guild.png"
+            name="Arc de Guild"
+            pieces={190}
           />
           <LegoCard
             href="/lego/LightGuildEmpireAssembly.pdf"
             img="/lego/guild-empire-light.png"
+            name="Guild Empire"
+            pieces={162}
           />
           <LegoCard
             href="/lego/DarkGuildEmpireAssembly.pdf"
             img="/lego/guild-empire-dark.png"
+            name="Guild Empire"
+            pieces={162}
           />
-          <LegoCard href="/lego/GuildCastleAssembly.pdf" img="/lego/castle.svg" />
-          <LegoCard href="/lego/GuildDudeAssembly.pdf" img="/lego/dude.svg" />
-          <LegoCard href="/lego/GuildFoxAssembly.pdf" img="/lego/fox.svg" />
-          <LegoCard href="/lego/GuildGhostAssembly.pdf" img="/lego/ghost.svg" />
+          <LegoCard
+            href="/lego/GuildCastleAssembly.pdf"
+            img="/lego/guild-castle.png"
+            name="Guild Castle"
+            pieces={59}
+          />
+          <LegoCard
+            href="/lego/GuildDudeAssembly.pdf"
+            img="/lego/guild-dude.png"
+            name="Guild Dude"
+            pieces={74}
+          />
+          <LegoCard
+            href="/lego/GuildFoxAssembly.pdf"
+            img="/lego/guild-fox.png"
+            name="Guild Fox"
+            pieces={58}
+          />
+          <LegoCard
+            href="/lego/GuildGhostAssembly.pdf"
+            img="/lego/guild-ghost.png"
+            name="Guild Ghost"
+            pieces={64}
+          />
         </SimpleGrid>
-      </Box>
+      </Stack>
     </Container>
   </>
 )
