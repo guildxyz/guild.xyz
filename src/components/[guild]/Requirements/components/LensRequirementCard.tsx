69dac293abc0d880ba3abebd390fb30ca56ff893
valid <valid@zgen.hu>
2022-10-03 23:45:14 +0200

    working LensRequirementCard

--- src/components/[guild]/Requirements/components/LensRequirementCard.tsx
+++ src/components/[guild]/Requirements/components/LensRequirementCard.tsx
@@ -1,14 +1,38 @@
 import { Requirement } from "types"
+import shortenHex from "utils/shortenHex"
+import BlockExplorerUrl from "./common/BlockExplorerUrl"
 import RequirementCard from "./common/RequirementCard"
 
 type Props = {
   requirement: Requirement
 }
 
-const LensRequirementCard = ({ requirement }: Props) => (
-  <RequirementCard requirement={requirement} image={<p>WIP</p>} footer={<p>WIP</p>}>
-    WIP
-  </RequirementCard>
-)
+const LensRequirementCard = ({ requirement }: Props) => {
+  requirement.chain = "POLYGON"
+
+  return (
+    <RequirementCard
+      requirement={requirement}
+      image={"requirementLogos/lens.png"}
+      footer={requirement.address && <BlockExplorerUrl requirement={requirement} />}
+    >
+      {requirement.address ? (
+        <>
+          {`Own the `}
+          <pre>{shortenHex(requirement.address)}</pre>
+          {` Lens Protocol collect/mirror NFT`}
+        </>
+      ) : requirement.data?.id ? (
+        <>
+          {`Follow `}
+          <pre>{requirement.data.id}</pre>
+          {` on the Lens protocol`}
+        </>
+      ) : (
+        "Have a Lens Protocol profile"
+      )}
+    </RequirementCard>
+  )
+}
 
 export default LensRequirementCard
