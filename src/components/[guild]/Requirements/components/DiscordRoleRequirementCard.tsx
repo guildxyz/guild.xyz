347633ef6e6ea60b0bd2cbe6e933fe793287d61b
Csete Balázs <csetebalu@gmail.com>
2022-10-04 12:26:06 +0200

    check for server data in requirement card

--- src/components/[guild]/Requirements/components/DiscordRoleRequirementCard.tsx
+++ src/components/[guild]/Requirements/components/DiscordRoleRequirementCard.tsx
@@ -1,4 +1,5 @@
 import { Icon } from "@chakra-ui/react"
+import useServerData from "hooks/useServerData"
 import { DiscordLogo } from "phosphor-react"
 import { Requirement } from "types"
 import ConnectRequirementPlatformButton from "./common/ConnectRequirementPlatformButton"
@@ -8,18 +9,29 @@ type Props = {
   requirement: Requirement
 }
 
-const DiscordRoleRequirementCard = ({ requirement }: Props) => (
-  <RequirementCard
-    requirement={requirement}
-    image={<Icon as={DiscordLogo} boxSize={6} />}
-    footer={<ConnectRequirementPlatformButton platform="DISCORD" />}
-  >
-    {`Have the "`}
-    <pre>{requirement.data.roleName}</pre>
-    {`" role in the "`}
-    <pre>{requirement.data.serverName}</pre>
-    {`" server`}
-  </RequirementCard>
-)
+const DiscordRoleRequirementCard = ({ requirement }: Props) => {
+  const {
+    data: { serverName, roles, isAdmin, serverIcon },
+  } = useServerData(requirement.data.serverId)
+
+  const role =
+    typeof isAdmin === "boolean"
+      ? roles?.find(({ id }) => id === requirement.data.roleId)
+      : undefined
+
+  return (
+    <RequirementCard
+      requirement={requirement}
+      image={serverIcon || <Icon as={DiscordLogo} boxSize={6} />}
+      footer={<ConnectRequirementPlatformButton platform="DISCORD" />}
+    >
+      {`Have the "`}
+      <pre>{role?.name ?? requirement.data.roleName}</pre>
+      {`" role in the "`}
+      <pre>{serverName ?? requirement.data.serverName}</pre>
+      {`" server`}
+    </RequirementCard>
+  )
+}
 
 export default DiscordRoleRequirementCard
