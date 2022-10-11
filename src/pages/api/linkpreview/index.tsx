d5eb588abfd33ca8e8715bbec1a07fe1cca70e7a
BrickheadJohnny <johnny.zgen@gmail.com>
2022-10-11 16:03:56 +0200

    explorer link preview - static guild list

--- src/pages/api/linkpreview/index.tsx
+++ src/pages/api/linkpreview/index.tsx
@@ -1,6 +1,5 @@
 import { ImageResponse } from "@vercel/og"
 import { GuildBase } from "types"
-import fetcher from "utils/fetcher"
 
 export const config = {
   runtime: "experimental-edge",
@@ -18,7 +17,234 @@ const dystopianFont = fetch(
 
 const handler = async () => {
   try {
-    const guilds = await fetcher(`/guild?order=members`).catch((_) => [])
+    // const guilds = await fetcher(`/guild?order=members`).catch((_) => [])
+
+    const guilds: GuildBase[] = [
+      {
+        id: 9839,
+        name: "Arbitrum",
+        imageUrl:
+          "https://guild-xyz.mypinata.cloud/ipfs/QmWgXnAcLXhXfRL4u4jcmpXNXXbLXSt3WvmETeaLjBgS5S",
+        urlName: "arbitrum",
+        roles: [
+          "Arbinaut 🚀",
+          "Arbisquad",
+          "Brinc fam 🔹",
+          "Dopex fam 💎",
+          "GMX fam 🫐",
+          "Galleon fam 🏴‍☠️",
+          "HOP fam 🐇",
+          "JonesDAO fam 🐂",
+          "LINK marine 🔗",
+          "Livepeer fam ▶️",
+          "Mycelium fam ✖️",
+          "PlutusDAO fam 🕳️",
+          "Sperax fam 🧩",
+          "Synapse fam 🟣",
+          "Treasure fam ✨",
+          "Umami fam 🍔",
+          "Vesta fam 🤍",
+        ],
+        platforms: ["DISCORD"],
+        memberCount: 70681,
+      },
+      {
+        id: 8212,
+        name: "SyncSwap Guild",
+        imageUrl:
+          "https://guild-xyz.mypinata.cloud/ipfs/QmbBY85pZnGNXRaKRpf5SqNNA971cGVRdrLJirYck6yaPx",
+        urlName: "syncswap",
+        roles: [
+          "Distro Collector",
+          "Guild Member",
+          "Proud Partner OAT Holder",
+          "Testnet Badge Holder",
+          "Testnet Voter",
+          "Twitter Fren",
+        ],
+        platforms: ["DISCORD"],
+        memberCount: 56366,
+      },
+      {
+        id: 3474,
+        name: "AxelarSea",
+        imageUrl:
+          "https://guild-xyz.mypinata.cloud/ipfs/QmUuVD8jSXY4vLQnxyMSAMjUX4FKakSKGZrj92q3yuqWQC",
+        urlName: "axelarsea",
+        roles: [
+          "NFT Bridge Tester",
+          "Testnet V1 Tester",
+          "Wallet verified",
+          "twitter verified",
+        ],
+        platforms: ["DISCORD"],
+        memberCount: 39520,
+      },
+      {
+        id: 7326,
+        name: "Galxe",
+        imageUrl:
+          "https://guild-xyz.mypinata.cloud/ipfs/QmXzePDa6MoJKQxbDbrAhFFBgp3UhM5MeK613cAiL7uD8E",
+        urlName: "galxe",
+
+        roles: ["Member"],
+        platforms: ["DISCORD"],
+        memberCount: 31228,
+      },
+      {
+        id: 3619,
+        name: "Increment",
+        imageUrl:
+          "https://guild-xyz.mypinata.cloud/ipfs/QmTDtbzdx11SvyqVoSmnDxaowCeXucanMFQvw731Qbqs7w",
+        urlName: "increment",
+        roles: ["Beta v1 Tester", "Beta v2 Tester"],
+        platforms: ["DISCORD"],
+        memberCount: 26640,
+      },
+      {
+        id: 1985,
+        name: "Our Guild",
+        imageUrl:
+          "https://guild-xyz.mypinata.cloud/ipfs/QmSJtjpHzaEdMuBE2uAPSN3r32eZkLXndMzQLBSbknFD1W",
+        urlName: "our-guild",
+
+        roles: [
+          "Early Tester Badge Holder",
+          "Guild Alpha",
+          "Guild Grower",
+          "Guild Maxi",
+          "Guild Member",
+          "Guild Members",
+          "Guild NFT Holder",
+          "Our Guild is evolving",
+        ],
+        platforms: ["DISCORD", "GITHUB", "GOOGLE", "TELEGRAM"],
+        memberCount: 20617,
+      },
+      {
+        id: 10762,
+        name: "Swapped Finance",
+        imageUrl:
+          "https://guild-xyz.mypinata.cloud/ipfs/QmWdknKNRRkASmVadCMTgRvsvY21ENaFvALNJNi4cK2NtU",
+        urlName: "swappedfinance",
+
+        roles: ["101 Master", "Guild Member", "TwitterSquad"],
+        platforms: ["DISCORD"],
+        memberCount: 18735,
+      },
+      {
+        id: 9267,
+        name: "Layer3",
+        imageUrl:
+          "https://guild-xyz.mypinata.cloud/ipfs/QmQLA3ydd2Hf6WrPEF2CrYfckdLq8uVpidmtHGRwoCLisH",
+        urlName: "layer3",
+
+        roles: ["Guild Member", "Guild Supporter"],
+        platforms: ["DISCORD"],
+        memberCount: 18474,
+      },
+      {
+        id: 11886,
+        name: "Orbiter Finance",
+        imageUrl:
+          "https://guild-xyz.mypinata.cloud/ipfs/QmSJZC3auCphTxLpm5weiUsHyeAKXjaxAuXUFToWhg5DRU",
+        urlName: "orbiter-finance",
+
+        roles: [
+          "Ace Pilot",
+          "Elite Pilot",
+          "Expert Pilot",
+          "Flying Alien",
+          "Member",
+          "Pilot",
+          "Trainee Pilot",
+        ],
+        platforms: ["DISCORD"],
+        memberCount: 18108,
+      },
+      {
+        id: 10457,
+        name: "Mint Square",
+        imageUrl:
+          "https://guild-xyz.mypinata.cloud/ipfs/QmNTfpCTbgpT2eZddtxFYoW1kTongnmuw2kzRRdPnoyZUg",
+        urlName: "mintsquare",
+
+        roles: ["Guild Member", "Test & Earn Badge"],
+        platforms: ["DISCORD"],
+        memberCount: 16079,
+      },
+      {
+        id: 7301,
+        name: "SealCred.xyz",
+        imageUrl:
+          "https://guild-xyz.mypinata.cloud/ipfs/QmYUXxamuBa5Vd31oseqTpTUkN6W4GVHj679oC7nexaMjG",
+        urlName: "sealcred",
+
+        roles: [
+          "@126.com owner (v0.2-goerli)",
+          "@139.com owner (v0.2-goerli)",
+          "@163.com owner (v0.2-goerli)",
+          "@allcmail.com owner (v0.2-goerli)",
+          "@foxmail.com owner (v0.2-goerli)",
+          "@gmail.com owner (v0.2-goerli)",
+          "@hotmail.com owner (v0.2-goerli)",
+          "@list.ru owner (v0.2-goerli)",
+          "@mail.ru owner (v0.2-goerli)",
+          "@naver.com owner (v0.2-goerli)",
+          "@outlook.com owner (v0.2-goerli)",
+          "@proton.me owner (v0.2-goerli)",
+          "@protonmail.com owner (v0.2-goerli)",
+          "@qq.com owner (v0.2-goerli)",
+          "@rambler.ru owner (v0.2-goerli)",
+          "@rambler.ua owner (v0.2-goerli)",
+          "@sina.com owner (v0.2-goerli)",
+          "@skiff.com owner (v0.2-goerli)",
+          "@ujinjx.cn owner (v0.2-goerli)",
+          "@vip.qq.com owner (v0.2-goerli)",
+          "@yahoo.com owner (v0.2-goerli)",
+          "@yeah.net owner (v0.2-goerli)",
+          "@zdee.cc owner (v0.2-goerli)",
+          "AppleFrens owner (v0.2-goerli)",
+          "AppleFrens owner (v0.2.4-goerli)",
+          "BananaFrens owner (v0.2-goerli)",
+          "BananaFrens owner (v0.2.4-goerli)",
+          "CherryFrens owner (v0.2-goerli)",
+          "CherryFrens owner (v0.2.4-goerli)",
+          "Fake ERC721 owner (v0.2-goerli)",
+          "Fake Loot owner (v0.2-goerli)",
+          "GrapeFrens owner (v0.2-goerli)",
+          "GrapeFrens owner (v0.2.4-goerli)",
+          "Influence Asteroids owner (v0.2-goerli)",
+          "Influence Crew owner (v0.2-goerli)",
+          "MultiFaucet NFT owner (v0.2-goerli)",
+          "MultiFaucet NFT owner (v0.2.4-goerli)",
+          "OrangeFrens owner (v0.2-goerli)",
+          "OrangeFrens owner (v0.2.4-goerli)",
+          "PearFrens owner (v0.2-goerli)",
+          "PearFrens owner (v0.2.4-goerli)",
+          "PoLido owner (v0.2-goerli)",
+          "StrawberryFrens owner (v0.2-goerli)",
+          "StrawberryFrens owner (v0.2.4-goerli)",
+          "Uniswap V3 Positions NFT-V1 owner (v0.2-goerli)",
+          "Verified Holder",
+          "Zora API Genesis Hackathon owner (v0.2.4-mainnet)",
+          "test owner (v0.2-goerli)",
+        ],
+        platforms: ["DISCORD"],
+        memberCount: 12539,
+      },
+      {
+        id: 2893,
+        name: "CyberConnect",
+        imageUrl:
+          "https://guild-xyz.mypinata.cloud/ipfs/QmVpg6vf8666CEjKXRa3wqhAKTPsC4nGedpDcWzw32yU86",
+        urlName: "cyberconnect",
+
+        roles: ["Member"],
+        platforms: ["DISCORD"],
+        memberCount: 12443,
+      },
+    ]
 
     const interFontData = await interFont
     const interBoldFontData = await interBoldFont
