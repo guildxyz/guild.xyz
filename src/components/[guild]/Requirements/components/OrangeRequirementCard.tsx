7fc49a2ac9a9e23127d3a9badd7751b5e4e319c2
kef1rke <kocsisbarnabas1@gmail.com>
2022-10-13 18:04:50 +0200

    orange integraation

--- src/components/[guild]/Requirements/components/OrangeRequirementCard.tsx
+++ src/components/[guild]/Requirements/components/OrangeRequirementCard.tsx
@@ -1,4 +1,6 @@
+import { Img } from "@chakra-ui/react"
 import { Requirement } from "types"
+import { RequirementLinkButton } from "./common/RequirementButton"
 import RequirementCard from "./common/RequirementCard"
 
 type Props = {
@@ -6,8 +8,19 @@ type Props = {
 }
 
 const OrangeRequirementCard = ({ requirement }: Props) => (
-  <RequirementCard requirement={requirement} image={<p>WIP</p>} footer={<p>WIP</p>}>
-    WIP
+  <RequirementCard
+    requirement={requirement}
+    image={<Img src="/requirementLogos/orange.png" />}
+    footer={
+      <RequirementLinkButton
+        imageUrl="https://app.orangeprotocol.io/logo.svg"
+        href={`https://app.orangeprotocol.io/campaigns/details/${requirement.data.id}`}
+      >
+        View on Orange
+      </RequirementLinkButton>
+    }
+  >
+    {`Have the ${requirement.data.id} Orange badge`}
   </RequirementCard>
 )
 
