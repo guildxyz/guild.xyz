43a43eb3415e3d03c2df9031021c195d6c475d9b
BrickheadJohnny <johnny.zgen@gmail.com>
2022-10-18 08:48:50 +0200

    Datadog - initialize only in production

--- src/components/_app/Datadog.tsx
+++ src/components/_app/Datadog.tsx
@@ -3,9 +3,9 @@ import { RumComponentContextProvider } from "@datadog/rum-react-integration"
 import { PropsWithChildren, useEffect } from "react"
 
 const Datadog = ({ children }: PropsWithChildren<unknown>): JSX.Element => {
+  const url = typeof window !== "undefined" ? window.location.host : ""
   useEffect(() => {
-    console.log("NODE_ENV", process.env.NODE_ENV)
-    if (process.env.NODE_ENV !== "production") return
+    if (process.env.NODE_ENV !== "production" || url !== "guild.xyz") return
     datadogRum.init({
       applicationId: "996b7a2a-d610-4235-a5b4-65391973ea76",
       clientToken: "pub7cf22f3b79a010363cf58c859cfa8ad8",
