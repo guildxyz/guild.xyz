d35cba7db2b62e6e08af288373e52137cf451a27
BrickheadJohnny <johnny.zgen@gmail.com>
2022-10-19 18:19:48 +0200

    middleware - remove datadog related stuff

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,16 +2,6 @@ import type { NextRequest } from "next/server"
 import { NextResponse } from "next/server"
 
 export function middleware(request: NextRequest) {
-  if (request.nextUrl.pathname === "/api/ddrum") {
-    const url = new URL(request.url)
-    const ddforward = url.searchParams.get("ddforward")
-
-    if (!ddforward) return NextResponse.next()
-
-    const redirectUrl = new URL(ddforward)
-    return NextResponse.rewrite(redirectUrl)
-  }
-
   const isGuildPage = request.nextUrl.pathname.split("/")?.length <= 2
 
   if (
