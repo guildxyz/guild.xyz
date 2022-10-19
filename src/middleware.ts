576500e33da2e75f671ba266d958d1647289f92f
BrickheadJohnny <johnny.zgen@gmail.com>
2022-10-19 15:29:41 +0200

    middleware - remove ip log

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,6 @@ import { NextResponse } from "next/server"
 
 export function middleware(request: NextRequest) {
   if (request.nextUrl.pathname === "/api/ddrum") {
-    console.log("IP", request.ip)
     const url = new URL(request.url)
     const ddforward = url.searchParams.get("ddforward")
 
