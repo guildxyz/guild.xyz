14c81609ed99a4b6c71c491ad3ffe727422b6563
BrickheadJohnny <johnny.zgen@gmail.com>
2022-10-19 15:13:38 +0200

    Datadot - always enable logging

--- src/components/_app/Datadog/Datadog.tsx
+++ src/components/_app/Datadog/Datadog.tsx
@@ -6,7 +6,7 @@ const Datadog = ({ children }: PropsWithChildren<unknown>): JSX.Element => {
   const url = typeof window !== "undefined" ? window.location.host : ""
 
   useEffect(() => {
-    if (process.env.NODE_ENV !== "production" || url !== "guild.xyz") return
+    // if (process.env.NODE_ENV !== "production" || url !== "guild.xyz") return
     datadogRum.init({
       applicationId: "996b7a2a-d610-4235-a5b4-65391973ea76",
       clientToken: "pub7cf22f3b79a010363cf58c859cfa8ad8",
