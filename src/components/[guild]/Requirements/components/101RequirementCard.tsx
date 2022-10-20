26a361759ab0648b9659909b9a70fcd6147bfc6d
valid <valid@zgen.hu>
2022-10-20 13:21:56 +0200

    101.xyz integration use api for rich data

--- src/components/[guild]/Requirements/components/101RequirementCard.tsx
+++ src/components/[guild]/Requirements/components/101RequirementCard.tsx
@@ -1,28 +1,50 @@
 import { Img } from "@chakra-ui/react"
 import DataBlock from "components/common/DataBlock"
+import use101Courses from "components/create-guild/Requirements/components/101FormCard/hooks/use101Courses"
 import { Requirement } from "types"
+import { RequirementLinkButton } from "./common/RequirementButton"
 import RequirementCard from "./common/RequirementCard"
 
 type Props = {
   requirement: Requirement
 }
 
-const HundredNOneRequirementCard = ({ requirement }: Props) => (
-  <RequirementCard
-    requirement={requirement}
-    image={<Img src="/requirementLogos/101.png" />}
-    // footer={
-    //   <RequirementLinkButton
-    //     imageUrl={"/requirementLogos/101.png"}
-    //     href={`https://101.xyz/course/${requirement.data.id}`}
-    //   >
-    //     View badge
-    //   </RequirementLinkButton>
-    // }
-  >
-    {`Have the badge of 101 course `}
-    <DataBlock>{`#${requirement.data.id}`}</DataBlock>
-  </RequirementCard>
-)
+const HundredNOneRequirementCard = ({ requirement }: Props) => {
+  const { data, isValidating } = use101Courses()
+
+  const course = data?.find(
+    (option) => option.badge.onChainId.toString() === requirement.data.id
+  )
+
+  return (
+    <RequirementCard
+      requirement={requirement}
+      image={
+        <Img
+          src={
+            course?.badge?.imageUri?.replace(
+              "ipfs://",
+              "https://ipfs.fleek.co/ipfs/"
+            ) ?? "/requirementLogos/101.png"
+          }
+        />
+      }
+      footer={
+        <RequirementLinkButton
+          imageUrl={"/requirementLogos/101.png"}
+          href={`https://101.xyz/course/${course?.id}`}
+        >
+          View course
+        </RequirementLinkButton>
+      }
+    >
+      {`Have the badge of the `}
+      <DataBlock isLoading={!course && isValidating}>
+        {course?.title ?? requirement.data.id}
+      </DataBlock>
+      {` 101 course `}
+    </RequirementCard>
+  )
+}
 
 export default HundredNOneRequirementCard
--- src/components/create-guild/Requirements/components/101FormCard/101FormCard.tsx
+++ src/components/create-guild/Requirements/components/101FormCard/101FormCard.tsx
@@ -1,7 +1,9 @@
-import { FormControl, FormLabel, Input } from "@chakra-ui/react"
+import { FormControl, FormLabel } from "@chakra-ui/react"
 import FormErrorMessage from "components/common/FormErrorMessage"
+import StyledSelect from "components/common/StyledSelect"
 import { Controller, useFormContext } from "react-hook-form"
-import { Requirement } from "types"
+import { Requirement, SelectOption } from "types"
+import use101Courses from "./hooks/use101Courses"
 
 type Props = {
   index: number
@@ -14,23 +16,35 @@ const HundredNOneFormCard = ({ index, field }: Props) => {
     formState: { errors },
   } = useFormContext()
 
+  const { data, isValidating } = use101Courses()
+
+  const options = data.map((course) => ({
+    value: course.badge.onChainId.toString(),
+    label: course.title,
+    img: course.creator.image,
+  }))
+
   return (
     <>
       <FormControl isRequired isInvalid={errors?.requirements?.[index]?.data?.id}>
-        <FormLabel>Course ID:</FormLabel>
+        <FormLabel>Course:</FormLabel>
 
         <Controller
           name={`requirements.${index}.data.id` as const}
           control={control}
           rules={{ required: "This field is required." }}
           render={({ field: { onChange, onBlur, value, ref } }) => (
-            <Input
-              type="number"
+            <StyledSelect
               ref={ref}
-              value={value ?? ""}
-              placeholder="number"
-              onChange={onChange}
+              isClearable
+              options={options}
+              value={options?.find((option) => option.value === value) ?? ""}
+              placeholder="Choose course"
+              onChange={(newSelectedOption: SelectOption) => {
+                onChange(newSelectedOption?.value)
+              }}
               onBlur={onBlur}
+              isLoading={!data && isValidating}
             />
           )}
         />
