d363af4e7e827f4ad8c632461cddeb99ca616fd4
BrickheadJohnny <johnny.zgen@gmail.com>
2022-10-26 15:39:41 +0200

    useTimeInaccuracy - add `shouldFetch` param

--- src/hooks/useTimeInaccuracy.ts
+++ src/hooks/useTimeInaccuracy.ts
@@ -3,25 +3,22 @@ import fetcher from "utils/fetcher"
 
 const INACCURACY_INTERVAL_MS = 15 * 60 * 1000
 
-const useTimeInaccuracy = () => {
-  /**
-   * Temporarily disable to see how much serverless function execution time can we
-   * save by not calling /api/timestamp for every session
-   */
-  return 0
+const useTimeInaccuracy = (shouldFetch = false) => {
+  const { data } = useSWRImmutable<number>(
+    shouldFetch ? "/api/timestamp" : null,
+    (url) =>
+      fetcher(url).then((ts) => {
+        const numTs = +ts
 
-  const { data } = useSWRImmutable<number>("/api/timestamp", (url) =>
-    fetcher(url).then((ts) => {
-      const numTs = +ts
-      const clientTime = Date.now()
-      if (
-        clientTime < numTs - INACCURACY_INTERVAL_MS ||
-        clientTime > numTs + INACCURACY_INTERVAL_MS
-      ) {
-        return numTs - clientTime
-      }
-      return 0
-    })
+        const clientTime = Date.now()
+        if (
+          clientTime < numTs - INACCURACY_INTERVAL_MS ||
+          clientTime > numTs + INACCURACY_INTERVAL_MS
+        ) {
+          return numTs - clientTime
+        }
+        return 0
+      })
   )
 
   return data || 0
