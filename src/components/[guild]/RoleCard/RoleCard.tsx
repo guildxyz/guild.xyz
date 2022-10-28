f685b26bc88ef26b58315b58c0ee083908589f79
BrickheadJohnny <johnny.zgen@gmail.com>
2022-10-28 10:25:28 +0200

    Card with forwardRef

--- src/components/[guild]/RoleCard/RoleCard.tsx
+++ src/components/[guild]/RoleCard/RoleCard.tsx
@@ -57,85 +57,83 @@ const RoleCard = memo(({ role }: Props) => {
   }
 
   return (
-    <Box
-      scrollMarginTop={"calc(var(--chakra-space-11) + (2 * var(--chakra-space-3)))"}
+    <Card
       ref={roleCardEl}
+      scrollMarginTop={"calc(var(--chakra-space-11) + (2 * var(--chakra-space-3)))"}
     >
-      <Card>
-        <SimpleGrid columns={{ base: 1, md: 2 }}>
-          <Flex
-            direction="column"
-            p={5}
-            borderRightWidth={{ base: 0, md: 1 }}
-            borderRightColor={colorMode === "light" ? "gray.200" : "gray.600"}
-          >
-            <HStack justifyContent="space-between" mb={6} spacing={4}>
-              <HStack spacing={4}>
-                <GuildLogo
-                  imageUrl={role.imageUrl}
-                  size={{ base: "48px", md: "52px" }}
-                />
-                <Heading as="h3" fontSize="xl" fontFamily="display">
-                  {role.name}
-                </Heading>
-              </HStack>
-
-              <MemberCount memberCount={role.memberCount} />
-
-              {DynamicEditRole && (
-                <>
-                  <Spacer />
-                  <DynamicEditRole roleId={role.id} />
-                </>
-              )}
+      <SimpleGrid columns={{ base: 1, md: 2 }}>
+        <Flex
+          direction="column"
+          p={5}
+          borderRightWidth={{ base: 0, md: 1 }}
+          borderRightColor={colorMode === "light" ? "gray.200" : "gray.600"}
+        >
+          <HStack justifyContent="space-between" mb={6} spacing={4}>
+            <HStack spacing={4}>
+              <GuildLogo
+                imageUrl={role.imageUrl}
+                size={{ base: "48px", md: "52px" }}
+              />
+              <Heading as="h3" fontSize="xl" fontFamily="display">
+                {role.name}
+              </Heading>
             </HStack>
 
-            {role.description && (
-              <Text mb={6} wordBreak="break-word">
-                {parseDescription(role.description)}
-              </Text>
+            <MemberCount memberCount={role.memberCount} />
+
+            {DynamicEditRole && (
+              <>
+                <Spacer />
+                <DynamicEditRole roleId={role.id} />
+              </>
             )}
+          </HStack>
 
-            <Box mt="auto">
-              {role.rolePlatforms?.map((platform) => (
-                <Reward
-                  key={platform.guildPlatformId}
-                  platform={platform}
-                  role={role}
-                />
-              ))}
-            </Box>
-          </Flex>
+          {role.description && (
+            <Text mb={6} wordBreak="break-word">
+              {parseDescription(role.description)}
+            </Text>
+          )}
 
-          <Flex
-            direction="column"
-            p={5}
-            pb={{ base: 14, md: 5 }}
-            position="relative"
-            bgColor={colorMode === "light" ? "gray.50" : "blackAlpha.300"}
-          >
-            <HStack mb={{ base: 4, md: 6 }}>
-              <Text
-                as="span"
-                mt="1"
-                mr="2"
-                fontSize="xs"
-                fontWeight="bold"
-                color="gray"
-                textTransform="uppercase"
-                noOfLines={1}
-              >
-                Requirements to qualify
-              </Text>
-              <Spacer />
-              <AccessIndicator roleId={role.id} onLoad={scrollToCard} />
-            </HStack>
+          <Box mt="auto">
+            {role.rolePlatforms?.map((platform) => (
+              <Reward
+                key={platform.guildPlatformId}
+                platform={platform}
+                role={role}
+              />
+            ))}
+          </Box>
+        </Flex>
 
-            <Requirements requirements={role.requirements} logic={role.logic} />
-          </Flex>
-        </SimpleGrid>
-      </Card>
-    </Box>
+        <Flex
+          direction="column"
+          p={5}
+          pb={{ base: 14, md: 5 }}
+          position="relative"
+          bgColor={colorMode === "light" ? "gray.50" : "blackAlpha.300"}
+        >
+          <HStack mb={{ base: 4, md: 6 }}>
+            <Text
+              as="span"
+              mt="1"
+              mr="2"
+              fontSize="xs"
+              fontWeight="bold"
+              color="gray"
+              textTransform="uppercase"
+              noOfLines={1}
+            >
+              Requirements to qualify
+            </Text>
+            <Spacer />
+            <AccessIndicator roleId={role.id} onLoad={scrollToCard} />
+          </HStack>
+
+          <Requirements requirements={role.requirements} logic={role.logic} />
+        </Flex>
+      </SimpleGrid>
+    </Card>
   )
 })
 
