e40688d83ac3ae638a3ab9b911bd54cbc9c1c619
valid <valid@zgen.hu>
2022-10-28 14:56:36 +0200

    feat: set custom Discord invite link (#538)
    * feat: DiscordRewardSettings to set custom invite
    
    * wire up new working BE endpoint
    
    * switch back to prod api

---
+++ src/components/[guild]/RolePlatforms/components/PlatformCard/components/useDiscordCardProps/DiscordCardMenu/components/DiscordRewardSettings.tsx.tsx
@@ -0,0 +1,114 @@
+import {
+  FormControl,
+  FormHelperText,
+  FormLabel,
+  Input,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+} from "@chakra-ui/react"
+import Button from "components/common/Button"
+import { Modal } from "components/common/Modal"
+import useGuild from "components/[guild]/hooks/useGuild"
+import useShowErrorToast from "hooks/useShowErrorToast"
+import { useSubmitWithSign } from "hooks/useSubmit"
+import useToast from "hooks/useToast"
+import { Controller, FormProvider, useForm } from "react-hook-form"
+import fetcher from "utils/fetcher"
+
+const DiscordRewardSettings = ({ isOpen, onClose, serverId }) => {
+  const { id, guildPlatforms, mutateGuild } = useGuild()
+  const showErrorToast = useShowErrorToast()
+  const toast = useToast()
+
+  const guildPlatform = guildPlatforms.find(
+    (platform) => platform.platformGuildId === serverId
+  )
+
+  const submit = ({ validation, data }) =>
+    fetcher(`/guild/${id}/platform/${guildPlatform.id}`, {
+      method: "PATCH",
+      validation,
+      body: data,
+    })
+
+  const { onSubmit, isLoading } = useSubmitWithSign(submit, {
+    onSuccess: () => {
+      toast({
+        status: "success",
+        title: "Successfully updated invite link",
+      })
+      methods.reset(undefined, { keepValues: true })
+      mutateGuild()
+      onClose()
+    },
+    onError: (err) => showErrorToast(err),
+  })
+
+  const defaultValues = {
+    platformGuildData: {
+      ...guildPlatform.platformGuildData,
+      invite: guildPlatform.invite,
+    },
+  }
+
+  const methods = useForm({
+    mode: "onSubmit",
+    defaultValues,
+  })
+
+  const handleClose = () => {
+    if (methods.formState.isDirty) methods.reset(defaultValues)
+    onClose()
+  }
+
+  return (
+    <Modal isOpen={isOpen} onClose={handleClose}>
+      <ModalOverlay />
+      <ModalContent maxW="lg">
+        <ModalHeader>{`Discord reward settings: ${guildPlatform.platformGuildName}`}</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          <FormProvider {...methods}>
+            <FormControl>
+              <FormLabel>Go to server link</FormLabel>
+              <Controller
+                name={`platformGuildData.invite`}
+                control={methods.control}
+                render={({ field: { onChange, onBlur, value, ref } }) => (
+                  <Input
+                    type="text"
+                    ref={ref}
+                    value={value ?? ""}
+                    placeholder="Paste invite link"
+                    onChange={(newChange) => {
+                      onChange(newChange.target.value)
+                    }}
+                    onBlur={onBlur}
+                  />
+                )}
+              />
+              <FormHelperText>
+                Use your custom invite link instead of the default generated one
+              </FormHelperText>
+            </FormControl>
+          </FormProvider>
+        </ModalBody>
+        <ModalFooter>
+          <Button
+            colorScheme="green"
+            onClick={methods.handleSubmit(onSubmit)}
+            isLoading={isLoading}
+          >
+            Save
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  )
+}
+
+export default DiscordRewardSettings
--- src/types.ts
+++ src/types.ts
@@ -153,6 +153,7 @@ type PlatformGuildData = {
   DISCORD: {
     role?: never
     inviteChannel: string
+    invite?: string
     joinButton?: boolean
     mimeType?: never
     iconLink?: never
