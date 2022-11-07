6117390380d254298e2c311ae3a1ebcc355c1e67
kef1rke <kocsisbarnabas1@gmail.com>
2022-11-08 00:50:31 +0100

    solution: useSound fetch all artists

--- src/components/create-guild/Requirements/components/SoundFormCard/hooks/useSoundSongs.ts
+++ src/components/create-guild/Requirements/components/SoundFormCard/hooks/useSoundSongs.ts
@@ -2,50 +2,67 @@ import useSWRImmutable from "swr/immutable"
 import fetcher from "utils/fetcher"
 
 type SongsSound = {
-  id: string
-  onChainId: number
-  imageUri: string
-  courses: {
-    id: string
-    title: string
-    creator: {
-      image: string
+  artists: {
+    edges: {
+      node: {
+        name: string
+        soundHandle: string
+        bannerImage: {
+          url: string
+        }
+      }
     }
-  }[]
+    pageInfo: { endCursor: string }
+  }
 }
 
-const fetchBadges = (endpoint: string) =>
+const fetch1000Songs = (endpoint: string, after: string) =>
   fetcher(endpoint, {
     headers: {
       Accept: "application/json",
+      "Content-Type": "application/json",
+      "X-Sound-Client-Key": "0326835a-0abe-4b77-8f8a-cc7bc7047ce0",
     },
     body: {
       query: `{
-        badges {
-          id
-          onChainId
-          imageUri
-          courses {
-            id
-            title
-            creator {
-              image
+        artists(pagination: {first: 1000, after: ${after}}) {
+          edges {
+            node {
+              name
+              soundHandle
+              bannerImage {
+                url
+              }
             }
+          }
+          pageInfo{
+            startCursor
+            endCursor
           }
         }
       }`,
     },
-  }).then((res) =>
-    res?.data?.badges.filter(
-      (badge) =>
-        badge.courses[0] &&
-        // Temporarily filtering out [Testnet] and [archive] badges so we don't get duplicate IDs here
-        !badge.courses[0].title?.toLowerCase()?.includes("[testnet]") &&
-        !badge.courses[0].title?.toLowerCase()?.includes("[archive]")
+  })
+
+const fetchSongs = async (endpoint: string) => {
+  let artists = []
+  let after = ""
+  const firstFetch = await fetch1000Songs(endpoint, after)
+  let newArtist = firstFetch
+  after = await firstFetch.then((e) => e.data.artists.pageInfo.endCursor)
+
+  do {
+    newArtist = await fetch1000Songs(endpoint, after).then(
+      (e) => e.data.artists.edges.node
     )
-  )
+    artists = artists.concat(newArtist)
+    after = await newArtist.data.artists.pageInfo.endCursor
+  } while (newArtist?.length > 0)
+
+  return artists
+}
 
 const useSoundSongs = () =>
-  useSWRImmutable<SongsSound[]>(`https://api.sound.xyz/graphql`, fetchBadges)
+  useSWRImmutable<SongsSound[]>(`https://api.sound.xyz/graphql`, fetchSongs)
 
 export default useSoundSongs
