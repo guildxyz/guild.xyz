6959e59a5060a45d7de67d6e2bf78ccaf570cf45
Csete Balázs <csetebalu@gmail.com>
2022-11-07 18:53:16 +0100

    link / id handling in select input

--- src/components/create-guild/Requirements/components/SpotifyFormCard/components/SpotifySearch.tsx
+++ src/components/create-guild/Requirements/components/SpotifyFormCard/components/SpotifySearch.tsx
@@ -1,26 +1,15 @@
-import {
-  Circle,
-  FormControl,
-  FormErrorMessage,
-  FormLabel,
-  HStack,
-  Img,
-  Input,
-  Text,
-  VStack,
-} from "@chakra-ui/react"
-import Link from "components/common/Link"
+import { FormControl, FormErrorMessage, FormLabel } from "@chakra-ui/react"
 import StyledSelect from "components/common/StyledSelect"
 import useDebouncedState from "hooks/useDebouncedState"
 import useGateables from "hooks/useGateables"
 import { useEffect, useState } from "react"
 import {
   useController,
-  useForm,
   useFormContext,
   useFormState,
   useWatch,
 } from "react-hook-form"
+import { useFetcherWithSign } from "utils/fetcher"
 
 type Props = {
   index: number
@@ -31,8 +20,10 @@ type Props = {
 const LINK_REGEX =
   /^(?:https\:\/\/)?(?:www\.)?open\.spotify\.com\/([a-z]*)\/(.*?)(?:\?.*)?$/
 
+const MAYBE_ID_REGEX = /^[a-z0-9]{22}$/i
+
 const SpotifySearch = ({ index, type, label }: Props) => {
-  const { setValue } = useFormContext()
+  const { setValue, setError } = useFormContext()
 
   const requirementLabel = useWatch({
     name: `requirements.${index}.data.params.label`,
@@ -52,72 +43,43 @@ const SpotifySearch = ({ index, type, label }: Props) => {
     },
     typeof debouncedSearchValue === "string" && debouncedSearchValue.length > 0
   )
-  const options = gateables ?? []
+  const [options, setOptions] = useState([])
 
-  const [selectValue, setSelectValue] = useState<string>()
+  useEffect(() => {
+    if (gateables) {
+      setOptions(gateables)
+    }
+  }, [gateables])
 
-  const selectedOption = options.find((option) => option.value === selectValue)
-
-  const reqParams = useWatch({ name: `requirements.${index}.data.params` })
-  const reqId = useWatch({ name: `requirements.${index}.data.id` })
-
-  const setRequirementFieldsFromOption = (option: {
-    value: string
-    label: string
-    img?: string
-    details?: string
-  }) => {
+  const selectOption = (option) => {
     setValue(`requirements.${index}.data.id`, option?.value)
     setValue(`requirements.${index}.data.params.label`, option?.label)
     setValue(`requirements.${index}.data.params.img`, option?.img)
     if (option?.details) {
       setValue(`requirements.${index}.data.params.artist`, option?.details)
     }
+    field.onChange(option?.value)
   }
 
-  const linkInputForm = useForm({ mode: "all", defaultValues: { linkInput: "" } })
   const { field } = useController({
-    control: linkInputForm.control,
-    name: "linkInput",
+    name: `requirements.${index}.data.id`,
     defaultValue: "",
     rules: {
-      required: "Thid field is required",
+      required: "This field is required",
+      validate: () => {
+        if (invalidLink) {
+          return "Invalid link"
+        }
+        return true
+      },
     },
   })
 
-  const debouncedLinkInputValue = useDebouncedState(field.value)
+  const selectedOption = options.find((option) => option.value === field.value)
 
-  const { gateables: linkItem } = useGateables(
-    "SPOTIFY",
-    undefined,
-    {
-      id: debouncedLinkInputValue,
-      type,
-    },
-    typeof debouncedLinkInputValue === "string" && debouncedLinkInputValue.length > 0
-  )
+  const fetcherWithSign = useFetcherWithSign()
 
-  useEffect(() => {
-    if (!linkItem) {
-      return
-    }
-
-    const item = linkItem as any as {
-      value: string
-      label: string
-      img?: string
-      details?: string
-    }
-
-    setRequirementFieldsFromOption(item)
-  }, [linkItem])
-
-  useEffect(() => {
-    if (!selectedOption) {
-      return
-    }
-    setRequirementFieldsFromOption(selectedOption)
-  }, [selectedOption])
+  const [invalidLink, setInvalidLink] = useState<boolean>(false)
 
   return (
     <>
@@ -126,15 +88,57 @@ const SpotifySearch = ({ index, type, label }: Props) => {
 
         <StyledSelect
           value={selectedOption}
-          onChange={(selected) => {
-            setSelectValue(selected?.value)
-          }}
+          onChange={(selected) => selectOption(selected)}
+          name={field.name}
+          onBlur={field.onBlur}
+          ref={field.ref}
           filterOption={() => true}
           options={options}
           isLoading={isValidating}
           onInputChange={(val, { action }) => {
             if (action !== "input-blur" && action !== "menu-close") {
-              setSearchValue(val)
+              setInvalidLink(false)
+              let parsedId = null
+              let parsedType = null
+              let linkMatched = false
+              let idMatched = false
+
+              if (LINK_REGEX.test(val)) {
+                const match = val.match(LINK_REGEX)
+                parsedType = match[1].trim()
+                parsedId = match[2].trim()
+                linkMatched = true
+              } else if (MAYBE_ID_REGEX.test(val)) {
+                parsedId = val.trim()
+                idMatched = true
+              }
+
+              if (linkMatched || idMatched) {
+                if (linkMatched && parsedType !== type) {
+                  setSearchValue(val)
+                  setInvalidLink(true)
+                } else {
+                  fetcherWithSign("/guild/listGateables", {
+                    method: "POST",
+                    body: { platformName: "SPOTIFY", id: parsedId, type: type },
+                  })
+                    .then(
+                      (res: {
+                        value: string
+                        label: string
+                        img?: string
+                        details?: string
+                      }) => {
+                        setSearchValue("")
+                        setOptions([res])
+                        selectOption(res)
+                      }
+                    )
+                    .catch(() => setSearchValue(val))
+                }
+              } else {
+                setSearchValue(val)
+              }
             }
           }}
           inputValue={searchValue}
@@ -145,63 +149,6 @@ const SpotifySearch = ({ index, type, label }: Props) => {
           {errors?.requirements?.[index]?.data?.id?.message}
         </FormErrorMessage>
       </FormControl>
-
-      <FormControl isInvalid={!!linkInputForm.formState.errors.linkInput?.message}>
-        <FormLabel>Or paste an id / link</FormLabel>
-
-        <Input
-          placeholder={"https://open.spotify.com/..."}
-          name={field.name}
-          ref={field.ref}
-          onBlur={field.onBlur}
-          value={field.value}
-          onChange={(event) => {
-            const linkOrId = event.target.value
-
-            if (LINK_REGEX.test(linkOrId)) {
-              const match = linkOrId.match(LINK_REGEX)
-              if (match?.[1] !== type) {
-                linkInputForm.setError("linkInput", {
-                  message: `Not a(n) ${type} id`,
-                })
-              }
-              linkInputForm.setValue(
-                "linkInput",
-                linkOrId.match(LINK_REGEX)?.[2] ?? ""
-              )
-            } else {
-              field.onChange(linkOrId)
-            }
-          }}
-        />
-
-        <FormErrorMessage>
-          {linkInputForm.formState.errors.linkInput?.message}
-        </FormErrorMessage>
-      </FormControl>
-
-      {reqParams?.label && (
-        <>
-          <FormLabel>Selected</FormLabel>
-          <HStack>
-            {reqParams?.img && (
-              <Circle overflow="hidden">
-                <Img src={reqParams?.img} width={10} height={10}></Img>
-              </Circle>
-            )}
-            <VStack alignItems={"start"} spacing={0}>
-              <Link isExternal href={`https://open.spotify.com/${type}/${reqId}`}>
-                {reqParams?.label}
-              </Link>
-              {reqParams?.artist && (
-                <Text color={"gray.500"} fontSize="sm">
-                  {reqParams?.artist}
-                </Text>
-              )}
-            </VStack>
-          </HStack>
-        </>
-      )}
     </>
   )
 }
