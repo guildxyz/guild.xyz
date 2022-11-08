5316ef2e9e8ea6140bec88fa99963e0e2fbc0df8
BrickheadJohnny <johnny.zgen@gmail.com>
2022-11-08 23:00:16 +0100

    useAbi - proxy support WIP

--- src/components/create-guild/Requirements/components/ContractStateFormCard/hooks/useAbi.ts
+++ src/components/create-guild/Requirements/components/ContractStateFormCard/hooks/useAbi.ts
@@ -2,23 +2,39 @@ import { Chains, RPC } from "connectors"
 import useSWRImmutable from "swr/immutable"
 import fetcher from "utils/fetcher"
 
-const fetchAbi = (url) =>
-  fetcher(url).then((res) => {
+const fetchAbi = (_: string, baseUrl: string, contract: string) =>
+  fetcher(
+    `${baseUrl}/api?module=contract&action=getsourcecode&address=${contract}`
+  ).then(async (res) => {
     if (res.status === "0") throw new Error(res.result)
 
-    const abi = JSON.parse(res.result)
-    return abi?.filter(
-      (method) => method.type === "function" && method.stateMutability === "view"
-    )
+    if (!res.result[0].Implementation) {
+      const abi = JSON.parse(res.result[0].ABI)
+
+      return abi?.filter(
+        (method) => method.type === "function" && method.stateMutability === "view"
+      )
+    }
+
+    // Waiting 5s so we don't get rate-limited
+    await new Promise((resolve) => setTimeout(resolve, 5000))
+
+    return fetcher(
+      `${baseUrl}/api?module=contract&action=getsourcecode&address=${res.result[0].Implementation}`
+    ).then((implRes) => {
+      if (implRes.status === "0") throw new Error(implRes.result)
+
+      const implAbi = JSON.parse(implRes.result[0].ABI)
+
+      return implAbi?.filter(
+        (method) => method.type === "function" && method.stateMutability === "view"
+      )
+    })
   })
 
 const useAbi = (chain: Chains, address: string) =>
   useSWRImmutable(
-    address
-      ? `${
-          RPC[chain ?? "ETHEREUM"].apiUrl
-        }/api?module=contract&action=getabi&address=${address}`
-      : null,
+    address ? ["getabi", RPC[chain ?? "ETHEREUM"].apiUrl, address] : null,
     fetchAbi
   )
 
