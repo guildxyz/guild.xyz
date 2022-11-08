691cda06d3147389b42b88cf56f185e1ffb9cb82
BrickheadJohnny <johnny.zgen@gmail.com>
2022-11-08 11:52:38 +0100

    Skeleton loaders in GuildRoleRequirementCard

--- src/components/[guild]/Requirements/components/GuildRoleRequirementCard.tsx
+++ src/components/[guild]/Requirements/components/GuildRoleRequirementCard.tsx
@@ -1,4 +1,4 @@
-import { Img } from "@chakra-ui/react"
+import { Img, Skeleton, Text } from "@chakra-ui/react"
 import DataBlock from "components/common/DataBlock"
 import useGuild from "components/[guild]/hooks/useGuild"
 import { Requirement } from "types"
@@ -9,7 +9,7 @@ type Props = {
 }
 
 const GuildRoleRequirementCard = ({ requirement }: Props): JSX.Element => {
-  const { name, roles } = useGuild(requirement.data.urlName)
+  const { name, roles, isLoading } = useGuild(requirement.data.urlName)
   const role = roles?.find((r) => r.id === requirement.data.roleId)
 
   return (
@@ -23,11 +23,15 @@ const GuildRoleRequirementCard = ({ requirement }: Props): JSX.Element => {
         ))
       }
     >
-      {"Have the "}
-      <DataBlock>{role?.name ?? "unknown"}</DataBlock>
-      {" role in the "}
-      <DataBlock>{name ?? "unknown"}</DataBlock>
-      {" guild"}
+      <Text as="span">{"Have the "}</Text>
+      <Skeleton as="span" isLoaded={!isLoading}>
+        {isLoading ? "Loading..." : <DataBlock>{role?.name ?? "unknown"}</DataBlock>}
+      </Skeleton>
+      <Text as="span">{" role in the "}</Text>
+      <Skeleton as="span" isLoaded={!isLoading}>
+        {isLoading ? "Loading..." : <DataBlock>{name ?? "unknown"}</DataBlock>}
+      </Skeleton>
+      <Text as="span">{" guild"}</Text>
     </RequirementCard>
   )
 }
