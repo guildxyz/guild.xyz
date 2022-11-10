3999a19d0c77fe8e00acbe617f054c17324c24ce
BrickheadJohnny <johnny.zgen@gmail.com>
2022-11-10 21:47:04 +0100

    Guild page - members section fixes

--- src/hooks/useUniqueMembers.ts
+++ src/hooks/useUniqueMembers.ts
@@ -1,9 +1,11 @@
 import { useMemo } from "react"
 import { Role } from "types"
 
-const useUniqueMembers = (roles: Role[]) =>
+const useUniqueMembers = (roles: Role[], additionalMembers: string[] = []) =>
   useMemo(() => {
-    const allMembers = roles?.flatMap((role) => role.members) ?? []
+    const allMembers = (roles?.flatMap((role) => role.members) ?? []).concat(
+      additionalMembers
+    )
     const uniqueMembers = [...new Set(allMembers)]
     const r = uniqueMembers?.filter((member) => typeof member === "string")
     return r
