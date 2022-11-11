7f398b6a878c7bf2ea2f348a97032ebf221c59ea
BrickheadJohnny <johnny.zgen@gmail.com>
2022-11-11 16:16:54 +0100

    useAbi - refactor & better error handling

--- src/components/create-guild/Requirements/components/ContractStateFormCard/hooks/useAbi.ts
+++ src/components/create-guild/Requirements/components/ContractStateFormCard/hooks/useAbi.ts
@@ -2,19 +2,27 @@ import { Chains, RPC } from "connectors"
 import useSWRImmutable from "swr/immutable"
 import fetcher from "utils/fetcher"
 
+const getContractMethods = (abi) => {
+  let parsedAbi
+  try {
+    parsedAbi = JSON.parse(abi)
+  } catch (_) {
+    // For some reason, "Contract source code not verified" message comes in the ABI property in this case
+    throw new Error(abi)
+  }
+
+  return parsedAbi?.filter(
+    (method) => method.type === "function" && method.stateMutability === "view"
+  )
+}
+
 const fetchAbi = (_: string, baseUrl: string, contract: string) =>
   fetcher(
     `${baseUrl}/api?module=contract&action=getsourcecode&address=${contract}`
   ).then(async (res) => {
     if (res.status === "0") throw new Error(res.result)
 
-    if (!res.result[0].Implementation) {
-      const abi = JSON.parse(res.result[0].ABI)
-
-      return abi?.filter(
-        (method) => method.type === "function" && method.stateMutability === "view"
-      )
-    }
+    if (!res.result[0].Implementation) return getContractMethods(res.result[0].ABI)
 
     // Waiting 5s so we don't get rate-limited
     await new Promise((resolve) => setTimeout(resolve, 5000))
@@ -23,12 +31,7 @@ const fetchAbi = (_: string, baseUrl: string, contract: string) =>
       `${baseUrl}/api?module=contract&action=getsourcecode&address=${res.result[0].Implementation}`
     ).then((implRes) => {
       if (implRes.status === "0") throw new Error(implRes.result)
-
-      const implAbi = JSON.parse(implRes.result[0].ABI)
-
-      return implAbi?.filter(
-        (method) => method.type === "function" && method.stateMutability === "view"
-      )
+      return getContractMethods(implRes.result[0].ABI)
     })
   })
 
