04818188c534ce73047741c336246900041069b7
balintligeti <balint.ligeti@hotmail.com>
2022-11-16 22:31:03 +0100

    feat(search): search by title on ecosystem page

--- src/pages/ecosystem.tsx
+++ src/pages/ecosystem.tsx
@@ -1,9 +1,41 @@
+import { usePrevious } from "@chakra-ui/react"
 import PageDetailsCard from "components/brain/pageDetailsCard"
 import Layout from "components/common/Layout"
 import LinkPreviewHead from "components/common/LinkPreviewHead"
 import CategorySection from "components/explorer/CategorySection"
+import SearchBar from "components/explorer/SearchBar"
 import { GetStaticProps } from "next"
+import { useEffect, useState } from "react"
 import { PageDetailsCardData } from "types"
+
+const searchByTitle = (cards: Array<PageDetailsCardData>, searchText: string) =>
+  cards.filter((card) => card.title.toLowerCase().includes(searchText.toLowerCase()))
+
+function Ecosystem({ cards }) {
+  const [search, setSearch] = useState<string>("")
+  const [renderedCards, setCard] = useState(cards)
+  const prevSearch = usePrevious(search)
+
+  useEffect(() => {
+    if (prevSearch === search || prevSearch === undefined) return
+    setCard(searchByTitle(cards, search))
+  }, [prevSearch, search, cards])
+
+  return (
+    <>
+      <LinkPreviewHead path="" />
+      <Layout title="Ecosystem" showBackButton={false}>
+        <SearchBar placeholder="Search guilds" {...{ search, setSearch }} />
+        <CategorySection fallbackText={"There are no pages"} mt="32px">
+          {renderedCards.map((card) => (
+            <PageDetailsCard pageData={card} key={card.id} />
+          ))}
+        </CategorySection>
+      </Layout>
+    </>
+  )
+}
+
 export const getStaticProps: GetStaticProps = async () => {
   const { Client } = require("@notionhq/client")
   const notion = new Client({ auth: process.env.NOTION_API_KEY })
@@ -29,21 +61,6 @@ export const getStaticProps: GetStaticProps = async () => {
     props: { cards },
     revalidate: 10,
   }
-}
-
-function Ecosystem({ cards }) {
-  return (
-    <>
-      <LinkPreviewHead path="" />
-      <Layout title="Ecosystem" showBackButton={false}>
-        <CategorySection fallbackText={"There are no pages"}>
-          {cards.map((card) => (
-            <PageDetailsCard pageData={card} key={card.id} />
-          ))}
-        </CategorySection>
-      </Layout>
-    </>
-  )
 }
 
 export default Ecosystem
