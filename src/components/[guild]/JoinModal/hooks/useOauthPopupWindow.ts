172873e3cd4debb737c67ceb558aeedae9fc51f6
Csete Balázs <csetebalu@gmail.com>
2022-11-17 19:03:27 +0100

    fix: csrf handling

--- src/components/[guild]/JoinModal/hooks/useOauthPopupWindow.ts
+++ src/components/[guild]/JoinModal/hooks/useOauthPopupWindow.ts
@@ -10,6 +10,13 @@ type OAuthData<Data> = {
   scope?: string
 } & Data
 
+const fallbackData = {
+  [process.env.NEXT_PUBLIC_DISCORD_CLIENT_ID]: randomBytes(16).toString("hex"),
+  [process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID]: randomBytes(16).toString("hex"),
+  [process.env.NEXT_PUBLIC_TWITTER_CLIENT_ID]: randomBytes(16).toString("hex"),
+  [process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID]: randomBytes(16).toString("hex"),
+}
+
 const fetcherWithAuthorization = async (authorization: string, endpoint: string) => {
   const response = await fetch(endpoint, {
     headers: {
@@ -48,21 +55,11 @@ const useOauthPopupWindow = <OAuthResponse = { code: string }>(
   const { addDatadogError, addDatadogAction } = useDatadog()
   const toast = useToast()
 
-  const {
-    data: csrfToken,
-    mutate: mutateCSRFToken,
-    isValidating,
-  } = useSWRImmutable(
+  const { data: csrfToken, mutate: mutateCSRFToken } = useSWRImmutable(
     ["CSRFToken", oauthOptions.client_id],
     () => randomBytes(16).toString("hex"),
-    { revalidateOnMount: false }
+    { revalidateOnMount: false, fallbackData: fallbackData[oauthOptions.client_id] }
   )
-
-  useEffect(() => {
-    if (!isValidating && !csrfToken) {
-      mutateCSRFToken()
-    }
-  }, [isValidating, csrfToken])
 
   const redirectUri =
     typeof window !== "undefined" &&
@@ -153,7 +150,7 @@ const useOauthPopupWindow = <OAuthResponse = { code: string }>(
         window.localStorage.removeItem(dataKey)
         setIsAuthenticating(false)
         window.localStorage.setItem(shouldCloseKey, "true")
-        mutateCSRFToken(undefined, { revalidate: false })
+        mutateCSRFToken(randomBytes(16).toString("hex"))
       })
   }, [windowInstance])
 
