fb7e338492f37f7bcd1f8f62929775ff6fc68dbd
valid <valid@zgen.hu>
2022-11-20 00:46:17 +0100

    101 integration: trim course title

--- src/requirements/101/101Requirement.tsx
+++ src/requirements/101/101Requirement.tsx
@@ -37,7 +37,7 @@ const HundredNOneRequirement = ({
     >
       {`Have the badge of the `}
       <DataBlock isLoading={!badge && isValidating}>
-        {badge?.courses?.[0]?.title ?? requirement.data.id}
+        {badge?.courses?.[0]?.title.trim() ?? requirement.data.id}
       </DataBlock>
       {` 101 course `}
     </Requirement>
