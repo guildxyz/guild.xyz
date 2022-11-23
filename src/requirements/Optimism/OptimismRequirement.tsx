c57e509a43878f01ef2579fd10f95aa1b35694c4
BrickheadJohnny <johnny.zgen@gmail.com>
2022-11-23 15:50:32 +0100

    OptimismRequirement component

--- src/requirements/Optimism/OptimismRequirement.tsx
+++ src/requirements/Optimism/OptimismRequirement.tsx
@@ -1,2 +1,34 @@
-const OptimismRequirement = (): JSX.Element => null
+import DataBlock from "components/common/DataBlock"
+import { RequirementComponentProps } from "requirements"
+import Requirement from "requirements/common/Requirement"
+import shortenHex from "utils/shortenHex"
+
+const OptimismRequirement = ({
+  requirement,
+  ...rest
+}: RequirementComponentProps): JSX.Element => (
+  <Requirement image={"networkLogos/optimism.svg"} {...rest}>
+    {requirement.type === "OPTIMISM_ATTESTATION" ? (
+      <>
+        {"Have "}
+        {requirement.data.val ? (
+          <>
+            {"the "}
+            <DataBlock>{requirement.data.val}</DataBlock>
+          </>
+        ) : (
+          "an"
+        )}
+        {" attestation from "}
+        <DataBlock>{shortenHex(requirement.data.creator, 3)}</DataBlock>
+        {" in the "}
+        <DataBlock>{requirement.data.key}</DataBlock>
+        {" theme"}
+      </>
+    ) : (
+      "Have an Optimism profile pic"
+    )}
+  </Requirement>
+)
+
 export default OptimismRequirement
