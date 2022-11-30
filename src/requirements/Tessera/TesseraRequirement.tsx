f84f40495e2abacbdc7e89fe2cf174d9be96ac9d
BrickheadJohnny <johnny.zgen@gmail.com>
2022-11-30 12:06:50 +0100

    TesseraRequirement component

--- src/requirements/Tessera/TesseraRequirement.tsx
+++ src/requirements/Tessera/TesseraRequirement.tsx
@@ -1,3 +1,48 @@
-const TesseraRequirement = (): JSX.Element => null
+import DataBlock from "components/common/DataBlock"
+import { RequirementComponentProps } from "requirements"
+import Requirement from "requirements/common/Requirement"
+import shortenHex from "utils/shortenHex"
+
+const TesseraRequirement = ({
+  requirement,
+  ...rest
+}: RequirementComponentProps): JSX.Element => (
+  <Requirement image="/requirementLogos/tessera.svg" {...rest}>
+    {(() => {
+      switch (requirement.type) {
+        case "TESSERA_HOLD":
+          return (
+            <>
+              {`Hold ${
+                requirement.data.minAmount > 0
+                  ? `at least ${requirement.data.minAmount}`
+                  : "a"
+              } fraction of the `}
+              <DataBlock>{shortenHex(requirement.data.vault, 3)}</DataBlock>
+              {" NFT"}
+            </>
+          )
+        case "TESSERA_VAULTSHARE":
+          return (
+            <>
+              {`Hold ${
+                requirement.data.minShare > 0
+                  ? `at least ${requirement.data.minShare}%`
+                  : "any percentage"
+              } of the fractions of the `}
+              <DataBlock>{shortenHex(requirement.data.vault, 3)}</DataBlock>
+              {" NFT"}
+            </>
+          )
+        case "TESSERA_LISTINGS":
+          return "TODO"
+        case "TESSERA_USER_SINCE":
+          return `Be a Guild.xyz user at least since ${
+            requirement.data.minDate?.split("T")[0]
+          }`
+      }
+    })()}
+  </Requirement>
+)
 
 export default TesseraRequirement
