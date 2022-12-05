36fe632d8ab07c7635d1cecc60a49a615718b244
BrickheadJohnny <johnny.zgen@gmail.com>
2022-12-05 10:37:01 +0100

    Rep3Requirement component

--- src/requirements/Rep3/Rep3Requirement.tsx
+++ src/requirements/Rep3/Rep3Requirement.tsx
@@ -1,3 +1,20 @@
-const Rep3Requirement = (): JSX.Element => null
+import DataBlock from "components/common/DataBlock"
+import { RequirementComponentProps } from "requirements"
+import BlockExplorerUrl from "requirements/common/BlockExplorerUrl"
+import Requirement from "requirements/common/Requirement"
+import shortenHex from "utils/shortenHex"
+
+const Rep3Requirement = ({
+  requirement,
+  ...rest
+}: RequirementComponentProps): JSX.Element => (
+  <Requirement
+    image={"/requirementLogos/rep3.jpeg"}
+    footer={<BlockExplorerUrl requirement={requirement} {...rest} />}
+  >
+    {`Have membership level of ${requirement.data.id} in `}
+    {requirement.name ?? <DataBlock>{shortenHex(requirement.address, 3)}</DataBlock>}
+  </Requirement>
+)
 
 export default Rep3Requirement
