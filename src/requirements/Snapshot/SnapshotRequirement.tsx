ffde1b07202e58d69af1a844dad9f489eeed9683
BrickheadJohnny <johnny.zgen@gmail.com>
2022-12-06 18:32:02 +0100

    SnapshotRequirement - full refactor

--- src/requirements/Snapshot/SnapshotRequirement.tsx
+++ src/requirements/Snapshot/SnapshotRequirement.tsx
@@ -1,86 +1,210 @@
-import {
-  Collapse,
-  Icon,
-  Table,
-  Tbody,
-  Td,
-  Th,
-  Thead,
-  Tr,
-  useDisclosure,
-} from "@chakra-ui/react"
-import CopyableAddress from "components/common/CopyableAddress"
+import { Collapse, Icon, Link, Stack, Text, useDisclosure } from "@chakra-ui/react"
+import DataBlock from "components/common/DataBlock"
 import { CaretDown } from "phosphor-react"
+import { Fragment } from "react"
 import { RequirementComponentProps } from "requirements"
 import Requirement from "../common/Requirement"
 import { RequirementButton } from "../common/RequirementButton"
+import StrategyParamsTable from "./components/StrategyParamsTable"
+import useProposal from "./hooks/useProposal"
+import useSpace from "./hooks/useSpace"
 
 const SnapshotRequirement = ({
   requirement,
   ...rest
 }: RequirementComponentProps): JSX.Element => {
   const { isOpen, onToggle } = useDisclosure()
+  const strategies = requirement.data.strategies
+
+  const { space } = useSpace(requirement.data.space)
+  const { proposal } = useProposal(requirement.data.proposal)
+
+  const formattedDate = requirement.data.since
+    ? new Date(requirement.data.since).toLocaleDateString()
+    : null
 
   return (
     <Requirement
       image="/requirementLogos/snapshot.png"
       footer={
-        <>
-          <RequirementButton
-            rightIcon={
-              <Icon
-                as={CaretDown}
-                transform={isOpen && "rotate(-180deg)"}
-                transition="transform .3s"
-              />
-            }
-            onClick={onToggle}
-          >
-            View parameters
-          </RequirementButton>
-          <Collapse in={isOpen}>
-            <Table
-              variant="simple"
-              w="full"
-              sx={{ tableLayout: "fixed", borderCollapse: "unset" }}
-              size="sm"
-              bg="blackAlpha.100"
-              borderWidth="1px"
-              borderRadius="md"
+        requirement.type === "SNAPSHOT_STRATEGY" && (
+          <>
+            <RequirementButton
+              rightIcon={
+                <Icon
+                  as={CaretDown}
+                  transform={isOpen && "rotate(-180deg)"}
+                  transition="transform .3s"
+                />
+              }
+              onClick={onToggle}
             >
-              <Thead>
-                <Tr>
-                  <Th>Param</Th>
-                  <Th>Value</Th>
-                </Tr>
-              </Thead>
-              <Tbody fontWeight="normal" fontSize="xs">
-                {Object.entries(requirement.data?.strategy?.params || {})?.map(
-                  ([name, value]) => (
-                    <Tr key={name}>
-                      <Td>{name}</Td>
-                      <Td>
-                        {value?.toString()?.startsWith("0x") ? (
-                          <CopyableAddress
-                            address={value.toString()}
-                            fontWeight="normal"
-                            fontSize="xs"
-                          />
-                        ) : (
-                          value?.toString()
-                        )}
-                      </Td>
-                    </Tr>
-                  )
-                )}
-              </Tbody>
-            </Table>
-          </Collapse>
-        </>
+              View parameters
+            </RequirementButton>
+            <Collapse style={{ marginTop: 0 }} in={isOpen}>
+              {requirement.data.strategies.map((strategy, i) => (
+                <Stack pt={2} pr={6} key={i}>
+                  <Text as="span" fontWeight="bold" fontSize="sm">
+                    {strategy.name}
+                  </Text>
+                  {!Object.keys(strategy.params ?? {}).length ? (
+                    <Text
+                      position="relative"
+                      top={-2}
+                      as="span"
+                      fontSize="sm"
+                      color="gray"
+                    >
+                      No parameters
+                    </Text>
+                  ) : (
+                    <StrategyParamsTable params={strategy.params} />
+                  )}
+                </Stack>
+              ))}
+            </Collapse>
+          </>
+        )
       }
       {...rest}
     >
-      {`Satisfy the ${requirement.data?.strategy?.name} snapshot strategy`}
+      {(() => {
+        switch (requirement.type) {
+          case "SNAPSHOT_STRATEGY":
+            return (
+              <>
+                {"Satisfy the "}
+                {strategies.map((s, i) => (
+                  <Fragment key={i}>
+                    <DataBlock>{s.name}</DataBlock>
+                    {i < strategies.length - 1 && ", "}
+                  </Fragment>
+                ))}
+                {` Snapshot ${strategies.length > 1 ? "Strategies" : "Strategy"}`}
+                {requirement.data.space &&
+                  ` in the ${space?.name ?? requirement.data.space} space`}
+              </>
+            )
+          case "SNAPSHOT_SPACE_ADMIN":
+            return (
+              <>
+                {`Be an admin of the `}
+                <Link
+                  href={`https://snapshot.org/#/${requirement.data.space}`}
+                  isExternal
+                  colorScheme="orange"
+                  fontWeight="medium"
+                >
+                  {space?.name ?? requirement.data.space}
+                </Link>
+                {` space`}
+              </>
+            )
+          case "SNAPSHOT_SPACE_AUTHOR":
+            return (
+              <>
+                {`Be an author in the `}
+                <Link
+                  href={`https://snapshot.org/#/${requirement.data.space}`}
+                  isExternal
+                  colorScheme="orange"
+                  fontWeight="medium"
+                >
+                  {space?.name ?? requirement.data.space}
+                </Link>
+                {` space`}
+              </>
+            )
+          case "SNAPSHOT_FOLLOW":
+          case "SNAPSHOT_FOLLOW_SINCE":
+            return (
+              <>
+                {`Follow the `}
+                <Link
+                  href={`https://snapshot.org/#/${requirement.data.space}`}
+                  isExternal
+                  colorScheme="orange"
+                  fontWeight="medium"
+                >
+                  {space?.name ?? requirement.data.space}
+                </Link>
+                {` space`}
+                {requirement.type === "SNAPSHOT_FOLLOW_SINCE" && (
+                  <>
+                    {` since at least `}
+                    <DataBlock>{formattedDate}</DataBlock>
+                  </>
+                )}
+              </>
+            )
+          case "SNAPSHOT_USER_SINCE":
+            return (
+              <>
+                {`Be a Snapshot user since at least `}
+                <DataBlock>{formattedDate}</DataBlock>
+              </>
+            )
+          case "SNAPSHOT_VOTES":
+            return (
+              <>
+                {`Vote ${requirement.data.minTimes} times`}
+                {requirement.data.space && (
+                  <>
+                    {` in the `}
+                    <Link
+                      href={`https://snapshot.org/#/${requirement.data.space}`}
+                      isExternal
+                      colorScheme="orange"
+                      fontWeight="medium"
+                    >
+                      {space?.name ?? requirement.data.space}
+                    </Link>
+                    {` space`}
+                  </>
+                )}
+                {requirement.data.proposal && (
+                  <>
+                    {` on proposal `}
+                    <DataBlock>
+                      {proposal?.title ?? requirement.data.proposal}
+                    </DataBlock>
+                  </>
+                )}
+              </>
+            )
+          case "SNAPSHOT_PROPOSALS":
+            return (
+              <>
+                {`Make at least ${requirement.data.minAmount}${
+                  requirement.data.successfulOnly ? " successful" : ""
+                } proposals`}
+                {requirement.data.space && (
+                  <>
+                    {` in the `}
+                    <Link
+                      href={`https://snapshot.org/#/${requirement.data.space}`}
+                      isExternal
+                      colorScheme="orange"
+                      fontWeight="medium"
+                    >
+                      {space?.name ?? requirement.data.space}
+                    </Link>
+                    {` space`}
+                  </>
+                )}
+                {requirement.data.state && (
+                  <>
+                    {` which are in an `}
+                    <DataBlock>{requirement.data.state}</DataBlock>
+                    {` state`}
+                  </>
+                )}
+              </>
+            )
+          case "SNAPSHOT_MAJORITY_VOTES":
+            return `Vote with majority in the rate of ${requirement.data.minRatio}`
+        }
+      })()}
     </Requirement>
   )
 }
