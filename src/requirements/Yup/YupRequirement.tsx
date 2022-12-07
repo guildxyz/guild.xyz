9b021d3eb22993642b9e025ca9a786400e8ddb4d
BrickheadJohnny <johnny.zgen@gmail.com>
2022-12-07 23:34:20 +0100

    Update logo & finish YupRequirement component

--- src/requirements/Yup/YupRequirement.tsx
+++ src/requirements/Yup/YupRequirement.tsx
@@ -1,3 +1,14 @@
-const YupRequirement = (): JSX.Element => null
+import { RequirementComponentProps } from "requirements"
+import Requirement from "requirements/common/Requirement"
+
+const YupRequirement = ({
+  requirement,
+  ...rest
+}: RequirementComponentProps): JSX.Element => (
+  <Requirement image="/requirementLogos/yup.svg" {...rest}>
+    {`Have at least ${requirement.data.minAmount} score`}
+    {requirement.data.adapter && ` of the ${requirement.data.adapter} adapter`}
+  </Requirement>
+)
 
 export default YupRequirement
