32fa6bf83386dcdc23532b90f7a91c1efe2cd665
BrickheadJohnny <johnny.zgen@gmail.com>
2022-12-16 20:52:27 +0100

    YupRequirement - adapter in DataBlock

--- src/requirements/Yup/YupRequirement.tsx
+++ src/requirements/Yup/YupRequirement.tsx
@@ -1,3 +1,4 @@
+import DataBlock from "components/common/DataBlock"
 import { RequirementComponentProps } from "requirements"
 import Requirement from "requirements/common/Requirement"
 
@@ -7,7 +8,13 @@ const YupRequirement = ({
 }: RequirementComponentProps): JSX.Element => (
   <Requirement image="/requirementLogos/yup.svg" {...rest}>
     {`Have at least ${requirement.data.minAmount} score`}
-    {requirement.data.adapter && ` of the ${requirement.data.adapter} adapter`}
+    {requirement.data.adapter && (
+      <>
+        {" of the "}
+        <DataBlock>{requirement.data.adapter}</DataBlock>
+        {" adapter"}
+      </>
+    )}
   </Requirement>
 )
 
