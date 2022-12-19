ea96a44e51aa7c690f88e97938ac93b9d62618f5
valid <valid@zgen.hu>
2022-12-19 21:25:23 +0100

    `/statusUpdate/guildify`: show error toast on error

--- src/components/[guild]/RolePlatforms/components/PlatformCard/components/useDiscordCardProps/DiscordCardMenu/DiscordCardMenu.tsx
+++ src/components/[guild]/RolePlatforms/components/PlatformCard/components/useDiscordCardProps/DiscordCardMenu/DiscordCardMenu.tsx
@@ -14,6 +14,7 @@ import {
 import CreatePoap from "components/[guild]/CreatePoap"
 import useGuild from "components/[guild]/hooks/useGuild"
 import SendDiscordJoinButtonModal from "components/[guild]/Onboarding/components/SummonMembers/components/SendDiscordJoinButtonModal"
+import useShowErrorToast from "hooks/useShowErrorToast"
 import { useSubmitWithSign } from "hooks/useSubmit"
 import {
   ArrowsCounterClockwise,
@@ -48,6 +49,7 @@ const DiscordCardMenu = ({ platformGuildId }: Props): JSX.Element => {
   } = useDisclosure()
 
   const { id, poaps } = useGuild()
+  const showErrorToast = useShowErrorToast()
 
   const syncMembersFromDiscord = async ({ validation, data }) =>
     fetcher(`/statusUpdate/guildify/${id}`, {
@@ -59,6 +61,7 @@ const DiscordCardMenu = ({ platformGuildId }: Props): JSX.Element => {
     syncMembersFromDiscord,
     {
       onSuccess: () => mutate(`/statusUpdate/guild/${id}`),
+      onError: (err) => showErrorToast(err),
     }
   )
 
