2965b4ce7f7112672767d06b3665e7b41e428a31
BrickheadJohnny <johnny.zgen@gmail.com>
2023-01-03 14:22:33 +0100

    RoleRequirements - variable list item sizes

--- src/components/[guild]/Requirements/RoleRequirements.tsx
+++ src/components/[guild]/Requirements/RoleRequirements.tsx
@@ -1,6 +1,6 @@
 import { Box, Collapse, Spinner, useColorModeValue, VStack } from "@chakra-ui/react"
-import React, { useState } from "react"
-import { FixedSizeList } from "react-window"
+import React, { memo, useEffect, useRef, useState } from "react"
+import { VariableSizeList } from "react-window"
 import { Role } from "types"
 import LogicDivider from "../LogicDivider"
 import ExpandRequirementsButton from "./components/ExpandRequirementsButton"
@@ -22,12 +22,34 @@ const RoleRequirements = ({ role }: Props) => {
     "var(--chakra-colors-gray-900)"
   )
 
-  const Row = ({ index, style }) => (
-    <Box key={index} style={style}>
-      <RequirementDisplayComponent requirement={hiddenRequirements[index]} />
-      {index < hiddenRequirements.length - 1 && <LogicDivider logic={role.logic} />}
-    </Box>
-  )
+  // Row related refs, state, and functions
+  const listRef = useRef(null)
+  const rowHeights = useRef<Record<number, number>>({})
+
+  const setRowHeight = (rowIndex: number, rowHeight: number) => {
+    listRef.current.resetAfterIndex(0)
+    rowHeights.current = { ...rowHeights.current, [rowIndex]: rowHeight }
+  }
+
+  const Row = memo(({ index, style }: any) => {
+    const rowRef = useRef<HTMLDivElement>(null)
+
+    useEffect(() => {
+      if (!rowRef.current) return
+      setRowHeight(index, rowRef.current.clientHeight)
+    }, [rowRef])
+
+    return (
+      <Box style={style}>
+        <Box ref={rowRef}>
+          <RequirementDisplayComponent requirement={hiddenRequirements[index]} />
+          {index < hiddenRequirements.length - 1 && (
+            <LogicDivider logic={role.logic} />
+          )}
+        </Box>
+      </Box>
+    )
+  })
 
   if (!role.requirements?.length)
     return (
@@ -38,21 +60,38 @@ const RoleRequirements = ({ role }: Props) => {
 
   if (role.requirements.length > 10)
     return (
-      <Box
-        sx={{
-          maskImage: `linear-gradient(to top, transparent 0%, black 5%, black 95%, transparent 100%)`,
-          WebkitMaskImage: `linear-gradient(to top, transparent 0%, black 5%, black 95%, transparent 100%)`,
-        }}
-      >
-        <FixedSizeList
-          height={318}
-          itemCount={hiddenRequirements.length}
-          itemSize={106}
-          className="custom-scrollbar"
+      <>
+        <Box
+          sx={{
+            maskImage: `linear-gradient(to top, transparent 0%, black 5%, black 95%, transparent 100%)`,
+            WebkitMaskImage: `linear-gradient(to top, transparent 0%, black 5%, black 95%, transparent 100%)`,
+          }}
         >
-          {Row}
-        </FixedSizeList>
-      </Box>
+          <VariableSizeList
+            ref={listRef}
+            height={318}
+            itemCount={hiddenRequirements.length}
+            itemSize={(i) => rowHeights.current[i] ?? 106}
+            className="custom-scrollbar"
+            style={{
+              paddingRight: "0.5rem",
+            }}
+          >
+            {Row}
+          </VariableSizeList>
+        </Box>
+
+        <Box
+          position="absolute"
+          bottom={{ base: 8, md: 0 }}
+          left={0}
+          right={0}
+          height={6}
+          bgGradient={`linear-gradient(to top, ${shadowColor}, transparent)`}
+          pointerEvents="none"
+          opacity={0.6}
+        />
+      </>
     )
 
   return (
