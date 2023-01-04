1a829ceefe75d055a416eda006bbf66d4cbaa920
valid <valid@zgen.hu>
2023-01-04 04:38:39 +0100

    don't auto-guildify on create, but manually at onboarding finish

--- src/components/create-guild/hooks/useCreateGuild.tsx
+++ src/components/create-guild/hooks/useCreateGuild.tsx
@@ -7,7 +7,7 @@ import { useSubmitWithSign, WithValidation } from "hooks/useSubmit"
 import useToast from "hooks/useToast"
 import { useRouter } from "next/router"
 import { Guild, PlatformType, Requirement } from "types"
-import fetcher, { useFetcherWithSign } from "utils/fetcher"
+import fetcher from "utils/fetcher"
 import replacer from "utils/guildJsonReplacer"
 import preprocessRequirements from "utils/preprocessRequirements"
 
@@ -22,7 +22,6 @@ const useCreateGuild = () => {
   const showErrorToast = useShowErrorToast()
   const triggerConfetti = useJsConfetti()
   const router = useRouter()
-  const fetcherWithSign = useFetcherWithSign()
 
   const fetchData = async ({
     validation,
@@ -50,13 +49,6 @@ const useCreateGuild = () => {
         status: "success",
       })
       router.push(`/${response_.urlName}`)
-
-      if (response_.guildPlatforms[0].platformId === PlatformType.DISCORD)
-        fetcherWithSign(`/statusUpdate/guildify/${response_.id}?force=true`, {
-          body: {
-            notifyUsers: false,
-          },
-        })
 
       matchMutate(/^\/guild\/address\//)
       matchMutate(/^\/guild\?order/)
