7e1a7aa69ea02cc993818d56dd5048af315cd741
BrickheadJohnny <johnny.zgen@gmail.com>
2023-01-09 08:43:42 +0100

    BasicInfo - UI simplification

--- src/components/create-guild/BasicInfo.tsx
+++ src/components/create-guild/BasicInfo.tsx
@@ -1,17 +1,17 @@
 import {
-  Center,
   FormControl,
+  FormHelperText,
   Icon,
   Input,
   InputGroup,
   InputLeftElement,
+  Link,
   Stack,
   Text,
 } from "@chakra-ui/react"
-import Button from "components/common/Button"
 import FormErrorMessage from "components/common/FormErrorMessage"
 import Section from "components/common/Section"
-import { DiscordLogo, TwitterLogo } from "phosphor-react"
+import { ArrowSquareOut, TwitterLogo } from "phosphor-react"
 import { useFormContext } from "react-hook-form"
 import { GuildFormType } from "types"
 import { useCreateGuildContext } from "./CreateGuildContext"
@@ -30,36 +30,17 @@ const BasicInfo = (): JSX.Element => {
       <Stack spacing={10}>
         <Section title="How could we contact you?">
           <FormControl>
-            <Stack alignItems="center" direction={{ base: "column", sm: "row" }}>
-              <Input
-                type="email"
-                maxW={{ base: "full", sm: "sm" }}
-                placeholder="Your e-mail address"
-              />
-              <Center w={{ base: "full", sm: 8 }} h={{ base: 6, sm: 10 }}>
-                <Text
-                  as="span"
-                  fontWeight="bold"
-                  colorScheme="gray"
-                  fontSize="sm"
-                  textTransform="uppercase"
-                >
-                  or
-                </Text>
-              </Center>
-              <Button
-                as="a"
-                href="https://discord.gg/guildxyz"
-                target="_blank"
-                leftIcon={<DiscordLogo />}
-                colorScheme="indigo"
-                w={{ base: "full", sm: "max-content" }}
-                h={10}
-                flexShrink={0}
-              >
-                Join our Discord
-              </Button>
-            </Stack>
+            <Input
+              maxW={{ base: "full", sm: "sm" }}
+              placeholder="E-mail address or Telegram handle"
+            />
+            <FormHelperText>
+              or{" "}
+              <Link isExternal href="https://discord.gg/guildxyz">
+                <Text as="span">join our Discord</Text>
+                <Icon ml={1} as={ArrowSquareOut} />
+              </Link>
+            </FormHelperText>
           </FormControl>
         </Section>
 
