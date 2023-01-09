8fa6ba3ab8b3ddb86278b966f146d6798d86f26b
BrickheadJohnny <johnny.zgen@gmail.com>
2023-01-09 09:23:22 +0100

    fix PaginationButtons order

--- src/components/[guild]/Onboarding/components/PaginationButtons.tsx
+++ src/components/[guild]/Onboarding/components/PaginationButtons.tsx
@@ -11,6 +11,16 @@ const PaginationButtons = ({
 }) => (
   <Flex width="full" justify={{ md: "flex-end" }} mt="7" mb={{ base: 5, md: 0 }}>
     <Button
+      isDisabled={isPrevDisabled}
+      mr={2}
+      onClick={prevStep}
+      size="sm"
+      variant="ghost"
+      data-dd-action-name={`${activeStep + 1}-prev [onboarding]`}
+    >
+      Prev
+    </Button>
+    <Button
       size="sm"
       onClick={nextStep}
       isLoading={nextLoading}
@@ -18,17 +28,6 @@ const PaginationButtons = ({
       data-dd-action-name={`${activeStep + 1}-next [onboarding]`}
     >
       {nextLabel}
-    </Button>
-    <Button
-      isDisabled={isPrevDisabled}
-      mr={2}
-      onClick={prevStep}
-      size="sm"
-      variant="ghost"
-      order={{ md: -1 }}
-      data-dd-action-name={`${activeStep + 1}-prev [onboarding]`}
-    >
-      Prev
     </Button>
   </Flex>
 )
