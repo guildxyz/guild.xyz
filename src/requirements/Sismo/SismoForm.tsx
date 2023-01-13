319f0f4fbc14dba7ba3a377c616d8587831c1169
BrickheadJohnny <johnny.zgen@gmail.com>
2023-01-13 13:17:34 +0100

    SismoForm - set address automatically

--- src/requirements/Sismo/SismoForm.tsx
+++ src/requirements/Sismo/SismoForm.tsx
@@ -9,6 +9,7 @@ import FormErrorMessage from "components/common/FormErrorMessage"
 import StyledSelect from "components/common/StyledSelect"
 import OptionImage from "components/common/StyledSelect/components/CustomSelectOption/components/OptionImage"
 import { Chain } from "connectors"
+import { useEffect } from "react"
 import { Controller, useFormContext, useWatch } from "react-hook-form"
 import { RequirementFormProps } from "requirements"
 import ChainPicker from "requirements/common/ChainPicker"
@@ -17,9 +18,9 @@ import parseFromObject from "utils/parseFromObject"
 import useSismoBadges, { SismoBadgeChain } from "./hooks/useSismoBadges"
 
 const sismoContracts: Record<SismoBadgeChain, string> = {
-  POLYGON: "0xF12494e3545D49616D9dFb78E5907E9078618a34",
+  POLYGON: "0xf12494e3545d49616d9dfb78e5907e9078618a34",
   GNOSIS: "0xa67f1c6c96cb5dd6ef24b07a77893693c210d846",
-  GOERLI: "0xA251eb9Be4e7E2bb382268eCdd0a5fca0A962E6c",
+  GOERLI: "0xa251eb9be4e7e2bb382268ecdd0a5fca0a962e6c",
 }
 
 const SismoForm = ({ baseFieldPath }: RequirementFormProps): JSX.Element => {
@@ -35,14 +36,17 @@ const SismoForm = ({ baseFieldPath }: RequirementFormProps): JSX.Element => {
 
   const pickedBadge = data?.find((option) => option.value === badgeId)
 
+  useEffect(
+    () => setValue(`${baseFieldPath}.address`, sismoContracts[chain]),
+    [chain]
+  )
+
   return (
     <Stack spacing={4} alignItems="start">
       <ChainPicker
         controlName={`${baseFieldPath}.chain`}
         supportedChains={Object.keys(sismoContracts) as Chain[]}
-        onChange={() => {
-          setValue(`${baseFieldPath}.data.id`, null)
-        }}
+        onChange={() => setValue(`${baseFieldPath}.data.id`, null)}
       />
 
       <FormControl
