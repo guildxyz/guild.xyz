9d869dc47dcc98d05db1a7102944c91be8068124
BrickheadJohnny <92519134+BrickheadJohnny@users.noreply.github.com>
2023-01-17 18:09:15 +0100

    Update Sismo requirements (#647)
    * Update Sismo requirements - WIP (#644)
    * update(sismo): add updated networks
    * doc(readme): add section install for windows users
    * fix(sismo): remove mainnet endpoint for now
    * fix: rename label
    * fix: rename placeholder
    * Small refactor & cleanup
    * SismoForm - set address automatically
    * Support for playground badges
    
    Co-authored-by: François <francois.guezengar@hotmail.fr>

--- src/requirements/Sismo/SismoRequirement.tsx
+++ src/requirements/Sismo/SismoRequirement.tsx
@@ -2,9 +2,13 @@ import { RequirementComponentProps } from "requirements"
 import DataBlock from "requirements/common/DataBlock"
 import Requirement from "../common/Requirement"
 import useSismoBadges from "./hooks/useSismoBadges"
+import { DEPRECATED_PLAYGROUND_ADDRESS } from "./SismoForm"
 
 const SismoRequirement = ({ requirement, ...rest }: RequirementComponentProps) => {
-  const { data, isValidating, error } = useSismoBadges(requirement.data.type)
+  const { data, isValidating, error } = useSismoBadges(
+    requirement.chain,
+    requirement.address === DEPRECATED_PLAYGROUND_ADDRESS
+  )
   const badge = data?.find((b) => b.value === requirement.data.id)
 
   return (
