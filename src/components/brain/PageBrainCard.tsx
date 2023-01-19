cce518890e3eede55fb44176ae3c8a9d024adf07
balintligeti <balint.ligeti@hotmail.com>
2023-01-19 10:53:11 +0100

    feat(page-brain-card): new design wtih hover anim

--- src/components/brain/PageBrainCard.tsx
+++ src/components/brain/PageBrainCard.tsx
@@ -1,4 +1,4 @@
-import { Card, Text, useColorMode } from "@chakra-ui/react"
+import { Card, Text } from "@chakra-ui/react"
 import Link from "components/common/Link"
 import { BrainCardData } from "types"
 
@@ -6,66 +6,52 @@ type Props = {
   pageData: BrainCardData
 }
 
-const PageBrainCard = ({ pageData }: Props): JSX.Element => {
-  const { colorMode } = useColorMode()
-
-  return (
-    <Link
-      href={`/brain/${pageData.id}`}
-      prefetch={false}
-      _hover={{ textDecor: "none" }}
+const PageBrainCard = ({ pageData }: Props): JSX.Element => (
+  <Link
+    href={`/brain/${pageData.id}`}
+    prefetch={false}
+    _hover={{ textDecor: "none" }}
+    borderRadius="2xl"
+  >
+    <Card
       borderRadius="2xl"
+      px={{ base: 5, sm: 6 }}
+      py="4"
+      w="full"
+      h="full"
+      background="rgba(63,63,70, 0.45)"
+      justifyContent="top"
+      _before={{
+        content: `""`,
+        position: "absolute",
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+        bg: "primary.300",
+        opacity: 0,
+        transition: "opacity 0.2s",
+        borderRadius: "2xl",
+      }}
+      _hover={{
+        _before: {
+          opacity: 0.1,
+          borderRadius: "2xl",
+        },
+      }}
     >
-      <Card
-        borderRadius="2xl"
-        px={{ base: 5, sm: 6 }}
-        py="4"
-        w="full"
-        h="full"
-        bg={colorMode === "light" ? "white" : "gray.700"}
-        justifyContent="top"
-        _before={{
-          content: `""`,
-          position: "absolute",
-          top: 0,
-          bottom: 0,
-          left: 0,
-          right: 0,
-          bg: "primary.300",
-          opacity: 0,
-          transition: "opacity 0.2s",
-        }}
-        _hover={{
-          _before: {
-            opacity: 0.1,
-            borderRadius: "2xl",
-          },
-        }}
-        _active={{
-          _before: {
-            opacity: 0.17,
-            borderRadius: "2xl",
-          },
-          _after: {
-            opacity: 0.17,
-            borderRadius: "2xl",
-            backgroundColor: "red",
-          },
-        }}
+      <Text
+        as="span"
+        fontFamily="display"
+        fontSize="lg"
+        fontWeight="bold"
+        letterSpacing="wide"
+        maxW="full"
       >
-        <Text
-          as="span"
-          fontFamily="display"
-          fontSize="lg"
-          fontWeight="bold"
-          letterSpacing="wide"
-          maxW="full"
-        >
-          {pageData.title}
-        </Text>
-      </Card>
-    </Link>
-  )
-}
+        {pageData.title}
+      </Text>
+    </Card>
+  </Link>
+)
 
 export default PageBrainCard
