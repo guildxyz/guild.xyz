0a4485031efebfff89e0c13a1c2cefb84e280050
BrickheadJohnny <johnny.zgen@gmail.com>
2023-01-19 18:41:05 +0100

    Sound `SOUND_TOP_COLLECTOR` requirement updates

---
+++ src/requirements/Sound/components/SoundTopCollector.tsx
@@ -0,0 +1,76 @@
+import {
+  FormControl,
+  FormLabel,
+  NumberDecrementStepper,
+  NumberIncrementStepper,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+} from "@chakra-ui/react"
+import FormErrorMessage from "components/common/FormErrorMessage"
+import { Controller, useFormContext } from "react-hook-form"
+import { RequirementFormProps } from "requirements"
+import parseFromObject from "utils/parseFromObject"
+import SoundArtistSelect from "./SoundArtistSelect"
+
+const SoundTopCollector = ({ baseFieldPath }: RequirementFormProps): JSX.Element => {
+  const {
+    control,
+    formState: { errors },
+  } = useFormContext()
+
+  return (
+    <>
+      <SoundArtistSelect baseFieldPath={baseFieldPath} />
+
+      <FormControl
+        isRequired
+        isInvalid={!!parseFromObject(errors, baseFieldPath)?.data?.topAmount}
+      >
+        <FormLabel>Top:</FormLabel>
+        <Controller
+          name={`${baseFieldPath}.data.topAmount` as const}
+          control={control}
+          rules={{
+            required: "This field is required.",
+            min: {
+              value: 1,
+              message: "Amount must be positive",
+            },
+            max: {
+              value: 51,
+              message: "Amount must be less than or equal to 51",
+            },
+          }}
+          defaultValue={10}
+          render={({ field: { onChange, onBlur, value, ref } }) => (
+            <NumberInput
+              ref={ref}
+              value={value ?? ""}
+              onChange={(newValue) => {
+                const parsedValue = parseInt(newValue)
+                onChange(isNaN(parsedValue) ? "" : parsedValue)
+              }}
+              onBlur={onBlur}
+              min={1}
+              max={51}
+              defaultValue={10}
+            >
+              <NumberInputField />
+              <NumberInputStepper>
+                <NumberIncrementStepper />
+                <NumberDecrementStepper />
+              </NumberInputStepper>
+            </NumberInput>
+          )}
+        />
+
+        <FormErrorMessage>
+          {parseFromObject(errors, baseFieldPath)?.data?.topAmount?.message}
+        </FormErrorMessage>
+      </FormControl>
+    </>
+  )
+}
+
+export default SoundTopCollector
