d494d17c4227045ad6d335d19cc81ea414542561
BrickheadJohnny <92519134+BrickheadJohnny@users.noreply.github.com>
2023-01-19 21:54:45 +0100

    Sound integration update (#661)

--- src/requirements/Sound/SoundRequirement.tsx
+++ src/requirements/Sound/SoundRequirement.tsx
@@ -86,7 +86,7 @@ const SoundRequirement = ({ requirement, ...rest }: RequirementComponentProps) =
           case "SOUND_TOP_COLLECTOR":
             return (
               <>
-                {`Be in the top 10 collectors of `}
+                {`Be in the top ${requirement.data.topAmount} collectors of `}
                 <ArtistLink {...{ artistData, requirement }} />
                 {` on Sound.xyz`}
               </>
