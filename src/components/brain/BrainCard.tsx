6b3fb4c7982f9efb2d0f2126a0ca9f24426ecfab
balintligeti <balint.ligeti@hotmail.com>
2023-01-30 15:34:07 +0100

    test(brain-card): fallback if 502 error occurs

--- src/components/brain/BrainCard.tsx
+++ src/components/brain/BrainCard.tsx
@@ -4,7 +4,6 @@ import ColorCardLabel from "components/common/ColorCard/ColorCardLabel"
 import Link from "components/common/Link"
 import Image from "next/image"
 import { BrainCardData } from "types"
-
 type Props = {
   pageData: BrainCardData
 }
@@ -15,107 +14,194 @@ const BrainCard = ({ pageData }: Props): JSX.Element => {
     .sort((a, b) => a.length - b.length)
 
   return (
-    <Link
-      href={`/brain/${pageData.id}`}
-      prefetch={false}
-      _hover={{ textDecor: "none" }}
-      borderRadius="2xl"
-    >
-      <ColorCard
-        color="gray.600"
-        w="full"
-        borderWidth={4}
-        p={0}
-        sx={{
-          "div:nth-child(2) div.background > span img": {
-            filter: "blur(20px)",
-          },
-          ":hover div:nth-child(2) div.background > span img": {
-            filter: "blur(2px)",
-          },
-        }}
+    (
+      <Link
+        href={`/brain/${pageData.id}`}
+        prefetch={false}
+        _hover={{ textDecor: "none" }}
+        borderRadius="2xl"
       >
-        <ColorCardLabel
-          mt="-1px"
-          ml="-1px"
-          labelSize="md"
-          label={pageData.title}
-          color="white"
-          borderBottomRightRadius="xl"
-          borderTopLeftRadius="xl"
-          backgroundColor="gray.600"
-          zIndex={1}
-          maxW="90%"
-        ></ColorCardLabel>
-        <Flex h="160px" flexDirection="column">
-          <Center
-            position="relative"
-            py={{ base: 12, md: 20 }}
-            overflow={"hidden"}
-            borderRadius="xl"
-            minHeight="160px"
-            className="background"
-          >
-            {pageData.backgroundImage && (
-              <Image
-                src={pageData?.backgroundImage}
-                alt="background"
-                layout="fill"
-                objectFit="cover"
-                quality="2"
-                style={{ transition: "filter 0.3s" }}
-              />
-            )}
-
-            {pageData.icon && (
-              <Center boxSize={16} position="relative">
-                <Image
-                  src={pageData?.icon}
-                  alt="icon"
-                  layout="fill"
-                  objectFit="contain"
-                  quality="25"
-                  style={{
-                    zIndex: "1",
-                    overflow: "visible",
-                  }}
-                />
-              </Center>
-            )}
-          </Center>
-          {renderedTags.length > 0 && (
-            <Box
-              right="0"
-              bottom="0"
-              h="29px"
-              position="absolute"
-              backgroundColor="blackAlpha.600"
-              borderTopLeftRadius="xl"
-              borderBottomRightRadius="xl"
-              zIndex="1"
-              py="3px"
-              px="18px"
+        <ColorCard
+          color="gray.600"
+          w="full"
+          borderWidth={4}
+          p={0}
+          sx={{
+            "div:nth-child(2) div.background > span img": {
+              filter: "blur(20px)",
+            },
+            ":hover div:nth-child(2) div.background > span img": {
+              filter: "blur(2px)",
+            },
+          }}
+        >
+          <ColorCardLabel
+            mt="-1px"
+            ml="-1px"
+            labelSize="md"
+            label={pageData.title}
+            color="white"
+            borderBottomRightRadius="xl"
+            borderTopLeftRadius="xl"
+            backgroundColor="gray.600"
+            zIndex={1}
+            maxW="90%"
+          ></ColorCardLabel>
+          <Flex h="160px" flexDirection="column">
+            <Center
+              position="relative"
+              py={{ base: 12, md: 20 }}
+              overflow={"hidden"}
+              borderRadius="xl"
+              minHeight="160px"
+              className="background"
             >
-              <HStack
-                divider={
-                  <Divider orientation="vertical" height="1em" borderWidth="1px" />
-                }
-                spacing="3"
+              {pageData.backgroundImage && (
+                <Image
+                  src={pageData?.backgroundImage}
+                  alt="background"
+                  layout="fill"
+                  objectFit="cover"
+                  quality="2"
+                  style={{ transition: "filter 0.3s" }}
+                />
+              )}
+
+              {pageData.icon && (
+                <Center boxSize={16} position="relative">
+                  <Image
+                    src={pageData?.icon}
+                    alt="icon"
+                    layout="fill"
+                    objectFit="contain"
+                    quality="25"
+                    style={{
+                      zIndex: "1",
+                      overflow: "visible",
+                    }}
+                  />
+                </Center>
+              )}
+            </Center>
+            {renderedTags.length > 0 && (
+              <Box
+                right="0"
+                bottom="0"
+                h="29px"
+                position="absolute"
+                backgroundColor="blackAlpha.600"
+                borderTopLeftRadius="xl"
+                borderBottomRightRadius="xl"
+                zIndex="1"
+                py="3px"
+                px="18px"
               >
-                {renderedTags.map((tag, index) => (
-                  <Box display="inline-flex" alignItems="center" key={index}>
-                    <Img src={`/${tag}.svg`} h="14px" mr="6px" alt="page logo" />
-                    <Text fontWeight="bold" fontSize="14px">
-                      {tag}
-                    </Text>
-                  </Box>
-                ))}
-              </HStack>
-            </Box>
-          )}
-        </Flex>
-      </ColorCard>
-    </Link>
+                <HStack
+                  divider={
+                    <Divider orientation="vertical" height="1em" borderWidth="1px" />
+                  }
+                  spacing="3"
+                >
+                  {renderedTags.map((tag, index) => (
+                    <Box display="inline-flex" alignItems="center" key={index}>
+                      <Img src={`/${tag}.svg`} h="14px" mr="6px" alt="page logo" />
+                      <Text fontWeight="bold" fontSize="14px">
+                        {tag}
+                      </Text>
+                    </Box>
+                  ))}
+                </HStack>
+              </Box>
+            )}
+          </Flex>
+        </ColorCard>
+      </Link>
+    ) || (
+      <Link
+        href={`/brain/${pageData.id}`}
+        prefetch={false}
+        _hover={{ textDecor: "none" }}
+        borderRadius="2xl"
+      >
+        <ColorCard color="gray.600" w="full" borderWidth={4} p={0}>
+          <ColorCardLabel
+            mt="-1px"
+            ml="-1px"
+            labelSize="md"
+            label={pageData.title}
+            color="white"
+            borderBottomRightRadius="xl"
+            borderTopLeftRadius="xl"
+            backgroundColor="gray.600"
+            zIndex={1}
+          ></ColorCardLabel>
+          <Flex h="165px" flexDirection="column">
+            <Center
+              position="relative"
+              py={{ base: 12, md: 20 }}
+              overflow={"hidden"}
+              borderRadius="xl"
+            >
+              {pageData.backgroundImage && (
+                <Img
+                  position="absolute"
+                  w="full"
+                  h="full"
+                  src={pageData.backgroundImage}
+                  alt={`${pageData.title} image`}
+                  opacity="90%"
+                  filter={`blur(8px)`}
+                  objectFit="cover"
+                  transition="filter 0.3s"
+                  _groupHover={{ filter: "blur(2px)" }}
+                />
+              )}
+
+              {pageData.icon && (
+                <Img
+                  zIndex="1"
+                  src={pageData.icon}
+                  alt="icon"
+                  maxH="75px"
+                  minW="40px"
+                  maxW="75px"
+                />
+              )}
+            </Center>
+            {renderedTags.length > 0 && (
+              <Box
+                right="0"
+                bottom="0"
+                h="29px"
+                position="absolute"
+                backgroundColor="blackAlpha.600"
+                borderTopLeftRadius="xl"
+                borderBottomRightRadius="xl"
+                zIndex="1"
+                py="3px"
+                px="18px"
+              >
+                <HStack
+                  divider={
+                    <Divider orientation="vertical" height="1em" borderWidth="1px" />
+                  }
+                  spacing="3"
+                >
+                  {renderedTags.map((tag, index) => (
+                    <Box display="inline-flex" alignItems="center" key={index}>
+                      <Img src={`/${tag}.svg`} h="14px" mr="6px" alt="page logo" />
+                      <Text fontWeight="bold" fontSize="14px">
+                        {tag}
+                      </Text>
+                    </Box>
+                  ))}
+                </HStack>
+              </Box>
+            )}
+          </Flex>
+        </ColorCard>
+      </Link>
+    )
   )
 }
 
