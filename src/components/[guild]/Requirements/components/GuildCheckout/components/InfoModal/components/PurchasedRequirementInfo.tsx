604b4859229934b08b3f266b7863f6c74649e98c
BrickheadJohnny <johnny.zgen@gmail.com>
2023-02-02 13:17:53 +0100

    PurchasedRequirementInfo - display image / type

--- src/components/[guild]/Requirements/components/GuildCheckout/components/InfoModal/components/PurchasedRequirementInfo.tsx
+++ src/components/[guild]/Requirements/components/GuildCheckout/components/InfoModal/components/PurchasedRequirementInfo.tsx
@@ -1,4 +1,11 @@
-import { Circle, HStack, Skeleton, Text, useColorModeValue } from "@chakra-ui/react"
+import {
+  Circle,
+  HStack,
+  Img,
+  Skeleton,
+  Text,
+  useColorModeValue,
+} from "@chakra-ui/react"
 import useTokenData from "hooks/useTokenData"
 import { useGuildCheckoutContext } from "../../GuildCheckoutContex"
 
@@ -7,12 +14,20 @@ const PurchasedRequirementInfo = (): JSX.Element => {
 
   const { requirement } = useGuildCheckoutContext()
   const {
-    data: { symbol },
+    data: { symbol, logoURI },
   } = useTokenData(requirement?.chain, requirement?.address)
 
   return (
     <HStack>
-      <Circle size="var(--chakra-space-11)" bgColor={circleBgColor} />
+      <Circle size="var(--chakra-space-11)" bgColor={circleBgColor}>
+        {logoURI ? (
+          <Img src={logoURI} alt={symbol} boxSize={6} />
+        ) : (
+          <Text as="span" fontWeight="bold" fontSize="xx-small">
+            ERC20
+          </Text>
+        )}
+      </Circle>
 
       <Skeleton isLoaded={symbol}>
         <Text as="span" fontWeight="bold">
