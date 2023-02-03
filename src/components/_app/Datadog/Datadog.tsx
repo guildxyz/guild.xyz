bc1ba256774bb0c2315289b6167d5a1a21ef4077
BrickheadJohnny <johnny.zgen@gmail.com>
2023-02-03 11:45:04 +0100

    Datadog - remove comments

--- src/components/_app/Datadog/Datadog.tsx
+++ src/components/_app/Datadog/Datadog.tsx
@@ -6,8 +6,7 @@ const Datadog = ({ children }: PropsWithChildren<unknown>): JSX.Element => {
   const url = typeof window !== "undefined" ? window.location.host : ""
 
   useEffect(() => {
-    // TODO: uncomment before merge!
-    // if (process.env.NODE_ENV !== "production" || url !== "guild.xyz") return
+    if (process.env.NODE_ENV !== "production" || url !== "guild.xyz") return
     datadogRum.init({
       applicationId: "996b7a2a-d610-4235-a5b4-65391973ea76",
       clientToken: "pub7cf22f3b79a010363cf58c859cfa8ad8",
@@ -36,9 +35,6 @@ const Datadog = ({ children }: PropsWithChildren<unknown>): JSX.Element => {
               event.error.stack.includes("chrome-extension")))
         )
           return false
-
-        // TODO: remove before merge!
-        console.log(event)
 
         return false
       },
