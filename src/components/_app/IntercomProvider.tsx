df201816d385623b38e6623a8c3eca95a8df9c56
BrickheadJohnny <johnny.zgen@gmail.com>
2023-02-07 12:06:35 +0100

    triggerChat method

--- src/components/_app/IntercomProvider.tsx
+++ src/components/_app/IntercomProvider.tsx
@@ -3,14 +3,18 @@ import { createContext, PropsWithChildren, useContext } from "react"
 const IntercomContext = createContext<{
   intercomSettings: Record<string, string | number>
   defineIntercomSettingsValue: (key: string, value: string | number) => void
-}>({ intercomSettings: undefined, defineIntercomSettingsValue: () => {} })
+  triggerChat: () => void
+}>({
+  intercomSettings: undefined,
+  defineIntercomSettingsValue: () => {},
+  triggerChat: () => {},
+})
 
 const IntercomProvider = ({ children }: PropsWithChildren<unknown>): JSX.Element => {
-  const intercomSettings = {}
+  let intercomSettings = {}
 
   const defineIntercomSettingsValue = (key: string, value: string | number) => {
     if (typeof window === "undefined") return
-
     const windowAsObject = window as Record<string, any>
 
     if (!windowAsObject.intercomSettings) windowAsObject.intercomSettings = {}
@@ -20,12 +24,21 @@ const IntercomProvider = ({ children }: PropsWithChildren<unknown>): JSX.Element
       [key]: value,
     }
 
+    intercomSettings = { ...windowAsObject.intercomSettings }
+
     windowAsObject.Intercom?.("update", windowAsObject.intercomSettings)
+  }
+
+  const triggerChat = () => {
+    if (typeof window === "undefined") return
+    const windowAsObject = window as Record<string, any>
+
+    windowAsObject.Intercom?.("show")
   }
 
   return (
     <IntercomContext.Provider
-      value={{ intercomSettings, defineIntercomSettingsValue }}
+      value={{ intercomSettings, defineIntercomSettingsValue, triggerChat }}
     >
       {children}
     </IntercomContext.Provider>
