6be3461a45e901e161e4905c4eea1066a98f73d4
BrickheadJohnny <johnny.zgen@gmail.com>
2023-02-08 20:44:23 +0100

    useEstimateGasFee refactor

--- src/hooks/useEstimateGasFee.ts
+++ src/hooks/useEstimateGasFee.ts
@@ -1,39 +1,8 @@
-import { BigNumber } from "@ethersproject/bignumber"
 import { Contract } from "@ethersproject/contracts"
 import { formatUnits } from "@ethersproject/units"
 import { Chain, Chains, RPC } from "connectors"
 import { fetchNativeCurrencyPriceInUSD } from "pages/api/fetchPrice"
 import useSWRImmutable from "swr/immutable"
-
-const estimateGasFee = async (
-  contract: Contract,
-  methodName: string,
-  ...params: any
-) => {
-  const [gasPrice, gas] = await Promise.all([
-    contract.provider.getGasPrice(),
-    contract.estimateGas[methodName](...params),
-  ])
-
-  return gasPrice.mul(gas)
-}
-
-const convertGasFeeToUSD = async (
-  _: string,
-  contract: Contract,
-  estimatedGasFee: BigNumber
-) => {
-  const { chainId } = await contract.provider.getNetwork()
-  const nativeCurrencyPriceInUSD = await fetchNativeCurrencyPriceInUSD(
-    Chains[chainId] as Chain
-  )
-
-  const estimatedGasFeeInFloat = parseFloat(
-    formatUnits(estimatedGasFee, RPC[Chains[chainId]].nativeCurrency.decimals)
-  )
-
-  return estimatedGasFeeInFloat * nativeCurrencyPriceInUSD
-}
 
 const useEstimateGasFee = (
   key: string,
@@ -41,15 +10,40 @@ const useEstimateGasFee = (
   methodName: string,
   params: any[]
 ) => {
+  /**
+   * Defining both estimateGasFee and convertGasFeeToUSD inside the hook, so we don't
+   * need to pass the contract, methodName, and params parameters to these functions
+   * inside useSWRImmutable
+   */
+  const estimateGasFee = async () => {
+    const [gasPrice, gas] = await Promise.all([
+      contract.provider.getGasPrice(),
+      contract.estimateGas[methodName](...params),
+    ])
+
+    return gasPrice.mul(gas)
+  }
+
   const shouldFetch = Boolean(contract && methodName && params?.length)
 
   const {
     data: estimatedGasFee,
     isValidating: isGasEstimationLoading,
     error: estimateGasError,
-  } = useSWRImmutable(shouldFetch ? ["estimateGas", key] : null, () =>
-    estimateGasFee(contract, methodName, ...params)
-  )
+  } = useSWRImmutable(shouldFetch ? ["estimateGas", key] : null, estimateGasFee)
+
+  const convertGasFeeToUSD = async () => {
+    const { chainId } = await contract.provider.getNetwork()
+    const nativeCurrencyPriceInUSD = await fetchNativeCurrencyPriceInUSD(
+      Chains[chainId] as Chain
+    )
+
+    const estimatedGasFeeInFloat = parseFloat(
+      formatUnits(estimatedGasFee, RPC[Chains[chainId]].nativeCurrency.decimals)
+    )
+
+    return estimatedGasFeeInFloat * nativeCurrencyPriceInUSD
+  }
 
   const shouldConvert = !!contract && !!estimatedGasFee
 
