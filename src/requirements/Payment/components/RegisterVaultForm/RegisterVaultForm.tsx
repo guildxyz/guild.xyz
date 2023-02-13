a11933c84add1b0fd6ec25241ef1f4678c6c03df
BrickheadJohnny <johnny.zgen@gmail.com>
2023-02-13 18:02:36 +0100

    RegisterVaultForm: display fee in USD

--- src/requirements/Payment/components/RegisterVaultForm/RegisterVaultForm.tsx
+++ src/requirements/Payment/components/RegisterVaultForm/RegisterVaultForm.tsx
@@ -4,6 +4,7 @@ import {
   FormLabel,
   Input,
   InputGroup,
+  InputRightAddon,
   NumberDecrementStepper,
   NumberIncrementStepper,
   NumberInput,
@@ -12,11 +13,25 @@ import {
   Stack,
 } from "@chakra-ui/react"
 import FormErrorMessage from "components/common/FormErrorMessage"
+import useFeeInUSD from "components/[guild]/CreatePoap/components/Requirements/components/MonetizePoap/hooks/useFeeInUSD"
 import { Chain } from "connectors"
 import { useController, useFormContext, useWatch } from "react-hook-form"
 import ChainPicker from "requirements/common/ChainPicker"
 import TokenPicker from "requirements/common/TokenPicker"
-import { paymentSupportedChains } from "utils/guildCheckout/constants"
+import {
+  ADDRESS_REGEX,
+  NULL_ADDRESS,
+  paymentSupportedChains,
+} from "utils/guildCheckout/constants"
+
+const coingeckoCoinIds: Partial<Record<Chain, string>> = {
+  // TODO: add these for the supported chains
+  // 1: "ethereum",
+  // 137: "matic-network",
+  // 100: "xdai",
+  // 56: "binancecoin",
+  GOERLI: "ethereum",
+}
 
 export type RegisterVaultFormType = {
   chain: Chain
@@ -28,8 +43,6 @@ export type RegisterVaultFormType = {
 type Props = {
   isDisabled?: boolean
 }
-
-const ADDRESS_REGEX = /^0x[A-F0-9]{40}$/i
 
 const RegisterVaultForm = ({ isDisabled }: Props): JSX.Element => {
   const {
@@ -64,6 +77,13 @@ const RegisterVaultForm = ({ isDisabled }: Props): JSX.Element => {
     return onChange(isNaN(parsedValue) ? "" : parsedValue)
   }
 
+  const token = useWatch({ name: "token" })
+
+  const { feeInUSD } = useFeeInUSD(
+    feeFieldValue,
+    token === NULL_ADDRESS ? coingeckoCoinIds[chain] : undefined
+  )
+
   return (
     <Stack spacing={4}>
       <ChainPicker
@@ -87,20 +107,19 @@ const RegisterVaultForm = ({ isDisabled }: Props): JSX.Element => {
             onChange={(newValue) => handleFeeChange(newValue, feeFieldOnChange)}
             onBlur={feeFieldOnBlur}
             min={0}
-            // TODO: fetch price in USD
-            // sx={
-            //   feeInUSD > 0 && {
-            //     "> input": {
-            //       borderRightRadius: 0,
-            //     },
-            //     "div div:first-of-type": {
-            //       borderTopRightRadius: 0,
-            //     },
-            //     "div div:last-of-type": {
-            //       borderBottomRightRadius: 0,
-            //     },
-            //   }
-            // }
+            sx={
+              feeInUSD > 0 && {
+                "> input": {
+                  borderRightRadius: 0,
+                },
+                "div div:first-of-type": {
+                  borderTopRightRadius: 0,
+                },
+                "div div:last-of-type": {
+                  borderBottomRightRadius: 0,
+                },
+              }
+            }
           >
             <NumberInputField />
             <NumberInputStepper>
@@ -109,11 +128,11 @@ const RegisterVaultForm = ({ isDisabled }: Props): JSX.Element => {
             </NumberInputStepper>
           </NumberInput>
 
-          {/* {feeInUSD > 0 && (
-                            <InputRightAddon fontSize="sm">
-                              {`$${feeInUSD.toFixed(2)}`}
-                            </InputRightAddon>
-                          )} */}
+          {feeInUSD > 0 && (
+            <InputRightAddon fontSize="sm">
+              {`$${feeInUSD.toFixed(2)}`}
+            </InputRightAddon>
+          )}
         </InputGroup>
 
         <FormHelperText>Creators keep 90% of their revenue</FormHelperText>
