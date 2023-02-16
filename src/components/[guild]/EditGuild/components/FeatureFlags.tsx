9d5bb86d6503f1b663bdda19b3d9ea9799b0bc36
BrickheadJohnny <johnny.zgen@gmail.com>
2023-02-16 11:36:14 +0100

    Add FeatureFlag component with dynamic import

---
+++ src/components/[guild]/EditGuild/components/FeatureFlags.tsx
@@ -0,0 +1,48 @@
+import { FormControl } from "@chakra-ui/react"
+import StyledSelect from "components/common/StyledSelect"
+import { useController } from "react-hook-form"
+import { SelectOption } from "types"
+import capitalize from "utils/capitalize"
+
+const FEATURE_FLAGS = ["PURCHASE_REQUIREMENTS", "PAYMENT_REQUIREMENT"] as const
+export type FeatureFlag = (typeof FEATURE_FLAGS)[number]
+
+const generateLabel = (flag: FeatureFlag) =>
+  flag
+    .toLowerCase()
+    .split("_")
+    .map((word) => capitalize(word))
+    .join(" ")
+
+const FeatureFlags = (): JSX.Element => {
+  const options: SelectOption<FeatureFlag>[] = FEATURE_FLAGS.map((flag) => ({
+    label: generateLabel(flag),
+    value: flag,
+  }))
+
+  const {
+    field: { ref, name, onChange, onBlur },
+  } = useController({ name: "featureFlags" })
+
+  return (
+    <FormControl>
+      <StyledSelect
+        ref={ref}
+        name={name}
+        isMulti
+        options={options}
+        onChange={(selectedOption: (SelectOption | string)[]) => {
+          onChange(
+            selectedOption.map((option) =>
+              typeof option === "string" ? option : option.value
+            )
+          )
+        }}
+        onBlur={onBlur}
+        chakraStyles={{ valueContainer: (base) => ({ ...base, py: 2 }) }}
+      />
+    </FormControl>
+  )
+}
+
+export default FeatureFlags
