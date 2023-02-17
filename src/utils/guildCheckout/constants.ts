74f8f6e6377d21a3d99a8e4491136291c513806b
BrickheadJohnny <johnny.zgen@gmail.com>
2023-02-17 23:06:51 +0100

    update `PAYMENT_ALLOWED_GUILDS`

--- src/utils/guildCheckout/constants.ts
+++ src/utils/guildCheckout/constants.ts
@@ -25,8 +25,11 @@ export const TOKEN_BUYER_CONTRACT: Partial<Record<Chain, string>> = {
 
 // 9839 - Arbitrum
 // 4486 - Johnny's guild
+// 1985 - Our Guild
+// 17068 - RAZ
+// 13846 - CHAOS
 export const PURCHASE_ALLOWED_GUILDS = [9839, 4486]
-export const PAYMENT_ALLOWED_GUILDS = [4486]
+export const PAYMENT_ALLOWED_GUILDS = [1985, 17068, 13846, 4486]
 
 export const ZEROX_API_URLS: Partial<Record<Chain, string>> = {
   // ETHEREUM: "https://api.0x.org",
