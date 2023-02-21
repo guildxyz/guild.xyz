ab617bf6d6f037271c1ba10eeb2e70f6267e442c
balintligeti <balint.ligeti@hotmail.com>
2023-02-21 12:33:51 +0100

    rollback

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,6 +7,7 @@ import { Web3ConnectionManager } from "components/_app/Web3ConnectionManager"
 import { connectors } from "connectors"
 import type { AppProps } from "next/app"
 import { useRouter } from "next/router"
+import Script from "next/script"
 import { IconContext } from "phosphor-react"
 import { Fragment } from "react"
 import { SWRConfig } from "swr"
@@ -23,7 +24,7 @@ const App = ({
 
   return (
     <>
-      {/* <Script src="/intercom.js" /> */}
+      <Script src="/intercom.js" />
       <Chakra cookies={pageProps.cookies}>
         <IconContext.Provider
           value={{
