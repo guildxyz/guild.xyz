657a385122440b4c08d762a57b18ec272c56f998
Csete Balázs <csetebalu@gmail.com>
2023-02-28 17:45:57 +0100

    only show tooltips for admin

--- src/components/[guild]/Visibility.tsx
+++ src/components/[guild]/Visibility.tsx
@@ -1,11 +1,16 @@
 import { Tag, TagLeftIcon, TagProps, Tooltip } from "@chakra-ui/react"
 import { Visibility as VisibilityType } from "types"
+import useGuildPermission from "./hooks/useGuildPermission"
 import { visibilityData } from "./SetVisibility"
 
 type Props = { entityVisibility: VisibilityType } & TagProps
 
 const Visibility = ({ entityVisibility, ...tagProps }: Props) => {
   const VisibilityIcon = visibilityData[entityVisibility].Icon
+
+  const { isAdmin } = useGuildPermission()
+
+  if (!isAdmin) return null
 
   return (
     <Tooltip
