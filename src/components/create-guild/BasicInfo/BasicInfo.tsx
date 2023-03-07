fe67ba101e6eb8f208bba3f6a19fb6f8f08572b3
valid <valid@zgen.hu>
2023-03-07 22:10:26 +0100

    feat(create-guild): show name & urlName & description inputs for every platform

--- src/components/create-guild/BasicInfo/BasicInfo.tsx
+++ src/components/create-guild/BasicInfo/BasicInfo.tsx
@@ -1,40 +1,72 @@
-import { Stack } from "@chakra-ui/react"
+import { Box, FormLabel, HStack, SimpleGrid, Stack } from "@chakra-ui/react"
 import Card from "components/common/Card"
 import Section from "components/common/Section"
-import useDatadog from "components/_app/Datadog/useDatadog"
+import UrlName from "components/[guild]/EditGuild/components/UrlName"
+import usePinata from "hooks/usePinata"
 import { useEffect } from "react"
 import { useFormContext, useWatch } from "react-hook-form"
+import slugify from "slugify"
 import { GuildFormType } from "types"
+import getRandomInt from "utils/getRandomInt"
 import CreateGuildButton from "../CreateGuildButton"
 import { useCreateGuildContext } from "../CreateGuildContext"
+import Description from "../Description"
+import IconSelector from "../IconSelector"
+import Name from "../Name"
 import Pagination from "../Pagination"
 import ContactInfo from "./components/ContactInfo"
-import PlatformlessGuildForm from "./components/PlatformlessGuildForm"
 import TwitterUrlInput from "./components/TwitterUrlInput"
 
 const BasicInfo = (): JSX.Element => {
-  const { addDatadogAction } = useDatadog()
-
-  const { platform, template } = useCreateGuildContext()
+  const { template } = useCreateGuildContext()
   const {
     control,
     getValues,
-    formState: { errors },
+    setValue,
+    formState: { errors, dirtyFields },
   } = useFormContext<GuildFormType>()
 
-  const guildName = useWatch({ control, name: "name" })
-  const contacts = useWatch({ control, name: "contacts" })
+  const name = useWatch({ control, name: "name" })
+
+  const iconUploader = usePinata({
+    onSuccess: ({ IpfsHash }) => {
+      setValue("imageUrl", `${process.env.NEXT_PUBLIC_IPFS_GATEWAY}${IpfsHash}`, {
+        shouldTouch: true,
+      })
+    },
+    onError: () => {
+      setValue("imageUrl", `/guildLogos/${getRandomInt(286)}.svg`, {
+        shouldTouch: true,
+      })
+    },
+  })
 
   useEffect(() => {
-    if (!contacts?.length) return
-    addDatadogAction("Added contact (basic info)")
-  }, [contacts])
+    if (name && !dirtyFields.urlName)
+      setValue("urlName", slugify(name), { shouldValidate: true })
+  }, [name, dirtyFields])
 
   return (
     <>
       <Card px={{ base: 5, sm: 6 }} py={8}>
         <Stack spacing={10}>
-          {platform === "DEFAULT" && <PlatformlessGuildForm />}
+          <Stack spacing={{ base: 5, md: 6 }}>
+            <SimpleGrid
+              w="full"
+              spacing="5"
+              templateColumns={{ base: "1fr", md: "1fr 1fr" }}
+            >
+              <Box flex="1">
+                <FormLabel>Logo and name</FormLabel>
+                <HStack alignItems="start">
+                  <IconSelector uploader={iconUploader} />
+                  <Name width={null} />
+                </HStack>
+              </Box>
+              <UrlName maxWidth="unset" />
+            </SimpleGrid>
+            <Description />
+          </Stack>
 
           <Section title="How could we contact you?" spacing="4">
             <ContactInfo showAddButton={false} />
@@ -51,7 +83,7 @@ const BasicInfo = (): JSX.Element => {
       <Pagination nextButtonHidden>
         <CreateGuildButton
           isDisabled={
-            !guildName ||
+            !name ||
             !!Object.values(errors).length ||
             (template === "GROWTH" && !getValues("socialLinks.TWITTER"))
           }
