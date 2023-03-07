2593d065c0c1dfec3dfafc5be058eb358f5b7912
BrickheadJohnny <johnny.zgen@gmail.com>
2023-03-07 13:05:13 +0100

    InfoModalFooter: add PostHog events

--- src/components/[guild]/Requirements/components/GuildCheckout/components/InfoModal/components/InfoModalFooter.tsx
+++ src/components/[guild]/Requirements/components/GuildCheckout/components/InfoModal/components/InfoModalFooter.tsx
@@ -2,10 +2,13 @@ import { ModalFooter } from "@chakra-ui/react"
 import Button from "components/common/Button"
 import useIsMember from "components/[guild]/hooks/useIsMember"
 import { useOpenJoinModal } from "components/[guild]/JoinModal/JoinModalProvider"
+import { usePostHog } from "posthog-js/react"
 import { useEffect } from "react"
 import { useGuildCheckoutContext } from "../../GuildCheckoutContex"
 
 const InfoModalFooter = (): JSX.Element => {
+  const posthog = usePostHog()
+
   const { onInfoModalClose, txSuccess } = useGuildCheckoutContext()
   const openJoinModal = useOpenJoinModal()
   const isMember = useIsMember()
@@ -17,13 +20,27 @@ const InfoModalFooter = (): JSX.Element => {
 
   const isJoinButton = txSuccess && !isMember
 
+  const onClick = () => {
+    if (isJoinButton) {
+      openJoinModal()
+    } else {
+      onInfoModalClose()
+    }
+
+    posthog.capture(
+      `Click: ${
+        isJoinButton ? "JoinGuildButton" : "CloseModalButton"
+      } (GuildCheckout)`
+    )
+  }
+
   return (
     <ModalFooter>
       <Button
         size="lg"
         colorScheme={isJoinButton ? "green" : "blue"}
         w="full"
-        onClick={isJoinButton ? openJoinModal : onInfoModalClose}
+        onClick={onClick}
         data-dd-action-name={
           isJoinButton
             ? "JoinGuildButton (GuildCheckout)"
