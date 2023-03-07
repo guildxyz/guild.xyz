82c222bc8425bff675adc7c5bf221ce059f67283
BrickheadJohnny <johnny.zgen@gmail.com>
2023-03-07 10:54:26 +0100

    PostHog - basic setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,16 +9,38 @@ import type { AppProps } from "next/app"
 import { useRouter } from "next/router"
 import Script from "next/script"
 import { IconContext } from "phosphor-react"
-import { Fragment } from "react"
+import posthog from "posthog-js"
+import { PostHogProvider } from "posthog-js/react"
+import { Fragment, useEffect } from "react"
 import { SWRConfig } from "swr"
 import "theme/custom-scrollbar.css"
 import fetcher from "utils/fetcher"
+
+if (typeof window !== "undefined") {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://app.posthog.com",
+    // Disable in development
+    loaded: (ph) => {
+      // if (process.env.NODE_ENV === 'development') ph.opt_out_capturing()
+    },
+  })
+}
 
 const App = ({
   Component,
   pageProps,
 }: AppProps<{ cookies: string }>): JSX.Element => {
   const router = useRouter()
+
+  // PostHog - Track page views
+  useEffect(() => {
+    const handleRouteChange = () => posthog.capture("$pageview")
+    router.events.on("routeChangeComplete", handleRouteChange)
+
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange)
+    }
+  }, [])
 
   const DatadogComponent = router.asPath.includes("linkpreview") ? Fragment : Datadog
 
@@ -38,11 +60,13 @@ const App = ({
             <Web3ReactProvider connectors={connectors}>
               <Web3ConnectionManager>
                 <DatadogComponent>
-                  <IntercomProvider>
-                    <ExplorerProvider>
-                      <Component {...pageProps} />
-                    </ExplorerProvider>
-                  </IntercomProvider>
+                  <PostHogProvider client={posthog}>
+                    <IntercomProvider>
+                      <ExplorerProvider>
+                        <Component {...pageProps} />
+                      </ExplorerProvider>
+                    </IntercomProvider>
+                  </PostHogProvider>
                 </DatadogComponent>
               </Web3ConnectionManager>
             </Web3ReactProvider>
