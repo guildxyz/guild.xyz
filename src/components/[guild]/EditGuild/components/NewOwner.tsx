23d1d9dd692fa5b652ac05a8872a7d1c793eaee5
balintligeti <balint.ligeti@hotmail.com>
2023-03-10 21:30:16 +0100

    feat(new-owner): confirmatory modal

--- src/components/[guild]/EditGuild/components/NewOwner.tsx
+++ src/components/[guild]/EditGuild/components/NewOwner.tsx
@@ -5,35 +5,84 @@ import {
   HStack,
   Icon,
   Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
   Stack,
   Text,
+  useDisclosure,
 } from "@chakra-ui/react"
+import ModalButton from "components/common/ModalButton"
 import { Warning } from "phosphor-react"
+import { useState } from "react"
 
-const NewOwner = () => (
-  <>
-    <FormControl w="full">
-      <HStack>
-        <FormLabel mb="0">Hand over owner role</FormLabel>
-      </HStack>
-      <Text colorScheme="gray" mb={2}>
-        You will lose your ownership and become an admin{" "}
-      </Text>
-      <Stack direction={["column", "row"]} spacing={4} alignItems="center">
-        <Input type="url" placeholder="new owner" />
-        <Button
-          // isDisabled="true"
-          px="64px"
-          as="label"
-          variant="outline"
-          leftIcon={<Icon as={Warning} color="red" />}
-          fontWeight="medium"
-        >
-          Hand over ownership
-        </Button>
-      </Stack>
-    </FormControl>
-  </>
-)
+const NewOwner = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const handleClose = () => {
+    onClose()
+  }
+  const [newOwner, setNewOwner] = useState("")
+  return (
+    <>
+      <FormControl w="full">
+        <HStack>
+          <FormLabel mb="0">Hand over owner role</FormLabel>
+        </HStack>
+        <Text colorScheme="gray" mb={2}>
+          You will lose your ownership and become an admin{" "}
+        </Text>
+        <Stack direction={["column", "row"]} spacing={4} alignItems="center">
+          <Input
+            type="url"
+            placeholder="new owner"
+            onChange={(e) => setNewOwner(e.target.value)}
+          />
+          <Button
+            isDisabled={newOwner.length !== 42}
+            px="64px"
+            as="label"
+            variant="outline"
+            leftIcon={<Icon as={Warning} color="red" />}
+            fontWeight="medium"
+            onClick={onOpen}
+            data-dd-action-name="hand over ownership"
+          >
+            Hand over ownership
+          </Button>
+        </Stack>
+      </FormControl>
+
+      <Modal
+        isOpen={isOpen}
+        onClose={handleClose}
+        colorScheme="dark"
+        scrollBehavior="inside"
+      >
+        <ModalOverlay />
+        <ModalContent>
+          {" "}
+          <ModalHeader pb="7">Hand over ownership</ModalHeader>
+          <ModalCloseButton />{" "}
+          <ModalBody>
+            <Text mb="6" fontSize={"lg"}>
+              Are you sure, that you hand over your ownership? Your role will switch
+              to owner.
+            </Text>
+            <ModalButton
+              mt="8"
+              // onClick={handleSubmit(onSubmit)}
+              colorScheme="green"
+            >
+              Hand over ownership
+            </ModalButton>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
+  )
+}
 
 export default NewOwner
