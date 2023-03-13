d4d26b27c37aa66476633e2b27f45a574aa3fc25
BrickheadJohnny <92519134+BrickheadJohnny@users.noreply.github.com>
2023-03-13 15:12:14 +0100

    feat: Fetch Snapshot assets from the Guild API (#710)
    * Fetch Snapshot assets from the Guild API
    
    * Use the new asset endpoints
    
    * fix(ProposalSelect): add optional chaining
    
    * Update renamed Juicebox/Galxe endpoints

--- src/requirements/Snapshot/SnapshotRequirement.tsx
+++ src/requirements/Snapshot/SnapshotRequirement.tsx
@@ -14,17 +14,22 @@ import Requirement, {
 import { RequirementButton } from "components/[guild]/Requirements/components/RequirementButton"
 import { useRequirementContext } from "components/[guild]/Requirements/components/RequirementContext"
 import { CaretDown } from "phosphor-react"
+import useSWRImmutable from "swr/immutable"
 import pluralize from "utils/pluralize"
 import SnapshotSpaceLink from "./components/SnapshotSpaceLink"
 import StrategyParamsTable from "./components/StrategyParamsTable"
-import useProposal from "./hooks/useProposal"
+import { Proposal } from "./hooks/useProposals"
 
 const SnapshotRequirement = (props: RequirementProps): JSX.Element => {
   const requirement = useRequirementContext()
 
   const strategies = requirement.data.strategies
 
-  const { proposal } = useProposal(requirement.data.proposal)
+  const { data: proposal } = useSWRImmutable<Proposal>(
+    requirement.data.proposal
+      ? `/assets/snapshot/proposal/${requirement.data.proposal}`
+      : null
+  )
 
   const formattedDate = requirement.data.since
     ? new Date(requirement.data.since).toLocaleDateString()
