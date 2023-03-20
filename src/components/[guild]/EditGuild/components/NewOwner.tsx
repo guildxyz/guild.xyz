180a37b824fdbe0c4916f1611bd1887a03b7049c
balintligeti <balint.ligeti@hotmail.com>
2023-03-20 11:47:31 +0100

    feat(fetch): save new owner

--- src/components/[guild]/EditGuild/components/NewOwner.tsx
+++ src/components/[guild]/EditGuild/components/NewOwner.tsx
@@ -16,15 +16,18 @@ import {
   useDisclosure,
 } from "@chakra-ui/react"
 import ModalButton from "components/common/ModalButton"
+import useGuild from "components/[guild]/hooks/useGuild"
+import useShowErrorToast from "hooks/useShowErrorToast"
+import { SignedValdation, useSubmitWithSign } from "hooks/useSubmit/useSubmit"
+import useToast from "hooks/useToast"
 import { Warning } from "phosphor-react"
 import { useState } from "react"
+import fetcher from "utils/fetcher"
 
 const NewOwner = () => {
   const { isOpen, onOpen, onClose } = useDisclosure()
-  const handleClose = () => {
-    onClose()
-  }
   const [newOwner, setNewOwner] = useState("")
+
   return (
     <>
       <FormControl w="full">
@@ -55,34 +58,62 @@ const NewOwner = () => {
         </Stack>
       </FormControl>
 
-      <Modal
-        isOpen={isOpen}
-        onClose={handleClose}
-        colorScheme="dark"
-        scrollBehavior="inside"
-      >
-        <ModalOverlay />
-        <ModalContent>
-          {" "}
-          <ModalHeader pb="7">Hand over ownership</ModalHeader>
-          <ModalCloseButton />{" "}
-          <ModalBody>
-            <Text mb="6" fontSize={"lg"}>
-              Are you sure, that you hand over your ownership? Your role will switch
-              to owner.
-            </Text>
-            <ModalButton
-              mt="8"
-              // onClick={handleSubmit(onSubmit)}
-              colorScheme="green"
-            >
-              Hand over ownership
-            </ModalButton>
-          </ModalBody>
-        </ModalContent>
-      </Modal>
+      <NewOwnerModal isOpen={isOpen} onClose={onClose} newOwner={newOwner} />
     </>
   )
 }
 
+const NewOwnerModal = ({ isOpen, onClose, newOwner }) => {
+  const { id } = useGuild()
+
+  const changeOwner = async (signedValidation: SignedValdation) => {
+    fetcher(`/guild/${id}/ownership`, {
+      method: "PUT",
+      ...signedValidation,
+    })
+  }
+  const toast = useToast()
+  const showErrorToast = useShowErrorToast()
+
+  const { onSubmit } = useSubmitWithSign(changeOwner, {
+    onSuccess: () => {
+      toast({
+        title: `Owner changed!`,
+        status: "success",
+      })
+      onClose()
+    },
+    onError: (error) => showErrorToast(error),
+    forcePrompt: true,
+  })
+
+  return (
+    <Modal
+      isOpen={isOpen}
+      onClose={onClose}
+      colorScheme="dark"
+      scrollBehavior="inside"
+    >
+      <ModalOverlay />
+      <ModalContent>
+        {" "}
+        <ModalHeader pb="7">Hand over ownership</ModalHeader>
+        <ModalCloseButton />{" "}
+        <ModalBody>
+          <Text mb="6" fontSize={"lg"}>
+            Are you sure, that you hand over your ownership? Your role will switch to
+            owner.
+          </Text>
+          <ModalButton
+            mt="8"
+            onClick={() => onSubmit({ to: newOwner })}
+            colorScheme="green"
+          >
+            Hand over ownership
+          </ModalButton>
+        </ModalBody>
+      </ModalContent>
+    </Modal>
+  )
+}
 export default NewOwner
