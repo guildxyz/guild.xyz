4da2a56f24a1b0dee65f64523f986f99f140769a
BrickheadJohnny <johnny.zgen@gmail.com>
2023-03-22 18:17:38 +0100

    AlchemyTxValue(Relative) component

--- src/requirements/WalletActivity/components/AlchemyTxValue.tsx
+++ src/requirements/WalletActivity/components/AlchemyTxValue.tsx
@@ -1,3 +1,28 @@
-const AlchemyTxValue = (): JSX.Element => null
+import { useWatch } from "react-hook-form"
+import { RequirementFormProps } from "requirements"
+import TokenPicker from "requirements/common/TokenPicker"
+import MinMaxBlockNumberFormFields from "./MinMaxBlockNumberFormFields"
+import TxCountFormField from "./TxCountFormField"
+
+const AlchemyTxValue = ({ baseFieldPath }: RequirementFormProps): JSX.Element => {
+  const chain = useWatch({ name: `${baseFieldPath}.chain` })
+
+  return (
+    <>
+      <TokenPicker
+        chain={chain}
+        fieldName={`${baseFieldPath}.address`}
+        rules={{ required: "This field is required" }}
+      />
+
+      <TxCountFormField
+        baseFieldPath={baseFieldPath}
+        formLabel="Number of transactions"
+      />
+
+      <MinMaxBlockNumberFormFields baseFieldPath={baseFieldPath} />
+    </>
+  )
+}
 
 export default AlchemyTxValue
