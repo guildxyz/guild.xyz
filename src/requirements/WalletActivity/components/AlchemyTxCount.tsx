e02693f2b8b5d0e80cb5ecf997b004678e0a106a
BrickheadJohnny <johnny.zgen@gmail.com>
2023-03-22 18:06:51 +0100

    AlchemyTxCount(Relative) component

--- src/requirements/WalletActivity/components/AlchemyTxCount.tsx
+++ src/requirements/WalletActivity/components/AlchemyTxCount.tsx
@@ -1,3 +1,16 @@
-const AlchemyTxCount = (): JSX.Element => null
+import { RequirementFormProps } from "requirements"
+import MinMaxBlockNumberFormFields from "./MinMaxBlockNumberFormFields"
+import TxCountFormField from "./TxCountFormField"
+
+const AlchemyTxCount = ({ baseFieldPath }: RequirementFormProps): JSX.Element => (
+  <>
+    <TxCountFormField
+      baseFieldPath={baseFieldPath}
+      formLabel="Number of transactions"
+    />
+
+    <MinMaxBlockNumberFormFields baseFieldPath={baseFieldPath} />
+  </>
+)
 
 export default AlchemyTxCount
--- src/requirements/WalletActivity/components/AlchemyTxCountRelative.tsx
+++ src/requirements/WalletActivity/components/AlchemyTxCountRelative.tsx
@@ -1,3 +1,18 @@
-const AlchemyTxCountRelative = (): JSX.Element => null
+import { RequirementFormProps } from "requirements"
+import MinMaxBlockNumberFormFields from "./MinMaxBlockNumberFormFields"
+import TxCountFormField from "./TxCountFormField"
+
+const AlchemyTxCountRelative = ({
+  baseFieldPath,
+}: RequirementFormProps): JSX.Element => (
+  <>
+    <TxCountFormField
+      baseFieldPath={baseFieldPath}
+      formLabel="Number of transactions"
+    />
+
+    <MinMaxBlockNumberFormFields baseFieldPath={baseFieldPath} />
+  </>
+)
 
 export default AlchemyTxCountRelative
