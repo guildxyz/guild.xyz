80ecd883e90f71a47d2e4fbce447d3a39fb6068b
balintligeti <balint.ligeti@hotmail.com>
2023-03-23 18:04:34 +0100

    fix(FilterSelect): only client side render

--- src/components/brain/FilterSelect.tsx
+++ src/components/brain/FilterSelect.tsx
@@ -1,4 +1,4 @@
-import { GroupBase, Select } from "chakra-react-select"
+import dynamic from "next/dynamic"
 import { FILTER_OPTIONS } from "pages/guildverse"
 
 export type FilterOption = {
@@ -10,25 +10,30 @@ type Props = {
   setFilterData
 }
 
-const FilterSelect = ({ setFilterData }: Props): JSX.Element => (
-  <Select<FilterOption, true, GroupBase<FilterOption>>
-    isMulti
-    instanceId="filter-select"
-    options={FILTER_OPTIONS}
-    placeholder="Filter"
-    chakraStyles={{
-      control: (provided) => ({
-        ...provided,
-        minH: "48px",
-        borderRadius: "0.75rem",
-      }),
-      menu: (provided) => ({
-        ...provided,
-        zIndex: 2,
-      }),
-    }}
-    onChange={(e) => setFilterData(e)}
-  />
-)
+const DynamicStyledSelect = dynamic(() => import("components/common/StyledSelect"))
+
+const FilterSelect = ({ setFilterData }: Props): JSX.Element => {
+  if (typeof window === "undefined") return null
+  return (
+    <DynamicStyledSelect
+      isMulti
+      instanceId="filter-select"
+      options={FILTER_OPTIONS}
+      placeholder="Filter"
+      chakraStyles={{
+        control: (provided) => ({
+          ...provided,
+          minH: "48px",
+          borderRadius: "0.75rem",
+        }),
+        menu: (provided) => ({
+          ...provided,
+          zIndex: 2,
+        }),
+      }}
+      onChange={(e) => setFilterData(e)}
+    />
+  )
+}
 
 export default FilterSelect
