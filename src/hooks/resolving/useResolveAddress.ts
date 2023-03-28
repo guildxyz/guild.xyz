d2287921ac583c9b93883d0bc944210804708ae7
balintligeti <balint.ligeti@hotmail.com>
2023-03-28 14:27:31 +0200

    feat(ens-resolve): refactor, early return

--- src/hooks/resolving/useResolveAddress.ts
+++ src/hooks/resolving/useResolveAddress.ts
@@ -6,11 +6,15 @@ import fetcher from "utils/fetcher"
 
 const fetchENSName = (provider, address) => provider.lookupAddress(address)
 
-const fetchNNSName = (provider, account) => {
+const fetchNNSName = async (provider, account) => {
   const NNS_REGISTRY = "0x3e1970dc478991b49c4327973ea8a4862ef5a4de"
+  const ENS_REGISTRY = "0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e"
   provider.network.ensAddress = NNS_REGISTRY
-  return provider.lookupAddress(account)
+  const NNSDomain = await provider.lookupAddress(account)
+  provider.network.ensAddress = ENS_REGISTRY
+  return NNSDomain
 }
+
 const fetchLensProtocolName = (account) =>
   fetcher("https://api.lens.dev/", {
     method: "POST",
@@ -27,34 +31,39 @@ const fetchLensProtocolName = (account) =>
 
 const fetchDotbitName = (dotbitResolver, account) =>
   dotbitResolver?.accountById(account)?.then((res) => res?.account)
-// .catch((err) => err)
 
-const fetchUnstoppableName = (resolver, account) =>
-  resolver
-    .reverse(account)
-    .then((res) => res)
-    .catch((err) => err) // why i need this?
+const fetchUnstoppableName = (resolver, account) => resolver.reverse(account)
 
 const useResolveAddress = (account) => {
   const { provider } = useWeb3React()
   const shouldFetch = Boolean(provider && account)
 
   const fetchDomains = async () => {
-    const NNS = await fetchNNSName(
+    const NNS = await fetchNNSName(provider, account) // "0xe5358cab95014e2306815743793f16c93a8a5c70"
+    if (NNS) return NNS
+
+    const ENS = await fetchENSName(
       provider,
-      "0xe5358cab95014e2306815743793f16c93a8a5c70"
-    ) // "0xe5358cab95014e2306815743793f16c93a8a5c70"
-    const ENS = await fetchENSName(provider, account) // "0xcd2E72aEBe2A203b84f46DEEC948E6465dB51c75"
+      "0x5df52E6B70F25919Ad29add390EFE2614f91b2C6"
+    ) // "0x5df52E6B70F25919Ad29add390EFE2614f91b2C6"
+    if (ENS) return ENS
+
     const lens = await fetchLensProtocolName(account) // "0xe055721b972d58f0bcf6370c357879fb3a37d2f3"
+    if (lens) return lens
+
     const unstoppableResolver = new Resolution()
+
     const unstoppableDomain = await fetchUnstoppableName(
       unstoppableResolver,
       account
     ) // "0x94ef5300cbc0aa600a821ccbc561b057e456ab23"
+    if (unstoppableDomain) return unstoppableDomain
+
     const dotbitResolver = createInstance()
     const dotbit = await fetchDotbitName(dotbitResolver, account) // "0x5728088435fb8788472a9ca601fbc0b9cbea8be3"
+    if (dotbit) return dotbit
 
-    return NNS || ENS || lens || unstoppableDomain || dotbit
+    return null
   }
 
   const { data } = useSWRImmutable(shouldFetch ? ["domain"] : null, fetchDomains)
