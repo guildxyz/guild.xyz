d11638105bea52357ba647eb2f7edbf2287cfbe2
valid <valid@zgen.hu>
2023-03-31 16:24:20 +0200

    feat(PaymentFormWrapper): disable closing modal until the transaction finishes

--- src/components/[guild]/CreatePoap/components/PoapRequirements/components/PaymentFormWrapper.tsx
+++ src/components/[guild]/CreatePoap/components/PoapRequirements/components/PaymentFormWrapper.tsx
@@ -2,19 +2,25 @@ import { forwardRef } from "react"
 import { FormProvider, useForm } from "react-hook-form"
 import PaymentForm from "requirements/Payment/PaymentForm"
 
-const PaymentFormWrapper = forwardRef(({ onAdd }: any, ref: any) => {
-  const methods = useForm({
-    mode: "all",
-    defaultValues: {
-      type: "PAYMENT",
-    },
-  })
+const PaymentFormWrapper = forwardRef(
+  ({ onAdd, setOnCloseAttemptToast }: any, ref: any) => {
+    const methods = useForm({
+      mode: "all",
+      defaultValues: {
+        type: "PAYMENT",
+      },
+    })
 
-  return (
-    <FormProvider {...methods}>
-      <PaymentForm baseFieldPath="" addRequirement={methods.handleSubmit(onAdd)} />
-    </FormProvider>
-  )
-})
+    return (
+      <FormProvider {...methods}>
+        <PaymentForm
+          baseFieldPath=""
+          addRequirement={methods.handleSubmit(onAdd)}
+          setOnCloseAttemptToast={setOnCloseAttemptToast}
+        />
+      </FormProvider>
+    )
+  }
+)
 
 export default PaymentFormWrapper
