297383897a51f7762f68e8535d425a727da7eff5
BrickheadJohnny <johnny.zgen@gmail.com>
2023-04-11 17:25:32 +0200

    ui/ux improvements

--- src/components/[guild]/ResendRewardButton.tsx
+++ src/components/[guild]/ResendRewardButton.tsx
@@ -1,12 +1,24 @@
 import { IconButton, Tooltip } from "@chakra-ui/react"
 import useLocalStorage from "hooks/useLocalStorage"
-import { ArrowsClockwise } from "phosphor-react"
+import useShowErrorToast from "hooks/useShowErrorToast"
+import { SignedValdation, useSubmitWithSign } from "hooks/useSubmit"
+import useToast from "hooks/useToast"
+import { ArrowsClockwise, Check } from "phosphor-react"
 import { useEffect, useState } from "react"
-import useJoin from "./JoinModal/hooks/useJoin"
+import fetcher from "utils/fetcher"
+import useGuild from "./hooks/useGuild"
 
 const TIMEOUT = 60_000
 
+const rejoin = (signedValidation: SignedValdation) =>
+  fetcher(`/user/join`, signedValidation)
+
 const ResendRewardButton = (): JSX.Element => {
+  const toast = useToast()
+  const showErrorToast = useShowErrorToast()
+
+  const { id } = useGuild()
+
   const [latestResendDate, setLatestResendDate] = useLocalStorage(
     "latestResendDate",
     -Infinity
@@ -14,7 +26,16 @@ const ResendRewardButton = (): JSX.Element => {
   const [dateNow, setDateNow] = useState(Date.now())
   const canResend = dateNow - latestResendDate > TIMEOUT
 
-  const { onSubmit, isLoading } = useJoin(() => setLatestResendDate(Date.now()))
+  const { onSubmit, isLoading, response } = useSubmitWithSign(rejoin, {
+    onSuccess: () => {
+      toast({
+        status: "success",
+        title: "Successfully sent rewards",
+      })
+      setLatestResendDate(Date.now())
+    },
+    onError: () => showErrorToast("Couldn't re-send rewards"),
+  })
 
   useEffect(() => {
     const interval = setInterval(() => setDateNow(Date.now()), TIMEOUT)
@@ -24,7 +45,9 @@ const ResendRewardButton = (): JSX.Element => {
   return (
     <Tooltip
       label={
-        isLoading
+        response
+          ? "Successfully sent rewards"
+          : isLoading
           ? "Sending rewards..."
           : canResend
           ? "Re-send rewards"
@@ -51,13 +74,15 @@ const ResendRewardButton = (): JSX.Element => {
     >
       <IconButton
         aria-label="Re-send rewards"
-        icon={<ArrowsClockwise />}
+        icon={response ? <Check /> : <ArrowsClockwise />}
         minW="44px"
         variant="ghost"
         rounded="full"
-        onClick={canResend ? onSubmit : undefined}
+        onClick={
+          !response && canResend ? () => onSubmit({ guildId: id }) : undefined
+        }
         animation={isLoading ? "rotate 1s infinite linear" : undefined}
-        isDisabled={isLoading || !canResend}
+        isDisabled={isLoading || response || !canResend}
       />
     </Tooltip>
   )
