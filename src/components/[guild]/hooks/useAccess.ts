e2549652998ac6c763df7763499d5cf93d850046
Csete Balázs <csetebalu@gmail.com>
2023-04-17 18:44:51 +0200

    unconnectedPlatforms and platformsToReconnect

--- src/components/[guild]/hooks/useAccess.ts
+++ src/components/[guild]/hooks/useAccess.ts
@@ -1,6 +1,7 @@
 import { useWeb3React } from "@web3-react/core"
 import useGuild from "components/[guild]/hooks/useGuild"
 import useSWRWithOptionalAuth from "hooks/useSWRWithOptionalAuth"
+import platforms from "platforms/platforms"
 import { usePostHog } from "posthog-js/react"
 import { SWRConfiguration } from "swr"
 
@@ -16,32 +17,31 @@ const useAccess = (roleId?: number, swrOptions?: SWRConfiguration) => {
     {
       shouldRetryOnError: false,
       onSuccess: (accessCheckResult: any) => {
-        const twitterNotConneted = accessCheckResult.some(({ errors }) =>
-          errors?.some(
-            ({ subType, errorType }) =>
-              subType.toUpperCase() === "TWITTER" &&
-              errorType === "PLATFORM_NOT_CONNECTED"
+        const unconnectedPlatforms = Object.keys(platforms).filter((platform) =>
+          accessCheckResult.some(({ errors }) =>
+            errors?.some(
+              ({ subType, errorType }) =>
+                subType.toUpperCase() === platform &&
+                errorType === "PLATFORM_NOT_CONNECTED"
+            )
           )
         )
 
-        const twitterNeedsToBeReconnected = accessCheckResult.some(({ errors }) =>
-          errors?.some(
-            ({ subType, errorType }) =>
-              subType.toUpperCase() === "TWITTER" &&
-              errorType === "PLATFORM_CONNECT_INVALID"
+        const platformsToReconnect = Object.keys(platforms).filter((platform) =>
+          accessCheckResult.some(({ errors }) =>
+            errors?.some(
+              ({ subType, errorType }) =>
+                subType.toUpperCase() === platform &&
+                errorType === "PLATFORM_CONNECT_INVALID"
+            )
           )
         )
 
-        const eventProps: any = { accessCheckResult }
-        if (twitterNotConneted) {
-          eventProps.twitterNotConneted = true
-        }
-
-        if (twitterNeedsToBeReconnected) {
-          eventProps.twitterNeedsToBeReconnected = true
-        }
-
-        posthog.capture("Access check", eventProps)
+        posthog.capture("Access check", {
+          accessCheckResult,
+          unconnectedPlatforms,
+          platformsToReconnect,
+        })
       },
       ...swrOptions,
     }
