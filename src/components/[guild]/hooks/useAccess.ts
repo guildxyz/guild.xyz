ac1a8553a2a60a81ef1a89ea3b14074e44d7ffd7
cs-balazs <44908417+cs-balazs@users.noreply.github.com>
2023-04-17 18:54:34 +0200

    Posthog events for Twitter connection (#773)
    * add posthog events for twitter connection
    
    * unconnectedPlatforms and platformsToReconnect

--- src/components/[guild]/hooks/useAccess.ts
+++ src/components/[guild]/hooks/useAccess.ts
@@ -1,6 +1,8 @@
 import { useWeb3React } from "@web3-react/core"
 import useGuild from "components/[guild]/hooks/useGuild"
 import useSWRWithOptionalAuth from "hooks/useSWRWithOptionalAuth"
+import platforms from "platforms/platforms"
+import { usePostHog } from "posthog-js/react"
 import { SWRConfiguration } from "swr"
 
 const useAccess = (roleId?: number, swrOptions?: SWRConfiguration) => {
@@ -8,10 +10,41 @@ const useAccess = (roleId?: number, swrOptions?: SWRConfiguration) => {
   const { id } = useGuild()
 
   const shouldFetch = account && id && roleId !== 0
+  const posthog = usePostHog()
 
   const { data, error, isValidating, mutate } = useSWRWithOptionalAuth(
     shouldFetch ? `/guild/access/${id}/${account}` : null,
-    { shouldRetryOnError: false, ...swrOptions }
+    {
+      shouldRetryOnError: false,
+      onSuccess: (accessCheckResult: any) => {
+        const unconnectedPlatforms = Object.keys(platforms).filter((platform) =>
+          accessCheckResult.some(({ errors }) =>
+            errors?.some(
+              ({ subType, errorType }) =>
+                subType.toUpperCase() === platform &&
+                errorType === "PLATFORM_NOT_CONNECTED"
+            )
+          )
+        )
+
+        const platformsToReconnect = Object.keys(platforms).filter((platform) =>
+          accessCheckResult.some(({ errors }) =>
+            errors?.some(
+              ({ subType, errorType }) =>
+                subType.toUpperCase() === platform &&
+                errorType === "PLATFORM_CONNECT_INVALID"
+            )
+          )
+        )
+
+        posthog.capture("Access check", {
+          accessCheckResult,
+          unconnectedPlatforms,
+          platformsToReconnect,
+        })
+      },
+      ...swrOptions,
+    }
   )
 
   const roleData = roleId && data?.find?.((role) => role.roleId === roleId)
