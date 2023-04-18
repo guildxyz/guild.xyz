109946e28adb45207a1359c06cd74ca958a3202b
BrickheadJohnny <johnny.zgen@gmail.com>
2023-04-18 07:35:27 +0200

    connectWallet command - possible fix

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -10,6 +10,8 @@ Cypress.Commands.add("connectWallet", () => {
   cy.findByText("Connect to a wallet").click()
   cy.findByText("MetaMask").click()
   cy.acceptMetamaskAccess()
+  // Sometimes the MetaMask popup doesn't close in time in CI, so waiting a bit here before verifying the account
+  cy.wait(2000)
   cy.findByText("Verify account").click()
   cy.confirmMetamaskSignatureRequest()
 })
