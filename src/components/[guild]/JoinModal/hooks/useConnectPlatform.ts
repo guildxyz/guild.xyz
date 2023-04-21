4ab042171f018d38827b8c79aaf742a1353c1ebd
BrickheadJohnny <johnny.zgen@gmail.com>
2023-04-21 22:55:33 +0200

    fix(useConnectPlatform): drop `params.code`

--- src/components/[guild]/JoinModal/hooks/useConnectPlatform.ts
+++ src/components/[guild]/JoinModal/hooks/useConnectPlatform.ts
@@ -37,7 +37,7 @@ const parseConnectError = (
     )
       return error
 
-    return { params, errors }
+    return { params: { ...params, code: undefined }, errors }
   } catch {
     return error
   }
