d5702ee1c56dc42f4d43a4a454598f8423770296
valid <valid@zgen.hu>
2023-05-04 14:35:15 +0200

    copy(BuyTotal): `with fees` to `with gas fee`

--- src/components/[guild]/Requirements/components/GuildCheckout/components/BuyTotal.tsx
+++ src/components/[guild]/Requirements/components/GuildCheckout/components/BuyTotal.tsx
@@ -31,7 +31,7 @@ const BuyTotal = (): JSX.Element => {
       buttonComponent={
         <HStack justifyContent={"space-between"} w="full">
           <Text as="span" colorScheme="gray">
-            Total with fees:
+            Total with gas fee:
           </Text>
 
           <PriceFallback {...{ error, pickedCurrency }}>
