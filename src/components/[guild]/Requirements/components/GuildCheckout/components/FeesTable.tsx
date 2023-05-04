e00d814e5b15762a38ada0340722c371dc410b65
BrickheadJohnny <johnny.zgen@gmail.com>
2023-05-04 08:56:09 +0200

    create generalized FeesTable component

---
+++ src/components/[guild]/Requirements/components/GuildCheckout/components/FeesTable.tsx
@@ -0,0 +1,58 @@
+import {
+  Collapse,
+  Icon,
+  Stack,
+  Table,
+  TableContainer,
+  Tbody,
+  useDisclosure,
+} from "@chakra-ui/react"
+import Button from "components/common/Button"
+import { CaretDown } from "phosphor-react"
+import { PropsWithChildren } from "react"
+
+type Props = {
+  buttonComponent: JSX.Element
+}
+
+const FeesTable = ({
+  buttonComponent,
+  children,
+}: PropsWithChildren<Props>): JSX.Element => {
+  const { isOpen, onToggle } = useDisclosure()
+
+  return (
+    <Stack spacing={0}>
+      <Button
+        display="flex"
+        justifyContent="start"
+        fontWeight="normal"
+        p={0}
+        h="auto"
+        maxW="max-content"
+        variant="unstyled"
+        onClick={onToggle}
+        rightIcon={
+          <Icon
+            as={CaretDown}
+            boxSize={3}
+            transition="transform 0.2s ease"
+            transform={`rotate(${isOpen ? "-180" : "0"}deg)`}
+          />
+        }
+      >
+        {buttonComponent}
+      </Button>
+
+      <Collapse in={isOpen} animateOpacity>
+        <TableContainer borderWidth={1} borderRadius="xl" mt={2}>
+          <Table variant="simple" size="sm" color="gray">
+            <Tbody>{children}</Tbody>
+          </Table>
+        </TableContainer>
+      </Collapse>
+    </Stack>
+  )
+}
+
+export default FeesTable
--- src/components/[guild]/Requirements/components/GuildCheckout/components/PurchaseFeeAndTotal.tsx
+++ src/components/[guild]/Requirements/components/GuildCheckout/components/PurchaseFeeAndTotal.tsx
@@ -1,32 +1,16 @@
-import {
-  Collapse,
-  HStack,
-  Icon,
-  Skeleton,
-  Stack,
-  Table,
-  TableContainer,
-  Tbody,
-  Td,
-  Text,
-  Tooltip,
-  Tr,
-  useDisclosure,
-} from "@chakra-ui/react"
+import { HStack, Icon, Skeleton, Td, Text, Tooltip, Tr } from "@chakra-ui/react"
 import { formatUnits } from "@ethersproject/units"
-import Button from "components/common/Button"
 import { RPC } from "connectors"
 import useTokenData from "hooks/useTokenData"
-import { CaretDown, Info, Question } from "phosphor-react"
+import { Info, Question } from "phosphor-react"
 import { GUILD_FEE_PERCENTAGE } from "utils/guildCheckout/constants"
 import usePrice from "../hooks/usePrice"
 import usePurchaseAsset from "../hooks/usePurchaseAsset"
+import FeesTable from "./FeesTable"
 import { useGuildCheckoutContext } from "./GuildCheckoutContex"
 import PriceFallback from "./PriceFallback"
 
 const PurchaseFeeAndTotal = (): JSX.Element => {
-  const { isOpen, onToggle } = useDisclosure()
-
   const { pickedCurrency, requirement } = useGuildCheckoutContext()
 
   const {
@@ -65,25 +49,8 @@ const PurchaseFeeAndTotal = (): JSX.Element => {
   )
 
   return (
-    <Stack spacing={0}>
-      <Button
-        display="flex"
-        justifyContent="start"
-        fontWeight="normal"
-        p={0}
-        h="auto"
-        maxW="max-content"
-        variant="unstyled"
-        onClick={onToggle}
-        rightIcon={
-          <Icon
-            as={CaretDown}
-            boxSize={3}
-            transition="transform 0.2s ease"
-            transform={`rotate(${isOpen ? "-180" : "0"}deg)`}
-          />
-        }
-      >
+    <FeesTable
+      buttonComponent={
         <HStack>
           <Text as="span" colorScheme="gray">
             Total:
@@ -114,90 +81,80 @@ const PurchaseFeeAndTotal = (): JSX.Element => {
             </Text>
           </PriceFallback>
         </HStack>
-      </Button>
+      }
+    >
+      <Tr>
+        <Td>
+          <HStack>
+            <Text as="span">Max price</Text>
 
-      <Collapse in={isOpen} animateOpacity>
-        <TableContainer borderWidth={1} borderRadius="xl" mt={2}>
-          <Table variant="simple" size="sm" color="gray">
-            <Tbody>
-              <Tr>
-                <Td>
-                  <HStack>
-                    <Text as="span">Max price</Text>
+            <Tooltip
+              placement="top"
+              hasArrow
+              label="You will pay no more than this, including slippage"
+            >
+              <Icon as={Question} boxSize={4} color="gray" />
+            </Tooltip>
+          </HStack>
+        </Td>
+        <Td color="WindowText">
+          <Skeleton isLoaded={!isValidating}>
+            <Text as="span">{`${maxPriceSum} ${symbol}`}</Text>
+          </Skeleton>
+        </Td>
+      </Tr>
 
-                    <Tooltip
-                      placement="top"
-                      hasArrow
-                      label="You will pay no more than this, including slippage"
-                    >
-                      <Icon as={Question} boxSize={4} color="gray" />
-                    </Tooltip>
-                  </HStack>
-                </Td>
-                <Td color="WindowText">
-                  <Skeleton isLoaded={!isValidating}>
-                    <Text as="span">{`${maxPriceSum} ${symbol}`}</Text>
-                  </Skeleton>
-                </Td>
-              </Tr>
+      <Tr>
+        <Td>Expected price</Td>
 
-              <Tr>
-                <Td>Expected price</Td>
+        <Td>
+          <Skeleton isLoaded={!isValidating}>
+            <Text as="span">{`${estimatedPriceSum} ${symbol}`}</Text>
+          </Skeleton>
+        </Td>
+      </Tr>
 
-                <Td>
-                  <Skeleton isLoaded={!isValidating}>
-                    <Text as="span">{`${estimatedPriceSum} ${symbol}`}</Text>
-                  </Skeleton>
-                </Td>
-              </Tr>
+      <Tr>
+        <Td>
+          <HStack>
+            <Text as="span">Guild fee</Text>
+            <Tooltip
+              label={`${GUILD_FEE_PERCENTAGE * 100}% of the token's price${
+                guildBaseFeeInSellToken
+                  ? ` + ${guildBaseFeeInSellToken} ${symbol}`
+                  : ""
+              }`}
+              placement="top"
+              hasArrow
+            >
+              <Icon as={Info} />
+            </Tooltip>
+          </HStack>
+        </Td>
+        <Td>
+          {guildFee < 0.001 ? "< 0.001 " : `${guildFee} `}
+          {symbol}
+        </Td>
+      </Tr>
 
-              <Tr>
-                <Td>
-                  <HStack>
-                    <Text as="span">Guild fee</Text>
-                    <Tooltip
-                      label={`${GUILD_FEE_PERCENTAGE * 100}% of the token's price${
-                        guildBaseFeeInSellToken
-                          ? ` + ${guildBaseFeeInSellToken} ${symbol}`
-                          : ""
-                      }`}
-                      placement="top"
-                      hasArrow
-                    >
-                      <Icon as={Info} />
-                    </Tooltip>
-                  </HStack>
-                </Td>
-                <Td>
-                  {guildFee < 0.001 ? "< 0.001 " : `${guildFee} `}
-                  {symbol}
-                </Td>
-              </Tr>
-
-              <Tr>
-                <Td>Gas fee</Td>
-                <Td>
-                  <Skeleton isLoaded={!isEstimateGasLoading}>
-                    <Text as="span">
-                      {error || estimateGasError || !estimatedGasFee
-                        ? "Couldn't calculate"
-                        : `${parseFloat(
-                            formatUnits(
-                              estimatedGasFee,
-                              RPC[requirement.chain].nativeCurrency.decimals
-                            )
-                          ).toFixed(8)} ${
-                            RPC[requirement.chain].nativeCurrency.symbol
-                          }`}
-                    </Text>
-                  </Skeleton>
-                </Td>
-              </Tr>
-            </Tbody>
-          </Table>
-        </TableContainer>
-      </Collapse>
-    </Stack>
+      <Tr>
+        <Td>Gas fee</Td>
+        <Td>
+          <Skeleton isLoaded={!isEstimateGasLoading}>
+            <Text as="span">
+              {error || estimateGasError || !estimatedGasFee
+                ? "Couldn't calculate"
+                : `${parseFloat(
+                    formatUnits(
+                      estimatedGasFee,
+                      RPC[requirement.chain].nativeCurrency.decimals
+                    )
+                  ).toFixed(8)} ${RPC[requirement.chain].nativeCurrency.symbol}`}
+            </Text>
+          </Skeleton>
+        </Td>
+      </Tr>
+    </FeesTable>
   )
 }
 
