54b8ff87c6a47556cff6a12fe5ec30b106cf1f8f
BrickheadJohnny <johnny.zgen@gmail.com>
2023-05-09 11:14:20 +0200

    Credential: remove link for now

--- src/components/common/Layout/components/Account/components/AccountModal/components/UsersGuildCredentials/Credential.tsx
+++ src/components/common/Layout/components/Account/components/AccountModal/components/UsersGuildCredentials/Credential.tsx
@@ -1,6 +1,4 @@
 import { Circle, Img, useColorModeValue } from "@chakra-ui/react"
-import Link from "components/common/Link"
-import { useWeb3ConnectionManager } from "components/_app/Web3ConnectionManager"
 
 type Props = {
   name: string
@@ -15,11 +13,12 @@ const Credential = ({ name, image, guildId }: Props) => {
     "0 0 0.25rem 0.15rem var(--chakra-colors-gray-800)"
   )
 
-  const { closeAccountModal } = useWeb3ConnectionManager()
-
   return (
-    <Link
-      href={`/${guildId}`}
+    <Circle
+      bgColor="#27272A"
+      size={24}
+      borderWidth={borderWidth}
+      boxShadow={boxShadow}
       ml={-12}
       _first={{ ml: 0 }}
       className="credential"
@@ -30,17 +29,9 @@ const Credential = ({ name, image, guildId }: Props) => {
           transform: "translateX(2rem)",
         },
       }}
-      onClick={closeAccountModal}
     >
-      <Circle
-        bgColor="#27272A"
-        size={24}
-        borderWidth={borderWidth}
-        boxShadow={boxShadow}
-      >
-        <Img src={image} alt={name} />
-      </Circle>
-    </Link>
+      <Img src={image} alt={name} />
+    </Circle>
   )
 }
 
