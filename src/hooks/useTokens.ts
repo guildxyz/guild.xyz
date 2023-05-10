4a6ca5730f570520ad1a7e51dae3123e6d796c9b
BrickheadJohnny <92519134+BrickheadJohnny@users.noreply.github.com>
2023-05-10 17:27:17 +0200

    Add new supported chains (#772)
    * Add Sepolia chain
    
    * Add Scroll alpha chain
    
    * Add zkSync Era chain
    
    * Add ZetaChain Athens chain
    
    * rename `SCROLL` to `SCROLL_ALPHA`
    
    * remove zkSync Era for now

--- src/hooks/useTokens.ts
+++ src/hooks/useTokens.ts
@@ -23,6 +23,9 @@ export const TokenApiURLs: Record<Chain, string[]> = {
     "https://raw.githubusercontent.com/DefiKingdoms/community-token-list/main/src/defikingdoms-default.tokenlist.json",
     "https://raw.githubusercontent.com/DefiKingdoms/community-token-list/main/build/defikingdoms-community.tokenlist.json",
   ],
+  ZETACHAIN_ATHENS: [],
+  SCROLL_ALPHA: [],
+  SEPOLIA: [],
   GOERLI: [
     "https://raw.githubusercontent.com/Uniswap/default-token-list/main/src/tokens/goerli.json",
   ],
