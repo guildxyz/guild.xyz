c267181032de1420d83c55557db3ec915013aea0
valid <valid@zgen.hu>
2023-05-11 16:07:49 +0200

    redirect base url to /explorer, remove auto navigation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,4 @@
 import { Box, useColorMode } from "@chakra-ui/react"
-import { useWeb3React } from "@web3-react/core"
-import { useWeb3ConnectionManager } from "components/_app/Web3ConnectionManager"
 import LinkPreviewHead from "components/common/LinkPreviewHead"
 import CallToAction from "components/index/CallToAction"
 import ComposableRequirements from "components/index/ComposableRequirements"
@@ -11,19 +9,18 @@ import GuildValues from "components/index/GuildValues"
 import Hero from "components/index/Hero"
 import PlatformAgnosticCommunities from "components/index/PlatformAgnosticCommunities"
 import Head from "next/head"
-import { useRouter } from "next/router"
 import { useEffect } from "react"
 
 const Page = (): JSX.Element => {
-  const { account } = useWeb3React()
-  const router: any = useRouter()
-  const hasNavigated = router.components && Object.keys(router.components).length > 2
-  const { triedEager } = useWeb3ConnectionManager()
+  // const { account } = useWeb3React()
+  // const router: any = useRouter()
+  // const hasNavigated = router.components && Object.keys(router.components).length > 2
+  // const { triedEager } = useWeb3ConnectionManager()
 
-  useEffect(() => {
-    if (router.isReady && !hasNavigated && triedEager && account)
-      router.push("/explorer")
-  }, [hasNavigated, account, triedEager, router])
+  // useEffect(() => {
+  //   if (router.isReady && !hasNavigated && triedEager && account)
+  //     router.push("/explorer")
+  // }, [hasNavigated, account, triedEager, router])
 
   // Setting up the dark mode, because this is a "static" page
   const { setColorMode } = useColorMode()
