33ffb25c382ab01a845323d9e1c271db7bd7a596
BrickheadJohnny <johnny.zgen@gmail.com>
2023-05-18 11:57:33 +0200

    fix(OptionCard): remove responsive value from image

--- src/components/common/OptionCard.tsx
+++ src/components/common/OptionCard.tsx
@@ -34,7 +34,7 @@ const OptionCard = ({
           src={image}
           alt={`${title} image`}
           borderRadius="full"
-          boxSize={{ base: 12 }}
+          boxSize={12}
           pos="relative"
         />
       ) : (
