58f0cd9ca3f1a6ede748967dd64da20111c08d51
valid <valid@zgen.hu>
2023-06-05 14:51:16 +0200

    remove hasClicked logic, only show if already connected

--- src/components/[guild]/Requirements/components/HiddenRequirement.tsx
+++ src/components/[guild]/Requirements/components/HiddenRequirement.tsx
@@ -3,33 +3,24 @@ import { useWeb3React } from "@web3-react/core"
 import useUser from "components/[guild]/hooks/useUser"
 import { useWeb3ConnectionManager } from "components/_app/Web3ConnectionManager"
 import Button from "components/common/Button"
-import useKeyPair from "hooks/useKeyPair"
 import { Info, Link, Question } from "phosphor-react"
 import platforms from "platforms/platforms"
-import { useEffect, useState } from "react"
 import Requirement from "./Requirement"
 
 const HiddenRequirement = () => {
   const { account } = useWeb3React()
   const { platformUsers } = useUser()
-  const { isValid } = useKeyPair()
-  const [hasClicked, setHasClicked] = useState<boolean>(false)
-  const { openAccountModal, openWalletSelectorModal } = useWeb3ConnectionManager()
+  const { openAccountModal } = useWeb3ConnectionManager()
 
-  const canConnectMorePlatforms = Object.keys(platforms).some(
-    (platform) =>
-      platform !== "POAP" &&
-      (platformUsers ?? []).every(
-        (platformUser) => platformUser.platformName !== platform
-      )
-  )
-
-  useEffect(() => {
-    if (hasClicked && isValid) {
-      openAccountModal()
-      setHasClicked(false)
-    }
-  }, [hasClicked, isValid])
+  const canConnectMorePlatforms =
+    account &&
+    Object.keys(platforms).some(
+      (platform) =>
+        platform !== "POAP" &&
+        (platformUsers ?? []).every(
+          (platformUser) => platformUser.platformName !== platform
+        )
+    )
 
   return (
     <Requirement
@@ -38,14 +29,7 @@ const HiddenRequirement = () => {
         canConnectMorePlatforms ? (
           <Button
             size="xs"
-            onClick={
-              account
-                ? openAccountModal
-                : () => {
-                    setHasClicked(true)
-                    openWalletSelectorModal()
-                  }
-            }
+            onClick={openAccountModal}
             colorScheme="primary"
             leftIcon={<Icon as={Link} />}
             iconSpacing="1"
@@ -55,9 +39,11 @@ const HiddenRequirement = () => {
         ) : null
       }
       rightElement={
-        <Tooltip label="By connecting more social accounts, you increase your chances for satisfying the hidden requirement(s)">
-          <Info />
-        </Tooltip>
+        canConnectMorePlatforms && (
+          <Tooltip label="By connecting more social accounts, you increase your chances for satisfying the hidden requirement(s)">
+            <Info />
+          </Tooltip>
+        )
       }
     >
       Some secret requirements
