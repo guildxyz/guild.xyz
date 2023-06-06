4af227ceb3c84fe966554dd03f1efd8ae28adbd1
BrickheadJohnny <johnny.zgen@gmail.com>
2023-06-06 13:50:24 +0200

    feat(useEagerConnect): add WalletConnect V2

--- src/connectors/walletConnectV2.ts
+++ src/connectors/walletConnectV2.ts
@@ -2,7 +2,7 @@ import { initializeConnector, Web3ReactHooks } from "@web3-react/core"
 import { WalletConnect as WalletConnectV2 } from "@web3-react/walletconnect-v2"
 import { RPC } from "connectors"
 
-const initializeWalletConnectConnector = (): [WalletConnectV2, Web3ReactHooks] => {
+const initializeWalletConnectV2Connector = (): [WalletConnectV2, Web3ReactHooks] => {
   /**
    * In edge runtime, the initializeConnector won't work, so as a workaround we're
    * using a try-catch here and returning an array with undefined values. This won't
@@ -40,4 +40,4 @@ const initializeWalletConnectConnector = (): [WalletConnectV2, Web3ReactHooks] =
   }
 }
 
-export default initializeWalletConnectConnector
+export default initializeWalletConnectV2Connector
