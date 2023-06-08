5d6bf7367e93f110a9f8888a01d653f68a216e7b
BrickheadJohnny <johnny.zgen@gmail.com>
2023-06-08 23:02:26 +0200

    fix(LeaderboardUserCard): variable position fontSize

--- src/components/leaderboard/LeaderboardUserCard.tsx
+++ src/components/leaderboard/LeaderboardUserCard.tsx
@@ -104,7 +104,13 @@ const LeaderboardUserCard = ({ address, score, position, pins }: Props) => {
               as="span"
               fontFamily="display"
               fontSize={
-                position <= 3 ? { base: "sm", sm: "md" } : { base: "lg", sm: "xl" }
+                position <= 3
+                  ? { base: "sm", sm: "md" }
+                  : position < 1000
+                  ? { base: "lg", sm: "xl" }
+                  : position < 10000
+                  ? { base: "md", sm: "lg" }
+                  : { base: "xs", sm: "sm" }
               }
               fontWeight="bold"
               letterSpacing="wide"
