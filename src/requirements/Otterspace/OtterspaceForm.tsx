535407e9b8de61719ac6c7161320f6f5b80caadd
BrickheadJohnny <johnny.zgen@gmail.com>
2023-06-08 18:35:33 +0200

    OtterspaceForm: use ControlledCombobox

--- src/requirements/Otterspace/OtterspaceForm.tsx
+++ src/requirements/Otterspace/OtterspaceForm.tsx
@@ -1,13 +1,6 @@
-import {
-  FormControl,
-  FormLabel,
-  InputGroup,
-  InputLeftElement,
-  Stack,
-} from "@chakra-ui/react"
-import ControlledSelect from "components/common/ControlledSelect"
+import { FormControl, FormLabel, Stack } from "@chakra-ui/react"
 import FormErrorMessage from "components/common/FormErrorMessage"
-import OptionImage from "components/common/StyledSelect/components/CustomSelectOption/components/OptionImage"
+import { ControlledCombobox } from "components/zag/Combobox"
 import { useFormContext, useWatch } from "react-hook-form"
 import { RequirementFormProps } from "requirements"
 import parseFromObject from "utils/parseFromObject"
@@ -21,11 +14,8 @@ const OtterspaceForm = ({ baseFieldPath }: RequirementFormProps) => {
   } = useFormContext()
 
   const chain = useWatch({ name: `${baseFieldPath}.chain` })
-  const badgeId = useWatch({ name: `${baseFieldPath}.data.id` })
 
   const { data } = useOtterspaceBadges(chain)
-
-  const pickedBadge = data?.find((option) => option.value === badgeId)
 
   return (
     <Stack spacing={4} alignItems="start">
@@ -38,24 +28,13 @@ const OtterspaceForm = ({ baseFieldPath }: RequirementFormProps) => {
       <FormControl isRequired>
         <FormLabel>Badge:</FormLabel>
 
-        <InputGroup>
-          {pickedBadge && (
-            <InputLeftElement>
-              <OptionImage
-                img={pickedBadge?.img as string}
-                alt={pickedBadge?.label}
-              />
-            </InputLeftElement>
-          )}
-
-          <ControlledSelect
-            name={`${baseFieldPath}.data.id`}
-            rules={{ required: "This field is required." }}
-            isClearable
-            options={data}
-            placeholder="Choose badge"
-          />
-        </InputGroup>
+        <ControlledCombobox
+          name={`${baseFieldPath}.data.id`}
+          rules={{ required: "This field is required." }}
+          isClearable
+          options={data}
+          placeholder="Choose badge"
+        />
 
         <FormErrorMessage>
           {parseFromObject(errors, baseFieldPath)?.data?.id?.message}
