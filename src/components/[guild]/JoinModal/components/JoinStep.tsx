6b00e2f8c0e21c4363621c86c1f1ad3354ddbb80
valid <valid@zgen.hu>
2023-06-13 17:11:34 +0200

    UI(JoinModal): remove checkmarks

--- src/components/[guild]/JoinModal/components/JoinStep.tsx
+++ src/components/[guild]/JoinModal/components/JoinStep.tsx
@@ -1,14 +1,5 @@
-import {
-  ButtonGroup,
-  ButtonProps,
-  Circle,
-  HStack,
-  Icon,
-  Text,
-  Tooltip,
-} from "@chakra-ui/react"
+import { ButtonGroup, ButtonProps, HStack, Text, Tooltip } from "@chakra-ui/react"
 import Button from "components/common/Button"
-import { Check } from "phosphor-react"
 import React, { PropsWithChildren } from "react"
 
 type Props = {
@@ -38,7 +29,7 @@ const JoinStep = ({
 
   return (
     <HStack>
-      <Circle
+      {/* <Circle
         size="5"
         border={"1px"}
         {...(isDone
@@ -49,7 +40,7 @@ const JoinStep = ({
           : { bg: "blackAlpha.100", borderColor: "whiteAlpha.100" })}
       >
         {isDone && <Icon as={Check} weight="bold" color={"white"} />}
-      </Circle>
+      </Circle> */}
       <Text w="full" fontWeight={"bold"} noOfLines={1}>
         {title}
         {isRequired && (
