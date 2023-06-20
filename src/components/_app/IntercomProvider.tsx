c783bc27431b8c5841b00657b4a0627b79477026
Csete Balázs <csetebalu@gmail.com>
2023-06-20 19:45:07 +0200

    add isV2 to intercom settings

--- src/components/_app/IntercomProvider.tsx
+++ src/components/_app/IntercomProvider.tsx
@@ -1,6 +1,7 @@
 import { useWeb3React } from "@web3-react/core"
 import useUser from "components/[guild]/hooks/useUser"
 import useMemberships from "components/explorer/hooks/useMemberships"
+import useIsV2 from "hooks/useIsV2"
 import { createContext, PropsWithChildren, useContext, useEffect } from "react"
 import { useSWRConfig } from "swr"
 import { GuildBase } from "types"
@@ -13,7 +14,9 @@ const IntercomContext = createContext<{
   triggerChat: () => {},
 })
 
-export const addIntercomSettings = (newData: Record<string, string | number>) => {
+export const addIntercomSettings = (
+  newData: Record<string, string | number | boolean>
+) => {
   if (typeof window === "undefined" || !newData) return
   const windowAsObject = window as Record<string, any>
 
@@ -61,6 +64,8 @@ const IntercomProvider = ({ children }: PropsWithChildren<unknown>): JSX.Element
 
   const { memberships } = useMemberships()
 
+  const isV2 = useIsV2()
+
   useEffect(() => {
     if (!cache || !account || !user || !memberships) return
 
@@ -85,6 +90,7 @@ const IntercomProvider = ({ children }: PropsWithChildren<unknown>): JSX.Element
       address: account.toLowerCase(),
       connectedPlatforms,
       managedGuilds,
+      isV2,
     })
   }, [cache, account, user, memberships])
 
