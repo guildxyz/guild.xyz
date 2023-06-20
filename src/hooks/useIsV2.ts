ed9fd083ee78b099af88510fcc25e18ca169b345
Csete Balázs <csetebalu@gmail.com>
2023-06-20 19:26:17 +0200

    use boolean feature flag for v2

--- src/hooks/useIsV2.ts
+++ src/hooks/useIsV2.ts
@@ -1,12 +1,11 @@
-import { useFeatureFlagVariantKey } from "posthog-js/react"
+import { useFeatureFlagEnabled } from "posthog-js/react"
 
-const V2_FEATURE_FLAG_KEY = "api-version"
-const V2_FEATURE_FLAG_VARIANT = "api-v2"
+const V2_FEATURE_FLAG_KEY = "api-v2"
 
 const useIsV2 = () => {
-  const variant = useFeatureFlagVariantKey(V2_FEATURE_FLAG_KEY)
+  const flagEnabled = useFeatureFlagEnabled(V2_FEATURE_FLAG_KEY)
 
-  return variant === V2_FEATURE_FLAG_VARIANT
+  return flagEnabled
 }
 
 export default useIsV2
