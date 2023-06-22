fb2817014e073139faf59c12089ba577474ebe51
BrickheadJohnny <johnny.zgen@gmail.com>
2023-06-22 11:46:41 +0200

    RequirementsCard redesign

--- src/components/[guild]/collect/components/RequirementsCard.tsx
+++ src/components/[guild]/collect/components/RequirementsCard.tsx
@@ -26,6 +26,7 @@ type Props = {
 
 const RequirementsCard = ({ chain, address, requirements, logic }: Props) => {
   const requirementsSectionBgColor = useColorModeValue("gray.50", "blackAlpha.300")
+  const requirementsSectionBorderColor = useColorModeValue("gray.200", "gray.600")
 
   const { data, isValidating } = useNftDetails(chain, address)
 
@@ -37,6 +38,8 @@ const RequirementsCard = ({ chain, address, requirements, logic }: Props) => {
         spacing={{ base: 4, md: 8 }}
         w="full"
         alignItems="center"
+        borderBottomWidth={1}
+        borderColor={requirementsSectionBorderColor}
       >
         <Text
           as="span"
@@ -58,64 +61,65 @@ const RequirementsCard = ({ chain, address, requirements, logic }: Props) => {
             </Fragment>
           ))}
         </Stack>
+      </Stack>
 
-        <Stack w="full" alignItems="center" spacing={4}>
-          <FeesTable
-            buttonComponent={
-              <HStack justifyContent={"space-between"} w="full">
-                <Text fontWeight={"medium"}>Minting fee:</Text>
+      <Stack p={{ base: 5, md: 8 }} w="full" alignItems="center" spacing={4}>
+        <FeesTable
+          buttonComponent={
+            <HStack justifyContent={"space-between"} w="full">
+              <Text fontWeight={"medium"}>Minting fee:</Text>
 
-                <Text as="span">
-                  {/* TODO: Dynamic price */}
-                  <Text as="span">1 MATIC</Text>
-                  <Text as="span" colorScheme="gray">
-                    {" + gas"}
-                  </Text>
+              <Text as="span">
+                {/* TODO: Dynamic price */}
+                <Text as="span">1 MATIC</Text>
+                <Text as="span" colorScheme="gray">
+                  {" + gas"}
                 </Text>
-              </HStack>
-            }
-          >
-            <Tr>
-              <Td>Price</Td>
-              <Td isNumeric>Free</Td>
-            </Tr>
-
-            <Tr>
-              <Td>Minting fee</Td>
-              <Td isNumeric>
-                {/* TODO: real data */}
-                <Skeleton isLoaded={true}>1 MATIC</Skeleton>
-              </Td>
-            </Tr>
-
-            <Tr>
-              <Td>Total</Td>
-              <Td isNumeric color="WindowText">
-                1 MATIC + gas
-              </Td>
-            </Tr>
-          </FeesTable>
-
-          <Button colorScheme="green" w="full">
-            Collect now
-          </Button>
-
-          {(data || isValidating) && (
-            <Skeleton maxW="max-content" isLoaded={!isValidating && !!data}>
-              <Text fontSize="sm" colorScheme="gray" fontWeight="medium">
-                {`${
-                  new Intl.NumberFormat("en", {
-                    notation: "standard",
-                  }).format(data?.totalCollectors) ?? 0
-                } collected - ${
-                  new Intl.NumberFormat("en", {
-                    notation: "standard",
-                  }).format(data?.totalCollectorsToday) ?? 0
-                } collected today`}
               </Text>
-            </Skeleton>
-          )}
-        </Stack>
+            </HStack>
+          }
+          bgColor={requirementsSectionBgColor}
+        >
+          <Tr>
+            <Td>Price</Td>
+            <Td isNumeric>Free</Td>
+          </Tr>
+
+          <Tr>
+            <Td>Minting fee</Td>
+            <Td isNumeric>
+              {/* TODO: real data */}
+              <Skeleton isLoaded={true}>1 MATIC</Skeleton>
+            </Td>
+          </Tr>
+
+          <Tr>
+            <Td>Total</Td>
+            <Td isNumeric color="WindowText">
+              1 MATIC + gas
+            </Td>
+          </Tr>
+        </FeesTable>
+
+        <Button colorScheme="green" w="full">
+          Collect now
+        </Button>
+
+        {(data || isValidating) && (
+          <Skeleton maxW="max-content" isLoaded={!isValidating && !!data}>
+            <Text fontSize="sm" colorScheme="gray" fontWeight="medium">
+              {`${
+                new Intl.NumberFormat("en", {
+                  notation: "standard",
+                }).format(data?.totalCollectors) ?? 0
+              } collected - ${
+                new Intl.NumberFormat("en", {
+                  notation: "standard",
+                }).format(data?.totalCollectorsToday) ?? 0
+              } collected today`}
+            </Text>
+          </Skeleton>
+        )}
       </Stack>
     </Card>
   )
