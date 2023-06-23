e55f2ec1dd2970e047e83d36bcac908e3a1822cb
Csete Balázs <csetebalu@gmail.com>
2023-06-23 16:49:03 +0200

    fix useV2Addresses

--- src/hooks/useV2Addresses.ts
+++ src/hooks/useV2Addresses.ts
@@ -5,7 +5,6 @@ const CONFIG_ITEM_NAME = "v2Addresses"
 const useV2Addresses = () => {
   const { data: v2Addresses } = useSWRImmutable<string[]>([
     `https://edge-config.vercel.com/${process.env.NEXT_PUBLIC_EDGE_CONFIG_ID}/item/${CONFIG_ITEM_NAME}`,
-    ,
     {
       headers: {
         Authorization: `Bearer ${process.env.NEXT_PUBLIC_EDGE_CONFIG_READ_ACCESS_TOKEN}`,
