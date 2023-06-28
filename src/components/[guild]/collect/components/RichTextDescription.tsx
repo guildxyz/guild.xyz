16f795d82e0747eed44969f46a9bfbb8173102c7
BrickheadJohnny <johnny.zgen@gmail.com>
2023-06-28 15:00:10 +0200

    fix(RichTextDescription): improve spacing

--- src/components/[guild]/collect/components/RichTextDescription.tsx
+++ src/components/[guild]/collect/components/RichTextDescription.tsx
@@ -1,4 +1,4 @@
-import { Box, Code, Heading } from "@chakra-ui/react"
+import { Box, Code, Heading, Text } from "@chakra-ui/react"
 import Link from "components/common/Link"
 import { forwardRef } from "react"
 import ReactMarkdown from "react-markdown"
@@ -14,39 +14,44 @@ const reactMarkdownComponents: Partial<
     </Link>
   ),
   h1: ({ children, node: _node, ...props }) => (
-    <Heading as="h1" fontFamily="display" fontSize="4xl" {...props}>
+    <Heading as="h1" fontFamily="display" fontSize="3xl" mb={4} {...props}>
       {children}
     </Heading>
   ),
   h2: ({ children, node: _node, ...props }) => (
-    <Heading as="h2" fontFamily="display" fontSize="3xl" {...props}>
+    <Heading as="h2" fontFamily="display" fontSize="2xl" mb={4} {...props}>
       {children}
     </Heading>
   ),
   h3: ({ children, node: _node, ...props }) => (
-    <Heading as="h3" fontFamily="display" fontSize="2xl" {...props}>
+    <Heading as="h3" fontFamily="display" fontSize="xl" mb={4} {...props}>
       {children}
     </Heading>
   ),
   h4: ({ children, node: _node, ...props }) => (
-    <Heading as="h4" fontFamily="display" fontSize="xl" {...props}>
+    <Heading as="h4" fontFamily="display" fontSize="lg" mb={4} {...props}>
       {children}
     </Heading>
   ),
   h5: ({ children, node: _node, ...props }) => (
-    <Heading as="h5" fontFamily="display" fontSize="lg" {...props}>
+    <Heading as="h5" fontFamily="display" fontSize="md" mb={4} {...props}>
       {children}
     </Heading>
   ),
   h6: ({ children, node: _node, ...props }) => (
-    <Heading as="h6" fontFamily="display" fontSize="md" {...props}>
+    <Heading as="h6" fontFamily="display" fontSize="sm" mb={4} {...props}>
       {children}
     </Heading>
   ),
   code: ({ children, node: _node, ...props }) => (
-    <Code display="block" px={4} py={2} {...props}>
+    <Code display="block" px={4} py={2} mb={4} {...props}>
       {children}
     </Code>
+  ),
+  p: ({ children, node: _node, ...props }) => (
+    <Text display="block" mb={4} {...props}>
+      {children}
+    </Text>
   ),
 }
 
