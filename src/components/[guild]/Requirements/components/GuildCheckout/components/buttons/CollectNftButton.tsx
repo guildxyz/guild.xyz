4761097cfd19ada721ee6e8d1df9f8e1fe620364
BrickheadJohnny <johnny.zgen@gmail.com>
2023-06-29 12:49:04 +0200

    feat(CollectNftButton): join before claim

--- src/components/[guild]/Requirements/components/GuildCheckout/components/buttons/CollectNftButton.tsx
+++ src/components/[guild]/Requirements/components/GuildCheckout/components/buttons/CollectNftButton.tsx
@@ -1,26 +1,55 @@
 import { useWeb3React } from "@web3-react/core"
 import Button from "components/common/Button"
+import useMemberships from "components/explorer/hooks/useMemberships"
 import useNftDetails from "components/[guild]/collect/hooks/useNftDetails"
 import useGuild from "components/[guild]/hooks/useGuild"
 import { usePostHogContext } from "components/_app/PostHogProvider"
 import { Chains } from "connectors"
 import useBalance from "hooks/useBalance"
+import useShowErrorToast from "hooks/useShowErrorToast"
+import { SignedValdation, useSubmitWithSign } from "hooks/useSubmit"
+import fetcher from "utils/fetcher"
 import useCollectNft from "../../hooks/useCollectNft"
 import { useCollectNftContext } from "../CollectNftContext"
 
+const rejoin = (signedValidation: SignedValdation) =>
+  fetcher(`/user/join`, signedValidation)
+
 const CollectNftButton = (): JSX.Element => {
   const { captureEvent } = usePostHogContext()
-  const { urlName } = useGuild()
+  const showErrorToast = useShowErrorToast()
 
-  const { chain, address } = useCollectNftContext()
+  const { roleId, chain, address } = useCollectNftContext()
+  const { id, urlName } = useGuild()
+
+  const {
+    memberships,
+    isLoading: isMembershipsLoading,
+    mutate: mutateMemberships,
+  } = useMemberships()
+  const isMemberOfRole = !!memberships
+    ?.find((membership) => membership.guildId === id)
+    ?.roleIds.find((rId) => rId === roleId)
+
   const { chainId } = useWeb3React()
   const shouldSwitchNetwork = chainId !== Chains[chain]
 
   const {
-    onSubmit,
+    onSubmit: onMintSubmit,
     isLoading: isMinting,
     loadingText: mintLoadingText,
   } = useCollectNft()
+
+  const { onSubmit: onJoinSubmit, isLoading: isJoinLoading } = useSubmitWithSign(
+    rejoin,
+    {
+      onSuccess: async () => {
+        await mutateMemberships()
+        onMintSubmit()
+      },
+      onError: () => showErrorToast("Couldn't check eligibility"),
+    }
+  )
 
   const { data: nftDetails, isValidating: isNftDetailsValidating } = useNftDetails(
     chain,
@@ -33,8 +62,18 @@ const CollectNftButton = (): JSX.Element => {
   const isSufficientBalance =
     nftDetails?.fee && coinBalance ? coinBalance.gt(nftDetails.fee) : undefined
 
-  const isLoading = isMinting || isNftDetailsValidating || isBalanceLoading
-  const loadingText = isMinting ? mintLoadingText : "Checking your balance"
+  const isLoading =
+    isMembershipsLoading ||
+    isJoinLoading ||
+    isMinting ||
+    isNftDetailsValidating ||
+    isBalanceLoading
+  const loadingText =
+    isJoinLoading || isMembershipsLoading
+      ? "Checking eligibility"
+      : isMinting
+      ? mintLoadingText
+      : "Checking your balance"
 
   const isDisabled = shouldSwitchNetwork || !isSufficientBalance || isLoading
 
@@ -47,10 +86,14 @@ const CollectNftButton = (): JSX.Element => {
       colorScheme={!isDisabled ? "blue" : "gray"}
       w="full"
       onClick={() => {
-        captureEvent("Click: CollectNftButton (GuildCheckout)", {
-          guild: urlName,
-        })
-        onSubmit()
+        if (!isMemberOfRole) {
+          onJoinSubmit({ guildId: id })
+        } else {
+          captureEvent("Click: CollectNftButton (GuildCheckout)", {
+            guild: urlName,
+          })
+          onMintSubmit()
+        }
       }}
     >
       {typeof isSufficientBalance === "boolean" && !isSufficientBalance
