4e6520c5ebafb3f282c50dc8162236ad2a533ea7
valid <valid@zgen.hu>
2023-07-05 19:36:57 +0200

    refactor: extract ExploreAllGuilds into separate component

--- src/pages/explorer.tsx
+++ src/pages/explorer.tsx
@@ -1,34 +1,13 @@
-import {
-  Center,
-  Divider,
-  GridItem,
-  SimpleGrid,
-  Spinner,
-  Stack,
-  Text,
-  useBreakpointValue,
-  useColorModeValue,
-  usePrevious,
-} from "@chakra-ui/react"
-import { useWeb3React } from "@web3-react/core"
-import { BATCH_SIZE } from "components/_app/ExplorerProvider"
+import { useBreakpointValue, useColorModeValue } from "@chakra-ui/react"
 import Layout from "components/common/Layout"
 import LinkPreviewHead from "components/common/LinkPreviewHead"
-import Section from "components/common/Section"
-import ExplorerCardMotionWrapper from "components/explorer/ExplorerCardMotionWrapper"
+import ExploreAllGuilds from "components/explorer/ExploreAllGuilds"
 import ExplorerTabs from "components/explorer/ExplorerTabs"
 import GoToCreateGuildButton from "components/explorer/GoToCreateGuildButton"
-import GuildCard from "components/explorer/GuildCard"
-import GuildCardsGrid from "components/explorer/GuildCardsGrid"
-import OrderSelect, { OrderOptions } from "components/explorer/OrderSelect"
-import SearchBar from "components/explorer/SearchBar"
 import YourGuilds from "components/explorer/YourGuilds"
-import { useQueryState } from "hooks/useQueryState"
 import useSWRWithOptionalAuth from "hooks/useSWRWithOptionalAuth"
-import useScrollEffect from "hooks/useScrollEffect"
 import { GetStaticProps } from "next"
-import { useEffect, useRef } from "react"
-import useSWRInfinite from "swr/infinite"
+import { useRef } from "react"
 import { GuildBase } from "types"
 import fetcher from "utils/fetcher"
 
@@ -37,53 +16,11 @@ type Props = {
 }
 
 const Page = ({ guilds: guildsInitial }: Props): JSX.Element => {
-  const { account } = useWeb3React()
-
-  const [search, setSearch] = useQueryState<string>("search", undefined)
-  const prevSearch = usePrevious(search)
-  const [order, setOrder] = useQueryState<OrderOptions>("order", "members")
-
-  const query = new URLSearchParams({ order, ...(search && { search }) }).toString()
-
   const yourGuildsRef = useRef(null)
   const allGuildsRef = useRef(null)
 
-  const {
-    data: filteredGuilds,
-    setSize,
-    isValidating,
-  } = useSWRInfinite(
-    (pageIndex, previousPageData) =>
-      Array.isArray(previousPageData) && previousPageData.length !== BATCH_SIZE
-        ? null
-        : `/guild?${query}&limit=${BATCH_SIZE}&offset=${pageIndex * BATCH_SIZE}`,
-    {
-      fallbackData: guildsInitial,
-      dedupingInterval: 60000, // one minute
-      revalidateFirstPage: false,
-    }
-  )
-  const renderedGuilds = filteredGuilds?.flat()
-
   // ? is included, so the request hits v2Replacer in fetcher
   const { data: usersGuilds } = useSWRWithOptionalAuth(`/guild?`)
-
-  useEffect(() => {
-    if (prevSearch === search || prevSearch === undefined) return
-    setSize(1)
-  }, [search, prevSearch])
-
-  // TODO: we use this behaviour in multiple places now, should make a useScrollBatchedRendering hook
-  useScrollEffect(() => {
-    if (
-      !allGuildsRef.current ||
-      allGuildsRef.current.getBoundingClientRect().bottom > window.innerHeight ||
-      isValidating
-    )
-      return
-
-    setSize((prev) => prev + 1)
-  }, [filteredGuilds, isValidating])
 
   const bgColor = useColorModeValue("var(--chakra-colors-gray-800)", "#37373a") // dark color is from whiteAlpha.200, but without opacity so it can overlay the banner image
   const bgOpacity = useColorModeValue(0.06, 0.1)
@@ -121,50 +58,7 @@ const Page = ({ guilds: guildsInitial }: Props): JSX.Element => {
 
         <YourGuilds ref={yourGuildsRef} />
 
-        <Stack spacing={{ base: 8, md: 10 }}>
-          {account && <Divider />}
-          <Section
-            title="Explore all guilds"
-            ref={allGuildsRef}
-            id="allGuilds"
-            scrollMarginTop={20}
-          >
-            <SimpleGrid
-              templateColumns={{ base: "auto 50px", md: "1fr 1fr 1fr" }}
-              gap={{ base: 2, md: "6" }}
-              pb={{ md: 1.5 }}
-              // needed so there's no gap on the right side of the page in mobile Safari
-              overflow={"hidden"}
-              // needed so the focus outline is not cut off because of the hidden overflow
-              p={"1px"}
-            >
-              <GridItem colSpan={{ base: 1, md: 2 }}>
-                <SearchBar placeholder="Search guilds" {...{ search, setSearch }} />
-              </GridItem>
-              <OrderSelect {...{ order, setOrder }} />
-            </SimpleGrid>
-
-            {!renderedGuilds.length ? (
-              isValidating ? null : !search?.length ? (
-                <Text>
-                  Can't fetch guilds from the backend right now. Check back later!
-                </Text>
-              ) : (
-                <Text>{`No results for ${search}`}</Text>
-              )
-            ) : (
-              <GuildCardsGrid>
-                {renderedGuilds.map((guild) => (
-                  <ExplorerCardMotionWrapper key={guild.urlName}>
-                    <GuildCard guildData={guild} />
-                  </ExplorerCardMotionWrapper>
-                ))}
-              </GuildCardsGrid>
-            )}
-          </Section>
-
-          <Center>{isValidating && <Spinner />}</Center>
-        </Stack>
+        <ExploreAllGuilds ref={allGuildsRef} {...{ guildsInitial }} />
       </Layout>
     </>
   )
