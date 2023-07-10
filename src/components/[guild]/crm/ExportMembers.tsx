d23c6a8d2031cd198929b45f64887744e9aa4cd9
valid <valid@zgen.hu>
2023-07-11 00:54:51 +0200

    copy(ExportMembers): responsive label

--- src/components/[guild]/crm/ExportMembers.tsx
+++ src/components/[guild]/crm/ExportMembers.tsx
@@ -1,9 +1,11 @@
+import { useBreakpointValue } from "@chakra-ui/react"
 import Button from "components/common/Button"
 import { Export } from "phosphor-react"
 import { useIsTabsStuck } from "../Tabs/Tabs"
 
 const ExportMembers = () => {
   const { isStuck } = useIsTabsStuck()
+  const label = useBreakpointValue({ base: "Export", md: "Export members" })
 
   return (
     <Button
@@ -12,7 +14,7 @@ const ExportMembers = () => {
       colorScheme={isStuck ? "gray" : "whiteAlpha"}
       size="sm"
     >
-      Export members
+      {label}
     </Button>
   )
 }
