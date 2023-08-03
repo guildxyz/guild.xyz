830af65207136f0ed9935e8013159f0858f3ff7b
cs-balazs <44908417+cs-balazs@users.noreply.github.com>
2023-08-03 11:53:10 +0200

    Covalent wallet activity requirements (#862)
    * add covalent wallet activity requirements
    
    * fix: keep undefined req data in preprocess
    
    * fix: blocknumber required fix
    
    * fix: remove falsy timestamps from covalent reqs

--- src/utils/preprocessRequirements.ts
+++ src/utils/preprocessRequirements.ts
@@ -1,4 +1,5 @@
-import { Requirement } from "types"
+import { COVALENT_CHAINS } from "requirements/WalletActivity/WalletActivityForm"
+import { Requirement, RequirementType } from "types"
 
 const preprocessRequirements = (requirements: Array<Requirement>) => {
   if (!requirements || !Array.isArray(requirements)) return undefined
@@ -15,11 +16,41 @@ const preprocessRequirements = (requirements: Array<Requirement>) => {
       .map((requirement) => {
         const processedRequirement: Requirement = {
           ...requirement,
-          data: {
-            ...requirement.data,
-            validAddresses: undefined,
-          },
+          data: requirement.data
+            ? {
+                ...requirement.data,
+                validAddresses: undefined,
+              }
+            : undefined,
           nftRequirementType: undefined,
+        }
+
+        if (
+          processedRequirement.type?.startsWith("ALCHEMY_") &&
+          COVALENT_CHAINS.has(processedRequirement.chain)
+        ) {
+          processedRequirement.type = processedRequirement.type.replace(
+            "ALCHEMY_",
+            "COVALENT_"
+          ) as RequirementType
+
+          if (processedRequirement?.data?.timestamps?.minAmount) {
+            processedRequirement.data.timestamps.minAmount *= 1000
+          }
+
+          if (processedRequirement?.data?.timestamps?.maxAmount) {
+            processedRequirement.data.timestamps.maxAmount *= 1000
+          }
+        }
+
+        if (processedRequirement.type?.startsWith("COVALENT_")) {
+          if (!processedRequirement?.data?.timestamps?.minAmount) {
+            delete processedRequirement.data.timestamps.minAmount
+          }
+
+          if (!processedRequirement?.data?.timestamps?.maxAmount) {
+            delete processedRequirement.data.timestamps.maxAmount
+          }
         }
 
         if (requirement.type === "COIN") {
