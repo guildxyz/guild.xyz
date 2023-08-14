2588a2cec2e404bad6594a6e3f0eaff8228cda5b
valid <valid@zgen.hu>
2023-08-14 14:05:49 +0200

    UI(SearchBarFilters): small consistency fixes

--- src/components/explorer/SearchBarFilters.tsx
+++ src/components/explorer/SearchBarFilters.tsx
@@ -1,4 +1,4 @@
-import { Button, Card, HStack, Icon, useColorModeValue } from "@chakra-ui/react"
+import { Box, Button, HStack, Icon, useColorModeValue } from "@chakra-ui/react"
 import { motion } from "framer-motion"
 import { CircleWavyCheck, PushPin, Sparkle } from "phosphor-react"
 
@@ -19,41 +19,41 @@ const icons: { [K in Filters]: any } = {
 const SearchBarFilters = ({ selected, onSelect }: Props): JSX.Element => {
   const selectedBg = useColorModeValue("white", "gray.600")
   const selectedShadow = "0 0.5px 2px 0 rgba(0, 0, 0, 0.2)"
-  const MotionCard = motion(Card)
+  const MotionBox = motion(Box)
 
   return (
     <HStack as="ul" gap={1.5}>
       {ExplorerFilters.map((filter) => (
-        <Button
-          key={filter}
-          leftIcon={<Icon as={icons[filter]} />}
-          as="label"
-          cursor="pointer"
-          borderRadius="lg"
-          alignSelf="center"
-          size="sm"
-          bgColor={"transparent"}
-          onClick={() => onSelect(filter)}
-          position={"relative"}
-          zIndex={1}
-          {...(selected === filter && { _hover: { bg: "transparent" } })}
-        >
-          {filter.toLowerCase()}
+        <Box key={filter} position={"relative"}>
+          <Button
+            leftIcon={<Icon as={icons[filter]} />}
+            as="label"
+            cursor="pointer"
+            borderRadius="lg"
+            alignSelf="center"
+            size="sm"
+            bgColor={"transparent"}
+            onClick={() => onSelect(filter)}
+            zIndex={1}
+            {...(selected === filter && { _hover: { bg: "transparent" } })}
+          >
+            {filter.toLowerCase()}
+          </Button>
           {selected === filter ? (
-            <MotionCard
+            <MotionBox
               style={{
                 position: "absolute",
-                zIndex: "-1",
-                width: "100%",
+                inset: 0,
               }}
               layoutId="slide-bg"
               transition={{ type: "spring", bounce: 0.25, duration: 0.5 }}
               boxShadow={selectedShadow}
+              borderRadius="lg"
               bgColor={`${selectedBg} !important`}
               height="var(--chakra-sizes-8)"
             />
           ) : null}
-        </Button>
+        </Box>
       ))}
     </HStack>
   )
