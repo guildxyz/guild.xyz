e66c96c8a64276fe0a48e7f4bc379dcbe2ad89c8
BrickheadJohnny <johnny.zgen@gmail.com>
2023-08-15 11:35:37 +0200

    fix(AddPoapPanel): remove modal elements

--- src/components/[guild]/CreatePoap/AddPoapPanel.tsx
+++ src/components/[guild]/CreatePoap/AddPoapPanel.tsx
@@ -1,17 +1,4 @@
-import {
-  Box,
-  HStack,
-  IconButton,
-  ModalBody,
-  ModalHeader,
-  Tab,
-  TabList,
-  TabPanel,
-  TabPanels,
-  Tabs,
-  Text,
-} from "@chakra-ui/react"
-import { ArrowLeft } from "phosphor-react"
+import { Box, Tab, TabList, TabPanel, TabPanels, Tabs } from "@chakra-ui/react"
 import { useFormContext } from "react-hook-form"
 import { useAddRewardContext } from "../AddRewardContext"
 import { CreatePoapProvider } from "./components/CreatePoapContext"
@@ -20,53 +7,36 @@ import CreatePoapForm from "./components/PoapDataForm/CreatePoapForm"
 import { SetupPoapRequirements } from "./components/PoapRequirements/PoapRequirements"
 
 const AddPoapPanel = (): JSX.Element => {
-  const { modalRef, setSelection, step, onClose } = useAddRewardContext()
+  const { step, onClose } = useAddRewardContext()
   const { reset } = useFormContext()
 
   return (
     <>
-      <ModalHeader>
-        <HStack>
-          <IconButton
-            rounded="full"
-            aria-label="Back"
-            size="sm"
-            mb="-3px"
-            icon={<ArrowLeft size={20} />}
-            variant="ghost"
-            onClick={() => setSelection(null)}
-          />
-          <Text>Add POAP</Text>
-        </HStack>
-      </ModalHeader>
-
-      <ModalBody ref={modalRef}>
-        {step === "ROLES_REQUIREMENTS" ? (
-          <SetupPoapRequirements
-            onSuccess={() => {
-              reset()
-              onClose()
-            }}
-          />
-        ) : (
-          <Box>
-            <Tabs size="sm" isFitted variant="solid" colorScheme="indigo">
-              <TabList mb="8">
-                <Tab>Create new POAP</Tab>
-                <Tab>Import existing</Tab>
-              </TabList>
-              <TabPanels>
-                <TabPanel>
-                  <CreatePoapForm />
-                </TabPanel>
-                <TabPanel>
-                  <ImportPoap />
-                </TabPanel>
-              </TabPanels>
-            </Tabs>
-          </Box>
-        )}
-      </ModalBody>
+      {step === "ROLES_REQUIREMENTS" ? (
+        <SetupPoapRequirements
+          onSuccess={() => {
+            reset()
+            onClose()
+          }}
+        />
+      ) : (
+        <Box>
+          <Tabs size="sm" isFitted variant="solid" colorScheme="indigo">
+            <TabList mb="8">
+              <Tab>Create new POAP</Tab>
+              <Tab>Import existing</Tab>
+            </TabList>
+            <TabPanels>
+              <TabPanel>
+                <CreatePoapForm />
+              </TabPanel>
+              <TabPanel>
+                <ImportPoap />
+              </TabPanel>
+            </TabPanels>
+          </Tabs>
+        </Box>
+      )}
     </>
   )
 }
