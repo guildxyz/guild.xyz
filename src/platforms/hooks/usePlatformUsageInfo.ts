3d82f6c2d405a9e092c38fc6b50ea243ac24db2f
BrickheadJohnny <johnny.zgen@gmail.com>
2023-08-17 06:53:44 +0200

    cleanup(usePlatformUsageInfo): simplify logic

--- src/platforms/hooks/usePlatformUsageInfo.ts
+++ src/platforms/hooks/usePlatformUsageInfo.ts
@@ -1,17 +1,17 @@
 import useGuild from "components/[guild]/hooks/useGuild"
 import useSWRWithOptionalAuth from "hooks/useSWRWithOptionalAuth"
-import platforms, { PlatformAsRewardRestrictions } from "platforms/platforms"
 import { Guild, PlatformName } from "types"
 
 const usePlatformUsageInfo = (
   platform: PlatformName,
   platformId: string
-): { isAlreadyInUse: boolean; guildUrlName?: string; isValidating: boolean } => {
-  const { asRewardRestriction } = platforms[platform]
-
-  const { id } = useGuild()
-
-  // Not sure if it'll be a problem (API performance wise) that we call this endpoint here, but
+): {
+  isAlreadyInUse: boolean
+  isUsedInCurrentGuild: boolean
+  guildUrlName?: string
+  isValidating: boolean
+} => {
+  const { urlName } = useGuild()
   const shouldFetch = platformId?.length > 0
   const { data: guildByPlatform, isValidating } = useSWRWithOptionalAuth<
     Partial<Guild>
@@ -19,20 +19,9 @@ const usePlatformUsageInfo = (
     shouldRetryOnError: false,
   })
 
-  const isOnGuildsPage = id === guildByPlatform?.id
-  const isAlreadyUsedInGuild = !isValidating && !!guildByPlatform?.id
-
-  const isAlreadyInUse = !isOnGuildsPage
-    ? isAlreadyUsedInGuild
-    : !isAlreadyUsedInGuild
-    ? false
-    : asRewardRestriction === PlatformAsRewardRestrictions.SINGLE_ROLE &&
-      guildByPlatform?.guildPlatforms?.some(
-        (gp) => gp.platformGuildId === platformId
-      )
-
   return {
-    isAlreadyInUse,
+    isAlreadyInUse: !!guildByPlatform?.id,
+    isUsedInCurrentGuild: !!guildByPlatform && urlName === guildByPlatform.urlName,
     guildUrlName: guildByPlatform?.urlName,
     isValidating,
   }
