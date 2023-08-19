5c1b6cf7b5ed70bde893dfc0de5d6940c87f02cb
Csete Balázs <csetebalu@gmail.com>
2023-08-19 13:50:47 +0200

    rename flow to job

--- src/components/[guild]/hooks/useAccess.ts
+++ src/components/[guild]/hooks/useAccess.ts
@@ -4,9 +4,9 @@ import useSWRImmutable from "swr/immutable"
 import { useFetcherWithSign } from "utils/fetcher"
 import useUser from "./useUser"
 
-type FlowCreationResponse = { jobId: string }
+type JobCreationResponse = { jobId: string }
 
-const ACCESS_FLOW_POLL_MS = 1000
+const ACCESS_JOB_POLL_MS = 1000
 
 type RequirementAccessResult = {
   requirementId: number
@@ -21,7 +21,7 @@ type AccessCheckResult = {
   errors: any[]
 }
 
-type FlowResult = {
+type Job = {
   id: string
   roleIds: number[]
   accessCheckResult: AccessCheckResult[]
@@ -56,15 +56,15 @@ const useAccess = <RoleId extends number | "UNSET" = "UNSET">(
 
   const pollParams = new URLSearchParams({ guildId: `${guildId}` }).toString()
 
-  const { data: flowId, mutate: mutateFlowId } = useSWRImmutable<string>(
-    ["accessFlowId", guildId],
+  const { data: jobId, mutate: mutatejobId } = useSWRImmutable<string>(
+    ["accessjobId", guildId],
     () => undefined,
     {
       revalidateOnMount: false,
     }
   )
 
-  const setFlowId = (data: string) => mutateFlowId(data, { revalidate: false })
+  const setjobId = (data: string) => mutatejobId(data, { revalidate: false })
 
   const shouldFetch = userId && guildId && roleId !== 0
 
@@ -72,54 +72,54 @@ const useAccess = <RoleId extends number | "UNSET" = "UNSET">(
     ? ([`/v2/actions/access-check?${pollParams}`, { method: "GET" }] as const)
     : null
 
-  const poll = useSWRImmutable<FlowResult>(
+  const poll = useSWRImmutable<Job>(
     pollKey,
     (props: typeof pollKey) =>
-      fetcherWithSign([...props]).then(async (result: FlowResult[]) => {
+      fetcherWithSign([...props]).then(async (result: Job[]) => {
         if (!Array.isArray(result) || result.length <= 0) {
-          await setFlowId(null)
+          await setjobId(null)
           return null
         }
 
-        if (flowId?.length > 0) {
-          const foundFlow = result.find(({ id }) => id === flowId)
-          // If the tracked flow has been finished, we mark the flowId as undefined, so no polling will happen (refreshInterval will be undefined), and we don't start a new flow (explicit null-check)
-          await setFlowId(foundFlow.done ? undefined : foundFlow.id)
-          if (foundFlow) return foundFlow
+        if (jobId?.length > 0) {
+          const foundJob = result.find(({ id }) => id === jobId)
+          // If the tracked job has been finished, we mark the jobId as undefined, so no polling will happen (refreshInterval will be undefined), and we don't start a new flow (explicit null-check)
+          await setjobId(foundJob.done ? undefined : foundJob.id)
+          if (foundJob) return foundJob
         }
-        const nonDoneFlow = result.find(({ done }) => !done)
-        if (nonDoneFlow) {
-          await setFlowId(nonDoneFlow.id)
-          return nonDoneFlow
+        const nonDoneJob = result.find(({ done }) => !done)
+        if (nonDoneJob) {
+          await setjobId(nonDoneJob.id)
+          return nonDoneJob
         }
 
-        await setFlowId(null)
+        await setjobId(null)
         return null
       }),
     {
       ...swrOptions,
       onSuccess: (val) => console.log("POLL", val),
-      refreshInterval: !!flowId ? ACCESS_FLOW_POLL_MS : undefined,
+      refreshInterval: !!jobId ? ACCESS_JOB_POLL_MS : undefined,
     }
   )
 
   // Explicit null check to differenciate from initial undefined
-  const shouldFechFlowCreation = flowId === null
-  const flowCreation = useSWRImmutable(
-    shouldFechFlowCreation
+  const shouldFechJobCreation = jobId === null
+  const jobCreation = useSWRImmutable(
+    shouldFechJobCreation
       ? [`/v2/actions/access-check`, { method: "POST", body: { guildId } }]
       : null,
     (props) =>
-      setFlowId(null)
+      setjobId(null)
         .then(() => poll.mutate(undefined, { revalidate: false }))
         .then(() =>
-          fetcherWithSign(props).then(({ jobId }: FlowCreationResponse) =>
-            setFlowId(jobId)
+          fetcherWithSign(props).then(
+            ({ jobId: createdJobId }: JobCreationResponse) => setjobId(createdJobId)
           )
         ),
     {
       ...swrOptions,
-      onSuccess: (createdFlowId) => console.log("CREATED", createdFlowId),
+      onSuccess: (createdjobId) => console.log("CREATED", createdjobId),
     }
   )
 
@@ -134,10 +134,10 @@ const useAccess = <RoleId extends number | "UNSET" = "UNSET">(
 
   return {
     data: (roleData ?? accessCheckResult) as any,
-    error: poll.error ?? flowCreation.error,
+    error: poll.error ?? jobCreation.error,
     hasAccess,
     isLoading: !accessCheckResult,
-    mutate: () => flowCreation.mutate(),
+    mutate: () => jobCreation.mutate(),
   }
 }
 
