689fbcc035fd1de98f619970fadefd772846bc7b
BrickheadJohnny <johnny.zgen@gmail.com>
2023-08-22 17:30:42 +0200

    fix(useGuildFee): fetch fee on every chain

--- src/components/[guild]/collect/hooks/useGuildFee.ts
+++ src/components/[guild]/collect/hooks/useGuildFee.ts
@@ -1,26 +1,30 @@
 import { BigNumber } from "@ethersproject/bignumber"
+import { Contract } from "@ethersproject/contracts"
+import { JsonRpcBatchProvider } from "@ethersproject/providers"
 import { GUILD_REWARD_NFT_FACTORY_ADDRESSES } from "components/[guild]/RolePlatforms/components/AddRoleRewardModal/components/AddContractCallPanel/components/CreateNftForm/hooks/useCreateNft"
-import { Chain } from "connectors"
-import useContract from "hooks/useContract"
+import { Chain, Chains, RPC } from "connectors"
 import GUILD_REWARD_NFT_FACTORY_ABI from "static/abis/guildRewardNFTFactory.json"
 import useSWRImmutable from "swr/immutable"
+
+const fetchGuildFee = async ([_, chain]) => {
+  const provider = new JsonRpcBatchProvider(RPC[chain].rpcUrls[0], Chains[chain])
+  const contract = new Contract(
+    GUILD_REWARD_NFT_FACTORY_ADDRESSES[chain],
+    GUILD_REWARD_NFT_FACTORY_ABI,
+    provider
+  )
+
+  return contract.fee()
+}
 
 const useGuildFee = (
   chain: Chain
 ): { guildFee: BigNumber; isLoading: boolean; error: any } => {
-  const guildRewardNftFactoryContract = useContract(
-    GUILD_REWARD_NFT_FACTORY_ADDRESSES[chain],
-    GUILD_REWARD_NFT_FACTORY_ABI
-  )
-
   const {
     data: guildFee,
     isLoading,
     error,
-  } = useSWRImmutable(
-    guildRewardNftFactoryContract ? ["guildFee", chain] : null,
-    () => guildRewardNftFactoryContract?.fee()
-  )
+  } = useSWRImmutable(chain ? ["guildFee", chain] : null, fetchGuildFee)
 
   return {
     guildFee,
