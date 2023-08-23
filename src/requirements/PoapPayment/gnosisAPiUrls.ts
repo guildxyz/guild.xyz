2d50713b18411a39c28775c92c8dc53656d190d7
Csete Balázs <csetebalu@gmail.com>
2023-08-23 17:31:37 +0200

    fix: gnosis api urls

--- src/requirements/PoapPayment/gnosisAPiUrls.ts
+++ src/requirements/PoapPayment/gnosisAPiUrls.ts
@@ -1,7 +1,7 @@
 enum GnosisApiUrls {
-  "https://safe-transaction.gnosis.io/api/v1" = 1,
-  "https://safe-transaction.goerli.gnosis.io/api/v1" = 5,
-  "https://safe-transaction.bsc.gnosis.io/api/v1" = 56,
+  "https://safe-transaction-gnosis-chain.safe.global.io/api/v1" = 1,
+  "https://safe-transaction-goerli.safe.global/api/v1" = 5,
+  "https://safe-transaction-bsc.safe.global/api/v1" = 56,
 }
 
 export default GnosisApiUrls
