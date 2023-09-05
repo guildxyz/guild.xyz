fbc98d811bca0f1dc95c60e869f2db40807bdb69
BrickheadJohnny <johnny.zgen@gmail.com>
2023-09-05 11:16:30 +0200

    guild log: show error if user is not an admin

--- src/pages/[guild]/activity.tsx
+++ src/pages/[guild]/activity.tsx
@@ -1,4 +1,6 @@
 import { Stack } from "@chakra-ui/react"
+import Card from "components/common/Card"
+import ErrorAlert from "components/common/ErrorAlert"
 import Layout from "components/common/Layout"
 import { SectionTitle } from "components/common/Section"
 import ActivityLogAction from "components/[guild]/activity/ActivityLogAction"
@@ -10,16 +12,16 @@ import ActivityLogFiltersBar from "components/[guild]/activity/ActivityLogFilter
 import ActivityLogSkeleton from "components/[guild]/activity/ActivityLogSkeleton"
 import useGuild from "components/[guild]/hooks/useGuild"
 import useGuildPermission from "components/[guild]/hooks/useGuildPermission"
+import useUser from "components/[guild]/hooks/useUser"
 import TabButton from "components/[guild]/Tabs/components/TabButton"
 import Tabs from "components/[guild]/Tabs/Tabs"
 import { ThemeProvider, useThemeContext } from "components/[guild]/ThemeContext"
 
 const ActivityLog = (): JSX.Element => {
   const { name, urlName } = useGuild()
+  const { id: userId } = useUser()
   const { isAdmin } = useGuildPermission()
   const { textColor, localThemeColor, localBackgroundImage } = useThemeContext()
-
-  // TODO: redirect if user is not an admin of the guild
 
   const { data, isValidating } = useActivityLog()
 
@@ -38,16 +40,27 @@ const ActivityLog = (): JSX.Element => {
         )}
       </Tabs>
 
-      <ActivityLogFiltersBar />
+      {userId && !isAdmin ? (
+        <Card>
+          <ErrorAlert
+            label="You do not have permission to view this activity log"
+            mb={0}
+          />
+        </Card>
+      ) : (
+        <>
+          <ActivityLogFiltersBar />
 
-      <SectionTitle title="Actions" mt={8} mb="4" />
-      <Stack spacing={2.5}>
-        {data?.entries?.length > 0 &&
-          data.entries.map((action) => (
-            <ActivityLogAction key={action.id} action={action} />
-          ))}
-        {isValidating && <ActivityLogSkeleton />}
-      </Stack>
+          <SectionTitle title="Actions" mt={8} mb="4" />
+          <Stack spacing={2.5}>
+            {data?.entries?.length > 0 &&
+              data.entries.map((action) => (
+                <ActivityLogAction key={action.id} action={action} />
+              ))}
+            {isValidating && <ActivityLogSkeleton />}
+          </Stack>
+        </>
+      )}
     </Layout>
   )
 }
