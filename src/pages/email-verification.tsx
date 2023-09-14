23eafd15b6f46e2b1002e7a19bf5d003b9c9bb87
Csete Balázs <csetebalu@gmail.com>
2023-09-14 12:21:15 +0200

    chakra steps

--- src/pages/email-verification.tsx
+++ src/pages/email-verification.tsx
@@ -1,23 +1,30 @@
 import {
   Center,
   FormControl,
-  FormLabel,
   HStack,
   Input,
   PinInput,
   PinInputField,
+  Text,
   VStack,
 } from "@chakra-ui/react"
+import { Step, Steps } from "chakra-ui-steps"
 import Button from "components/common/Button"
 import Card from "components/common/Card"
 import useSubmit from "hooks/useSubmit"
 import { useRouter } from "next/router"
+import { useState } from "react"
 import { useController, useForm, useWatch } from "react-hook-form"
 import { useFetcherWithSignWithKeyPairOfUser } from "utils/fetcher"
 import timeoutPromise from "utils/timeoutPromise"
 import { OAUTH_CONFIRMATION_TIMEOUT_MS } from "./oauth"
 
 const PIN_LENGTH = 6
+
+const steps = {
+  EMAIL_ENTRY: 0,
+  PIN_ENTRY: 1,
+}
 
 const EmailVerificationPage = () => {
   const router = useRouter()
@@ -32,8 +39,12 @@ const EmailVerificationPage = () => {
   })
   const { field } = useController({ control, name: "code" })
   const code = useWatch({ control, name: "code" })
+  const email = useWatch({ control, name: "email" })
+
+  const [activeStep, setActiveStep] = useState(steps.EMAIL_ENTRY)
 
   const submitVerificationRequest = async (emailAddress: string) => {
+    setActiveStep(steps.PIN_ENTRY)
     return true
     const verificationResponse = await fetcherWithSign([
       `/v2/email/verifications`,
@@ -55,7 +66,7 @@ const EmailVerificationPage = () => {
       ? { from: null, csrfToken: null }
       : JSON.parse(window.localStorage.getItem(`EMAIL_oauthinfo`) ?? "{}")
 
-  const sendCodeBackToMainWindow = (code: string) => {
+  const sendCodeBackToMainWindow = (pin: string) => {
     const channel = new BroadcastChannel(csrfToken)
 
     const isMessageConfirmed = timeoutPromise(
@@ -67,7 +78,7 @@ const EmailVerificationPage = () => {
       .then(() => true)
       .catch(() => false)
 
-    const response = { type: "OAUTH_SUCCESS", data: { code } }
+    const response = { type: "OAUTH_SUCCESS", data: { code: pin } }
     channel.postMessage(response)
 
     isMessageConfirmed.then((isReceived) => {
@@ -92,53 +103,71 @@ const EmailVerificationPage = () => {
 
   return (
     <Center height={"100vh"}>
-      <Card p={10}>
-        <VStack spacing={7} as="form" onSubmit={submit}>
-          <FormControl isDisabled={!!shouldShowPinEntry}>
-            <FormLabel>E-Mail address</FormLabel>
-            <Input
-              autoFocus
-              type="email"
-              {...register("email", {
-                required: true,
-                // We should get native validation
-                // pattern: { value: EMAIL_REGEX, message: "Invalid E-Mail address" },
-              })}
-            />
-          </FormControl>
+      <Card p={10} minW={500}>
+        <Steps
+          // onClickStep={setActiveStep}
+          activeStep={activeStep}
+          colorScheme="primary"
+          orientation={"vertical"}
+          size="sm"
+        >
+          <Step
+            label={
+              activeStep === steps.PIN_ENTRY ? (
+                <HStack spacing={6}>
+                  <Text>{email}</Text>
+                  <Button
+                    variant="link"
+                    opacity={0.75}
+                    size="xs"
+                    onClick={() => {
+                      setValue("code", "")
+                      resetVerificationRequest()
+                      setActiveStep(steps.EMAIL_ENTRY)
+                    }}
+                  >
+                    use different email
+                  </Button>
+                </HStack>
+              ) : (
+                "Enter E-Mail address"
+              )
+            }
+            key={"email-entry"}
+          >
+            <FormControl>
+              <Input
+                autoFocus
+                type="email"
+                {...register("email", {
+                  required: true,
+                  // We should get native validation
+                  // pattern: { value: EMAIL_REGEX, message: "Invalid E-Mail address" },
+                })}
+              />
+            </FormControl>
+          </Step>
+          <Step label={"Enter PIN"} key={"pin-entry"}>
+            <FormControl>
+              <HStack justifyContent={"center"}>
+                <PinInput
+                  value={field.value}
+                  onChange={(value) => field.onChange(value)}
+                >
+                  <PinInputField autoFocus />
+                  <PinInputField />
+                  <PinInputField />
+                  <PinInputField />
+                  <PinInputField />
+                  <PinInputField />
+                </PinInput>
+              </HStack>
+            </FormControl>
+          </Step>
+        </Steps>
 
-          <FormControl isDisabled={!shouldShowPinEntry}>
-            <FormLabel>Verification code</FormLabel>
-            <HStack>
-              <PinInput
-                isDisabled={!shouldShowPinEntry}
-                value={field.value}
-                onChange={(value) => field.onChange(value)}
-              >
-                <PinInputField autoFocus />
-                <PinInputField />
-                <PinInputField />
-                <PinInputField />
-                <PinInputField />
-                <PinInputField />
-              </PinInput>
-            </HStack>
-          </FormControl>
-
-          <HStack w={"full"} justifyContent="end" spacing={4}>
-            {shouldShowPinEntry && (
-              <Button
-                variant="link"
-                opacity={0.75}
-                size="sm"
-                onClick={() => {
-                  setValue("code", "")
-                  resetVerificationRequest()
-                }}
-              >
-                Use different email
-              </Button>
-            )}
+        <VStack spacing={7} as="form" onSubmit={submit} mt={8}>
+          <HStack w={"full"} justifyContent="end">
             <Button
               isLoading={isVerificationRequestLoading}
               colorScheme="green"
