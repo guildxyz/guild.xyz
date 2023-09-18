77482e1d6bd2dcc14da6b906e1680b1a67c34320
BrickheadJohnny <johnny.zgen@gmail.com>
2023-09-18 19:28:15 +0200

    add DateRangeInput display logic

--- src/components/[guild]/activity/ActivityLogFiltersBar/ActivityLogFiltersBar.tsx
+++ src/components/[guild]/activity/ActivityLogFiltersBar/ActivityLogFiltersBar.tsx
@@ -1,20 +1,31 @@
 import { GridItem, SimpleGrid } from "@chakra-ui/react"
 import StickyBar from "components/common/Layout/StickyBar"
+import useGuild from "components/[guild]/hooks/useGuild"
+import { useActivityLog } from "../ActivityLogContext"
 import DateRangeInput from "./components/DateRangeInput"
 import FiltersInput from "./components/FiltersInput"
 
-const ActivityLogFiltersBar = (): JSX.Element => (
-  <StickyBar>
-    <SimpleGrid columns={3} gap={4}>
-      <GridItem colSpan={{ base: 3, md: 2 }}>
-        <FiltersInput />
-      </GridItem>
+const ActivityLogFiltersBar = (): JSX.Element => {
+  const { isUserActivityLog } = useActivityLog()
+  const { featureFlags } = useGuild()
 
-      <GridItem colSpan={{ base: 3, md: 1 }}>
-        <DateRangeInput />
-      </GridItem>
-    </SimpleGrid>
-  </StickyBar>
-)
+  const shouldShowDateRangeInput = isUserActivityLog || featureFlags?.includes("CRM")
+
+  return (
+    <StickyBar>
+      <SimpleGrid columns={3} gap={4}>
+        <GridItem colSpan={shouldShowDateRangeInput ? { base: 3, md: 2 } : 3}>
+          <FiltersInput />
+        </GridItem>
+
+        {shouldShowDateRangeInput && (
+          <GridItem colSpan={{ base: 3, md: 1 }}>
+            <DateRangeInput />
+          </GridItem>
+        )}
+      </SimpleGrid>
+    </StickyBar>
+  )
+}
 
 export default ActivityLogFiltersBar
