a22330c536f5dcddaf466b8ca2aa5eeea597409b
BrickheadJohnny <johnny.zgen@gmail.com>
2023-09-27 11:38:03 +0200

    useTextCardProps: update circle bg color

--- src/platforms/Text/useTextCardProps.tsx
+++ src/platforms/Text/useTextCardProps.tsx
@@ -1,16 +1,20 @@
-import { Circle, Icon } from "@chakra-ui/react"
+import { Circle, Icon, useColorModeValue } from "@chakra-ui/react"
 import platforms from "platforms/platforms"
 import { GuildPlatform, PlatformGuildData, PlatformName } from "types"
 
-const useTextCardProps = (guildPlatform: GuildPlatform) => ({
-  name: "Text",
-  type: "TEXT" as PlatformName,
-  image: (
-    <Circle size={10}>
-      <Icon as={platforms.TEXT.icon} boxSize={6} />
-    </Circle>
-  ),
-  info: (guildPlatform.platformGuildData as PlatformGuildData["TEXT"]).text,
-})
+const useTextCardProps = (guildPlatform: GuildPlatform) => {
+  const bgColor = useColorModeValue("gray.700", "gray.600")
+
+  return {
+    name: "Text",
+    type: "TEXT" as PlatformName,
+    image: (
+      <Circle size={10} bgColor={bgColor}>
+        <Icon as={platforms.TEXT.icon} boxSize={6} />
+      </Circle>
+    ),
+    info: (guildPlatform.platformGuildData as PlatformGuildData["TEXT"]).text,
+  }
+}
 
 export default useTextCardProps
