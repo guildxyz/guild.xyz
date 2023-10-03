b854142966d97398ae073f66701b48b848414010
BrickheadJohnny <johnny.zgen@gmail.com>
2023-10-03 15:09:39 +0200

    fix(TextReward): handle disabled states, add tooltip

--- src/platforms/Text/TextReward.tsx
+++ src/platforms/Text/TextReward.tsx
@@ -1,11 +1,17 @@
-import { Spinner, Text } from "@chakra-ui/react"
+import { Icon, Spinner, Text, Tooltip } from "@chakra-ui/react"
+import { useWeb3React } from "@web3-react/core"
 import Button from "components/common/Button"
+import useAccess from "components/[guild]/hooks/useAccess"
+import useGuild from "components/[guild]/hooks/useGuild"
+import useIsMember from "components/[guild]/hooks/useIsMember"
+import { useOpenJoinModal } from "components/[guild]/JoinModal/JoinModalProvider"
 import {
   RewardDisplay,
   RewardIcon,
   RewardProps,
 } from "components/[guild]/RoleCard/components/Reward"
-import { ArrowSquareOut } from "phosphor-react"
+import { ArrowSquareOut, LockSimple } from "phosphor-react"
+import { useMemo } from "react"
 import useClaimText, { ClaimTextModal } from "./hooks/useClaimText"
 
 const TextReward = ({ platform, withMotionImg }: RewardProps) => {
@@ -18,6 +24,37 @@ const TextReward = ({ platform, withMotionImg }: RewardProps) => {
     response,
     modalProps: { isOpen, onOpen, onClose },
   } = useClaimText(platform.id)
+  const { roles } = useGuild()
+  const role = roles.find((r) =>
+    r.rolePlatforms.some((rp) => rp.guildPlatformId === platform.guildPlatformId)
+  )
+
+  const isMember = useIsMember()
+  const { hasAccess, isValidating: isAccessValidating } = useAccess(role.id)
+  const { account } = useWeb3React()
+  const openJoinModal = useOpenJoinModal()
+
+  const state = useMemo(() => {
+    if (isMember && hasAccess)
+      return {
+        tooltipLabel: "Reveal secret",
+        buttonProps: {},
+      }
+    if (!account || (!isMember && hasAccess))
+      return {
+        tooltipLabel: (
+          <>
+            <Icon as={LockSimple} display="inline" mb="-2px" mr="1" />
+            Join guild to get access
+          </>
+        ),
+        buttonProps: { onClick: openJoinModal },
+      }
+    return {
+      tooltipLabel: "You don't satisfy the requirements to this role",
+      buttonProps: { isDisabled: true },
+    }
+  }, [isMember, hasAccess, account])
 
   return (
     <>
@@ -37,21 +74,24 @@ const TextReward = ({ platform, withMotionImg }: RewardProps) => {
           isLoading ? (
             <Text opacity={0.5}>Claiming reward...</Text>
           ) : (
-            <>
+            <Tooltip label={state.tooltipLabel} hasArrow shouldWrapChildren>
               {`Reveal secret: `}
               <Button
                 variant="link"
-                rightIcon={<ArrowSquareOut />}
+                rightIcon={
+                  isAccessValidating ? <Spinner boxSize="1em" /> : <ArrowSquareOut />
+                }
                 iconSpacing="1"
                 maxW="full"
                 onClick={() => {
                   onSubmit()
                   onOpen()
                 }}
+                {...state.buttonProps}
               >
                 {platformGuildData.name ?? "Secret"}
               </Button>
-            </>
+            </Tooltip>
           )
         }
       />
