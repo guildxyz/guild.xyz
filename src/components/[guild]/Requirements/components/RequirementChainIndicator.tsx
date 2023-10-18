14b7c1a63654e821ece26d9672e056ee6cda5ed9
valid <valid@zgen.hu>
2023-10-18 19:58:39 +0200

    UI(RequirementChainIndicator): less button-like appearance

--- src/components/[guild]/Requirements/components/RequirementChainIndicator.tsx
+++ src/components/[guild]/Requirements/components/RequirementChainIndicator.tsx
@@ -1,16 +1,17 @@
-import { Img, Tag, TagLabel } from "@chakra-ui/react"
+import { Img, Tag, TagLabel, useColorModeValue } from "@chakra-ui/react"
 import { RPC } from "connectors"
 import { useRequirementContext } from "./RequirementContext"
 
 const RequirementChainIndicator = () => {
   const { chain } = useRequirementContext()
+  const bg = useColorModeValue("white", "blackAlpha.300")
 
   const { chainName, iconUrls } = RPC[chain] ?? {}
 
   if (!chainName || !iconUrls?.length) return null
 
   return (
-    <Tag size="sm">
+    <Tag size="sm" bg={bg}>
       <Img src={iconUrls[0]} alt={chainName} boxSize={3} mr={1} />
       <TagLabel>{chainName}</TagLabel>
     </Tag>
