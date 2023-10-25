b6a95476ac29b10eb6b0866ede46ee9d66d114d0
valid <valid@zgen.hu>
2023-10-26 01:47:17 +0200

    add confirmation alert to delete campaign

--- src/components/[guild]/AccessHub/components/CampaignCards/components/CampaignCardMenu.tsx
+++ src/components/[guild]/AccessHub/components/CampaignCards/components/CampaignCardMenu.tsx
@@ -1,6 +1,19 @@
-import { Box, MenuItem, useColorModeValue, useDisclosure } from "@chakra-ui/react"
+import {
+  AlertDialogBody,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogOverlay,
+  Box,
+  MenuItem,
+  useColorModeValue,
+  useDisclosure,
+} from "@chakra-ui/react"
 import PlatformCardMenu from "components/[guild]/RolePlatforms/components/PlatformCard/components/PlatformCardMenu"
+import Button from "components/common/Button"
+import { Alert } from "components/common/Modal"
 import { PencilSimple, TrashSimple } from "phosphor-react"
+import { useRef } from "react"
 import useDeleteRoleGroup from "../hooks/useDeleteRoleGroup"
 import EditCampaignModal from "./EditCampaignModal"
 
@@ -10,10 +23,13 @@ type Props = {
 
 const CampaignCardMenu = ({ groupId }: Props) => {
   const { isOpen, onOpen, onClose } = useDisclosure()
+  const {
+    isOpen: isDeleteOpen,
+    onOpen: onDeleteOpen,
+    onClose: onDeleteClose,
+  } = useDisclosure()
 
   const removeMenuItemColor = useColorModeValue("red.600", "red.300")
-  const { onSubmit: onDeleteRoleGroup, isLoading: isDeleteRoleGroupLoading } =
-    useDeleteRoleGroup(groupId)
 
   return (
     <Box position="absolute" top={2} right={2}>
@@ -24,13 +40,17 @@ const CampaignCardMenu = ({ groupId }: Props) => {
         <MenuItem
           icon={<TrashSimple />}
           color={removeMenuItemColor}
-          isDisabled={isDeleteRoleGroupLoading}
-          onClick={() => onDeleteRoleGroup()}
+          onClick={onDeleteOpen}
         >
           Delete campaign
         </MenuItem>
       </PlatformCardMenu>
 
+      <DeleteCampaignAlert
+        isOpen={isDeleteOpen}
+        onClose={onDeleteClose}
+        groupId={groupId}
+      />
       <EditCampaignModal
         isOpen={isOpen}
         onClose={onClose}
@@ -40,4 +60,34 @@ const CampaignCardMenu = ({ groupId }: Props) => {
     </Box>
   )
 }
+
+const DeleteCampaignAlert = ({ groupId, isOpen, onClose }) => {
+  const { onSubmit: onDeleteRoleGroup, isLoading } = useDeleteRoleGroup(groupId)
+  const cancelRef = useRef(null)
+
+  return (
+    <Alert isOpen={isOpen} onClose={onClose} leastDestructiveRef={cancelRef}>
+      <AlertDialogOverlay />
+      <AlertDialogContent>
+        <AlertDialogHeader pb="6">Delete campaign</AlertDialogHeader>
+        <AlertDialogBody>
+          Are you sure to delete this campaign and all roles in it?
+        </AlertDialogBody>
+        <AlertDialogFooter display={"flex"} gap={2}>
+          <Button ref={cancelRef} onClick={onClose} variant="outline">
+            Cancel
+          </Button>
+          <Button
+            onClick={() => onDeleteRoleGroup()}
+            isLoading={isLoading}
+            colorScheme="red"
+          >
+            Delete campaign
+          </Button>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </Alert>
+  )
+}
+
 export default CampaignCardMenu
