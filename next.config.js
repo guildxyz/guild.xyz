bdc1f24c85099403b47edd52de306a9d9dc6cb2a
BrickheadJohnny <johnny.zgen@gmail.com>
2023-10-29 08:37:15 +0100

    fix: update config so OG generation works properly

--- next.config.js
+++ next.config.js
@@ -29,6 +29,7 @@ module.exports = {
         "crypto-addr-codec": false,
         "@ensdomains/address-encoder": false,
         "decode-named-character-reference": false,
+        isows: false,
       }
     }
 
