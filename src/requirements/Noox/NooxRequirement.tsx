26ac07e3a2218b3e3f436ce04131b80f2fa3f14b
BrickheadJohnny <johnny.zgen@gmail.com>
2023-11-11 11:11:43 +0100

    fix(NooxRequirement): get badge data from contract

--- src/requirements/Noox/NooxRequirement.tsx
+++ src/requirements/Noox/NooxRequirement.tsx
@@ -4,29 +4,52 @@ import Requirement, {
   RequirementProps,
 } from "components/[guild]/Requirements/components/Requirement"
 import { useRequirementContext } from "components/[guild]/Requirements/components/RequirementContext"
+import { nooxAbi } from "static/abis/noox"
 import useSWRImmutable from "swr/immutable"
-import { NooxBadge } from "./NooxForm"
+import { mainnet, useContractRead } from "wagmi"
+
+type NooxBadgeMetadata = {
+  name: string
+  image_thumbnail: string
+}
 
 const NooxRequirement = (props: RequirementProps) => {
   const requirement = useRequirementContext()
 
-  const { data, isValidating, error } = useSWRImmutable<NooxBadge[]>("/api/noox")
+  const {
+    data: ipfsURL,
+    isLoading: isContractCallLoading,
+    isError,
+  } = useContractRead({
+    abi: nooxAbi,
+    address: "0xf1c121a563a84d62a5f11152d064dd0d554024f9",
+    chainId: mainnet.id,
+    functionName: "uri",
+    args: [BigInt(requirement.data.id)],
+  })
 
-  const badgeData = data?.find((badge) => badge.id === requirement.data.id)
+  const { data: badgeData, isValidating } =
+    useSWRImmutable<NooxBadgeMetadata>(ipfsURL)
+
+  const isLoading = isContractCallLoading || isValidating
 
   return (
-    <Requirement image={badgeData?.image} isImageLoading={isValidating} {...props}>
+    <Requirement
+      image={badgeData?.image_thumbnail?.replace("ipfs://", "https://ipfs.io/ipfs/")}
+      isImageLoading={isLoading}
+      {...props}
+    >
       {`Have the `}
-      {!badgeData || isValidating || error ? (
+      {!badgeData || isLoading || isError ? (
         <DataBlock
-          isLoading={isValidating}
-          error={error && "API error, please contact Noox to report."}
+          isLoading={isLoading}
+          error={isError && "Couldn't fetch Noox badge data"}
         >
           {`#${requirement.data.id}`}
         </DataBlock>
       ) : (
         <Link
-          href={`https://noox.world/badge/${badgeData?.id}`}
+          href={`https://noox.world/badge/${requirement.data.id}`}
           isExternal
           display="inline"
           colorScheme="blue"
