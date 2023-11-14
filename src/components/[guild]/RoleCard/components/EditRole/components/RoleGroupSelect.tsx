a0a3866ff9a2214dd4cbea5d4ce4bfdb1525899c
BrickheadJohnny <johnny.zgen@gmail.com>
2023-11-14 17:16:24 +0100

    feat(EditRole): add `RoleGroupSelect` component

---
+++ src/components/[guild]/RoleCard/components/EditRole/components/RoleGroupSelect.tsx
@@ -0,0 +1,44 @@
+import { FormControl, FormLabel, Select } from "@chakra-ui/react"
+import useGuild from "components/[guild]/hooks/useGuild"
+import { useController } from "react-hook-form"
+import { RoleEditFormData } from "../EditRole"
+
+const RoleGroupSelect = () => {
+  const { groups } = useGuild()
+
+  const {
+    field: { onChange, value, ...rest },
+  } = useController<RoleEditFormData, "groupId">({
+    name: "groupId",
+  })
+
+  if (!groups.length) return null
+
+  return (
+    <FormControl>
+      <FormLabel>Add to page</FormLabel>
+      <Select
+        w={{ base: "full", sm: "52" }}
+        {...rest}
+        value={value ?? ""}
+        onChange={(e) => {
+          let valueAsNumber
+
+          try {
+            valueAsNumber = parseInt(e.target.value)
+          } catch {}
+
+          onChange(!isNaN(valueAsNumber) ? valueAsNumber : null)
+        }}
+      >
+        <option>Home page</option>
+        {groups.map((group) => (
+          <option key={group.id} value={group.id}>
+            {group.name}
+          </option>
+        ))}
+      </Select>
+    </FormControl>
+  )
+}
+export default RoleGroupSelect
