15b72367a785c45f3a9406b5a52d507f72f82c4e
vm_rc_ <marci@zgen.hu>
2023-11-15 20:47:39 +0100

    fix: clickable intercom on wide screen

--- src/components/create-guild/GuildCreationProgress/GuildCreationProgress.tsx
+++ src/components/create-guild/GuildCreationProgress/GuildCreationProgress.tsx
@@ -27,38 +27,45 @@ const GuildCreationProgress = ({
       zIndex={1201} // above intercom floating button
     >
       <Container maxW={"container.lg"} px={{ base: 0, md: 8, lg: 10 }}>
-        <Card
-          borderRadius={0}
-          borderTopRadius={{ md: "2xl" }}
-          borderWidth={{ base: "1px 0 0 0", md: "1px 1px 0 1px" }}
-          shadow={"rgba(0, 0, 0, 0.1) 0px 5px 10px,rgba(0, 0, 0, 0.2) 0px 15px 40px"}
-        >
-          <HStack justify={"space-between"} py={3} px={{ base: 2, md: 3 }}>
-            <HStack gap={4} px={3}>
-              <GuildLottieProgress progress={progress} />
-              <Text colorScheme="gray" fontWeight={"semibold"} fontSize={"sm"}>
-                Guild {progressText} completed
-              </Text>
+        <Box position="relative">
+          <Card
+            borderRadius={0}
+            borderTopRadius={{ md: "2xl" }}
+            borderWidth={{ base: "1px 0 0 0", md: "1px 1px 0 1px" }}
+            shadow={
+              "rgba(0, 0, 0, 0.1) 0px 5px 10px,rgba(0, 0, 0, 0.2) 0px 15px 40px"
+            }
+            position="absolute"
+            bottom={0}
+            w="full"
+          >
+            <HStack justify={"space-between"} py={3} px={{ base: 2, md: 3 }}>
+              <HStack gap={4} px={3}>
+                <GuildLottieProgress progress={progress} />
+                <Text colorScheme="gray" fontWeight={"semibold"} fontSize={"sm"}>
+                  Guild {progressText} completed
+                </Text>
+              </HStack>
+              {children ?? (
+                <Button colorScheme={"green"} onClick={next} isDisabled={isDisabled}>
+                  Continue
+                </Button>
+              )}
             </HStack>
-            {children ?? (
-              <Button colorScheme={"green"} onClick={next} isDisabled={isDisabled}>
-                Continue
-              </Button>
-            )}
-          </HStack>
-          <Progress
-            borderRadius="full"
-            h={1}
-            w="100%"
-            value={progress}
-            colorScheme="primary"
-            sx={{
-              "& > div:first-child": {
-                transitionProperty: "width",
-              },
-            }}
-          />
-        </Card>
+            <Progress
+              borderRadius="full"
+              h={1}
+              w="100%"
+              value={progress}
+              colorScheme="primary"
+              sx={{
+                "& > div:first-child": {
+                  transitionProperty: "width",
+                },
+              }}
+            />
+          </Card>
+        </Box>
       </Container>
     </Box>
   )
