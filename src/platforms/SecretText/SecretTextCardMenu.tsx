5b4132c784ecffc31d27a6b9b69980202aec6e22
BrickheadJohnny <johnny.zgen@gmail.com>
2023-11-23 17:56:43 +0100

    feat: EditRewardAvailibilityMenuItem component

--- src/platforms/SecretText/SecretTextCardMenu.tsx
+++ src/platforms/SecretText/SecretTextCardMenu.tsx
@@ -1,11 +1,8 @@
 import { MenuItem, useDisclosure } from "@chakra-ui/react"
+import EditRewardAvailibilityMenuItem from "components/[guild]/AccessHub/components/EditRewardAvailibilityMenuItem"
 import RemovePlatformMenuItem from "components/[guild]/AccessHub/components/RemovePlatformMenuItem"
-import useEditRolePlatform from "components/[guild]/AccessHub/hooks/useEditRolePlatform"
-import EditRolePlatformCapacityTimeModal from "components/[guild]/RolePlatforms/components/EditRolePlatformCapacityTimeModal"
 import useGuild from "components/[guild]/hooks/useGuild"
-import useToast from "hooks/useToast"
-import { Clock, PencilSimple } from "phosphor-react"
-import { PlatformName, PlatformType } from "types"
+import { PencilSimple } from "phosphor-react"
 import PlatformCardMenu from "../../components/[guild]/RolePlatforms/components/PlatformCard/components/PlatformCardMenu"
 import EditSecretTextModal from "./EditSecretTextModal"
 
@@ -14,33 +11,12 @@ type Props = {
 }
 
 const SecretTextCardMenu = ({ platformGuildId }: Props): JSX.Element => {
-  const { guildPlatforms, roles } = useGuild()
+  const { guildPlatforms } = useGuild()
   const guildPlatform = guildPlatforms?.find(
     (gp) => gp.platformGuildId === platformGuildId
   )
-  const rolePlatform = roles
-    .flatMap((role) => role.rolePlatforms)
-    .find((rp) => rp.guildPlatformId === guildPlatform?.id)
 
   const { isOpen, onOpen, onClose } = useDisclosure()
-  const {
-    isOpen: isCapacityTimeOpen,
-    onOpen: onCapacityTimeOpen,
-    onClose: onCapacityTimeClose,
-  } = useDisclosure()
-
-  const toast = useToast()
-
-  const { onSubmit, isLoading } = useEditRolePlatform({
-    rolePlatformId: rolePlatform?.id,
-    onSuccess: () => {
-      toast({
-        status: "success",
-        title: "Successfully updated reward",
-      })
-      onCapacityTimeClose()
-    },
-  })
 
   return (
     <>
@@ -48,11 +24,7 @@ const SecretTextCardMenu = ({ platformGuildId }: Props): JSX.Element => {
         <MenuItem icon={<PencilSimple />} onClick={onOpen}>
           Edit secret
         </MenuItem>
-        {rolePlatform && (
-          <MenuItem icon={<Clock />} onClick={onCapacityTimeOpen}>
-            Edit availibility
-          </MenuItem>
-        )}
+        <EditRewardAvailibilityMenuItem platformGuildId={platformGuildId} />
         <RemovePlatformMenuItem platformGuildId={platformGuildId} />
       </PlatformCardMenu>
 
@@ -61,19 +33,6 @@ const SecretTextCardMenu = ({ platformGuildId }: Props): JSX.Element => {
         onClose={onClose}
         guildPlatformId={guildPlatform?.id}
         platformGuildData={guildPlatform?.platformGuildData}
-      />
-
-      <EditRolePlatformCapacityTimeModal
-        isOpen={isCapacityTimeOpen}
-        onClose={onCapacityTimeClose}
-        platformType={PlatformType[guildPlatform.platformId] as PlatformName}
-        defaultValues={{
-          capacity: rolePlatform?.capacity,
-          startTime: rolePlatform?.startTime,
-          endTime: rolePlatform?.endTime,
-        }}
-        isLoading={isLoading}
-        onDone={onSubmit}
       />
     </>
   )
--- src/platforms/UniqueText/UniqueTextCardMenu.tsx
+++ src/platforms/UniqueText/UniqueTextCardMenu.tsx
@@ -1,11 +1,8 @@
 import { MenuItem, useDisclosure } from "@chakra-ui/react"
+import EditRewardAvailibilityMenuItem from "components/[guild]/AccessHub/components/EditRewardAvailibilityMenuItem"
 import RemovePlatformMenuItem from "components/[guild]/AccessHub/components/RemovePlatformMenuItem"
-import useEditRolePlatform from "components/[guild]/AccessHub/hooks/useEditRolePlatform"
-import EditRolePlatformCapacityTimeModal from "components/[guild]/RolePlatforms/components/EditRolePlatformCapacityTimeModal"
 import useGuild from "components/[guild]/hooks/useGuild"
-import useToast from "hooks/useToast"
-import { Clock, PencilSimple } from "phosphor-react"
-import { PlatformName, PlatformType } from "types"
+import { PencilSimple } from "phosphor-react"
 import PlatformCardMenu from "../../components/[guild]/RolePlatforms/components/PlatformCard/components/PlatformCardMenu"
 import EditUniqueTextModal from "./EditUniqueTextModal"
 
@@ -14,34 +11,12 @@ type Props = {
 }
 
 const UniqueTextCardMenu = ({ platformGuildId }: Props): JSX.Element => {
-  const { guildPlatforms, roles } = useGuild()
+  const { guildPlatforms } = useGuild()
   const guildPlatform = guildPlatforms?.find(
     (gp) => gp.platformGuildId === platformGuildId
   )
-  const rolePlatform = roles
-    .flatMap((role) => role.rolePlatforms)
-    .find((rp) => rp.guildPlatformId === guildPlatform?.id)
 
   const { isOpen, onOpen, onClose } = useDisclosure()
-
-  const {
-    isOpen: isCapacityTimeOpen,
-    onOpen: onCapacityTimeOpen,
-    onClose: onCapacityTimeClose,
-  } = useDisclosure()
-
-  const toast = useToast()
-
-  const { onSubmit, isLoading } = useEditRolePlatform({
-    rolePlatformId: rolePlatform?.id,
-    onSuccess: () => {
-      toast({
-        status: "success",
-        title: "Successfully updated reward",
-      })
-      onCapacityTimeClose()
-    },
-  })
 
   return (
     <>
@@ -49,9 +24,7 @@ const UniqueTextCardMenu = ({ platformGuildId }: Props): JSX.Element => {
         <MenuItem icon={<PencilSimple />} onClick={onOpen}>
           Edit unique secret
         </MenuItem>
-        <MenuItem icon={<Clock />} onClick={onCapacityTimeOpen}>
-          Edit availibility
-        </MenuItem>
+        <EditRewardAvailibilityMenuItem platformGuildId={platformGuildId} />
         <RemovePlatformMenuItem platformGuildId={platformGuildId} />
       </PlatformCardMenu>
 
@@ -60,19 +33,6 @@ const UniqueTextCardMenu = ({ platformGuildId }: Props): JSX.Element => {
         onClose={onClose}
         guildPlatformId={guildPlatform?.id}
         platformGuildData={guildPlatform?.platformGuildData}
-      />
-
-      <EditRolePlatformCapacityTimeModal
-        isOpen={isCapacityTimeOpen}
-        onClose={onCapacityTimeClose}
-        platformType={PlatformType[guildPlatform.platformId] as PlatformName}
-        defaultValues={{
-          capacity: rolePlatform?.capacity,
-          startTime: rolePlatform?.startTime,
-          endTime: rolePlatform?.endTime,
-        }}
-        isLoading={isLoading}
-        onDone={onSubmit}
       />
     </>
   )
