f6e67e5616b13c9543e4373335f7d76c57124d08
valid <valid@zgen.hu>
2023-11-27 17:22:09 +0100

    fix: don't show OnboardingMarkers incorrectly

--- src/components/[guild]/Onboarding/Onboarding.tsx
+++ src/components/[guild]/Onboarding/Onboarding.tsx
@@ -2,15 +2,20 @@ import { Collapse } from "@chakra-ui/react"
 import CreateGuildStepper from "components/create-guild/CreateGuildStepper"
 import GuildCreationProgress from "components/create-guild/GuildCreationProgress"
 import { atom, useAtom } from "jotai"
+import { useEffect } from "react"
 import { useThemeContext } from "../ThemeContext"
 import useGuild from "../hooks/useGuild"
 
-export const onboardingStepAtom = atom(3)
+export const onboardingStepAtom = atom(0)
 
 const Onboarding = (): JSX.Element => {
   const { onboardingComplete } = useGuild()
   const { localThemeColor, textColor } = useThemeContext()
   const [activeStep, setActiveStep] = useAtom(onboardingStepAtom)
+
+  useEffect(() => {
+    setActiveStep(3)
+  }, [])
 
   return (
     <>
