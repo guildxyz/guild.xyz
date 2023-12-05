306af0c2a546f562d6e21af9c8452369bc30ea04
BrickheadJohnny <johnny.zgen@gmail.com>
2023-12-05 10:29:19 +0100

    fix: WalletConnect modal z-index

--- src/connectors.ts
+++ src/connectors.ts
@@ -53,8 +53,9 @@ const connectors = process.env.NEXT_PUBLIC_MOCK_CONNECTOR
               "4622a2b2d6af1c9844944291e5e7351a6aa24cd7b23099efac1b2fd875da31a0", // Trust
             ],
             themeVariables: {
+              "--wcm-z-index": "10001",
               "--w3m-z-index": "10001",
-            },
+            } as any, // casting it, so `--wcm-z-index` is accepted
           },
         },
       }),
