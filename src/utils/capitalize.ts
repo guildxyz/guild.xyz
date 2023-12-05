ecf415b8692b2c49c59f0e6762ee3ff1555edc01
BrickheadJohnny <johnny.zgen@gmail.com>
2023-12-05 16:24:28 +0100

    temp fix for `capitalize` util function

--- src/utils/capitalize.ts
+++ src/utils/capitalize.ts
@@ -1,5 +1,6 @@
 const capitalize = (text: string) => {
-  if (text.length > 1) {
+  // Temporary solition, because the audit log api doesn't work properly on dev infra - we should revert this change before merging the PR
+  if (text?.length > 1) {
     return text.charAt(0).toUpperCase() + text.slice(1)
   }
 
