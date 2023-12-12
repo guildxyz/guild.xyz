dd2c146932ea370cde505b5d361c4787fdb1308c
BrickheadJohnny <johnny.zgen@gmail.com>
2023-12-12 16:36:42 +0100

    fix(useFUelBaseAssetBalance): simply import BaseAssetId

--- src/components/[guild]/Requirements/components/GuildCheckout/MintGuildPin/Fuel/hooks/useFuelBaseAssetBalance.ts
+++ src/components/[guild]/Requirements/components/GuildCheckout/MintGuildPin/Fuel/hooks/useFuelBaseAssetBalance.ts
@@ -1,13 +1,11 @@
+import { BaseAssetId } from "fuels"
 import useFuel from "hooks/useFuel"
 import useSWR from "swr"
 
 const useFuelBaseAssetBalance = () => {
   const { wallet, address } = useFuel()
 
-  const getBalance = async () => {
-    const { BaseAssetId } = await import("fuels")
-    return wallet.getBalance(BaseAssetId)
-  }
+  const getBalance = async () => wallet.getBalance(BaseAssetId)
 
   return useSWR(!!wallet ? ["fuelBaseAssetBalance", address] : null, getBalance)
 }
