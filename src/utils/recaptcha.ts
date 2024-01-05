4e65aacd3dfdf17d7e500f6069a01ebcb71b0952
Csete Balázs <csetebalu@gmail.com>
2024-01-05 12:03:57 +0100

    remove unused getRecaptchaToken

--- src/utils/recaptcha.ts
+++ src/utils/recaptcha.ts
@@ -1,21 +1,4 @@
 import { atom } from "jotai"
-import { MutableRefObject } from "react"
 import ReCAPTCHA from "react-google-recaptcha"
-
-export const getRecaptchaToken = async (
-  recaptchaRef: MutableRefObject<ReCAPTCHA>,
-  isVerified = false
-) => {
-  const token =
-    !recaptchaRef.current || isVerified
-      ? undefined
-      : await recaptchaRef.current.executeAsync()
-
-  if (token) {
-    recaptchaRef.current.reset()
-  }
-
-  return token
-}
 
 export const recaptchaAtom = atom(null as ReCAPTCHA)
