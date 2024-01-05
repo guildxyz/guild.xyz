788152dae15d19a4bf3a5acc839fb62225e76687
Csete Balázs <csetebalu@gmail.com>
2024-01-05 12:01:08 +0100

    revert AccountConnections.tsx changes

--- src/components/common/Layout/components/Account/components/AccountModal/components/AccountConnections.tsx
+++ src/components/common/Layout/components/Account/components/AccountModal/components/AccountConnections.tsx
@@ -4,16 +4,6 @@ import {
   Heading,
   HStack,
   Icon,
-  IconButton,
-  Img,
-  Input,
-  Modal,
-  ModalBody,
-  ModalCloseButton,
-  ModalContent,
-  ModalFooter,
-  ModalHeader,
-  ModalOverlay,
   Popover,
   PopoverArrow,
   PopoverBody,
@@ -22,26 +12,15 @@ import {
   Spacer,
   Stack,
   Text,
-  Tooltip,
   useColorModeValue,
-  useDisclosure,
 } from "@chakra-ui/react"
-import type { UserAddress } from "@guildxyz/types"
-import useUser, { useUserPublic } from "components/[guild]/hooks/useUser"
+import useUser from "components/[guild]/hooks/useUser"
 import useWeb3ConnectionManager from "components/_app/Web3ConnectionManager/hooks/useWeb3ConnectionManager"
-import Button from "components/common/Button"
 import { SectionProps } from "components/common/Section"
-import { randomBytes } from "crypto"
-import useSubmit from "hooks/useSubmit"
-import useToast from "hooks/useToast"
 import { Question } from "phosphor-react"
 import platforms from "platforms/platforms"
-import { useMemo, useState } from "react"
-import { mutate } from "swr"
+import { useMemo } from "react"
 import { PlatformName } from "types"
-import { useFetcherWithSign } from "utils/fetcher"
-import type { CWaaSConnector } from "waasConnector"
-import { useConnect } from "wagmi"
 import useDelegateVaults from "../../delegate/useDelegateVaults"
 import LinkAddressButton from "./LinkAddressButton"
 import LinkDelegateVaultButton from "./LinkDelegateVaultButton"
@@ -53,7 +32,6 @@ const AccountConnections = () => {
   const { isLoading, addresses, platformUsers, sharedSocials } = useUser()
   const { address, type } = useWeb3ConnectionManager()
   const vaults = useDelegateVaults()
-  const { id: userId } = useUserPublic()
 
   const orderedSocials = useMemo(() => {
     const connectedPlatforms =
@@ -79,62 +57,7 @@ const AccountConnections = () => {
   const linkedAddresses = addresses?.filter(
     (addr) =>
       (typeof addr === "string" ? addr : addr?.address)?.toLowerCase() !==
-      address?.toLowerCase()
-  )
-
-  const coinbaseRestoreModal = useDisclosure()
-  const [backupData, setBackupData] = useState("")
-  const { connectors, connect } = useConnect()
-  const fetcherWithSign = useFetcherWithSign()
-  const toast = useToast()
-  const restore = useSubmit(
-    async () => {
-      const cwaasConnector = connectors.find(
-        ({ id }) => id === "cwaasWallet"
-      ) as CWaaSConnector
-
-      const { account } = await cwaasConnector.restoreWallet(backupData)
-
-      const timestamp = Date.now()
-      const nonce = randomBytes(32).toString("hex")
-      const addr = account.address.toLowerCase()
-      const message = `Address: ${addr}\nNonce: ${nonce}\n Timestamp: ${timestamp}`
-      const signature = await account.signMessage({
-        message,
-      })
-
-      const body = {
-        address: addr,
-        signature,
-        nonce,
-        timestamp,
-        isPrimary: true,
-        isDelegated: false,
-      }
-
-      const userAddress: UserAddress = await fetcherWithSign([
-        `/v2/users/${userId}/addresses`,
-        { method: "POST", body },
-      ])
-
-      await mutate(
-        [`/v2/users/${userId}/profile`, { method: "GET", body: {} }],
-        (prev) => ({
-          ...prev,
-          addresses: [userAddress],
-        }),
-        { revalidate: false }
-      )
-
-      connect({ connector: cwaasConnector })
-    },
-    {
-      onSuccess: () => {
-        toast({ status: "success", title: "Wallet restored!" })
-        coinbaseRestoreModal.onClose()
-        setBackupData("")
-      },
-    }
+      address.toLowerCase()
   )
 
   return (
@@ -198,62 +121,7 @@ const AccountConnections = () => {
                 <LinkDelegateVaultButton vaults={vaults} />
               </ButtonGroup>
             ) : (
-              <>
-                <ButtonGroup isAttached>
-                  <LinkAddressButton />
-
-                  <Divider
-                    orientation="vertical"
-                    borderColor="whiteAlpha.500"
-                    h="8"
-                  />
-
-                  <Tooltip label="Restore and link Coinbase wallet">
-                    <IconButton
-                      onClick={() => {
-                        coinbaseRestoreModal.onOpen()
-                      }}
-                      size={"sm"}
-                      icon={
-                        <Img w={5} h={5} src="/walletLogos/coinbasewallet.png" />
-                      }
-                      aria-label="Restore and link Coinbase wallet"
-                    />
-                  </Tooltip>
-                </ButtonGroup>
-                <Modal
-                  isOpen={coinbaseRestoreModal.isOpen}
-                  onClose={coinbaseRestoreModal.onClose}
-                >
-                  <ModalOverlay />
-                  <ModalContent>
-                    <ModalHeader>Restore and link Coinbase wallet</ModalHeader>
-                    <ModalCloseButton />
-                    <ModalBody>
-                      <Stack>
-                        <Text>
-                          Paste the backup data of the Coinbase wallet here
-                        </Text>
-                        <Input
-                          value={backupData}
-                          onChange={({ target: { value } }) => setBackupData(value)}
-                        />
-                      </Stack>
-                    </ModalBody>
-                    <ModalFooter>
-                      <Button
-                        colorScheme="green"
-                        isDisabled={!backupData}
-                        isLoading={restore.isLoading}
-                        loadingText="Restoring"
-                        onClick={restore.onSubmit}
-                      >
-                        Restore
-                      </Button>
-                    </ModalFooter>
-                  </ModalContent>
-                </Modal>
-              </>
+              <LinkAddressButton />
             )}
           </Stack>
         ) : (
