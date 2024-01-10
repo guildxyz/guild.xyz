e1a0eee8ab816e690ef29be1dec69e4434629273
Gabor Nemeth <gabor@zgen.hu>
2024-01-10 13:51:45 +0100

    feat: create EditRewardModal

---
+++ src/components/[guild]/RoleCard/components/EditRewardDescription/EditRewardModalContent.tsx
@@ -0,0 +1,79 @@
+import React from "react"
+import useToast from "../../../../../hooks/useToast"
+import { Controller, FormProvider, useForm } from "react-hook-form"
+import useEditGuildPlatform from "../../../AccessHub/hooks/useEditGuildPlatform"
+import {
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  ModalBody,
+  ModalCloseButton,
+  ModalFooter,
+  ModalHeader,
+} from "@chakra-ui/react"
+import RichTextDescriptionEditor from "../../../RolePlatforms/components/AddRoleRewardModal/components/AddContractCallPanel/components/CreateNftForm/components/RichTextDescriptionEditor"
+import Button from "../../../../common/Button"
+import { GuildPlatform } from "../../../../../types"
+
+type Props = {
+  guildPlatform: GuildPlatform
+  onClose: () => void
+}
+
+export const EditRewardModalContent: React.FC<Props> = ({
+  guildPlatform: { id, platformGuildData },
+  onClose,
+}) => {
+  const methods = useForm({ defaultValues: { ...platformGuildData } })
+  const { formState, control, handleSubmit } = methods
+
+  const toast = useToast()
+
+  const { onSubmit, isLoading } = useEditGuildPlatform({
+    guildPlatformId: id,
+    onSuccess: () => {
+      toast({
+        status: "success",
+        title: "Successfully updated reward description",
+      })
+      onClose()
+    },
+  })
+
+  return (
+    <FormProvider {...methods}>
+      <ModalCloseButton />
+      <ModalHeader>{platformGuildData.name}</ModalHeader>
+      <ModalBody>
+        <FormControl isInvalid={!!formState.errors?.description}>
+          <FormLabel>Reward description</FormLabel>
+          <Controller
+            name={"description"}
+            control={control}
+            rules={{ required: { value: true, message: "This field is required" } }}
+            render={({ field }) => (
+              <RichTextDescriptionEditor
+                minHeight={"100px"}
+                defaultValue={field.value}
+                onChange={field.onChange}
+              />
+            )}
+          />
+          <FormErrorMessage>
+            {formState.errors?.description?.message}
+          </FormErrorMessage>
+        </FormControl>
+      </ModalBody>
+      <ModalFooter>
+        <Button
+          colorScheme={"green"}
+          isDisabled={!formState.isDirty || !formState.isValid}
+          onClick={handleSubmit((data) => onSubmit({ platformGuildData: data }))}
+          isLoading={isLoading}
+        >
+          Save
+        </Button>
+      </ModalFooter>
+    </FormProvider>
+  )
+}
---
+++ src/components/[guild]/RoleCard/components/EditRewardDescription/index.ts
@@ -0,0 +1,3 @@
+import { EditRewardDescriptionModal } from "./EditRewardDescriptionModal"
+
+export default EditRewardDescriptionModal
