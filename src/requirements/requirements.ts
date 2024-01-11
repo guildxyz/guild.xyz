cf00fdb258f98724aaa0448af27c4a45e239f446
BrickheadJohnny <johnny.zgen@gmail.com>
2024-01-11 17:33:21 +0100

    pid -> PolygonID

--- src/requirements/requirements.ts
+++ src/requirements/requirements.ts
@@ -274,10 +274,10 @@ export const REQUIREMENTS_DATA = [
     icon: "/requirementLogos/polygonId.svg",
     name: "PolygonID",
     displayComponent: dynamic<RequirementProps>(
-      () => import("requirements/pid/PolygonIDRequirement")
+      () => import("requirements/PolygonID/PolygonIDRequirement")
     ),
     formComponent: dynamic<RequirementFormProps>(
-      () => import("requirements/pid/PolygonIDForm")
+      () => import("requirements/PolygonID/PolygonIDForm")
     ),
     types: ["POLYGON_ID_QUERY", "POLYGON_ID_BASIC"],
     isCustomizable: true,
