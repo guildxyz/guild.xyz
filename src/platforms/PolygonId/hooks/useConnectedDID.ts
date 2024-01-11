1bee965a7932b18fe2f11484d53528c34da09153
BrickheadJohnny <johnny.zgen@gmail.com>
2024-01-11 13:21:14 +0100

    useConnectedDID cleanup

--- src/platforms/PolygonId/hooks/useConnectedDID.ts
+++ src/platforms/PolygonId/hooks/useConnectedDID.ts
@@ -3,19 +3,15 @@ import useSWRImmutable from "swr/immutable"
 
 const useConnectedDID = () => {
   const { id: userId } = useUser()
-  const did = useSWRImmutable<string>(
+
+  return useSWRImmutable<string>(
     userId
       ? `${process.env.NEXT_PUBLIC_POLYGONID_API}/v1/users/${userId}/polygon-id`
       : null,
-    null,
     {
-      onErrorRetry: (err) => {
-        if (err.status === 500) return
-      },
+      shouldRetryOnError: false,
     }
   )
-
-  return did
 }
 
 export default useConnectedDID
