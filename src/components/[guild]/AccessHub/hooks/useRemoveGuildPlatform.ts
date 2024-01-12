e233c2762d702b40ebe0aa2371a438e0d03f517f
Gabor <gabor@zgen.hu>
2024-01-12 11:41:58 +0100

    fix: removed prop that removes the opened modal

--- src/components/[guild]/AccessHub/hooks/useRemoveGuildPlatform.ts
+++ src/components/[guild]/AccessHub/hooks/useRemoveGuildPlatform.ts
@@ -1,17 +1,13 @@
 import useGuild from "components/[guild]/hooks/useGuild"
-import useGateables from "hooks/useGateables"
 import useShowErrorToast from "hooks/useShowErrorToast"
 import { SignedValdation, useSubmitWithSign } from "hooks/useSubmit"
 import useToast from "hooks/useToast"
 import fetcher from "utils/fetcher"
 
 const useRemoveGuildPlatform = (guildPlatformId: number) => {
-  const { id, guildPlatforms, mutateGuild } = useGuild()
+  const { id, mutateGuild } = useGuild()
   const toast = useToast()
   const showErrorToast = useShowErrorToast()
-  const { mutate: mutateGateables } = useGateables(
-    guildPlatforms?.find((gp) => gp.id === guildPlatformId)?.platformId
-  )
 
   const submit = async (signedValidation: SignedValdation) =>
     fetcher(`/v2/guilds/${id}/guild-platforms/${guildPlatformId}`, {
@@ -46,8 +42,6 @@ const useRemoveGuildPlatform = (guildPlatformId: number) => {
         }),
         { revalidate: false }
       )
-
-      mutateGateables()
     },
     onError: (error) => showErrorToast(error),
   })
