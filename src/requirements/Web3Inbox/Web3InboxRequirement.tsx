e8519df12339197eb1e01e71861168a94e5a30fb
BrickheadJohnny <92519134+BrickheadJohnny@users.noreply.github.com>
2024-01-15 20:34:35 +0100

    feat: `WEB3INBOX_SUBSCRIBERS` requirement type (#1009)
    * feat: `WEB3INBOX_SUBSCRIBERS` requirement type
    
    * feat: add "Subscribe" button to requirements
    
    * fix: adjust icon spacing on the subscribe button
    
    * update copy

---
+++ src/requirements/Web3Inbox/Web3InboxRequirement.tsx
@@ -0,0 +1,50 @@
+import { Link, Text } from "@chakra-ui/react"
+import Requirement, {
+  RequirementProps,
+} from "components/[guild]/Requirements/components/Requirement"
+import { useRequirementContext } from "components/[guild]/Requirements/components/RequirementContext"
+import useAccess from "components/[guild]/hooks/useAccess"
+import Button from "components/common/Button"
+import { ArrowSquareOut } from "phosphor-react"
+import { APP_DETAILS } from "./Web3InboxForm"
+
+const Web3InboxRequirement = (props: RequirementProps) => {
+  const { id, roleId, data } = useRequirementContext()
+  const { data: roleAccess, isValidating } = useAccess(roleId)
+  const hasAccess = roleAccess?.requirements.find(
+    (req) => req.requirementId === id
+  )?.access
+
+  return (
+    <Requirement
+      image={APP_DETAILS[data.app].image}
+      footer={
+        !isValidating &&
+        !hasAccess && (
+          <Link
+            href="https://app.web3inbox.com"
+            isExternal
+            _hover={{
+              textDecoration: "none",
+            }}
+          >
+            <Button
+              size="xs"
+              colorScheme="WEB3INBOX"
+              rightIcon={<ArrowSquareOut />}
+              iconSpacing={1}
+            >
+              Subscribe
+            </Button>
+          </Link>
+        )
+      }
+      {...props}
+    >
+      <Text as="span">
+        {`Subscribe to ${APP_DETAILS[data.app].name} app on Web3Inbox`}
+      </Text>
+    </Requirement>
+  )
+}
+export default Web3InboxRequirement
