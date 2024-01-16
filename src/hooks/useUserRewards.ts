f9ec879b6ea4c3fcacf948efbe95fce2025d759f
Gabor <gabor@zgen.hu>
2024-01-16 18:17:37 +0100

    fix: add UserReward type

--- src/hooks/useUserRewards.ts
+++ src/hooks/useUserRewards.ts
@@ -2,6 +2,14 @@ import { useFetcherWithSign } from "../utils/fetcher"
 import useSWR from "swr"
 import useGuild from "../components/[guild]/hooks/useGuild"
 import useUser from "../components/[guild]/hooks/useUser"
+import { RolePlatform } from "../types"
+
+export type UserReward = {
+  claimed: boolean
+  id: number
+  rolePlatformId: RolePlatform["id"]
+  uniqueValue: string | null
+}
 
 export const useUserRewards = () => {
   const { id: guildId } = useGuild()
@@ -17,7 +25,7 @@ export const useUserRewards = () => {
       },
     ])
 
-  return useSWR<any[]>(
+  return useSWR<UserReward[]>(
     userId && guildId ? `/v2/users/${userId}/rewards?guildId=${guildId}` : null,
     fetchUsersRewards
   )
