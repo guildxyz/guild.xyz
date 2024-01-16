e2a9aa64096cbaeb20d3081094ecec06a233da75
Gabor <gabor@zgen.hu>
2024-01-16 18:23:05 +0100

    fix: remove unnecessary ternary operator

--- src/hooks/useIsRewardClaimed.ts
+++ src/hooks/useIsRewardClaimed.ts
@@ -4,9 +4,9 @@ import { useUserRewards } from "./useUserRewards"
 export const useIsRewardClaimed = (rolePlatformId: RolePlatform["id"]) => {
   const { isLoading, data: userRewards } = useUserRewards()
 
-  const claimed = !isLoading
-    ? userRewards?.find((reward) => reward.id === rolePlatformId)?.claimed
-    : undefined
+  const claimed = userRewards?.find(
+    (reward) => reward.id === rolePlatformId
+  )?.claimed
 
   return { isLoading, claimed }
 }
