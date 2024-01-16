504dd574e4741358372774732dabdd99b797dae9
Gabor <gabor@zgen.hu>
2024-01-16 16:35:46 +0100

    feat: Create hook to get users claimed reward list

---
+++ src/hooks/useGetUserRewards.ts
@@ -0,0 +1,24 @@
+import { useFetcherWithSign } from "../utils/fetcher"
+import useSWR from "swr"
+import useGuild from "../components/[guild]/hooks/useGuild"
+import useUser from "../components/[guild]/hooks/useUser"
+
+export const useGetUserRewards = () => {
+  const { id: guildId } = useGuild()
+  const { id: userId } = useUser()
+
+  const fetcherWithSign = useFetcherWithSign()
+  const fetcher = (key: string) =>
+    fetcherWithSign([
+      key,
+      {
+        method: "GET",
+        body: {},
+      },
+    ])
+
+  return useSWR<any[]>(
+    userId && guildId ? `/v2/users/${userId}/rewards?guildId=${guildId}` : null,
+    fetcher
+  )
+}
