38d99b3e7f80a41f87572b21dfcd2dc09fa787d5
Gabor <gabor@zgen.hu>
2024-01-16 18:21:17 +0100

    fix: rename platformId prop to RolePlatformId

--- src/hooks/useIsRewardClaimed.ts
+++ src/hooks/useIsRewardClaimed.ts
@@ -1,11 +1,11 @@
 import { RolePlatform } from "../types"
 import { useUserRewards } from "./useUserRewards"
 
-export const useIsRewardClaimed = (platformId: RolePlatform["id"]) => {
+export const useIsRewardClaimed = (rolePlatformId: RolePlatform["id"]) => {
   const { isLoading, data: userRewards } = useUserRewards()
 
   const claimed = !isLoading
-    ? userRewards?.find((reward) => reward.id === platformId)?.claimed
+    ? userRewards?.find((reward) => reward.id === rolePlatformId)?.claimed
     : undefined
 
   return { isLoading, claimed }
