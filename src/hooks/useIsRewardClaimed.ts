68531e3830a526c31ae9fbb99ded683236bcb6b5
Gabor <gabor@zgen.hu>
2024-01-17 10:27:23 +0100

    feat(useClaimText): Add useIsRewardClaimed uniqueValue to the response fix(useIsRewardClaimed): Add plus property to the returned values

--- src/hooks/useIsRewardClaimed.ts
+++ src/hooks/useIsRewardClaimed.ts
@@ -3,10 +3,13 @@ import { useUserRewards } from "./useUserRewards"
 
 export const useIsRewardClaimed = (rolePlatformId: RolePlatform["id"]) => {
   const { isLoading, data: userRewards } = useUserRewards()
+  const userReward = userRewards?.find(
+    (reward) => reward.rolePlatformId === rolePlatformId
+  )
 
-  const claimed = userRewards?.find(
-    (reward) => reward.id === rolePlatformId
-  )?.claimed
-
-  return { isLoading, claimed }
+  return {
+    isLoading,
+    claimed: userReward?.claimed,
+    uniqueValue: userReward?.uniqueValue,
+  }
 }
