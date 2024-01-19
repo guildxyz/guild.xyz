bcc36006ac99a1ef9979aaf4b95d914c8957ec56
Csete Balázs <csetebalu@gmail.com>
2024-01-19 16:50:11 +0100

    add isLoading to copy backup button

--- src/components/_app/Web3ConnectionManager/components/WalletSelectorModal/components/GoogleLoginButton/components/CopyCWaaSBackupData.tsx
+++ src/components/_app/Web3ConnectionManager/components/WalletSelectorModal/components/GoogleLoginButton/components/CopyCWaaSBackupData.tsx
@@ -112,6 +112,7 @@ const CopyCWaaSBackupData = () => {
       <IconButton
         size="sm"
         variant="outline"
+        isLoading={copyBackup.isLoading}
         onClick={copyBackup.onSubmit}
         isLoading={copyBackup.isLoading}
         icon={<Icon as={Copy} p="1px" />}
