996cdd0781878e1aa6b32b5615c177e49e55425c
FBalint <balint@zgen.hu>
2024-01-22 14:05:58 +0100

    feat: editable points reward

--- src/platforms/Points/EditPointsModal.tsx
+++ src/platforms/Points/EditPointsModal.tsx
@@ -4,11 +4,16 @@ import {
   ModalContent,
   ModalHeader,
   ModalOverlay,
+  Stack,
 } from "@chakra-ui/react"
 import useEditGuildPlatform from "components/[guild]/AccessHub/hooks/useEditGuildPlatform"
+import { AddPointsFormType } from "components/[guild]/RolePlatforms/components/AddRoleRewardModal/components/AddPointsPanel/AddPointsPanel"
+import AddNewPointsType from "components/[guild]/RolePlatforms/components/AddRoleRewardModal/components/AddPointsPanel/components/AddNewPointsType"
 import useGuild from "components/[guild]/hooks/useGuild"
+import Button from "components/common/Button"
 import { Modal } from "components/common/Modal"
 import useToast from "hooks/useToast"
+import { FormProvider, useForm, useWatch } from "react-hook-form"
 import { GuildPlatform } from "types"
 
 type Props = {
@@ -25,7 +30,7 @@ const EditPointsModal = ({
   platformGuildData,
 }: Props) => {
   const { isDetailed } = useGuild()
-  const { name, imageUrl, text } = platformGuildData
+  const { name: currentName, imageUrl: currentImage, text } = platformGuildData
 
   const toast = useToast()
   const { onSubmit, isLoading } = useEditGuildPlatform({
@@ -39,7 +44,24 @@ const EditPointsModal = ({
     },
   })
 
-  const onEditTextRewardSubmit = () => {}
+  const onEditPointsRewardSubmit = (data) => {
+    onSubmit({ platformGuildData: data })
+  }
+
+  const methods = useForm<AddPointsFormType>({
+    mode: "all",
+    defaultValues: {
+      name: currentName,
+      imageUrl: currentImage,
+    },
+  })
+
+  const { control } = methods
+  const localName = useWatch({ control, name: "name" })
+  const localImage = useWatch({ control, name: "imageUrl" })
+
+  const name = localName ?? currentName
+  const imageUrl = localImage ?? currentImage
 
   return (
     <Modal isOpen={isOpen} onClose={onClose} size="4xl">
@@ -47,8 +69,29 @@ const EditPointsModal = ({
       <ModalContent>
         <ModalHeader>Edit points</ModalHeader>
         <ModalCloseButton />
+        <ModalBody>
+          <FormProvider {...methods}>
+            <Stack spacing={8}>
+              <AddNewPointsType
+                name={name}
+                imageUrl={imageUrl}
+                isOptional={false}
+              ></AddNewPointsType>
 
-        <ModalBody></ModalBody>
+              <Button
+                colorScheme="indigo"
+                isDisabled={!name?.length}
+                w="max-content"
+                ml="auto"
+                onClick={methods.handleSubmit(onEditPointsRewardSubmit)}
+                isLoading={isLoading}
+                loadingText="Saving reward"
+              >
+                Save
+              </Button>
+            </Stack>
+          </FormProvider>
+        </ModalBody>
       </ModalContent>
     </Modal>
   )
