6ab56d520f20053ccb8bfc0d866715aef4aed806
valid <valid@zgen.hu>
2024-01-24 00:28:50 +0100

    cleanup(UserActivityLogFiltersBar): remove unrelevant logic

--- src/components/[guild]/activity/ActivityLogFiltersBar/UserActivityLogFiltersBar.tsx
+++ src/components/[guild]/activity/ActivityLogFiltersBar/UserActivityLogFiltersBar.tsx
@@ -1,29 +1,10 @@
-import { GridItem, SimpleGrid } from "@chakra-ui/react"
-import useGuild from "components/[guild]/hooks/useGuild"
 import StickyBar from "components/common/Layout/StickyBar"
-import DateRangeInput from "./components/DateRangeInput"
 import FiltersInput from "./components/FiltersInput"
 
-const UserActivityLogFiltersBar = (): JSX.Element => {
-  const { featureFlags } = useGuild()
-
-  const shouldShowDateRangeInput = featureFlags?.includes("CRM")
-
-  return (
-    <StickyBar>
-      <SimpleGrid columns={3} gap={4}>
-        <GridItem colSpan={shouldShowDateRangeInput ? { base: 3, md: 2 } : 3}>
-          <FiltersInput />
-        </GridItem>
-
-        {shouldShowDateRangeInput && (
-          <GridItem colSpan={{ base: 3, md: 1 }}>
-            <DateRangeInput />
-          </GridItem>
-        )}
-      </SimpleGrid>
-    </StickyBar>
-  )
-}
+const UserActivityLogFiltersBar = (): JSX.Element => (
+  <StickyBar>
+    <FiltersInput />
+  </StickyBar>
+)
 
 export default UserActivityLogFiltersBar
