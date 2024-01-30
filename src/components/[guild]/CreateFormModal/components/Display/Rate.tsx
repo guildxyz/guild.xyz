8de31c1cd123e3e210c24fc46db80d94a8098695
BrickheadJohnny <johnny.zgen@gmail.com>
2024-01-30 14:55:12 +0100

    feat: `RateSetup`

---
+++ src/components/[guild]/CreateFormModal/components/Display/Rate.tsx
@@ -0,0 +1,86 @@
+import {
+  Box,
+  ButtonGroup,
+  Collapse,
+  HStack,
+  Stack,
+  Text,
+  UseRadioProps,
+  useRadio,
+} from "@chakra-ui/react"
+import Button from "components/common/Button"
+import { CreateFieldParams } from "../../schemas"
+
+type Props = {
+  field: CreateFieldParams
+  isDisabled?: boolean
+}
+
+const Rate = ({ field, isDisabled }: Props) => {
+  // We probably won't run into this case, but needed to add this line to get valid intellisense
+  if (field.type !== "RATE") return null
+
+  return (
+    <Stack spacing={0}>
+      <RateRadioGroup
+        isDisabled={isDisabled}
+        // TODO: we'll need to use option for display, but option.value for setup...
+        options={field.options?.map((option) => ({
+          value: option.value,
+          label: option.value,
+        }))}
+      />
+
+      <Collapse in={!!field.worstLabel || !!field.bestLabel} animateOpacity>
+        <HStack justifyContent="space-between" fontSize="small">
+          <Box h={6}>
+            <Text as="span" colorScheme="gray">
+              {field.worstLabel}
+            </Text>
+          </Box>
+
+          <Box h={6}>
+            <Text as="span" colorScheme="gray">
+              {field.bestLabel}
+            </Text>
+          </Box>
+        </HStack>
+      </Collapse>
+    </Stack>
+  )
+}
+
+/**
+ * The general RadioButtonGroup component doesn't work properly (it can't handle
+ * disabled state?), so I decided to add a really simple radio group component here &
+ * we'll refactor it later if needed.
+ */
+const RateRadioGroup = ({
+  options,
+  ...props
+}: {
+  options: { label: string | number; value: string | number }[]
+} & UseRadioProps) => {
+  const { getInputProps, getRadioProps } = useRadio(props)
+
+  return (
+    <Box as="label">
+      <input {...getInputProps()} />
+      <ButtonGroup w="full">
+        {options?.map(({ label, value }) => (
+          <Button
+            key={value}
+            {...getRadioProps()}
+            size="sm"
+            w="full"
+            borderRadius="lg"
+          >
+            {label}
+          </Button>
+        ))}
+      </ButtonGroup>
+    </Box>
+  )
+}
+
+export default Rate
---
+++ src/components/[guild]/CreateFormModal/components/Setup/RateSetup.tsx
@@ -0,0 +1,124 @@
+import { Flex, Grid, Input, Stack, Text } from "@chakra-ui/react"
+import StyledSelect from "components/common/StyledSelect"
+import { useEffect } from "react"
+import { useFormContext, useWatch } from "react-hook-form"
+import { SelectOption } from "types"
+import { CreateFormParams } from "../../schemas"
+import Rate from "../Display/Rate"
+
+type Props = {
+  index: number
+}
+
+const optionsDefaultValue = [...Array(10)].map((_, i) => ({ value: i + 1 }))
+
+const RateSetup = ({ index }: Props) => {
+  const { setValue } = useFormContext<CreateFormParams>()
+  const field = useWatch({ name: `fields.${index}` })
+
+  useEffect(() => {
+    if (field.options?.length > 0) return
+    setValue(`fields.${index}.options`, optionsDefaultValue)
+  }, [])
+
+  return (
+    <Stack>
+      <Rate field={field} isDisabled />
+
+      <Grid templateColumns="2fr 1fr" gap={2}>
+        <Grid templateColumns="auto var(--chakra-sizes-24) 1fr" gap={2}>
+          <LabelSetup type="worst" fieldIndex={index} />
+          <LabelSetup type="best" fieldIndex={index} />
+        </Grid>
+      </Grid>
+    </Stack>
+  )
+}
+
+type LabelSetupProps = {
+  type: "worst" | "best"
+  fieldIndex: number
+}
+
+const baseOptionsArray = [...Array(10)].map((_, i) => {
+  const value = i + 1
+  return {
+    label: value.toString(),
+    value,
+  }
+})
+
+const labelSetupConfig: Record<
+  LabelSetupProps["type"],
+  {
+    label: string
+    options: SelectOption<number>[]
+    labelFieldName: string
+  }
+> = {
+  worst: {
+    label: "Start",
+    options: baseOptionsArray.slice(0, 2),
+    labelFieldName: "worstLabel",
+  },
+  best: {
+    label: "End",
+    options: baseOptionsArray.slice(2, baseOptionsArray.length),
+    labelFieldName: "bestLabel",
+  },
+}
+
+const LabelSetup = ({ fieldIndex, type }: LabelSetupProps) => {
+  const { register, getValues, setValue } = useFormContext()
+  const optionsFieldName = `fields.${fieldIndex}.options`
+
+  return (
+    <>
+      <Flex alignItems="center">
+        <Text
+          as="span"
+          fontWeight="semibold"
+          fontSize="small"
+          textTransform="uppercase"
+          colorScheme="gray"
+        >
+          {labelSetupConfig[type].label}
+        </Text>
+      </Flex>
+      <StyledSelect
+        placeholder=""
+        options={labelSetupConfig[type].options}
+        onChange={({ value }) => {
+          const currentRateOptions = getValues(optionsFieldName)
+          if (type === "worst") {
+            setValue(
+              optionsFieldName,
+              generateOptionsFromValue(value, currentRateOptions.at(-1).value)
+            )
+          } else {
+            setValue(
+              optionsFieldName,
+              generateOptionsFromValue(currentRateOptions[0].value, value)
+            )
+          }
+        }}
+      />
+      <Input
+        {...register(`fields.${fieldIndex}${labelSetupConfig[type].labelFieldName}`)}
+        placeholder="Label (optional)"
+      />
+    </>
+  )
+}
+
+const generateOptionsFromValue = (min: number, max: number): number[] => {
+  const generatedArray = []
+
+  for (let i = min; i <= max; i++) {
+    generatedArray.push({ label: i.toString(), value: i })
+  }
+
+  return generatedArray
+}
+
+export default RateSetup
