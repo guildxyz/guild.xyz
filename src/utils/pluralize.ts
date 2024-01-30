cb8719cfb9af2784228ec183a723818fd2e7a968
Gabor <gabor@zgen.hu>
2024-01-30 08:30:42 +0100

    feat(pluralize): Improve pluralize function to cover all plural grammar rules

--- src/utils/pluralize.ts
+++ src/utils/pluralize.ts
@@ -1,4 +1,24 @@
-const pluralize = (count: number, noun: string, suffix = "s") =>
-  `${count ?? 0} ${noun}${count !== 1 ? suffix : ""}`
+function pluralize(count: number, word: string | [string, string]) {
+  if (Array.isArray(word)) {
+    return `${count ?? 0} ${count === 1 ? word[0] : word[1]}`
+  }
+
+  if (count !== 1) {
+    if (word.endsWith("y")) {
+      return `${count ?? 0} ${word.slice(0, -1)}ies`
+    } else if (
+      word.endsWith("s") ||
+      word.endsWith("x") ||
+      word.endsWith("z") ||
+      word.endsWith("ch") ||
+      word.endsWith("sh")
+    ) {
+      return `${count ?? 0} ${word}es`
+    } else {
+      return `${count ?? 0} ${word}s`
+    }
+  }
+  return word
+}
 
 export default pluralize
