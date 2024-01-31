4c9eff0dacff6f9f40dfc9aa6eaa498b8c36672d
BrickheadJohnny <johnny.zgen@gmail.com>
2024-01-31 13:39:46 +0100

    fix(useForms): add type

--- src/components/[guild]/hooks/useForms.ts
+++ src/components/[guild]/hooks/useForms.ts
@@ -1,9 +1,10 @@
 import useSWRImmutable from "swr/immutable"
+import { Form } from "../CreateFormModal/schemas"
 import useGuild from "./useGuild"
 
 const useForms = () => {
   const { id } = useGuild()
-  return useSWRImmutable(`/v2/guilds/${id}/forms`)
+  return useSWRImmutable<Form[]>(`/v2/guilds/${id}/forms`)
 }
 
 export default useForms
