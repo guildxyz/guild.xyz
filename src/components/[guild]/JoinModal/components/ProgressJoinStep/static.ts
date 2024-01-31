1f63b560da92336c9e65459aa6bf222534526408
valid <valid@zgen.hu>
2024-01-31 20:04:48 +0100

    copy: join status improvements

--- src/components/[guild]/JoinModal/components/ProgressJoinStep/static.ts
+++ src/components/[guild]/JoinModal/components/ProgressJoinStep/static.ts
@@ -13,15 +13,18 @@ export const JOIN_LOADING_TEXTS: Record<
   INITIAL: ["Preparing access check", null],
   PREPARING: [
     "Preparing access check",
-    "There are a lot of users joining right now, so you have to wait a bit. There're POSITION users before you",
+    "There are a lot of users joining right now, so you have to wait a bit. There are POSITION users before you",
   ],
-  CHECKING: [null, "Waiting for the next one, there're POSITION users before you"],
+  CHECKING: [
+    null,
+    "Waiting to check the next one, there are POSITION users before you",
+  ],
   MANAGING_ROLES: [
     "Evaluating which roles you have access to",
-    "There are a lot of users joining right now, so you have to wait a bit. There're POSITION users before you",
+    "There are a lot of users joining right now, so you have to wait a bit. There are POSITION users before you",
   ],
   MANAGING_REWARDS: [
     "Evaluating which rewards you will get",
-    "There are a lot of users joining right now, so you have to wait a bit. There're POSITION users before you",
+    "There are a lot of users joining right now, so you have to wait a bit. There are POSITION users before you",
   ],
 }
