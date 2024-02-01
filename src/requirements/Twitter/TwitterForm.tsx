651082aeefab0be0f24869ec9f49672218e11bd8
BrickheadJohnny <johnny.zgen@gmail.com>
2024-02-01 10:40:36 +0100

    fix(TwitterForm): remove legacy requirements

--- src/requirements/Twitter/TwitterForm.tsx
+++ src/requirements/Twitter/TwitterForm.tsx
@@ -5,116 +5,113 @@ import {
   FormLabel,
   Stack,
 } from "@chakra-ui/react"
-import useGuild from "components/[guild]/hooks/useGuild"
 import ControlledSelect from "components/common/ControlledSelect"
 import { useFormContext, useFormState, useWatch } from "react-hook-form"
 import { RequirementFormProps } from "requirements"
 import parseFromObject from "utils/parseFromObject"
 import TwitterAccountAge from "./components/TwitterAccountAge"
 import TwitterAccountAgeRelative from "./components/TwitterAccountAgeRelative"
-import TwitterMinimumCount from "./components/TwitterMinimumCount"
+import TwitterAccountVerified from "./components/TwitterAccountVerified"
 import TwitterListInput from "./components/TwitterListInput"
+import TwitterMinimumCount from "./components/TwitterMinimumCount"
 import TwitterTextToInclude from "./components/TwitterTextToInclude"
 import TwitterTweetInput from "./components/TwitterTweetInput"
 import TwitterUserInput from "./components/TwitterUserInput"
-import TwitterAccountVerified from "./components/TwitterAccountVerified"
+
+const twitterRequirementTypes = [
+  {
+    label: "Have verified account",
+    value: "TWITTER_ACCOUNT_VERIFIED",
+    TwitterRequirement: TwitterAccountVerified,
+  },
+  {
+    label: "Have at least x followers",
+    value: "TWITTER_FOLLOWER_COUNT",
+    TwitterRequirement: TwitterMinimumCount,
+  },
+  {
+    label: "Follow user",
+    value: "TWITTER_FOLLOW",
+    TwitterRequirement: TwitterUserInput,
+  },
+  {
+    label: "Be followed by user",
+    value: "TWITTER_FOLLOWED_BY",
+    TwitterRequirement: TwitterUserInput,
+  },
+  {
+    label: "Like post",
+    value: "TWITTER_LIKE_V2",
+    TwitterRequirement: TwitterTweetInput,
+  },
+  {
+    label: "Repost",
+    value: "TWITTER_RETWEET_V2",
+    TwitterRequirement: TwitterTweetInput,
+  },
+  {
+    label: "Account age (absolute)",
+    value: "TWITTER_ACCOUNT_AGE",
+    TwitterRequirement: TwitterAccountAge,
+  },
+  {
+    label: "Account age (relative)",
+    value: "TWITTER_ACCOUNT_AGE_RELATIVE",
+    TwitterRequirement: TwitterAccountAgeRelative,
+  },
+  { label: "Have protected account", value: "TWITTER_ACCOUNT_PROTECTED" },
+  {
+    label: "Follow at least x users",
+    value: "TWITTER_FOLLOWING_COUNT",
+    TwitterRequirement: TwitterMinimumCount,
+  },
+  {
+    label: "Have at least x posts",
+    value: "TWITTER_TWEET_COUNT",
+    TwitterRequirement: TwitterMinimumCount,
+  },
+  {
+    label: "Give at least x total likes",
+    value: "TWITTER_LIKE_COUNT",
+    TwitterRequirement: TwitterMinimumCount,
+  },
+  {
+    label: "Have specific text in username",
+    value: "TWITTER_NAME",
+    TwitterRequirement: TwitterTextToInclude,
+  },
+  {
+    label: "Have specific text in bio",
+    value: "TWITTER_BIO",
+    TwitterRequirement: TwitterTextToInclude,
+  },
+  // {
+  //   label: "Follow list",
+  //   value: "TWITTER_LIST_FOLLOW",
+  //   TwitterRequirement: TwitterListInput,
+  // },
+  {
+    label: "Be a member of list",
+    value: "TWITTER_LIST_MEMBER",
+    TwitterRequirement: TwitterListInput,
+  },
+  // ...(featureFlags?.includes("TWITTER_EXTRA_REQUIREMENT")
+  //   ? [
+  //       {
+  //         label: "Like post (legacy)",
+  //         value: "TWITTER_LIKE",
+  //         TwitterRequirement: TwitterTweetInput,
+  //       },
+  //       {
+  //         label: "Repost (legacy)",
+  //         value: "TWITTER_RETWEET",
+  //         TwitterRequirement: TwitterTweetInput,
+  //       },
+  //     ]
+  //   : []),
+]
 
 const TwitterForm = ({ baseFieldPath, field }: RequirementFormProps) => {
-  const { featureFlags } = useGuild()
-
-  const twitterRequirementTypes = [
-    {
-      label: "Have verified account",
-      value: "TWITTER_ACCOUNT_VERIFIED",
-      TwitterRequirement: TwitterAccountVerified,
-    },
-    {
-      label: "Have at least x followers",
-      value: "TWITTER_FOLLOWER_COUNT",
-      TwitterRequirement: TwitterMinimumCount,
-    },
-    {
-      label: "Follow user",
-      value: "TWITTER_FOLLOW",
-      TwitterRequirement: TwitterUserInput,
-    },
-    {
-      label: "Be followed by user",
-      value: "TWITTER_FOLLOWED_BY",
-      TwitterRequirement: TwitterUserInput,
-    },
-    {
-      label: "Like post",
-      value: "TWITTER_LIKE_V2",
-      TwitterRequirement: TwitterTweetInput,
-    },
-    {
-      label: "Repost",
-      value: "TWITTER_RETWEET_V2",
-      TwitterRequirement: TwitterTweetInput,
-    },
-    {
-      label: "Account age (absolute)",
-      value: "TWITTER_ACCOUNT_AGE",
-      TwitterRequirement: TwitterAccountAge,
-    },
-    {
-      label: "Account age (relative)",
-      value: "TWITTER_ACCOUNT_AGE_RELATIVE",
-      TwitterRequirement: TwitterAccountAgeRelative,
-    },
-    { label: "Have protected account", value: "TWITTER_ACCOUNT_PROTECTED" },
-    {
-      label: "Follow at least x users",
-      value: "TWITTER_FOLLOWING_COUNT",
-      TwitterRequirement: TwitterMinimumCount,
-    },
-    {
-      label: "Have at least x posts",
-      value: "TWITTER_TWEET_COUNT",
-      TwitterRequirement: TwitterMinimumCount,
-    },
-    {
-      label: "Give at least x total likes",
-      value: "TWITTER_LIKE_COUNT",
-      TwitterRequirement: TwitterMinimumCount,
-    },
-    {
-      label: "Have specific text in username",
-      value: "TWITTER_NAME",
-      TwitterRequirement: TwitterTextToInclude,
-    },
-    {
-      label: "Have specific text in bio",
-      value: "TWITTER_BIO",
-      TwitterRequirement: TwitterTextToInclude,
-    },
-    // {
-    //   label: "Follow list",
-    //   value: "TWITTER_LIST_FOLLOW",
-    //   TwitterRequirement: TwitterListInput,
-    // },
-    {
-      label: "Be a member of list",
-      value: "TWITTER_LIST_MEMBER",
-      TwitterRequirement: TwitterListInput,
-    },
-    ...(featureFlags?.includes("TWITTER_EXTRA_REQUIREMENT")
-      ? [
-          {
-            label: "Like post (legacy)",
-            value: "TWITTER_LIKE",
-            TwitterRequirement: TwitterTweetInput,
-          },
-          {
-            label: "Repost (legacy)",
-            value: "TWITTER_RETWEET",
-            TwitterRequirement: TwitterTweetInput,
-          },
-        ]
-      : []),
-  ]
-
   const { errors } = useFormState()
   const { resetField } = useFormContext()
 
