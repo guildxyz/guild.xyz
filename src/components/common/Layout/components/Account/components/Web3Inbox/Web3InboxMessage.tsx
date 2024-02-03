00083b5d9a0be563e5727e1df81558263d17ad06
BrickheadJohnny <johnny.zgen@gmail.com>
2024-02-03 10:30:05 +0100

    feat: use the stable Web3Inbox SDK

--- src/components/common/Layout/components/Account/components/Web3Inbox/Web3InboxMessage.tsx
+++ src/components/common/Layout/components/Account/components/Web3Inbox/Web3InboxMessage.tsx
@@ -1,5 +1,4 @@
 import {
-  Circle,
   HStack,
   Img,
   ModalBody,
@@ -19,27 +18,24 @@ import { Modal } from "components/common/Modal"
 import { ArrowRight } from "phosphor-react"
 import formatRelativeTimeFromNow from "utils/formatRelativeTimeFromNow"
 
-const FALLBACK_ICON = "/requirementLogos/guild.png"
+const GUILD_NOTIFICATION_ICON = "/requirementLogos/guild.png"
 
 const Web3InboxMessage = ({
-  publishedAt,
+  sentAt,
   title,
-  icon,
   body,
   url,
 }: {
-  publishedAt: number
+  sentAt: number
   title: string
-  icon: string
   body: string
   url: string
 }) => {
   const { isOpen, onOpen, onClose } = useDisclosure()
   const hoverBgColor = useColorModeValue("blackAlpha.300", "whiteAlpha.200")
-  const circleBgColor = useColorModeValue("gray.700", "gray.600")
   const isMobile = useBreakpointValue({ base: true, sm: false })
 
-  const prettyDate = new Date(publishedAt).toLocaleDateString("en-US", {
+  const prettyDate = new Date(sentAt).toLocaleDateString("en-US", {
     year: "numeric",
     month: "numeric",
     day: "numeric",
@@ -66,15 +62,12 @@ const Web3InboxMessage = ({
         transition="background 0.2s ease"
         onClick={onOpen}
       >
-        {!icon ? (
-          <Img src={FALLBACK_ICON} alt={title} boxSize={10} borderRadius="full" />
-        ) : icon?.startsWith("/guildLogos") ? (
-          <Circle bgColor={circleBgColor} size={10}>
-            <Img src={icon} alt={title} boxSize={6} />
-          </Circle>
-        ) : (
-          <Img src={icon} alt={title} boxSize={10} borderRadius="full" />
-        )}
+        <Img
+          src={GUILD_NOTIFICATION_ICON}
+          alt={title}
+          boxSize={10}
+          borderRadius="full"
+        />
 
         <Stack spacing={0} w="full">
           <HStack justifyContent="space-between">
@@ -82,7 +75,7 @@ const Web3InboxMessage = ({
               {title}
             </Text>
             <Text as="span" colorScheme="gray" fontSize="xs">
-              {formatRelativeTimeFromNow(Date.now() - publishedAt)}
+              {formatRelativeTimeFromNow(Date.now() - sentAt)}
             </Text>
           </HStack>
           <Text noOfLines={1} colorScheme="gray">
@@ -97,7 +90,7 @@ const Web3InboxMessage = ({
           <ModalHeader pb="6">
             <HStack spacing={3}>
               <Img
-                src={icon || FALLBACK_ICON}
+                src={GUILD_NOTIFICATION_ICON}
                 alt={title}
                 boxSize={8}
                 borderRadius="full"
