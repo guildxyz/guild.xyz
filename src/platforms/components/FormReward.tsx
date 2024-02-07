ead1ba60c7dc8bd9af5a8699aa312f8a6717f873
BrickheadJohnny <johnny.zgen@gmail.com>
2024-02-07 14:47:00 +0100

    feat(FormReward): add tooltip & disabled state

--- src/platforms/components/FormReward.tsx
+++ src/platforms/components/FormReward.tsx
@@ -1,3 +1,4 @@
+import { Tooltip } from "@chakra-ui/react"
 import {
   RewardDisplay,
   RewardIcon,
@@ -7,40 +8,56 @@ import useForms from "components/[guild]/hooks/useForms"
 import useGuild from "components/[guild]/hooks/useGuild"
 import LinkButton from "components/common/LinkButton"
 import { ArrowSquareOut } from "phosphor-react"
+import useUserSubmission from "platforms/Forms/hooks/useUserSubmission"
 import platforms from "platforms/platforms"
 import { PlatformType } from "types"
 
 const FormReward = ({ platform, withMotionImg }: RewardProps) => {
   const { urlName } = useGuild()
   const { platformId, platformGuildData } = platform.guildPlatform
-  const { data: forms } = useForms()
+  const { data: forms, isValidating: isFormsValidating } = useForms()
 
   const form = forms?.find((f) => f.id === platformGuildData?.formId)
 
+  const { data: userSubmission, isValidating: isUserSubmissionValidating } =
+    useUserSubmission(form)
+
   return (
-    <RewardDisplay
-      icon={
-        <RewardIcon
-          rolePlatformId={platform.id}
-          guildPlatform={platform?.guildPlatform}
-          withMotionImg={withMotionImg}
-        />
-      }
-      label={
-        <>
-          {"Fill form: "}
-          <LinkButton
-            href={`/${urlName}/forms/${platformGuildData.formId}`}
-            variant="link"
-            rightIcon={<ArrowSquareOut />}
-            iconSpacing="1"
-            maxW="full"
-          >
-            {form?.name ?? platforms[PlatformType[platformId]].name}
-          </LinkButton>
-        </>
-      }
-    />
+    <Tooltip
+      label={!!userSubmission ? "Response already submitted" : "Fill form"}
+      hasArrow
+      shouldWrapChildren
+    >
+      <RewardDisplay
+        icon={
+          <RewardIcon
+            rolePlatformId={platform.id}
+            guildPlatform={platform?.guildPlatform}
+            withMotionImg={withMotionImg}
+          />
+        }
+        label={
+          <>
+            {"Fill form: "}
+            <LinkButton
+              isLoading={isFormsValidating || isUserSubmissionValidating}
+              isDisabled={!!userSubmission}
+              href={
+                !userSubmission
+                  ? `/${urlName}/forms/${platformGuildData.formId}`
+                  : "#"
+              }
+              variant="link"
+              rightIcon={<ArrowSquareOut />}
+              iconSpacing="1"
+              maxW="full"
+            >
+              {form?.name ?? platforms[PlatformType[platformId]].name}
+            </LinkButton>
+          </>
+        }
+      />
+    </Tooltip>
   )
 }
 export default FormReward
