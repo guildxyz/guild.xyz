75620487c33315a63f7759c63550e9a48dacc2e8
BrickheadJohnny <johnny.zgen@gmail.com>
2024-02-07 14:23:29 +0100

    fix(getFieldIndexesToSwap): update comment & variable names

--- src/utils/getFieldsToSwap.ts
+++ src/utils/getFieldsToSwap.ts
@@ -1,13 +1,16 @@
 /**
- * Can be used in Reorder.Group when we'd like to reorder form fields It just return
- * an array with 2 indexes which we can pass to the `swap` function
+ * Can be used in Reorder.Group when we'd like to reorder form fields.
+ *
+ * Returns an array with 2 indexes which we can pass to the `swap` function
  */
 
 const getFieldIndexesToSwap = (originalOrder: string[], newOrder: string[]) => {
   const indexesToSwap: number[] = []
 
-  for (const [i, field] of originalOrder.entries()) {
-    const fieldIndexInNewOrder = newOrder.findIndex((f) => f === field)
+  for (const [i, fieldId] of originalOrder.entries()) {
+    const fieldIndexInNewOrder = newOrder.findIndex(
+      (newFieldId) => newFieldId === fieldId
+    )
     if (fieldIndexInNewOrder !== i) indexesToSwap.push(i)
   }
 
