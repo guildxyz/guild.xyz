efbed4bb3dca00f93cbc971906eb28c1b584caad
Gabor <gabor@zgen.hu>
2024-02-09 09:53:20 +0100

    fix(SendNewMessage): Separate a bit

--- src/components/[guild]/messages/SendNewMessage/SendNewMessage.tsx
+++ src/components/[guild]/messages/SendNewMessage/SendNewMessage.tsx
@@ -38,38 +38,9 @@ type SendMessageForm = {
 }
 
 const SendNewMessage = (props: ButtonProps) => {
-  const methods = useForm<SendMessageForm>({
-    mode: "all",
-    defaultValues: {
-      protocol: "WEB3INBOX",
-      destination: "ROLES",
-      roleIds: [],
-      message: "",
-    },
-  })
-  const {
-    control,
-    register,
-    formState: { errors },
-    reset,
-    handleSubmit,
-  } = methods
-
   const { isOpen, onOpen, onClose } = useDisclosure()
   const { isStuck } = useIsTabsStuck()
   const { textColor, buttonColorScheme } = useThemeContext()
-
-  const { onSubmit, isLoading } = useSendMessage(() => {
-    reset()
-    onClose()
-  })
-
-  const roleIds = useWatch({ control, name: "roleIds" })
-  const { targetedCount, isTargetedCountValidating } = useTargetedCount(roleIds)
-  const { data: reachableUsers, isValidating: isReachableUsersLoading } =
-    useReachableUsers("WEB3INBOX", "ROLES", roleIds)
-
-  const greenTextColor = useColorModeValue("green.600", "green.300")
 
   return (
     <>
@@ -86,95 +57,131 @@ const SendNewMessage = (props: ButtonProps) => {
       </Button>
 
       <Modal isOpen={isOpen} onClose={onClose} size="xl">
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>Send new message</ModalHeader>
-          <ModalCloseButton />
-
-          <ModalBody>
-            <FormProvider {...methods}>
-              <Stack spacing={6}>
-                <Stack>
-                  <FormControl isRequired isInvalid={!!errors.roleIds}>
-                    <FormLabel>Recipient roles</FormLabel>
-                    <RoleIdsSelect />
-                    <FormErrorMessage>{errors.roleIds?.message}</FormErrorMessage>
-
-                    <Text colorScheme="gray" pt={2}>
-                      <Text
-                        as="span"
-                        fontWeight="bold"
-                        color={reachableUsers?.length > 0 && greenTextColor}
-                      >
-                        {isReachableUsersLoading ? (
-                          <Spinner size="xs" />
-                        ) : (
-                          reachableUsers?.length ?? 0
-                        )}
-                      </Text>
-                      <Text
-                        as="span"
-                        color={reachableUsers?.length > 0 && greenTextColor}
-                      >
-                        {" reachable "}
-                      </Text>
-                      <Text as="span" color="chakra-body-text">
-                        {"/ "}
-                      </Text>
-                      <Text as="span" fontWeight="bold">
-                        {isTargetedCountValidating ? (
-                          <Spinner size="xs" />
-                        ) : (
-                          targetedCount
-                        )}
-                      </Text>
-                      {" targeted"}
-                    </Text>
-                  </FormControl>
-
-                  <Text>
-                    {`You can only message users who've subscribed to the Guild.xyz app on `}
-                    <Link href="https://web3inbox.com" colorScheme="blue" isExternal>
-                      Web3Inbox
-                    </Link>
-                    {`. They can do it from the notifications menu easily!`}
-                  </Text>
-                </Stack>
-
-                <FormControl isRequired isInvalid={!!errors.message}>
-                  <FormLabel>Message</FormLabel>
-                  <Textarea
-                    placeholder="Write your message here"
-                    {...register("message", {
-                      required: "This field is required",
-                      maxLength: {
-                        value: 255,
-                        message:
-                          "Maximum Web3Inbox message length is 255 characters",
-                      },
-                    })}
-                  />
-                  <FormErrorMessage>{errors.message?.message}</FormErrorMessage>
-                </FormControl>
-              </Stack>
-            </FormProvider>
-          </ModalBody>
-
-          <ModalFooter>
-            <Button
-              ml="auto"
-              h={10}
-              colorScheme="green"
-              rightIcon={<PaperPlaneRight />}
-              onClick={handleSubmit(onSubmit)}
-              isLoading={isLoading}
-              loadingText="Sending"
-            >
-              Send
-            </Button>
-          </ModalFooter>
-        </ModalContent>
+        <MessageModalContent onClose={onClose} />
       </Modal>
+    </>
+  )
+}
+type MessageModalContentProps = { onClose: () => void }
+
+const MessageModalContent = ({ onClose }: MessageModalContentProps) => {
+  const methods = useForm<SendMessageForm>({
+    mode: "all",
+    defaultValues: {
+      protocol: "WEB3INBOX",
+      destination: "ROLES",
+      roleIds: [],
+      message: "",
+    },
+  })
+  const {
+    control,
+    register,
+    formState: { errors },
+    reset,
+    handleSubmit,
+  } = methods
+  const { onSubmit, isLoading } = useSendMessage(() => {
+    reset()
+    onClose()
+  })
+
+  const roleIds = useWatch({ control, name: "roleIds" })
+  const { targetedCount, isTargetedCountValidating } = useTargetedCount(roleIds)
+  const { data: reachableUsers, isValidating: isReachableUsersLoading } =
+    useReachableUsers("WEB3INBOX", "ROLES", roleIds)
+
+  const greenTextColor = useColorModeValue("green.600", "green.300")
+
+  return (
+    <>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Send new message</ModalHeader>
+        <ModalCloseButton />
+
+        <ModalBody>
+          <FormProvider {...methods}>
+            <Stack spacing={6}>
+              <Stack>
+                <FormControl isRequired isInvalid={!!errors.roleIds}>
+                  <FormLabel>Recipient roles</FormLabel>
+                  <RoleIdsSelect />
+                  <FormErrorMessage>{errors.roleIds?.message}</FormErrorMessage>
+
+                  <Text colorScheme="gray" pt={2}>
+                    <Text
+                      as="span"
+                      fontWeight="bold"
+                      color={reachableUsers?.length > 0 && greenTextColor}
+                    >
+                      {isReachableUsersLoading ? (
+                        <Spinner size="xs" />
+                      ) : (
+                        reachableUsers?.length ?? 0
+                      )}
+                    </Text>
+                    <Text
+                      as="span"
+                      color={reachableUsers?.length > 0 && greenTextColor}
+                    >
+                      {" reachable "}
+                    </Text>
+                    <Text as="span" color="chakra-body-text">
+                      {"/ "}
+                    </Text>
+                    <Text as="span" fontWeight="bold">
+                      {isTargetedCountValidating ? (
+                        <Spinner size="xs" />
+                      ) : (
+                        targetedCount
+                      )}
+                    </Text>
+                    {" targeted"}
+                  </Text>
+                </FormControl>
+
+                <Text>
+                  {`You can only message users who've subscribed to the Guild.xyz app on `}
+                  <Link href="https://web3inbox.com" colorScheme="blue" isExternal>
+                    Web3Inbox
+                  </Link>
+                  {`. They can do it from the notifications menu easily!`}
+                </Text>
+              </Stack>
+
+              <FormControl isRequired isInvalid={!!errors.message}>
+                <FormLabel>Message</FormLabel>
+                <Textarea
+                  placeholder="Write your message here"
+                  {...register("message", {
+                    required: "This field is required",
+                    maxLength: {
+                      value: 255,
+                      message: "Maximum Xmtp message length is 255 characters",
+                    },
+                  })}
+                />
+                <FormErrorMessage>{errors.message?.message}</FormErrorMessage>
+              </FormControl>
+            </Stack>
+          </FormProvider>
+        </ModalBody>
+
+        <ModalFooter>
+          <Button
+            ml="auto"
+            h={10}
+            colorScheme="green"
+            rightIcon={<PaperPlaneRight />}
+            onClick={handleSubmit(onSubmit)}
+            isLoading={isLoading}
+            loadingText="Sending"
+          >
+            Send
+          </Button>
+        </ModalFooter>
+      </ModalContent>
     </>
   )
 }
