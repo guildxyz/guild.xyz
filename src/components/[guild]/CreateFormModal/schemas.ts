d59e60e833ab3613b6f509190af1a163ebec11e1
BrickheadJohnny <johnny.zgen@gmail.com>
2024-02-13 12:00:39 +0100

    fix FormCreationFormSchema

--- src/components/[guild]/CreateFormModal/schemas.ts
+++ src/components/[guild]/CreateFormModal/schemas.ts
@@ -65,13 +65,14 @@ export const FormSchema = FormCreationPayloadSchema.extend({
 // Frontend-specific
 export const FormCreationFormSchema = z
   .any()
-  .transform((value) => {
+  .transform((_value) => {
+    const value = structuredClone(_value)
     if (Array.isArray(value.fields)) {
-      value.fields = value.fields.map((field) => {
+      value.fields = value.fields?.map((field) => {
         if (!Array.isArray(field.options)) return field
         return {
           ...field,
-          options: field.options.map((option) => {
+          options: field.options?.map((option) => {
             if (typeof option === "string" || typeof option === "number")
               return option
             return option.value
