dfd26459f606bfe261c581f532cc0306a2a9307e
valid <valid@zgen.hu>
2024-02-14 20:07:37 +0100

    FormRequirement: link instead of button

--- src/requirements/Form/FormRequirement.tsx
+++ src/requirements/Form/FormRequirement.tsx
@@ -1,46 +1,46 @@
-import { Icon, Skeleton, Text } from "@chakra-ui/react"
+import { Icon, Skeleton, Text, Tooltip } from "@chakra-ui/react"
 import Requirement, {
   RequirementProps,
 } from "components/[guild]/Requirements/components/Requirement"
 import { useRequirementContext } from "components/[guild]/Requirements/components/RequirementContext"
 import useForms from "components/[guild]/hooks/useForms"
 import useGuild from "components/[guild]/hooks/useGuild"
-import LinkButton from "components/common/LinkButton"
-import { ArrowSquareIn } from "phosphor-react"
+import Link from "components/common/Link"
 import useUserSubmission from "platforms/Forms/hooks/useUserSubmission"
 import platforms from "platforms/platforms"
 
 const FormRequirement = (props: RequirementProps) => {
   const { urlName } = useGuild()
   const { data } = useRequirementContext()
-  const { data: forms, isValidating: isFormsValidating } = useForms()
+  const { data: forms } = useForms()
   const form = forms?.find((f) => f.id === data?.id)
-  const { data: userSubmission, isValidating } = useUserSubmission(form)
+  const { data: userSubmission } = useUserSubmission(form)
 
   return (
-    <Requirement
-      image={<Icon as={platforms.FORM.icon} boxSize={6} />}
-      footer={
-        <LinkButton
-          isDisabled={!form || !!userSubmission}
-          isLoading={isFormsValidating || isValidating}
-          prefetch={false}
-          href={!!form && !userSubmission ? `/${urlName}/forms/${form?.id}` : "#"}
-          colorScheme="blue"
-          size="xs"
-          rightIcon={<Icon as={ArrowSquareIn} />}
-          borderRadius="md"
-          fontWeight="medium"
-        >
-          Fill form
-        </LinkButton>
-      }
-      {...props}
-    >
+    <Requirement image={<Icon as={platforms.FORM.icon} boxSize={6} />} {...props}>
       <Text as="span">{"Fill the "}</Text>
-      <Skeleton display="inline-block" isLoaded={!!form}>
-        {form?.name ?? "Loading form..."}
-      </Skeleton>
+      <Tooltip
+        label="Respone already submitted"
+        isDisabled={!userSubmission}
+        hasArrow
+        placement="top"
+      >
+        <Skeleton display="inline-block" isLoaded={!!form}>
+          <Link
+            {...(!!userSubmission
+              ? {
+                  opacity: 0.6,
+                  cursor: "not-allowed",
+                  href: "#",
+                }
+              : { href: `/${urlName}/forms/${form?.id}` })}
+            prefetch={false}
+            colorScheme="blue"
+          >
+            {form?.name ?? "Loading form..."}
+          </Link>
+        </Skeleton>
+      </Tooltip>
       <Text as="span">{" form"}</Text>
     </Requirement>
   )
