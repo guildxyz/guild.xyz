99fa9a3f52ade36af5d925fde516b44196368c88
valid <valid@zgen.hu>
2024-02-19 17:41:26 +0100

    feat(RecheckAccessButton): status indication like on join (#1072)

--- src/components/[guild]/RecheckAccessesButton.tsx
+++ src/components/[guild]/RecheckAccessesButton.tsx
@@ -1,4 +1,17 @@
-import { ButtonProps, Icon, IconButton, Tooltip } from "@chakra-ui/react"
+import {
+  ButtonProps,
+  Divider,
+  Icon,
+  IconButton,
+  Popover,
+  PopoverArrow,
+  PopoverBody,
+  PopoverContent,
+  PopoverHeader,
+  PopoverTrigger,
+  Portal,
+  VStack,
+} from "@chakra-ui/react"
 import { usePostHogContext } from "components/_app/PostHogProvider"
 import useShowErrorToast from "hooks/useShowErrorToast"
 import useToast from "hooks/useToast"
@@ -6,6 +19,9 @@ import { useAtom } from "jotai"
 import { atomWithStorage } from "jotai/utils"
 import { ArrowsClockwise, Check } from "phosphor-react"
 import { useEffect, useState } from "react"
+import GetRewardsJoinStep from "./JoinModal/components/progress/GetRewardsJoinStep"
+import GetRolesJoinStep from "./JoinModal/components/progress/GetRolesJoinStep"
+import SatisfyRequirementsJoinStep from "./JoinModal/components/progress/SatisfyRequirementsJoinStep"
 import useMembershipUpdate from "./JoinModal/hooks/useMembershipUpdate"
 import { useIsTabsStuck } from "./Tabs/Tabs"
 import { useThemeContext } from "./ThemeContext"
@@ -16,6 +32,14 @@ const TIMEOUT = 60_000
 type Props = {
   tooltipLabel?: string
 } & ButtonProps
+
+const POPOVER_HEADER_STYLES = {
+  fontWeight: "medium",
+  border: 0,
+  fontSize: "sm",
+  py: "1.5",
+  px: "3",
+}
 
 const latestResendDateAtom = atomWithStorage("latestResendDate", -Infinity)
 
@@ -34,27 +58,28 @@ const RecheckAccessesButton = ({
   const [dateNow, setDateNow] = useState(Date.now())
   const canResend = dateNow - latestResendDate > TIMEOUT
 
-  const { triggerMembershipUpdate, isLoading, isFinished } = useMembershipUpdate(
-    () => {
-      toast({
-        status: "success",
-        title: "Successfully updated accesses",
-      })
-      setLatestResendDate(Date.now())
-    },
-    (error) => {
-      const errorMsg = "Couldn't update accesses"
-      const correlationId = error.correlationId
-      showErrorToast(
-        correlationId
-          ? {
-              error: errorMsg,
-              correlationId,
-            }
-          : errorMsg
-      )
-    }
-  )
+  const { triggerMembershipUpdate, isLoading, isFinished, joinProgress } =
+    useMembershipUpdate(
+      () => {
+        toast({
+          status: "success",
+          title: "Successfully updated accesses",
+        })
+        setLatestResendDate(Date.now())
+      },
+      (error) => {
+        const errorMsg = "Couldn't update accesses"
+        const correlationId = error.correlationId
+        showErrorToast(
+          correlationId
+            ? {
+                error: errorMsg,
+                correlationId,
+              }
+            : errorMsg
+        )
+      }
+    )
 
   useEffect(() => {
     const interval = setInterval(() => setDateNow(Date.now()), TIMEOUT)
@@ -68,54 +93,81 @@ const RecheckAccessesButton = ({
     })
   }
 
+  const isDisabled = isLoading || !!isFinished || !canResend
+
   return (
-    <Tooltip
-      label={
-        isFinished
-          ? "Successfully updated accesses"
-          : isLoading
-          ? "Checking accesses..."
-          : canResend
-          ? tooltipLabel
-          : "You can only use this function once per minute"
-      }
-      sx={{
-        "@-webkit-keyframes rotate": {
-          from: {
-            transform: "rotate(0)",
-          },
-          to: {
-            transform: "rotate(360deg)",
-          },
-        },
-        "@keyframes rotate": {
-          from: {
-            transform: "rotate(0)",
-          },
-          to: {
-            transform: "rotate(360deg)",
-          },
-        },
-      }}
-      hasArrow
-    >
-      <IconButton
-        aria-label="Re-check accesses"
-        icon={
-          isFinished ? (
-            <Check />
+    <Popover trigger="hover" placement="bottom" isLazy>
+      <PopoverTrigger>
+        <IconButton
+          aria-label="Re-check accesses"
+          icon={
+            isFinished ? (
+              <Check />
+            ) : (
+              <Icon
+                as={ArrowsClockwise}
+                animation={isLoading ? "rotate 1s infinite linear" : undefined}
+              />
+            )
+          }
+          // artificial disabled state, so the popover still works
+          {...(isDisabled
+            ? {
+                opacity: 0.5,
+                cursor: "default",
+                _hover: { bg: undefined },
+                _focus: { bg: undefined },
+                _active: { bg: undefined },
+              }
+            : {
+                onClick,
+              })}
+          sx={{
+            "@-webkit-keyframes rotate": {
+              from: {
+                transform: "rotate(0)",
+              },
+              to: {
+                transform: "rotate(360deg)",
+              },
+            },
+            "@keyframes rotate": {
+              from: {
+                transform: "rotate(0)",
+              },
+              to: {
+                transform: "rotate(360deg)",
+              },
+            },
+          }}
+          {...rest}
+        />
+      </PopoverTrigger>
+      <Portal>
+        <PopoverContent {...(!isLoading ? { minW: "max-content", w: "unset" } : {})}>
+          <PopoverArrow />
+          {isFinished ? (
+            <PopoverHeader {...POPOVER_HEADER_STYLES}>
+              Successfully updated accesses
+            </PopoverHeader>
+          ) : isLoading ? (
+            <PopoverBody pb={3} px={4}>
+              <VStack spacing={2.5} alignItems={"flex-start"} divider={<Divider />}>
+                <SatisfyRequirementsJoinStep joinState={joinProgress} />
+                <GetRolesJoinStep joinState={joinProgress} />
+                <GetRewardsJoinStep joinState={joinProgress} />
+              </VStack>
+            </PopoverBody>
+          ) : canResend ? (
+            <PopoverHeader {...POPOVER_HEADER_STYLES}>{tooltipLabel}</PopoverHeader>
           ) : (
-            <Icon
-              as={ArrowsClockwise}
-              animation={isLoading ? "rotate 1s infinite linear" : undefined}
-            />
-          )
-        }
-        onClick={!isFinished && canResend ? onClick : undefined}
-        isDisabled={isLoading || !!isFinished || !canResend}
-        {...rest}
-      />
-    </Tooltip>
+            <PopoverHeader {...POPOVER_HEADER_STYLES}>
+              You can only use this function once per minute
+            </PopoverHeader>
+          )}
+        </PopoverContent>
+      </Portal>
+    </Popover>
   )
 }
 
