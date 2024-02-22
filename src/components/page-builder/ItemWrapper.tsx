140c8e46ac0d9576d47a7afb3d347c581dc21a99
BrickheadJohnny <johnny.zgen@gmail.com>
2024-02-22 12:47:35 +0100

    page-build optimizations & ItemWrapper resize functionality

--- src/components/page-builder/ItemWrapper.tsx
+++ src/components/page-builder/ItemWrapper.tsx
@@ -1,32 +1,38 @@
 import {
   Box,
   EASINGS,
-  Flex,
   GridItem,
   HStack,
   IconButton,
   Popover,
+  PopoverArrow,
   PopoverBody,
   PopoverContent,
   PopoverHeader,
   PopoverTrigger,
   SimpleGrid,
+  usePopper,
 } from "@chakra-ui/react"
 import Card from "components/common/Card"
-import { AnimatePresence, motion } from "framer-motion"
+import { AnimatePresence, DragHandlers, motion } from "framer-motion"
 import { Item } from "pages/page-builder"
 import { ArrowsOutSimple, TrashSimple } from "phosphor-react"
-import { DragEventHandler, forwardRef, useState } from "react"
+import { PropsWithChildren, useState } from "react"
 
 type Props = {
   item: Item
-  onDrag: DragEventHandler
-  onDragEnd: DragEventHandler
+  onDrag: DragHandlers["onDrag"]
+  onDragEnd: DragHandlers["onDragEnd"]
+  onResize: (
+    width: Item["desktop"]["width"],
+    height: Item["desktop"]["height"]
+  ) => void
+  onRemove: () => void
   "data-item-id": string
 }
 
 const MotionCard = motion(Card)
-const MotionFlex = motion(Flex)
+const MotionBox = motion(Box)
 
 const motionMountProps = {
   initial: {
@@ -45,135 +51,199 @@ const motionMountProps = {
   },
 }
 
-const ItemWrapper = forwardRef(
-  ({ item, onDrag, onDragEnd, ...rest }: Props, ref) => {
-    const x = item.desktop.x
-    const y = item.desktop.y
+const ItemWrapper = ({
+  item,
+  onDrag,
+  onDragEnd,
+  onResize,
+  onRemove,
+  children,
+  ...rest
+}: PropsWithChildren<Props>) => {
+  const x = item.desktop.x
+  const y = item.desktop.y
 
-    const [isDragging, setDragging] = useState(false)
-    const [shouldDisplayMenu, setShouldDisplayMenu] = useState(false)
+  const [isDragging, setDragging] = useState(false)
+  const [shouldDisplayMenu, setShouldDisplayMenu] = useState(false)
 
-    return (
-      <GridItem
-        ref={ref}
-        as={MotionCard}
-        role="group"
-        gridColumnStart={x}
-        gridRowStart={y}
-        colSpan={typeof item.desktop.width === "number" ? item.desktop.width : 6}
-        rowSpan={Number(item.desktop.height)}
-        cursor={isDragging ? "grabbing" : "grab"}
-        layout
-        drag
-        onDragStart={() => setDragging(true)}
-        onDrag={onDrag}
-        onDragEnd={(e) => {
-          setDragging(false)
-          onDragEnd(e)
-        }}
-        dragSnapToOrigin
-        zIndex={isDragging ? 1001 : 11}
-        overflow="visible"
-        {...motionMountProps}
-        onHoverStart={() => setShouldDisplayMenu(true)}
-        onHoverEnd={() => setShouldDisplayMenu(false)}
-      >
-        <Flex
-          {...rest} // TODO: not sure why doesn't this work if I add it to the GridItem component directly
-          w="full"
-          h="full"
-          position="relative"
-          alignItems="center"
-          justifyContent="center"
-        >
-          Card
-          <AnimatePresence>
-            {shouldDisplayMenu && !isDragging && (
-              <MotionFlex
-                {...motionMountProps}
-                position="absolute"
-                left={0}
-                right={0}
-                bottom={-11}
-                zIndex="popover"
-              >
-                <HStack
-                  mx="auto"
-                  bgColor="gray.900"
-                  borderRadius="lg"
-                  p={1}
-                  boxShadow="md"
-                  spacing={0}
-                >
-                  <SizePopover />
-                  <IconButton
-                    aria-label="Remove item"
-                    size="sm"
-                    icon={<TrashSimple />}
-                    borderRadius="md"
-                    variant="ghost"
-                  />
-                </HStack>
-              </MotionFlex>
-            )}
-          </AnimatePresence>
-        </Flex>
-      </GridItem>
-    )
-  }
-)
+  const { referenceRef, getPopperProps } = usePopper({
+    placement: "bottom",
+    strategy: "absolute",
+    offset: [0, -4],
+  })
 
-const SizePopover = () => (
-  <Popover>
-    <PopoverTrigger>
-      <IconButton
-        aria-label="Edit size"
-        size="sm"
-        icon={<ArrowsOutSimple />}
-        borderRadius="md"
-        variant="ghost"
-      />
-    </PopoverTrigger>
-    <PopoverContent
-      maxW="max-content"
-      background="gray.900"
-      borderWidth={0}
-      borderRadius="lg"
-      padding={0}
+  return (
+    <MotionCard
+      as={GridItem}
+      role="group"
+      gridColumnStart={x}
+      gridRowStart={y}
+      colSpan={typeof item.desktop.width === "number" ? item.desktop.width : 6}
+      rowSpan={Number(item.desktop.height)}
+      cursor={isDragging ? "grabbing" : "grab"}
+      layout
+      style={{
+        borderRadius: "var(--chakra-radii-2xl)",
+      }}
+      drag
+      onDragStart={() => setDragging(true)}
+      onDrag={onDrag}
+      onDragEnd={(event, info) => {
+        setDragging(false)
+        onDragEnd(event, info)
+      }}
+      whileDrag={{
+        boxShadow: "var(--chakra-shadows-xl)",
+      }}
+      dragSnapToOrigin
+      zIndex={isDragging ? "popover" : undefined}
+      overflow="visible"
+      {...motionMountProps}
+      onHoverStart={() => setShouldDisplayMenu(true)}
+      onHoverEnd={() => setShouldDisplayMenu(false)}
     >
-      <PopoverHeader
-        borderBottom="none"
-        textTransform="uppercase"
-        fontSize="xs"
-        fontWeight="semibold"
-        textAlign="center"
-        textColor="GrayText"
-        pt={1}
-        pb={0}
+      {children}
+
+      <Box
+        {...rest} // TODO: not sure why doesn't this work if I add it to the GridItem component directly
+        w="full"
+        h="full"
+        position="relative"
+        alignItems="center"
+        justifyContent="center"
+        ref={referenceRef}
       >
-        Size
-      </PopoverHeader>
-      <PopoverBody padding={1}>
-        <SimpleGrid spacing={1} columns={6}>
-          {[...Array(18)].map((_, i) => {
-            const row = Math.floor(i / 6)
-            const column = i % 6
-            return (
-              <Box
-                tabIndex={0}
-                key={i}
-                borderWidth={1}
-                borderRadius="sm"
-                bgColor="whiteAlpha.100"
-                boxSize={4}
-                fontSize="xs"
-                onClick={() => console.log(row, column)}
-              />
-            )
-          })}
-        </SimpleGrid>
-      </PopoverBody>
-    </PopoverContent>
-  </Popover>
-)
+        <AnimatePresence>
+          {shouldDisplayMenu && !isDragging && (
+            <MotionBox
+              {...getPopperProps()}
+              initial={{
+                opacity: 0,
+              }}
+              animate={{
+                opacity: 1,
+                transition: { duration: 0.1, ease: EASINGS.easeOut },
+              }}
+              exit={{
+                opacity: 0,
+                transition: { duration: 0.1, ease: EASINGS.easeIn },
+              }}
+              py={2}
+              zIndex="popover"
+              isolation="isolate"
+            >
+              <HStack
+                bgColor="gray.900"
+                borderRadius="lg"
+                p={1}
+                boxShadow="md"
+                spacing={0}
+                cursor="default"
+              >
+                <SizePopover onResize={onResize} />
+                <IconButton
+                  aria-label="Remove item"
+                  size="sm"
+                  icon={<TrashSimple />}
+                  borderRadius="md"
+                  variant="ghost"
+                  onClick={() => onRemove()}
+                />
+              </HStack>
+            </MotionBox>
+          )}
+        </AnimatePresence>
+      </Box>
+    </MotionCard>
+  )
+}
+
+const SizePopover = ({ onResize }: { onResize: Props["onResize"] }) => {
+  // Would be hot af if we could solve this without an extra state / with just plain CSS
+  const [hoveredPosition, setHoveredPosition] = useState<{
+    x: number
+    y: number
+  } | null>(null)
+
+  return (
+    <Popover gutter={10}>
+      <PopoverTrigger>
+        <IconButton
+          aria-label="Edit size"
+          size="sm"
+          icon={<ArrowsOutSimple />}
+          borderRadius="md"
+          variant="ghost"
+        />
+      </PopoverTrigger>
+      <PopoverContent
+        maxW="max-content"
+        borderWidth={0}
+        borderRadius="lg"
+        p={1}
+        sx={{
+          "--popper-bg": "var(--chakra-colors-gray-900)!important",
+          "--popper-arrow-shadow-color": "transparent!important",
+        }}
+      >
+        <PopoverArrow borderColor="red" />
+        <PopoverHeader
+          borderBottom="none"
+          textTransform="uppercase"
+          fontSize="xs"
+          fontWeight="bold"
+          textAlign="center"
+          textColor="GrayText"
+          pt={1}
+          pb={0}
+        >
+          Size
+        </PopoverHeader>
+        <PopoverBody padding={1}>
+          <SimpleGrid
+            spacing={1}
+            columns={6}
+            onMouseLeave={() => setHoveredPosition(null)}
+          >
+            {[...Array(18)].map((_, i) => {
+              const row = Math.floor(i / 6)
+              const column = i % 6
+
+              const x = column + 1
+              const y = row + 1
+
+              const isDisabled = column > 2
+
+              return (
+                <Box
+                  data-x={x}
+                  data-y={y}
+                  tabIndex={0}
+                  key={i}
+                  borderWidth={1}
+                  borderRadius="sm"
+                  bgColor={
+                    x <= hoveredPosition?.x && y <= hoveredPosition?.y
+                      ? "indigo.500"
+                      : "whiteAlpha.100"
+                  }
+                  boxSize={4}
+                  fontSize="xs"
+                  // TODO: eliminate any casts
+                  onClick={() => onResize(x as any, y as any)}
+                  cursor={!isDisabled && "pointer"}
+                  borderStyle={isDisabled ? "dashed" : "solid"}
+                  opacity={isDisabled ? 0.5 : 1}
+                  onMouseOver={
+                    isDisabled ? undefined : () => setHoveredPosition({ x, y })
+                  }
+                />
+              )
+            })}
+          </SimpleGrid>
+        </PopoverBody>
+      </PopoverContent>
+    </Popover>
+  )
+}
 export default ItemWrapper
--- src/pages/page-builder.tsx
+++ src/pages/page-builder.tsx
@@ -1,10 +1,11 @@
-import { Container, EASINGS, Grid, GridItem, Stack } from "@chakra-ui/react"
+import { Box, Container, EASINGS, Grid, GridItem, Stack } from "@chakra-ui/react"
 import Button from "components/common/Button"
 import Card from "components/common/Card"
 import ItemWrapper from "components/page-builder/ItemWrapper"
-import { AnimatePresence, LayoutGroup, motion } from "framer-motion"
+import { AnimatePresence, DragHandlers, LayoutGroup, motion } from "framer-motion"
+import Head from "next/head"
 import { Plus } from "phosphor-react"
-import { DragEventHandler, useRef, useState } from "react"
+import { useRef, useState } from "react"
 import move, { handleWrap } from "utils/pageBuilder"
 import { uuidv7 } from "uuidv7"
 
@@ -129,8 +130,12 @@ const PageBuilder = () => {
 
   const [items, setItems] = useState<Item[]>(initialItems)
 
-  const onDrag: DragEventHandler<HTMLDivElement> = (event) => {
-    if (!containerRef.current) return
+  const onDrag: DragHandlers["onDrag"] = (event, info) => {
+    if (
+      !containerRef.current ||
+      (Math.abs(info.delta.x) < 1 && Math.abs(info.delta.y) < 1)
+    )
+      return
 
     const element = event.target as HTMLDivElement
     const htmlDataItemId = element.getAttribute("data-item-id")
@@ -142,10 +147,7 @@ const PageBuilder = () => {
 
     if (!itemToMove) return
 
-    if (!placeholderPosition) {
-      console.log("setPlaceholderPosition1 called")
-      setPlaceholderPosition(itemToMove.desktop)
-    }
+    if (!placeholderPosition) setPlaceholderPosition(itemToMove.desktop)
 
     const newPosition = elementPositionToXY(element, {
       ...itemToMove.desktop,
@@ -176,129 +178,150 @@ const PageBuilder = () => {
         return isUpdated
       })
     ) {
-      console.log("setPlaceholderPosition2 called")
       setPlaceholderPosition({
         ...itemToMove.desktop,
         ...newPosition,
       })
-
-      console.log("setItems called")
       setItems(itemsClone)
     }
   }
 
   return (
-    <Container
-      maxW="container.lg"
-      py={{ base: 6, md: 9 }}
-      px={{ base: 4, sm: 6, md: 8, lg: 10 }}
-    >
-      <Stack spacing={4}>
-        <Button
-          leftIcon={<Plus />}
-          maxW="max-content"
-          onClick={() =>
-            setItems((prevItems) => {
-              const id = uuidv7()
-              if (!prevItems?.length)
-                return [
-                  {
-                    id,
-                    type: "ROLE",
-                    data: {},
-                    desktop: {
-                      x: 1,
-                      y: 1,
-                      width: 1,
-                      height: 1,
-                    },
-                  } satisfies Item,
-                ]
-
-              // It's safe to select the last item, because the array is sorted properly
-              const lastItem = prevItems.at(-1)
-
-              return [
-                ...prevItems,
-                {
-                  id,
-                  type: "ROLE",
-                  data: {},
-                  desktop: handleWrap({
-                    width: 1,
-                    height: 1,
-                    x: lastItem.desktop.x + lastItem.desktop.width,
-                    y: lastItem.desktop.y,
-                  }),
-                } satisfies Item,
-              ]
-            })
-          }
+    <>
+      <Head>
+        <title>Guild Page Builder</title>
+      </Head>
+      <Box w="full" minH="100vh" overflow="hidden">
+        <Container
+          maxW="container.lg"
+          py={{ base: 6, md: 9 }}
+          px={{ base: 4, sm: 6, md: 8, lg: 10 }}
         >
-          Add card
-        </Button>
+          <Stack spacing={4}>
+            <Button
+              leftIcon={<Plus />}
+              maxW="max-content"
+              onClick={() =>
+                setItems((prevItems) => {
+                  const id = uuidv7()
+                  if (!prevItems?.length)
+                    return [
+                      {
+                        id,
+                        type: "ROLE",
+                        data: {},
+                        desktop: {
+                          x: 1,
+                          y: 1,
+                          width: 1,
+                          height: 1,
+                        },
+                      } satisfies Item,
+                    ]
 
-        <Grid
-          templateColumns="repeat(6, 1fr)"
-          gridAutoRows="minmax(145px, auto)"
-          gap="0.875rem"
-          ref={containerRef}
-        >
-          <LayoutGroup>
-            <AnimatePresence>
-              {items?.map((item) => (
-                <ItemWrapper
-                  key={item.id}
-                  data-item-id={item.id.toString()}
-                  item={item}
-                  onDrag={onDrag}
-                  onDragEnd={() => setPlaceholderPosition(null)}
-                />
-              ))}
+                  // It's safe to select the last item, because the array is sorted properly
+                  const lastItem = prevItems.at(-1)
 
-              {placeholderPosition && (
-                <GridItem
-                  as={MotionCard}
-                  initial={{
-                    opacity: 0,
-                    scale: 0.95,
-                  }}
-                  animate={{
-                    opacity: 1,
-                    scale: 1,
-                    transition: { duration: 0.1, ease: EASINGS.easeOut },
-                  }}
-                  exit={{
-                    opacity: 0,
-                    scale: 0.95,
-                    transition: { duration: 0.1, ease: EASINGS.easeIn },
-                  }}
-                  layout
-                  zIndex={10}
-                  background="transparent"
-                  border={2}
-                  shadow="none"
-                  borderColor="whiteAlpha.300"
-                  borderStyle="dashed"
-                  gridColumnStart={placeholderPosition.x}
-                  gridRowStart={placeholderPosition.y}
-                  colSpan={
-                    typeof placeholderPosition.width === "number"
-                      ? placeholderPosition.width
-                      : 6
-                  }
-                  rowSpan={
-                    typeof placeholderPosition.height === "number"
-                      ? placeholderPosition.height
-                      : 1
-                  }
-                />
-              )}
-            </AnimatePresence>
-          </LayoutGroup>
-        </Grid>
-      </Stack>
-    </Container>
+                  return [
+                    ...prevItems,
+                    {
+                      id,
+                      type: "ROLE",
+                      data: {},
+                      desktop: handleWrap({
+                        width: 1,
+                        height: 1,
+                        x: lastItem.desktop.x + lastItem.desktop.width,
+                        y: lastItem.desktop.y,
+                      }),
+                    } satisfies Item,
+                  ]
+                })
+              }
+            >
+              Add card
+            </Button>
+
+            <Grid
+              templateColumns="repeat(6, 1fr)"
+              gridAutoRows="minmax(145px, auto)"
+              gap="0.875rem"
+              ref={containerRef}
+            >
+              <LayoutGroup>
+                {placeholderPosition && (
+                  <GridItem
+                    as={MotionCard}
+                    initial={{
+                      opacity: 0,
+                      scale: 0.95,
+                    }}
+                    animate={{
+                      opacity: 1,
+                      scale: 1,
+                      transition: { duration: 0.1, ease: EASINGS.easeOut },
+                    }}
+                    exit={{
+                      opacity: 0,
+                      scale: 0.95,
+                      transition: { duration: 0.1, ease: EASINGS.easeIn },
+                    }}
+                    layout
+                    zIndex={10}
+                    background="transparent"
+                    border={2}
+                    shadow="none"
+                    borderColor="whiteAlpha.300"
+                    borderStyle="dashed"
+                    gridColumnStart={placeholderPosition.x}
+                    gridRowStart={placeholderPosition.y}
+                    colSpan={
+                      typeof placeholderPosition.width === "number"
+                        ? placeholderPosition.width
+                        : 6
+                    }
+                    rowSpan={
+                      typeof placeholderPosition.height === "number"
+                        ? placeholderPosition.height
+                        : 1
+                    }
+                  />
+                )}
+
+                <AnimatePresence>
+                  {items?.map((item) => (
+                    <ItemWrapper
+                      key={item.id}
+                      data-item-id={item.id}
+                      item={item}
+                      onDrag={onDrag}
+                      onDragEnd={() => setPlaceholderPosition(null)}
+                      onResize={(width, height) => {
+                        const itemsClone = structuredClone(items)
+                        const itemToResize = itemsClone.find(
+                          (_item) => _item.id === item.id
+                        )
+                        move(itemsClone, itemToResize, {
+                          ...item.desktop,
+                          width,
+                          height,
+                        })
+                        setItems(itemsClone)
+                      }}
+                      onRemove={() =>
+                        setItems((prevItems) =>
+                          prevItems.filter(({ id }) => id !== item.id)
+                        )
+                      }
+                    />
+                  ))}
+                </AnimatePresence>
+              </LayoutGroup>
+            </Grid>
+          </Stack>
+        </Container>
+      </Box>
+    </>
   )
 }
 
