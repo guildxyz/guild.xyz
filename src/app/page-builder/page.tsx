41d5f9e23b6a3efac867919cd5835589b72561e1
BrickheadJohnny <johnny.zgen@gmail.com>
2024-02-24 08:52:47 +0100

    fix: remove debug text

--- src/app/page-builder/page.tsx
+++ src/app/page-builder/page.tsx
@@ -1,4 +1,4 @@
-import { Container, Text } from "@chakra-ui/react"
+import { Container } from "@chakra-ui/react"
 import PageBuilder from "./components/PageBuilder"
 
 const page = () => (
@@ -7,7 +7,6 @@ const page = () => (
     py={{ base: 6, md: 9 }}
     px={{ base: 4, sm: 6, md: 8, lg: 10 }}
   >
-    <Text fontFamily="display">Asd</Text>
     <PageBuilder />
   </Container>
 )
