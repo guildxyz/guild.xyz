d2035818c34d26ed8334460fa9c6fb639822eb1f
BrickheadJohnny <johnny.zgen@gmail.com>
2024-02-28 17:25:59 +0100

    feat: useEditRequirement hook

---
+++ src/components/create-guild/Requirements/hooks/useEditRequirement.ts
@@ -0,0 +1,60 @@
+import useGuild from "components/[guild]/hooks/useGuild"
+import useShowErrorToast from "hooks/useShowErrorToast"
+import useSubmit from "hooks/useSubmit"
+import useToast from "hooks/useToast"
+import { Requirement } from "types"
+import { useFetcherWithSign } from "utils/fetcher"
+
+const useEditRequirement = (roleId: number, config?: { onSuccess?: () => void }) => {
+  const { id: guildId, mutateGuild } = useGuild()
+  const toast = useToast()
+  const showErrorToast = useShowErrorToast()
+
+  const fetcherWithSign = useFetcherWithSign()
+
+  const editRequirement = async (data: Requirement): Promise<Requirement> =>
+    fetcherWithSign([
+      `/v2/guilds/${guildId}/roles/${roleId}/requirements/${data.id}`,
+      {
+        method: "PUT",
+        body: {
+          ...data,
+          // Removing fields which are only used on the frontend
+          id: undefined,
+          requirementId: undefined,
+          logic: undefined,
+          formFieldId: undefined,
+          balancyDecimals: undefined,
+        },
+      },
+    ])
+
+  return useSubmit<Requirement, Requirement>(editRequirement, {
+    onSuccess: (editedRequirement) => {
+      toast({
+        status: "success",
+        title: "Successfully updated requirement",
+      })
+
+      mutateGuild((prevGuild) => ({
+        ...prevGuild,
+        roles: prevGuild.roles.map((role) => {
+          if (role.id !== roleId) return role
+
+          return {
+            ...role,
+            requirements: role.requirements.map((requirement) => {
+              if (requirement.id !== editedRequirement.id) return requirement
+              return editedRequirement
+            }),
+          }
+        }),
+      }))
+
+      config?.onSuccess?.()
+    },
+    onError: (error) => showErrorToast(error),
+  })
+}
+
+export default useEditRequirement
