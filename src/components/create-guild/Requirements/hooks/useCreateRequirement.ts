85f6609d0bb8b0e695c72ab6b57c28b5c1546d4f
BrickheadJohnny <johnny.zgen@gmail.com>
2024-02-28 17:25:30 +0100

    feat: useCreateRequirement hook

---
+++ src/components/create-guild/Requirements/hooks/useCreateRequirement.ts
@@ -0,0 +1,43 @@
+import useGuild from "components/[guild]/hooks/useGuild"
+import useShowErrorToast from "hooks/useShowErrorToast"
+import { SignedValidation, useSubmitWithSign } from "hooks/useSubmit"
+import useToast from "hooks/useToast"
+import { Requirement } from "types"
+import fetcher from "utils/fetcher"
+
+const useCreateRequirement = (
+  roleId: number,
+  config?: { onSuccess?: () => void }
+) => {
+  const { id: guildId, mutateGuild } = useGuild()
+  const toast = useToast()
+  const showErrorToast = useShowErrorToast()
+
+  const createRequirement = async (
+    signedValidation: SignedValidation
+  ): Promise<Requirement> =>
+    fetcher(`/v2/guilds/${guildId}/roles/${roleId}/requirements`, signedValidation)
+
+  return useSubmitWithSign<Requirement>(createRequirement, {
+    onSuccess: (newRequirement) => {
+      toast({
+        status: "success",
+        title: "Successfully created requirement",
+      })
+
+      mutateGuild((prevGuild) => ({
+        ...prevGuild,
+        roles: prevGuild.roles.map((role) => {
+          if (role.id !== roleId) return role
+
+          return { ...role, requirements: [...role.requirements, newRequirement] }
+        }),
+      }))
+
+      config?.onSuccess?.()
+    },
+    onError: (error) => showErrorToast(error),
+  })
+}
+
+export default useCreateRequirement
