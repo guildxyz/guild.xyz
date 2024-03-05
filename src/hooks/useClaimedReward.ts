c93d41f5702a0a065b743d5cca27c16b0235a431
FBalint <balint@zgen.hu>
2024-03-05 15:09:10 +0100

    fix: remove unused mutate

--- src/hooks/useClaimedReward.ts
+++ src/hooks/useClaimedReward.ts
@@ -2,7 +2,7 @@ import { RolePlatform } from "../types"
 import { useUserRewards } from "./useUserRewards"
 
 export const useClaimedReward = (rolePlatformId: RolePlatform["id"]) => {
-  const { isLoading, mutate, data: userRewards } = useUserRewards()
+  const { isLoading, data: userRewards } = useUserRewards()
   const userReward = userRewards?.find(
     (reward) => reward.rolePlatformId === rolePlatformId
   )
@@ -11,6 +11,5 @@ export const useClaimedReward = (rolePlatformId: RolePlatform["id"]) => {
     isLoading,
     claimed: userReward?.claimed,
     uniqueValue: userReward?.uniqueValue,
-    mutate,
   }
 }
