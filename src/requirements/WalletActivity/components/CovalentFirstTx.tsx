3f83f32f5140cf39f6263e0efe50c9ee21cb5360
BrickheadJohnny <johnny.zgen@gmail.com>
2024-03-07 08:01:46 +0100

    fix: remove unnecessary fragments

--- src/requirements/WalletActivity/components/CovalentFirstTx.tsx
+++ src/requirements/WalletActivity/components/CovalentFirstTx.tsx
@@ -2,20 +2,18 @@ import { Text } from "@chakra-ui/react"
 import { RequirementFormProps } from "requirements"
 import MinMaxAmountFormControls from "requirements/Github/components/MinMaxAmountFormControls"
 
-const CovalentFirstTx = ({ baseFieldPath }: RequirementFormProps): JSX.Element => {
-  return (
-    <>
-      <Text colorScheme="gray" fontSize="sm" mt={-2} pl={3}>
-        A wallet's age is determined by the time of its first transaction.
-      </Text>
+const CovalentFirstTx = ({ baseFieldPath }: RequirementFormProps): JSX.Element => (
+  <>
+    <Text colorScheme="gray" fontSize="sm" mt={-2} pl={3}>
+      A wallet's age is determined by the time of its first transaction.
+    </Text>
 
-      <MinMaxAmountFormControls
-        baseFieldPath={baseFieldPath}
-        minAmountLabel="Wallet created after (date)"
-        maxAmountLabel="Wallet created before (date)"
-      />
-    </>
-  )
-}
+    <MinMaxAmountFormControls
+      baseFieldPath={baseFieldPath}
+      minAmountLabel="Wallet created after (date)"
+      maxAmountLabel="Wallet created before (date)"
+    />
+  </>
+)
 
 export default CovalentFirstTx
--- src/requirements/WalletActivity/components/CovalentFirstTxRelative.tsx
+++ src/requirements/WalletActivity/components/CovalentFirstTxRelative.tsx
@@ -4,20 +4,18 @@ import MinMaxAmountFormControls from "requirements/Github/components/MinMaxAmoun
 
 const CovalentFirstTxRelative = ({
   baseFieldPath,
-}: RequirementFormProps): JSX.Element => {
-  return (
-    <>
-      <Text colorScheme="gray" fontSize="sm" mt={-2} pl={3}>
-        A wallet's age is determined by the time of its first transaction.
-      </Text>
-      <MinMaxAmountFormControls
-        baseFieldPath={baseFieldPath}
-        type="RELATIVE"
-        minAmountLabel="Wallet younger than (period)"
-        maxAmountLabel="Wallet older than (period)"
-      />
-    </>
-  )
-}
+}: RequirementFormProps): JSX.Element => (
+  <>
+    <Text colorScheme="gray" fontSize="sm" mt={-2} pl={3}>
+      A wallet's age is determined by the time of its first transaction.
+    </Text>
+    <MinMaxAmountFormControls
+      baseFieldPath={baseFieldPath}
+      type="RELATIVE"
+      minAmountLabel="Wallet younger than (period)"
+      maxAmountLabel="Wallet older than (period)"
+    />
+  </>
+)
 
 export default CovalentFirstTxRelative
