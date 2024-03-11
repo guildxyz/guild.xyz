3a28b5de57b695e2a8ae7c5693f53df05afc7970
FBalint <balint@zgen.hu>
2024-03-11 10:45:07 +0100

    refactor: remove motion component

--- src/components/common/StyledSelect/components/CustomIndicatorsContainer.tsx
+++ src/components/common/StyledSelect/components/CustomIndicatorsContainer.tsx
@@ -1,13 +1,9 @@
-import { IconButton, useColorModeValue } from "@chakra-ui/react"
+import { IconButton, theme, useClipboard, useColorModeValue } from "@chakra-ui/react"
 import { transparentize } from "@chakra-ui/theme-tools"
 import { GroupBase, IndicatorsContainerProps, components } from "chakra-react-select"
-import { motion } from "framer-motion"
 import { Check, Copy } from "phosphor-react"
-import { useState } from "react"
 import parseFromObject from "utils/parseFromObject"
 import { StyledSelectProps } from "../StyledSelect"
-
-const MotionIconButton = motion(IconButton)
 
 const CustomIndicatorsContainer = ({
   children,
@@ -17,34 +13,25 @@ const CustomIndicatorsContainer = ({
   value: StyledSelectProps["value"]
   pathToCopy: string
 } & IndicatorsContainerProps<unknown, boolean, GroupBase<unknown>>) => {
-  const [copied, setCopied] = useState(false)
   const successIconColor = useColorModeValue("green.800", "green.200")
 
-  const handleCopyToClipboard = async () => {
-    try {
-      await navigator.clipboard.writeText(parseFromObject(value, "value"))
-      setCopied(true)
-      setTimeout(() => {
-        setCopied(false)
-      }, 1000)
-    } catch (err) {
-      console.error("Failed to copy:", err)
-    }
-  }
+  const { onCopy, hasCopied } = useClipboard(parseFromObject(value, "value"), 1000)
 
   return (
     <components.IndicatorsContainer {...rest}>
       {value && (
-        <MotionIconButton
-          bg={copied && transparentize(successIconColor, 0.12)}
-          color={copied && successIconColor}
-          icon={copied ? <Check /> : <Copy />}
+        <IconButton
+          backgroundColor={
+            hasCopied && transparentize(successIconColor, 0.12)(theme)
+          }
+          color={hasCopied && successIconColor}
+          icon={hasCopied ? <Check /> : <Copy />}
           aria-label={"Copy"}
           size={"xs"}
           rounded={"full"}
           variant={"ghost"}
           onMouseDown={() => {
-            handleCopyToClipboard()
+            onCopy()
           }}
         />
       )}
