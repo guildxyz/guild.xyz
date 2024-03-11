620977cecac5f3bac650c05c451031a62356cd75
Gabor <gabor@zgen.hu>
2024-03-11 21:32:51 +0100

    Add button that navigate to Form Responses page

---
+++ src/components/[guild]/activity/ActivityLogAction/components/ClickableTagPopover/components/ViewInFormResponses.tsx
@@ -0,0 +1,37 @@
+import useGuild from "components/[guild]/hooks/useGuild"
+import Button from "components/common/Button"
+import { useRouter } from "next/router"
+import { ArrowSquareOut } from "phosphor-react"
+
+type Props = {
+  label: string
+  formId: number
+  guildId: number
+  userId: number
+}
+
+const ViewInFormResponses = ({
+  label,
+  formId,
+  guildId,
+  userId,
+}: Props): JSX.Element => {
+  const router = useRouter()
+  const { urlName } = useGuild(guildId)
+
+  return (
+    <Button
+      variant="ghost"
+      leftIcon={<ArrowSquareOut />}
+      size="sm"
+      borderRadius={0}
+      onClick={() =>
+        router.push(`/${urlName}/forms/${formId}/responses?search=${userId}`)
+      }
+      justifyContent="start"
+    >
+      {label}
+    </Button>
+  )
+}
+export default ViewInFormResponses
