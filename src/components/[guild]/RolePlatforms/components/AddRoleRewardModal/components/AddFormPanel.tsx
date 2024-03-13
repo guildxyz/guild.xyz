b65de29138c8a036db2f12ba9095c0850d5bdeb4
Gabor <gabor@zgen.hu>
2024-03-13 19:16:51 +0100

    Add select for non connected forms

--- src/components/[guild]/RolePlatforms/components/AddRoleRewardModal/components/AddFormPanel.tsx
+++ src/components/[guild]/RolePlatforms/components/AddRoleRewardModal/components/AddFormPanel.tsx
@@ -1,13 +1,17 @@
-import { Stack } from "@chakra-ui/react"
+import { Box, FormLabel, Stack } from "@chakra-ui/react"
 import { Schemas } from "@guildxyz/types"
 import { zodResolver } from "@hookform/resolvers/zod"
 import CreateFormForm from "components/[guild]/CreateFormModal/components/CreateFormForm"
 import useCreateForm from "components/[guild]/CreateFormModal/hooks/useCreateForm"
 import { FormCreationSchema } from "components/[guild]/CreateFormModal/schemas"
+import useGuild from "components/[guild]/hooks/useGuild"
 import Button from "components/common/Button"
+import StyledSelect from "components/common/StyledSelect"
 import { ArrowRight } from "phosphor-react"
 import { AddPlatformPanelProps } from "platforms/platforms"
+import { useMemo } from "react"
 import { FormProvider, useForm, useWatch } from "react-hook-form"
+import useSWRImmutable from "swr/immutable"
 import { PlatformGuildData, PlatformType } from "types"
 import { uuidv7 } from "uuidv7"
 
@@ -27,13 +31,34 @@ export type CreateForm = Omit<Schemas["FormCreationPayload"], "fields"> & {
   fields: MapOptions<Schemas["FormCreationPayload"]["fields"][number]>[]
 }
 
-const defaultValues: CreateForm = {
+const useGetNotConnectedForms = () => {
+  const { id: guildId, guildPlatforms } = useGuild()
+
+  const { data, isLoading } = useSWRImmutable<Schemas["Form"][]>(
+    `/v2/guilds/${guildId}/forms/`
+  )
+
+  const notConnectedForms = useMemo(() => {
+    if (isLoading) return
+    return data.filter(
+      (form) =>
+        !guildPlatforms.filter((fgp) => fgp.platformGuildData?.formId === form.id)
+          .length
+    )
+  }, [guildPlatforms, data, isLoading])
+  return { notConnectedForms, isLoading }
+}
+
+const defaultValues = {
   name: "",
   description: "",
   fields: [],
 }
 
 const AddFormPanel = ({ onAdd }: AddPlatformPanelProps) => {
+  const { notConnectedForms, isLoading: isRawFormsLoading } =
+    useGetNotConnectedForms()
+
   const methods = useForm<CreateForm>({
     mode: "all",
     resolver: zodResolver(FormCreationSchema),
@@ -71,6 +96,29 @@ const AddFormPanel = ({ onAdd }: AddPlatformPanelProps) => {
   return (
     <FormProvider {...methods}>
       <Stack spacing={6}>
+        <Box maxW="64">
+          <FormLabel> Select existing forms</FormLabel>
+          <StyledSelect
+            isLoading={isRawFormsLoading}
+            onChange={({ value }) =>
+              onAdd({
+                guildPlatform: {
+                  platformName: "FORM",
+                  platformId: value.FORM,
+                  platformGuildId: `form-${value.id}`,
+                  platformGuildData: {
+                    formId: value.id,
+                  } satisfies PlatformGuildData["FORM"],
+                },
+                isNew: false,
+              })
+            }
+            options={notConnectedForms?.map((form) => ({
+              label: form.name,
+              value: form,
+            }))}
+          />
+        </Box>
         <CreateFormForm />
         <Button
           colorScheme="green"
