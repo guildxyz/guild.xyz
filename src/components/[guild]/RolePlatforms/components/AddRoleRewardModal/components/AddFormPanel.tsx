ef994238ae5ffba06b362320ec0ed11688466ac5
valid <valid@zgen.hu>
2024-03-15 01:31:11 +0100

    AddFormPanel: UI impros & extract alert into separate component

--- src/components/[guild]/RolePlatforms/components/AddRoleRewardModal/components/AddFormPanel.tsx
+++ src/components/[guild]/RolePlatforms/components/AddRoleRewardModal/components/AddFormPanel.tsx
@@ -4,7 +4,8 @@ import {
   AlertIcon,
   AlertTitle,
   Box,
-  HStack,
+  Center,
+  Collapse,
   Stack,
 } from "@chakra-ui/react"
 import { Schemas } from "@guildxyz/types"
@@ -16,8 +17,8 @@ import useGuild from "components/[guild]/hooks/useGuild"
 import Button from "components/common/Button"
 import StyledSelect from "components/common/StyledSelect"
 import { ArrowRight } from "phosphor-react"
-import { useMemo } from "react"
 import { AddRewardPanelProps } from "platforms/rewards"
+import { useMemo } from "react"
 import { FormProvider, useForm, useWatch } from "react-hook-form"
 import useSWRImmutable from "swr/immutable"
 import { PlatformGuildData, PlatformType } from "types"
@@ -39,34 +40,13 @@ export type CreateForm = Omit<Schemas["FormCreationPayload"], "fields"> & {
   fields: MapOptions<Schemas["FormCreationPayload"]["fields"][number]>[]
 }
 
-const useGetNotConnectedForms = () => {
-  const { id: guildId, guildPlatforms } = useGuild()
-
-  const { data, isLoading } = useSWRImmutable<Schemas["Form"][]>(
-    `/v2/guilds/${guildId}/forms/`
-  )
-
-  const notConnectedForms = useMemo(() => {
-    if (isLoading) return
-    return data.filter(
-      (form) =>
-        !guildPlatforms.filter((fgp) => fgp.platformGuildData?.formId === form.id)
-          .length
-    )
-  }, [guildPlatforms, data, isLoading])
-  return { notConnectedForms, isLoading }
-}
-
-const defaultValues = {
+const defaultValues: CreateForm = {
   name: "",
   description: "",
   fields: [],
 }
 
 const AddFormPanel = ({ onAdd }: AddRewardPanelProps) => {
-  const { notConnectedForms, isLoading: isRawFormsLoading } =
-    useGetNotConnectedForms()
-
   const methods = useForm<CreateForm>({
     mode: "all",
     resolver: zodResolver(FormCreationSchema),
@@ -104,49 +84,7 @@ const AddFormPanel = ({ onAdd }: AddRewardPanelProps) => {
   return (
     <FormProvider {...methods}>
       <Stack spacing={6}>
-        {!isLoading && notConnectedForms?.length && (
-          <Alert
-            status="info"
-            alignItems={{ base: "start", md: "center" }}
-            pb={5}
-            flexDirection={{ base: "column", md: "row" }}
-            gap={{ base: 5, md: 3 }}
-          >
-            <HStack>
-              <AlertIcon />
-              <Stack>
-                <AlertTitle>Continue with existing form?</AlertTitle>
-                <AlertDescription>
-                  There're forms that you've created but haven't added as a reward
-                  yet.
-                </AlertDescription>
-              </Stack>
-            </HStack>
-
-            <Box flexShrink={0} w={{ base: "100%", md: 48 }}>
-              <StyledSelect
-                isLoading={isRawFormsLoading}
-                onChange={({ value }) =>
-                  onAdd({
-                    guildPlatform: {
-                      platformName: "FORM",
-                      platformId: value.FORM,
-                      platformGuildId: `form-${value.id}`,
-                      platformGuildData: {
-                        formId: value.id,
-                      } satisfies PlatformGuildData["FORM"],
-                    },
-                    isNew: true,
-                  })
-                }
-                options={notConnectedForms?.map((form) => ({
-                  label: form.name,
-                  value: form,
-                }))}
-              />
-            </Box>
-          </Alert>
-        )}
+        <ContinueWithExistingFormAlert {...{ onAdd }} />
         <CreateFormForm />
         <Button
           colorScheme="green"
@@ -162,6 +100,72 @@ const AddFormPanel = ({ onAdd }: AddRewardPanelProps) => {
         </Button>
       </Stack>
     </FormProvider>
+  )
+}
+
+const useGetNotConnectedForms = () => {
+  const { id: guildId, guildPlatforms } = useGuild()
+
+  const { data, isLoading } = useSWRImmutable<Schemas["Form"][]>(
+    `/v2/guilds/${guildId}/forms/`
+  )
+
+  const notConnectedForms = useMemo(() => {
+    if (isLoading) return
+    return data.filter(
+      (form) =>
+        !guildPlatforms.filter((fgp) => fgp.platformGuildData?.formId === form.id)
+          .length
+    )
+  }, [guildPlatforms, data, isLoading])
+  return { notConnectedForms, isLoading }
+}
+
+const ContinueWithExistingFormAlert = ({ onAdd }) => {
+  const { notConnectedForms, isLoading: isRawFormsLoading } =
+    useGetNotConnectedForms()
+
+  return (
+    <Collapse in={!!notConnectedForms?.length}>
+      <Alert status="info">
+        <AlertIcon />
+        <Stack
+          direction={{ base: "column", md: "row" }}
+          justifyContent={"space-between"}
+          gap={{ base: 3, md: 4 }}
+          w="full"
+        >
+          <Box>
+            <AlertTitle>Continue with existing form?</AlertTitle>
+            <AlertDescription>
+              There're forms that you've created but haven't added as a reward yet
+            </AlertDescription>
+          </Box>
+          <Center flexShrink={0} w="full" maxW={48}>
+            <StyledSelect
+              isLoading={isRawFormsLoading}
+              onChange={({ value }) =>
+                onAdd({
+                  guildPlatform: {
+                    platformName: "FORM",
+                    platformId: value.FORM,
+                    platformGuildId: `form-${value.id}`,
+                    platformGuildData: {
+                      formId: value.id,
+                    } satisfies PlatformGuildData["FORM"],
+                  },
+                  isNew: true,
+                })
+              }
+              options={notConnectedForms?.map((form) => ({
+                label: form.name,
+                value: form,
+              }))}
+            />
+          </Center>
+        </Stack>
+      </Alert>
+    </Collapse>
   )
 }
 
