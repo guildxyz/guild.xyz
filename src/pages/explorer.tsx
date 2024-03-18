3006ab9ae1cde1cae4aa4bba398492a1081fbc14
FBalint <balint@zgen.hu>
2024-03-18 10:26:38 +0100

    fix: 'manual' scroll restoration

--- src/pages/explorer.tsx
+++ src/pages/explorer.tsx
@@ -6,14 +6,18 @@ import ExploreAllGuilds from "components/explorer/ExploreAllGuilds"
 import ExplorerTabs from "components/explorer/ExplorerTabs"
 import GoToCreateGuildButton from "components/explorer/GoToCreateGuildButton"
 import YourGuilds, { useYourGuilds } from "components/explorer/YourGuilds"
+import { atom, useAtom } from "jotai"
 import { GetStaticProps } from "next"
-import { useRef } from "react"
+import { useRouter } from "next/router"
+import { useEffect, useRef } from "react"
 import { GuildBase } from "types"
 import fetcher from "utils/fetcher"
 
 type Props = {
   guilds: GuildBase[]
 }
+
+const ExplorerScrollPositionAtom = atom(0)
 
 const Page = ({ guilds: guildsInitial }: Props): JSX.Element => {
   const yourGuildsRef = useRef(null)
@@ -24,6 +28,42 @@ const Page = ({ guilds: guildsInitial }: Props): JSX.Element => {
   const bgColor = useColorModeValue("var(--chakra-colors-gray-800)", "#37373a") // dark color is from whiteAlpha.200, but without opacity so it can overlay the banner image
   const bgOpacity = useColorModeValue(0.06, 0.1)
   const bgLinearPercentage = useBreakpointValue({ base: "50%", sm: "55%" })
+
+  const router = useRouter()
+  const [explorerScrollPosition, setExplorerScrollPositionAtom] = useAtom(
+    ExplorerScrollPositionAtom
+  )
+
+  useEffect(() => {
+    const handleRouteChangeStart = () => {
+      setExplorerScrollPositionAtom(window.scrollY)
+    }
+
+    const handleRouteChangeComplete = () => {
+      /**
+       * For some reason, without the delay, the scrolling is not executed. It might
+       * be caused by the default 'scrollRestoration', which probably overwrites our
+       * own scrolling, if not delayed.
+       */
+      setTimeout(
+        () =>
+          window.scrollTo({
+            top: explorerScrollPosition,
+            left: 0,
+            behavior: "smooth",
+          }),
+        10
+      )
+    }
+
+    router.events.on("routeChangeStart", handleRouteChangeStart)
+    router.events.on("routeChangeComplete", handleRouteChangeComplete)
+
+    return () => {
+      router.events.off("routeChangeStart", handleRouteChangeStart)
+      router.events.off("routeChangeComplete", handleRouteChangeComplete)
+    }
+  }, [router, explorerScrollPosition])
 
   return (
     <>
