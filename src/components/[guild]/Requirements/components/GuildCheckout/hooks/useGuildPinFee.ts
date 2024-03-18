b41703aab3c9bec008b8d69e5d62d6b416c1619a
BrickheadJohnny <johnny.zgen@gmail.com>
2024-03-18 15:06:38 +0100

    fix(useGuildPinFee): use `publicClient.readContract`

--- src/components/[guild]/Requirements/components/GuildCheckout/hooks/useGuildPinFee.ts
+++ src/components/[guild]/Requirements/components/GuildCheckout/hooks/useGuildPinFee.ts
@@ -1,8 +1,27 @@
 import useGuild from "components/[guild]/hooks/useGuild"
 import guildPinAbi from "static/abis/guildPin"
+import useSWR from "swr"
 import { GUILD_PIN_CONTRACTS } from "utils/guildCheckout/constants"
-import { useReadContract } from "wagmi"
-import { Chains } from "wagmiConfig/chains"
+import { createPublicClient, http, type Chain as ViemChain } from "viem"
+import { wagmiConfig } from "wagmiConfig"
+import { Chains, type Chain } from "wagmiConfig/chains"
+
+/**
+ * For some reason, `useReadContract` didn't work on Ontology EVM, so we use the
+ * simple `publicClient.readContract` action instead
+ */
+const fetchFee = (chain: Chain) => {
+  const publicClient = createPublicClient({
+    chain: wagmiConfig.chains.find((c) => Chains[c.id] === chain) as ViemChain,
+    transport: http(),
+  })
+
+  return publicClient.readContract({
+    abi: guildPinAbi,
+    address: GUILD_PIN_CONTRACTS[chain],
+    functionName: "fee",
+  })
+}
 
 const useGuildPinFee = (): {
   guildPinFee: bigint
@@ -12,18 +31,24 @@ const useGuildPinFee = (): {
   const { guildPin } = useGuild()
 
   const {
-    data,
+    data: guildPinFee,
     isLoading: isGuildPinFeeLoading,
     error: guildPinFeeError,
-  } = useReadContract({
-    abi: guildPinAbi,
-    address: GUILD_PIN_CONTRACTS[guildPin.chain],
-    functionName: "fee",
-    chainId: Chains[guildPin.chain],
-  })
+  } = useSWR(["fee", guildPin.chain], ([_, c]) => fetchFee(c as Chain))
+
+  // const {
+  //   data,
+  //   isLoading: isGuildPinFeeLoading,
+  //   error: guildPinFeeError,
+  // } = useReadContract({
+  //   abi: guildPinAbi,
+  //   address: GUILD_PIN_CONTRACTS[guildPin.chain],
+  //   functionName: "fee",
+  //   chainId: Chains[guildPin.chain],
+  // })
 
   return {
-    guildPinFee: data as unknown as bigint,
+    guildPinFee,
     isGuildPinFeeLoading,
     guildPinFeeError,
   }
