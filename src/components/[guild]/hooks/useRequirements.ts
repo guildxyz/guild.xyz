17d589de6e3d275a480341e7d77b7e09fd86790f
BrickheadJohnny <johnny.zgen@gmail.com>
2024-03-26 16:12:18 +0100

    fix(useRequirements): use the existing type for now

--- src/components/[guild]/hooks/useRequirements.ts
+++ src/components/[guild]/hooks/useRequirements.ts
@@ -1,12 +1,12 @@
-import { Schemas } from "@guildxyz/types"
 import useSWRImmutable from "swr/immutable"
+import { Requirement } from "types"
 import useGuild from "./useGuild"
 
 const useRequirements = (roleId: number) => {
   const { id: guildId } = useGuild()
 
   // TODO: authenticated request
-  return useSWRImmutable<Schemas["Requirement"][]>(
+  return useSWRImmutable<Requirement[]>(
     guildId && roleId ? `/v2/guilds/${guildId}/roles/${roleId}/requirements` : null
   )
 }
