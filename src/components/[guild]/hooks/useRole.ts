b3377d17404b7a82c17fdcc7dfdf9b968477581d
BrickheadJohnny <johnny.zgen@gmail.com>
2024-04-02 11:55:31 +0200

    fix(useRole): check data in `guild-page` cache before fetching

--- src/components/[guild]/hooks/useRole.ts
+++ src/components/[guild]/hooks/useRole.ts
@@ -1,19 +1,28 @@
 import useSWRImmutable from "swr/immutable"
 import { SimpleRole } from "types"
 import { useFetcherWithSign } from "utils/fetcher"
+import useGuild from "./useGuild"
 
 const useRole = (guildId: number | string, roleId: number) => {
   const url = `/v2/guilds/${guildId}/roles/${roleId}`
 
+  /**
+   * If the role is from the current guild, we don't need to fetch it separately,
+   * since we already have the necessary data in the `useGuild` SWR cache
+   */
+  const { id, roles } = useGuild()
+  const isFromCurrentGuild = guildId === id
+  const shouldFetch = !!guildId && !!roleId && !isFromCurrentGuild
+
   const { data: unauthenticatedData, isLoading: isUnauthenticatedRequestLoading } =
-    useSWRImmutable<SimpleRole>(guildId && roleId ? url : null, {
+    useSWRImmutable<SimpleRole>(shouldFetch ? url : null, {
       shouldRetryOnError: false,
     })
 
   const fetcherWithSign = useFetcherWithSign()
   const { data: authenticatedData, isLoading: isAuthenticatedRequestLoading } =
     useSWRImmutable<SimpleRole>(
-      guildId && roleId && !isUnauthenticatedRequestLoading && !unauthenticatedData
+      shouldFetch && !isUnauthenticatedRequestLoading && !unauthenticatedData
         ? [url, { method: "GET", body: {} }]
         : null,
       fetcherWithSign,
@@ -23,7 +32,9 @@ const useRole = (guildId: number | string, roleId: number) => {
     )
 
   return {
-    ...(unauthenticatedData || authenticatedData),
+    ...(isFromCurrentGuild
+      ? roles.find((role) => role.id === roleId)
+      : unauthenticatedData || authenticatedData),
     isLoading: isUnauthenticatedRequestLoading || isAuthenticatedRequestLoading,
   }
 }
