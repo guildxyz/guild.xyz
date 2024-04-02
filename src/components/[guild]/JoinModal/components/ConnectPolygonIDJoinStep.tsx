95a74f647c910c7d069dac6acadec5d544c2213c
BrickheadJohnny <johnny.zgen@gmail.com>
2024-04-02 15:04:08 +0200

    feat(ConnectPolygonIDJoinStep): use the `gate-proof-existence` endpoint

--- src/components/[guild]/JoinModal/components/ConnectPolygonIDJoinStep.tsx
+++ src/components/[guild]/JoinModal/components/ConnectPolygonIDJoinStep.tsx
@@ -1,6 +1,5 @@
 import { Img, useDisclosure } from "@chakra-ui/react"
-import useGuild from "components/[guild]/hooks/useGuild"
-import useMembership from "components/explorer/hooks/useMembership"
+import useSWRWithOptionalAuth from "hooks/useSWRWithOptionalAuth"
 import { ConnectPolygonIDModal } from "requirements/PolygonID/components/ConnectPolygonID"
 import { useAccount } from "wagmi"
 import JoinStep from "./JoinStep"
@@ -8,40 +7,23 @@ import JoinStep from "./JoinStep"
 const ConnectPolygonIDJoinStep = (): JSX.Element => {
   const { isConnected } = useAccount()
 
-  const { roles, requiredPlatforms } = useGuild()
-
-  // TODO: return POLYGON_ID in requiredPlatforms if we need to show it here
-  // const requirements = roles?.flatMap((role) => role.requirements) ?? []
-  const requirements = []
-  const polygonIDRequirements = requirements
-    ?.filter((req) => req.type === "POLYGON_ID_BASIC")
-    .map((req) => req.id)
-
-  // TODO: I think this logic won't work anymore...
-  const { membership } = useMembership()
-  const requirementAccesses = membership?.roles?.flatMap((role) =>
-    role.requirements?.filter((req) => req.access)
-  )
-
-  const isPIDConnected = requirementAccesses?.some(
-    (reqAccess) =>
-      polygonIDRequirements.includes(reqAccess.requirementId) && reqAccess.access
+  const { data: isDone, isLoading } = useSWRWithOptionalAuth(
+    `/v2/util/gate-proof-existence/POLYGON_ID_BASIC_MAIN`
   )
 
   const { onOpen, onClose, isOpen } = useDisclosure()
 
-  if (!polygonIDRequirements?.length) return null
-
   return (
     <>
       <JoinStep
-        isDone={isPIDConnected}
+        isDone={isDone}
         colorScheme="purple"
         icon={<Img src="/requirementLogos/polygonId_white.svg" width="1.5em" />}
         title="Connect PolygonID"
-        buttonLabel={isPIDConnected ? "Connected" : "Connect"}
+        buttonLabel={isDone ? "Connected" : "Connect"}
         onClick={onOpen}
         isDisabled={!isConnected && "Connect wallet first"}
+        isLoading={isLoading}
       />
 
       <ConnectPolygonIDModal
