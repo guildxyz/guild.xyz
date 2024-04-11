2a2c753cdecda617b6211b0d3ad0f44487a42ad0
Csete Balázs <csetebalu@gmail.com>
2024-04-11 12:22:40 +0200

    load SearchableVirtualListModal dynamically

--- src/requirements/Allowlist/AllowlistRequirement.tsx
+++ src/requirements/Allowlist/AllowlistRequirement.tsx
@@ -6,8 +6,12 @@ import Requirement, {
 } from "components/[guild]/Requirements/components/Requirement"
 import { useRequirementContext } from "components/[guild]/Requirements/components/RequirementContext"
 import Button from "components/common/Button"
+import dynamic from "next/dynamic"
 import { ArrowSquareIn, ListPlus } from "phosphor-react"
-import SearchableVirtualListModal from "requirements/common/SearchableVirtualListModal"
+
+const DynamicSearchableVirtualListModal = dynamic(
+  () => import("requirements/common/SearchableVirtualListModal")
+)
 
 function HiddenAllowlistText({ isEmail }: { isEmail: boolean }) {
   return (
@@ -63,12 +67,15 @@ const AllowlistRequirement = ({ ...rest }: RequirementProps): JSX.Element => {
           {`${isEmail ? "email " : ""}allowlist`}
         </Button>
       )}
-      <SearchableVirtualListModal
-        initialList={addresses}
-        isOpen={isOpen}
-        onClose={onClose}
-        title={isEmail ? "Email allowlist" : "Allowlist"}
-      />
+
+      {!("ipfsHash" in requirement.data) && (
+        <DynamicSearchableVirtualListModal
+          initialList={addresses}
+          isOpen={isOpen}
+          onClose={onClose}
+          title={isEmail ? "Email allowlist" : "Allowlist"}
+        />
+      )}
     </Requirement>
   )
 }
