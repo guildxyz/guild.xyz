7eedb3e5cfeda7bd02b7c8fe998e5a01dadc77dc
Csete Balázs <csetebalu@gmail.com>
2024-04-11 15:12:01 +0200

    lp vault label change

--- src/requirements/Uniswap/UniswapForm.tsx
+++ src/requirements/Uniswap/UniswapForm.tsx
@@ -98,7 +98,7 @@ const UniswapForm = ({
             !!parseFromObject(errors, baseFieldPath)?.data?.lpVault || !!error
           }
         >
-          <FormLabel>LP Vault</FormLabel>
+          <FormLabel>LP Vault address or URL</FormLabel>
           <Input
             {...register(`${baseFieldPath}.data.lpVault`, {
               required: true,
