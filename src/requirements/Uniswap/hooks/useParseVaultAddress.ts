13732968760b8ae55283c676a59ddf0379f376ea
Csete Balázs <csetebalu@gmail.com>
2024-04-11 15:38:33 +0200

    remove setValue call from useParseVaultAddress

--- src/requirements/Uniswap/hooks/useParseVaultAddress.ts
+++ src/requirements/Uniswap/hooks/useParseVaultAddress.ts
@@ -1,18 +1,16 @@
 import { useMemo } from "react"
-import { useFormContext, useWatch } from "react-hook-form"
+import { useWatch } from "react-hook-form"
 
 // Note: We might want to put these kind of constants to `@guildxyz/types/consts`
 export const ADDRESS_REGEX = /0x[0-9a-f]{40}/i
 
 export function useParseVaultAddress(baseFieldPath: string) {
-  const { setValue } = useFormContext()
   const lpVaultInputValue = useWatch({ name: `${baseFieldPath}.data.lpVault` })
 
   const lpVaultAddress = useMemo(() => {
     if (!lpVaultInputValue || typeof lpVaultInputValue !== "string") return null
     const [match] = lpVaultInputValue.match(ADDRESS_REGEX) || []
     if (!match) return null
-    setValue(`${baseFieldPath}.data.lpVault`, match)
     return match as `0x${string}`
   }, [lpVaultInputValue])
 
