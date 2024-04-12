468bb395735e89c31730930784699a4666133b24
BrickheadJohnny <johnny.zgen@gmail.com>
2024-04-12 13:20:49 +0200

    fix(AddAndOrderRoles): fill useEffect dependency array

--- src/components/[guild]/AddAndOrderRoles/AddAndOrderRoles.tsx
+++ src/components/[guild]/AddAndOrderRoles/AddAndOrderRoles.tsx
@@ -37,7 +37,7 @@ const AddAndOrderRoles = ({ setIsStuck = null }): JSX.Element => {
   const { ref: addRoleButtonRef, isStuck } = useIsStuck()
   useEffect(() => {
     setIsStuck?.(isStuck)
-  }, [isStuck])
+  }, [isStuck, setIsStuck])
 
   return (
     <>
