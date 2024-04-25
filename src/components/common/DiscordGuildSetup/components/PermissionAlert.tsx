151c1ca22f5b959a361f9c8d658194ac81f2f80d
BrickheadJohnny <johnny.zgen@gmail.com>
2024-04-25 12:35:51 +0200

    fix(PermissionAlert): remove wrapper element

--- src/components/common/DiscordGuildSetup/components/PermissionAlert.tsx
+++ src/components/common/DiscordGuildSetup/components/PermissionAlert.tsx
@@ -3,7 +3,6 @@ import {
   AlertDescription,
   AlertIcon,
   AlertProps,
-  Box,
   Collapse,
 } from "@chakra-ui/react"
 import { atom, useAtomValue } from "jotai"
@@ -14,17 +13,19 @@ export default function PermissionAlert(props: AlertProps) {
   const shouldShowPermissionAlert = useAtomValue(shouldShowPermissionAlertAtom)
 
   return (
-    // Not sure what the exact reason if, but without a Box wrapper, it had invalid height
-    <Box>
-      <Collapse in={shouldShowPermissionAlert} autoFocus>
-        <Alert status="warning" alignItems={"center"} {...props}>
-          <AlertIcon />
-          <AlertDescription>
-            Try adding the bot again, and make sure to grant all requested
-            permissions
-          </AlertDescription>
-        </Alert>
-      </Collapse>
-    </Box>
+    <Collapse
+      in={shouldShowPermissionAlert}
+      autoFocus
+      style={{
+        flexShrink: 0,
+      }}
+    >
+      <Alert status="warning" alignItems="center" {...props}>
+        <AlertIcon />
+        <AlertDescription>
+          Try adding the bot again, and make sure to grant all requested permissions
+        </AlertDescription>
+      </Alert>
+    </Collapse>
   )
 }
