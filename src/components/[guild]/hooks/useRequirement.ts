b85641c88efcde0fbf88908b1ea9c2b9a2ddf845
Csete Balázs <csetebalu@gmail.com>
2024-05-08 11:44:12 +0200

    use immutable SWR & remove body

--- src/components/[guild]/hooks/useRequirement.ts
+++ src/components/[guild]/hooks/useRequirement.ts
@@ -1,17 +1,17 @@
-import useSWR from "swr"
+import useSWRImmutable from "swr/immutable"
 import { Requirement } from "types"
 import useGuild from "./useGuild"
 
 const useRequirement = (roleId: number, requirementId: number, search?: string) => {
   const { id: guildId } = useGuild()
 
-  return useSWR<Requirement>(
+  return useSWRImmutable<Requirement>(
     guildId && roleId && requirementId
       ? [
-          `/v2/guilds/${guildId}/roles/${roleId}/requirements/${requirementId}/${
+          `/v2/guilds/${guildId}/roles/${roleId}/requirements/${requirementId}${
             search ? `?search=${search}` : ""
           }`,
-          { method: "GET", body: {} },
+          { method: "GET" },
         ]
       : null
   )
