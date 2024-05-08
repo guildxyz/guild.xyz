5a6c9f9c3683275ce2aa3d3f7c180b57bff3ffe6
valid <valid@zgen.hu>
2024-05-08 12:12:42 +0200

    show address in case of google login too

--- src/components/[guild]/JoinModal/components/WalletAuthButton.tsx
+++ src/components/[guild]/JoinModal/components/WalletAuthButton.tsx
@@ -1,33 +1,21 @@
-import useUser from "components/[guild]/hooks/useUser"
 import { walletSelectorModalAtom } from "components/_app/Web3ConnectionManager/components/WalletSelectorModal"
 import useWeb3ConnectionManager from "components/_app/Web3ConnectionManager/hooks/useWeb3ConnectionManager"
 import { useSetAtom } from "jotai"
-import { EnvelopeSimple, SignIn, Wallet } from "phosphor-react"
+import { SignIn, Wallet } from "phosphor-react"
 import shortenHex from "utils/shortenHex"
-import { useAccount } from "wagmi"
 import JoinStep from "./JoinStep"
 
 const WalletAuthButton = (): JSX.Element => {
   const setIsWalletSelectorModalOpen = useSetAtom(walletSelectorModalAtom)
   const { address } = useWeb3ConnectionManager()
-  const { connector } = useAccount()
-  const { emails } = useUser()
-
-  const isGoogleLogin = connector?.id === "waas-connector"
 
   return (
     <JoinStep
       title="Sign in"
       isRequired
-      icon={!address ? <SignIn /> : isGoogleLogin ? <EnvelopeSimple /> : <Wallet />}
+      icon={!address ? <SignIn /> : <Wallet />}
       isDone={!!address}
-      buttonLabel={
-        !address
-          ? "Sign in"
-          : isGoogleLogin
-          ? emails?.emailAddress
-          : shortenHex(address, 3)
-      }
+      buttonLabel={!address ? "Sign in" : shortenHex(address, 3)}
       colorScheme="gray"
       onClick={() => setIsWalletSelectorModalOpen(true)}
     />
