f5eab6def3538313b9ea461ef7f8a6674f974760
BrickheadJohnny <johnny.zgen@gmail.com>
2024-05-13 18:57:45 +0200

    fix(wagmiConfig): change Base Mainnet RPC

--- src/wagmiConfig/index.ts
+++ src/wagmiConfig/index.ts
@@ -130,7 +130,7 @@ export const wagmiConfig = createConfig({
     [mainnet.id]: http(),
     [polygon.id]: http("https://polygon-bor-rpc.publicnode.com"),
     [polygonZkEvm.id]: http(),
-    [base.id]: http("https://base.llamarpc.com"),
+    [base.id]: http("https://base-pokt.nodies.app"),
     [baseSepolia.id]: http(),
     [optimism.id]: http(),
     [arbitrum.id]: http(),
