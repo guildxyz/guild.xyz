b4c8d117645ec1235ae23b8528c1bd51495c40f7
valid <valid@zgen.hu>
2024-05-14 01:43:07 +0200

    perf(GuildPinRewardCard): remove CardMotionWrapper & custom logic for Our Guild

--- src/components/[guild]/AccessHub/components/GuildPinRewardCard.tsx
+++ src/components/[guild]/AccessHub/components/GuildPinRewardCard.tsx
@@ -2,7 +2,6 @@ import { Icon, Tooltip, useColorModeValue } from "@chakra-ui/react"
 import { useMintGuildPinContext } from "components/[guild]/Requirements/components/GuildCheckout/MintGuildPinContext"
 import useGuild from "components/[guild]/hooks/useGuild"
 import useGuildPermission from "components/[guild]/hooks/useGuildPermission"
-import CardMotionWrapper from "components/common/CardMotionWrapper"
 import RewardCard from "components/common/RewardCard"
 import dynamic from "next/dynamic"
 import { CircleWavyCheck, Question } from "phosphor-react"
@@ -28,50 +27,48 @@ const GuildPinRewardCard = () => {
   const { isInvalidImage, isTooSmallImage } = useMintGuildPinContext()
 
   return (
-    <CardMotionWrapper>
-      <RewardCard
-        data-test="guild-pin-reward-card"
-        label={
-          <>
-            <Icon as={CircleWavyCheck} mb="-2px" mr="1.5" />
-            Guild.xyz
-            <Tooltip label="This is a built in reward by Guild.xyz" hasArrow>
-              <Icon as={Question} mb="-2px" ml="1.5" />
-            </Tooltip>
-          </>
-        }
-        title="Guild Pin"
-        image="/img/guild-pin-key-3d.svg"
-        colorScheme={!guildPin?.isActive ? "gray" : "GUILD"}
-        borderStyle={!guildPin?.isActive && "dashed"}
-        description={
-          !guildPin?.isActive
-            ? "Mintable badge of membership"
-            : "Onchain badge that shows your support and belonging to this community."
-        }
-        bg={bgColor}
-        _before={{
-          content: '""',
-          position: "absolute",
-          top: 0,
-          bottom: 0,
-          left: 0,
-          right: 0,
-          bg: `linear-gradient(to top right, ${bgColor} 70%, transparent), url('/landing/${bgFile}')`,
-          bgSize: "140%",
-          bgRepeat: "no-repeat",
-          bgPosition: "top 7px right 7px",
-          opacity: "0.07",
-        }}
-      >
-        {(!(isInvalidImage || isTooSmallImage) || isAdmin) &&
-          (guildPin?.chain !== "FUEL" ? (
-            <DynamicMintGuildPin />
-          ) : (
-            <DynamicMintFuelGuildPin />
-          ))}
-      </RewardCard>
-    </CardMotionWrapper>
+    <RewardCard
+      data-test="guild-pin-reward-card"
+      label={
+        <>
+          <Icon as={CircleWavyCheck} mb="-2px" mr="1.5" />
+          Guild.xyz
+          <Tooltip label="This is a built in reward by Guild.xyz" hasArrow>
+            <Icon as={Question} mb="-2px" ml="1.5" />
+          </Tooltip>
+        </>
+      }
+      title="Guild Pin"
+      image="/img/guild-pin-key-3d.svg"
+      colorScheme={!guildPin?.isActive ? "gray" : "GUILD"}
+      borderStyle={!guildPin?.isActive && "dashed"}
+      description={
+        !guildPin?.isActive
+          ? "Mintable badge of membership"
+          : "Onchain badge that shows your support and belonging to this community."
+      }
+      bg={bgColor}
+      _before={{
+        content: '""',
+        position: "absolute",
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+        bg: `linear-gradient(to top right, ${bgColor} 70%, transparent), url('/landing/${bgFile}')`,
+        bgSize: "140%",
+        bgRepeat: "no-repeat",
+        bgPosition: "top 7px right 7px",
+        opacity: "0.07",
+      }}
+    >
+      {(!(isInvalidImage || isTooSmallImage) || isAdmin) &&
+        (guildPin?.chain !== "FUEL" ? (
+          <DynamicMintGuildPin />
+        ) : (
+          <DynamicMintFuelGuildPin />
+        ))}
+    </RewardCard>
   )
 }
 
