f07e25f2d58da3ba33e9465a078932bb6f79d466
BrickheadJohnny <johnny.zgen@gmail.com>
2024-05-15 16:42:58 +0200

    refactor(ContractCallCardMenu): render Modals outside of the Menu

--- src/platforms/ContractCall/ContractCallCardMenu.tsx
+++ src/platforms/ContractCall/ContractCallCardMenu.tsx
@@ -1,16 +1,57 @@
+import { MenuItem, useDisclosure } from "@chakra-ui/react"
 import RemovePlatformMenuItem from "components/[guild]/AccessHub/components/RemovePlatformMenuItem"
+import { ContractCallFunction } from "components/[guild]/RolePlatforms/components/AddRoleRewardModal/components/AddContractCallPanel/components/CreateNftForm/hooks/useCreateNft"
 import PlatformCardMenu from "components/[guild]/RolePlatforms/components/PlatformCard/components/PlatformCardMenu"
-import EditNftMenuItem from "./components/EditNftMenuItem"
+import useGuild from "components/[guild]/hooks/useGuild"
+import { PencilSimple } from "phosphor-react"
+import EditNFTDescriptionModal from "./components/EditNFTDescriptionModal"
+import EditNftModal from "./components/EditNftModal"
 
 type Props = {
   platformGuildId: string
 }
 
-const ContractCallCardMenu = ({ platformGuildId }: Props): JSX.Element => (
-  <PlatformCardMenu>
-    <EditNftMenuItem platformGuildId={platformGuildId} />
-    <RemovePlatformMenuItem platformGuildId={platformGuildId} />
-  </PlatformCardMenu>
-)
+const ContractCallCardMenu = ({ platformGuildId }: Props): JSX.Element => {
+  const { guildPlatforms } = useGuild()
+  const guildPlatform = guildPlatforms?.find(
+    (gp) => gp.platformGuildId === platformGuildId
+  )
+
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const {
+    isOpen: legacyIsOpen,
+    onOpen: legacyOnOpen,
+    onClose: legacyOnClose,
+  } = useDisclosure()
+
+  const isLegacy =
+    guildPlatform.platformGuildData.function ===
+    ContractCallFunction.DEPRECATED_SIMPLE_CLAIM
+
+  return (
+    <>
+      <PlatformCardMenu>
+        <MenuItem icon={<PencilSimple />} onClick={isLegacy ? legacyOnOpen : onOpen}>
+          {isLegacy ? "Edit NFT description" : "Edit NFT"}
+        </MenuItem>
+        <RemovePlatformMenuItem platformGuildId={platformGuildId} />
+      </PlatformCardMenu>
+
+      {isLegacy ? (
+        <EditNFTDescriptionModal
+          isOpen={legacyIsOpen}
+          onClose={legacyOnClose}
+          guildPlatform={guildPlatform}
+        />
+      ) : (
+        <EditNftModal
+          isOpen={isOpen}
+          onClose={onClose}
+          guildPlatform={guildPlatform}
+        />
+      )}
+    </>
+  )
+}
 
 export default ContractCallCardMenu
