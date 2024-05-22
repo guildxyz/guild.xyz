3b6656263ed4c39a0480f45c2c42add03bdfeff9
cs-balazs <44908417+cs-balazs@users.noreply.github.com>
2024-05-22 11:07:47 +0200

    feat: Sign-in-with-Ethereum (#1185)
    * add getMessageToSign param & siwe message builder
    
    * temporary change: try out preview domain
    
    * Revert "temporary change: try out preview domain"
    
    This reverts commit 7fdb7674e5d64ea7948af81aa9e26568518eb37e.
    
    * fix(useSetKeypair): typo in comment

--- src/hooks/useSubmit/useSubmit.ts
+++ src/hooks/useSubmit/useSubmit.ts
@@ -99,7 +99,7 @@ const signCallbacks = [
   },
 ]
 
-type MessageParams = {
+export type MessageParams = {
   msg: string
   addr: string
   method: ValidationMethod
@@ -264,6 +264,7 @@ type SignBaseProps = {
   keyPair?: CryptoKeyPair
   msg?: string
   ts?: number
+  getMessageToSign?: (params: MessageParams) => string
 }
 
 export type SignProps = SignBaseProps & {
@@ -280,7 +281,7 @@ const createMessageParams = (
   payload: string
 ): MessageParams => ({
   addr: address.toLowerCase(),
-  nonce: randomBytes(32).toString("base64"),
+  nonce: randomBytes(32).toString("hex"),
   ts: ts.toString(),
   hash: payload !== "{}" ? keccak256(stringToBytes(payload)) : undefined,
   method: null,
@@ -370,6 +371,7 @@ export const sign = async ({
   forcePrompt,
   msg = DEFAULT_MESSAGE,
   ts,
+  getMessageToSign = getMessage,
 }: SignProps): Promise<[string, Validation]> => {
   const params = createMessageParams(address, ts ?? Date.now(), msg, payload)
   let sig = null
@@ -402,13 +404,13 @@ export const sign = async ({
     if (walletClient?.account?.type === "local") {
       // For local accounts, such as CWaaS, we request the signature on the account. Otherwise it sends a personal_sign to the rpc
       sig = await walletClient.account.signMessage({
-        message: getMessage(params),
+        message: getMessageToSign(params),
       })
     } else {
       sig = await walletClient
         .signMessage({
           account: address,
-          message: getMessage(params),
+          message: getMessageToSign(params),
         })
         .catch((error) => {
           if (error instanceof UnauthorizedProviderError) {
