7ba3339d8eba4b37d1f970d5dfacc0558352f791
valid <valid@zgen.hu>
2024-05-24 11:46:41 +0200

    fix: pass providerTypesOnly when there're no reqs yet too

--- src/components/[guild]/RolePlatforms/components/AddRoleRewardModal/components/DynamicSetup/BaseValueModal.tsx
+++ src/components/[guild]/RolePlatforms/components/AddRoleRewardModal/components/DynamicSetup/BaseValueModal.tsx
@@ -109,7 +109,7 @@ const BaseValueModal = ({ roleId, isOpen, onClose, onSelect }: Props) => {
                 {isLoading || isCreateRequirementLoading ? (
                   <Loader />
                 ) : (
-                  <AddRequirement onAdd={addRequirement} />
+                  <AddRequirement onAdd={addRequirement} providerTypesOnly />
                 )}
               </>
             )}
