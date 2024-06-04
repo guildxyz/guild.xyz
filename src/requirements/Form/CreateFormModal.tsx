0339e443b79e9450b91d41a70ce0800f16d7310e
FBalint <balint@zgen.hu>
2024-06-04 11:54:02 +0200

    refactor(CreateFormModal): remove unnecessary logic

--- src/requirements/Form/CreateFormModal.tsx
+++ src/requirements/Form/CreateFormModal.tsx
@@ -1,27 +1,17 @@
 import { Modal, ModalOverlay } from "@chakra-ui/react"
-import { useAddRewardContext } from "components/[guild]/AddRewardContext"
 import rewards, { modalSizeForPlatform } from "platforms/rewards"
-import { useEffect } from "react"
 
-const CreateFormModal = ({ isOpen, onClose, onAdd }) => {
-  const { setSelection } = useAddRewardContext()
-
-  useEffect(() => {
-    setSelection("FORM")
-  }, [])
-
-  return (
-    <Modal
-      isOpen={isOpen}
-      onClose={onClose}
-      size={modalSizeForPlatform("FORM")}
-      scrollBehavior="inside"
-      colorScheme="dark"
-    >
-      <ModalOverlay />
-      <rewards.FORM.AddRewardPanel onAdd={onAdd} />
-    </Modal>
-  )
-}
+const CreateFormModal = ({ isOpen, onClose, onAdd }) => (
+  <Modal
+    isOpen={isOpen}
+    onClose={onClose}
+    size={modalSizeForPlatform("FORM")}
+    scrollBehavior="inside"
+    colorScheme="dark"
+  >
+    <ModalOverlay />
+    <rewards.FORM.AddRewardPanel onAdd={onAdd} />
+  </Modal>
+)
 
 export default CreateFormModal
