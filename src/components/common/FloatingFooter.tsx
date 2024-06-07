0c31423821e505ff76d12a526ea0923d971e08e7
valid <valid@zgen.hu>
2024-06-07 18:36:04 +0200

    feat(FillFormProgress): make bar sticky (#1262)

--- src/components/common/FloatingFooter.tsx
+++ src/components/common/FloatingFooter.tsx
@@ -1,9 +1,18 @@
 import { Box, Container, ContainerProps } from "@chakra-ui/react"
+import useIsStuck from "hooks/useIsStuck"
 import { PropsWithChildren } from "react"
 import Card from "./Card"
 
 type Props = { maxWidth?: ContainerProps["maxWidth"] }
 
+/**
+ * These two components are really similar and I wanted to just keep the new simpler
+ * sticky one, but couldn't handle cases in create-guild so ended up just keeping the
+ * floating one too, separately. When we rework the create-guild flow, we might be
+ * able to delete it
+ */
+
+// for fixed, always floating footers, like now on create-guild page
 const FloatingFooter = ({
   maxWidth = "container.lg",
   children,
@@ -36,5 +45,30 @@ const FloatingFooter = ({
     </Container>
   </Box>
 )
+
+// for sticky footers, similar to StickyBar, but on the bottom
+export const StickyFooter = ({ children, ...rest }: PropsWithChildren<any>) => {
+  const { ref, isStuck } = useIsStuck()
+
+  return (
+    <Card
+      ref={ref}
+      position="sticky"
+      bottom="-1px"
+      mt="6"
+      zIndex={1201} // above intercom floating button
+      isFullWidthOnMobile={isStuck}
+      {...(isStuck && {
+        borderBottomRadius: "0 !important",
+        borderWidth: { base: "1px 0 0 0", md: "1px 1px 0 1px" },
+        boxShadow:
+          "rgba(0, 0, 0, 0.1) 0px 5px 10px,rgba(0, 0, 0, 0.2) 0px 15px 40px",
+      })}
+      {...rest}
+    >
+      {children}
+    </Card>
+  )
+}
 
 export default FloatingFooter
