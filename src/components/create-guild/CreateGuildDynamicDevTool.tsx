3cd8aa523f87ccb441ebba52e62d01e8a225c100
Dominik Stumpf <s.dominik.work@gmail.com>
2024-06-07 18:03:46 +0200

    refactor: move `CreateGuild` components into `create-guild.tsx`

--- src/components/create-guild/CreateGuildDynamicDevTool.tsx
+++
@@ -1,8 +0,0 @@
-import DynamicDevTool from "components/create-guild/DynamicDevTool"
-import { useFormContext } from "react-hook-form"
-import { GuildFormType } from "types"
-
-export function CreateGuildDynamicDevTool() {
-  const { control } = useFormContext<GuildFormType>()
-  return <DynamicDevTool control={control} />
-}
--- src/pages/create-guild.tsx
+++ src/pages/create-guild.tsx
@@ -1,11 +1,57 @@
 import { ThemeProvider } from "components/[guild]/ThemeContext"
 import { Layout } from "components/common/Layout"
-import { CreateGuildBackground } from "components/create-guild/CreateGuildBackground"
 import { CreateGuildProvider } from "components/create-guild/CreateGuildContext"
-import { CreateGuildDynamicDevTool } from "components/create-guild/CreateGuildDynamicDevTool"
-import { CreateGuildHead } from "components/create-guild/CreateGuildHead"
-import { CreateGuildHeadline } from "components/create-guild/CreateGuildHeadline"
 import { CreateGuildMainSection } from "components/create-guild/CreateGuildMainSection"
+import { useWatch } from "react-hook-form"
+import { useThemeContext } from "components/[guild]/ThemeContext"
+import GuildLogo from "components/common/GuildLogo"
+import DynamicDevTool from "components/create-guild/DynamicDevTool"
+import { useFormContext } from "react-hook-form"
+import { GuildFormType } from "types"
+
+function CreateGuildHead() {
+  const name = useWatch({ name: "name" })
+  const imageUrl = useWatch({ name: "imageUrl" })
+
+  return <Layout.Head ogTitle={name || "Create Guild"} imageUrl={imageUrl} />
+}
+
+function CreateGuildBackground() {
+  const { localThemeColor, localBackgroundImage } = useThemeContext()
+  const themeColor = useWatch({ name: "theme.color" })
+  const color = localThemeColor !== themeColor ? themeColor : localThemeColor
+
+  return (
+    <Layout.Background offset={47} background={color} image={localBackgroundImage} />
+  )
+}
+
+function CreateGuildHeadline() {
+  const name = useWatch({ name: "name" })
+  const { textColor } = useThemeContext()
+  const imageUrl = useWatch({ name: "imageUrl" })
+
+  return (
+    <Layout.Headline
+      title={name || "Create Guild"}
+      image={
+        imageUrl && (
+          <GuildLogo
+            imageUrl={imageUrl}
+            size={{ base: "56px", lg: "72px" }}
+            mt={{ base: 1, lg: 2 }}
+            bgColor={textColor === "primary.800" ? "primary.800" : "transparent"}
+          />
+        )
+      }
+    />
+  )
+}
+
+function CreateGuildDynamicDevTool() {
+  const { control } = useFormContext<GuildFormType>()
+  return <DynamicDevTool control={control} />
+}
 
 const CreateGuildPage = (): JSX.Element => (
   <>
