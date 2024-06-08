b12de6d4bfce15e56059010e1937d36c88f0f754
BrickheadJohnny <92519134+BrickheadJohnny@users.noreply.github.com>
2024-06-08 06:37:29 +0200

    feat: interactive `TiltCard` component (#1261)
    * feat: `TiltCard` component
    
    * feat(CollectibleImage): `imageWrapper` prop
    
    * feat(Collect page): use `TiltCard` as `imageWrapper`
    
    * feat: use `TiltCard` inside `ImagePicker`
    
    * fix: more subtle animation & better glare effect

--- src/components/[guild]/RolePlatforms/components/AddRoleRewardModal/components/AddContractCallPanel/components/CreateNftForm/components/ImagePicker.tsx
+++ src/components/[guild]/RolePlatforms/components/AddRoleRewardModal/components/AddContractCallPanel/components/CreateNftForm/components/ImagePicker.tsx
@@ -9,12 +9,13 @@ import {
   Text,
   VStack,
 } from "@chakra-ui/react"
+import TiltCard from "components/[guild]/collect/components/TiltCard"
 import Button from "components/common/Button"
 import FormErrorMessage from "components/common/FormErrorMessage"
 import useDropzone from "hooks/useDropzone"
 import usePinata from "hooks/usePinata"
 import { Image } from "phosphor-react"
-import { useState } from "react"
+import { Fragment, useState } from "react"
 import { useController, useFormContext } from "react-hook-form"
 import { CreateNftFormType } from "./NftDataForm"
 
@@ -49,62 +50,66 @@ const ImagePicker = () => {
     },
   })
 
+  const WrapperElement = imageField.value ? TiltCard : Fragment
+
   return (
     <FormControl isInvalid={!!fileRejections?.[0] || !!errors?.image}>
       <FormLabel>Media</FormLabel>
 
-      <AspectRatio ratio={1}>
-        <Button
-          p={0}
-          borderWidth={1}
-          variant="ghost"
-          w="full"
-          {...getRootProps()}
-          sx={
-            !!imageField.value && {
-              "> div": {
-                width: "100%",
-                height: "100%",
-              },
+      <WrapperElement>
+        <AspectRatio ratio={1}>
+          <Button
+            p={0}
+            borderWidth={1}
+            variant="ghost"
+            w="full"
+            {...getRootProps()}
+            sx={
+              !!imageField.value && {
+                "> div": {
+                  width: "100%",
+                  height: "100%",
+                },
+              }
             }
-          }
-        >
-          {isUploading ? (
-            <VStack p={4}>
-              <Text colorScheme="gray" fontSize="sm" fontWeight="normal">
-                Uploading...
-              </Text>
-              <Progress
-                borderRadius="full"
-                w="full"
-                value={progress * 100}
-                colorScheme="gray"
+          >
+            {isUploading ? (
+              <VStack p={4}>
+                <Text colorScheme="gray" fontSize="sm" fontWeight="normal">
+                  Uploading...
+                </Text>
+                <Progress
+                  borderRadius="full"
+                  w="full"
+                  value={progress * 100}
+                  colorScheme="gray"
+                />
+              </VStack>
+            ) : imageField.value ? (
+              <Img
+                aspectRatio={1}
+                objectFit="cover"
+                boxSize="full"
+                src={imageField.value}
+                alt="NFT image"
               />
-            </VStack>
-          ) : imageField.value ? (
-            <Img
-              aspectRatio={1}
-              objectFit="cover"
-              boxSize="full"
-              src={imageField.value}
-              alt="NFT image"
-            />
-          ) : (
-            <VStack p={4}>
-              <Icon as={Image} weight="light" boxSize={16} color="gray" />
-              <Text
-                display={{ base: "none", md: "inline" }}
-                colorScheme="gray"
-                fontSize="sm"
-                fontWeight="normal"
-              >
-                {isDragActive ? "Drop" : "Choose or drop image here"}
-              </Text>
-            </VStack>
-          )}
-          <input {...getInputProps()} hidden />
-        </Button>
-      </AspectRatio>
+            ) : (
+              <VStack p={4}>
+                <Icon as={Image} weight="light" boxSize={16} color="gray" />
+                <Text
+                  display={{ base: "none", md: "inline" }}
+                  colorScheme="gray"
+                  fontSize="sm"
+                  fontWeight="normal"
+                >
+                  {isDragActive ? "Drop" : "Choose or drop image here"}
+                </Text>
+              </VStack>
+            )}
+            <input {...getInputProps()} hidden />
+          </Button>
+        </AspectRatio>
+      </WrapperElement>
 
       <FormHelperText>
         JPG / PNG / GIF format, max 100MB. A 1:1 aspect ratio is preferred, but any
