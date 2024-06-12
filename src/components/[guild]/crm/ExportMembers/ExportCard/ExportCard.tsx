ce199aa6230b7f87bbc53c7e654491f0ffff0d0a
valid <valid@zgen.hu>
2024-06-12 20:19:32 +0200

    replace collapse with download button that fetches & starts export automatically

--- src/components/[guild]/crm/ExportMembers/ExportCard/ExportCard.tsx
+++ src/components/[guild]/crm/ExportMembers/ExportCard/ExportCard.tsx
@@ -1,28 +1,23 @@
 import {
   Box,
-  ButtonGroup,
-  Collapse,
-  HStack,
-  Icon,
+  IconButton,
   Spinner,
   Tag,
   TagLeftIcon,
   Text,
   Tooltip,
-  useClipboard,
-  useDisclosure,
 } from "@chakra-ui/react"
 import Card from "components/common/Card"
-import { CaretDown, Check, Copy, Download } from "phosphor-react"
+import { Download } from "phosphor-react"
 import formatRelativeTimeFromNow from "utils/formatRelativeTimeFromNow"
 
 import MemberCount from "components/[guild]/RoleCard/components/MemberCount"
 import useGuild from "components/[guild]/hooks/useGuild"
-import Button from "components/common/Button"
 import CardMotionWrapper from "components/common/CardMotionWrapper"
-import ErrorAlert from "components/common/ErrorAlert"
-import useSWRWithOptionalAuth from "hooks/useSWRWithOptionalAuth"
+import useShowErrorToast from "hooks/useShowErrorToast"
+import { useSubmitWithSign } from "hooks/useSubmit"
 import { atom, useAtom } from "jotai"
+import fetcher from "utils/fetcher"
 import { ExportData } from "../useExports"
 import ExportParamsTags from "./ExportParamsTags"
 
@@ -33,123 +28,87 @@ const ExportCard = ({ exp }: { exp: ExportData }) => {
   const timeDifference = Date.now() - date.getTime()
   const since = formatRelativeTimeFromNow(timeDifference)
   const [isAbsoluteTime, setIsAbsoluteTime] = useAtom(absoluteTimeAtom)
-
-  const { isOpen, onToggle } = useDisclosure()
+  const toggleAbsoluteTime = () => setIsAbsoluteTime((prev) => !prev)
 
   return (
     <CardMotionWrapper>
-      <Card>
-        <Button
-          p="4"
-          variant="unstyled"
-          h="auto"
-          onClick={exp.status === "FINISHED" ? onToggle : null}
-          cursor={exp.status === "FINISHED" ? "pointer" : "default"}
-        >
-          <HStack>
-            <Box mr="auto">
-              <Tooltip
-                label={isAbsoluteTime ? "Show relative time" : "Show timestamp"}
-                hasArrow
-              >
-                <Text
-                  fontWeight={"bold"}
-                  textAlign={"left"}
-                  mb="0.5"
-                  mr="auto"
-                  onClick={(e) => {
-                    e.stopPropagation()
-                    setIsAbsoluteTime((prev) => !prev)
-                  }}
-                  cursor="pointer"
-                >
-                  {isAbsoluteTime
-                    ? `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`
-                    : `${since} ago`}
-                </Text>
-              </Tooltip>
-              <ExportParamsTags {...exp.data.params} />
-            </Box>
-            {exp.status === "FINISHED" ? (
-              <>
-                <MemberCount memberCount={exp.data.count} mt="0" />
-                <Icon
-                  as={CaretDown}
-                  aria-label="Open export"
-                  transform={isOpen && "rotate(180deg)"}
-                  transition="transform .3s"
-                />
-              </>
-            ) : exp.status === "FAILED" ? (
-              <Tag colorScheme="red">Failed</Tag>
-            ) : (
-              <Tag colorScheme="blue">
-                <TagLeftIcon as={Spinner} />
-                Creating
-              </Tag>
-            )}
-          </HStack>
-        </Button>
-        <Collapse in={isOpen} unmountOnExit>
-          <ExportControls filename={exp.filename} />
-        </Collapse>
+      <Card p="4" alignItems="center" flexDir="row">
+        <Box mr="auto">
+          <Tooltip
+            label={isAbsoluteTime ? "Show relative time" : "Show timestamp"}
+            hasArrow
+          >
+            <Text
+              fontWeight={"bold"}
+              textAlign={"left"}
+              onClick={toggleAbsoluteTime}
+              cursor="pointer"
+              mb="1"
+            >
+              {isAbsoluteTime
+                ? `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`
+                : `${since} ago`}
+            </Text>
+          </Tooltip>
+          <ExportParamsTags {...exp.data.params} />
+        </Box>
+        {exp.status === "FINISHED" ? (
+          <>
+            <MemberCount memberCount={exp.data.count} mt="0" mr="0.5" size="sm" />
+            <DownloadExportButton filename={exp.filename} />
+          </>
+        ) : exp.status === "FAILED" ? (
+          <Tag colorScheme="red">Failed</Tag>
+        ) : (
+          <Tag colorScheme="blue">
+            <TagLeftIcon as={Spinner} />
+            Creating
+          </Tag>
+        )}
       </Card>
     </CardMotionWrapper>
   )
 }
 
-const ExportControls = ({ filename }) => {
+const DownloadExportButton = ({ filename }) => {
   const { id } = useGuild()
-  const { data, isLoading, error } = useSWRWithOptionalAuth(
-    `/v2/crm/guilds/${id}/exports/${filename}`,
-    null,
-    false,
-    true
-  )
+  const showErrorToast = useShowErrorToast()
 
-  const { onCopy, hasCopied } = useClipboard(data, 4000)
-  const csvContent = encodeURI("data:text/csv;charset=utf-8," + data)
+  const fetchExport = (signedValidation) =>
+    fetcher(`/v2/crm/guilds/${id}/exports/${filename}`, {
+      method: "GET",
+      ...signedValidation,
+    })
+
+  const { onSubmit, isLoading } = useSubmitWithSign(fetchExport, {
+    onSuccess: (res) =>
+      downloadFile(encodeURI(`data:text/csv;charset=utf-8,${res}`), filename),
+    onError: (err) => showErrorToast(err),
+  })
 
   return (
-    <Box p="4">
-      {error && <ErrorAlert label="Couldn't fetch export" />}
-      <ButtonGroup
-        size="sm"
-        mt="2"
-        ml="auto"
-        spacing="1"
-        justifyContent={"flex-end"}
-        w="full"
-      >
-        <Button
-          leftIcon={hasCopied ? <Check /> : <Copy />}
-          aria-label={"Download export"}
-          variant="outline"
-          isLoading={isLoading}
-          isDisabled={hasCopied || error}
-          loadingText="Copy addresses"
-          onClick={onCopy}
-          borderRadius="lg"
-        >
-          {hasCopied ? "Copied" : "Copy addresses"}
-        </Button>
-        <Button
-          as="a"
-          download={filename}
-          href={csvContent}
-          leftIcon={<Download />}
-          aria-label={"Download export"}
-          colorScheme="blue"
-          isLoading={isLoading}
-          isDisabled={error}
-          loadingText="Download"
-          borderRadius="lg"
-        >
-          Download
-        </Button>
-      </ButtonGroup>
-    </Box>
+    <Tooltip
+      label={isLoading ? "Download starting..." : "Download export"}
+      shouldWrapChildren
+      hasArrow
+    >
+      <IconButton
+        variant="ghost"
+        colorScheme="blue"
+        h="10"
+        icon={<Download />}
+        onClick={() => onSubmit()}
+        aria-label={"Download export"}
+        isLoading={isLoading}
+      />
+    </Tooltip>
   )
 }
+
+const downloadFile = (url, filename) =>
+  Object.assign(document.createElement("a"), {
+    href: url,
+    download: filename,
+  }).click()
 
 export default ExportCard
