3239342dff81ce60945412a993421572568b3d77
Dominik Stumpf <s.dominik.work@gmail.com>
2024-06-14 22:50:11 +0200

    chore: remove comments, unused vars

--- src/components/create-guild/Requirements/components/LogicFormControl.tsx
+++ src/components/create-guild/Requirements/components/LogicFormControl.tsx
@@ -1,20 +1,5 @@
-import {
-  FormControl,
-  HStack,
-  NumberDecrementStepper,
-  NumberIncrementStepper,
-  NumberInput,
-  NumberInputField,
-  NumberInputStepper,
-  Select,
-  Stack,
-  Text,
-  useBreakpointValue,
-  usePrevious,
-} from "@chakra-ui/react"
-import FormErrorMessage from "components/common/FormErrorMessage"
-import { useEffect } from "react"
-import { useController, useFormContext, useWatch } from "react-hook-form"
+import { HStack, Select, Stack, Text } from "@chakra-ui/react"
+import { useController, useWatch } from "react-hook-form"
 import { Requirement } from "types"
 import pluralize from "utils/pluralize"
 
@@ -25,15 +10,9 @@ type Props = {
 const LogicFormControl = ({
   requirements: requirementsFromProps,
 }: Props): JSX.Element => {
-  const {
-    resetField,
-    formState: { isDirty, errors },
-  } = useFormContext()
-
   const requirementsFromFormContext = useWatch({ name: "requirements" })
   const requirements = requirementsFromProps || requirementsFromFormContext
   const requirementCount = requirements?.length ?? 0
-  const prevRequirementCount = usePrevious(requirementCount)
 
   const {
     field: { onChange: logicOnChange, value: logic },
@@ -43,40 +22,11 @@ const LogicFormControl = ({
   })
 
   const {
-    field: { onChange: anyOfNumOnChange, value: anyOfNum, ...anyOfNumFieldProps },
+    field: { onChange: anyOfNumOnChange, value: anyOfNum },
   } = useController({
     name: "anyOfNum",
     defaultValue: 1,
-    // rules: {
-    //   required: logic !== "AND" && "This field is required",
-    //   min: {
-    //     value: 1,
-    //     message: "Minimum value is 1",
-    //   },
-    //   max: {
-    //     value: Math.max(requirementCount - 1, 1),
-    //     message: "Must be less than the number of requirements",
-    //   },
-    // },
   })
-
-  // useEffect(() => {
-  //   if (!isDirty || logic === "AND") return
-  //
-  //   if (anyOfNum === requirementCount)
-  //     anyOfNumOnChange(Math.max(requirementCount - 1, 1))
-  //
-  //   if (requirementCount < prevRequirementCount && requirementCount <= 2)
-  //     logicOnChange("OR")
-  // }, [
-  //   isDirty,
-  //   logic,
-  //   anyOfNum,
-  //   requirementCount,
-  //   anyOfNumOnChange,
-  //   prevRequirementCount,
-  //   logicOnChange,
-  // ])
 
   return (
     <Stack alignItems="start" direction={{ base: "column", sm: "row" }}>
@@ -125,37 +75,3 @@ const LogicFormControl = ({
 }
 
 export default LogicFormControl
-
-// <NumberInput
-//   w={28}
-//   position="relative"
-//   top={-0.5}
-//   {...anyOfNumFieldProps}
-//   onChange={(_, valueAsNumber) => {
-//     if (!isNaN(valueAsNumber)) {
-//       anyOfNumOnChange(valueAsNumber)
-//
-//       logicOnChange(
-//         valueAsNumber === 1 || valueAsNumber === requirementCount
-//           ? "OR"
-//           : "ANY_OF"
-//       )
-//     } else {
-//       anyOfNumOnChange("")
-//     }
-//   }}
-//   value={value ?? ""}
-//   min={1}
-//   max={Math.max(requirementCount - 1, 1)}
-// >
-//   <NumberInputField />
-//   <NumberInputStepper>
-//     <NumberIncrementStepper />
-//     <NumberDecrementStepper />
-//   </NumberInputStepper>
-// </NumberInput>
-// <FormControl isInvalid={!!errors?.anyOfNum}>
-//   <FormErrorMessage>
-//     {errors?.anyOfNum?.message?.toString()}
-//   </FormErrorMessage>
-// </FormControl>
