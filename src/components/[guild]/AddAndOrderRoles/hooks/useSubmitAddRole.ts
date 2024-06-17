efb1e0d7988b75e3ec39322bd18cd6556fa354f8
FBalint <balint@zgen.hu>
2024-06-17 18:35:42 +0200

    refactor: structuring & error handling

--- src/components/[guild]/AddAndOrderRoles/hooks/useSubmitAddRole.ts
+++ src/components/[guild]/AddAndOrderRoles/hooks/useSubmitAddRole.ts
@@ -1,9 +1,12 @@
 import useGuild from "components/[guild]/hooks/useGuild"
 import useCreateRole from "components/create-guild/hooks/useCreateRole"
 import usePinata from "hooks/usePinata"
+import useShowErrorToast from "hooks/useShowErrorToast"
+import useSubmit from "hooks/useSubmit"
 import useSubmitWithUpload from "hooks/useSubmitWithUpload"
 import useToast from "hooks/useToast"
 import { MutableRefObject } from "react"
+import { Requirement, Role, RolePlatform } from "types"
 import { useFetcherWithSign } from "utils/fetcher"
 import preprocessRequirement from "utils/preprocessRequirement"
 
@@ -18,18 +21,10 @@ const useSubmitAddRole = ({
 }) => {
   const toast = useToast()
   const fetcherWithSign = useFetcherWithSign()
-  const { id: guildId } = useGuild()
+  const { id: guildId, mutateGuild } = useGuild()
+  const showErrorToast = useShowErrorToast()
 
-  const { onSubmit, isLoading, isSigning, signLoadingText } = useCreateRole({
-    onSuccess: () => {
-      toast({
-        title: "Role successfully created",
-        status: "success",
-      })
-      methods.reset(methods.defaultValues)
-      onSuccess?.()
-    },
-  })
+  const { onSubmit, isSigning, signLoadingText } = useCreateRole({})
 
   const iconUploader = usePinata({
     fieldToSetOnSuccess: "imageUrl",
@@ -37,74 +32,148 @@ const useSubmitAddRole = ({
     control: methods.control,
   })
 
-  const newSubmit = async (data) => {
-    const { requirements, rolePlatforms, ...role } = data
-
-    const createdRole = await onSubmit({ ...role, requirements: [{ type: "FREE" }] })
-    const roleId = createdRole.id
-
-    const requirementIdMap = requirements.reduce((acc, req) => {
-      acc[req.id] = req
-      return acc
-    }, {})
-
-    const requirementCreations = Promise.all(
-      requirements.map((req) =>
-        fetcherWithSign([
-          `/v2/guilds/${guildId}/roles/${roleId}/requirements`,
-          {
-            method: "POST",
-            body: preprocessRequirement(req),
-          },
-        ]).then((res) => {
-          requirementIdMap[req.id] = res.id
-          return res
-        })
-      )
-    )
-
-    const createdRequirements = await requirementCreations
-
-    let rolePlatformsToCreate = rolePlatforms.map((rp) => {
-      if (!rp.dynamicAmount) return rp
-
-      const {
-        dynamicAmount: {
-          operation: { input },
-        },
-      } = rp
-      const requirementId = requirementIdMap[input.requirementId]
-
-      return {
-        ...rp,
-        dynamicAmount: {
-          ...rp.dynamicAmount,
-          operation: {
-            ...rp.dynamicAmount.operation,
-            input: {
-              ...input,
-              requirementId,
-              roleId,
-            },
-          },
-        },
-      }
-    })
-
-    const rolePlatformCreations = Promise.all(
-      rolePlatformsToCreate.map((rolePlatform) =>
-        fetcherWithSign([
-          `/v2/guilds/${guildId}/roles/${roleId}/role-platforms`,
-          { method: "POST", body: rolePlatform },
-        ]).catch((error) => error)
-      )
-    )
-
-    const createdRolePlatforms = await rolePlatformCreations
+  const createRole = async (role: Role & { guildId: number }) => {
+    try {
+      return await onSubmit({ ...role, requirements: [{ type: "FREE" }] })
+    } catch (error) {
+      showErrorToast("Failed to create role")
+      return null
+    }
   }
 
+  const createRequirements = async (
+    requirements: Partial<Requirement>[],
+    roleId: number
+  ) => {
+    const requirementIdMap = {}
+
+    const promises = requirements.map((req) =>
+      fetcherWithSign([
+        `/v2/guilds/${guildId}/roles/${roleId}/requirements`,
+        {
+          method: "POST",
+          body: preprocessRequirement(req),
+        },
+      ])
+        .then((res) => {
+          requirementIdMap[req.id] = res.id
+          return { status: "fulfilled", result: res }
+        })
+        .catch((error) => {
+          showErrorToast("Failed to create a requirement")
+          console.error(error)
+          return { status: "rejected", result: error }
+        })
+    )
+
+    const results = await Promise.all(promises)
+    const createdRequirements = results
+      .filter((res) => res.status === "fulfilled")
+      .map((res) => res.result)
+
+    return { createdRequirements, requirementIdMap }
+  }
+
+  const transformRolePlatform = (
+    rp: RolePlatform,
+    requirementIdMap: Record<number, number>,
+    roleId: number
+  ) => {
+    if (!rp.dynamicAmount) return rp
+
+    const input: any = rp.dynamicAmount.operation.input
+    const requirementId = requirementIdMap[input.requirementId]
+
+    if (!requirementId) {
+      showErrorToast(
+        `Skipping a reward creation, as its referenced requirement was not successfully created`
+      )
+      return null
+    }
+
+    return {
+      ...rp,
+      dynamicAmount: {
+        ...rp.dynamicAmount,
+        operation: {
+          ...rp.dynamicAmount.operation,
+          input: {
+            ...input,
+            requirementId,
+            roleId,
+          },
+        },
+      },
+    }
+  }
+
+  const createRolePlatforms = async (
+    rolePlatforms: RolePlatform[],
+    roleId: number
+  ) => {
+    const promises = rolePlatforms.map((rolePlatform) =>
+      fetcherWithSign([
+        `/v2/guilds/${guildId}/roles/${roleId}/role-platforms`,
+        { method: "POST", body: rolePlatform },
+      ])
+        .then((res) => {
+          return { status: "fulfilled", result: res }
+        })
+        .catch((error) => {
+          showErrorToast("Failed to create a reward")
+          console.error(error)
+          return { status: "rejected", result: error }
+        })
+    )
+
+    const results = await Promise.all(promises)
+    return results
+      .filter((res) => res.status === "fulfilled")
+      .map((res) => res.result)
+  }
+
+  const submit = async (data) => {
+    const { requirements, rolePlatforms, ...role } = data
+
+    const createdRole = await createRole(role)
+    if (!createdRole) return
+    const roleId = createdRole.id
+
+    const { createdRequirements, requirementIdMap } = await createRequirements(
+      requirements,
+      roleId
+    )
+    const transformedRolePlatforms = rolePlatforms
+      .map((rp) => transformRolePlatform(rp, requirementIdMap, roleId))
+      .filter((rp) => rp !== null)
+
+    const createdRolePlatforms = await createRolePlatforms(
+      transformedRolePlatforms,
+      roleId
+    )
+
+    return { createdRole, createdRequirements, createdRolePlatforms }
+  }
+
+  const { onSubmit: submitWrapper, isLoading } = useSubmit(submit, {
+    onSuccess: (res) => {
+      const { createdRole, createdRequirements, createdRolePlatforms } = res
+      toast({
+        title: "Role successfully created",
+        status: "success",
+      })
+      methods.reset(methods.defaultValues)
+      mutateGuild()
+      onSuccess?.()
+    },
+    onError: (error) => {
+      showErrorToast("Failed to create role due to an unexpected error")
+      console.error(error)
+    },
+  })
+
   const { handleSubmit, isUploadingShown, uploadLoadingText } = useSubmitWithUpload(
-    methods.handleSubmit(newSubmit, (formErrors) => {
+    methods.handleSubmit(submitWrapper, (formErrors) => {
       if (formErrors.requirements && drawerBodyRef.current) {
         drawerBodyRef.current.scrollBy({
           top: drawerBodyRef.current.scrollHeight,
