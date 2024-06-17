db2954074f1992869f4445b829086f01aa5cfcf9
FBalint <balint@zgen.hu>
2024-06-17 17:44:10 +0200

    draft: submit order test

--- src/components/[guild]/AddAndOrderRoles/hooks/useSubmitAddRole.ts
+++ src/components/[guild]/AddAndOrderRoles/hooks/useSubmitAddRole.ts
@@ -1,8 +1,11 @@
+import useGuild from "components/[guild]/hooks/useGuild"
 import useCreateRole from "components/create-guild/hooks/useCreateRole"
 import usePinata from "hooks/usePinata"
 import useSubmitWithUpload from "hooks/useSubmitWithUpload"
 import useToast from "hooks/useToast"
 import { MutableRefObject } from "react"
+import { useFetcherWithSign } from "utils/fetcher"
+import preprocessRequirement from "utils/preprocessRequirement"
 
 const useSubmitAddRole = ({
   drawerBodyRef,
@@ -14,6 +17,8 @@ const useSubmitAddRole = ({
   onSuccess: () => void
 }) => {
   const toast = useToast()
+  const fetcherWithSign = useFetcherWithSign()
+  const { id: guildId } = useGuild()
 
   const { onSubmit, isLoading, isSigning, signLoadingText } = useCreateRole({
     onSuccess: () => {
@@ -32,8 +37,74 @@ const useSubmitAddRole = ({
     control: methods.control,
   })
 
+  const newSubmit = async (data) => {
+    const { requirements, rolePlatforms, ...role } = data
+
+    const createdRole = await onSubmit({ ...role, requirements: [{ type: "FREE" }] })
+    const roleId = createdRole.id
+
+    const requirementIdMap = requirements.reduce((acc, req) => {
+      acc[req.id] = req
+      return acc
+    }, {})
+
+    const requirementCreations = Promise.all(
+      requirements.map((req) =>
+        fetcherWithSign([
+          `/v2/guilds/${guildId}/roles/${roleId}/requirements`,
+          {
+            method: "POST",
+            body: preprocessRequirement(req),
+          },
+        ]).then((res) => {
+          requirementIdMap[req.id] = res.id
+          return res
+        })
+      )
+    )
+
+    const createdRequirements = await requirementCreations
+
+    let rolePlatformsToCreate = rolePlatforms.map((rp) => {
+      if (!rp.dynamicAmount) return rp
+
+      const {
+        dynamicAmount: {
+          operation: { input },
+        },
+      } = rp
+      const requirementId = requirementIdMap[input.requirementId]
+
+      return {
+        ...rp,
+        dynamicAmount: {
+          ...rp.dynamicAmount,
+          operation: {
+            ...rp.dynamicAmount.operation,
+            input: {
+              ...input,
+              requirementId,
+              roleId,
+            },
+          },
+        },
+      }
+    })
+
+    const rolePlatformCreations = Promise.all(
+      rolePlatformsToCreate.map((rolePlatform) =>
+        fetcherWithSign([
+          `/v2/guilds/${guildId}/roles/${roleId}/role-platforms`,
+          { method: "POST", body: rolePlatform },
+        ]).catch((error) => error)
+      )
+    )
+
+    const createdRolePlatforms = await rolePlatformCreations
+  }
+
   const { handleSubmit, isUploadingShown, uploadLoadingText } = useSubmitWithUpload(
-    methods.handleSubmit(onSubmit, (formErrors) => {
+    methods.handleSubmit(newSubmit, (formErrors) => {
       if (formErrors.requirements && drawerBodyRef.current) {
         drawerBodyRef.current.scrollBy({
           top: drawerBodyRef.current.scrollHeight,
