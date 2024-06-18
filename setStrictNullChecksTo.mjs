5cffd8e8d316221c6358becf2d199f995c145621
Dominik Stumpf <s.dominik.work@gmail.com>
2024-06-18 21:33:23 +0200

    chore: handle undefined case

--- setStrictNullChecksTo.mjs
+++ setStrictNullChecksTo.mjs
@@ -3,12 +3,12 @@ import { resolve } from "node:path";
 import prettier from "prettier";
 
 const prettierOptions = prettier.resolveConfigFile(resolve('./.prettierrc'));
-const strictNullCheckValue = process.argv.at(2).toLowerCase() === 'true' ? true : false
+const strictNullCheckValue = process.argv.at(2)?.toLowerCase() === 'true' ? true : false
 
 function setStrictNullChecksTo(value) {
   const config = JSON.parse(readFileSync('./tsconfig.json').toString())
   if (config?.compilerOptions?.strictNullChecks === undefined) {
-    throw new Error("strictNullChecks doesn't exist on tsconfig.json")
+    throw new Error("strictNullChecks doesn't exist in tsconfig.json")
   } else {
     config.compilerOptions.strictNullChecks = value
   }
