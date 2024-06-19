8d5266b42d364017fd93649dbbf5ff3d7ec62b08
valid <valid@zgen.hu>
2024-06-19 13:00:22 +0200

    Revert "handle FORM_SUBMISSION_DETAILED type"
    This reverts commit 48c87c78a12c52ba1bd000bbebab037d3a44a1cc.

--- src/requirements/Form/FormForm.tsx
+++ src/requirements/Form/FormForm.tsx
@@ -1,13 +1,11 @@
 import { Stack } from "@chakra-ui/react"
 import useGuild from "components/[guild]/hooks/useGuild"
-import { useEffect } from "react"
-import { useController, useFormContext, useWatch } from "react-hook-form"
+import { useController, useWatch } from "react-hook-form"
 import { RequirementFormProps } from "requirements"
 import FormSelector from "./components/FormSelector"
 import SetExpectedAnswers from "./components/SetExpectedAnswers"
 
 const FormForm = ({ baseFieldPath }: RequirementFormProps) => {
-  const { setValue } = useFormContext()
   const { id } = useGuild()
 
   const selectedFormId = useWatch({ name: `${baseFieldPath}.data.id` })
@@ -17,11 +15,6 @@ const FormForm = ({ baseFieldPath }: RequirementFormProps) => {
     name: `${baseFieldPath}.data.guildId`,
     defaultValue: id,
   })
-
-  useEffect(() => {
-    if (answers?.length) setValue("type", "FORM_SUBMISSION_DETAILED")
-    else setValue("type", "FORM_SUBMISSION")
-  }, [answers?.length])
 
   return (
     <Stack spacing={5} alignItems="start">
