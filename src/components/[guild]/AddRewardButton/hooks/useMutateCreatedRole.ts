3514a806e9dd3f691f33a2aa342406f44fea416d
FBalint <balint@zgen.hu>
2024-06-19 22:13:34 +0200

    fix(useMutateCreatedRole): set createdGuildPlatforms

--- src/components/[guild]/AddRewardButton/hooks/useMutateCreatedRole.ts
+++ src/components/[guild]/AddRewardButton/hooks/useMutateCreatedRole.ts
@@ -2,7 +2,8 @@ import useGuild from "components/[guild]/hooks/useGuild"
 import { mutateGuildsCache } from "components/create-guild/hooks/useCreateRole"
 import { useYourGuilds } from "components/explorer/YourGuilds"
 import useMatchMutate from "hooks/useMatchMutate"
-import { GuildBase, Requirement, Role, RolePlatform } from "types"
+import { GuildBase, GuildPlatform, Requirement, Role } from "types"
+import { CreateRolePlatformResponse } from "./useCreateRolePlatforms"
 
 const useMutateCreatedRole = () => {
   const { mutate: mutateYourGuilds } = useYourGuilds()
@@ -12,7 +13,7 @@ const useMutateCreatedRole = () => {
   const mutateCreatedRole = (
     createdRole: Role,
     createdRequirements: Requirement[],
-    createdRolePlatforms: RolePlatform[]
+    createdRolePlatforms: CreateRolePlatformResponse[]
   ) => {
     const completeRole = {
       ...createdRole,
@@ -28,9 +29,18 @@ const useMutateCreatedRole = () => {
       (prev) => mutateGuildsCache(prev, guildId),
       { revalidate: false }
     )
+
+    const createdGuildPlatforms = createdRolePlatforms
+      .map((rp) => rp.createdGuildPlatform)
+      .filter(Boolean)
+
     mutateGuild(
       (prev) => ({
         ...prev,
+        guildPlatforms: [
+          ...prev.guildPlatforms,
+          ...(createdGuildPlatforms as GuildPlatform[]),
+        ],
         roles: prev.roles.some((role) => role.id === createdRole.id)
           ? prev.roles.map((role) =>
               role.id === createdRole.id ? completeRole : role
