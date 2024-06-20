4a6a978e5de8650f52d1aeef2694332558000b47
BrickheadJohnny <johnny.zgen@gmail.com>
2024-06-20 21:04:03 +0200

    one more redesign

--- src/components/create-guild/CreateGuildButton.tsx
+++ src/components/create-guild/CreateGuildButton.tsx
@@ -1,4 +1,8 @@
+import { Collapse } from "@chakra-ui/react"
+import { walletSelectorModalAtom } from "components/_app/Web3ConnectionManager/components/WalletSelectorModal"
+import useWeb3ConnectionManager from "components/_app/Web3ConnectionManager/hooks/useWeb3ConnectionManager"
 import Button from "components/common/Button"
+import { useSetAtom } from "jotai"
 import { useFormContext } from "react-hook-form"
 import { CreateGuildFormType } from "./CreateGuildForm"
 import useCreateGuild from "./hooks/useCreateGuild"
@@ -7,19 +11,36 @@ const CreateGuildButton = () => {
   const { handleSubmit } = useFormContext<CreateGuildFormType>()
   const { onSubmit, isLoading } = useCreateGuild()
 
+  const { isWeb3Connected } = useWeb3ConnectionManager()
+  const setIsWalletSelectorModalOpen = useSetAtom(walletSelectorModalAtom)
+
   return (
-    <Button
-      colorScheme="green"
-      ml="auto"
-      size="xl"
-      borderRadius="2xl"
-      w="full"
-      isLoading={isLoading}
-      loadingText="Creating guild"
-      onClick={handleSubmit(onSubmit)}
-    >
-      Create guild
-    </Button>
+    <>
+      <Collapse in={!isWeb3Connected}>
+        <Button
+          size="xl"
+          colorScheme="blue"
+          onClick={() => setIsWalletSelectorModalOpen(true)}
+          isDisabled={isWeb3Connected}
+          w="full"
+        >
+          Connect wallet
+        </Button>
+      </Collapse>
+      <Button
+        colorScheme="green"
+        ml="auto"
+        size="xl"
+        borderRadius="2xl"
+        w="full"
+        isLoading={isLoading}
+        loadingText="Creating guild"
+        onClick={handleSubmit(onSubmit)}
+        isDisabled={!isWeb3Connected}
+      >
+        Create guild
+      </Button>
+    </>
   )
 }
 
--- src/pages/create-guild.tsx
+++ src/pages/create-guild.tsx
@@ -1,14 +1,10 @@
 import { ThemeProvider, useThemeContext } from "components/[guild]/ThemeContext"
-import { walletSelectorModalAtom } from "components/_app/Web3ConnectionManager/components/WalletSelectorModal"
-import useWeb3ConnectionManager from "components/_app/Web3ConnectionManager/hooks/useWeb3ConnectionManager"
 import ClientOnly from "components/common/ClientOnly"
 import { Layout } from "components/common/Layout"
 import CreateGuildForm, {
   CreateGuildFormType,
 } from "components/create-guild/CreateGuildForm"
 import DynamicDevTool from "components/create-guild/DynamicDevTool"
-import { useAtom } from "jotai"
-import { useEffect } from "react"
 import { FormProvider, useForm, useFormContext, useWatch } from "react-hook-form"
 import getRandomInt from "utils/getRandomInt"
 
@@ -48,16 +44,6 @@ const CreateGuildPage = (): JSX.Element => {
       ],
     },
   })
-
-  const { isWeb3Connected } = useWeb3ConnectionManager()
-  const [isWalletSelectorModalOpen, setIsWalletSelectorModalOpen] = useAtom(
-    walletSelectorModalAtom
-  )
-
-  useEffect(() => {
-    if (isWeb3Connected || isWalletSelectorModalOpen) return
-    setIsWalletSelectorModalOpen(true)
-  }, [isWeb3Connected, isWalletSelectorModalOpen, setIsWalletSelectorModalOpen])
 
   return (
     <ClientOnly>
