92fdb36fe2480c2cf1bdeb85abd1ba2b3424673d
Csete Balázs <csetebalu@gmail.com>
2024-06-21 18:16:08 +0200

    add FARCASTER color scheme

--- src/platforms/Farcaster/index.ts
+++ src/platforms/Farcaster/index.ts
@@ -5,7 +5,7 @@ export default {
   icon: FarcasterIcon,
   imageUrl: "/platforms/poap.png",
   name: "Farcaster",
-  colorScheme: "indigo",
+  colorScheme: "FARCASTER",
   gatedEntity: "farcaster",
   isPlatform: true,
   asRewardRestriction: PlatformAsRewardRestrictions.NOT_APPLICABLE,
