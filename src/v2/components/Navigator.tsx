f412125340f3cc6ef9aa05677b9c2a900628f464
Dominik Stumpf <s.dominik.work@gmail.com>
2024-06-25 20:18:48 +0200

    refactor: adjust navigator style

--- src/v2/components/Navigator.tsx
+++ src/v2/components/Navigator.tsx
@@ -29,97 +29,9 @@ import XLogo from "static/icons/x.svg"
 import GuildCastle from "components/explorer/AnimatedLogo"
 import { buttonVariants } from "@/components/ui/Button"
 import { ThemeToggle } from "./ThemeToggle"
+import Link from "next/link"
 
 export function Navigator() {
-  // return (
-  //   <div className="border rounded-lg top-1/2 left-0 fixed z-50 overflow-clip">
-  //     <div className="flex gap-12 px-3 py-4 bg-card">
-  //       <ul className="flex flex-col gap-1">
-  //         <li className="text-card-foreground text-sm font-bold px-4 my-1">Navigation</li>
-  //         <ListItem href="/explorer">
-  //           <House />
-  //           <span>Explore guilds</span>
-  //         </ListItem>
-  //         <ListItem href="/create-guild">
-  //           <Plus />
-  //           <span>Create guild</span>
-  //         </ListItem>
-  //         <ListItem href="/leaderboard">
-  //           <CircleWavyCheck />
-  //           <span>Guild Pins leaderboard</span>
-  //         </ListItem>
-  //         <ListItem target="_blank" href="https://help.guild.xyz" rel="noopener">
-  //           <Info />
-  //           <span>Guide</span>
-  //         </ListItem>
-  //         <ListItem
-  //           target="_blank"
-  //           href="https://help.guild.xyz/en/collections/9537762-case-studies"
-  //           rel="noopener"
-  //         >
-  //           <Book />
-  //           <span>Case studies</span>
-  //         </ListItem>
-  //         <ListItem href="/privacy-policy">
-  //           <Shield />
-  //           <span>Privacy Policy</span>
-  //         </ListItem>
-  //         <ListItem href="/terms-of-use">
-  //           <File />
-  //           <span>Terms of Use</span>
-  //         </ListItem>
-  //       </ul>
-  //       <ul className="flex flex-col gap-1">
-  //         <li className="text-card-foreground text-sm font-bold px-4 my-1">Other</li>
-  //         <ListItem
-  //           target="_blank"
-  //           href="https://discord.gg/KUkghUdk2G"
-  //           rel="noopener"
-  //         >
-  //           <DiscordLogo />
-  //           <span>Discord</span>
-  //         </ListItem>
-  //         <ListItem target="_blank" href="https://twitter.com/guildxyz" rel="noopener">
-  //           <XLogo />
-  //           <span>Twitter</span>
-  //         </ListItem>
-  //         <ListItem
-  //           target="_blank"
-  //           href="https://github.com/guildxyz/guild.xyz"
-  //           rel="noopener"
-  //         >
-  //           <Code />
-  //           <span>Code</span>
-  //         </ListItem>
-  //         <ListItem
-  //           target="_blank"
-  //           href="https://github.com/guildxyz/guild-sdk"
-  //           rel="noopener"
-  //         >
-  //           <Package />
-  //           <span>Guild SDK</span>
-  //         </ListItem>
-  //         <ListItem
-  //           target="_blank"
-  //           href="https://guildxyz.notion.site/Join-the-Guild-Team-7ffd822c4d7749cb9c1adb525c858ae1"
-  //           rel="noopener"
-  //         >
-  //           <UsersThree />
-  //           <span>Team</span>
-  //         </ListItem>
-  //         <ListItem
-  //           target="_blank"
-  //           href="https://guild.xyz/guild-xyz-brand-kit.zip"
-  //           rel="noopener"
-  //         >
-  //           <Palette />
-  //           <span>Brand kit</span>
-  //         </ListItem>
-  //       </ul>
-  //     </div>
-  //     <div className="px-7 py-4 bg-muted text-muted-foreground flex justify-between items-center"><span>Theme:</span><ThemeToggle /></div>
-  //   </div>
-  // )
   return (
     <NavigationMenu>
       <NavigationMenuList>
@@ -128,8 +40,8 @@ export function Navigator() {
             <GuildCastle />
             <span className="font-display font-bold text-base">Guild</span>
           </NavigationMenuTrigger>
-          <NavigationMenuContent className="flex flex-col border rounded-lg">
-            <div className="flex gap-12 px-3 py-4 bg-card">
+          <NavigationMenuContent className="flex flex-col rounded-lg">
+            <div className="flex gap-12 px-3 py-4">
               <ul className="flex flex-col gap-1">
                 <li className="text-card-foreground text-sm font-bold px-4 my-1">
                   Navigation
@@ -236,45 +148,26 @@ export function Navigator() {
   )
 }
 
-function ListItem({ children, ...rest }: React.ComponentProps<"a">) {
-  return (
-    <li>
-      <a
+const ListItem = React.forwardRef<
+  React.ElementRef<"a">,
+  React.ComponentPropsWithoutRef<typeof Link>
+>(({ className, children, ...props }, ref) => (
+  <li>
+    <Link
+      passHref
+      legacyBehavior
+      ref={ref}
+      {...props}
+    >
+      <NavigationMenuLink
         className={cn(
           buttonVariants({ variant: "ghost" }),
-          "flex gap-2 items-center whitespace-nowrap justify-start"
+          "flex gap-2 items-center whitespace-nowrap justify-start",
+          className
         )}
-        {...rest}
       >
         {children}
-      </a>
-    </li>
-  )
-}
-
-// const ListItem = React.forwardRef<
-//   React.ElementRef<"a">,
-//   React.ComponentPropsWithoutRef<"a">
-// >(({ className, title, children, ...props }, ref) => {
-//   return (
-//     <li>
-//       <NavigationMenuLink asChild>
-//         <a
-//           ref={ref}
-//           className={cn(
-//             "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
-//             className,
-//           )}
-//           {...props}
-//         >
-//           <div className="text-sm font-medium leading-none">{title}</div>
-//           <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
-//             {children}
-//           </p>
-//         </a>
-//       </NavigationMenuLink>
-//     </li>
-//   );
-// });
-//
-// ListItem.displayName = "ListItem";
+      </NavigationMenuLink>
+    </Link>
+  </li>
+))
