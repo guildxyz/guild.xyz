b418b5f33c4aac0ab892358bbccb2a40ca146320
valid <valid@zgen.hu>
2024-06-25 23:26:50 +0200

    feat: show responses count in AccessHub form cards

--- src/components/[guild]/CreateFormModal/hooks/useCreateForm.ts
+++ src/components/[guild]/CreateFormModal/hooks/useCreateForm.ts
@@ -13,7 +13,7 @@ const useCreateForm = (onSuccess?: (createdForm: Schemas["Form"]) => void) => {
   const toast = useToast()
   const showErrorToast = useShowErrorToast()
 
-  return useSubmitWithSign<Schemas["Form"]>(
+  return useSubmitWithSign<Schemas["Form"] & { submissionCount: number }>(
     (signedValidation: SignedValidation) =>
       fetcher(`/v2/guilds/${id}/forms`, signedValidation),
     {
