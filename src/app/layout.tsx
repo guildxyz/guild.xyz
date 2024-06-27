4ee8b91125ee88e6ef6b7ddacb75415d95ee8594
Dominik Stumpf <s.dominik.work@gmail.com>
2024-06-27 11:30:40 +0200

    feat: add theme toggle

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,32 @@
+import type { Metadata } from "next"
+import { ThemeProvider } from "@/components/ThemeProvider"
+import { inter, dystopian } from "fonts"
 import "./globals.css"
+import clsx from "clsx"
 
-import { cn } from "@/lib/utils"
-import { inter } from "fonts"
+interface RootLayoutProps {
+  children: React.ReactNode
+}
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export const metadata: Metadata = {
+  title: "Guildhall",
+  description:
+    "Automated membership management for the platforms your community already uses.",
+}
+
+export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en" suppressHydrationWarning>
       <head />
-      <body
-        className={cn(
-          "font-inter min-h-screen bg-background antialiased",
-          inter.variable
-        )}
-      >
-        {children}
+      <body className={clsx(dystopian.variable, inter.variable)}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   )
--- src/app/playground/page.tsx
+++ src/app/playground/page.tsx
@@ -1,16 +1,10 @@
-import { Button } from "@/ui/Button"
+import { ThemeToggle } from "../../v2/components/ThemeToggle"
 
 export default function Page() {
   return (
     <div>
       <h1>Playground</h1>
-      <div className="space-x-2">
-        <Button variant="default">primary (default)</Button>
-        <Button variant="accent">accent</Button>
-        <Button variant="outline">outline</Button>
-        <Button variant="ghost">ghost</Button>
-        <Button variant="destructive">destructive</Button>
-      </div>
+      <ThemeToggle />
     </div>
   )
 }
