71a186f424f0337049422af1b8d588d589eeb304
BrickheadJohnny <johnny.zgen@gmail.com>
2024-06-27 10:11:26 +0200

    add basic `layout.tsx`

---
+++ src/app/layout.tsx
@@ -0,0 +1,20 @@
+import "@/styles/globals.css"
+
+import { cn } from "@/lib/utils"
+import { inter } from "fonts"
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <head />
+      <body
+        className={cn(
+          "font-inter min-h-screen bg-background antialiased",
+          inter.variable
+        )}
+      >
+        {children}
+      </body>
+    </html>
+  )
+}
