290b6375b005b38460a9ead33c7ab89b79f0dfb6
Dominik Stumpf <s.dominik.work@gmail.com>
2024-06-27 13:04:05 +0200

    feat: add button

--- src/app/playground/page.tsx
+++ src/app/playground/page.tsx
@@ -2,10 +2,19 @@ import { Card } from "@/components/ui/Card"
 import { Metadata } from "next"
 import { PropsWithChildren } from "react"
 import { ThemeToggle } from "../../v2/components/ThemeToggle"
+import { Button } from "@/components/ui/Button"
 
 export const metadata: Metadata = {
   title: "Playground",
 }
+
+
+const Section = ({ title, children }: PropsWithChildren<{ title: string }>) => (
+  <section className="flex flex-col gap-2">
+    <h2 className="text-lg font-bold">{title}</h2>
+    {children}
+  </section>
+)
 
 export default function Page() {
   return (
@@ -20,14 +29,13 @@ export default function Page() {
         <Section title="Card">
           <Card className="p-4">This is a card</Card>
         </Section>
+        <Button variant='default'>primary (default)</Button>
+        <Button variant='outline'>outline</Button>
+        <Button variant='ghost'>ghost</Button>
+        <Button variant='accent'>accent</Button>
+        <Button variant='destructive'>destructive</Button>
+        <Button variant='success'>success</Button>
       </div>
     </div>
   )
 }
-
-const Section = ({ title, children }: PropsWithChildren<{ title: string }>) => (
-  <section className="flex flex-col gap-2">
-    <h2 className="text-lg font-bold">{title}</h2>
-    {children}
-  </section>
-)
