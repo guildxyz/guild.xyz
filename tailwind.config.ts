46143209f80209bd40ef487df08ac53f4edc5d79
Dominik Stumpf <s.dominik.work@gmail.com>
2024-06-27 13:11:31 +0200

    feat: color button

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -40,6 +40,10 @@ const config = {
           DEFAULT: "hsl(var(--destructive))",
           foreground: "hsl(var(--destructive-foreground))",
         },
+        success: {
+          DEFAULT: "hsl(var(--success))",
+          foreground: "hsl(var(--success-foreground))",
+        },
         muted: {
           DEFAULT: "hsl(var(--muted))",
           foreground: "hsl(var(--muted-foreground))",
@@ -57,11 +61,6 @@ const config = {
           foreground: "hsl(var(--card-foreground))",
         },
       },
-      // borderRadius: {
-      //   lg: "var(--radius)",
-      //   md: "calc(var(--radius) - 2px)",
-      //   sm: "calc(var(--radius) - 4px)",
-      // },
       keyframes: {
         "accordion-down": {
           from: { height: "0" },
