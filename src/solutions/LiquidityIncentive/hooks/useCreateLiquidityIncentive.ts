cb88094b495db53e11d95d8570802d490e10ec20
FBalint <balint@zgen.hu>
2024-06-27 18:01:15 +0200

    refactor(useCreateLiquidityIncentive): decomposing

--- src/solutions/LiquidityIncentive/hooks/useCreateLiquidityIncentive.ts
+++ src/solutions/LiquidityIncentive/hooks/useCreateLiquidityIncentive.ts
@@ -25,74 +25,14 @@ const useCreateLiquidityIncentive = (onSuccess: () => void) => {
   const showErrorToast = useShowErrorToast()
 
   const submit = async (data: LiquidityIncentiveForm) => {
-    const uniswapRequirement = {
-      id: Date.now(),
-      type: "UNISWAP_V3_POSITIONS",
-      visibility: Visibility.PUBLIC,
-      isNegated: false,
-      data: data.pool.data,
-      chain: data.pool.chain,
-    } satisfies RoleToCreate["requirements"][number] & { id: number }
-
-    const pointsReward = {
-      ...(data.pointsId !== CREATE_NEW_OPTION && data.pointsId !== null
-        ? {
-            guildPlatformId: data.pointsId,
-            guildPlatform: {
-              platformName: "POINTS",
-              platformId: PlatformType.POINTS,
-              platformGuildId: "",
-              platformGuildData: {},
-            } satisfies GuildPlatform,
-          }
-        : {
-            guildPlatform: {
-              platformName: "POINTS",
-              platformId: PlatformType.POINTS,
-              platformGuildId: `points-${id}-${
-                data?.name?.toLowerCase() || "points"
-              }`,
-              platformGuildData: {
-                name: data.name,
-                imageUrl: data.imageUrl,
-              } satisfies PlatformGuildData["POINTS"],
-            } satisfies GuildPlatform,
-          }),
-      dynamicAmount: {
-        operation: {
-          type: "LINEAR",
-          params: {
-            addition: 0,
-            multiplier: data.conversion,
-            shouldFloorResult: true,
-          },
-          input: {
-            type: "REQUIREMENT_AMOUNT",
-            requirementId: uniswapRequirement.id,
-            roleId: -1, // will be overwritten by useCreateRRR
-          },
-        },
-      } as any as DynamicAmount,
-      platformRoleData: {
-        score: 0,
-      },
-      visibility: Visibility.PUBLIC,
-    } satisfies Omit<RolePlatform, "id">
-
     if (!id) {
       showErrorToast("Couldn't fetch Guild Id")
       return
     }
 
-    const role = {
-      guildId: id,
-      name: DEFAULT_NAME,
-      description: "",
-      logic: "AND" as Logic,
-      imageUrl: `/guildLogos/${getRandomInt(286)}.svg`,
-      visibility: Visibility.PUBLIC,
-      anyOfNum: 1,
-    } satisfies Omit<RoleToCreate, "requirements" | "rolePlatforms">
+    const uniswapRequirement = createUniswapRequirement(data)
+    const pointsReward = createPointsReward(data, id, uniswapRequirement.id)
+    const role = createRole(id)
 
     const dataToSubmit = {
       ...role,
@@ -108,5 +48,77 @@ const useCreateLiquidityIncentive = (onSuccess: () => void) => {
     isLoading,
   }
 }
+
+const createUniswapRequirement = (
+  data: LiquidityIncentiveForm
+): RoleToCreate["requirements"][number] & { id: number } => ({
+  id: Date.now(),
+  type: "UNISWAP_V3_POSITIONS",
+  visibility: Visibility.PUBLIC,
+  isNegated: false,
+  data: data.pool.data,
+  chain: data.pool.chain,
+})
+
+const createPointsReward = (
+  data: LiquidityIncentiveForm,
+  guildId: number,
+  requirementId: number
+): Omit<RolePlatform, "id"> => ({
+  ...(data.pointsId !== CREATE_NEW_OPTION && data.pointsId !== null
+    ? {
+        guildPlatformId: data.pointsId,
+        guildPlatform: {
+          platformName: "POINTS",
+          platformId: PlatformType.POINTS,
+          platformGuildId: "",
+          platformGuildData: {},
+        } satisfies GuildPlatform,
+      }
+    : {
+        guildPlatform: {
+          platformName: "POINTS",
+          platformId: PlatformType.POINTS,
+          platformGuildId: `points-${guildId}-${
+            data?.name?.toLowerCase() || "points"
+          }`,
+          platformGuildData: {
+            name: data.name,
+            imageUrl: data.imageUrl,
+          } satisfies PlatformGuildData["POINTS"],
+        } satisfies GuildPlatform,
+      }),
+  dynamicAmount: {
+    operation: {
+      type: "LINEAR",
+      params: {
+        addition: 0,
+        multiplier: data.conversion,
+        shouldFloorResult: true,
+      },
+      input: {
+        type: "REQUIREMENT_AMOUNT",
+        requirementId: requirementId,
+        roleId: -1, // will be overwritten by useCreateRRR
+      },
+    },
+  } as any as DynamicAmount,
+  platformRoleData: {
+    score: 0,
+  },
+  visibility: Visibility.PUBLIC,
+})
+
+const createRole = (
+  guildId: number
+): Omit<RoleToCreate, "requirements" | "rolePlatforms"> => ({
+  guildId: guildId,
+  name: DEFAULT_NAME,
+  description: "",
+  logic: "AND" as Logic,
+  imageUrl: `/guildLogos/${getRandomInt(286)}.svg`,
+  visibility: Visibility.PUBLIC,
+  anyOfNum: 1,
+})
 
 export default useCreateLiquidityIncentive
