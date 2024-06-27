565da808a356c6ca2ebf30ab357f7d3182de1ffb
BrickheadJohnny <johnny.zgen@gmail.com>
2024-06-27 12:24:16 +0200

    chore: better playground page

--- src/app/playground/page.tsx
+++ src/app/playground/page.tsx
@@ -1,10 +1,33 @@
+import { Card } from "@/components/ui/Card"
+import { Metadata } from "next"
+import { PropsWithChildren } from "react"
 import { ThemeToggle } from "../../v2/components/ThemeToggle"
+
+export const metadata: Metadata = {
+  title: "Playground",
+}
 
 export default function Page() {
   return (
-    <div>
-      <h1>Playground</h1>
-      <ThemeToggle />
+    <div className="p-8">
+      <h1 className="text-2xl font-bold">Playground</h1>
+
+      <div className="flex flex-col items-start gap-4">
+        <Section title="Theme toggle">
+          <ThemeToggle />
+        </Section>
+
+        <Section title="Card">
+          <Card className="p-4">This is a card</Card>
+        </Section>
+      </div>
     </div>
   )
 }
+
+const Section = ({ title, children }: PropsWithChildren<{ title: string }>) => (
+  <section className="flex flex-col gap-2">
+    <h2 className="text-lg font-bold">{title}</h2>
+    {children}
+  </section>
+)
