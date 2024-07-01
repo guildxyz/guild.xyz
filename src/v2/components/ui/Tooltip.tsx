f6694f416b9ed7f744c1add0a148f3478f0df9b5
Dominik Stumpf <122315398+dominik-stumpf@users.noreply.github.com>
2024-07-01 13:29:58 +0200

    feat: add tooltip (#1335)
    * feat: add tooltip
    
    * add `@radix-ui/react-tooltip` package
    
    * update styles & add story
    
    ---------
    
    Co-authored-by: BrickheadJohnny <johnny.zgen@gmail.com>

---
+++ src/v2/components/ui/Tooltip.tsx
@@ -0,0 +1,35 @@
+"use client"
+
+import * as TooltipPrimitive from "@radix-ui/react-tooltip"
+import * as React from "react"
+
+import { cn } from "@/lib/utils"
+
+const TooltipProvider = TooltipPrimitive.Provider
+
+const Tooltip: React.FC<
+  React.ComponentPropsWithoutRef<typeof TooltipPrimitive.Root>
+> = ({ ...props }) => <TooltipPrimitive.Root delayDuration={0} {...props} />
+
+const TooltipTrigger = TooltipPrimitive.Trigger
+
+const TooltipContent = React.forwardRef<
+  React.ElementRef<typeof TooltipPrimitive.Content>,
+  React.ComponentPropsWithoutRef<typeof TooltipPrimitive.Content>
+>(({ className, sideOffset = 4, children, ...props }, ref) => (
+  <TooltipPrimitive.Content
+    ref={ref}
+    sideOffset={sideOffset}
+    className={cn(
+      "bg-tooltip text-tooltip-foreground z-50 overflow-hidden rounded-xl px-3 py-1.5 text-sm shadow-md duration-0 animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
+      className
+    )}
+    {...props}
+  >
+    {children}
+    {/* <TooltipPrimitive.Arrow className="fill-popover-foreground" /> */}
+  </TooltipPrimitive.Content>
+))
+TooltipContent.displayName = TooltipPrimitive.Content.displayName
+
+export { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger }
