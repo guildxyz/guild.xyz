31dd8586befad86ce18f556fff0c0b98971821a3
FBalint <balint@zgen.hu>
2024-07-04 16:29:06 +0200

    remove unnecessary fragment

--- src/solutions/components/SolutionCard.tsx
+++ src/solutions/components/SolutionCard.tsx
@@ -31,70 +31,63 @@ const SolutionCard = ({
   const cardBg = useColorModeValue("white", "gray.800")
 
   return (
-    <>
-      <DisplayCard
-        boxShadow={"none"}
-        bg={cardBg}
-        outline="1px solid"
-        outlineColor={outlineColor}
-        outlineOffset="-1px"
-        px={4}
-        py={4}
-        position="relative"
-        onClick={onClick}
-        zIndex={2}
-        _before={{
-          content: `""`,
-          position: "absolute",
-          top: 0,
-          left: 0,
-          bg: `url('${bgImageUrl}')`,
-          bgRepeat: "no-repeat",
-          bgPosition: "center",
-          bgSize: "cover",
-          width: "100%",
-          height: "100%",
-          opacity: 0.15,
-          transition: "0.3s",
-          filter: `blur(2px) saturate(70%)`,
-        }}
-        _hover={{
-          _before: {
-            opacity: 0.25,
-            filter: `blur(0) saturate(100%)`,
-            transform: `scale(1.02)`,
-          },
-        }}
-      >
-        <Stack spacing={3} zIndex={1} justifyContent={"space-between"} h={"100%"}>
-          <HStack gap={3}>
-            <Circle
-              size="12"
-              pos="relative"
-              overflow="hidden"
-              bgColor={circleBgColor}
-            >
-              <Image src={imageUrl} alt="Guild logo" fill sizes="3rem" />
-            </Circle>
-            <Heading
-              fontSize={"normal"}
-              fontWeight="bold"
-              letterSpacing="wide"
-              maxW="full"
-              noOfLines={1}
-            >
-              {title}
-            </Heading>
-          </HStack>
-          {description && (
-            <Text colorScheme="gray" lineHeight={1.33}>
-              {description}
-            </Text>
-          )}
-          {children}
-        </Stack>
-      </DisplayCard>
-    </>
+    <DisplayCard
+      boxShadow={"none"}
+      bg={cardBg}
+      outline="1px solid"
+      outlineColor={outlineColor}
+      outlineOffset="-1px"
+      px={4}
+      py={4}
+      position="relative"
+      onClick={onClick}
+      zIndex={2}
+      _before={{
+        content: `""`,
+        position: "absolute",
+        top: 0,
+        left: 0,
+        bg: `url('${bgImageUrl}')`,
+        bgRepeat: "no-repeat",
+        bgPosition: "center",
+        bgSize: "cover",
+        width: "100%",
+        height: "100%",
+        opacity: 0.15,
+        transition: "0.3s",
+        filter: `blur(2px) saturate(70%)`,
+      }}
+      _hover={{
+        _before: {
+          opacity: 0.25,
+          filter: `blur(0) saturate(100%)`,
+          transform: `scale(1.02)`,
+        },
+      }}
+    >
+      <Stack spacing={3} zIndex={1} justifyContent={"space-between"} h={"100%"}>
+        <HStack gap={3}>
+          <Circle size="12" pos="relative" overflow="hidden" bgColor={circleBgColor}>
+            <Image src={imageUrl} alt="Guild logo" fill sizes="3rem" />
+          </Circle>
+          <Heading
+            fontSize={"normal"}
+            fontWeight="bold"
+            letterSpacing="wide"
+            maxW="full"
+            noOfLines={1}
+          >
+            {title}
+          </Heading>
+        </HStack>
+        {description && (
+          <Text colorScheme="gray" lineHeight={1.33}>
+            {description}
+          </Text>
+        )}
+        {children}
+      </Stack>
+    </DisplayCard>
   )
 }
 
