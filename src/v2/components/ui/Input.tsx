8454fb0e3ee68c7ed9df309de31029ec2289168f
BrickheadJohnny <johnny.zgen@gmail.com>
2024-07-05 10:38:11 +0200

    feat: add `Input` component

---
+++ src/v2/components/ui/Input.tsx
@@ -0,0 +1,21 @@
+import { cn } from "@/lib/utils"
+import { forwardRef, InputHTMLAttributes } from "react"
+
+export type InputProps = InputHTMLAttributes<HTMLInputElement>
+
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ className, type, ...props }, ref) => (
+    <input
+      type={type}
+      className={cn(
+        "flex h-10 w-full rounded-md border border-input bg-muted px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
+        className
+      )}
+      ref={ref}
+      {...props}
+    />
+  )
+)
+Input.displayName = "Input"
+
+export { Input }
