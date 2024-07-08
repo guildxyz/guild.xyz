b1bf5227a874e76eced5e527af0b4cfa0a1995a0
BrickheadJohnny <92519134+BrickheadJohnny@users.noreply.github.com>
2024-07-08 14:26:22 +0200

    feat: `DropdownMenu` component (#1359)
    * feat: `DropdownMenu` component
    
    * fix: rename `border-secondary` to `border-muted`

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -24,6 +24,7 @@ const config = {
       },
       colors: {
         border: "hsl(var(--border))",
+        "border-muted": "hsl(var(--border-muted))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
         background: "hsl(var(--background))",
