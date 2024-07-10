b34cf9e138e60034f2b38b6bff323523acca65a5
Dominik Stumpf <122315398+dominik-stumpf@users.noreply.github.com>
2024-07-10 19:16:15 +0200

    ci: add unsafe flag to biome (#1374)

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,6 +1,6 @@
 const config = {
   "*.{js,ts,cjs,mjs,d.cts,d.mts,jsx,tsx,json,jsonc}": [
-    "biome check --write --no-errors-on-unmatched", // Format, sort imports, lint, and apply safe fixes
+    "biome check --write --unsafe --no-errors-on-unmatched", // Format, sort imports, lint, and apply safe fixes
   ],
 }
 
