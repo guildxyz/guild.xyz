c3ce80e743224f10d16a50affdc906df4342255e
Dominik Stumpf <122315398+dominik-stumpf@users.noreply.github.com>
2024-07-10 17:34:53 +0200

    biome lint staged fix, move circular import check to vercel (#1371)
    * build: move circular check to vercel only
    
    * fix: lint staged

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,6 +1,6 @@
 const config = {
-  "*.{mjs,js,jsx,ts,tsx}": ["biome check --write"],
-  "*.json": ["biome format --write"],
+  "*.{mjs,js,jsx,ts,tsx}": ["biome check --write ."],
+  "*.json": ["biome format --write ."],
 }
 
 export default config
