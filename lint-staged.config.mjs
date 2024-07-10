2d655871058df8682bcfaa345aea51866c8e2dfb
Dominik Stumpf <122315398+dominik-stumpf@users.noreply.github.com>
2024-07-10 17:47:57 +0200

    ci: add more files to lint staged filter (#1372)

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,6 +1,7 @@
 const config = {
-  "*.{mjs,js,jsx,ts,tsx}": ["biome check --write ."],
-  "*.json": ["biome format --write ."],
+  "*.{js,ts,cjs,mjs,d.cts,d.mts,jsx,tsx,json,jsonc}": [
+    "biome check --write --no-errors-on-unmatched", // Format, sort imports, lint, and apply safe fixes
+  ],
 }
 
 export default config
