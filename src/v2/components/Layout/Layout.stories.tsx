56f192536898835a96d00eb4158063e9f0c6242a
Dominik Stumpf <s.dominik.work@gmail.com>
2024-07-11 16:07:12 +0200

    test: update stories

--- src/v2/components/Layout/Layout.stories.tsx
+++ src/v2/components/Layout/Layout.stories.tsx
@@ -2,23 +2,21 @@ import type { Meta, StoryFn } from "@storybook/react"
 import { Layout } from "."
 
 const meta: Meta = {
-  title: 'Design system/Layout',
+  title: "Design system/Layout",
   parameters: {
-    layout: 'fullscreen'
-  }
+    layout: "fullscreen",
+  },
 }
 
 export default meta
 
 export const Static: StoryFn = () => (
   <Layout.Root>
-    <Layout.Header>
+    <Layout.Hero>
       <Layout.Banner />
-      <Layout.Headline title='Layout title' />
-    </Layout.Header>
-    <Layout.Main>
-      Page contents
-    </Layout.Main>
+      <Layout.Headline title="Layout title" />
+    </Layout.Hero>
+    <Layout.Main>Page contents</Layout.Main>
     <Layout.Footer />
   </Layout.Root>
 )
