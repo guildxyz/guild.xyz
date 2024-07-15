7226346a691629d74e175d5f480ba7d219215d15
Dominik Stumpf <s.dominik.work@gmail.com>
2024-07-15 11:43:41 +0200

    chore: remove account and navmenu from header

--- src/v2/components/Header/Header.tsx
+++ src/v2/components/Header/Header.tsx
@@ -1,12 +1,18 @@
 "use client"
 
-import { Account } from "../Account"
-import { NavMenu } from "./NavMenu"
+// import { Account } from "../Account"
+// import { NavMenu } from "./NavMenu"
+//
+// const Header = () => (
+//   <header className="relative flex h-16 w-full items-center justify-between p-2">
+//     <NavMenu />
+//     <Account />
+//   </header>
+// )
 
 const Header = () => (
   <header className="relative flex h-16 w-full items-center justify-between p-2">
-    <NavMenu />
-    <Account />
+    bare header
   </header>
 )
 
