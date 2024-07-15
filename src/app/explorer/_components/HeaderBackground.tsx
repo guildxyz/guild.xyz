2254532991bbec23195f9bd6c4fb6160958a2669
BrickheadJohnny <johnny.zgen@gmail.com>
2024-07-15 16:04:15 +0200

    fix(HeaderBackground): light mode colors

--- src/app/explorer/_components/HeaderBackground.tsx
+++ src/app/explorer/_components/HeaderBackground.tsx
@@ -11,10 +11,10 @@ export const HeaderBackground = () => {
   return (
     <div
       className={cn(
-        "fixed inset-x-0 top-0 z-10 h-0 bg-background shadow-md transition-all duration-200",
+        "fixed inset-x-0 top-0 z-10 h-0 bg-card shadow-md transition-all duration-200 data-[theme='dark']:bg-background",
         {
           "h-16": isNavStuck,
-          "h-[calc(theme(space.36)+theme(space.2))] bg-gradient-to-b from-background to-card-secondary sm:h-[calc(theme(space.28)-theme(space.2))]":
+          "h-[calc(theme(space.36)+theme(space.2))] bg-gradient-to-b from-card to-background data-[theme='dark']:from-background data-[theme='dark']:to-card-secondary sm:h-[calc(theme(space.28)-theme(space.2))]":
             isSearchStuck,
         }
       )}
