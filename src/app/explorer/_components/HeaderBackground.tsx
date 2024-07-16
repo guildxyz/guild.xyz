45914e00b199a777ad51d6481fefd28ddd99e0b4
Dominik Stumpf <s.dominik.work@gmail.com>
2024-07-16 13:19:41 +0200

    chore(css): move antialiased, fix sticky header bg

--- src/app/explorer/_components/HeaderBackground.tsx
+++ src/app/explorer/_components/HeaderBackground.tsx
@@ -14,7 +14,7 @@ export const HeaderBackground = () => {
         "fixed inset-x-0 top-0 z-10 h-0 bg-card shadow-md transition-all duration-200 data-[theme='dark']:bg-background",
         {
           "h-16": isNavStuck,
-          "h-[calc(theme(space.36)+theme(space.2))] bg-gradient-to-b from-card to-background data-[theme='dark']:from-background data-[theme='dark']:to-card-secondary sm:h-[calc(theme(space.28)-theme(space.2))]":
+          "h-[calc(theme(space.36)+theme(space.2))] bg-gradient-to-t from-card to-background data-[theme='dark']:from-background data-[theme='dark']:to-card-secondary sm:h-[calc(theme(space.28)-theme(space.2))]":
             isSearchStuck,
         }
       )}
