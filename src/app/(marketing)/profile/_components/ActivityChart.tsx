01dfd334b4eb372036280d1f624398fab44e0421
Dominik Stumpf <s.dominik.work@gmail.com>
2024-07-23 12:10:12 +0200

    feat: improve contribution card view

---
+++ src/app/(marketing)/profile/_components/ActivityChart.tsx
@@ -0,0 +1,101 @@
+"use client"
+
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/Chart"
+import { Bar, BarChart, CartesianGrid, XAxis } from "recharts"
+
+const chartData = [
+  { date: "2024-06-19", experience: 341 },
+  { date: "2024-06-20", experience: 408 },
+  { date: "2024-06-21", experience: 169 },
+  { date: "2024-06-22", experience: 317 },
+  { date: "2024-06-23", experience: 480 },
+  { date: "2024-06-24", experience: 132 },
+  { date: "2024-06-25", experience: 141 },
+  { date: "2024-06-26", experience: 434 },
+  { date: "2024-06-13", experience: 81 },
+  { date: "2024-06-14", experience: 426 },
+  { date: "2024-06-15", experience: 307 },
+  { date: "2024-06-16", experience: 371 },
+  { date: "2024-06-17", experience: 475 },
+  { date: "2024-06-18", experience: 107 },
+  { date: "2024-06-19", experience: 341 },
+  { date: "2024-06-20", experience: 408 },
+  { date: "2024-06-21", experience: 169 },
+  { date: "2024-06-22", experience: 317 },
+  { date: "2024-06-23", experience: 480 },
+  { date: "2024-06-24", experience: 132 },
+  { date: "2024-06-25", experience: 141 },
+  { date: "2024-06-26", experience: 434 },
+  { date: "2024-06-27", experience: 448 },
+  { date: "2024-06-28", experience: 149 },
+  { date: "2024-06-29", experience: 103 },
+  { date: "2024-06-30", experience: 446 },
+  { date: "2024-06-24", experience: 132 },
+  { date: "2024-06-25", experience: 141 },
+  { date: "2024-06-26", experience: 434 },
+  { date: "2024-06-27", experience: 448 },
+  { date: "2024-06-28", experience: 149 },
+  { date: "2024-06-29", experience: 103 },
+  { date: "2024-06-30", experience: 446 },
+]
+const chartConfig = {
+  views: {
+    label: "Gained XP",
+  },
+  experience: {
+    label: "experience",
+    color: "hsl(var(--primary))",
+  },
+} satisfies ChartConfig
+
+export function ActivityChart() {
+  return (
+    <ChartContainer config={chartConfig} className="aspect-auto h-[65px] w-full">
+      <BarChart
+        accessibilityLayer
+        data={chartData}
+        margin={{
+          left: 12,
+          right: 12,
+        }}
+      >
+        <CartesianGrid vertical={false} />
+        <XAxis
+          dataKey="date"
+          tickLine={false}
+          axisLine={false}
+          tick={false}
+          height={0}
+          tickFormatter={(value) => {
+            const date = new Date(value)
+            return date.toLocaleDateString("en-US", {
+              month: "short",
+              day: "numeric",
+            })
+          }}
+        />
+        <ChartTooltip
+          content={
+            <ChartTooltipContent
+              className="w-[150px]"
+              nameKey="views"
+              labelFormatter={(value) => {
+                return new Date(value).toLocaleDateString("en-US", {
+                  month: "short",
+                  day: "numeric",
+                  year: "numeric",
+                })
+              }}
+            />
+          }
+        />
+        <Bar dataKey="experience" fill="var(--color-experience)" radius={999} />
+      </BarChart>
+    </ChartContainer>
+  )
+}
--- src/app/(marketing)/profile/page.tsx
+++ src/app/(marketing)/profile/page.tsx
@@ -9,11 +9,15 @@ import {
 } from "@/components/Layout"
 import { Avatar, AvatarFallback } from "@/components/ui/Avatar"
 import { AvatarGroup } from "@/components/ui/AvatarGroup"
+import { Badge } from "@/components/ui/Badge"
+import { Button } from "@/components/ui/Button"
 import { Card } from "@/components/ui/Card"
 import { ChartContainer } from "@/components/ui/Chart"
+import { Progress } from "@/components/ui/Progress"
 import { Separator } from "@/components/ui/Separator"
 import { CaretDown, CircleWavyCheck, Users } from "@phosphor-icons/react"
 import { RadialBar, RadialBarChart } from "recharts"
+import { ActivityChart } from "./_components/ActivityChart"
 
 const chartData = [{ experience: 1, fill: "hsl(var(--primary))" }]
 
@@ -69,7 +73,7 @@ const Page = () => {
                 <div className="font-bold text-lg">3232</div>
                 <div className="text-muted-foreground">Guildmates</div>
               </div>
-              <Separator orientation="vertical" className="hidden h-12 sm:block" />
+              <Separator orientation="vertical" className="h-12" />
               <div className="flex flex-col items-center leading-tight">
                 <div className="font-bold text-lg">0</div>
                 <div className="text-muted-foreground">Followers</div>
@@ -86,59 +90,84 @@ const Page = () => {
           </div>
           <Separator className="mt-16 mb-8" />
           <h2 className="mb-3 font-bold text-lg">Experience</h2>
-          <div className="grid grid-cols-2 gap-3">
+          <div className="grid grid-cols-1 gap-3 md:grid-cols-2">
             <Card className="p-6">
-              <h3 className="font-bold">Champion</h3>
-              <p className="text-muted-foreground">Description</p>
+              <div className="grid grid-cols-[auto_1fr] grid-rows-[auto_auto_auto] items-center gap-x-6 gap-y-2">
+                <div className="row-span-3 flex size-12 items-center justify-center place-self-center rounded-xl bg-primary font-extrabold text-xl shadow-lg">
+                  22
+                </div>
+                <div className="flex flex-col justify-between gap-2 sm:flex-row">
+                  <h3 className="font-bold">Champion</h3>
+                  <p className="text-muted-foreground">1322 / 9999 XP</p>
+                </div>
+                <Progress value={77} />
+                <p className="text-muted-foreground">
+                  This is a description that perfectly matches the 80 character
+                  description limit.
+                </p>
+              </div>
             </Card>
-            <Card className="p-6">
-              <h3>Engagement this month</h3>
+            <Card className="space-y-4 p-6">
+              <div className="flex flex-col items-start justify-between gap-2 sm:flex-row">
+                <h3 className="font-bold">Engagement this month</h3>
+                <Badge colorScheme="blue">+72 XP</Badge>
+              </div>
+              <ActivityChart />
             </Card>
           </div>
           <Separator className="my-8" />
           <h2 className="mb-3 font-bold text-lg">Top contributions</h2>
           <div className="grid grid-cols-1 gap-3">
-            <Card className="flex border-2">
-              <div className="relative h-full w-10 bg-border">
-                <div className="-translate-x-1/2 -rotate-90 absolute bottom-1/2 left-1/2 flex translate-y-1/2 items-center gap-1">
-                  <Avatar size="sm">
-                    <AvatarFallback>#</AvatarFallback>
-                  </Avatar>
-                  <div className="max-w-12 truncate font-bold font-display">
-                    Guild
-                  </div>
-                </div>
-              </div>
-              <div className="grid w-full grid-cols-[auto_auto_1fr] items-center gap-4 p-6">
-                <Avatar size="2xl">
-                  <AvatarFallback>#</AvatarFallback>
-                </Avatar>
-                <div>
-                  <div className="font-extrabold text-muted-foreground text-xs uppercase">
-                    TOP ROLE
-                  </div>
-                  <h3 className="mb-1 font-bold font-display text-xl tracking-tight">
-                    Enter Farcaster
-                  </h3>
-                  <p className="flex items-center gap-2 text-muted-foreground text-sm">
-                    <Users weight="bold" />
-                    Only 3.4% of users have this role
-                  </p>
-                </div>
-                <div className="flex items-center gap-2 justify-self-end">
-                  <div className="font-extrabold text-muted-foreground text-xs uppercase">
-                    COLLECTION:
-                  </div>
-                  <AvatarGroup imageUrls={["", "", ""]} count={87} size="lg" />
-                  <CaretDown weight="bold" className="ml-2 size-5" />
-                </div>
-              </div>
-            </Card>
+            <ContributionCard />
+            <ContributionCard />
+            <ContributionCard />
+            <Button size="sm" variant="outline" className="place-self-center">
+              See more involvement
+            </Button>
           </div>
         </div>
         <Separator className="mt-8 mb-32" />
       </LayoutMain>
     </Layout>
+  )
+}
+const ContributionCard = () => {
+  return (
+    <Card className="flex border-2">
+      <div className="relative h-full w-10 bg-border">
+        <div className="-translate-x-1/2 -rotate-90 absolute bottom-1/2 left-1/2 flex translate-y-1/2 items-center gap-1">
+          <Avatar size="sm">
+            <AvatarFallback>#</AvatarFallback>
+          </Avatar>
+          <div className="max-w-12 truncate font-bold font-display">Guild</div>
+        </div>
+      </div>
+      <div className="grid w-full grid-cols-[auto_1fr] items-center gap-4 p-6 md:grid-cols-[auto_auto_1fr]">
+        <Avatar className="size-16 sm:size-20">
+          <AvatarFallback>#</AvatarFallback>
+        </Avatar>
+        <div>
+          <div className="font-extrabold text-muted-foreground text-xs uppercase">
+            TOP ROLE
+          </div>
+          <h3 className="mb-1 font-bold font-display text-xl tracking-tight">
+            Enter Farcaster
+          </h3>
+          <div className="flex items-center gap-2 text-muted-foreground">
+            <Users weight="bold" className="min-w-4" />
+            <p className="line-clamp-1 text-sm">Only 3.4% of users have this role</p>
+          </div>
+        </div>
+        <div className="col-span-2 flex w-full flex-col gap-2 justify-self-end md:col-span-1 md:w-auto md:flex-row md:items-center">
+          <Separator className="mb-2 md:hidden" />
+          <div className="font-extrabold text-muted-foreground text-xs uppercase">
+            COLLECTION:
+          </div>
+          <AvatarGroup imageUrls={["", "", ""]} count={87} size="lg" />
+          <CaretDown weight="bold" className="ml-2 hidden size-5 md:block" />
+        </div>
+      </div>
+    </Card>
   )
 }
 
