d72ff8736b476cdef190e1d6294fd566a437c373
BrickheadJohnny <johnny.zgen@gmail.com>
2024-07-23 13:43:59 +0200

    fix(playwright): always generate html report

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: process.env.CI ? "github" : "html",
+  reporter: "html",
   outputDir: "playwright/results/",
 
   use: {
