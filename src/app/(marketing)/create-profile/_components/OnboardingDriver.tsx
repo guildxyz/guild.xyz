30f4c243b87d87d64c872098e15bfb602e33dce0
Dominik Stumpf <s.dominik.work@gmail.com>
2024-07-24 15:14:32 +0200

    chore: reposition driver buttons

--- src/app/(marketing)/create-profile/_components/OnboardingDriver.tsx
+++ src/app/(marketing)/create-profile/_components/OnboardingDriver.tsx
@@ -40,7 +40,7 @@ export const OnboardingDriver = () => {
 
   return (
     <>
-      <div className="fixed right-4 bottom-4 z-10 space-x-2">
+      <div className="fixed bottom-4 left-4 z-10 space-x-2">
         <Button onClick={() => dispatchChainAction("previous")}>previous</Button>
         <Button onClick={() => dispatchChainAction("next")}>next</Button>
       </div>
