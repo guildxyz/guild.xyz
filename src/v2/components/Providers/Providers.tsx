2c284531df00f6f47fffe04bd0fcb2bb239e7d6a
Dominik Stumpf <s.dominik.work@gmail.com>
2024-07-26 19:16:14 +0200

    chore: remove providers and account

--- src/v2/components/Providers/Providers.tsx
+++ src/v2/components/Providers/Providers.tsx
@@ -1,24 +1,15 @@
 "use client"
-
-import { FuelProvider } from "@fuels/react"
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { QueryClient } from "@tanstack/react-query"
 import { env } from "env"
-import { fuelConfig } from "fuelConfig"
 import { useAtomValue } from "jotai"
 import { ThemeProvider } from "next-themes"
 import dynamic from "next/dynamic"
-import { ReactNode, Suspense } from "react"
+import { ReactNode } from "react"
 import { SWRConfig } from "swr"
 import { fetcherForSWR } from "utils/fetcher"
 import { shouldUseReCAPTCHAAtom } from "utils/recaptcha"
-import { WagmiProvider } from "wagmi"
-import { wagmiConfig } from "wagmiConfig"
-import { AccountModal } from "../Account/components/AccountModal"
-import { Web3ConnectionManager } from "../Web3ConnectionManager"
 import { Toaster } from "../ui/Toaster"
 import { TooltipProvider } from "../ui/Tooltip"
-import { IntercomProvider } from "./IntercomProvider"
-import { PostHogProvider } from "./PostHogProvider"
 
 const DynamicReCAPTCHA = dynamic(() => import("v2/components/ReCAPTCHA"))
 
@@ -37,24 +28,7 @@ export function Providers({ children }: { children: ReactNode }) {
         disableTransitionOnChange
       >
         <TooltipProvider>
-          <SWRConfig value={{ fetcher: fetcherForSWR }}>
-            <WagmiProvider config={wagmiConfig} reconnectOnMount={false}>
-              <QueryClientProvider client={queryClient}>
-                <FuelProvider ui={false} fuelConfig={fuelConfig}>
-                  <PostHogProvider>
-                    <IntercomProvider>
-                      {children}
-                      <AccountModal />
-                      <Suspense>
-                        <Web3ConnectionManager />
-                      </Suspense>
-                    </IntercomProvider>
-                  </PostHogProvider>
-                </FuelProvider>
-              </QueryClientProvider>
-            </WagmiProvider>
-          </SWRConfig>
-
+          <SWRConfig value={{ fetcher: fetcherForSWR }}>{children}</SWRConfig>
           <Toaster />
         </TooltipProvider>
       </ThemeProvider>
