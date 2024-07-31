8fcaebcc1f78847b65381f55504cdb04a679d741
Dominik Stumpf <s.dominik.work@gmail.com>
2024-07-31 16:58:06 +0200

    test: add ToggleGroup story

--- src/v2/components/ui/ToggleGroup.stories.tsx
+++ src/v2/components/ui/ToggleGroup.stories.tsx
@@ -1,18 +1,15 @@
-import type { Meta, StoryFn } from "@storybook/react"
+import type { Meta, StoryObj } from "@storybook/react"
+import { ComponentProps, FunctionComponent } from "react"
 import { Toggle } from "./Toggle"
 import { ToggleGroup } from "./ToggleGroup"
 
-const meta: Meta = {
-  title: "Design system/ToggleGroup",
-}
-
 const items = ["Alpha", "Beta", "Gamma", "Delta"] as const
 
-export default meta
-
-export const Static: StoryFn = () => {
+const ToggleGroupStory: FunctionComponent<ComponentProps<typeof ToggleGroup>> = (
+  props
+) => {
   return (
-    <ToggleGroup type="single">
+    <ToggleGroup {...props}>
       {items.map((item, _) => (
         <Toggle value={item} key={item}>
           {item}
@@ -20,4 +17,45 @@ export const Static: StoryFn = () => {
       ))}
     </ToggleGroup>
   )
+}
+
+const meta: Meta<typeof ToggleGroupStory> = {
+  title: "Design system/ToggleGroup",
+  component: ToggleGroupStory,
+}
+
+export default meta
+
+type Story = StoryObj<typeof ToggleGroupStory>
+type ToggleGroupProps = Parameters<typeof ToggleGroupStory>[0]
+
+export const Default: Story = {
+  args: {
+    type: "single",
+    variant: "primary",
+    size: "md",
+  },
+  argTypes: {
+    type: {
+      type: "string",
+      control: "select",
+      options: ["multiple", "single"] satisfies ToggleGroupProps["type"][],
+    },
+    variant: {
+      type: "string",
+      control: "select",
+      options: [
+        "secondary",
+        "primary",
+        "primary-ghost",
+        "mono",
+        "outline",
+      ] satisfies ToggleGroupProps["variant"][],
+    },
+    size: {
+      type: "string",
+      control: "select",
+      options: ["sm", "md", "lg", "icon"] satisfies ToggleGroupProps["size"][],
+    },
+  },
 }
