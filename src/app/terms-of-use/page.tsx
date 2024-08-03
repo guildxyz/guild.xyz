b1c90e0a6f78af77223a405881ba95ab4e2c20d3
BrickheadJohnny <johnny.zgen@gmail.com>
2024-08-03 09:09:52 +0200

    feat: migrate the `/privacy-policy` page to Tailwind CSS

--- src/app/terms-of-use/page.tsx
+++ src/app/terms-of-use/page.tsx
@@ -21,6 +21,7 @@ const Page = (): JSX.Element => (
         <LayoutTitle>Terms of Use</LayoutTitle>
       </LayoutHeadline>
     </LayoutHero>
+
     <LayoutMain className="prose prose-counters flex max-w-screen-md flex-col gap-8 prose-headings:font-display prose-headings:text-foreground prose-li:text-foreground text-foreground marker:text-foreground">
       <section>
         <h2>Welcome to Guild.xyz!</h2>
