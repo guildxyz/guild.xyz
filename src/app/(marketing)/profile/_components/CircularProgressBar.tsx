b1c08d1bbc8d6faccd5ba6fb0e6b375b3b93b1c4
Dominik Stumpf <s.dominik.work@gmail.com>
2024-08-07 21:40:54 +0200

    Revert "chore: remove xp metagame related components"
    This reverts commit 4d47e768bf72ffa0a5c0af18b26cfd5b37575cd8.

---
+++ src/app/(marketing)/profile/_components/CircularProgressBar.tsx
@@ -0,0 +1,10 @@
+"use client"
+
+const chartData = [{ experience: 1, fill: "hsl(var(--primary))" }]
+
+// TODO: use svg clip-path
+export const CircularProgressBar = ({ progress }: { progress: number }) => {
+  const size = 224
+  const strokeWidth = 10
+  return null
+}
---
+++ src/app/(marketing)/profile/_components/LevelBadge.tsx
@@ -0,0 +1,47 @@
+import { cn } from "@/lib/utils"
+import { MAX_LEVEL, RANKS } from "../[username]/constants"
+
+type PolygonProps = {
+  sides: number
+  radius: number
+  color: string
+}
+
+const Polygon = ({ sides, radius, color }: PolygonProps) => {
+  const angleStep = (2 * Math.PI) / sides
+  const points = Array.from({ length: sides }, (_, i) => {
+    const x = 20 + radius * Math.cos(i * angleStep)
+    const y = 20 + radius * Math.sin(i * angleStep)
+    return `${x},${y}`
+  }).join(" ")
+
+  return (
+    <svg width={100} height={100}>
+      <polygon
+        points={points}
+        stroke={color}
+        fill={color}
+        strokeWidth="10"
+        strokeLinejoin="round"
+      />
+    </svg>
+  )
+}
+
+type LevelBadgeProps = {
+  level: number
+  className?: string
+}
+
+export const LevelBadge = ({ level, className }: LevelBadgeProps) => {
+  const rank =
+    RANKS[Math.min(Math.floor((level / MAX_LEVEL) * RANKS.length), RANKS.length - 1)]
+
+  return (
+    <div className={cn("relative grid size-12 place-items-center", className)}>
+      <div className="size-full rounded-full" style={{ background: rank.color }} />
+      <div className="absolute select-none font-extrabold text-xl">{level}</div>
+    </div>
+  )
+}
+// <Polygon sides={5} radius={20} color={"hsl(var(--primary))"} />
