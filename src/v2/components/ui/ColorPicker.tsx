47948488b241f7a7a98ce55dd2a2b7706028ef4e
valid <valid@zgen.hu>
2024-08-09 01:13:10 +0200

    chore(ColorPicker): use Form components

--- src/v2/components/ui/ColorPicker.tsx
+++ src/v2/components/ui/ColorPicker.tsx
@@ -3,7 +3,8 @@ import { Palette } from "@phosphor-icons/react"
 import Color from "color"
 import useDebouncedState from "hooks/useDebouncedState"
 import { useEffect, useState } from "react"
-import { useController, useWatch } from "react-hook-form"
+import { useWatch } from "react-hook-form"
+import { FormControl, FormErrorMessage, FormField, FormItem } from "./Form"
 import { Input } from "./Input"
 
 type Props = {
@@ -12,8 +13,6 @@ type Props = {
 }
 
 export const ColorPicker = ({ fieldName, onChange }: Props) => {
-  const { field } = useController({ name: fieldName })
-
   const [isIconLight, setIsIconLight] = useState(false)
 
   const pickedColor = useWatch({ name: fieldName })
@@ -27,17 +26,35 @@ export const ColorPicker = ({ fieldName, onChange }: Props) => {
   }, [debouncedPickedColor, onChange])
 
   return (
-    <div className="flex gap-2">
-      <div className="flex size-10 shrink-0 items-center justify-center overflow-hidden rounded-xl border">
-        <input className="size-16 shrink-0 cursor-pointer" type="color" {...field} />
-        <Icon
-          as={Palette}
-          pos="absolute"
-          pointerEvents={"none"}
-          color={isIconLight ? "whiteAlpha.800" : "blackAlpha.800"}
-        />
-      </div>
-      <Input className="h-10 max-w-40" {...field} placeholder="Pick a color" />
-    </div>
+    <FormField
+      name={fieldName}
+      render={({ field }) => (
+        <FormItem>
+          <FormControl>
+            <div className="flex gap-1.5">
+              <div className="flex size-10 shrink-0 items-center justify-center overflow-hidden rounded-xl border">
+                <input
+                  className="size-16 shrink-0 cursor-pointer"
+                  type="color"
+                  {...field}
+                />
+                <Icon
+                  as={Palette}
+                  pos="absolute"
+                  pointerEvents={"none"}
+                  color={isIconLight ? "whiteAlpha.800" : "blackAlpha.800"}
+                />
+              </div>
+              <Input
+                className="h-10 max-w-40"
+                placeholder="Pick a color"
+                {...field}
+              />
+            </div>
+          </FormControl>
+          <FormErrorMessage />
+        </FormItem>
+      )}
+    />
   )
 }
