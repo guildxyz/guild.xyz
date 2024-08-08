51579f667767f8e59c9a4da001e3f09d3c1c5e2d
Dominik Stumpf <s.dominik.work@gmail.com>
2024-08-08 12:27:22 +0200

    feat: add profile owner guard

--- src/app/(marketing)/profile/[username]/page.tsx
+++ src/app/(marketing)/profile/[username]/page.tsx
@@ -7,117 +7,24 @@ import {
   LayoutHero,
   LayoutMain,
 } from "@/components/Layout"
-// import { useContribution } from "../_hooks/useContribution"
-// import { useProfile } from "../_hooks/useProfile"
 import { SWRProvider } from "@/components/SWRProvider"
 import { Anchor } from "@/components/ui/Anchor"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/Avatar"
 import { AvatarGroup } from "@/components/ui/AvatarGroup"
 import { Separator } from "@/components/ui/Separator"
 import { Skeleton } from "@/components/ui/Skeleton"
-// import { useUserPublic } from "@/hooks/useUserPublic"
+import { Schemas } from "@guildxyz/types"
 import { ArrowRight } from "@phosphor-icons/react/dist/ssr"
+import { env } from "env"
+import { unstable_serialize } from "swr"
 import { ContributionCard } from "../_components/ContributionCard"
 import { EditContributions } from "../_components/EditContributions"
 import { EditProfile } from "../_components/EditProfile"
 import { OperatedGuildCard } from "../_components/OperatedGuildCard"
-// import { ProfileSkeleton } from "../_components/ProfileSkeleton"
+import { ProfileOwnerGuard } from "../_components/ProfileOwnerGuard"
 import { RecentActivity } from "../_components/RecentActivity"
 
-// async function getProfileData(username: string) {
-//   const req = `https://api.guild.xyz/v2/profiles/${username}`
-//   try {
-//     const res = await fetch(req)
-//     if (!res.ok) {
-//       throw new Error("Failed to fetch profile data")
-//     }
-//     return res.json() as Promise<Schemas["Profile"]>
-//   } catch (e) {
-//     // mocking for the time being if fetch fails
-//     console.error(e)
-//     const res = {
-//       id: 4,
-//       userId: 6027190,
-//       username: "durumm",
-//       name: "durum",
-//       bio: null,
-//       profileImageUrl:
-//         "https://guild-xyz.mypinata.cloud/ipfs/QmWGdo6FkjSz22oDZFMJysx3hGKoVqtzTWVMx9tTSP7jvi",
-//       backgroundImageUrl: null,
-//       createdAt: "2024-07-25T10:04:20.781Z",
-//       updatedAt: "2024-07-25T10:04:20.781Z",
-//     }
-//     return res
-//   }
-// }
-
-import { Schemas } from "@guildxyz/types"
-import { env } from "env"
-// import { Header } from "@/components/Header"
-// import {
-//   Layout,
-//   LayoutBanner,
-//   LayoutFooter,
-//   LayoutHeadline,
-//   LayoutHero,
-//   LayoutMain,
-//   LayoutTitle,
-// } from "@/components/Layout"
-// import { env } from "env"
-import { unstable_serialize } from "swr"
-// import { SearchParams } from "types"
-// import { Explorer } from "./_components/Explorer"
-// import { ExplorerSWRProvider } from "./_components/ExplorerSWRProvider"
-// import { HeaderBackground } from "./_components/HeaderBackground"
-// import { ActiveSection } from "./types"
-//
-// export const metadata = {
-//   icons: {
-//     // @ts-ignore: "as" prop not typed out.
-//     other: [{ rel: "preload", url: "/banner.svg", as: "image" }],
-//   },
-// }
-//
-// const Page = async ({ searchParams }: { searchParams: SearchParams }) => {
-//   const featuredPath = `/v2/guilds?order=FEATURED&offset=0&limit=24`
-//   const newestPath = `/v2/guilds?order=NEWEST&offset=0&limit=24`
-//   const [ssrFeaturedGuilds, ssrNewestGuilds] = await Promise.all([
-//     fetch(`${env.NEXT_PUBLIC_API.replace("/v1", "")}${featuredPath}`, {
-//       next: {
-//         revalidate: 600,
-//       },
-//     })
-//       .then((res) => res.json())
-//       .catch((_) => []),
-//     fetch(`${env.NEXT_PUBLIC_API.replace("/v1", "")}${newestPath}`, {
-//       next: {
-//         revalidate: 600,
-//       },
-//     })
-//       .then((res) => res.json())
-//       .catch((_) => []),
-//   ])
-//
-//   return (
-//     <ExplorerSWRProvider
-//       value={{
-//         fallback: {
-//           [infinite_unstable_serialize(() => featuredPath)]: ssrFeaturedGuilds,
-//           [infinite_unstable_serialize(() => newestPath)]: ssrNewestGuilds,
-//         },
-//       }}
-//     >
-
 const Page = async ({ params: { username } }: { params: { username: string } }) => {
-  // const { data: profile, isLoading } = useProfile()
-  // const contributions = useContribution()
-  // const { id: publicUserId } = useUserPublic()
-  // const isProfileOwner = !!profile?.userId && publicUserId === profile.userId
-  //
-  // if (!profile || isLoading) {
-  //   return <ProfileSkeleton />
-  // }
-  const isProfileOwner = false // !!profile?.userId && publicUserId === profile.userId
   const api = env.NEXT_PUBLIC_API.replace("/v1", "")
   const path = `/v2/profiles/${username}`
   const contributionsPath = `${path}/contributions`
@@ -137,7 +44,6 @@ const Page = async ({ params: { username } }: { params: { username: string } }) 
     })
   ).json()) as Schemas["Profile"]
 
-  if (!profile) return
   return (
     <SWRProvider
       value={{
@@ -158,7 +64,9 @@ const Page = async ({ params: { username } }: { params: { username: string } }) 
         <LayoutMain>
           <div className="mt-24">
             <div className="relative mb-24 flex flex-col items-center">
-              {isProfileOwner && <EditProfile />}
+              <ProfileOwnerGuard>
+                <EditProfile />
+              </ProfileOwnerGuard>
               <div className="relative mb-12 flex items-center justify-center">
                 <Avatar className="size-48">
                   <AvatarImage
@@ -206,7 +114,9 @@ const Page = async ({ params: { username } }: { params: { username: string } }) 
             <OperatedGuildCard />
             <div className="mt-8 mb-3 flex items-center justify-between">
               <h2 className="font-bold text-lg">Top contributions</h2>
-              {isProfileOwner && <EditContributions />}
+              <ProfileOwnerGuard>
+                <EditContributions />
+              </ProfileOwnerGuard>
             </div>
             <div className="grid grid-cols-1 gap-3">
               {contributions.map((contribution) => (
