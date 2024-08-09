46d6c658201c9f00475693d2cadea37646bdb1f5
BrickheadJohnny <johnny.zgen@gmail.com>
2024-08-09 09:23:57 +0200

    fix(PlatformCardMenu): lighter icon button color in light mode

--- src/components/[guild]/RolePlatforms/components/PlatformCard/components/PlatformCardMenu.tsx
+++ src/components/[guild]/RolePlatforms/components/PlatformCard/components/PlatformCardMenu.tsx
@@ -1,23 +1,38 @@
-import { IconButton, Menu, MenuButton, MenuList, Portal } from "@chakra-ui/react"
+import {
+  ButtonProps,
+  IconButton,
+  Menu,
+  MenuButton,
+  MenuList,
+  Portal,
+  useColorModeValue,
+} from "@chakra-ui/react"
 import { DotsThree } from "@phosphor-icons/react"
 import { PropsWithChildren } from "react"
 
-const PlatformCardMenu = ({ children }: PropsWithChildren<unknown>) => (
-  <Menu placement="bottom-end" closeOnSelect={false}>
-    <MenuButton
-      as={IconButton}
-      icon={<DotsThree />}
-      aria-label="Reward menu"
-      boxSize={8}
-      minW={8}
-      rounded="full"
-      colorScheme="alpha"
-    />
+const PlatformCardMenu = ({ children }: PropsWithChildren<unknown>) => {
+  const colorScheme = useColorModeValue<
+    ButtonProps["colorScheme"],
+    ButtonProps["colorScheme"]
+  >(undefined, "alpha")
 
-    <Portal>
-      <MenuList>{children}</MenuList>
-    </Portal>
-  </Menu>
-)
+  return (
+    <Menu placement="bottom-end" closeOnSelect={false}>
+      <MenuButton
+        as={IconButton}
+        icon={<DotsThree />}
+        aria-label="Reward menu"
+        boxSize={8}
+        minW={8}
+        rounded="full"
+        colorScheme={colorScheme}
+      />
+
+      <Portal>
+        <MenuList>{children}</MenuList>
+      </Portal>
+    </Menu>
+  )
+}
 
 export default PlatformCardMenu
