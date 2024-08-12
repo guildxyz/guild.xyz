273b64d55b56c3bd6c592f87faa9dc19a3b635c6
BrickheadJohnny <johnny.zgen@gmail.com>
2024-08-12 12:58:22 +0200

    feat(PlatformCardMenu): ghost variant

--- src/components/[guild]/RolePlatforms/components/PlatformCard/components/PlatformCardMenu.tsx
+++ src/components/[guild]/RolePlatforms/components/PlatformCard/components/PlatformCardMenu.tsx
@@ -1,38 +1,23 @@
-import {
-  ButtonProps,
-  IconButton,
-  Menu,
-  MenuButton,
-  MenuList,
-  Portal,
-  useColorModeValue,
-} from "@chakra-ui/react"
+import { IconButton, Menu, MenuButton, MenuList, Portal } from "@chakra-ui/react"
 import { DotsThree } from "@phosphor-icons/react"
 import { PropsWithChildren } from "react"
 
-const PlatformCardMenu = ({ children }: PropsWithChildren<unknown>) => {
-  const colorScheme = useColorModeValue<
-    ButtonProps["colorScheme"],
-    ButtonProps["colorScheme"]
-  >(undefined, "alpha")
+const PlatformCardMenu = ({ children }: PropsWithChildren<unknown>) => (
+  <Menu placement="bottom-end" closeOnSelect={false}>
+    <MenuButton
+      as={IconButton}
+      icon={<DotsThree />}
+      aria-label="Reward menu"
+      boxSize={8}
+      minW={8}
+      rounded="full"
+      variant="ghost"
+    />
 
-  return (
-    <Menu placement="bottom-end" closeOnSelect={false}>
-      <MenuButton
-        as={IconButton}
-        icon={<DotsThree />}
-        aria-label="Reward menu"
-        boxSize={8}
-        minW={8}
-        rounded="full"
-        colorScheme={colorScheme}
-      />
-
-      <Portal>
-        <MenuList>{children}</MenuList>
-      </Portal>
-    </Menu>
-  )
-}
+    <Portal>
+      <MenuList>{children}</MenuList>
+    </Portal>
+  </Menu>
+)
 
 export default PlatformCardMenu
