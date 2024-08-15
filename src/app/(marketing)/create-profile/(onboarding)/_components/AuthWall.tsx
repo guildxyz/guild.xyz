11c5265af84623809e8ca12b8f9d5434178f824d
Dominik Stumpf <s.dominik.work@gmail.com>
2024-08-15 19:30:36 +0200

    chore: clean up comments and unused file

--- src/app/(marketing)/create-profile/(onboarding)/_components/AuthWall.tsx
+++ src/app/(marketing)/create-profile/(onboarding)/_components/AuthWall.tsx
@@ -7,26 +7,11 @@ import { ChainSkeleton } from "./ChainSkeleton"
 
 export const AuthWall = ({ children }: PropsWithChildren) => {
   const { isWeb3Connected } = useWeb3ConnectionManager()
-  // const { toast } = useToast()
   const router = useRouter()
-  // const referrerUserId = useSearchParams()?.get(REFERRER_USER_SEARCH_PARAM_KEY)
-  // const user = useUser(referrerUserId)
 
   useEffect(() => {
     if (isWeb3Connected === false) router.replace("/create-profile")
   }, [isWeb3Connected, router.replace])
-
-  // useEffect(() => {
-  //   if (isWeb3Connected === false) router.replace("/create-profile")
-  //   if (!referrerUserId) router.push("claim-pass")
-  //   if (user.guildProfile) router.replace("choose-pass")
-  //   toast({
-  //     variant: "error",
-  //     title: "Failed to identify referrer profile",
-  //     description: "Enter the username below and make sure the profile exists",
-  //   })
-  //   router.replace("claim-pass")
-  // }, [router.replace, isWeb3Connected, user, referrerUserId])
 
   if (!isWeb3Connected) {
     return <ChainSkeleton />
