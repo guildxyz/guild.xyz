126454412419a3dde3fdca93779a83386b8dceba
Dominik Stumpf <s.dominik.work@gmail.com>
2024-08-16 11:31:37 +0200

    chore: improve auth wall page design

--- src/app/(marketing)/create-profile/page.tsx
+++ src/app/(marketing)/create-profile/page.tsx
@@ -2,7 +2,9 @@
 
 import { walletSelectorModalAtom } from "@/components/Providers/atoms"
 import { useWeb3ConnectionManager } from "@/components/Web3ConnectionManager/hooks/useWeb3ConnectionManager"
-import { Button } from "@/components/ui/Button"
+import { Anchor } from "@/components/ui/Anchor"
+import { Button, buttonVariants } from "@/components/ui/Button"
+import { ArrowLeft, SignIn } from "@phosphor-icons/react"
 import { useSetAtom } from "jotai"
 import { useRouter } from "next/navigation"
 
@@ -14,19 +16,35 @@ const Page = () => {
   if (isWeb3Connected) {
     router.replace("/create-profile/claim-pass")
   }
-
   return (
-    <div className="flex w-[28rem] items-center gap-8 space-y-3 p-8">
-      <h1 className="text-pretty font-bold font-display text-2xl leading-none tracking-tight">
+    <div className="flex max-w-[28rem] flex-col items-center gap-4 space-y-3">
+      <h1 className="text-pretty px-8 pt-8 font-bold font-display text-2xl leading-none tracking-tight">
         Sign in to create your profile
       </h1>
-      <Button
-        onClick={() => setIsWalletSelectorModalOpen(true)}
-        colorScheme="primary"
-        size="lg"
-      >
-        <span>Sign in</span>
-      </Button>
+      <div className="w-full space-y-4 bg-card-secondary p-8">
+        <Button
+          onClick={() => setIsWalletSelectorModalOpen(true)}
+          colorScheme="primary"
+          size="lg"
+          className="w-full"
+        >
+          <SignIn weight="bold" />
+          <span>Sign in</span>
+        </Button>
+        <Anchor
+          href="/"
+          variant="unstyled"
+          className={buttonVariants({
+            colorScheme: "secondary",
+            variant: "ghost",
+            size: "lg",
+            className: "w-full",
+          })}
+        >
+          <ArrowLeft />
+          <span>Return to homepage</span>
+        </Anchor>
+      </div>
     </div>
   )
 }
