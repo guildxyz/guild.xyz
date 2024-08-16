41525e7ecb60adf7de4620fec99998a50631d090
Dominik Stumpf <s.dominik.work@gmail.com>
2024-08-16 15:00:14 +0200

    chore: compress guild pass models

--- src/app/(marketing)/create-profile/_components/GuildPassScene.tsx
+++ src/app/(marketing)/create-profile/_components/GuildPassScene.tsx
@@ -1,48 +1,25 @@
 "use client"
 
-import {
-  AccumulativeShadows,
-  Environment,
-  Float,
-  Gltf,
-  RandomizedLight,
-  useGLTF,
-} from "@react-three/drei"
+import { Environment, Float } from "@react-three/drei"
 import { Canvas } from "@react-three/fiber"
 import { FunctionComponent } from "react"
 import * as THREE from "three"
 import { SUBSCRIPTIONS } from "../constants"
+import { Model as BasicModel } from "./BasicGuildPass"
+import { Model as GoldModel } from "./GoldGuildPass"
 
 type SceneVariant = (typeof SUBSCRIPTIONS)[number]["title"]
 
 function SinglePass() {
-  return (
-    <Gltf
-      src={"models/basic_guild_pass.glb"}
-      rotation={new THREE.Euler(Math.PI / 2, 0.0, 0.0)}
-      scale={2}
-    />
-  )
+  return <BasicModel rotation={new THREE.Euler(Math.PI / 2, 0.0, 0.0)} scale={2} />
 }
 
 function BundlePass() {
-  return (
-    <Gltf
-      src={"models/basic_guild_pass.glb"}
-      rotation={new THREE.Euler(Math.PI / 2, 0.0, 0.0)}
-      scale={2}
-    />
-  )
+  return <BasicModel rotation={new THREE.Euler(Math.PI / 2, 0.0, 0.0)} scale={2} />
 }
 
 function LifetimePass() {
-  return (
-    <Gltf
-      src={"models/gold_guild_pass.glb"}
-      rotation={new THREE.Euler(Math.PI / 2, 0.0, 0.0)}
-      scale={2}
-    />
-  )
+  return <GoldModel rotation={new THREE.Euler(Math.PI / 2, 0.0, 0.0)} scale={2} />
 }
 
 const Variants: Record<SceneVariant, FunctionComponent> = {
@@ -67,29 +44,7 @@ export const GuildPassScene = ({ sceneVariant }: { sceneVariant: SceneVariant })
       <Float floatIntensity={6} speed={2.4}>
         <Variant />
       </Float>
-      <AccumulativeShadows
-        temporal
-        frames={100}
-        color="white"
-        colorBlend={2}
-        toneMapped={true}
-        alphaTest={0.75}
-        opacity={2}
-        scale={12}
-      >
-        <RandomizedLight
-          intensity={Math.PI}
-          amount={8}
-          radius={4}
-          ambient={0.5}
-          position={[5, 5, -10]}
-          bias={0.001}
-        />
-      </AccumulativeShadows>
       <Environment preset="city" />
     </Canvas>
   )
 }
-
-useGLTF.preload("models/gold_guild_pass.glb")
-useGLTF.preload("models/basic_guild_pass.glb")
