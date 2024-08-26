4dce736166f466f9c1de7af135cbc1724b84b08a
Dominik Stumpf <s.dominik.work@gmail.com>
2024-08-26 12:35:09 +0200

    chore: prevent pointer event overlay

--- src/app/(marketing)/profile/_components/JoinProfileAction.tsx
+++ src/app/(marketing)/profile/_components/JoinProfileAction.tsx
@@ -14,31 +14,29 @@ export const JoinProfileAction = () => {
     return
   }
   return (
-    <div className="fixed bottom-8 flex w-full justify-center px-4">
+    <div className="pointer-events-none fixed bottom-6 flex w-full justify-center px-4">
       <Anchor
         variant="unstyled"
-        className="rounded-2xl"
+        className="slide-in-from-bottom fade-in pointer-events-auto animate-in rounded-2xl duration-700"
         href={`/create-profile/claim-pass?${REFERRER_USER_SEARCH_PARAM_KEY}=${profile.data.username}`}
       >
         <Card
-          className="slide-in-from-bottom fade-in max-w-md animate-in border border-transparent font-medium leading-tight duration-700"
+          className="flex max-w-md items-center gap-3 border border-transparent p-4 font-medium leading-tight"
           style={{
             background:
               "padding-box linear-gradient(hsl(var(--card-secondary)), hsl(var(--card-secondary))), border-box linear-gradient(45deg, hsl(var(--primary)), violet, yellow, orange)",
           }}
         >
-          <div className="flex items-center gap-3 p-4">
-            <div className="flex aspect-square items-center justify-center rounded-2xl border bg-card p-2">
-              <Mountains className="size-5" weight="fill" />
-            </div>
-            <div className="text-pretty">
-              Join{" "}
-              <span className="font-extrabold text-primary-subtle">
-                {profile.data.name || profile.data.username}
-              </span>{" "}
-              on their adventure
-              <ArrowRight weight="bold" className="ml-2 inline size-4" />
-            </div>
+          <div className="flex aspect-square items-center justify-center rounded-2xl border bg-card p-2">
+            <Mountains className="size-5" weight="fill" />
+          </div>
+          <div className="text-pretty">
+            Join{" "}
+            <span className="font-extrabold text-primary-subtle">
+              {profile.data.name || profile.data.username}
+            </span>{" "}
+            on their adventure
+            <ArrowRight weight="bold" className="ml-2 inline size-4" />
           </div>
         </Card>
       </Anchor>
