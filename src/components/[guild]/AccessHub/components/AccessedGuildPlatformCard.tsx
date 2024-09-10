df45f9e4b5ffdc28631f7812e440888720d7bdfb
BrickheadJohnny <johnny.zgen@gmail.com>
2024-09-10 11:17:12 +0200

    feat(AccessedGuildPlatformCard): always display warning component

--- src/components/[guild]/AccessHub/components/AccessedGuildPlatformCard.tsx
+++ src/components/[guild]/AccessHub/components/AccessedGuildPlatformCard.tsx
@@ -2,45 +2,55 @@ import PlatformCard from "components/[guild]/RolePlatforms/components/PlatformCa
 import { useRolePlatform } from "components/[guild]/RolePlatforms/components/RolePlatformProvider"
 import useGuild from "components/[guild]/hooks/useGuild"
 import useGuildPermission from "components/[guild]/hooks/useGuildPermission"
-import rewards from "rewards"
 import { cardPropsHooks } from "rewards/cardPropsHooks"
-import rewardComponents from "rewards/components"
+import rewardComponentsConfig from "rewards/components"
 import { PlatformName, PlatformType } from "types"
 import PlatformAccessButton from "./PlatformAccessButton"
 
 const AccessedGuildPlatformCard = () => {
-  const { guildPlatform } = useRolePlatform()
+  const rolePlatform = useRolePlatform()
   const { isDetailed } = useGuild()
   const { isAdmin } = useGuildPermission()
 
-  if (!rewards[PlatformType[guildPlatform.platformId]]) return null
+  const rewardComponents =
+    rewardComponentsConfig[
+      PlatformType[rolePlatform.guildPlatform.platformId] as PlatformName
+    ]
+  const useCardProps =
+    cardPropsHooks[PlatformType[rolePlatform.guildPlatform.platformId]]
+
+  if (!rewardComponents || !useCardProps) return null
 
   const {
     cardMenuComponent: PlatformCardMenu,
     cardWarningComponent: PlatformCardWarning,
     cardButton: PlatformCardButton,
-  } = rewardComponents[PlatformType[guildPlatform.platformId] as PlatformName]
-  const useCardProps = cardPropsHooks[PlatformType[guildPlatform.platformId]]
+  } = rewardComponents
 
   return (
     <PlatformCard
       usePlatformCardProps={useCardProps}
-      guildPlatform={guildPlatform}
+      guildPlatform={rolePlatform.guildPlatform}
       cornerButton={
-        isAdmin && isDetailed && PlatformCardMenu ? (
-          <PlatformCardMenu platformGuildId={guildPlatform.platformGuildId} />
-        ) : PlatformCardWarning ? (
-          <PlatformCardWarning guildPlatform={guildPlatform} />
-        ) : null
+        <>
+          {PlatformCardWarning && (
+            <PlatformCardWarning rolePlatform={rolePlatform} />
+          )}
+          {isAdmin && isDetailed && PlatformCardMenu && (
+            <PlatformCardMenu
+              platformGuildId={rolePlatform.guildPlatform.platformGuildId}
+            />
+          )}
+        </>
       }
       h="full"
       p={{ base: 3, sm: 4 }}
       boxShadow="none"
     >
       {PlatformCardButton ? (
-        <PlatformCardButton platform={guildPlatform} />
+        <PlatformCardButton platform={rolePlatform.guildPlatform} />
       ) : (
-        <PlatformAccessButton platform={guildPlatform} />
+        <PlatformAccessButton platform={rolePlatform.guildPlatform} />
       )}
     </PlatformCard>
   )
