97c6ab2417fe178db1a872013e46ab70b65dece3
BrickheadJohnny <johnny.zgen@gmail.com>
2024-09-10 11:12:32 +0200

    fix(GoogleCardWarning): remove unused logic/comments + refactor

--- src/rewards/Google/GoogleCardWarning.tsx
+++ src/rewards/Google/GoogleCardWarning.tsx
@@ -9,33 +9,18 @@ import {
 } from "@chakra-ui/react"
 import { WarningCircle } from "@phosphor-icons/react"
 import useGuild from "components/[guild]/hooks/useGuild"
-import { GuildPlatform } from "types"
-
-type Props = {
-  guildPlatform: GuildPlatform
-  roleMemberCount?: number
-  size?: "sm" | "md"
-}
+import { ReactNode } from "react"
+import { CardWarningComponentProps } from "rewards/types"
 
 const GoogleCardWarning = ({
-  guildPlatform,
-  roleMemberCount,
-  size = "md",
-}: Props): JSX.Element => {
+  rolePlatform,
+}: CardWarningComponentProps): ReactNode => {
   const { roles } = useGuild()
-  const rolesWithPlatform = roles.filter((role) =>
-    role.rolePlatforms?.some(
-      (rolePlatform) => rolePlatform.guildPlatformId === guildPlatform?.id
-    )
-  )
-  // const eligibleMembers = useUniqueMembers(rolesWithPlatform)
+  const roleMemberCount =
+    roles?.find((role) => role.rolePlatforms.some((rp) => rp.id === rolePlatform.id))
+      ?.memberCount ?? 0
 
-  // if (eligibleMembers.length < 600) return null
-  if (
-    roleMemberCount < 0 ||
-    !rolesWithPlatform?.some((role) => role.memberCount >= 600)
-  )
-    return null
+  if (roleMemberCount <= 600) return null
 
   return (
     <Popover trigger="hover" openDelay={0}>
@@ -44,9 +29,7 @@ const GoogleCardWarning = ({
           as={WarningCircle}
           color="orange.300"
           weight="fill"
-          // boxSize={size === "sm" ? 5 : 6}
           boxSize={6}
-          p={size === "sm" ? "2px" : 0}
           tabIndex={0}
         />
       </PopoverTrigger>
@@ -54,11 +37,8 @@ const GoogleCardWarning = ({
         <PopoverContent>
           <PopoverArrow />
           <PopoverBody>
-            {/* {`Google limits documentum sharing to 600 users, and there're already ${eligibleMembers.length}
-            eligible members, so you might not get access to this reward.`} */}
             {`Google limits documentum sharing to 600 users, and there're already ${
-              roleMemberCount ??
-              rolesWithPlatform?.find((role) => role.memberCount >= 600)?.memberCount
+              roleMemberCount
             }
             eligible members, so you might not get access to this reward.`}
           </PopoverBody>
