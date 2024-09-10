553d87c442183153844d4a5b0a41e1739441fac4
BrickheadJohnny <johnny.zgen@gmail.com>
2024-09-10 11:23:01 +0200

    feat: `CardWarningComponentBase` component

--- src/rewards/Google/GoogleCardWarning.tsx
+++ src/rewards/Google/GoogleCardWarning.tsx
@@ -1,15 +1,7 @@
-import {
-  Icon,
-  Popover,
-  PopoverArrow,
-  PopoverBody,
-  PopoverContent,
-  PopoverTrigger,
-  Portal,
-} from "@chakra-ui/react"
-import { WarningCircle } from "@phosphor-icons/react"
+import {} from "@chakra-ui/react"
 import useGuild from "components/[guild]/hooks/useGuild"
 import { ReactNode } from "react"
+import { CardWarningComponentBase } from "rewards/components/CardWarningComponentBase"
 import { CardWarningComponentProps } from "rewards/types"
 
 const GoogleCardWarning = ({
@@ -23,28 +15,12 @@ const GoogleCardWarning = ({
   if (roleMemberCount <= 600) return null
 
   return (
-    <Popover trigger="hover" openDelay={0}>
-      <PopoverTrigger>
-        <Icon
-          as={WarningCircle}
-          color="orange.300"
-          weight="fill"
-          boxSize={6}
-          tabIndex={0}
-        />
-      </PopoverTrigger>
-      <Portal>
-        <PopoverContent>
-          <PopoverArrow />
-          <PopoverBody>
-            {`Google limits documentum sharing to 600 users, and there're already ${
-              roleMemberCount
-            }
+    <CardWarningComponentBase>
+      {`Google limits documentum sharing to 600 users, and there're already ${
+        roleMemberCount
+      }
             eligible members, so you might not get access to this reward.`}
-          </PopoverBody>
-        </PopoverContent>
-      </Portal>
-    </Popover>
+    </CardWarningComponentBase>
   )
 }
 
