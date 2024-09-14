501649ae3c599619734ad96576d954ec611d87af
BrickheadJohnny <johnny.zgen@gmail.com>
2024-09-14 12:38:00 +0200

    feat: `Checkbox` component & story

---
+++ src/v2/components/ui/Checkbox.stories.tsx
@@ -0,0 +1,38 @@
+import { Meta, StoryObj } from "@storybook/react"
+import { Checkbox, CheckboxProps } from "./Checkbox"
+import { Label } from "./Label"
+
+const CheckboxExample = (props: CheckboxProps) => {
+  return (
+    <div className="flex items-center space-x-2">
+      <Checkbox id="terms" {...props} />
+      <Label
+        htmlFor="terms"
+        className="peer-disabled:cursor-not-allowed peer-disabled:opacity-50"
+      >
+        Accept terms and conditions
+      </Label>
+    </div>
+  )
+}
+
+const meta: Meta<typeof CheckboxExample> = {
+  title: "Design system/Checkbox",
+  component: CheckboxExample,
+}
+
+export default meta
+
+type Story = StoryObj<typeof CheckboxExample>
+
+export const Default: Story = {
+  args: {
+    disabled: false,
+  },
+  argTypes: {
+    disabled: {
+      type: "boolean",
+      control: "boolean",
+    },
+  },
+}
