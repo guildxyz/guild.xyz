2e6a9f59812cd159390ae8b18c1e5d42d40f899f
BrickheadJohnny <johnny.zgen@gmail.com>
2024-09-15 15:04:16 +0200

    fix(playwright config): increase workers limit

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 3 : undefined,
   reporter: "html",
   outputDir: "playwright/results/",
 
