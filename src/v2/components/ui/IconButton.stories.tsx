0354ac9498284c8f1d5366b4c6b7ff648f0caed5
BrickheadJohnny <johnny.zgen@gmail.com>
2024-09-18 10:20:20 +0200

    feat: `IconButton` component & stories

---
+++ src/v2/components/ui/IconButton.stories.tsx
@@ -0,0 +1,85 @@
+import { CircleDashed } from "@phosphor-icons/react/dist/ssr"
+import { Meta, StoryObj } from "@storybook/react"
+import { IconButton, IconButtonProps } from "./IconButton"
+
+type IconButtonExampleProps = Omit<IconButtonProps, "icon">
+
+const IconButtonExample = (props: IconButtonExampleProps) => (
+  <IconButton {...props} icon={<CircleDashed weight="bold" />} />
+)
+
+const meta: Meta<typeof IconButtonExample> = {
+  title: "Design system/IconButton",
+  component: IconButtonExample,
+}
+
+export default meta
+
+type Story = StoryObj<typeof IconButton>
+
+export const Solid: Story = {
+  args: {
+    size: "md",
+    variant: "solid",
+    colorScheme: "primary",
+    isLoading: false,
+    disabled: false,
+  },
+  argTypes: {
+    size: {
+      type: "string",
+      control: "select",
+      options: ["sm", "md", "lg"] satisfies IconButtonProps["size"][],
+    },
+    colorScheme: {
+      type: "string",
+      control: "select",
+      options: [
+        "primary",
+        "secondary",
+        "info",
+        "destructive",
+        "success",
+      ] satisfies IconButtonProps["colorScheme"][],
+    },
+    variant: {
+      control: {
+        disable: true,
+      },
+    },
+    disabled: {
+      type: "boolean",
+      control: "boolean",
+    },
+  },
+}
+
+export const Outline: Story = {
+  args: {
+    ...Solid.args,
+    variant: "outline",
+  },
+  argTypes: {
+    ...Solid.argTypes,
+  },
+}
+
+export const Ghost: Story = {
+  args: {
+    ...Solid.args,
+    variant: "ghost",
+  },
+  argTypes: {
+    ...Solid.argTypes,
+  },
+}
+
+export const Subtle: Story = {
+  args: {
+    ...Solid.args,
+    variant: "subtle",
+  },
+  argTypes: {
+    ...Solid.argTypes,
+  },
+}
