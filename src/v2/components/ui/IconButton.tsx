e6d106a1b9ecdb0c2d50ee03f2382853c924c367
BrickheadJohnny <92519134+BrickheadJohnny@users.noreply.github.com>
2024-09-18 11:03:40 +0200

    feat: `IconButton` component & stories (#1493)
    * feat: `IconButton` component & stories
    
    * fix: add `aria-label` attribute to example

---
+++ src/v2/components/ui/IconButton.tsx
@@ -0,0 +1,48 @@
+import { cn } from "@/lib/utils"
+import { VariantProps, cva } from "class-variance-authority"
+import { ReactNode, forwardRef } from "react"
+import { Button, ButtonProps } from "./Button"
+
+const iconButtonVariants = cva("p-0", {
+  variants: {
+    size: {
+      sm: "size-8",
+      md: "size-10",
+      lg: "size-12",
+    },
+  },
+  defaultVariants: {
+    size: "md",
+  },
+})
+
+export interface IconButtonProps
+  extends Omit<
+      ButtonProps,
+      | "size"
+      | "loadingText"
+      | "leftIcon"
+      | "rightIcon"
+      | "asChild"
+      | "children"
+      | "aria-label"
+    >,
+    VariantProps<typeof iconButtonVariants> {
+  "aria-label": string
+  // icon: ButtonProps["leftIcon"] // TODO: uncomment this in the Button refactor PR
+  icon: ReactNode
+}
+
+const IconButton = forwardRef<HTMLButtonElement, IconButtonProps>(
+  ({ size, icon, className, ...props }, ref) => (
+    <Button
+      ref={ref}
+      {...props}
+      className={cn(iconButtonVariants({ size, className }))}
+    >
+      {icon}
+    </Button>
+  )
+)
+
+export { IconButton }
