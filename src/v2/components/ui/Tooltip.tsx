adb8c675450797cfd5d1a6e93a47cb8e2761fac1
BrickheadJohnny <johnny.zgen@gmail.com>
2024-09-24 10:56:28 +0200

    feat(Tooltip): add `popover` variant

--- src/v2/components/ui/Tooltip.tsx
+++ src/v2/components/ui/Tooltip.tsx
@@ -2,7 +2,24 @@
 
 import { cn } from "@/lib/utils"
 import * as TooltipPrimitive from "@radix-ui/react-tooltip"
+import { VariantProps, cva } from "class-variance-authority"
 import { ComponentPropsWithoutRef, ElementRef, FC, forwardRef } from "react"
+
+const tooltipVariants = cva(
+  "fade-in-0 zoom-in-95 data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-tooltip max-w-sm animate-in rounded-xl px-3 py-1.5 text-center font-medium font-sans text-sm shadow-md data-[state=closed]:animate-out",
+  {
+    variants: {
+      variant: {
+        tooltip: "bg-tooltip text-tooltip-foreground [&_svg]:fill-tooltip",
+        popover:
+          "bg-popover text-popover-foreground outline outline-1 outline-border [&_svg]:fill-popover [&_svg]:[filter:drop-shadow(1px_0_0_hsl(var(--border)))_drop-shadow(-1px_0_0_hsl(var(--border)))_drop-shadow(0_1px_0_hsl(var(--border)))]",
+      },
+    },
+    defaultVariants: {
+      variant: "tooltip",
+    },
+  }
+)
 
 const TooltipProvider = TooltipPrimitive.Provider
 
@@ -26,21 +43,22 @@ const TooltipTrigger = forwardRef<
   </TooltipPrimitive.Trigger>
 ))
 
+export interface TooltipContentProps
+  extends ComponentPropsWithoutRef<typeof TooltipPrimitive.Content>,
+    VariantProps<typeof tooltipVariants> {}
+
 const TooltipContent = forwardRef<
   ElementRef<typeof TooltipPrimitive.Content>,
-  ComponentPropsWithoutRef<typeof TooltipPrimitive.Content>
->(({ className, sideOffset = 4, children, ...props }, ref) => (
+  TooltipContentProps
+>(({ className, sideOffset = 4, variant, children, ...props }, ref) => (
   <TooltipPrimitive.Content
     ref={ref}
     sideOffset={sideOffset}
-    className={cn(
-      "fade-in-0 zoom-in-95 data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-tooltip max-w-sm animate-in rounded-xl bg-tooltip px-3 py-1.5 text-center font-medium font-sans text-sm text-tooltip-foreground shadow-md data-[state=closed]:animate-out",
-      className
-    )}
+    className={cn(tooltipVariants({ variant, className }))}
     {...props}
   >
     {children}
-    <TooltipPrimitive.Arrow className="fill-tooltip" />
+    <TooltipPrimitive.Arrow />
   </TooltipPrimitive.Content>
 ))
 TooltipContent.displayName = TooltipPrimitive.Content.displayName
