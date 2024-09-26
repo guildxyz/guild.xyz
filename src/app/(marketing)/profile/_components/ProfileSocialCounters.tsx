db89126b23402ff04dfb651de712bf852be0224f
Dominik Stumpf <122315398+dominik-stumpf@users.noreply.github.com>
2024-09-26 14:38:19 +0200

    add option to view guildmates (#1508)
    * feat: show guildmates and describe it more
    
    * chore: add border to avatar
    
    * fix: generate new package-lock
    
    * refactor: make guildmates modal less apparent
    
    ---------
    
    Co-authored-by: BrickheadJohnny <johnny.zgen@gmail.com>

--- src/app/(marketing)/profile/_components/ProfileSocialCounters.tsx
+++ src/app/(marketing)/profile/_components/ProfileSocialCounters.tsx
@@ -1,8 +1,25 @@
 import FarcasterImage from "@/../static/socialIcons/farcaster.svg"
+import { CopyLink } from "@/components/CopyLink"
+import { ProfileAvatar } from "@/components/ProfileAvatar"
+import { Anchor } from "@/components/ui/Anchor"
+import { Avatar } from "@/components/ui/Avatar"
 import { AvatarGroup } from "@/components/ui/AvatarGroup"
+import { Badge } from "@/components/ui/Badge"
+import {
+  Dialog,
+  DialogBody,
+  DialogCloseButton,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/Dialog"
+import { ScrollArea } from "@/components/ui/ScrollArea"
 import { Separator } from "@/components/ui/Separator"
 import { Skeleton } from "@/components/ui/Skeleton"
 import { cn } from "@/lib/utils"
+import { REFERRER_USER_SEARCH_PARAM_KEY } from "@app/(marketing)/create-profile/(onboarding)/constants"
 import { PropsWithChildren } from "react"
 import { RequiredFields } from "types"
 import pluralize from "utils/pluralize"
@@ -13,6 +30,7 @@ import {
 } from "../_hooks/useFarcasterProfile"
 import { useProfile } from "../_hooks/useProfile"
 import { useReferredUsers } from "../_hooks/useReferredUsers"
+import { ProfileOwnerGuard } from "./ProfileOwnerGuard"
 
 type DisplayableUser = RequiredFields<User, "pfp_url" | "display_name">
 
@@ -24,6 +42,9 @@ export const ProfileSocialCounters = ({ className }: any) => {
   const relevantFollowersFiltered = relevantFollowers?.filter(
     (user) => user && user.pfp_url && user.display_name
   ) as undefined | DisplayableUser[]
+  const inviteLink =
+    profile &&
+    `https://guild.xyz/create-profile/prompt-referrer?${REFERRER_USER_SEARCH_PARAM_KEY}=${profile.username}`
 
   return (
     <div
@@ -33,7 +54,75 @@ export const ProfileSocialCounters = ({ className }: any) => {
       )}
     >
       {referredUsers ? (
-        <SocialCountTile count={referredUsers.length}>Guildmates</SocialCountTile>
+        <Dialog>
+          <DialogTrigger className="group">
+            <SocialCountTile count={referredUsers.length}>
+              <div className="underline decoration-dotted underline-offset-4 transition-colors group-hover:text-foreground">
+                Guildmates
+              </div>
+            </SocialCountTile>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle className="flex items-center gap-3">
+                Guildmates
+                <Badge size="sm" className="font-sans">
+                  {referredUsers.length}
+                </Badge>
+              </DialogTitle>
+              <DialogDescription>
+                <ProfileOwnerGuard reverseLogic>
+                  Profiles created using this referral
+                </ProfileOwnerGuard>
+              </DialogDescription>
+              <DialogCloseButton />
+            </DialogHeader>
+            <DialogBody>
+              <ProfileOwnerGuard>
+                {inviteLink && (
+                  <>
+                    <p className="mb-2 text-pretty font-medium">
+                      Share this link and earn XP for each user who joins:
+                    </p>
+                    <CopyLink href={inviteLink} />
+                    <Separator className="my-8" />
+                  </>
+                )}
+              </ProfileOwnerGuard>
+              <ScrollArea className="h-96">
+                {referredUsers.length ? (
+                  referredUsers.map((user) => (
+                    <div key={user.userId} className="flex items-center gap-2 pb-4">
+                      <Avatar className="border">
+                        <ProfileAvatar
+                          username={user.username}
+                          profileImageUrl={user.profileImageUrl}
+                        />
+                      </Avatar>
+                      <div className="leading-tight">
+                        <div className="max-w-64 truncate">
+                          {user.name || user.username}
+                        </div>
+                        <Anchor
+                          href={`/profile/${user.username}`}
+                          variant="muted"
+                          target="_blank"
+                          className="text-sm"
+                        >
+                          @{user.username}
+                        </Anchor>
+                      </div>
+                    </div>
+                  ))
+                ) : (
+                  <p className="text-muted-foreground">
+                    This profile has no guildmates to show yet.
+                  </p>
+                )}
+              </ScrollArea>
+            </DialogBody>
+          </DialogContent>
+        </Dialog>
       ) : (
         <Skeleton className="h-12 w-20" />
       )}
