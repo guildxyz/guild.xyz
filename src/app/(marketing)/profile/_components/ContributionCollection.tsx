2d96535b62c886df927428a8d9ab6247e9717d47
Dominik Stumpf <122315398+dominik-stumpf@users.noreply.github.com>
2024-09-27 14:04:42 +0200

    feat: add interactivity to profile (#1510)
    * feat: add interactivity to profile
    
    * feat: add hover card for nft and pin
    
    * chore: adjust pin hover card
    
    * chore: adjust pin hover card background
    
    * chore: add time to created at tooltip
    
    * fix: re-generate package-lock
    
    * fix(ContributionCollection): remove code duplication
    
    * chore: inline date formatting
    
    ---------
    
    Co-authored-by: BrickheadJohnny <johnny.zgen@gmail.com>

--- src/app/(marketing)/profile/_components/ContributionCollection.tsx
+++ src/app/(marketing)/profile/_components/ContributionCollection.tsx
@@ -1,22 +1,32 @@
+import { Anchor } from "@/components/ui/Anchor"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/Avatar"
-import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/Tooltip"
-import { GuildReward, Schemas } from "@guildxyz/types"
+import { Badge } from "@/components/ui/Badge"
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@/components/ui/HoverCard"
+import { Guild, GuildReward, Schemas } from "@guildxyz/types"
 import { Ranking } from "@phosphor-icons/react"
+import { ArrowRight } from "@phosphor-icons/react/dist/ssr"
 import { GuildAction } from "components/[guild]/Requirements/components/GuildCheckout/MintGuildPinContext"
 import { env } from "env"
+import Image from "next/image"
+import { ReactNode } from "react"
 import Star from "static/icons/star.svg"
 import useSWRImmutable from "swr/immutable"
+import shortenHex from "utils/shortenHex"
 
 export const ContributionCollection = ({
   collection,
-  guildId,
-}: { collection: Schemas["ContributionCollection"]; guildId: number }) => {
+  guild,
+}: { collection: Schemas["ContributionCollection"]; guild: Guild }) => {
   const collectionPoint = collection.points.at(0)
   const collectionNft = collection.NFTs.at(0)
   const collectionPin = collection.pins.at(0)
   const { data: pinHash } = useSWRImmutable<string>(
     collectionPin
-      ? `/v2/guilds/${guildId}/pin?guildAction=${GuildAction[collectionPin.action]}`
+      ? `/v2/guilds/${guild.id}/pin?guildAction=${GuildAction[collectionPin.action]}`
       : null
   )
   const pin = pinHash ? new URL(pinHash, env.NEXT_PUBLIC_IPFS_GATEWAY) : undefined
@@ -29,33 +39,79 @@ export const ContributionCollection = ({
   return (
     <>
       {pin && (
-        <Tooltip>
-          <TooltipTrigger>
+        <HoverCard openDelay={100} closeDelay={100}>
+          <HoverCardTrigger>
             <Avatar size="lg" className="-ml-3 border-2 border-card">
-              <AvatarImage src={pin.href} alt="avatar" width={32} height={32} />
+              <AvatarImage src={pin.href} alt="guild pin" width={32} height={32} />
               <AvatarFallback />
             </Avatar>
-          </TooltipTrigger>
-          <TooltipContent>Minted Guild Pin</TooltipContent>
-        </Tooltip>
-      )}
-      {collectionNft?.data.imageUrl && (
-        <Tooltip>
-          <TooltipTrigger>
-            <Avatar size="lg" className="-ml-3 border-2 border-card">
-              <AvatarImage
-                src={collectionNft.data.imageUrl}
-                alt="avatar"
+          </HoverCardTrigger>
+          <HoverCardContent className="overflow-hidden bg-card-secondary p-0">
+            <div className="relative grid size-64 place-items-center overflow-hidden bg-background">
+              <Image
+                src={pin.href}
+                className="absolute size-full translate-y-1/2 rotate-180 scale-x-90 scale-y-125 blur-3xl saturate-150"
+                alt="guild pin"
                 width={32}
                 height={32}
               />
+              <Avatar
+                size="lg"
+                className="mt-2.5 size-48 animate-float border"
+                style={{ animationDuration: "4s" }}
+              >
+                <AvatarImage
+                  src={pin.href}
+                  alt="guild pin"
+                  width={192}
+                  height={192}
+                />
+                <AvatarFallback />
+              </Avatar>
+            </div>
+
+            <CollectionItemHoverCardDetails
+              badges={<Badge size="sm">Guild Pin</Badge>}
+              title={`Minted ${guild.name} pin`}
+            />
+          </HoverCardContent>
+        </HoverCard>
+      )}
+      {collectionNft?.data.imageUrl && (
+        <HoverCard openDelay={100} closeDelay={100}>
+          <HoverCardTrigger>
+            <Avatar size="lg" className="-ml-3 border-2 border-card">
+              <AvatarImage
+                src={collectionNft.data.imageUrl}
+                alt={collectionNft.data.name || "nft"}
+                width={256}
+                height={256}
+              />
               <AvatarFallback />
             </Avatar>
-          </TooltipTrigger>
-          <TooltipContent>
-            Minted <strong>{collectionNft.data.name}</strong>
-          </TooltipContent>
-        </Tooltip>
+          </HoverCardTrigger>
+          <HoverCardContent className="overflow-hidden bg-card-secondary p-0">
+            <Image
+              src={collectionNft.data.imageUrl}
+              alt={collectionNft.data.name || "nft"}
+              width={256}
+              height={256}
+            />
+            <CollectionItemHoverCardDetails
+              badges={
+                <>
+                  <Badge size="sm">NFT</Badge>
+                  <Badge size="sm" variant="outline">
+                    {shortenHex(collectionNft.data.contractAddress)}
+                  </Badge>
+                </>
+              }
+              title={collectionNft.data.name ?? "Unknown NFT"}
+              href={`/${guild.urlName}/collect/${collectionNft.data.chain}/${collectionNft.data.contractAddress}`}
+              description={collectionNft.data.description}
+            />
+          </HoverCardContent>
+        </HoverCard>
       )}
       {point && collectionPoint && (
         <>
@@ -88,3 +144,38 @@ export const ContributionCollection = ({
     </>
   )
 }
+
+const CollectionItemHoverCardDetails = ({
+  badges,
+  title,
+  href,
+  description,
+}: {
+  badges: ReactNode
+  title: string
+  href?: string
+  description?: string
+}) => (
+  <article className="flex flex-col gap-2 border-t p-3">
+    <div className="space-x-1">{badges}</div>
+
+    <section className="flex flex-col">
+      <h3 className="font-bold">
+        {href ? (
+          <Anchor href={href} target="_blank" variant="unstyled" className="group">
+            {title}
+            <ArrowRight
+              weight="bold"
+              className="mx-1 mb-0.5 inline opacity-0 transition-opacity group-hover:opacity-100"
+            />
+          </Anchor>
+        ) : (
+          title
+        )}
+      </h3>
+      {!!description && (
+        <p className="text-muted-foreground text-sm leading-normal">{description}</p>
+      )}
+    </section>
+  </article>
+)
