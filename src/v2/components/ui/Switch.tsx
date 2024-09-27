769bd70ab509d5bfd2a66666a6abe1b28948f1d8
BrickheadJohnny <johnny.zgen@gmail.com>
2024-09-27 09:50:32 +0200

    cleanup: remove `* as React` imports

--- src/v2/components/ui/Switch.tsx
+++ src/v2/components/ui/Switch.tsx
@@ -1,13 +1,12 @@
 "use client"
 
-import * as SwitchPrimitives from "@radix-ui/react-switch"
-import * as React from "react"
-
 import { cn } from "@/lib/utils"
+import * as SwitchPrimitives from "@radix-ui/react-switch"
+import { ComponentPropsWithoutRef, ElementRef, forwardRef } from "react"
 
-const Switch = React.forwardRef<
-  React.ElementRef<typeof SwitchPrimitives.Root>,
-  React.ComponentPropsWithoutRef<typeof SwitchPrimitives.Root>
+const Switch = forwardRef<
+  ElementRef<typeof SwitchPrimitives.Root>,
+  ComponentPropsWithoutRef<typeof SwitchPrimitives.Root>
 >(({ className, ...props }, ref) => (
   <SwitchPrimitives.Root
     className={cn(
