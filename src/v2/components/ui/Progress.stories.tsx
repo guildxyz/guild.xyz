f18491a96f7a1624c8ea8dff94975e1ead430dc3
BrickheadJohnny <johnny.zgen@gmail.com>
2024-10-02 11:15:23 +0200

    chore(Progress story): add controllable value prop

--- src/v2/components/ui/Progress.stories.tsx
+++ src/v2/components/ui/Progress.stories.tsx
@@ -1,10 +1,10 @@
 import { Meta } from "@storybook/react"
 import { ProgressIndicator, ProgressRoot } from "./Progress"
 
-export const Progress = () => {
+export const Progress = ({ value }: { value: number }) => {
   return (
     <ProgressRoot>
-      <ProgressIndicator value={0.44} />
+      <ProgressIndicator value={value} />
     </ProgressRoot>
   )
 }
@@ -12,6 +12,9 @@ export const Progress = () => {
 const meta: Meta<typeof Progress> = {
   title: "Design system/Progress",
   component: Progress,
+  args: {
+    value: 0.44,
+  },
 }
 
 export default meta
