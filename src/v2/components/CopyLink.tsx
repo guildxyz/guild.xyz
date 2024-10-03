297a3c02d0d35953c0de4b9ccbde8ef0ef0deaf7
BrickheadJohnny <johnny.zgen@gmail.com>
2024-10-03 17:19:54 +0200

    feat: custom `useCopyToClipboard` hook with timeout

--- src/v2/components/CopyLink.tsx
+++ src/v2/components/CopyLink.tsx
@@ -1,12 +1,11 @@
+import { useCopyToClipboard } from "@/hooks/useCopyToClipboard"
 import { Check, Copy } from "@phosphor-icons/react"
-import { useState } from "react"
-import { useCopyToClipboard } from "usehooks-ts"
 import { IconButton } from "./ui/IconButton"
 import { ScrollArea, ScrollBar } from "./ui/ScrollArea"
 
 export const CopyLink = ({ href }: { href: string }) => {
-  const [_, copy] = useCopyToClipboard()
-  const [showSuccess, setShowSuccess] = useState(false)
+  const { hasCopied, copyToClipboard } = useCopyToClipboard()
+
   return (
     <div className="flex overflow-hidden rounded-lg border bg-secondary">
       <ScrollArea className="whitespace-nowrap px-2 py-2.5 font-mono text-muted-foreground text-sm">
@@ -15,13 +14,9 @@ export const CopyLink = ({ href }: { href: string }) => {
       </ScrollArea>
       <IconButton
         className="flex items-center justify-center rounded-none border-l"
-        onClick={() => {
-          copy(href)
-          setShowSuccess(true)
-          window.setTimeout(() => setShowSuccess(false), 1000)
-        }}
+        onClick={() => copyToClipboard(href)}
         icon={
-          showSuccess ? (
+          hasCopied ? (
             <Check weight="bold" className="text-success-subtle-foreground" />
           ) : (
             <Copy weight="bold" />
