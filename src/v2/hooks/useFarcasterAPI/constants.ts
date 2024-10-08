bab5607a150c2b2ce72437190dd6c58063728cb0
BrickheadJohnny <92519134+BrickheadJohnny@users.noreply.github.com>
2024-10-08 19:12:07 +0200

    feat: proxy neynar api requests (#1519)

--- src/v2/hooks/useFarcasterAPI/constants.ts
+++ src/v2/hooks/useFarcasterAPI/constants.ts
@@ -1,4 +1,4 @@
-export const NEYNAR_BASE_URL = "https://api.neynar.com/v2/farcaster" as const
+export const NEYNAR_BASE_URL = "/api/neynar" as const
 export const NEYNAR_DEFAULT_HEADERS = {
   api_key: "NEYNAR_API_DOCS",
 } as const
