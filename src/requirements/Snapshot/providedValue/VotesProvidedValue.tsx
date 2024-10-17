76d7a94cf3d6b333f7529347fa187e7c11a4b263
BrickheadJohnny <johnny.zgen@gmail.com>
2024-10-17 13:58:00 +0200

    feat(SnapshotRequirement): use Tailwind CSS and Radix UI

--- src/requirements/Snapshot/providedValue/VotesProvidedValue.tsx
+++ src/requirements/Snapshot/providedValue/VotesProvidedValue.tsx
@@ -1,12 +1,18 @@
 import { HStack, Text } from "@chakra-ui/react"
 import type { ProvidedValueDisplayProps } from "requirements/requirementProvidedValues"
-import SnapshotSpaceLink from "../components/SnapshotSpaceLink"
+import {
+  SnapshotSpaceLink,
+  SnapshotSpaceLinkProps,
+} from "../components/SnapshotSpaceLink"
 
 const VotesProvidedValue = ({ requirement }: ProvidedValueDisplayProps) =>
   !!requirement?.data?.space ? (
     <HStack wrap={"wrap"} gap={1}>
       <Text>
-        Number of votes in <SnapshotSpaceLink requirement={requirement} />
+        Number of votes in{" "}
+        <SnapshotSpaceLink
+          requirement={requirement as SnapshotSpaceLinkProps["requirement"]}
+        />
       </Text>
     </HStack>
   ) : (
