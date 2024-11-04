868f6d9d1884b5737d3b33624b260e24fd14b4a9
FBalint <balint@zgen.hu>
2024-11-04 11:42:53 +0100

    fix: use v2 hook

--- src/v2/components/CopyableCorrelationId.tsx
+++ src/v2/components/CopyableCorrelationId.tsx
@@ -1,37 +1,19 @@
-import { Check, Copy, X } from "@phosphor-icons/react"
-import { useState } from "react"
-import { useCopyToClipboard } from "usehooks-ts"
+import { useCopyToClipboard } from "@/hooks/useCopyToClipboard"
+import { Check, Copy } from "@phosphor-icons/react/dist/ssr"
 
 export const CopyableCorrelationId = ({
   correlationId,
 }: { correlationId: string }) => {
-  const [, copyToClipboard] = useCopyToClipboard()
-  const [isCopied, setIsCopied] = useState(false)
-  const [hasFailed, setHasFailed] = useState(false)
-
-  const handleCopy = () => {
-    copyToClipboard(correlationId).then((success) => {
-      if (success) {
-        setIsCopied(true)
-        setHasFailed(false)
-        setTimeout(() => setIsCopied(false), 2000)
-      } else {
-        setHasFailed(true)
-        setTimeout(() => setHasFailed(false), 2000)
-      }
-    })
-  }
+  const { hasCopied, copyToClipboard } = useCopyToClipboard()
 
   return (
     <pre
       className="group/copy flex cursor-pointer flex-row gap-1 text-xs opacity-60 hover:opacity-100"
-      onClick={handleCopy}
+      onClick={() => copyToClipboard(correlationId)}
     >
       {`ID: ${correlationId}`}
-      {isCopied ? (
+      {hasCopied ? (
         <Check size={14} className="text-success" />
-      ) : hasFailed ? (
-        <X size={14} className="text-error" />
       ) : (
         <Copy
           size={14}
