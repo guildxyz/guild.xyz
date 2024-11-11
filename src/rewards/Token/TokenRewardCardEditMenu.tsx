02ca32447d90dbc70e6ce3e230a18a6e4e757441
BrickheadJohnny <92519134+BrickheadJohnny@users.noreply.github.com>
2024-11-11 07:42:20 +0100

    feat(token reward): transfer pool ownership (#1542)
    * feat(TokenRewardCardEditMenu): add "Copy pool ID" button
    
    * feat(token reward): transfer pool ownership
    
    * fix(EditTokenModal): edit snapshot requirement properly
    
    * fix(ClaimTokenButton): get `guildPlatform` from `TokenRewardContext`

--- src/rewards/Token/TokenRewardCardEditMenu.tsx
+++ src/rewards/Token/TokenRewardCardEditMenu.tsx
@@ -1,18 +1,34 @@
+import { useCopyToClipboard } from "@/hooks/useCopyToClipboard"
+import { useDisclosure } from "@/hooks/useDisclosure"
+import { MenuDivider, MenuItem, useColorModeValue } from "@chakra-ui/react"
 import {
-  MenuDivider,
-  MenuItem,
-  useColorModeValue,
-  useDisclosure,
-} from "@chakra-ui/react"
-import { Coin, Pencil, TrashSimple, Wallet } from "@phosphor-icons/react"
+  ArrowsLeftRight,
+  Check,
+  Coin,
+  Copy,
+  Pencil,
+  TrashSimple,
+  Wallet,
+} from "@phosphor-icons/react"
 import EditRewardAvailabilityMenuItem from "components/[guild]/AccessHub/components/EditRewardAvailabilityMenuItem"
 import PlatformCardMenu from "components/[guild]/RolePlatforms/components/PlatformCard/components/PlatformCardMenu"
 import useToast from "hooks/useToast"
+import dynamic from "next/dynamic"
+import { useState } from "react"
 import { GuildPlatform } from "types"
+import { ERC20_SUPPORTED_CHAINS } from "utils/guildCheckout/constants"
+import { useAccount } from "wagmi"
 import EditTokenModal from "./EditTokenModal"
 import FundPoolModal from "./FundPoolModal"
 import RemoveTokenRewardConfirmation from "./RemoveTokenRewardConfirmation"
 import WithdrawPoolModal from "./WithdrawPoolModal"
+import usePool from "./hooks/usePool"
+
+const DynamicTransferPoolOwnershipDialog = dynamic(() =>
+  import("./TransferPoolOwnershipDialog").then(
+    (module) => module.TransferPoolOwnershipDialog
+  )
+)
 
 const TokenRewardCardEditMenu = ({
   guildPlatform,
@@ -38,6 +54,14 @@ const TokenRewardCardEditMenu = ({
   } = useDisclosure()
 
   const {
+    onOpen: transferOwnershipOnOpen,
+    isOpen: transferOwnershipIsOpen,
+    setValue: transferOwnershipSetValue,
+  } = useDisclosure()
+
+  const { copyToClipboard, hasCopied } = useCopyToClipboard()
+
+  const {
     isOpen: deleteIsOpen,
     onOpen: deleteOnOpen,
     onClose: deleteOnClose,
@@ -46,6 +70,18 @@ const TokenRewardCardEditMenu = ({
   const toast = useToast()
 
   const removeColor = useColorModeValue("red.600", "red.300")
+
+  const { address } = useAccount()
+  const { data } = usePool(
+    // TODO: should we use `guildPlatform.platformGuildData.contractAddress` here instead?
+    guildPlatform.platformGuildData
+      ?.chain as (typeof ERC20_SUPPORTED_CHAINS)[number],
+    BigInt(guildPlatform.platformGuildData?.poolId ?? "0") // We'll never use this fallback, since poolId is defined at this point
+  )
+
+  const isPoolOwner = data?.owner?.toLowerCase() === address?.toLowerCase()
+
+  const [shouldHideTransferButton, setShouldHideTransferButton] = useState(false)
 
   return (
     <>
@@ -62,6 +98,21 @@ const TokenRewardCardEditMenu = ({
         </MenuItem>
         <MenuItem icon={<Wallet />} onClick={withdrawOnOpen}>
           Withdraw from pool
+        </MenuItem>
+        {isPoolOwner && !shouldHideTransferButton && (
+          <MenuItem icon={<ArrowsLeftRight />} onClick={transferOwnershipOnOpen}>
+            Transfer pool ownership
+          </MenuItem>
+        )}
+        <MenuItem
+          icon={hasCopied ? <Check /> : <Copy />}
+          onClick={() =>
+            copyToClipboard(
+              guildPlatform.platformGuildData?.poolId?.toString() ?? "Unknown pool"
+            )
+          }
+        >
+          Copy pool ID
         </MenuItem>
         <MenuDivider />
         <MenuItem icon={<TrashSimple />} onClick={deleteOnOpen} color={removeColor}>
@@ -95,6 +146,17 @@ const TokenRewardCardEditMenu = ({
       />
 
       <EditTokenModal onClose={editOnClose} isOpen={editIsOpen} />
+
+      {isPoolOwner && (
+        <DynamicTransferPoolOwnershipDialog
+          open={transferOwnershipIsOpen}
+          onOpenChange={transferOwnershipSetValue}
+          /**
+           * The proper way of doing this would be to wait for the TX receipt when transferring ownership, then mutate `usePool`'s data, but this will work too for now
+           */
+          onSuccess={() => setShouldHideTransferButton(true)}
+        />
+      )}
     </>
   )
 }
