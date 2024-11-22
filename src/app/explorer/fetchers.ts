398c40d2d56f19bd625be890d3a8131c80e238f2
BrickheadJohnny <johnny.zgen@gmail.com>
2024-11-22 08:55:48 +0100

    fix(getGuildSearch): throw on error

--- src/app/explorer/fetchers.ts
+++ src/app/explorer/fetchers.ts
@@ -4,9 +4,12 @@ import { PAGE_SIZE } from "./constants";
 export const getGuildSearch =
   (search = "") =>
   async ({ pageParam }: { pageParam: number }) => {
-    return (
-      await fetch(
-        `${env.NEXT_PUBLIC_API}/guild/search?page=${pageParam}&pageSize=${PAGE_SIZE}&search=${search}`,
-      )
-    ).json() as Promise<PaginatedResponse<Guild>>;
+    const res = await fetch(
+      `${env.NEXT_PUBLIC_API}/guild/search?page=${pageParam}&pageSize=${PAGE_SIZE}&search=${search}`,
+    );
+    const json = await res.json();
+
+    if (json.error) throw new Error(json.error);
+
+    return json as PaginatedResponse<Guild>;
   };
