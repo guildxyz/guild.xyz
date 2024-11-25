226b023965bc7e6089c9ab3418bcc9b99648e068
BrickheadJohnny <johnny.zgen@gmail.com>
2024-11-25 12:35:52 +0100

    feat: add background image

--- src/app/create-guild/page.tsx
+++ src/app/create-guild/page.tsx
@@ -2,6 +2,7 @@ import { AuthBoundary } from "@/components/AuthBoundary";
 import { ConfettiProvider } from "@/components/ConfettiProvider";
 import { SignInButton } from "@/components/SignInButton";
 import { Card } from "@/components/ui/Card";
+import svgToTinyDataUri from "mini-svg-data-uri";
 import { CreateGuildButton } from "./components/CreateGuildButton";
 import { CreateGuildForm } from "./components/CreateGuildForm";
 import { CreateGuildFormProvider } from "./components/CreateGuildFormProvider";
@@ -11,8 +12,16 @@ export const metadata = {
 };
 
 const CreateGuild = () => (
-  <main className="container mx-auto grid max-w-lg gap-8 px-4 py-8">
-    {/* TODO: make a common layout component & use it here too */}
+  <main className="container mx-auto grid max-w-lg gap-8 px-4 py-16">
+    <div
+      className="-z-10 absolute inset-0 opacity-40 dark:opacity-60"
+      style={{
+        background: `radial-gradient(ellipse at center, transparent -250%, var(--background) 80%), url("${svgToTinyDataUri(
+          `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="30" height="30" fill="none" stroke="#666"><path d="M0 .5H31.5V32"/></svg>`,
+        )}")`,
+      }}
+    />
+
     <CreateGuildFormProvider>
       <ConfettiProvider>
         <Card className="flex flex-col px-5 py-6 shadow-lg md:px-6">
