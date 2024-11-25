b05474dc08aa2fd0d63c4253555f16625b4f0b70
Dominik Stumpf <122315398+dominik-stumpf@users.noreply.github.com>
2024-11-25 16:26:59 +0100

    add static pages (#1557)
    * feat: move in static pages and typography
    
    * feat: add public prose and static pages
    
    * chore: exclude node from props
    
    * docs: fix spacing and headers

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,6 @@
 import type { Config } from "tailwindcss"
 import animatePlugin from "tailwindcss-animate";
+import typography from "@tailwindcss/typography"
 
 const config = {
   darkMode: ["class"],
@@ -73,7 +74,7 @@ const config = {
       },
     },
   },
-  plugins: [animatePlugin],
+  plugins: [animatePlugin, typography],
 } satisfies Config
 
 export default config
