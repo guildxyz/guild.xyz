2b50af133d6f14df87093048f882e870fa67c7ba
BrickheadJohnny <johnny.zgen@gmail.com>
2024-11-25 18:21:35 +0100

    fix: update environment variables

--- src/config/pinata.server.ts
+++ src/config/pinata.server.ts
@@ -4,6 +4,6 @@ import { env } from "@/lib/env";
 import { PinataSDK } from "pinata-web3";
 
 export const pinata = new PinataSDK({
-  pinataJwt: `${env.PINATA_JWT}`,
+  pinataJwt: `${env.PINATA_ADMIN_JWT}`,
   pinataGateway: `${env.NEXT_PUBLIC_PINATA_GATEWAY_URL}`,
 });
