15a967efe348c4532c2486783ba6d955565d244b
Dominik Stumpf <s.dominik.work@gmail.com>
2024-11-25 21:16:21 +0100

    test: add storybook for missing ui components

---
+++ src/stories/Anchor.stories.tsx
@@ -0,0 +1,69 @@
+import { Anchor } from "@/components/ui/Anchor";
+import type { Meta, StoryObj } from "@storybook/react";
+
+const meta: Meta<typeof Anchor> = {
+  title: "Design system/Anchor",
+  component: Anchor,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Anchor>;
+
+export const Default: Story = {
+  args: {
+    children: "Default",
+    href: "#",
+    variant: "default",
+    showExternal: false,
+  },
+  argTypes: {
+    variant: {
+      control: {
+        disable: true,
+      },
+    },
+    showExternal: {
+      type: "boolean",
+      control: "boolean",
+    },
+    asChild: {
+      control: {
+        disable: true,
+      },
+    },
+  },
+};
+
+export const Muted: Story = {
+  args: {
+    ...Default.args,
+    variant: "muted",
+    children: "Muted",
+  },
+  argTypes: {
+    ...Default.argTypes,
+  },
+};
+
+export const Highlighted: Story = {
+  args: {
+    ...Default.args,
+    variant: "highlighted",
+    children: "Highlighted",
+  },
+  argTypes: {
+    ...Default.argTypes,
+  },
+};
+
+export const Unstyled: Story = {
+  args: {
+    ...Default.args,
+    variant: "unstyled",
+    children: "Unstyled",
+  },
+  argTypes: {
+    ...Default.argTypes,
+  },
+};
