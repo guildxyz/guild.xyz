b185823d0cc5a0158909834abdf70b2c420f406a
BrickheadJohnny <johnny.zgen@gmail.com>
2024-11-26 15:07:02 +0100

    fix: ImageUrlSchema

--- src/lib/schemas/common.ts
+++ src/lib/schemas/common.ts
@@ -4,7 +4,7 @@ export const NameSchema = z
   .string()
   .max(255, "Maximum name length is 255 characters");
 
-export const ImageUrlSchema = z.string().url().max(255);
+export const ImageUrlSchema = z.literal("").or(z.string().url().max(255));
 
 export const LogicSchema = z.enum(["AND", "OR", "ANY_OF"]);
 
