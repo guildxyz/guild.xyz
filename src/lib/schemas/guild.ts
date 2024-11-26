d6bab0adf7c944a5ab5b53ff9153cfff5c2555d1
BrickheadJohnny <92519134+BrickheadJohnny@users.noreply.github.com>
2024-11-26 14:42:33 +0100

    feat: Guild, Role, RoleGroup schemas (#1567)
    * feat: Guild, Role, RoleGroup schemas
    
    * fix: use `.nullish()`
    
    * fix: use `.nonnegative()` for memberCount and roleCount
    
    * fix: change `groupId`'s type to UUID

--- src/lib/schemas/guild.ts
+++ src/lib/schemas/guild.ts
@@ -1,12 +1,11 @@
 import { z } from "zod";
+import { DateLike, ImageUrlSchema, NameSchema } from "./common";
 
 export const CreateGuildSchema = z.object({
-  name: z
-    .string()
-    .min(1, "You must specify a name for your guild")
-    .max(255, "Maximum name length is 255 characters"),
+  name: NameSchema.min(1, "You must specify a name for your guild"),
   urlName: z.string().max(255).optional(),
-  imageUrl: z.string().max(255).optional(),
+  imageUrl: ImageUrlSchema.optional(),
+  description: z.string().optional(),
   contact: z.string().email(),
 });
 
@@ -14,6 +13,10 @@ export type CreateGuildForm = z.infer<typeof CreateGuildSchema>;
 
 const GuildSchema = CreateGuildSchema.extend({
   id: z.string().uuid(),
+  createdAt: DateLike,
+  updatedAt: DateLike,
+  roleCount: z.number().nonnegative(),
+  memberCount: z.number().nonnegative(),
 });
 
 export type Guild = z.infer<typeof GuildSchema>;
---
+++ src/lib/schemas/roleGroup.ts
@@ -0,0 +1,18 @@
+import { z } from "zod";
+import { DateLike, ImageUrlSchema, NameSchema } from "./common";
+
+const CreateRoleGroupSchema = z.object({
+  name: NameSchema.min(1, "You must specify a name for the role group"),
+  description: z.string().nullish(),
+  imageUrl: ImageUrlSchema.nullish(),
+});
+
+export type CreateRoleGroupForm = z.infer<typeof CreateRoleGroupSchema>;
+
+const RoleGroupSchema = CreateRoleGroupSchema.extend({
+  id: z.string().uuid(),
+  createdAt: DateLike,
+  updatedAt: DateLike,
+});
+
+export type RoleGroup = z.infer<typeof RoleGroupSchema>;
