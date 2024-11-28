b6bea92dd9ddb47a38c935fab0155c430901d1c7
BrickheadJohnny <johnny.zgen@gmail.com>
2024-11-28 10:51:57 +0100

    feat: simplify the DataBlock component

--- src/components/requirements/DataBlock.tsx
+++ src/components/requirements/DataBlock.tsx
@@ -1,9 +1,4 @@
 import { Skeleton } from "@/components/ui/Skeleton";
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipTrigger,
-} from "@/components/ui/Tooltip";
 import { Warning } from "@phosphor-icons/react/dist/ssr";
 import type { PropsWithChildren } from "react";
 
@@ -20,21 +15,14 @@ export const DataBlock = ({
   if (isLoading) return <Skeleton className="inline w-24" />;
 
   return (
-    <Tooltip open={error ? undefined : false}>
-      <TooltipTrigger asChild>
-        <span className="break-words rounded-md bg-blackAlpha px-1.5 py-0.5 text-sm dark:bg-blackAlpha-hard">
-          {error && (
-            <Warning
-              weight="bold"
-              className="-top-px relative mr-1.5 inline-flex text-icon-warning"
-            />
-          )}
-          <span className="font-mono">{children}</span>
-        </span>
-      </TooltipTrigger>
-      <TooltipContent side="top">
-        <p>{error}</p>
-      </TooltipContent>
-    </Tooltip>
+    <span className="break-words rounded-md bg-blackAlpha px-1.5 py-0.5 text-sm dark:bg-blackAlpha-hard">
+      {error && (
+        <Warning
+          weight="bold"
+          className="-top-px relative mr-1.5 inline-flex text-icon-warning"
+        />
+      )}
+      <span className="font-mono">{children}</span>
+    </span>
   );
 };
--- src/stories/requirements/DataBlock.stories.tsx
+++ src/stories/requirements/DataBlock.stories.tsx
@@ -1,19 +1,16 @@
 import { DataBlock } from "@/components/requirements/DataBlock";
 import { Card } from "@/components/ui/Card";
-import { TooltipProvider } from "@/components/ui/Tooltip";
 import type { Meta, StoryObj } from "@storybook/react";
 
 const meta: Meta<typeof DataBlock> = {
   title: "Requirement building blocks/DataBlock",
   component: DataBlock,
   decorators: (Story) => (
-    <TooltipProvider>
-      <Card className="p-4">
-        <span>{"This is a "}</span>
-        <Story />
-        <span>{" example"}</span>
-      </Card>
-    </TooltipProvider>
+    <Card className="p-4">
+      <span>{"This is a "}</span>
+      <Story />
+      <span>{" example"}</span>
+    </Card>
   ),
 };
 
@@ -24,14 +21,5 @@ type Story = StoryObj<typeof DataBlock>;
 export const Default: Story = {
   args: {
     children: "DataBlock",
-    error: "",
-  },
-};
-
-export const ErrorState: Story = {
-  name: "Error state",
-  args: {
-    ...Default.args,
-    error: "An error occurred",
   },
 };
