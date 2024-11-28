dcbf40c0ba6ed08873bd9943f9188188cb4f1eef
BrickheadJohnny <johnny.zgen@gmail.com>
2024-11-28 11:57:10 +0100

    cleanup(DataBlock): remove unused stuff

--- src/components/requirements/DataBlock.tsx
+++ src/components/requirements/DataBlock.tsx
@@ -1,28 +1,9 @@
-import { Skeleton } from "@/components/ui/Skeleton";
-import { Warning } from "@phosphor-icons/react/dist/ssr";
 import type { PropsWithChildren } from "react";
 
-type Props = {
-  isLoading?: boolean;
-  error?: string;
-};
-
-export const DataBlock = ({
-  isLoading,
-  error,
-  children,
-}: PropsWithChildren<Props>): JSX.Element => {
-  if (isLoading) return <Skeleton className="inline w-24" />;
-
+export const DataBlock = ({ children }: PropsWithChildren): JSX.Element => {
   return (
-    <span className="break-words rounded-md bg-blackAlpha px-1.5 py-0.5 text-sm dark:bg-blackAlpha-hard">
-      {error && (
-        <Warning
-          weight="bold"
-          className="-top-px relative mr-1.5 inline-flex text-icon-warning"
-        />
-      )}
-      <span className="font-mono">{children}</span>
+    <span className="break-words rounded-md bg-blackAlpha px-1.5 py-0.5 font-mono text-sm dark:bg-blackAlpha-hard">
+      {children}
     </span>
   );
 };
