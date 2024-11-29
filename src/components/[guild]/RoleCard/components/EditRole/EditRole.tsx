9e3c9953df90e1cc02659e215e2029e071c1cd04
BrickheadJohnny <johnny.zgen@gmail.com>
2024-11-29 08:23:36 +0100

    feat: load edit role drawer on click

--- src/components/[guild]/RoleCard/components/EditRole/EditRole.tsx
+++ src/components/[guild]/RoleCard/components/EditRole/EditRole.tsx
@@ -1,143 +1,61 @@
-import {
-  Box,
-  Drawer,
-  DrawerBody,
-  DrawerContent,
-  DrawerOverlay,
-  FormLabel,
-  HStack,
-  Icon,
-  IconButton,
-  VStack,
-  useDisclosure,
-} from "@chakra-ui/react"
+import { Icon, IconButton, IconButtonProps, useDisclosure } from "@chakra-ui/react"
 import { PencilSimple } from "@phosphor-icons/react"
-import { ApiRequirementHandlerProvider } from "components/[guild]/RequirementHandlerContext"
 import useGuild from "components/[guild]/hooks/useGuild"
-import DiscardAlert from "components/common/DiscardAlert"
-import Section from "components/common/Section"
-import Description from "components/create-guild/Description"
-import DynamicDevTool from "components/create-guild/DynamicDevTool"
-import IconSelector from "components/create-guild/IconSelector"
-import Name from "components/create-guild/Name"
-import EditRequirements from "components/create-guild/Requirements/EditRequirements"
-import useWarnIfUnsavedChanges from "hooks/useWarnIfUnsavedChanges"
-import { useRef } from "react"
-import { FormProvider } from "react-hook-form"
-import EditRoleFooter from "./components/EditRoleFooter"
-import EditRoleHeader from "./components/EditRoleHeader"
-import EditRolePlatforms from "./components/EditRolePlatforms"
-import RoleGroupSelect from "./components/RoleGroupSelect"
-import useEditRoleForm from "./hooks/useEditRoleForm"
-import useSubmitEditRole from "./hooks/useSubmitEditRole"
+
+import dynamic from "next/dynamic"
+import { useState } from "react"
+
+const DynamicEditRole = dynamic(
+  () =>
+    import("./components/EditRoleDrawer").then((module) => module.EditRoleDrawer),
+  {
+    ssr: false,
+    loading: () => <EditRoleButton isLoading />,
+  }
+)
+
+const EditRoleButton = ({
+  isLoading,
+  onClick,
+}: Pick<IconButtonProps, "isLoading" | "onClick">) => (
+  <IconButton
+    size="sm"
+    rounded="full"
+    aria-label="Edit role"
+    isLoading={isLoading}
+    onClick={onClick}
+    icon={<Icon as={PencilSimple} />}
+  />
+)
 
 type Props = { roleId: number }
 
-const EditRole = ({ roleId }: Props): JSX.Element => {
-  const { isOpen, onOpen, onClose } = useDisclosure()
-  const btnRef = useRef()
-
-  const methods = useEditRoleForm(roleId)
-  const { control, reset, formState, defaultValues } = methods
-
-  /**
-   * TODO: for some reason, isDirty was true & dirtyFields was an empty object and I
-   * couldn't find the underlying problem, so used this workaround here, but we
-   * should definitely find out what causes this strange behaviour!
-   */
-  const isDirty = Object.values(formState.dirtyFields).length > 0
-  useWarnIfUnsavedChanges(isDirty && !formState.isSubmitted)
-
-  const {
-    isOpen: isAlertOpen,
-    onOpen: onAlertOpen,
-    onClose: onAlertClose,
-  } = useDisclosure()
-
-  const onCloseAndClear = () => {
-    reset(defaultValues)
-    onAlertClose()
-    onClose()
-  }
-
-  const { onSubmit, isLoading, loadingText, iconUploader } = useSubmitEditRole({
-    roleId,
-    methods,
-    onSuccess: onClose,
-  })
-
-  const drawerBodyRef = useRef<HTMLDivElement>()
-
-  return (
-    <>
-      <IconButton
-        ref={btnRef}
-        icon={<Icon as={PencilSimple} />}
-        size="sm"
-        rounded="full"
-        aria-label="Edit role"
-        onClick={onOpen}
-      />
-
-      <Drawer
-        isOpen={isOpen}
-        placement="left"
-        size={{ base: "full", md: "lg" }}
-        onClose={isDirty ? onAlertOpen : onClose}
-        finalFocusRef={btnRef}
-      >
-        <DrawerOverlay />
-        <DrawerContent>
-          <DrawerBody ref={drawerBodyRef} className="custom-scrollbar" pb={28}>
-            <FormProvider {...methods}>
-              <ApiRequirementHandlerProvider roleId={roleId}>
-                <EditRoleHeader onClose={onCloseAndClear} roleId={roleId} />
-                <VStack spacing={10} alignItems="start">
-                  <EditRolePlatforms roleId={roleId} />
-                  <Section title="General">
-                    <Box>
-                      <FormLabel>Logo and name</FormLabel>
-                      <HStack spacing={2} alignItems="start">
-                        <IconSelector uploader={iconUploader} />
-                        <Name />
-                      </HStack>
-                    </Box>
-                    <Description />
-                    <RoleGroupSelect />
-                  </Section>
-
-                  <EditRequirements roleId={roleId} />
-                </VStack>
-              </ApiRequirementHandlerProvider>
-            </FormProvider>
-          </DrawerBody>
-
-          <EditRoleFooter
-            onClose={onCloseAndClear}
-            onSubmit={onSubmit}
-            isLoading={isLoading}
-            loadingText={loadingText}
-            isDirty={isDirty || iconUploader.isUploading}
-          />
-        </DrawerContent>
-        <DynamicDevTool control={control} />
-      </Drawer>
-
-      <DiscardAlert
-        isOpen={isAlertOpen}
-        onClose={onAlertClose}
-        onDiscard={onCloseAndClear}
-      />
-    </>
-  )
-}
-
 const EditRoleWrapper = ({ roleId }: Props) => {
   const { isDetailed } = useGuild()
-  if (!isDetailed)
-    return <IconButton size="sm" rounded="full" aria-label="Edit role" isLoading />
+  const [hasClicked, setHasClicked] = useState(false)
 
-  return <EditRole roleId={roleId} />
+  const { isOpen, onOpen, onClose } = useDisclosure()
+
+  if (!isDetailed) return <EditRoleButton isLoading />
+
+  if (!hasClicked)
+    return (
+      <EditRoleButton
+        onClick={() => {
+          setHasClicked(true)
+          onOpen()
+        }}
+      />
+    )
+
+  return (
+    <DynamicEditRole
+      roleId={roleId}
+      isOpen={isOpen}
+      onOpen={onOpen}
+      onClose={onClose}
+    />
+  )
 }
 
 export default EditRoleWrapper
---
+++ src/components/[guild]/RoleCard/components/EditRole/components/EditRoleDrawer.tsx
@@ -0,0 +1,139 @@
+import {
+  Box,
+  Drawer,
+  DrawerBody,
+  DrawerContent,
+  DrawerOverlay,
+  FormLabel,
+  HStack,
+  Icon,
+  IconButton,
+  VStack,
+  useDisclosure,
+} from "@chakra-ui/react"
+import { PencilSimple } from "@phosphor-icons/react"
+import { ApiRequirementHandlerProvider } from "components/[guild]/RequirementHandlerContext"
+import DiscardAlert from "components/common/DiscardAlert"
+import Section from "components/common/Section"
+import Description from "components/create-guild/Description"
+import DynamicDevTool from "components/create-guild/DynamicDevTool"
+import IconSelector from "components/create-guild/IconSelector"
+import Name from "components/create-guild/Name"
+import EditRequirements from "components/create-guild/Requirements/EditRequirements"
+import useWarnIfUnsavedChanges from "hooks/useWarnIfUnsavedChanges"
+import { useRef } from "react"
+import { FormProvider } from "react-hook-form"
+import useEditRoleForm from "../hooks/useEditRoleForm"
+import useSubmitEditRole from "../hooks/useSubmitEditRole"
+import EditRoleFooter from "./EditRoleFooter"
+import EditRoleHeader from "./EditRoleHeader"
+import EditRolePlatforms from "./EditRolePlatforms"
+import RoleGroupSelect from "./RoleGroupSelect"
+
+type Props = {
+  roleId: number
+  isOpen: boolean
+  onOpen: () => void
+  onClose: () => void
+}
+
+export const EditRoleDrawer = ({
+  roleId,
+  isOpen,
+  onOpen,
+  onClose,
+}: Props): JSX.Element => {
+  const btnRef = useRef<HTMLButtonElement>(null)
+
+  const methods = useEditRoleForm(roleId)
+  const { control, reset, formState, defaultValues } = methods
+
+  /**
+   * TODO: for some reason, isDirty was true & dirtyFields was an empty object and I
+   * couldn't find the underlying problem, so used this workaround here, but we
+   * should definitely find out what causes this strange behaviour!
+   */
+  const isDirty = Object.values(formState.dirtyFields).length > 0
+  useWarnIfUnsavedChanges(isDirty && !formState.isSubmitted)
+
+  const {
+    isOpen: isAlertOpen,
+    onOpen: onAlertOpen,
+    onClose: onAlertClose,
+  } = useDisclosure()
+
+  const onCloseAndClear = () => {
+    reset(defaultValues)
+    onAlertClose()
+    onClose()
+  }
+
+  const { onSubmit, isLoading, loadingText, iconUploader } = useSubmitEditRole({
+    roleId,
+    methods,
+    onSuccess: onClose,
+  })
+
+  return (
+    <>
+      <IconButton
+        ref={btnRef}
+        icon={<Icon as={PencilSimple} />}
+        size="sm"
+        rounded="full"
+        aria-label="Edit role"
+        onClick={onOpen}
+      />
+
+      <Drawer
+        isOpen={isOpen}
+        placement="left"
+        size={{ base: "full", md: "lg" }}
+        onClose={isDirty ? onAlertOpen : onClose}
+        finalFocusRef={btnRef}
+      >
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerBody className="custom-scrollbar" pb={28}>
+            <FormProvider {...methods}>
+              <ApiRequirementHandlerProvider roleId={roleId}>
+                <EditRoleHeader onClose={onCloseAndClear} roleId={roleId} />
+                <VStack spacing={10} alignItems="start">
+                  <EditRolePlatforms roleId={roleId} />
+                  <Section title="General">
+                    <Box>
+                      <FormLabel>Logo and name</FormLabel>
+                      <HStack spacing={2} alignItems="start">
+                        <IconSelector uploader={iconUploader} />
+                        <Name />
+                      </HStack>
+                    </Box>
+                    <Description />
+                    <RoleGroupSelect />
+                  </Section>
+
+                  <EditRequirements roleId={roleId} />
+                </VStack>
+              </ApiRequirementHandlerProvider>
+            </FormProvider>
+          </DrawerBody>
+
+          <EditRoleFooter
+            onClose={onCloseAndClear}
+            onSubmit={onSubmit}
+            isLoading={isLoading}
+            loadingText={loadingText}
+            isDirty={isDirty || iconUploader.isUploading}
+          />
+        </DrawerContent>
+        <DynamicDevTool control={control} />
+      </Drawer>
+
+      <DiscardAlert
+        isOpen={isAlertOpen}
+        onClose={onAlertClose}
+        onDiscard={onCloseAndClear}
+      />
+    </>
+  )
+}
