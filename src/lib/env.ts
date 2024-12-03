2cec819462fbdec48104a0c06258e91184da1e30
Dominik Stumpf <s.dominik.work@gmail.com>
2024-12-03 02:14:53 +0100

    fix: add default for LOGGING env var and make it shared

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -4,18 +4,18 @@ import { z } from "zod";
 export const env = createEnv({
   server: {
     PINATA_ADMIN_JWT: z.string(),
-    LOGGING: z.coerce.number().nonnegative().int().max(1),
+  },
+  shared: {
+    LOGGING: z.coerce.number().nonnegative().int().max(1).default(0),
   },
   client: {
     NEXT_PUBLIC_API: z.string().url(),
     NEXT_PUBLIC_PINATA_GATEWAY_URL: z.string().url(),
-    NEXT_PUBLIC_LOGGING: z.coerce.number().nonnegative().int().max(1),
   },
   runtimeEnv: {
     NEXT_PUBLIC_API: process.env.NEXT_PUBLIC_API_V3,
     PINATA_ADMIN_JWT: process.env.PINATA_ADMIN_JWT,
     NEXT_PUBLIC_PINATA_GATEWAY_URL: process.env.NEXT_PUBLIC_PINATA_GATEWAY_URL,
-    NEXT_PUBLIC_LOGGING: process.env.LOGGING,
     LOGGING: process.env.LOGGING,
   },
 });
