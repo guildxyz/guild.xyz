b215b4eb7ed16668bc693cae2075c63e14515b78
Dominik Stumpf <s.dominik.work@gmail.com>
2024-12-03 02:07:31 +0100

    chore: expose LOGGING env var to client

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -9,11 +9,13 @@ export const env = createEnv({
   client: {
     NEXT_PUBLIC_API: z.string().url(),
     NEXT_PUBLIC_PINATA_GATEWAY_URL: z.string().url(),
+    NEXT_PUBLIC_LOGGING: z.coerce.number().nonnegative().int().max(1),
   },
   runtimeEnv: {
     NEXT_PUBLIC_API: process.env.NEXT_PUBLIC_API_V3,
     PINATA_ADMIN_JWT: process.env.PINATA_ADMIN_JWT,
     NEXT_PUBLIC_PINATA_GATEWAY_URL: process.env.NEXT_PUBLIC_PINATA_GATEWAY_URL,
+    NEXT_PUBLIC_LOGGING: process.env.LOGGING,
     LOGGING: process.env.LOGGING,
   },
 });
