8c9d24d0325bc35329f2cb9a4c4da2655311a608
BrickheadJohnny <johnny.zgen@gmail.com>
2025-01-10 15:45:53 +0100

    cleanup: remove todo comment

--- src/app/(dashboard)/[guildUrlName]/page.tsx
+++ src/app/(dashboard)/[guildUrlName]/page.tsx
@@ -1,7 +1,6 @@
 import type { DynamicRoute } from "@/lib/types";
 import GuildPage from "./[pageUrlName]/page";
 
-// TODO: we could use catch-all route here?
 const DefaultGuildPage = async (
   props: DynamicRoute<{ guildUrlName: string; pageUrlName: string }>,
 ) => {
