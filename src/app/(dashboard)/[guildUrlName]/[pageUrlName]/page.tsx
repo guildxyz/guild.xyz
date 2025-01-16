416720cca7d9ca257ccb011e2a816c69df301fde
BrickheadJohnny <johnny.zgen@gmail.com>
2025-01-16 15:14:04 +0100

    feat: collapsible role description and scrollable requirements

--- src/app/(dashboard)/[guildUrlName]/[pageUrlName]/page.tsx
+++ src/app/(dashboard)/[guildUrlName]/[pageUrlName]/page.tsx
@@ -5,13 +5,21 @@ import { rewardCards } from "@/components/rewards/rewardCards";
 import { Badge } from "@/components/ui/Badge";
 import { Button, buttonVariants } from "@/components/ui/Button";
 import { Card } from "@/components/ui/Card";
+import { ScrollArea } from "@/components/ui/ScrollArea";
 import { Skeleton } from "@/components/ui/Skeleton";
+import { useDisclosure } from "@/hooks/useDisclosure";
+import { useMeasure } from "@/hooks/useMeasure";
 import { useUser } from "@/hooks/useUser";
 import { cn } from "@/lib/cssUtils";
 import { fetchGuildApiData } from "@/lib/fetchGuildApi";
 import type { GuildReward, GuildRewardType } from "@/lib/schemas/guildReward";
 import type { Role } from "@/lib/schemas/role";
-import { Check, ImageSquare, LockSimple } from "@phosphor-icons/react/dist/ssr";
+import {
+  ArrowDown,
+  Check,
+  ImageSquare,
+  LockSimple,
+} from "@phosphor-icons/react/dist/ssr";
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { useSetAtom } from "jotai";
 import { Suspense } from "react";
@@ -37,9 +45,9 @@ const GuildPage = () => {
 };
 
 const RoleCard = ({ role }: { role: Role }) => (
-  <Card className="flex flex-col md:flex-row" key={role.id}>
-    <div className="@container flex flex-col border-r p-5 md:w-1/2">
-      <div className="mb-2 flex items-center gap-3">
+  <Card className="grid md:grid-cols-2" key={role.id}>
+    <div className="@container flex flex-col border-r md:min-h-[22rem]">
+      <div className="flex items-center gap-3 p-5">
         {role.imageUrl ? (
           <img
             className="size-14 rounded-full border"
@@ -53,38 +61,111 @@ const RoleCard = ({ role }: { role: Role }) => (
         )}
         <h3 className="font-extrabold text-xl">{role.name}</h3>
       </div>
-      <p className="mb-4 text-foreground-dimmed leading-relaxed">
-        {role.description}
-      </p>
+
+      <RoleDescription role={role} />
 
       <Suspense fallback={<p>Loading rewards...</p>}>
         <RoleRewards roleId={role.id} roleRewards={role.rewards} />
       </Suspense>
     </div>
 
-    <div className="bg-card-secondary md:w-1/2">
+    <div className="flex flex-col bg-card-secondary">
       <div className="flex items-center justify-between p-5">
-        <span className="font-bold text-foreground-secondary text-xs">
-          REQUIREMENTS
+        <span className="font-bold text-foreground-secondary text-xs uppercase md:hidden lg:inline">
+          Requirements
         </span>
 
         <AccessIndicator roleId={role.id} className="hidden sm:flex" />
       </div>
 
       {/* TODO group rules by access groups */}
-      <div className="grid px-5 pb-5">
-        {role.accessGroups[0].rules?.map((rule) => (
-          <RequirementDisplayComponent
-            key={rule.accessRuleId}
-            requirement={rule}
-          />
-        ))}
-      </div>
+      <ScrollArea
+        className={cn(
+          "scroll-shadow relative flex w-full flex-grow basis-80 flex-col overflow-y-auto md:basis-0",
+          {
+            // "basis-full": (requirements?.length ?? 0) < 3,
+            "basis-full": true,
+          },
+        )}
+      >
+        <div className="grid px-5 pb-5">
+          {role.accessGroups[0].rules?.map((rule) => (
+            <RequirementDisplayComponent
+              key={rule.accessRuleId}
+              requirement={rule}
+            />
+          ))}
+        </div>
+      </ScrollArea>
 
       <AccessIndicator roleId={role.id} className="sm:hidden" />
     </div>
   </Card>
 );
+
+const MAX_INITIAL_DESCRIPTION_HEIGHT_WITH_REWARDS = 192; // 12rem
+const MAX_INITIAL_DESCRIPTION_HEIGHT = 260; // 16.25rem (22rem - 92px, the height of the role card's header)
+const RoleDescription = ({ role }: { role: Role }) => {
+  const maxDescriptionHeight =
+    role.rewards?.length > 0
+      ? MAX_INITIAL_DESCRIPTION_HEIGHT_WITH_REWARDS
+      : MAX_INITIAL_DESCRIPTION_HEIGHT;
+
+  const { ref, bounds } = useMeasure<HTMLDivElement>();
+
+  const shouldShowViewMoreButton =
+    !!bounds && bounds.height > maxDescriptionHeight;
+
+  const { isOpen, onToggle } = useDisclosure();
+
+  return (
+    <div
+      className="group relative overflow-hidden px-5 pb-4 text-foreground-dimmed leading-relaxed transition-all"
+      style={
+        shouldShowViewMoreButton
+          ? {
+              // + 16 is pb-4 (1rem)
+              height: isOpen ? bounds.height + 16 : maxDescriptionHeight,
+              maxHeight: "none",
+            }
+          : {
+              // Defining an initial max height to avoid a jump on initial load
+              maxHeight: maxDescriptionHeight,
+            }
+      }
+    >
+      <p ref={ref}>{role.description}</p>
+
+      {shouldShowViewMoreButton && (
+        <div
+          className={cn(
+            "absolute bottom-0 left-0 flex w-full justify-center break-words bg-gradient-to-t from-black/10 to-transparent pb-2 transition-colors dark:from-black/25",
+            {
+              "from-transparent dark:from-transparent": isOpen,
+            },
+          )}
+        >
+          <Card className="opacity-0 transition-opacity group-hover:opacity-100">
+            <Button
+              size="xs"
+              onClick={() => onToggle()}
+              rightIcon={
+                <ArrowDown
+                  weight="bold"
+                  className={cn({
+                    "-rotate-180": isOpen,
+                  })}
+                />
+              }
+            >
+              {isOpen ? "Collapse" : "Click to expand"}
+            </Button>
+          </Card>
+        </div>
+      )}
+    </div>
+  );
+};
 
 const RoleRewards = ({
   roleId,
@@ -100,7 +181,7 @@ const RoleRewards = ({
   });
 
   return roleRewards?.length > 0 && rewards?.length > 0 ? (
-    <div className="mt-auto grid @[26rem]:grid-cols-2 gap-2">
+    <div className="mt-auto grid @[26rem]:grid-cols-2 gap-2 p-5">
       {roleRewards.map((roleReward) => {
         const guildReward = rewards.find((gr) => gr.id === roleReward.rewardId);
         if (!guildReward) return null;
@@ -132,7 +213,7 @@ const RoleRewards = ({
 
 // TODO: handle state during join & error/no access states too
 const ACCESS_INDICATOR_CLASS =
-  "rounded-b-2xl rounded-t-none min-w-full justify-between sm:rounded-b-xl sm:rounded-t-xl sm:min-w-max";
+  "rounded-b-2xl rounded-t-none min-w-full justify-between sm:rounded-b-xl sm:rounded-t-xl sm:min-w-max h-8 shrink-0 ml-auto";
 const AccessIndicator = ({
   roleId,
   className,
