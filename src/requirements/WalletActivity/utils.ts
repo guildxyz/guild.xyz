cad2c20ffcdf692ebc5d5aae38295205f2e42c16
BrickheadJohnny <johnny.zgen@gmail.com>
2025-03-18 16:59:12 +0100

    fix(abiItemToFunctionSignature): handle `tuple` type

--- src/requirements/WalletActivity/utils.ts
+++ src/requirements/WalletActivity/utils.ts
@@ -1,11 +1,19 @@
-import { AbiFunction } from "viem"
+import { AbiFunction, AbiParameter } from "viem"
+
+const getType = (param: AbiParameter) => {
+  if (param.type === "tuple" && "components" in param) {
+    return `(${param.components.map((component) => `${component.name ? `${component.name} ` : ""}${component.type}`).join(", ")})`
+  }
+
+  return param.type
+}
 
 export const abiItemToFunctionSignature = (
   item: AbiFunction,
   mode: "PARAM_TYPES" | "PARAM_NAMES" = "PARAM_TYPES"
 ) => {
   if (mode === "PARAM_NAMES")
-    return `${item.name}(${item.inputs.map((input) => `${input.name ? `${input.name} ` : ""}${input.type}`).join(", ")})`
+    return `${item.name}(${item.inputs.map((input) => `${input.name ? `${input.name} ` : ""}${getType(input)}`).join(", ")})`
 
-  return `${item.name}(${item.inputs.map((input) => input.type).join(", ")})`
+  return `${item.name}(${item.inputs.map((input) => getType(input)).join(", ")})`
 }
