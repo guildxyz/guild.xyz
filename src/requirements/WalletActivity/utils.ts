90215b4198ba06f4ead8f2bbb3074ad2f9ec5795
BrickheadJohnny <johnny.zgen@gmail.com>
2025-03-18 20:34:08 +0100

    fix(abiItemToFunctionSignature - getType): only list param types

--- src/requirements/WalletActivity/utils.ts
+++ src/requirements/WalletActivity/utils.ts
@@ -2,7 +2,7 @@ import { AbiFunction, AbiParameter } from "viem"
 
 const getType = (param: AbiParameter) => {
   if (param.type === "tuple" && "components" in param) {
-    return `(${param.components.map((component) => `${component.name ? `${component.name} ` : ""}${component.type}`).join(", ")})`
+    return `(${param.components.map((component) => component.type).join(", ")})`
   }
 
   return param.type
