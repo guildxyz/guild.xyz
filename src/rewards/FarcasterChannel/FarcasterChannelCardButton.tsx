6b6437b3166b7ee2df9efe713730cc251ab986ea
BrickheadJohnny <johnny.zgen@gmail.com>
2025-03-20 16:35:01 +0100

    fix: improve `FarcasterChannelCardButton` based on feedback

--- src/rewards/FarcasterChannel/FarcasterChannelCardButton.tsx
+++ src/rewards/FarcasterChannel/FarcasterChannelCardButton.tsx
@@ -1,20 +1,30 @@
-import useGuild from "components/[guild]/hooks/useGuild"
+import { ToastAction } from "@/components/ui/Toast"
 import { useRolePlatform } from "components/[guild]/RolePlatforms/components/RolePlatformProvider"
+import useGuild from "components/[guild]/hooks/useGuild"
+import useUser from "components/[guild]/hooks/useUser"
 import { useRoleMembership } from "components/explorer/hooks/useMembership"
 import { useClaimedReward } from "hooks/useClaimedReward"
 import { SignedValidation, useSubmitWithSign } from "hooks/useSubmit"
 import { useUserRewards } from "hooks/useUserRewards"
 import { RewardCardButton } from "rewards/components/RewardCardButton"
+import FarcasterLogo from "static/socialIcons/farcaster.svg"
 import fetcher from "utils/fetcher"
-import useShowErrorToast from "hooks/useShowErrorToast"
+import { useErrorToast } from "v2/components/ui/hooks/useErrorToast"
+import { useToast } from "v2/components/ui/hooks/useToast"
 
 const FarcasterChannelCardButton = () => {
+  const { id: userId } = useUser()
+  const { id: guildId, guildPlatforms } = useGuild()
+
   const rolePlatform = useRolePlatform()
+  const guildPlatform = guildPlatforms?.find(
+    (gp) => gp.id === rolePlatform.guildPlatformId
+  )
+
   const { isMember: hasRole } = useRoleMembership(rolePlatform?.roleId)
   const { mutate: mutateUserReward } = useUserRewards()
   const { claimed } = useClaimedReward(rolePlatform?.id)
 
-  const { id: guildId } = useGuild()
   const claimFetcher = (signedValidation: SignedValidation) =>
     fetcher(
       `/v2/guilds/${guildId}/roles/${rolePlatform.roleId}/role-platforms/${rolePlatform.id}/claim`,
@@ -24,27 +34,54 @@ const FarcasterChannelCardButton = () => {
       }
     )
 
-  const showErrorToast = useShowErrorToast()
+  const channelLink = `https://warpcast.com/~/channel/${guildPlatform?.platformGuildId}`
 
+  const { toast } = useToast()
+  const showErrorToast = useErrorToast()
   const { onSubmit: onGetInviteSubmit, isLoading } = useSubmitWithSign(
     claimFetcher,
     {
       onSuccess: () => {
         mutateUserReward()
+        toast({
+          variant: "success",
+          title: "Successfully claimed invite link",
+          action: (
+            <ToastAction altText="Visit channel" asChild>
+              <a href={channelLink} target="_blank">
+                <FarcasterLogo className="size-4" />
+                <span>Visit channel</span>
+              </a>
+            </ToastAction>
+          ),
+        })
       },
       onError: (error) => showErrorToast(error),
     }
   )
 
+  const shouldShowVisitChannel =
+    claimed || userId === guildPlatform?.platformGuildData?.moderatorUserId
+
+  const buttonProps = shouldShowVisitChannel
+    ? ({
+        as: "a",
+        target: "_blank",
+        href: channelLink,
+      } as const)
+    : ({
+        onClick: () => onGetInviteSubmit(),
+      } as const)
+
   return (
     <RewardCardButton
+      {...buttonProps}
       isDisabled={!hasRole}
       colorScheme="FARCASTER"
       isLoading={isLoading}
       loadingText="Getting invite..."
-      onClick={() => onGetInviteSubmit()}
     >
-      {claimed ? "Visit channel" : "Get invite"}
+      {shouldShowVisitChannel ? "Visit channel" : "Get invite"}
     </RewardCardButton>
   )
 }
