1e2a419c14675c3a8724d447ea74d2ab32a74daf
Csete Balázs <csetebalu@gmail.com>
2025-07-04 15:46:12 +0200

    fix(useAlphaGuilds): pass isVerified=true in query param

--- src/app/explorer/consts.ts
+++ src/app/explorer/consts.ts
@@ -1,2 +1,3 @@
 export const ALPHA_GUILDS_SWR_KEY = "alpha-guilds"
-export const ALPHA_GUILDS_API_URL = "https://api-alpha.guild.xyz/guilds"
+export const ALPHA_GUILDS_API_URL =
+  "https://api-alpha.guild.xyz/guilds?isVerified=true"
--- src/app/explorer/hooks/useAlphaGuilds.ts
+++ src/app/explorer/hooks/useAlphaGuilds.ts
@@ -4,8 +4,6 @@ import { ALPHA_GUILDS_API_URL, ALPHA_GUILDS_SWR_KEY } from "../consts"
 import { AlphaGuild } from "../types"
 
 export const useAlphaGuilds = () =>
-  useSWRImmutable(ALPHA_GUILDS_SWR_KEY, () =>
-    fetcher(ALPHA_GUILDS_API_URL).then((guilds: AlphaGuild[]) =>
-      guilds.filter((g) => g.isVerified)
-    )
+  useSWRImmutable<AlphaGuild[]>(ALPHA_GUILDS_SWR_KEY, () =>
+    fetcher(ALPHA_GUILDS_API_URL)
   )
